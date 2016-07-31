2c80b4369074f35dcc7d0dbaaa8118400efeadfa
Fishercoder <fishercoder@gmail.com>
2016-07-31 11:07:00 -0700

    merge for loops

--- MEDIUM/src/medium/UniquePaths.java
+++ MEDIUM/src/medium/UniquePaths.java
@@ -31,6 +31,18 @@ public class UniquePaths {
         return dp[m-1][n-1];
     }
     
+    //and we can actually put the two initialization for loop into the one
+    public int uniquePaths_merged_for_loop(int m, int n) {
+        int[][] dp = new int[m][n];
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 || j == 0) dp[i][j] = 0;
+                else dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+    
     public static void main(String...strings){
         UniquePaths test = new UniquePaths();
         int m = 1;
