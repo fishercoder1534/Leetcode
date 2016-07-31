19b62b4b2152bb4a2f9e37885addc62d006e285a
Fishercoder <fishercoder@gmail.com>
2016-07-31 12:04:10 -0700

    correct typo

--- MEDIUM/src/medium/UniquePaths.java
+++ MEDIUM/src/medium/UniquePaths.java
@@ -36,7 +36,7 @@ public class UniquePaths {
         int[][] dp = new int[m][n];
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if(i == 0 || j == 0) dp[i][j] = 0;
+                if(i == 0 || j == 0) dp[i][j] = 1;
                 else dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
