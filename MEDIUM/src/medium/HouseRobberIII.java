44ba403e08506ae43e3d18b06a7f719785be1ab2
Fishercoder <fishercoder@gmail.com>
2016-08-01 22:14:19 -0700

    house robber III

--- MEDIUM/src/medium/HouseRobberIII.java
+++ MEDIUM/src/medium/HouseRobberIII.java
@@ -5,6 +5,30 @@ import java.util.Map;
 
 import classes.TreeNode;
 
+/**337. House Robber III  QuestionEditorial Solution  My Submissions
+Total Accepted: 19180
+Total Submissions: 49342
+Difficulty: Medium
+The thief has found himself a new place for his thievery again. There is only one entrance to this area, called the "root." Besides the root, each house has one and only one parent house. After a tour, the smart thief realized that "all houses in this place forms a binary tree". It will automatically contact the police if two directly-linked houses were broken into on the same night.
+
+Determine the maximum amount of money the thief can rob tonight without alerting the police.
+
+Example 1:
+     3
+    / \
+   2   3
+    \   \ 
+     3   1
+Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+Example 2:
+     3
+    / \
+   4   5
+  / \   \ 
+ 1   3   1
+Maximum amount of money the thief can rob = 4 + 5 = 9.
+Credits:
+Special thanks to @dietpepsi for adding this problem and creating all test cases.*/
 public class HouseRobberIII {
 
     //simple recursion without cacheing: 1189 ms
