a0f8a9d715057842a31560c94f5fbe7cc657cb1e
Fishercoder <fishercoder@gmail.com>
2016-08-04 08:19:06 -0700

    length of increasing subsequence using binary search

--- Common/src/utils/CommonUtils.java
+++ Common/src/utils/CommonUtils.java
@@ -8,53 +8,61 @@ import classes.ListNode;
 
 public class CommonUtils {
 
-	private static final int DEFAULT_TREE_SIZE = 10;
-	private static final int DEFAULT_UPPER_BOUND = 100;
+    private static final int DEFAULT_TREE_SIZE = 10;
+    private static final int DEFAULT_UPPER_BOUND = 100;
 
-	public static void print(String message) {
-		System.out.print(message);
-	}
-	
-	public static void print(int num) {
-		System.out.print(num);
-	}
+    public static void printArray(int[] nums) {
+        for(int i : nums){
+            System.out.print(i + ", ");
+        }
+        System.out.println();
+    }
+    
+    public static void print(String message) {
+        System.out.print(message);
+    }
 
-	public static void println(String message) {
-		System.out.println(message);
-	}
-	
-	public static void println(int num) {
-		System.out.println(num);
-	}
+    public static void print(int num) {
+        System.out.print(num);
+    }
 
-	public static void println() {
-		System.out.println();
-	}
+    public static void println(String message) {
+        System.out.println(message);
+    }
 
-	//overloaded method to take only one argument
-	public static List<Integer> randomIntArrayGenerator(int size) {
-		return CommonUtils.randomIntArrayGenerator(size, DEFAULT_UPPER_BOUND);
-	}
+    public static void println(int num) {
+        System.out.println(num);
+    }
 
-	//overloaded method to take no argument
-	public static List<Integer> randomIntArrayGenerator() {
-		return CommonUtils.randomIntArrayGenerator(CommonUtils.DEFAULT_TREE_SIZE, DEFAULT_UPPER_BOUND);
-	}
+    public static void println() {
+        System.out.println();
+    }
 
-	//this one has two other overloaded methods as above
-	public static List<Integer> randomIntArrayGenerator(int size, int upperBound) {
-		List<Integer> result = new ArrayList<Integer>();
-		Random random = new Random();
-		for (int i = 0; i < size; i++) {
-			int randomInt = random.nextInt(upperBound);
-			result.add(randomInt);
-			print(String.valueOf(randomInt) + " ");
-		}
-		println();
-		return result;
-	}
-	
-	   //this one has two other overloaded methods as above
+    // overloaded method to take only one argument
+    public static List<Integer> randomIntArrayGenerator(int size) {
+        return CommonUtils.randomIntArrayGenerator(size, DEFAULT_UPPER_BOUND);
+    }
+
+    // overloaded method to take no argument
+    public static List<Integer> randomIntArrayGenerator() {
+        return CommonUtils.randomIntArrayGenerator(CommonUtils.DEFAULT_TREE_SIZE,
+                DEFAULT_UPPER_BOUND);
+    }
+
+    // this one has two other overloaded methods as above
+    public static List<Integer> randomIntArrayGenerator(int size, int upperBound) {
+        List<Integer> result = new ArrayList<Integer>();
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            int randomInt = random.nextInt(upperBound);
+            result.add(randomInt);
+            print(String.valueOf(randomInt) + " ");
+        }
+        println();
+        return result;
+    }
+
+    // this one has two other overloaded methods as above
     public static List<Integer> uniqueIntArrayGenerator(int size) {
         List<Integer> result = new ArrayList<Integer>();
         for (int i = 0; i < size; i++) {
@@ -64,59 +72,59 @@ public class CommonUtils {
         return result;
     }
 
-//	@Notes(context = "I'm assuing only classes in this PACKAGE will call the following two methods, so just leave the modifier as default, i.e. no public, private, or protected.")
-    public
-	static void printWhitespaces(int count) {
-		for (int i = 0; i < count; i++)
-			System.out.print(" ");
-	}
+    // @Notes(context =
+    // "I'm assuing only classes in this PACKAGE will call the following two methods, so just leave the modifier as default, i.e. no public, private, or protected.")
+    public static void printWhitespaces(int count) {
+        for (int i = 0; i < count; i++)
+            System.out.print(" ");
+    }
 
-	public static <T> boolean isAllElementsNull(List<T> list) {
-		for (Object object : list) {
-			if (object != null)
-				return false;
-		}
-	
-		return true;
-	}
-	
-	/**
-	 * If you want to print the reversed list out, you need to return the
-	 * reversed list's head, which was the end node of the original node. using
-	 * the following function.
-	 */
-	public static ListNode reverseList(ListNode head) {
-		if (head == null || head.next == null) {
-			return head;
-		}
-		ListNode previous, current, next;
-		previous = head;
-		current = head.next;
-		while (current != null) {
-			next = current.next;
-			current.next = previous;
-			previous = current;
-			current = next;
-		}
-		head.next = null;
-		return previous;
-	}
+    public static <T> boolean isAllElementsNull(List<T> list) {
+        for (Object object : list) {
+            if (object != null)
+                return false;
+        }
 
-	public static void printList(final ListNode head) {
-	    ListNode temp = head;
-		System.out.println("--------------------------------------------");
-		while (temp != null) {
-			System.out.print(temp.val);
-			temp = temp.next;
-			if(temp != null) System.out.print("->");
-		}
-		System.out.println();
-	}
-	
-	public static void printMatrix(int[][] matrix) {
+        return true;
+    }
+
+    /**
+     * If you want to print the reversed list out, you need to return the reversed list's head,
+     * which was the end node of the original node. using the following function.
+     */
+    public static ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode previous, current, next;
+        previous = head;
+        current = head.next;
+        while (current != null) {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        head.next = null;
+        return previous;
+    }
+
+    public static void printList(final ListNode head) {
+        ListNode temp = head;
+        System.out.println("--------------------------------------------");
+        while (temp != null) {
+            System.out.print(temp.val);
+            temp = temp.next;
+            if (temp != null)
+                System.out.print("->");
+        }
+        System.out.println();
+    }
+
+    public static void printMatrix(int[][] matrix) {
         System.out.println("Matrix is: ");
-        for(int i = 0; i < matrix.length; i++){
-            for(int j = 0; j < matrix[0].length; j++){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
                 System.out.print(matrix[i][j] + "\t");
             }
             System.out.println();
--- MEDIUM/src/medium/LengthIncreasingSubsequence.java
+++ MEDIUM/src/medium/LengthIncreasingSubsequence.java
@@ -1,5 +1,7 @@
 package medium;
 
+import java.util.Arrays;
+
 import utils.CommonUtils;
 
 /**300. Longest Increasing Subsequence  QuestionEditorial Solution  My Submissions
@@ -19,13 +21,43 @@ Follow up: Could you improve it to O(n log n) time complexity?
 Credits:
 Special thanks to @pbrother for adding this problem and creating all test cases.*/
 public class LengthIncreasingSubsequence {
+    
+    public int lengthOfLIS_using_binary_search_from_discuss(int[] nums) {
+        /**Java doc for this Arrays.binarySearch method:
+         * int java.util.Arrays.binarySearch(int[] a, int fromIndex, int toIndex, int key)
+
+Searches a range of the specified array of ints for the specified value using the binary search algorithm. The range must be sorted (as by the sort(int [], int, int) method) prior to making this call. If it is not sorted, the results are undefined. If the range contains multiple elements with the specified value, there is no guarantee which one will be found.
+
+Parameters:
+a the array to be searched
+fromIndex the index of the first element (inclusive) to be searched
+toIndex the index of the last element (exclusive) to be searched
+key the value to be searched for
+
+Returns:
+index of the search key, if it is contained in the array within the specified range; otherwise, (-(insertion point) - 1). The insertion point is defined as the point at which the key would be inserted into the array: the index of the first element in the range greater than the key, or toIndex if all elements in the range are less than the specified key. Note that this guarantees that the return value will be >= 0 if and only if the key is found.*/
+        int[] dp = new int[nums.length];
+        int len = 0;
+        for(int x : nums){
+            int i = Arrays.binarySearch(dp, 0, len, x);
+            System.out.println("i = " + i + "\tx = " + x);
+            if(i < 0) i = -(i+1);
+            dp[i] = x;
+            if(i == len) len++;
+        }
+        CommonUtils.printArray(nums);
+        CommonUtils.printArray(dp);
+        return len;
+    }
+    
     public static void main(String...strings){
         LengthIncreasingSubsequence test = new LengthIncreasingSubsequence();
         int[] nums = new int[]{10, 9, 2, 5, 3, 7, 101, 18};
 //        int[] nums = new int[]{10,9,2,5,3,4};
 //        int[] nums = new int[]{1,3,6,7,9,4,10,5,6};
 //        int[] nums = new int[]{18,55,66,2,3,54};
-        System.out.println(test.lengthOfLIS(nums));
+//        System.out.println(test.lengthOfLIS(nums));
+        System.out.println(test.lengthOfLIS_using_binary_search_from_discuss(nums));
     }
 
     /**This is the closest I got, passed all normal cases, made it to 22/23 test cases, but got TLE, as I expected,
