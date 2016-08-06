ab33bf1fcb5d29196a4383e666f3085fcd556f86
Fishercoder <fishercoder@gmail.com>
2016-08-06 14:16:57 -0700

    added some utility classes

--- Common/src/classes/TreeNode.java
+++ Common/src/classes/TreeNode.java
@@ -4,5 +4,12 @@ public class TreeNode {
     public int val;
     public TreeNode left;
     public TreeNode right;
+    
     public TreeNode(int x){this.val = x;}
+    
+    public TreeNode(TreeNode left, int val, TreeNode right) {
+        this.left = left;
+        this.val = val;
+        this.right = right;
+    }
 }
---
+++ Common/src/utils/Notes.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package utils;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.SOURCE)
+public @interface Notes {
+
+	String todo() default "";
+
+	String problemReview() default "";
+
+	String issue() default "";
+
+	String context() default ""; // this variable is used to state how I solved
+									// this problem, whether completely made it
+									// myself, or copied it from online, or a
+									// combination of both approaches.
+
+	boolean needsReview() default true;
+
+}
