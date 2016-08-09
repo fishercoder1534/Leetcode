a6f9f5ae7a6550ed9f7cb2aa82607aa0d80a7046
Fishercoder <fishercoder@gmail.com>
2016-08-09 08:32:39 -0700

    draft for atoi

--- EASY/src/easy/StringToInteger.java
+++ EASY/src/easy/StringToInteger.java
@@ -1,4 +1,6 @@
 package easy;
+
+import java.util.HashSet;
 /**8. String to Integer (atoi)  QuestionEditorial Solution  My Submissions
 Total Accepted: 115114
 Total Submissions: 839893
@@ -8,6 +10,7 @@ Implement atoi to convert a string to an integer.
 Hint: Carefully consider all possible input cases. If you want a challenge, please do not see below and ask yourself what are the possible input cases.
 
 Notes: It is intended for this problem to be specified vaguely (ie, no given input specs). You are responsible to gather all the input requirements up front.*/
+import java.util.Set;
 
 /**Requirements for atoi:
 The function first discards as many whitespace characters as necessary until the first non-whitespace character is found. Then, starting from this character, takes an optional initial plus or minus sign followed by as many numerical digits as possible, and interprets them as a numerical value.
@@ -28,6 +31,59 @@ public class StringToInteger {
 		//case 4: there're many leading whitespace characters which we'll have to ignore
 		
 		//case 5: when finding the first non-whitespace character, it could possibly be a '+' or '-' sign, after that, we parse all the consecutive numbers
-		return 0;
+		
+	    str = str.trim();//cut off its leading and trailing whitespace characters
+	    if(str == null || str.isEmpty()) return 0;
+	    Set<Character> numbers = new HashSet();
+	    for(int i = 0; i < 10; i++){
+	        numbers.add(Character.forDigit(i, 10));
+	    }
+	    
+	    char[] chars = str.toCharArray();
+	    StringBuilder sb = new StringBuilder();
+	    boolean negative;
+	    int i = 0;
+	    if(chars[0] == '-') {
+	        negative = true;
+	        i++;//let i start from 1 in this case
+	    }
+	    else negative = false;
+	    
+	    //it might be a floating number, so consider '.'
+	    int period = 0;
+	    while(i < chars.length && numbers.contains(chars[i])){
+	        if(chars[i] == '.') period++;
+	        if(period > 1) break;
+	        sb.append(chars[i++]);
+	    }
+	    
+	    int result = 0;
+	    if(period > 0){
+	        //use Double to parse
+	        try{
+	            result = (int) Double.parseDouble(sb.toString());
+	        } catch(Exception e){
+	            System.out.println(e);
+	        }
+	    } else {
+	        //use Integer to parse
+	        try{
+                result = Integer.parseInt(sb.toString());
+            } catch(Exception e){
+                System.out.println(e);
+            }
+	    }
+	    
+	    return result;
+	}
+	
+	public static void main(String...strings){
+	    StringToInteger test = new StringToInteger();
+	    String str = "1";
+	    System.out.println(test.myAtoi(str));
+	    
+	    
+//	    System.out.println(Double.parseDouble("1.2098"));
+//	    System.out.println(Integer.parseInt("123456789"));
 	}
 }
