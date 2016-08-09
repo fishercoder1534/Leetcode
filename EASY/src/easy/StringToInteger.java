823401c231d409fa1abd10fd5bab26052791f8fe
fishercoder1534 <fishercoder@gmail.com>
2016-08-09 10:11:49 -0700

    accepted for StringToInteger

--- EASY/src/easy/StringToInteger.java
+++ EASY/src/easy/StringToInteger.java
@@ -21,6 +21,9 @@ If the first sequence of non-whitespace characters in str is not a valid integra
 
 If no valid conversion could be performed, a zero value is returned. If the correct value is out of the range of representable values, INT_MAX (2147483647) or INT_MIN (-2147483648) is returned.*/
 public class StringToInteger {
+	//TODO: look at others' solutions
+	
+	//Eventually, made it AC'ed, lots of corner cases, but now, really felt much easier and the though process is super clear than the first time I tried to solve it which was 3~4 years ago from now. 8/9/2016
 	public int myAtoi(String str) {
 		//case 1: str is greater than Integer.MAX_VALUE, return Integer.MAX_VALUE as the question states it
 		
@@ -42,12 +45,22 @@ public class StringToInteger {
 	    char[] chars = str.toCharArray();
 	    StringBuilder sb = new StringBuilder();
 	    boolean negative;
+	    int minuSignCount = 0, plusSignCount = 0;
 	    int i = 0;
-	    if(chars[0] == '-') {
-	        negative = true;
-	        i++;//let i start from 1 in this case
+	    while(i < chars.length){
+	    	if(chars[i] == '-'){
+	    		minuSignCount++;
+	    		i++;
+	    	} else if(chars[i] == '+'){
+	    		plusSignCount++;
+	    		i++;
+	    	} else {
+	    		break;
+	    	}
 	    }
-	    else negative = false;
+	    if((plusSignCount > 0 && minuSignCount > 0) || minuSignCount > 1 || plusSignCount > 1) return 0;
+	    negative = minuSignCount%2 != 0;
+	    if(i >= chars.length) return 0;
 	    
 	    //it might be a floating number, so consider '.'
 	    int period = 0;
@@ -56,6 +69,8 @@ public class StringToInteger {
 	        if(period > 1) break;
 	        sb.append(chars[i++]);
 	    }
+	    
+	    if(sb == null || sb.length() == 0) return 0;
 	    
 	    int result = 0;
 	    if(period > 0){
@@ -66,20 +81,44 @@ public class StringToInteger {
 	            System.out.println(e);
 	        }
 	    } else {
-	        //use Integer to parse
-	        try{
-                result = Integer.parseInt(sb.toString());
-            } catch(Exception e){
-                System.out.println(e);
-            }
+	        //use Long to parse to handle integer overflow case
+	        	long temp = 0;
+	        	if(sb.length() >= Long.toString(Long.MAX_VALUE).length() && negative){
+	        		return Integer.MIN_VALUE;
+	        	} else if(sb.length() >= Long.toString(Long.MAX_VALUE).length() && !negative){
+	        		return Integer.MAX_VALUE;
+	        	} else {
+		        	try{
+		        		temp = Long.parseLong(sb.toString());
+		        	} catch(Exception e){
+		        		if(sb.length() >= Integer.MAX_VALUE) result = Integer.MAX_VALUE;
+		        	}
+		        	if(temp > (long) Integer.MAX_VALUE+1) {
+		        		if(!negative)return Integer.MAX_VALUE;
+		        		else return Integer.MIN_VALUE;
+		        	}
+		        	else if(temp == (long) Integer.MAX_VALUE+1 && negative) return Integer.MIN_VALUE;
+		        	else if(temp == (long) Integer.MAX_VALUE+1) return Integer.MAX_VALUE;
+		        	else if(temp < Integer.MIN_VALUE) result = Integer.MIN_VALUE;
+		        	else result = (int) temp;
+	        	}
 	    }
 	    
+	    if(negative) result = -result;
 	    return result;
 	}
 	
 	public static void main(String...strings){
 	    StringToInteger test = new StringToInteger();
-	    String str = "1";
+//	    String str = "2147483648";
+//	    String str = "+-2";//a really interesting test case, you never know how stupid one's input could be like, this is to challenge your program to be more robust. It's expecting to return 0 for this case which means it's not a valid number
+//	    String str = "+";
+//	    String str = "abc";
+//	    String str = "1";
+//	    String str = "-2147483648";
+//	    String str = "++1";//I'm really amazed by OJ's test case variety, it's expecting 0 in this case
+//	    String str = "-2147483649";
+	    String str = "9223372036854775809";
 	    System.out.println(test.myAtoi(str));
 	    
 	    
