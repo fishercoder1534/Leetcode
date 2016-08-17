fd548a841cddcb3ee5859adc41ad0004e42fe4f8
fishercoder1534 <fishercoder@gmail.com>
2016-08-17 10:01:29 -0700

    both iterative and recursive solutions for Reverse Linked List.

--- EASY/src/easy/ReverseLinkedList.java
+++ EASY/src/easy/ReverseLinkedList.java
@@ -4,8 +4,12 @@ import utils.CommonUtils;
 import classes.ListNode;
 
 public class ReverseLinkedList {
-    //there's no reversion if the list is null or it contains only one node, so at a minimum, there should be 2 nodes
-    public ListNode reverseList(ListNode head) {
+    //creating a newHead = null is a very common/smart way to handle such cases, the logic flows out very naturally:
+	//create a new node called "next" to hold current head's next node
+	//then we could redirect head's next pointer to point to newHead which is head's previous node
+	//the above two steps finished the reversion, to continue this process until we reach the end of the original list, 
+	//we'll assign current "head" to new "newHead", and current "next" to be new "head" for the next iteration, here's the code
+    public ListNode reverseList_iterative(ListNode head) {
         ListNode newHead = null;
         while(head != null){
             ListNode next = head.next;
@@ -15,8 +19,35 @@ public class ReverseLinkedList {
         }
         return newHead;
     }
+    
+    //following the above iterative version, the recursive solution flows out so naturally, basically, we just replaced the while loop with a recursive function
+    //still, a null newHead proves to be very helpful.
+    public ListNode reverseList_recursive(ListNode head) {
+    	ListNode newHead = null;
+    	return reverse(head, newHead);
+    }
 
-    public static void main(String...strings){
+    private ListNode reverse(ListNode head, ListNode newHead) {
+    	if(head != null){
+    		ListNode next = head.next;
+    		head.next = newHead;
+    		newHead = head;
+    		head = next;
+    		return reverse(head, newHead);
+    	}
+    	else return newHead;
+	}
+    
+    //the above recursive function could of course be shortened to below, but the above one is just more intuitive and easier to follow and sort out your logic
+    private ListNode reverse_more_concise(ListNode head, ListNode newHead) {
+    	if(head != null){
+    		ListNode next = head.next;
+    		head.next = newHead;
+    		return reverse_more_concise(next, head);
+    	} else return newHead;
+    }
+
+	public static void main(String...strings){
         ReverseLinkedList test = new ReverseLinkedList();
         ListNode head = new ListNode(1);
         head.next = new ListNode(2);
@@ -24,7 +55,8 @@ public class ReverseLinkedList {
         head.next.next.next = new ListNode(4);
         head.next.next.next.next = new ListNode(5);
         CommonUtils.printList(head);
-        ListNode result = test.reverseList(head);
+//        ListNode result = test.reverseList_iterative(head);
+        ListNode result = test.reverseList_recursive(head);
         CommonUtils.printList(result);
     }
 
