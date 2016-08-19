7f2aef54f2f73cf3b07d4968c1c0bec900d7dfa6
Fishercoder <fishercoder@gmail.com>
2016-08-18 20:08:27 -0700

    valid palindrome

---
+++ EASY/src/easy/ValidPalindrome.java
@@ -0,0 +1,17 @@
+package easy;
+
+public class ValidPalindrome {
+
+    public boolean isPalindrome(String s) {
+        int i = 0, j = s.length()-1;
+        char[] chars = s.toCharArray();
+        while(i < j){
+            while(i < j && !Character.isLetterOrDigit(chars[i])) i++;
+            while(i < j && !Character.isLetterOrDigit(chars[j])) j--;
+            if(Character.toLowerCase(chars[i]) != Character.toLowerCase(chars[j])) return false;
+            i++; j--;
+        }
+        return true;
+    }
+
+}
