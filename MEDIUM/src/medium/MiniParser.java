107f4051bf656bca36dba496dcc998c6140adfc4
Fishercoder <fishercoder@gmail.com>
2016-08-19 07:57:08 -0700

    move self implemented NestedInteger class to common package

--- MEDIUM/src/medium/MiniParser.java
+++ MEDIUM/src/medium/MiniParser.java
@@ -1,8 +1,8 @@
 package medium;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Stack;
+
+import classes.NestedInteger;
 
 public class MiniParser {
     
@@ -98,65 +98,3 @@ public class MiniParser {
         System.out.println(result.printNi(result, new StringBuilder()));
     }
 }
-
-class NestedInteger {
-    private List<NestedInteger> list;
-    private Integer integer;
-    
-    public NestedInteger(List<NestedInteger> list){
-        this.list = list;
-    }
-    
-    public void add(NestedInteger nestedInteger) {
-        if(this.list != null){
-            this.list.add(nestedInteger);
-        } else {
-            this.list = new ArrayList();
-            this.list.add(nestedInteger);
-        }
-    }
-
-    public void setInteger(int num) {
-        this.integer = num;
-    }
-
-    public NestedInteger(Integer integer){
-        this.integer = integer;
-    }
-
-    public NestedInteger() {
-        this.list = new ArrayList();
-    }
-
-    public boolean isInteger() {
-        return integer != null;
-    }
-
-    public Integer getInteger() {
-        return integer;
-    }
-
-    public List<NestedInteger> getList() {
-        return list;
-    }
-    
-    public String printNi(NestedInteger thisNi, StringBuilder sb){
-        if(thisNi.isInteger()) {
-            sb.append(thisNi.integer);
-            sb.append(",");
-        }
-        sb.append("[");
-        for(NestedInteger ni : thisNi.list){
-            if(ni.isInteger()) {
-                sb.append(ni.integer);
-                sb.append(",");
-            }
-            else {
-                printNi(ni, sb);
-            }
-        }
-        sb.append("]");
-        return sb.toString();
-    }
-}
-
