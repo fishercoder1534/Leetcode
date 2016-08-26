95a44e4f29e90a80c5c1985d2d7cd09f9d5ca7ca
Fishercoder <fishercoder@gmail.com>
2016-08-26 08:33:05 -0700

    EASY/src/easy/TwoSumIIIDataStructureDesign.java

---
+++ EASY/src/easy/TwoSumIIIDataStructureDesign.java
@@ -0,0 +1,22 @@
+package easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+//Your TwoSum object will be instantiated and called as such:
+//TwoSum twoSum = new TwoSum();
+//twoSum.add(number);
+//twoSum.find(value);
+public class TwoSumIIIDataStructureDesign {
+    private Set<Integer> set = new HashSet();
+    
+ // Add the number to an internal data structure.
+    public void add(int number) {
+        set.add(number);
+    }
+
+    // Find if there exists any pair of numbers which sum is equal to the value.
+    public boolean find(int value) {
+        return 
+    }
+}
