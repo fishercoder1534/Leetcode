d0d6adbed82b898d721cb64093c4b2012d4ba869
Fishercoder <fishercoder@gmail.com>
2016-09-08 08:35:27 -0700

    MEDIUM/src/medium/_3Sum.java

---
+++ MEDIUM/src/medium/_3Sum.java
@@ -0,0 +1,31 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class _3Sum {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList();
+        int left = 0, right = nums.length-1, mid = 0;
+        while(left+1 < right){
+            mid = left + (right-left)/2;
+            int sum = nums[left] + nums[mid] + nums[right];
+            if(sum == 0){
+                List<Integer> list = new ArrayList();
+                list.add(nums[left]);
+                list.add(nums[mid]);
+                list.add(nums[right]);
+                
+                //move left
+            } else if(sum > 0){
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+    }
+
+}
