0a654b0d4d5ae7834df529014eb280593b64a5cd
Fishercoder <fishercoder@gmail.com>
2016-09-07 20:35:53 -0700

    add problem description

--- Lintcode/src/chapter2_binary_search/SearchInsertPosition.java
+++ Lintcode/src/chapter2_binary_search/SearchInsertPosition.java
@@ -1,5 +1,19 @@
 package chapter2_binary_search;
+/**Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 
+You may assume NO duplicates in the array.
+
+Have you met this question in a real interview? Yes
+Example
+[1,3,5,6], 5 → 2
+
+[1,3,5,6], 2 → 1
+
+[1,3,5,6], 7 → 4
+
+[1,3,5,6], 0 → 0
+
+*/
 public class SearchInsertPosition {
 
     /** 
