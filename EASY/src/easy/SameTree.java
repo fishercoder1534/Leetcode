c4edf2d5e0af2b24bd976a04e14d2e4f1a8f6278
Fishercoder <fishercoder@gmail.com>
2016-09-24 15:54:10 -0700

    reduce three lines to two lines

--- EASY/src/easy/SameTree.java
+++ EASY/src/easy/SameTree.java
@@ -15,7 +15,6 @@ public class SameTree {
 	//recursion idea flows out naturally.
 	public boolean isSameTree(TreeNode p, TreeNode q) {
 		if(p == null || q == null) return p == q;
-		if(p.val != q.val) return false;
-		return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+		return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 	}
 }
