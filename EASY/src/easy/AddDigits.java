cd289b14b62ba52f2ddf8e491e9bc3b9c1563368
Fishercoder <fishercoder@gmail.com>
2016-09-24 15:01:17 -0700

    EASY/src/easy/AddDigits.java

---
+++ EASY/src/easy/AddDigits.java
@@ -0,0 +1,11 @@
+package easy;
+
+public class AddDigits {
+//only three cases as the code shows
+    public int addDigits(int num) {
+        if(num == 0) return 0;
+        if(num % 9 == 0) return 9;
+        return num%9;
+    }
+
+}
