902357379491986e94d142c858146e1c585aa3e5
Fishercoder <fishercoder@gmail.com>
2016-09-24 15:37:47 -0700

    EASY/src/easy/RansomNote.java

--- EASY/src/easy/RansomNote.java
+++ EASY/src/easy/RansomNote.java
@@ -14,6 +14,23 @@ canConstruct("aa", "ab") -> false
 canConstruct("aa", "aab") -> true
 */
 public class RansomNote {
+
+    public boolean canConstruct_20160924(String ransomNote, String magazine) {
+        char[] mchars = magazine.toCharArray();
+        int[] mcnt = new int[256];
+        for(int i = 0; i < mchars.length; i++){
+            mcnt[mchars[i] - 'a']++;
+        }
+        
+        char[] rchars = ransomNote.toCharArray();
+        for(int i = 0; i < rchars.length; i++){
+            if(mcnt[rchars[i] - 'a'] <= 0) return false;
+            mcnt[rchars[i] - 'a']--;
+        }
+        return true;
+    }
+
+    
     public boolean canConstruct(String ransomNote, String magazine) {
         Map<Character, Integer> ransomMap = new HashMap();
         Set<Character> ransomSet = new HashSet();
