71a28e6e3633e4321703f0ddd0bfd08d2ab8fb84
Fishercoder <fishercoder@gmail.com>
2016-09-24 23:19:12 -0700

    EASY/src/easy/StrobogrammaticNumber.java

--- EASY/src/easy/StrobogrammaticNumber.java
+++ EASY/src/easy/StrobogrammaticNumber.java
@@ -1,10 +1,32 @@
 package easy;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class StrobogrammaticNumber {
-    public boolean isStrobogrammatic(String num) {
+
+    public boolean isStrobogrammatic_map(String num) {
+        int i = 0, j = num.length()-1;
+        Map<Character, Character> map = new HashMap();
+        map.put('8', '8');
+        map.put('1', '1');
+        map.put('0', '0');
+        if(j == 0) return map.containsKey(num.charAt(i));
+        
+        map.put('9', '6');
+        map.put('6', '9');
+        while(i < j){
+            if(!map.containsKey(num.charAt(i)) || !map.containsKey(num.charAt(j))) return false;
+            if(map.get(num.charAt(i)) != num.charAt(j)) return false;
+            i++;j--;
+        }
+        return map.containsKey(num.charAt(i));
+    }
+
+    
+    public boolean isStrobogrammatic_set(String num) {
         Set<Character> set = new HashSet();
         set.add('0');
         set.add('1');
