447a2728a920f27acd756fcd49ae76e0c627ebdf
Fishercoder <fishercoder@gmail.com>
2016-09-24 20:35:36 -0700

    EASY/src/easy/RomantoInteger.java

---
+++ EASY/src/easy/RomantoInteger.java
@@ -0,0 +1,30 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomantoInteger {
+
+    public int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        
+        char[] schar = s.toCharArray();
+        int result = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(i > 0 && map.get(schar[i]) > map.get(schar[i-1])){
+                result = result + map.get(schar[i]) - 2*map.get(schar[i-1]);
+            } else {
+                result = result + map.get(schar[i]);
+            }
+        }
+        return result;
+    }
+
+}
