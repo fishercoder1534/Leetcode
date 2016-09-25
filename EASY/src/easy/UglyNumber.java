a4cc4feb3156049a7284e05b999c8680305ed796
Fishercoder <fishercoder@gmail.com>
2016-09-24 22:21:15 -0700

    EASY/src/easy/UglyNumber.java

---
+++ EASY/src/easy/UglyNumber.java
@@ -0,0 +1,16 @@
+package easy;
+
+public class UglyNumber {
+
+    public boolean isUgly(int num) {
+        if(num == 0) return false;
+        int[] divisors = new int[]{5,3,2};
+        for(int divisor : divisors){
+            while(num%divisor == 0){
+                num /= divisor;
+            }
+        }
+        return num == 1;
+    }
+
+}
