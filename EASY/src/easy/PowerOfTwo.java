1ee9a95bb905f24eec47ccbf1b6faba9852881c8
Fishercoder <fishercoder@gmail.com>
2016-09-24 21:42:01 -0700

    EASY/src/easy/PowerOfTwo.java

--- EASY/src/easy/PowerOfTwo.java
+++ EASY/src/easy/PowerOfTwo.java
@@ -9,8 +9,7 @@ public class PowerOfTwo {
         //after writing out the binary representation of some numbers: 1,2,4,8,16,32, you can easily figure out that
         //every number that is power of two has only one bit that is 1
         //then we can apply that cool trick that we learned from {@link easy.NumberOfIBits}: n&(n-1) which will clear the least significant bit in n to zero
-        if(n <= 0) return false;
-        return (n&(n-1)) == 0;
+        return n> 0 && (n&(n-1)) == 0;
     }
     
     public static void main(String...strings){
