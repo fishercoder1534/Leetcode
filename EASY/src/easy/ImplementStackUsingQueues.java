bb2b2e101efc15c8c0fc042d0cc07c499be488f2
Fishercoder <fishercoder@gmail.com>
2016-09-28 12:35:32 -0700

    EASY/src/easy/ImplementStackUsingQueues.java

---
+++ EASY/src/easy/ImplementStackUsingQueues.java
@@ -0,0 +1,34 @@
+package easy;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ImplementStackUsingQueues {
+    class MyStack {
+        
+        Queue<Integer> q = new LinkedList();
+        
+        // Push element x onto stack.
+        public void push(int x) {
+            q.offer(x);
+            for(int i = 1; i < q.size(); i++){
+                q.offer(q.remove());
+            }
+        }
+
+        // Removes the element on top of the stack.
+        public void pop() {
+            q.poll();
+        }
+
+        // Get the top element.
+        public int top() {
+            return q.peek();
+        }
+
+        // Return whether the stack is empty.
+        public boolean empty() {
+            return q.isEmpty();
+        }
+    }
+}
