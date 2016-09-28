e78bcb8a786abc8ade2417e06d5b747767fec5db
Fishercoder <fishercoder@gmail.com>
2016-09-28 12:31:18 -0700

    EASY/src/easy/ImplementQueueUsingStacks.java

---
+++ EASY/src/easy/ImplementQueueUsingStacks.java
@@ -0,0 +1,38 @@
+package easy;
+
+import java.util.Stack;
+
+public class ImplementQueueUsingStacks {
+
+    class MyQueue {
+        
+        Stack<Integer> input = new Stack();
+        Stack<Integer> output = new Stack();
+        
+        // Push element x to the back of queue.
+        public void push(int x) {
+            input.push(x);
+        }
+
+        // Removes the element from in front of queue.
+        public void pop() {
+            peek();
+            output.pop();
+        }
+
+        // Get the front element.
+        public int peek() {
+            if(output.isEmpty()){
+                while(!input.isEmpty()){
+                    output.push(input.pop());
+                }
+            }
+            return output.peek();
+        }
+
+        // Return whether the queue is empty.
+        public boolean empty() {
+            return input.isEmpty() && output.isEmpty();
+        }
+    }
+}
