ea2523930e28e3e98f25b9d170a6df5a0b038d85
Fishercoder <fishercoder@gmail.com>
2016-09-30 23:11:10 -0700

    clone graph using BFS

---
+++ Common/src/classes/UndirectedGraphNode.java
@@ -0,0 +1,12 @@
+package classes;
+
+import java.util.*;
+
+/**
+ * Created by fishercoder1534 on 9/30/16.
+ */
+public class UndirectedGraphNode {
+    public int label;
+    public List<UndirectedGraphNode> neighbors;
+    public UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+}
