bf94760d818e1ba3d458ceac44406835c51f79d9
Steve Sun <fishercoder@gmail.com>
2016-11-02 09:37:16 -0700

    word pattern

---
+++ EASY/src/easy/WordPattern.java
@@ -0,0 +1,24 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordPattern {
+
+    public boolean wordPattern(String pattern, String str) {
+        String[] words = str.split(" ");
+        char[] patterns = pattern.toCharArray();
+        Map<Character, String> map = new HashMap();
+        if(patterns.length != words.length) return false;
+        for(int i = 0; i < patterns.length; i++){
+            if(map.containsKey(patterns[i])){
+                if(!map.get(patterns[i]).equals(words[i])) return false;
+            } else {
+                if(map.containsValue(words[i])) return false;//this is for this case: "abba", "dog dog dog dog"
+                map.put(patterns[i], words[i]);
+            }
+        }
+        return true;
+    }
+
+}
