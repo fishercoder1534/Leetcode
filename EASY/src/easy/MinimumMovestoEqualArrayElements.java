a8656bcfe14c439dc5ac1314dde3d6e6680c49e3
Steve Sun <fishercoder@gmail.com>
2016-11-07 08:40:19 -0800

    correct minimum moves to equal array elements

--- EASY/src/easy/MinimumMovestoEqualArrayElements.java
+++ EASY/src/easy/MinimumMovestoEqualArrayElements.java
@@ -1,41 +1,16 @@
 package easy;
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.Queue;
-
 public class MinimumMovestoEqualArrayElements {
-
+    /**Looked at this solution: https://discuss.leetcode.com/topic/66557/java-o-n-solution-short
+     * i.e. Add 1 to n-1 elements basically equals to subtracting 1 from one element. So the easiest way
+     * to make all elements in this array equal is to make all of them equal to the minimum element.*/
     public static int minMoves(int[] nums) {
-        Queue<Integer> heap = new PriorityQueue<Integer>(new Comparator<Integer>(){
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                if(o1 > o2) return 1;
-                else if(o1 < o2) return -1;
-                else return 0;
-            }
-        });//in ascending order
-        
-        int max = Integer.MIN_VALUE;
-        for(int i = 0; i < nums.length; i++){
-            max = Math.max(max, nums[i]);
-            heap.offer(nums[i]);
-        }
-        
-        int moves = 0;
-        while(!heap.isEmpty()){
-            //always increments two elements
-            int one = heap.poll();
-            int two = heap.poll();
-            one++;
-            two++;
-            moves++;
-            max = Math.max(max, Math.max(one, two)); 
-            heap.offer(one);
-            heap.offer(two);
-            if(heap.peek() == max) break;
-        }
-        return moves;
+        if(nums.length == 0) return 0;
+        int min = nums[0];
+        for(int n : nums) min = Math.min(min, n);
+        int res = 0;
+        for(int n : nums) res += n - min;
+        return res;
     }
     
     public static void main(String...args){
