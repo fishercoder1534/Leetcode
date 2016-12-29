62a3935602a9f07a4915b551d104eadce9b69050
Steve Sun <stevesun@coupang.com>
2016-12-29 12:09:55 -0800

    add problem descriptions

--- src/stevesun/algorithms/NumberOfIslandsUnionFind.java
+++ src/stevesun/algorithms/NumberOfIslandsUnionFind.java
@@ -1,10 +1,25 @@
 package stevesun.algorithms;
 
 /**
- * Created by fishercoder1534 on 9/30/16.
+ * Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+ Example 1:
+
+ 11110
+ 11010
+ 11000
+ 00000
+ Answer: 1
+
+ Example 2:
+
+ 11000
+ 11000
+ 00100
+ 00011
+ Answer: 3
  */
 
-//Inspired by this post: https://discuss.leetcode.com/topic/39980/1d-union-find-java-solution-easily-generalized-to-other-problems
 public class NumberOfIslandsUnionFind {
 
     class UnionFind{
--- src/stevesun/algorithms/NumberofIslandsDFS.java
+++ src/stevesun/algorithms/NumberofIslandsDFS.java
@@ -1,7 +1,23 @@
 package stevesun.algorithms;
 
 /**
- * Created by fishercoder1534 on 9/29/16.
+ * Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+ Example 1:
+
+ 11110
+ 11010
+ 11000
+ 00000
+ Answer: 1
+
+ Example 2:
+
+ 11000
+ 11000
+ 00100
+ 00011
+ Answer: 3
  */
 public class NumberofIslandsDFS {
 
