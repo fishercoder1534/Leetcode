b0023c33fb3218ae89dfa2ca1278ad04bb7c767c
Steve Sun <stevesun@coupang.com>
2016-12-29 16:31:49 -0800

    add a clean version of atoi

--- src/stevesun/algorithms/StringToInteger.java
+++ src/stevesun/algorithms/StringToInteger.java
@@ -12,17 +12,36 @@ Hint: Carefully consider all possible input cases. If you want a challenge, plea
 Notes: It is intended for this problem to be specified vaguely (ie, no given input specs). You are responsible to gather all the input requirements up front.*/
 import java.util.Set;
 
-/**Requirements for atoi:
+/**
+ * Implement atoi to convert a string to an integer.
+ Hint: Carefully consider all possible input cases. If you want a challenge, please do not see below and ask yourself what are the possible input cases.
+ Notes: It is intended for this problem to be specified vaguely (ie, no given input specs). You are responsible to gather all the input requirements up front.
+
+ Requirements for atoi:
 The function first discards as many whitespace characters as necessary until the first non-whitespace character is found. Then, starting from this character, takes an optional initial plus or minus sign followed by as many numerical digits as possible, and interprets them as a numerical value.
-
 The string can contain additional characters after those that form the integral number, which are ignored and have no effect on the behavior of this function.
-
 If the first sequence of non-whitespace characters in str is not a valid integral number, or if no such sequence exists because either str is empty or it contains only whitespace characters, no conversion is performed.
-
 If no valid conversion could be performed, a zero value is returned. If the correct value is out of the range of representable values, INT_MAX (2147483647) or INT_MIN (-2147483648) is returned.*/
 public class StringToInteger {
-	//TODO: look at others' solutions
-	
+	public int myAtoi_clean_version(String str){
+		int p = 0, result = 0;
+		while(p < str.length() && Character.isWhitespace(str.charAt(p))) p++;
+		if(p == str.length()) return 0;
+		boolean negativeFlag = (str.charAt(p) == '-');
+		if(str.charAt(p) == '+' || str.charAt(p) == '-') p++;
+		for(; p<str.length(); p++){
+			if(str.charAt(p) > '9' || str.charAt(p) < '0'){
+				break;
+			} else {
+				int digit = str.charAt(p) - '0';
+				if(!negativeFlag && result > (Integer.MAX_VALUE - digit)/10) return Integer.MAX_VALUE;
+				else if (negativeFlag && result < (Integer.MIN_VALUE + digit)/10) return Integer.MIN_VALUE;
+				result = result*10 + (negativeFlag? -digit:digit);
+			}
+		}
+		return result;
+	}
+
 	//Eventually, made it AC'ed, lots of corner cases, but now, really felt much easier and the though process is super clear than the first time I tried to solve it which was 3~4 years ago from now. 8/9/2016
 	public int myAtoi(String str) {
 		//case 1: str is greater than Integer.MAX_VALUE, return Integer.MAX_VALUE as the question states it
