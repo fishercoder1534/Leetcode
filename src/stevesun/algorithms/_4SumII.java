6afd7571539aaef005f3b2e39199053b43b5950a
Steve Sun <stevesun@coupang.com>
2016-12-29 17:34:32 -0800

    add problem description

--- src/stevesun/algorithms/_4SumII.java
+++ src/stevesun/algorithms/_4SumII.java
@@ -3,10 +3,28 @@ package stevesun.algorithms;
 import java.util.HashMap;
 import java.util.Map;
 
+/**Given four lists A, B, C, D of integer values, compute how many tuples (i, j, k, l) there are such that A[i] + B[j] + C[k] + D[l] is zero.
+
+ To make problem a bit easier, all A, B, C, D have same length of N where 0 ≤ N ≤ 500. All integers are in the range of -228 to 228 - 1 and the result is guaranteed to be at most 231 - 1.
+
+ Example:
+
+ Input:
+ A = [ 1, 2]
+ B = [-2,-1]
+ C = [-1, 2]
+ D = [ 0, 2]
+
+ Output:
+ 2
+
+ Explanation:
+ The two tuples are:
+ 1. (0, 0, 0, 1) -> A[0] + B[0] + C[0] + D[1] = 1 + (-2) + (-1) + 2 = 0
+ 2. (1, 1, 0, 0) -> A[1] + B[1] + C[0] + D[0] = 2 + (-1) + (-1) + 0 = 0*/
+
 public class _4SumII {
 
-    /**Although it's tagged with BinarySearch and HashTable, I didn't come up with a good BinarySearch solution, then looked at this post:
-     * https://discuss.leetcode.com/topic/67658/simple-java-solution-with-explanation*/
     public static int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
         Map<Integer, Integer> map = new HashMap();
         int result = 0, len = A.length;
