8656834ca47bf5f9937e9dec166abbd37d963b72
Steve Sun <stevesun@coupang.com>
2016-12-31 17:23:38 -0800

    Integer to English Words

--- README.md
+++ README.md
@@ -94,7 +94,7 @@ Your ideas/fixes/algorithms are more than welcome!
 |278|[First Bad Version](https://leetcode.com/problems/first-bad-version/)|[Solution](../../blob/master/src/stevesun/algorithms/FirstBadVersion.java)| O(logn)|O(1) | Easy| Binary Search
 |277|[Find the Celebrity](https://leetcode.com/problems/find-the-celebrity/)|[Solution](../../blob/master/src/stevesun/algorithms/FindtheCelebrity.java)| O(n)|O(1) | Medium| 
 |276|[Paint Fence](https://leetcode.com/problems/paint-fence/)|[Solution](../../blob/master/src/stevesun/algorithms/PaintFence.java)| O(n)|O(1) | Easy| DP
-|273|[Integer to English Words](https://leetcode.com/problems/integer-to-english-words/)|[Solution]| 
+|273|[Integer to English Words](https://leetcode.com/problems/integer-to-english-words/)|[Solution](../../blob/master/src/stevesun/algorithms/IntegertoEnglishWords.java)| O(n)|O(1) | Hard| 
 |272|[Closest Binary Search Tree Value II](https://leetcode.com/problems/closest-binary-search-tree-value-ii/)|[Solution](../../blob/master/src/stevesun/algorithms/ClosestBinarySearchTreeValueII.java)| O(h+k)|O(h) | Hard| Stack
 |271|[Encode and Decode Strings](https://leetcode.com/problems/encode-and-decode-strings/)|[Solution](../../blob/master/src/stevesun/algorithms/EncodeandDecodeStrings.java)| O(n)|O(1) | Medium| 
 |270|[Closest Binary Search Tree Value](https://leetcode.com/problems/closest-binary-search-tree-value/)|[Solution](../../blob/master/src/stevesun/algorithms/ClosestBinarySearchTreeValue.java)| O(h)|O(1) | Easy| DFS
---
+++ src/stevesun/algorithms/IntegertoEnglishWords.java
@@ -0,0 +1,60 @@
+package stevesun.algorithms;
+
+/**
+ * Convert a non-negative integer to its english words representation. Given input is guaranteed to be less than 231 - 1.
+
+ For example,
+ 123 -> "One Hundred Twenty Three"
+ 12345 -> "Twelve Thousand Three Hundred Forty Five"
+ 1234567 -> "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"
+ Hint:
+
+ Did you see a pattern in dividing the number into chunk of words? For example, 123 and 123000.
+ Group the number by thousands (3 digits). You can write a helper function that takes a number less than 1000 and convert just that chunk to words.
+ There are many edge cases. What are some good test cases? Does your code work with input such as 0? Or 1000010? (middle chunk is zero and should not be printed out)
+ */
+public class IntegertoEnglishWords {
+
+    private String[] digit = new String[] {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    private String[] teen = new String[] {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    private String[] ten = new String[] {"Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    private String[] thousand = new String[] {"Thousand", "Million", "Billion"};
+
+    public String numberToWords(int num) {
+        String ans;
+        if (num == 0)
+            return digit[num];
+
+        ans = hundredHelper(num%1000);
+        num = num/1000;
+        int i = 0;
+        while (i < 3 && num > 0) {
+            if (num % 1000 > 0)
+                ans = hundredHelper(num%1000) + thousand[i] + " " + ans;
+            num = num/1000;
+            i++;
+        }
+
+        return ans.trim();
+    }
+
+    public String hundredHelper(int num) {
+        String nstr = "";
+        if (num >= 100) {
+            nstr = digit[num/100] + " Hundred ";
+        }
+        num = num%100;
+        if (num >= 20) {
+            if (num % 10 != 0)
+                nstr = nstr + ten[num/10 - 2] + " " +  digit[num%10] + " ";
+            else
+                nstr = nstr + ten[num/10 - 2] + " ";
+        } else if (num >= 10) {
+            nstr = nstr + teen[num%10] + " ";
+        } else if (num > 0){
+            nstr = nstr + digit[num] + " ";
+        }
+        return nstr;
+    }
+
+}
