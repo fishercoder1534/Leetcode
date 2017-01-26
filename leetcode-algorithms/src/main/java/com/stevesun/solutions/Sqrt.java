476dcaa43ceb96a4c550e654f5e85fd607f58443
Steve Sun <stevesun@coupang.com>
2017-01-25 17:31:50 -0800

    [LEET-0000] Sqrt(x)

---
+++ leetcode-algorithms/src/main/java/com/stevesun/solutions/Sqrt.java
@@ -0,0 +1,23 @@
+package com.stevesun.solutions;
+
+/**
+ * Implement int sqrt(int x).
+
+ Compute and return the square root of x.
+
+ * Created by stevesun on 1/25/17.
+ */
+public class Sqrt {
+    public int mySqrt(int x) {
+        long i = 0, j = x/2+1;
+        while(i <= j){
+            long mid = (i+j)/2;
+            long result = mid*mid;
+            if(result == (long) x) return (int) mid;
+            else if(result > x) j = mid-1;
+            else i = mid+1;
+            System.out.println(mid + " * " + mid + " = " + result + "\ti = " + i + "\tj = " + j);
+        }
+        return (int) j;
+    }
+}
---
+++ leetcode-algorithms/src/test/java/com/stevesun/SqrtxTest.java
@@ -0,0 +1,40 @@
+package com.stevesun;
+
+import com.stevesun.solutions.Sqrt;
+import com.stevesun.solutions._4SumII;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by stevesun on 1/25/17.
+ */
+public class SqrtxTest {
+    private static Sqrt test;
+    private static int expected;
+    private static int actual;
+    private static int input;
+
+    @BeforeClass
+    public static void setup(){
+        test = new Sqrt();
+    }
+
+    @Before
+    public void setupForEachTest(){
+        expected = 0;
+        actual = 0;
+        input = 0;
+    }
+
+    @Test
+    public void test1(){
+        expected = 4;
+        input = 16;
+        actual = test.mySqrt(input);
+        assertEquals(expected, actual);
+
+    }
+}
