361f2db2c492183941c0d750107795fcf7a27d84
Steve Sun <stevesun@coupang.com>
2017-01-28 19:24:10 -0800

    [LEET-0000] add problem description

--- leetcode-algorithms/src/main/java/com/stevesun/solutions/InterleavingString.java
+++ leetcode-algorithms/src/main/java/com/stevesun/solutions/InterleavingString.java
@@ -1,6 +1,15 @@
 package com.stevesun.solutions;
 
 public class InterleavingString {
+    /**Given s1, s2, s3, find whether s3 is formed by the interleaving of s1 and s2.
+
+     For example,
+     Given:
+     s1 = "aabcc",
+     s2 = "dbbca",
+
+     When s3 = "aadbbcbcac", return true.
+     When s3 = "aadbbbaccc", return false.*/
     public boolean isInterleave(String s1, String s2, String s3) {
         // write your code here
         int m = s1.length(), n = s2.length();
