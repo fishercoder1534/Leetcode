9c2aabbd3f2471b60cb3b8decdcdf9dbdd9b811a
Steve Sun <stevesun@coupang.com>
2017-02-01 21:14:51 -0800

    [LEET-0000] add problem descriptions

--- leetcode-algorithms/src/main/java/com/stevesun/solutions/SparseMatrixMultiplication.java
+++ leetcode-algorithms/src/main/java/com/stevesun/solutions/SparseMatrixMultiplication.java
@@ -1,5 +1,26 @@
 package com.stevesun.solutions;
 
+/**Given two sparse matrices A and B, return the result of AB.
+
+ You may assume that A's column number is equal to B's row number.
+
+ Example:
+
+ A = [
+ [ 1, 0, 0],
+ [-1, 0, 3]
+ ]
+
+ B = [
+ [ 7, 0, 0 ],
+ [ 0, 0, 0 ],
+ [ 0, 0, 1 ]
+ ]
+
+
+      |  1 0 0 |   | 7 0 0 |   |  7 0 0 |
+ AB = | -1 0 3 | x | 0 0 0 | = | -7 0 3 |
+                   | 0 0 1 |*/
 public class SparseMatrixMultiplication {
 
     public int[][] multiply(int[][] A, int[][] B) {
