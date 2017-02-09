e566521ef557375e4b239f742321ee59175dba2f
Steve Sun <stevesun@coupang.com>
2017-02-09 08:24:02 -0800

    [LEET-0000] clean up

--- leetcode-algorithms/src/main/java/com/stevesun/solutions/BestTimeToBuyAndSellStockIII.java
+++ leetcode-algorithms/src/main/java/com/stevesun/solutions/BestTimeToBuyAndSellStockIII.java
@@ -2,9 +2,6 @@ package com.stevesun.solutions;
 
 
 /**123. Best Time to Buy and Sell Stock III  QuestionEditorial Solution  My Submissions
-Total Accepted: 62880
-Total Submissions: 231914
-Difficulty: Hard
 Say you have an array for which the ith element is the price of a given stock on day i.
 
 Design an algorithm to find the maximum profit. You may complete at most two transactions.
