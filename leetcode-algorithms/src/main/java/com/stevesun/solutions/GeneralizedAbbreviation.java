f27e35c09562755e883eff13390a31d34b371ef2
stevesun <stevesun@coupang.com>
2017-02-10 21:41:15 -0800

    [LEET-320] add 320 and test

---
+++ leetcode-algorithms/src/main/java/com/stevesun/solutions/GeneralizedAbbreviation.java
@@ -0,0 +1,32 @@
+package com.stevesun.solutions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Write a function to generate the generalized abbreviations of a word.
+
+ Example:
+
+ Given word = "word", return the following list (order does not matter):
+ ["word", "1ord", "w1rd", "wo1d", "wor1", "2rd", "w2d", "wo2", "1o1d", "1or1", "w1r1", "1o2", "2r1", "3d", "w3", "4"]
+ */
+public class GeneralizedAbbreviation {
+
+    public List<String> generateAbbreviations(String word) {
+        List<String> result = new ArrayList<>();
+        backtrack(word, result, 0, "", 0);
+        return  result;
+    }
+
+    private void backtrack(String word, List<String> result, int position, String current, int count) {
+        if (position == word.length()){
+            if (count > 0) current += count;
+            result.add(current);
+        } else {
+            backtrack(word, result, position+1, current, count+1);
+            backtrack(word, result, position+1, current + (count > 0 ? count : "") + word.charAt(position), 0);
+        }
+    }
+
+}
