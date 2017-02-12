4b081cda81c8a1e2cc3d17ef084b03f3ad3f881a
stevesun <stevesun@coupang.com>
2017-02-12 08:04:36 -0800

    [LEET-513] add 513

--- leetcode-algorithms/README.md
+++ leetcode-algorithms/README.md
@@ -3,6 +3,7 @@
 ## Algorithms
 |  #  |      Title     |   Solutions   | Time          | Space         | Difficulty  | Tag          | Notes
 |-----|----------------|---------------|---------------|---------------|-------------|--------------|-----
+|513|[Find Left Most Element](https://leetcode.com/problems/find-left-most-element/)|[Solution](../../master/leetcode-algorithms/src/main/java/com/stevesun/solutions/FindLeftMostElement.java) | O(n) |O(k) | Medium| BFS
 |504|[Base 7](https://leetcode.com/problems/base-7/)|[Solution](../../master/leetcode-algorithms/src/main/java/com/stevesun/solutions/Base7.java) | O(1) |O(1) | Easy| 
 |501|[Find Mode in Binary Tree](https://leetcode.com/problems/find-mode-in-binary-tree/)|[Solution](../../master/leetcode-algorithms/src/main/java/com/stevesun/solutions/FindModeinBinaryTree.java) | O(n) |O(k) | Easy| Binary Tree
 |492|[Construct the Rectangle](https://leetcode.com/problems/construct-the-rectangle/)|[Solution](../../master/leetcode-algorithms/src/main/java/com/stevesun/solutions/ConstructTheRectangle.java) | O(n) |O(1) | Easy| Array
---
+++ leetcode-algorithms/src/test/java/com/stevesun/FindLeftMostElementTest.java
@@ -0,0 +1,56 @@
+package com.stevesun;
+
+import com.stevesun.common.classes.TreeNode;
+import com.stevesun.solutions.FindLeftMostElement;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by stevesun on 1/15/17.
+ */
+public class FindLeftMostElementTest {
+    private static FindLeftMostElement test;
+    private static int expected;
+    private static int actual;
+    private static TreeNode root;
+
+    @BeforeClass
+    public static void setup(){
+        test = new FindLeftMostElement();
+    }
+
+    @Before
+    public void setupForEachTest(){
+        expected = 0;
+        actual = 0;
+        root = new TreeNode(0);
+    }
+
+    @Test
+    public void test1(){
+        TreeNode root = new TreeNode(2);
+        root.left = new TreeNode(1);
+        root.right= new TreeNode(3);
+        expected = 1;
+        actual = test.findLeftMostNode(root);
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void test2(){
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right= new TreeNode(3);
+        root.left.left= new TreeNode(4);
+        root.right.left= new TreeNode(5);
+        root.right.right= new TreeNode(6);
+        root.right.left.left= new TreeNode(7);
+        expected = 7;
+        actual = test.findLeftMostNode(root);
+        assertEquals(expected, actual);
+    }
+}
