81a480e98be413555bd43a6efd3f7ad7546feb01
stevesun <stevesun@coupang.com>
2017-03-16 18:29:14 -0700

    [LEET-536] add 536

---
+++ leetcode-algorithms/src/test/java/com/stevesun/ConstructBinaryTreefromStringTest.java
@@ -0,0 +1,37 @@
+package com.stevesun;
+
+import com.stevesun.common.classes.TreeNode;
+import com.stevesun.solutions.ConstructBinaryTreefromString;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class ConstructBinaryTreefromStringTest {
+    private static ConstructBinaryTreefromString test;
+    private static TreeNode expected;
+    private static TreeNode actual;
+    private static String s;
+
+    @BeforeClass
+    public static void setup(){
+        test = new ConstructBinaryTreefromString();
+    }
+
+    @Before
+    public void setupForEachTest(){
+        expected = null;
+        actual = null;
+        s = "";
+    }
+
+    @Test
+    public void test1(){
+        s = "";
+        expected = null;
+        actual = test.str2tree(s);
+        assertEquals(expected, actual);
+
+    }
+}
