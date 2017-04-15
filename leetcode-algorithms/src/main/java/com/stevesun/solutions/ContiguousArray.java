296f63551d8b5151ce51e384be22734cdac9eeae
stevesun <stevesun@coupang.com>
2017-04-15 11:08:34 -0700

    [LEET-525] add 525

--- leetcode-algorithms/src/main/java/com/stevesun/solutions/ContiguousArray.java
+++ leetcode-algorithms/src/main/java/com/stevesun/solutions/ContiguousArray.java
@@ -26,13 +26,14 @@ public class ContiguousArray {
         int count = 0;
         int max = 0;
         Map<Integer, Integer> map = new HashMap();
-        map.put(0, -1);//initialize the map, which means at index zero, the length of contiguous subarray is -1, which is to be overwritten by subsequent array elements
+        map.put(0, -1);//initialize the map, which means at index zero, the length of contiguous subarray is -1
         for (int i = 0; i < nums.length; i++) {
             count += nums[i] == 1 ? 1 : -1;
             if (map.containsKey(count)) {
                 max = Math.max(i - map.get(count), max);
             } else {
-                map.put(count, i);//keep overwriting the map
+                map.put(count, i);//only when the map does not have this key, we put it in the map, this avoids overwriting the map
+                //also, it helps us keep the most leftside value in the map to help us compute the longest contigous array length
             }
         }
         return max;
