c89cc86d29c5c5c5d1587e7a2156505c4c9b3314
stevesun <stevesun@coupang.com>
2017-04-18 08:04:14 -0700

    [LEET-000] add util methods for ListNode

--- leetcode-algorithms/src/main/java/com/stevesun/common/classes/ListNode.java
+++ leetcode-algorithms/src/main/java/com/stevesun/common/classes/ListNode.java
@@ -87,4 +87,32 @@ public class ListNode {
         printList(head);
         System.out.println("The end.");
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ListNode)) return false;
+
+        ListNode listNode = (ListNode) o;
+
+        if (val != listNode.val) return false;
+        return next != null ? next.equals(listNode.next) : listNode.next == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = val;
+        result = 31 * result + (next != null ? next.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+
+        return "ListNode{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
+    }
+
 }
