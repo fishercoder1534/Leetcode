e933fad1127371d085482c5db3619ad0ee726b7a
stevesun <stevesun@coupang.com>
2017-04-19 16:04:43 -0700

    [LEET-0] reformat

--- leetcode-algorithms/src/main/java/com/stevesun/solutions/ConstructBinaryTreefromPreorderandInorderTraversal.java
+++ leetcode-algorithms/src/main/java/com/stevesun/solutions/ConstructBinaryTreefromPreorderandInorderTraversal.java
@@ -9,29 +9,22 @@ import com.stevesun.common.classes.TreeNode;
 public class ConstructBinaryTreefromPreorderandInorderTraversal {
 
 	public static TreeNode buildTree(int[] preorder, int[] inorder) {
-		if (preorder.length == 0)
-			return null;
+		if (preorder.length == 0) return null;
 		return buildTree(preorder, inorder, 0, 0, inorder.length - 1);
 	}
 
-	private static TreeNode buildTree(int[] preorder, int[] inorder,
-			int preorderIndex, int start, int end) {
-		if (start > end)
-			return null;
+	private static TreeNode buildTree(int[] preorder, int[] inorder, int preorderIndex, int start, int end) {
+		if (start > end) return null;
 		TreeNode node = new TreeNode(preorder[preorderIndex]);
-		int inorderIndex = findInorderIndex(inorder, start, end,
-				preorder[preorderIndex]);
+		int inorderIndex = findInorderIndex(inorder, start, end, preorder[preorderIndex]);
 		int leftTreeSize = inorderIndex - start;
 		int rightTreeSize = end - inorderIndex;
-		node.left = buildTree(preorder, inorder, preorderIndex + 1, start,
-				inorderIndex - 1);
-		node.right = buildTree(preorder, inorder, preorderIndex + leftTreeSize
-				+ 1, inorderIndex + 1, end);
+		node.left = buildTree(preorder, inorder, preorderIndex + 1, start, inorderIndex - 1);
+		node.right = buildTree(preorder, inorder, preorderIndex + leftTreeSize + 1, inorderIndex + 1, end);
 		return node;
 	}
 
-	private static int findInorderIndex(int[] inorder, int start, int end,
-			int key) {
+	private static int findInorderIndex(int[] inorder, int start, int end, int key) {
 		for (int i = start; i <= end; i++) {
 			if (inorder[i] == key)
 				return i;
