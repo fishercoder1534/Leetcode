5c7972b68321fe0748842659fcead60cc7a80c23
stevesun <stevesun@coupang.com>
2017-05-23 11:04:09 -0700

    add 592

---
+++ src/test/java/com/stevesun/_592Test.java
@@ -0,0 +1,26 @@
+package com.stevesun;
+
+import com.stevesun.solutions._592;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by stevesun on 5/23/17.
+ */
+public class _592Test {
+    private static _592 test;
+    private static String expression;
+
+    @BeforeClass
+    public static void setup(){
+        test = new _592();
+    }
+
+    @Test
+    public void test1(){
+        expression = "-1/2+1/2+1/3";
+        assertEquals("1/3", test.fractionAddition(expression));
+    }
+}
