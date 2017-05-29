4092f20021cd8284383e6b6683c1e3c90dc212fb
stevesun <stevesun@coupang.com>
2017-05-29 16:07:29 -0700

    add 450

--- src/main/java/com/fishercoder/solutions/_450.java
+++ src/main/java/com/fishercoder/solutions/_450.java
@@ -45,9 +45,39 @@ import com.fishercoder.common.classes.TreeNode;
  */
 public class _450 {
 
+    /**credit: https://discuss.leetcode.com/topic/65792/recursive-easy-to-understand-java-solution
+     * Steps:
+     1. Recursively find the node that has the same value as the key, while setting the left/right nodes equal to the returned subtree
+     2. Once the node is found, have to handle the below 4 cases
+        a. node doesn't have left or right - return null
+        b. node only has left subtree- return the left subtree
+        c. node only has right subtree- return the right subtree
+        d. node has both left and right - find the minimum value in the right subtree, set that value to the currently found node, then recursively delete the minimum value in the right subtree*/
     public TreeNode deleteNode(TreeNode root, int key) {
-        //step 1: find the node
+        if (root == null) return root;
+        if (root.val > key) {
+            root.left = deleteNode(root.left, key);
+        } else if (root.val < key) {
+            root.right = deleteNode(root.right, key);
+        } else {
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+
+            TreeNode minNode = getMin(root.right);
+            root.val = minNode.val;
+            root.right = deleteNode(root.right, root.val);
+        }
         return root;
+    }
+
+    private TreeNode getMin(TreeNode node) {
+        while (node.left != null) {
+            node = node.left;
+        }
+        return node;
     }
 
 }
---
+++ src/test/java/com/fishercoder/_450Test.java
@@ -0,0 +1,41 @@
+package com.fishercoder;
+
+import com.fishercoder.common.classes.TreeNode;
+import com.fishercoder.solutions._450;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by stevesun on 5/29/17.
+ */
+public class _450Test {
+    private static _450 test;
+    private static TreeNode expected;
+    private static TreeNode actual;
+    private static TreeNode root;
+
+    @BeforeClass
+    public static void setup(){
+        test = new _450();
+    }
+
+    @Test
+    public void test1(){
+        root = new TreeNode(5);
+        root.left = new TreeNode(3);
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(4);
+        root.right = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+
+        expected = new TreeNode(5);
+        expected.left = new TreeNode(4);
+        expected.right = new TreeNode(6);
+        expected.left.left = new TreeNode(2);
+        expected.right.right = new TreeNode(7);
+
+        assertEquals(expected, test.deleteNode(root, 3));
+    }
+}
