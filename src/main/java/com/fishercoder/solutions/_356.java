b0003e5dfbb2d2923ce11d45d761ff4222efb141
stevesun <stevesun@coupang.com>
2017-06-01 09:36:47 -0700

    add 356

---
+++ src/main/java/com/fishercoder/solutions/_356.java
@@ -0,0 +1,43 @@
+package com.fishercoder.solutions;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Given n points on a 2D plane, find if there is such a line parallel to y-axis that reflect the given points.
+
+ Example 1:
+ Given points = [[1,1],[-1,1]], return true.
+
+ Example 2:
+ Given points = [[1,1],[-1,-1]], return false.
+
+ Follow up:
+ Could you do better than O(n2)?
+
+ Hint:
+
+ Find the smallest and largest x-value for all points.
+ If there is a line then it should be at y = (minX + maxX) / 2.
+ For each point, make sure that it has a reflected point in the opposite side.
+ */
+public class _356 {
+    /**credit: https://discuss.leetcode.com/topic/48172/simple-java-hashset-solution*/
+    public boolean isReflected(int[][] points) {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        Set<String> set = new HashSet<>();
+        for (int[] point : points) {
+            max = Math.max(max, point[0]);
+            min = Math.min(min, point[0]);
+            String str = point[0] + "a" + point[1];
+            set.add(str);
+        }
+        int sum = max+min;
+        for (int[] p : points) {
+            String str = (sum - p[0]) + "a" + p[1];
+            if (!set.contains(str)) return false;
+        }
+        return true;
+    }
+}
---
+++ src/test/java/com/fishercoder/_356Test.java
@@ -0,0 +1,38 @@
+package com.fishercoder;
+
+import com.fishercoder.solutions._356;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by stevesun on 6/1/17.
+ */
+public class _356Test {
+    private static _356 test;
+    private static int[][] points;
+
+    @BeforeClass
+    public static void setup(){
+        test = new _356();
+    }
+
+    @Test
+    public void test1(){
+        points = new int[][]{
+                {1,1},
+                {-1,1},
+        };
+        assertEquals(true, test.isReflected(points));
+    }
+
+    @Test
+    public void test2(){
+        points = new int[][]{
+                {1,1},
+                {-1,-1},
+        };
+        assertEquals(false, test.isReflected(points));
+    }
+}
