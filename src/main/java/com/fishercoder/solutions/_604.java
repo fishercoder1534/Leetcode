11c4cd3bfcbf577d23650eeddfd78d4aac2eb0f3
stevesun <stevesun@coupang.com>
2017-06-11 08:46:44 -0700

    add 604

---
+++ src/main/java/com/fishercoder/solutions/_604.java
@@ -0,0 +1,65 @@
+package com.fishercoder.solutions;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 604. Design Compressed String Iterator
+ *
+ * Design and implement a data structure for a compressed string iterator. It should support the following operations: next and hasNext.
+ The given compressed string will be in the form of each letter followed by a positive integer representing
+ the number of this letter existing in the original uncompressed string.
+
+ next() - if the original string still has uncompressed characters, return the next letter; Otherwise return a white space.
+ hasNext() - Judge whether there is any letter needs to be uncompressed.
+
+ Note:
+ Please remember to RESET your class variables declared in StringIterator,
+ as static/class variables are persisted across multiple test cases. Please see here for more details.
+
+ Example:
+
+ StringIterator iterator = new StringIterator("L1e2t1C1o1d1e1");
+
+ iterator.next(); // return 'L'
+ iterator.next(); // return 'e'
+ iterator.next(); // return 'e'
+ iterator.next(); // return 't'
+ iterator.next(); // return 'C'
+ iterator.next(); // return 'o'
+ iterator.next(); // return 'd'
+ iterator.hasNext(); // return true
+ iterator.next(); // return 'e'
+ iterator.hasNext(); // return false
+ iterator.next(); // return ' '
+
+ */
+public class _604 {
+    public static class StringIterator {
+
+        Deque<int[]> deque;
+        public StringIterator(String compressedString) {
+            deque = new ArrayDeque<>();
+            int len = compressedString.length();
+            int i = 0;
+            while (i < len) {
+                int j = i + 1;
+                while (j < len && Character.isDigit(compressedString.charAt(j))) j++;
+                deque.addLast(new int[]{compressedString.charAt(i) - 'A', Integer.parseInt(compressedString.substring(i+1, j))});
+                i = j;
+            }
+        }
+
+        public char next() {
+            if (deque.isEmpty()) return ' ';
+            int[] top = deque.peek();
+            top[1]--;
+            if (top[1] == 0) deque.pollFirst();
+            return (char) ('A' + top[0]);
+        }
+
+        public boolean hasNext() {
+            return !deque.isEmpty();
+        }
+    }
+}
---
+++ src/test/java/com/fishercoder/_604Test.java
@@ -0,0 +1,91 @@
+package com.fishercoder;
+
+import com.fishercoder.solutions._604;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by stevesun on 6/10/17.
+ */
+public class _604Test {
+    private static _604.StringIterator test;
+
+    @Test
+    public void test1(){
+        test = new _604.StringIterator("L1e2t1C1o1d1e1");
+        System.out.println(test.hasNext());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.hasNext());
+    }
+
+    @Test
+    public void test2(){
+        test = new _604.StringIterator("L10e2t1C1o1d1e11");
+        System.out.println(test.hasNext());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.hasNext());
+    }
+
+    @Test
+    public void test3(){
+        test = new _604.StringIterator("x6");
+        System.out.println(test.hasNext());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.hasNext());
+        System.out.println(test.next());
+        System.out.println(test.hasNext());
+    }
+
+    @Test
+    public void test4(){
+        test = new _604.StringIterator("X15D18V8");
+        System.out.println(test.hasNext());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        System.out.println(test.next());
+        assertEquals(true, test.hasNext());
+        System.out.println(test.next());
+        assertEquals(true, test.hasNext());
+    }
+}
