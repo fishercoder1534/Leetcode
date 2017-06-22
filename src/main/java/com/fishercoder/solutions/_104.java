c635b70519cceedd998e27a87d7084135ae4c489
stevesun <stevesun@coupang.com>
2017-06-22 09:11:40 +0900

    add checkstyle

--- src/main/java/com/fishercoder/solutions/_104.java
+++ src/main/java/com/fishercoder/solutions/_104.java
@@ -3,23 +3,18 @@ package com.fishercoder.solutions;
 import com.fishercoder.common.classes.TreeNode;
 
 /**104. Maximum Depth of Binary Tree
- *
 Given a binary tree, find its maximum depth.
-
-The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.*/
+ The maximum depth is the number of
+ nodes along the longest path from the root node down to the farthest leaf node.
+ */
 public class _104 {
     
     //more verbose version
     public int maxDepth(TreeNode root) {
         if(root == null) return 0;
-        int leftDepth = 0;
-        if(root.left != null) leftDepth = maxDepth(root.left)+1;
-        int rightDepth = 0;
-        if(root.right != null) rightDepth = maxDepth(root.right)+1;
-        return Math.max(1, Math.max(leftDepth, rightDepth));//the reason we need to max with 1 here is actually
-        //for this case: if(root != null), it's implicit here, because we checked root.left != null and root.right != null
-        //then it comes to root != null
-        //example test case for the above scenario: nums = {1,1,1}
+        int leftDepth = maxDepth(root.left)+1;
+        int rightDepth = maxDepth(root.right)+1;
+        return Math.max(1, Math.max(leftDepth, rightDepth));
     }
     
     //more concise version
---
+++ src/main/java/com/fishercoder/solutions/_564.java
@@ -0,0 +1,27 @@
+package com.fishercoder.solutions;
+
+/**
+ * 564. Find the Closest Palindrome
+ *
+ * Given an integer n, find the closest integer (not including itself), which is a palindrome.
+
+ The 'closest' is defined as absolute difference minimized between two integers.
+
+ Example 1:
+
+ Input: "123"
+ Output: "121"
+
+ Note:
+
+ The input n is a positive integer represented by string, whose length will not exceed 18.
+ If there is a tie, return the smaller one as answer.
+
+ */
+public class _564 {
+
+    public String nearestPalindromic(String n) {
+        return "";
+    }
+
+}
