015d920bd9dce117438fa9343a56f25e22cd1f12
stevesun <stevesun@coupang.com>
2017-07-04 06:36:48 +0800

    edit 357

--- src/main/java/com/fishercoder/solutions/_357.java
+++ src/main/java/com/fishercoder/solutions/_357.java
@@ -14,7 +14,7 @@ package com.fishercoder.solutions;
  Let f(k) = count of numbers with unique digits with length equals k.
  f(1) = 10, ..., f(k) = 9 * 9 * 8 * ... (9 - k + 2) [The first factor is 9 because a number cannot start with 0].
  */
-public class CountNumbersWithUniqueDigits {
+public class _357 {
 
     public int countNumbersWithUniqueDigits(int n) {
         if (n == 0)     return 1;
