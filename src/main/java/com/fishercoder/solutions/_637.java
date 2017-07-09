212c3a523caa8f27b20aaf8edd87b3643b2c9d2d
stevesun <stevesun@coupang.com>
2017-07-09 11:32:59 -0700

    add 637

---
+++ src/main/java/com/fishercoder/solutions/_637.java
@@ -0,0 +1,57 @@
+package com.fishercoder.solutions;
+
+import com.fishercoder.common.classes.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 637. Average of Levels in Binary Tree
+ *
+ Given a non-empty binary tree, return the average value of the nodes on each level in the form of an array.
+
+ Example 1:
+ Input:
+  3
+ / \
+9  20
+  /  \
+ 15   7
+
+ Output: [3, 14.5, 11]
+ Explanation:
+
+ The average value of nodes on level 0 is 3,  on level 1 is 14.5, and on level 2 is 11. Hence return [3, 14.5, 11].
+ Note:
+ The range of node's value is in the range of 32-bit signed integer.
+ */
+public class _637 {
+
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new ArrayList<>();
+        if (root == null) return result;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            double sum = 0.0;
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = queue.poll();
+                if (curr != null) {
+                    sum += curr.val;
+                }
+                if (curr.left != null) {
+                    queue.offer(curr.left);
+                }
+                if (curr.right != null) {
+                    queue.offer(curr.right);
+                }
+            }
+            result.add(sum/size);
+        }
+        return result;
+    }
+
+}
