8d019b68b157a72960d45723dfd7a365d5b31bef
stevesun <stevesun@coupang.com>
2017-07-20 09:12:31 -0700

    edit 219

--- src/main/java/com/fishercoder/solutions/_219.java
+++ src/main/java/com/fishercoder/solutions/_219.java
@@ -6,7 +6,8 @@ import java.util.Map;
 /**219. Contains Duplicate II
  *
 Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and the difference between i and j is at most k.*/
-public class ContainsDuplicateII {
+public class _219 {
+
     //pretty straightforward, use a hashmap, key is the number itself, value is the last index that this value appeared in the array
     //we can keep updating the value as we move forward, since if the current index minus the last index cannot be smaller than k, then
     //the later indices won't even do either. So, we only need to keep one index in the value of the HashMap. Cheers!
