74c6f36a5e27e735de5a2498bbdbcacbf2c34ba7
stevesun <stevesun@coupang.com>
2017-07-24 15:09:53 -0700

    edit 278

--- src/main/java/com/fishercoder/solutions/_278.java
+++ src/main/java/com/fishercoder/solutions/_278.java
@@ -5,7 +5,7 @@ package com.fishercoder.solutions;
  Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
 
  You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.*/
-public class FirstBadVersion {
+public class _278 {
 
 	public int firstBadVersion(int n) {
 		int left = 1, right = n;
