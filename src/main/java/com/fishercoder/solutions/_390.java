1ada2709b11a2821862f401bbcc136f1ad20356a
stevesun <stevesun@coupang.com>
2017-07-25 11:34:44 -0700

    edit 390

--- src/main/java/com/fishercoder/solutions/_390.java
+++ src/main/java/com/fishercoder/solutions/_390.java
@@ -3,13 +3,13 @@ package com.fishercoder.solutions;
 import java.util.ArrayList;
 import java.util.List;
 
-/**There is a list of sorted integers from 1 to n. Starting from left to right, remove the first number and every other number afterward until you reach the end of the list.
-
- Repeat the previous step again, but this time from right to left, remove the right most number and every other number from the remaining numbers.
-
- We keep repeating the steps again, alternating left to right and right to left, until a single number remains.
-
- Find the last number that remains starting with a list of length n.
+/**There is a list of sorted integers from 1 to n.
+ * Starting from left to right,
+ * remove the first number and every other number afterward until you reach the end of the list.
+ * Repeat the previous step again, but this time from right to left,
+ * remove the right most number and every other number from the remaining numbers.
+ * We keep repeating the steps again, alternating left to right and right to left, until a single number remains.
+ * Find the last number that remains starting with a list of length n.
 
  Example:
 
@@ -21,8 +21,10 @@ import java.util.List;
  6
 
  Output:
- 6*/
-public class EliminationGame {
+ 6
+
+ */
+public class _390 {
     
     //then I turned to Discuss and found this post: https://discuss.leetcode.com/topic/55870/share-my-solutions-for-contest-2
     //instead of literally removing half of the elements in each scan, this solution is just moving the pointer to point to next start position
