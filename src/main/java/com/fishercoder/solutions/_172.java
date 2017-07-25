de223cbbf822864cf4b476745b975cb07d43c69d
stevesun <stevejsun@gmail.com>
2017-07-25 11:57:04 -0700

    edit 172

--- src/main/java/com/fishercoder/solutions/_172.java
+++ src/main/java/com/fishercoder/solutions/_172.java
@@ -2,7 +2,7 @@ package com.fishercoder.solutions;
 /**Given an integer n, return the number of trailing zeroes in n!.
 
  Note: Your solution should be in logarithmic time complexity.*/
-public class FactorialTrailingZeroes {
+public class _172 {
 
     public int trailingZeroes(int n) {
         int result = 0;
