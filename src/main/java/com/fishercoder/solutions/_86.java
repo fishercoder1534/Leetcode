633abea23abee88a5cf10a361267f0e45b5cbc1b
stevesun <stevejsun@gmail.com>
2017-07-25 21:59:27 -0700

    edit 86

--- src/main/java/com/fishercoder/solutions/_86.java
+++ src/main/java/com/fishercoder/solutions/_86.java
@@ -3,6 +3,8 @@ package com.fishercoder.solutions;
 import com.fishercoder.common.classes.ListNode;
 
 /**
+ * 86. Partition List
+ *
  * Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
 
  You should preserve the original relative order of the nodes in each of the two partitions.
@@ -14,172 +16,24 @@ import com.fishercoder.common.classes.ListNode;
 public class _86 {
 
     public ListNode partition(ListNode head, int x) {
-        if(head == null){
-            return head;
+        if (head == null || head.next == null) return head;
+        ListNode left = new ListNode(0);
+        ListNode right = new ListNode(0);
+        ListNode less = left;
+        ListNode greater = right;
+        while (head != null) {
+            if (head.val < x) {
+                less.next = head;
+                less = less.next;
+            } else {
+                greater.next = head;
+                greater = greater.next;
+            }
+            head = head.next;
         }
-        else{
-            ListNode keeper, detector, detPre, keepNxt, myHead;/* initialize two pointers, "keeper" is used to
-			indicate where the next node that is smaller than x should be appended while
-			detector moves in the forefront to detect whether each node is smaller than x */
-            detector = head;
-            keeper = head;
-            detPre = head;
-            keepNxt = head;
-            myHead = new ListNode(Integer.MAX_VALUE);
-            myHead.next = head;
-
-            if(head.val >= x){
-                keeper = myHead;
-
-				/* we use two while loops:
-				 * first one to locate where the initial position of keeper should be;
-				 * second one to start traversing the whole linkedlist */
-                while(detector != null){
-					/* first while loop*/
-                    if(detector.val < x){
-                        detPre.next = detector.next;
-                        keeper = detector;
-                        keeper.next = head;
-                        keepNxt = keeper.next;
-                        myHead = keeper;
-                        detector = detPre.next;
-                        break;
-                    }
-                    else{
-                        if(detector.next != null){
-                            detPre = detector;
-                            detector = detector.next;
-                        }
-                        else{
-                            break;
-                        }
-                    }
-                }
-
-                while(detector != null){
-					/* second while loop */
-                    if(detector.val >= x){
-                        if(detector.next != null){
-                            detPre = detector;
-                            detector = detector.next;
-                        }
-                        else{
-                            if(Integer.MAX_VALUE == myHead.val){
-                                myHead = myHead.next;
-                            }
-                            else
-                                break;
-                        }
-                    }
-                    else{
-                        if(detector.next != null){
-                            detPre.next = detector.next;
-                            keeper.next = detector;
-                            keeper.next.next = keepNxt;
-                            detector = detPre.next;
-
-							/* then I'll have to update the keeper pointer and keepNxt pointer*/
-                            keeper = keeper.next;
-                            keepNxt = keeper.next;
-                        }
-                        else{
-                            keeper.next = detector;
-                            keeper.next.next = keepNxt;
-                            detPre.next = null;
-                            break;
-                        }
-                    }
-                    System.out.println("\nIn second while loop: keeper.val = " + keeper.val + "\tkeepNxt.val = " + keepNxt.val + "\tdetector.val = " + detector.val
-                            + "\tdetPre.val = " + detPre.val);
-                }
-                System.out.println();
-                ListNode temp = myHead;
-                while(temp != null){
-                    System.out.print(temp.val);
-                    temp = temp.next;
-                }
-                return myHead;
-            }
-            else{/* when the very first node is greater or equal than x */
-
-				/* we use two while loops:
-				 * first one to locate where the initial position of keeper should be;
-				 * second one to start traversing the whole linkedlist */
-                while(detector != null ){
-					/* first while loop*/
-                    if(detector.val >= x){
-                        break;
-                    }
-                    else{
-                        keeper = detector;
-                        detector = detector.next;
-                    }
-                }
-                if(detector != null){
-                    detPre = keeper;
-                    keepNxt = keeper.next;
-                    while(detector != null){
-						/* second while loop */
-                        if(detector.val >= x){
-                            if(detector.next != null){
-                                detPre = detector;
-                                detector = detector.next;
-                            }
-                            else{
-                                break;
-                            }
-                        }
-                        else{
-                            if(detector.next != null){
-                                detPre.next = detector.next;
-                                keeper.next = detector;
-                                keeper.next.next = keepNxt;
-                                detector = detPre.next;
-
-								/* then I'll have to update the keeper pointer and keepNxt pointer*/
-                                keeper = keeper.next;
-                                keepNxt = keeper.next;
-                            }
-                            else{
-                                keeper.next = detector;
-                                keeper.next.next = keepNxt;
-                                detPre.next = null;
-                                break;
-                            }
-                        }
-                        System.out.println("\nIn second while loop: keeper.val = " + keeper.val + "\tkeepNxt.val = " + keepNxt.val + "\tdetector.val = " + detector.val
-                                + "\tdetPre.val = " + detPre.val);
-                        ListNode temp = head;
-                        while(temp != null){
-                            System.out.print(temp.val);
-                            temp = temp.next;
-                        }
-                    }
-                    System.out.println();
-                    ListNode temp = head;
-                    while(temp != null){
-                        System.out.print(temp.val);
-                        temp = temp.next;
-                    }
-                    return head;
-                }
-                else if(detector == null){
-                    System.out.println("\nIn second while loop: keeper.val = " + keeper.val + "\tkeepNxt.val = " + keepNxt.val + "\tdetPre.val = " + detPre.val);
-                    ListNode temp = head;
-                    while(temp != null){
-                        System.out.print(temp.val);
-                        temp = temp.next;
-                    }
-                    return head;
-                }
-            }
-            ListNode temp = head;
-            while(temp != null){
-                System.out.print(temp.val);
-                temp = temp.next;
-            }
-            return head;
-        }
+        greater.next = null;
+        less.next = right.next;
+        return left.next;
     }
 
 }
