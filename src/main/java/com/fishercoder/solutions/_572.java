44aeeb1f0eb690f851e544bb4cde9a697e7d75b7
stevesun <fishercoder@gmail.com>
2017-07-30 15:49:30 -0700

    edit 572

--- src/main/java/com/fishercoder/solutions/_572.java
+++ src/main/java/com/fishercoder/solutions/_572.java
@@ -4,7 +4,9 @@ import com.fishercoder.common.classes.TreeNode;
 
 /**
  * 572. Subtree of Another Tree
- Given two non-empty binary trees s and t, check whether tree t has exactly the same structure and node values with a subtree of s. A subtree of s is a tree consists of a node in s and all of this node's descendants. The tree s could also be considered as a subtree of itself.
+ Given two non-empty binary trees s and t,
+ check whether tree t has exactly the same structure and node values with a subtree of s.
+ A subtree of s is a tree consists of a node in s and all of this node's descendants. The tree s could also be considered as a subtree of itself.
 
  Example 1:
  Given tree s:
