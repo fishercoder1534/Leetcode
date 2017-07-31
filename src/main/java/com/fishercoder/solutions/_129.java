b0b76d49b28ff2ca4810ef35b9a233d294352d2f
stevesun <fishercoder@gmail.com>
2017-07-31 13:17:09 -0700

    edit 129

--- src/main/java/com/fishercoder/solutions/_129.java
+++ src/main/java/com/fishercoder/solutions/_129.java
@@ -48,24 +48,16 @@ public class _129 {
         sb.deleteCharAt(sb.length()-1);
     }
     
-    public static void main(String... strings) {
-        more_concise_version test = new more_concise_version();
-        TreeNode root = new TreeNode(1);
-        root.left = new TreeNode(2);
-        root.right = new TreeNode(3);
-        System.out.println(test.sumNumbers(root));
+    class more_concise_version {
+        public int sumNumbers(TreeNode root) {
+            return dfs(root, 0);
+        }
+
+        private int dfs(TreeNode root, int sum) {
+            if (root == null) return 0;
+            if (root.left == null && root.right == null) return sum * 10 + root.val;
+            return dfs(root.left, sum * 10 + root.val) + dfs(root.right, sum * 10 + root.val);
+        }
     }
+
 }
-
-class more_concise_version{
-    public int sumNumbers(TreeNode root) {
-        return dfs(root, 0);
-    }
-
-    private int dfs(TreeNode root, int sum) {
-        if(root == null) return 0;
-        if(root.left == null && root.right == null) return sum*10+root.val;
-        return dfs(root.left, sum*10+root.val) + dfs(root.right, sum*10+root.val);
-    }
-}
-
