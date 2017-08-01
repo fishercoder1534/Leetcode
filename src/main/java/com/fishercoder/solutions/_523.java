b723e86568e07d6d5e5ba7fe29600bb4a0f6953d
stevesun <fishercoder@gmail.com>
2017-07-31 22:37:12 -0700

    edit 523

--- src/main/java/com/fishercoder/solutions/_523.java
+++ src/main/java/com/fishercoder/solutions/_523.java
@@ -27,6 +27,15 @@ import java.util.Map;
  */
 public class _523 {
 
+    /**reference: https://discuss.leetcode.com/topic/80793/java-o-n-time-o-k-space/20
+     *
+     * "The reason we use modulus is:
+     * (a+(n*x))%x is same as (a%x)
+     * e.g. in case of the array [23,2,6,4,7] the running sum is [23,25,31,35,42]
+     * and the remainders are [5,1,1,5,0].
+     * We got reminder 5 at index 0 and at index 3.
+     * That means, in between these two indexes we must have added a number which is multiple of the k.
+     * Hope this clarifies your doubt :)"*/
     public boolean checkSubarraySumOnTimeO1Space(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
         map.put(0, -1);
