b6fd947b91450f68c857d20ab8e541d0232c11f0
stevesun <fishercoder@gmail.com>
2017-08-01 07:52:09 -0700

    edit 1

--- src/main/java/com/fishercoder/solutions/_1.java
+++ src/main/java/com/fishercoder/solutions/_1.java
@@ -18,7 +18,7 @@ import java.util.Map;
  */
 public class _1 {
 
-    public int[] twoSum_ON(int[] nums, int target) {
+    public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap();
         int[] result = new int[2];
         for(int i = 0; i < nums.length; i++){
@@ -28,20 +28,6 @@ public class _1 {
                 break;
             } else {
                 map.put(nums[i], i);
-            }
-        }
-        return result;
-    }
-
-    public int[] twoSum_ON2(int[] nums, int target) {
-        int[] result = new int[2];
-        for(int i = 0; i < nums.length-1; i++){
-            for(int j = i+1; j < nums.length; j++){
-                if(nums[i] + nums[j] == target){
-                    result[0] = i;
-                    result[1] = j;
-                    break;
-                }
             }
         }
         return result;
