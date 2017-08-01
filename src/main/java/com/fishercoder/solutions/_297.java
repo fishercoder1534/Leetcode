c927d7c2b1c39d5ee10927ade2b9fcceda09e1f3
stevesun <fishercoder@gmail.com>
2017-08-01 09:01:59 -0700

    edit 297

--- src/main/java/com/fishercoder/solutions/_297.java
+++ src/main/java/com/fishercoder/solutions/_297.java
@@ -6,9 +6,15 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 /**
- * Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
-
- Design an algorithm to serialize and deserialize a binary tree. There is no restriction on how your serialization/deserialization algorithm should work. You just need to ensure that a binary tree can be serialized to a string and this string can be deserialized to the original tree structure.
+ * 297. Serialize and Deserialize Binary Tree
+ *
+ * Serialization is the process of converting a data structure or object into a sequence of bits
+ * so that it can be stored in a file or memory buffer,
+ * or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
+ * Design an algorithm to serialize and deserialize a binary tree.
+ * There is no restriction on how your serialization/deserialization algorithm should work.
+ * You just need to ensure that a binary tree can be serialized to a string and this string can
+ * be deserialized to the original tree structure.
 
  For example, you may serialize the following tree
 
@@ -18,7 +24,9 @@ import java.util.Queue;
   / \
  4   5
 
- as "[1,2,3,null,null,4,5]", just the same as how LeetCode OJ serializes a binary tree. You do not necessarily need to follow this format, so please be creative and come up with different approaches yourself.
+ as "[1,2,3,null,null,4,5]",
+ just the same as how LeetCode OJ serializes a binary tree.
+ You do not necessarily need to follow this format, so please be creative and come up with different approaches yourself.
 
  Note: Do not use class member/global/static variables to store states. Your serialize and deserialize algorithms should be stateless.
  */
@@ -27,7 +35,7 @@ public class _297 {
     /**The idea is very straightforward:
      use "#" as the terminator, do BFS, level order traversal to store all nodes values into a StringBuilder.
 
-     When deserializing, also, use a queue: pop the root into the queue first, then use a for loop to construct each node,
+     When deserializing, also use a queue: pop the root into the queue first, then use a for loop to construct each node,
      then eventually just return the root.
      */
 
