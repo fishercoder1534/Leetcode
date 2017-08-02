507ca82298fa882d15dc5a58841af6d99ca73458
stevesun <fishercoder@gmail.com>
2017-08-01 20:11:13 -0700

    edit 48

--- src/main/java/com/fishercoder/solutions/_48.java
+++ src/main/java/com/fishercoder/solutions/_48.java
@@ -12,18 +12,17 @@ package com.fishercoder.solutions;
  */
 public class _48 {
 
-    //credit: https://discuss.leetcode.com/topic/9744/ac-java-in-place-solution-with-explanation-easy-to-understand
     public void rotate_O1(int[][] matrix) {
         /**First swap the elements on the diagonal, then reverse each row:
          * 1, 2, 3                    1, 4, 7                      7, 4, 1
          * 4, 5, 6         becomes    2, 5, 8           becomes    8, 5, 2
          * 7, 8, 9                    3, 6, 9                      9, 6, 3
-         This could be done in O(1) space!
+         This is done in O(1) space!
          **/
         int m = matrix.length;
         int n = matrix[0].length;
         for (int i = 0; i < m; i++) {
-            for (int j = i; j < n; j++) {
+            for (int j = i; j < n; j++) {/**ATTN: j starts from i, so that the diagonal changes with itself, no change.*/
                 int tmp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = tmp;
@@ -39,13 +38,12 @@ public class _48 {
         }
     }
 
-    //credit: https://discuss.leetcode.com/topic/6796/a-common-method-to-rotate-the-image
     /**First swap the rows bottom up, then swap the element on the diagonal:
      * 1, 2, 3                         7, 8, 9                           7, 4, 1
      * 4, 5, 6           becomes       4, 5, 6           becomes         8, 5, 2
      * 7, 8, 9                         1, 2, 3                           9, 6, 3
      * */
-    //however, this is using O(n) of extra space
+    /**This is using O(n) of extra space*/
     public void rotate_On(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
