fa2be8929f37bdadd118b90fbebb79265f0fd90b
stevesun <fishercoder@gmail.com>
2017-08-03 08:19:10 -0700

    edit 5

--- src/main/java/com/fishercoder/solutions/_5.java
+++ src/main/java/com/fishercoder/solutions/_5.java
@@ -1,45 +1,44 @@
 package com.fishercoder.solutions;
 
 /**
+ * 5. Longest Palindromic Substring
+ *
  * Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
 
  Example:
-
  Input: "babad"
-
  Output: "bab"
-
  Note: "aba" is also a valid answer.
+
  Example:
-
  Input: "cbbd"
-
  Output: "bb"
  */
 public class _5 {
 
-    private int lo, maxLen;
+    private int low, maxLen;
 
     public String longestPalindrome(String s) {
         int len = s.length();
-        if (len < 2)
+        if (len < 2) {
             return s;
+        }
 
         for (int i = 0; i < len - 1; i++) {
             extendPalindrome(s, i, i); // assume odd length, try to extend Palindrome as possible
             extendPalindrome(s, i, i + 1); // assume even length.
         }
-        return s.substring(lo, lo + maxLen);
+        return s.substring(low, low + maxLen);
     }
 
-    private void extendPalindrome(String s, int j, int k) {
-        while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
-            j--;
-            k++;
+    private void extendPalindrome(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
         }
-        if (maxLen < k - j - 1) {
-            lo = j + 1;
-            maxLen = k - j - 1;
+        if (maxLen < right - left - 1) {
+            low = left + 1;
+            maxLen = right - left - 1;
         }
     }
 
