482edc69bae745086f98c0305511b3613454ecbd
stevesun <fishercoder@gmail.com>
2017-08-05 08:57:59 -0700

    edit 206

--- src/main/java/com/fishercoder/solutions/_206.java
+++ src/main/java/com/fishercoder/solutions/_206.java
@@ -2,13 +2,17 @@ package com.fishercoder.solutions;
 
 import com.fishercoder.common.classes.ListNode;
 
-/**Reverse a singly linked list.*/
+/**
+ * 206. Reverse Linked List
+ *
+ * Reverse a singly linked list.*/
 public class _206 {
-    //creating a newHead = null is a very common/smart way to handle such cases, the logic flows out very naturally:
-    //create a new node called "next" to hold current head's next node
-    //then we could redirect head's next pointer to point to newHead which is head's previous node
-    //the above two steps finished the reversion, to continue this process until we reach the end of the original list,
-    //we'll assign current "head" to new "newHead", and current "next" to be new "head" for the next iteration, here's the code
+
+    /**creating a newHead = null is a very common/smart way to handle such cases, the logic flows out very naturally:
+    create a new node called "next" to hold current head's next node
+    then we could redirect head's next pointer to point to newHead which is head's previous node
+    the above two steps finished the reversion, to continue this process until we reach the end of the original list,
+    we'll assign current "head" to new "newHead", and current "next" to be new "head" for the next iteration, here's the code*/
     public ListNode reverseList_iterative(ListNode head) {
         /**It works out the best to set up a debug point and visualize this process:
          * e.g. 1->2->3-null
@@ -26,30 +30,22 @@ public class _206 {
         return newHead;
     }
 
-    //following the above iterative version, the recursive solution flows out so naturally, basically, we just replaced the while loop with a recursive function
-    //still, a null newHead proves to be very helpful.
+    /**
+     * following the above iterative version, the recursive solution flows out so naturally, basically, we just replaced the while loop with a recursive function
+     * still, a null newHead proves to be very helpful.
+     */
     public ListNode reverseList_recursive(ListNode head) {
         ListNode newHead = null;
         return reverse(head, newHead);
     }
 
-    private ListNode reverse(ListNode head, ListNode newHead) {
-        if (head != null) {
-            ListNode next = head.next;
-            head.next = newHead;
-            newHead = head;
-            head = next;
-            return reverse(head, newHead);
-        } else return newHead;
-    }
-
-    //the above recursive function could of course be shortened to below, but the above one is just more intuitive and easier to follow and sort out your logic
-    private ListNode reverse_more_concise(ListNode head, ListNode newHead) {
-        if (head != null) {
-            ListNode next = head.next;
-            head.next = newHead;
-            return reverse_more_concise(next, head);
-        } else return newHead;
+    ListNode reverse(ListNode head, ListNode newHead) {
+        if (head == null) return newHead;
+        ListNode next = head.next;
+        head.next = newHead;
+        newHead = head;
+        head = next;
+        return reverse(head, newHead);
     }
 
 }
