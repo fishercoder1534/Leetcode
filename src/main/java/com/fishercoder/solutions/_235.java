9ca291d1f4d15731ef0cdcc8b22cd86ef5a9f7f1
stevesun <fishercoder@gmail.com>
2017-08-05 10:19:28 -0700

    edit 235

--- src/main/java/com/fishercoder/solutions/_235.java
+++ src/main/java/com/fishercoder/solutions/_235.java
@@ -2,9 +2,12 @@ package com.fishercoder.solutions;
 
 import com.fishercoder.common.classes.TreeNode;
 
-/**Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
-
- According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes v and w as the lowest node in T that has both v and w as descendants (where we allow a node to be a descendant of itself).”
+/**
+ * 235. Lowest Common Ancestor of a Binary Search Tree
+ * Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+ * According to the definition of LCA on Wikipedia:
+ * “The lowest common ancestor is defined between two nodes v and w as the lowest node in T that has both v and w as descendants
+ * (where we allow a node to be a descendant of itself).”
 
  _______6______
  /              \
@@ -13,15 +16,19 @@ import com.fishercoder.common.classes.TreeNode;
  0      _4       7       9
  /  \
  3   5
- For example, the lowest common ancestor (LCA) of nodes 2 and 8 is 6. Another example is LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+
+ For example, the lowest common ancestor (LCA) of nodes 2 and 8 is 6.
+ Another example is LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
 
  */
 public class _235 {
+
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null || p == root || q == root) return root;
-        if ((root.val - p.val) * (root.val - q.val) <= 0) return root;
-        if ((root.val - p.val) * (root.val - q.val) > 0 && (root.val - p.val) > 0) return lowestCommonAncestor(root.left, p, q);
-        if ((root.val - p.val) * (root.val - q.val) > 0 && (root.val - p.val) < 0) return lowestCommonAncestor(root.right, p, q);
+        if ((root.val - p.val) * (root.val - q.val) > 0) {
+            if (root.val - p.val > 0) return lowestCommonAncestor(root.left, p, q);
+            return lowestCommonAncestor(root.right, p, q);
+        }
         return root;
     }
 }
