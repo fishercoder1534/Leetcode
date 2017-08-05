f563b57df921d206def9a1f3152e6a5d48d3462b
stevesun <fishercoder@gmail.com>
2017-08-05 10:37:48 -0700

    edit 21

--- src/main/java/com/fishercoder/solutions/_21.java
+++ src/main/java/com/fishercoder/solutions/_21.java
@@ -2,10 +2,13 @@ package com.fishercoder.solutions;
 
 import com.fishercoder.common.classes.ListNode;
 
-/**Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.*/
+/**
+ * 21. Merge Two Sorted Lists
+ * Merge two sorted linked lists and return it as a new list.
+ * The new list should be made by splicing together the nodes of the first two lists.*/
+
 public class _21 {
 
-    //credit: https://discuss.leetcode.com/topic/45002/java-1-ms-4-lines-codes-using-recursion
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if (l1 == null) return l2;
         if (l2 == null) return l1;
