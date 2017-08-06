88c504e17287edc8452c9a10d7b434b468754178
Eason Yan <easonyan18@gmail.com>
2017-08-05 18:22:35 -0700

    Add title

--- src/main/java/com/fishercoder/solutions/_611.java
+++ src/main/java/com/fishercoder/solutions/_611.java
@@ -3,6 +3,7 @@ package com.fishercoder.solutions;
 import java.util.Arrays;
 
 /**
+ * 611. Valid Triangle Number
  * Given an array consists of non-negative integers,
  * your task is to count the number of triplets chosen
  * from the array that can make triangles if we take them as side lengths of a triangle.
