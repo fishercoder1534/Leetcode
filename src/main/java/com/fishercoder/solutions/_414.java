3a65dc89b3145243f77c9a74b0d5eaa6d86c17ca
stevesun <fishercoder@gmail.com>
2017-08-08 09:26:15 -0700

    edit 414

--- src/main/java/com/fishercoder/solutions/_414.java
+++ src/main/java/com/fishercoder/solutions/_414.java
@@ -28,56 +28,20 @@ package com.fishercoder.solutions;
  */
 public class _414 {
 
-    public static int thirdMax(int[] nums) {
-
-        if (nums == null || nums.length == 0) return 0;
-        if (nums.length < 3) {
-           int max = 0;
-           for (int i : nums) {
-               max = Math.max(i, max);
-           }
-           return max;
+    public int thirdMax(int[] nums) {
+        long max1 = Long.MIN_VALUE, max2 = Long.MIN_VALUE, max3 = Long.MIN_VALUE;
+        for (int i : nums) {
+            max1 = Math.max(max1, i);
         }
-        
-        long first = Long.MIN_VALUE, second = Long.MIN_VALUE, third = Long.MIN_VALUE;
-        
-        for (int i = 0; i < nums.length; i++){
-            if (nums[i] > first) {
-                long tmpFirst = first;
-                first = nums[i];
-                
-                long tmpSecond = second;
-                second = tmpFirst;
-                
-                third = tmpSecond;
-            } else if (nums[i] == first) continue;
-            else if (nums[i] > second) {
-                long tmpSecond = second;
-                second = nums[i];
-                
-                third = tmpSecond;
-            } else if (nums[i] == second) continue; 
-            else if (nums[i] > third) {
-                third = nums[i];
-            }
+        for (int i : nums) {
+            if (i == max1) continue;
+            max2 = Math.max(max2, i);
         }
-        
-        if (third == Long.MIN_VALUE){
-           return (int) first;
+        for (int i : nums) {
+            if (i == max1 || i == max2) continue;
+            max3 = Math.max(max3, i);
         }
-        
-        return (int) third;
-    
-    }
-    
-    public static void main(String...strings){
-//        int[] nums = new int[]{1,2};
-//        int[] nums = new int[]{2,2,3,1};
-//        int[] nums = new int[]{1,1,2};//should be 2
-        int[] nums = new int[]{1,2,-2147483648};//should be -2147483648
-//        int[] nums = new int[]{3,2,1};
-//        System.out.println(thirdMax(nums));
-        System.out.println(thirdMax(nums));
+        return (int) (max3 == Long.MIN_VALUE ? max1 : max3);
     }
 
 }
