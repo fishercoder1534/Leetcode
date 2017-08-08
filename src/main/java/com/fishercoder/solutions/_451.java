09aa9102335d194024dd5c02a61f6e6ba8f653c0
Eason Yan <easonyan18@gmail.com>
2017-08-08 13:20:09 -0700

    format and add main()

--- src/main/java/com/fishercoder/solutions/_451.java
+++ src/main/java/com/fishercoder/solutions/_451.java
@@ -44,17 +44,23 @@ import java.util.*;
  */
 public class _451 {
 
-    public String frequencySort(String s) {
+    public static String frequencySort(String s) {
         Map<Character, Integer> map = new HashMap();
-        for (char c : s.toCharArray()) map.put(c, map.getOrDefault(c, 0) + 1);
-            List<Map.Entry<Character, Integer>> list = new ArrayList<>(map.entrySet());
-            Collections.sort(list, (o1, o2) -> (o2.getValue()).compareTo(o1.getValue()));
-            StringBuilder stringBuilder = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        List<Map.Entry<Character, Integer>> list = new ArrayList<>(map.entrySet());
+        Collections.sort(list, (o1, o2) -> (o2.getValue()).compareTo(o1.getValue()));
+        StringBuilder stringBuilder = new StringBuilder();
         for (Map.Entry<Character, Integer> entry : list) {
             for (int i = 0; i < entry.getValue(); i++) {
                 stringBuilder.append(entry.getKey());
             }
         }
         return stringBuilder.toString();
+    }
+    public static void main(String[] args) {
+        String s = "trete" ;
+        System.out.println(frequencySort(s));
     }
 }
