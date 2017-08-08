b2c80a414fc8df6bbf6039f6b5edd0186a68caa5
stevesun <fishercoder@gmail.com>
2017-08-07 23:14:36 -0700

    edit 90

--- src/main/java/com/fishercoder/solutions/_90.java
+++ src/main/java/com/fishercoder/solutions/_90.java
@@ -1,18 +1,14 @@
 package com.fishercoder.solutions;
 
-import com.fishercoder.common.utils.CommonUtils;
-
 import java.util.*;
 
 /**90. Subsets II
  *
- *  Given a collection of integers that might contain duplicates, nums, return all possible subsets.
-
+ *Given a collection of integers that might contain duplicates, nums, return all possible subsets.
  Note: The solution set must not contain duplicate subsets.
 
  For example,
  If nums = [1,2,2], a solution is:
-
  [
  [2],
  [1],
@@ -24,32 +20,47 @@ import java.util.*;
  */
 public class _90 {
 
-    public static List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> result = new ArrayList();
-        List<Integer> empty = new ArrayList();
-        result.add(empty);
-        if(nums == null) return result;
-        Arrays.sort(nums);
-        for(int i = 0; i < nums.length; i++){
-            Set<List<Integer>> temp = new HashSet();
-            for(List<Integer> list : result){
-                List<Integer> newList = new ArrayList(list);
-                newList.add(nums[i]);
-                temp.add(newList);
+    public static class IterativeSolution {
+        public static List<List<Integer>> subsetsWithDup(int[] nums) {
+            List<List<Integer>> result = new ArrayList();
+            List<Integer> empty = new ArrayList();
+            result.add(empty);
+            if (nums == null) return result;
+            Arrays.sort(nums);
+            for (int i = 0; i < nums.length; i++) {
+                Set<List<Integer>> temp = new HashSet();
+                for (List<Integer> list : result) {
+                    List<Integer> newList = new ArrayList(list);
+                    newList.add(nums[i]);
+                    temp.add(newList);
+                }
+                result.addAll(temp);
             }
-            result.addAll(temp);
+            Set<List<Integer>> resultSet = new HashSet();
+            resultSet.addAll(result);
+            result.clear();
+            result.addAll(resultSet);
+            return result;
         }
-        Set<List<Integer>> resultSet = new HashSet();
-        resultSet.addAll(result);
-        result.clear();
-        result.addAll(resultSet);
-        return result;
     }
     
-    public static void main(String...args){
-        int[] nums = new int[]{1,2,2};
-        List<List<Integer>> result = subsetsWithDup(nums);
-        CommonUtils.printListList(result);
+    public static class BacktrackingSolution {
+        public List<List<Integer>> subsetsWithDup(int[] nums) {
+            List<List<Integer>> result = new ArrayList();
+            Arrays.sort(nums);
+            backtrack(nums, 0, new ArrayList(), result);
+            return result;
+        }
+
+        void backtrack(int[] nums, int start, List<Integer> curr, List<List<Integer>> result) {
+            result.add(new ArrayList(curr));
+            for (int i = start; i < nums.length; i++) {
+                if (i > start && nums[i] == nums[i - 1]) continue;
+                curr.add(nums[i]);
+                backtrack(nums, i + 1, curr, result);
+                curr.remove(curr.size() - 1);
+            }
+        }
     }
 
 }
