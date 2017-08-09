0ddb2e5bfb1dd6c1c98ea08ac0c362b7bb45030f
stevesun <fishercoder@gmail.com>
2017-08-08 21:46:58 -0700

    edit 118

--- src/main/java/com/fishercoder/solutions/_118.java
+++ src/main/java/com/fishercoder/solutions/_118.java
@@ -1,7 +1,9 @@
 package com.fishercoder.solutions;
 import java.util.*;
 
-/**Given numRows, generate the first numRows of Pascal's triangle.
+/**
+ * 118. Pascal's Triangle
+ * Given numRows, generate the first numRows of Pascal's triangle.
 
  For example, given numRows = 5,
  Return
@@ -12,30 +14,29 @@ import java.util.*;
    [1,2,1],
   [1,3,3,1],
  [1,4,6,4,1]
- ]*/
+ ]
+
+ */
 public class _118 {
 
-    public static List<List<Integer>> generate(int numRows) {
+    public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> result = new ArrayList();
-        if(numRows < 1) return result;
-        List<Integer> row = new ArrayList();
-        row.add(1);
-        result.add(row);
-        for(int i = 1; i < numRows; i++){
-            List<Integer> newRow = new ArrayList();
-            newRow.add(1);
-            List<Integer> lastRow = result.get(i-1);
-            for(int j = 1; j < lastRow.size(); j++){
-                newRow.add(lastRow.get(j-1) + lastRow.get(j));
+        int len = 1;
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList(len);
+            row.add(1);
+            if (i > 0) {
+                List<Integer> lastRow = result.get(i - 1);
+                for (int j = 1; j < len; j++) {
+                    if (j < lastRow.size()) {
+                        row.add(lastRow.get(j - 1) + lastRow.get(j));
+                    }
+                }
+                row.add(1);
             }
-            newRow.add(1);
-            result.add(newRow);
+            result.add(row);
+            len++;
         }
         return result;
-    }
-
-    public static void main(String...strings){
-        int numRows = 2;
-        generate(numRows);
     }
 }
