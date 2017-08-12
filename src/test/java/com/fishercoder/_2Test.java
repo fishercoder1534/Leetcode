d03c3334a9aee96966398400d60997b98f852b26
stevesun <fishercoder@gmail.com>
2017-08-12 13:47:09 -0700

    edit 2 and add LinkedListUtils

---
+++ src/test/java/com/fishercoder/_2Test.java
@@ -0,0 +1,45 @@
+package com.fishercoder;
+
+import com.fishercoder.common.classes.ListNode;
+import com.fishercoder.common.utils.LinkedListUtils;
+import com.fishercoder.solutions._2;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class _2Test {
+    private static _2.Solution2 solution2;
+    private static ListNode l1;
+    private static ListNode l2;
+    private static ListNode expected;
+
+    @BeforeClass
+    public static void setup(){
+        solution2 = new _2.Solution2();
+    }
+
+    @Test
+    public void test1(){
+        l1 = LinkedListUtils.contructLinkedList(new int[]{2, 4, 3});
+        l2 = LinkedListUtils.contructLinkedList(new int[]{5, 6, 4});
+        expected = LinkedListUtils.contructLinkedList(new int[]{7, 0, 8});
+        assertEquals(expected, solution2.addTwoNumbers(l1, l2));
+    }
+
+    @Test
+    public void test2(){
+        l1 = LinkedListUtils.contructLinkedList(new int[]{1, 8});
+        l2 = LinkedListUtils.contructLinkedList(new int[]{0});
+        expected = LinkedListUtils.contructLinkedList(new int[]{1, 8});
+        assertEquals(expected, solution2.addTwoNumbers(l1, l2));
+    }
+
+    @Test
+    public void test3(){
+        l1 = LinkedListUtils.contructLinkedList(new int[]{5});
+        l2 = LinkedListUtils.contructLinkedList(new int[]{5});
+        expected = LinkedListUtils.contructLinkedList(new int[]{0, 1});
+        assertEquals(expected, solution2.addTwoNumbers(l1, l2));
+    }
+}
