9f3e3ee5ed32d40ad502857f53f07135b250f8ca
xdongyan <easonyan18@gmail.com>
2017-08-12 13:07:28 -0700

    Eason (#10)
    * edit 344
    
    * edit 344

--- src/main/java/com/fishercoder/solutions/_344.java
+++ src/main/java/com/fishercoder/solutions/_344.java
@@ -12,20 +12,15 @@ public class _344 {
         return new StringBuilder(s).reverse().toString();
     }
     
-    public String reverseString(String s) {
+    public String reverseString(String s){
         int i = 0, j = s.length()-1;
         char[] chars = s.toCharArray();
         while(i < j){
             char temp = chars[i];
-            chars[i] = chars[j];
-            chars[j] = temp;
-            i++;
-            j--;
+            chars[i++] = chars[j];
+            chars[j--] = temp;
+
         }
-        StringBuilder sb = new StringBuilder();
-        for(char c : chars){
-            sb.append(c);
-        }
-        return sb.toString();
+        return new String(chars);
     }
 }
