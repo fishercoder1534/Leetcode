d1fab0e9e4d51a60cbb17433e0b01e1685882a45
stevesun <fishercoder@gmail.com>
2017-08-16 09:13:13 -0700

    refactor 536

--- src/test/java/com/fishercoder/_536Test.java
+++ src/test/java/com/fishercoder/_536Test.java
@@ -9,14 +9,16 @@ import org.junit.Test;
 import static junit.framework.Assert.assertEquals;
 
 public class _536Test {
-    private static _536 test;
+    private static _536.Solution1 solution1;
+    private static _536.Solution2 solution2;
     private static TreeNode expected;
     private static TreeNode actual;
     private static String s;
 
     @BeforeClass
     public static void setup(){
-        test = new _536();
+        solution1 = new _536.Solution1();
+        solution2 = new _536.Solution2();
     }
 
     @Before
@@ -30,8 +32,10 @@ public class _536Test {
     public void test1(){
         s = "";
         expected = null;
-        actual = test.str2tree(s);
+        actual = solution1.str2tree(s);
         assertEquals(expected, actual);
 
+        actual = solution2.str2tree(s);
+        assertEquals(expected, actual);
     }
 }
