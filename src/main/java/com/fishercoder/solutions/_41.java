6759a4bd030bc60b2ba5081465be7e3a5628cf07
stevesun <fishercoder@gmail.com>
2017-08-16 07:49:05 -0700

    refactor 41

--- src/main/java/com/fishercoder/solutions/_41.java
+++ src/main/java/com/fishercoder/solutions/_41.java
@@ -1,6 +1,7 @@
 package com.fishercoder.solutions;
 
 /**
+ *41. First Missing Positive
  * Given an unsorted integer array, find the first missing positive integer.
 
  For example,
@@ -8,25 +9,28 @@ package com.fishercoder.solutions;
  and [3,4,-1,1] return 2.
 
  Your algorithm should run in O(n) time and uses constant space.
-
  */
+
 public class _41 {
 
     public int firstMissingPositive(int[] A) {
         int i = 0;
         while (i < A.length) {
-            if (A[i] > 0 && A[i] != i + 1 && A[i] - 1 < A.length
+            if (A[i] > 0 && A[i] != i + 1
+                    && A[i] - 1 < A.length
                     && A[i] != A[A[i] - 1]) {
                 swap(A, i, A[i] - 1);
             } else {
                 i++;
             }
         }
+
         for (int j = 0; j < A.length; j++) {
             if (A[j] != j + 1) {
                 return j + 1;
             }
         }
+
         return A.length + 1;// if all values are in the correct position, then
         // we return the last one + 1
     }
