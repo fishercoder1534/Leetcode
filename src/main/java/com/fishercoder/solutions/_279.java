a91046f187f227d5983758a79e731957acc89a55
stevesun <fishercoder@gmail.com>
2017-08-16 20:01:11 -0700

    reformat 279

--- src/main/java/com/fishercoder/solutions/_279.java
+++ src/main/java/com/fishercoder/solutions/_279.java
@@ -23,11 +23,11 @@ public class _279 {
         Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
         dp[1] = 1;
-        for(int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             int min = Integer.MAX_VALUE;
             int j = 1;
-            while(i -j*j >= 0) {
-                min = Math.min(min, dp[i - j*j] + 1);
+            while (i - j * j >= 0) {
+                min = Math.min(min, dp[i - j * j] + 1);
                 j++;
             }
             dp[i] = min;
