ddc166e4f7af8f33228aa8ce30f93dc7f922f00e
stevesun <fishercoder@gmail.com>
2017-08-19 11:01:11 -0700

    refactor 186

--- src/main/java/com/fishercoder/solutions/_186.java
+++ src/main/java/com/fishercoder/solutions/_186.java
@@ -1,6 +1,8 @@
 package com.fishercoder.solutions;
 
 /**
+ * 186. Reverse Words in a String II
+ *
  * Given an input string, reverse the string word by word. A word is defined as a sequence of non-space characters.
 
  The input string does not contain leading or trailing spaces and the words are always separated by a single space.
@@ -32,10 +34,8 @@ public class _186 {
     private void reverse(char[] s, int start, int end) {
         while (start < end) {
             char temp = s[start];
-            s[start] = s[end];
-            s[end] = temp;
-            start++;
-            end--;
+            s[start++] = s[end];
+            s[end--] = temp;
         }
     }
 
