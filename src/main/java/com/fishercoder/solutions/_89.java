b16de1a5fb9eeadcf7c231c6fff02a9fa51d5793
stevesun <fishercoder@gmail.com>
2017-08-19 14:01:00 -0700

    refactor 89

--- src/main/java/com/fishercoder/solutions/_89.java
+++ src/main/java/com/fishercoder/solutions/_89.java
@@ -1,5 +1,7 @@
 package com.fishercoder.solutions;
 
+import com.fishercoder.common.utils.CommonUtils;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,8 +9,9 @@ import java.util.List;
  * 89. Gray Code
  *
  * The gray code is a binary numeral system where two successive values differ in only one bit.
-
- Given a non-negative integer n representing the total number of bits in the code, print the sequence of gray code. A gray code sequence must begin with 0.
+ * Given a non-negative integer n representing the total number of bits in the code,
+ * print the sequence of gray code.
+ * A gray code sequence must begin with 0.
 
  For example, given n = 2, return [0,1,3,2]. Its gray code sequence is:
 
@@ -16,7 +19,9 @@ import java.util.List;
  01 - 1
  11 - 3
  10 - 2
+
  Note:
+
  For a given n, a gray code sequence is not uniquely defined.
 
  For example, [0,2,3,1] is also a valid gray code sequence according to the above definition.
@@ -34,4 +39,11 @@ public class _89 {
         return result;
     }
 
+    public static void main(String... args) {
+        int n = 3;
+        System.out.println("1 << n = " + (1 << n));
+        _89 test = new _89();
+        List<Integer> result = test.grayCode(n);
+        CommonUtils.printList(result);
+    }
 }
