84a5d38401e342cd656f226bb3b98dbf1c4a237e
stevesun <stevesun@coupang.com>
2017-08-21 18:23:33 -0700

    enable ParameterName check

--- src/main/java/com/fishercoder/solutions/_568.java
+++ src/main/java/com/fishercoder/solutions/_568.java
@@ -83,19 +83,19 @@ public class _568 {
         return dfs(flights, days, 0, 0, memo);
     }
 
-    public int dfs(int[][] flights, int[][] days, int cur_city, int weekno, int[][] memo) {
+    public int dfs(int[][] flights, int[][] days, int curCity, int weekno, int[][] memo) {
         if (weekno == days[0].length)
             return 0;
-        if (memo[cur_city][weekno] != Integer.MIN_VALUE)
-            return memo[cur_city][weekno];
+        if (memo[curCity][weekno] != Integer.MIN_VALUE)
+            return memo[curCity][weekno];
         int maxvac = 0;
         for (int i = 0; i < flights.length; i++) {
-            if (flights[cur_city][i] == 1 || i == cur_city) {
+            if (flights[curCity][i] == 1 || i == curCity) {
                 int vac = days[i][weekno] + dfs(flights, days, i, weekno + 1, memo);
                 maxvac = Math.max(maxvac, vac);
             }
         }
-        memo[cur_city][weekno] = maxvac;
+        memo[curCity][weekno] = maxvac;
         return maxvac;
     }
 
