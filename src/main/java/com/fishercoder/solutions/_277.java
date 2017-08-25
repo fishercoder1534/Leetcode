4a1170b639b08522c529e4380f0291da19f6572b
stevesun <stevesun@coupang.com>
2017-08-25 08:47:49 -0700

    refactor for format

--- src/main/java/com/fishercoder/solutions/_277.java
+++ src/main/java/com/fishercoder/solutions/_277.java
@@ -11,11 +11,14 @@ public class _277 {
     public int findCelebrity(int n) {
         int candidate = 0;
         for (int i = 1; i < n; i++) {
-            if (knows(candidate, i))
+            if (knows(candidate, i)) {
                 candidate = i;
+            }
         }
         for (int i = 0; i < n; i++) {
-            if (i != candidate && (knows(candidate, i) || !knows(i, candidate))) return -1;
+            if (i != candidate && (knows(candidate, i) || !knows(i, candidate))) {
+                return -1;
+            }
         }
         return candidate;
     }
--- src/main/java/com/fishercoder/solutions/_278.java
+++ src/main/java/com/fishercoder/solutions/_278.java
@@ -10,19 +10,22 @@ public class _278 {
 	public int firstBadVersion(int n) {
 		int left = 1;
 		int right = n;
-		if (isBadVersion(left))
+		if (isBadVersion(left)) {
 			return left;
+		}
 
 		while (left + 1 < right) {
 			int mid = left + (right - left) / 2;
-			if (isBadVersion(mid))
+			if (isBadVersion(mid)) {
 				right = mid;
-			else
+			} else {
 				left = mid;
+			}
 		}
 
-		if (isBadVersion(left))
+		if (isBadVersion(left)) {
 			return left;
+		}
 		return right;
 	}
 
