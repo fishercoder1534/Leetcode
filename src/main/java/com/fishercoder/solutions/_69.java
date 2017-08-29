a62b5b908f1cd147d15eaf81b613777679177d95
stevesun <stevesun@coupang.com>
2017-08-29 08:52:41 -0700

    refactor 69

--- src/main/java/com/fishercoder/solutions/_69.java
+++ src/main/java/com/fishercoder/solutions/_69.java
@@ -1,27 +1,26 @@
 package com.fishercoder.solutions;
 
 /**
+ * 69. Sqrt(x)
  * Implement int sqrt(int x).
-
- Compute and return the square root of x.
-
- * Created by fishercoder on 1/25/17.
+ * Compute and return the square root of x.
  */
+
 public class _69 {
     public int mySqrt(int x) {
-        long i = 0;
-        long j = x / 2 + 1;
-        while (i <= j) {
-            long mid = (i + j) / 2;
+        long left = 0;
+        long right = x / 2 + 1;
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
             long result = mid * mid;
             if (result == (long) x) {
                 return (int) mid;
             } else if (result > x) {
-                j = mid - 1;
+                right = mid - 1;
             } else {
-                i = mid + 1;
+                left = mid + 1;
             }
         }
-        return (int) j;
+        return (int) right;
     }
 }
