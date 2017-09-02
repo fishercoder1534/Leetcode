b11d11ae681a88feb024068c12dd8adba77aad58
stevesun <fishercoder@gmail.com>
2017-09-02 10:15:02 -0700

    refactor 6

--- src/main/java/com/fishercoder/solutions/_6.java
+++ src/main/java/com/fishercoder/solutions/_6.java
@@ -1,16 +1,20 @@
 package com.fishercoder.solutions;
 
-/**The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+/**
+ * 6. ZigZag Conversion
+ *
+ * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this:
+ * (you may want to display this pattern in a fixed font for better legibility)
 
  P   A   H   N
  A P L S I I G
  Y   I   R
+
  And then read line by line: "PAHNAPLSIIGYIR"
  Write the code that will take a string and make this conversion given a number of rows:
 
  string convert(string text, int nRows);
  convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
-
  */
 
 public class _6 {
