90cf3be3d4c28887701727a1ba7f8901f70c57ab
stevesun <fishercoder@gmail.com>
2017-09-04 17:49:44 -0700

    add 601

--- README.md
+++ README.md
@@ -632,6 +632,7 @@ Your ideas/fixes/algorithms are more than welcome!
 |608|[Tree Node](https://leetcode.com/problems/tree-node/)|[Solution](../master/database/_608.sql) | | | Medium | Union
 |607|[Sales Person](https://leetcode.com/problems/sales-person/)|[Solution](../master/database/_607.sql) | | | Easy |
 |602|[Friend Requests II: Who Has the Most Friends](https://leetcode.com/problems/friend-requests-ii-who-has-the-most-friends/)|[Solution](../master/database/_602.sql) | | | Medium |
+|601|[Human Traffic of Stadium](https://leetcode.com/problems/human-traffic-of-stadium/)|[Solution](../master/database/_601.sql) | | | Hard |
 |597|[Friend Requests I: Overall Acceptance Rate](https://leetcode.com/problems/friend-requests-i-overall-acceptance-rate/)|[Solution](../master/database/_597.sql) | | | Easy |
 |596|[Classes More Than 5 Students](https://leetcode.com/problems/classes-more-than-5-students/)|[Solution](../master/database/_596.sql) | || Easy |
 |595|[Big Countries](https://leetcode.com/problems/big-countries/)|[Solution](../master/database/_595.sql) | O(n) |O(1) | Easy |
---
+++ database/_601.sql
@@ -0,0 +1,50 @@
+-- 601. Human Traffic of Stadium
+--
+--X city built a new stadium, each day many people visit it and the stats are saved as these columns: id, date, people
+--
+--Please write a query to display the records which have 3 or more consecutive rows and the amount of people more than 100(inclusive).
+--For example, the table stadium:
+--
+--+------+------------+-----------+
+--| id   | date       | people    |
+--+------+------------+-----------+
+--| 1    | 2017-01-01 | 10        |
+--| 2    | 2017-01-02 | 109       |
+--| 3    | 2017-01-03 | 150       |
+--| 4    | 2017-01-04 | 99        |
+--| 5    | 2017-01-05 | 145       |
+--| 6    | 2017-01-06 | 1455      |
+--| 7    | 2017-01-07 | 199       |
+--| 8    | 2017-01-08 | 188       |
+--+------+------------+-----------+
+--
+--For the sample data above, the output is:
+--
+--+------+------------+-----------+
+--| id   | date       | people    |
+--+------+------------+-----------+
+--| 5    | 2017-01-05 | 145       |
+--| 6    | 2017-01-06 | 1455      |
+--| 7    | 2017-01-07 | 199       |
+--| 8    | 2017-01-08 | 188       |
+--+------+------------+-----------+
+--
+--Note:
+--Each day only have one row record, and the dates are increasing with id increasing.
+
+SELECT s1.* FROM stadium AS s1, stadium AS s2, stadium as s3
+    WHERE
+    ((s1.id + 1 = s2.id
+    AND s1.id + 2 = s3.id)
+    OR
+    (s1.id - 1 = s2.id
+    AND s1.id + 1 = s3.id)
+    OR
+    (s1.id - 2 = s2.id
+    AND s1.id - 1 = s3.id)
+    )
+    AND s1.people>=100
+    AND s2.people>=100
+    AND s3.people>=100
+
+    GROUP BY s1.id
