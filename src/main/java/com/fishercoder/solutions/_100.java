adae65502902f29edf339957757a4fe73fc2ac42
stevesun <fishercoder@gmail.com>
2017-09-06 08:09:57 -0700

    refactor 100

--- src/main/java/com/fishercoder/solutions/_100.java
+++ src/main/java/com/fishercoder/solutions/_100.java
@@ -4,18 +4,17 @@ import com.fishercoder.common.classes.TreeNode;
 
 /**
  * 100. Same Tree
- * <p>
  * Given two binary trees, write a function to check if they are equal or not.
- * <p>
  * Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
  */
 
 public class _100 {
-    //recursion idea flows out naturally.
+
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null || q == null) {
             return p == q;
         }
         return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
+
 }
