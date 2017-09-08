4351ac5eba4ff2b89aad05ee4302e1bffb567a3f
stevesun <fishercoder@gmail.com>
2017-09-08 07:51:05 -0700

    add one random question

---
+++ src/test/java/com/fishercoder/_99999RandomQuestionsTest.java
@@ -0,0 +1,79 @@
+package com.fishercoder;
+
+import com.fishercoder.solutions._99999RandomQuestions;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by stevesun on 9/7/17.
+ */
+public class _99999RandomQuestionsTest {
+    private static _99999RandomQuestions test;
+
+    @BeforeClass
+    public static void setup() {
+        test = new _99999RandomQuestions();
+    }
+
+    @Test
+    public void test1() {
+        assertEquals(true, test.isValid("()"));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(true, test.isValid("(*)"));
+    }
+
+    @Test
+    public void test3() {
+        assertEquals(true, test.isValid("(*))"));
+    }
+
+    @Test
+    public void test4() {
+        assertEquals(false, test.isValid(")("));
+    }
+
+    @Test
+    public void test5() {
+        assertEquals(false, test.isValid("(*()"));
+    }
+
+    @Test
+    public void test6() {
+        assertEquals(false, test.isValid("((*)"));
+    }
+
+    @Test
+    public void test7() {
+        assertEquals(true, test.isValid("((*)))"));
+    }
+
+    @Test
+    public void test8() {
+        assertEquals(true, test.isValid("()()"));
+    }
+
+    @Test
+    public void test9() {
+        assertEquals(true, test.isValid("(((())))"));
+    }
+
+    @Test
+    public void test10() {
+        assertEquals(true, test.isValid("(((******)))"));
+    }
+
+    @Test
+    public void test11() {
+        assertEquals(false, test.isValid("(((******))"));
+    }
+
+    @Test
+    public void test12() {
+        assertEquals(true, test.isValid("((*)****)"));
+    }
+}
