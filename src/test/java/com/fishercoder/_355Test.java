b0c06a385133902994d31861941d546ca74fd2ad
stevesun <fishercoder@gmail.com>
2017-09-10 17:41:43 -0700

    edit 355

--- src/test/java/com/fishercoder/_355Test.java
+++ src/test/java/com/fishercoder/_355Test.java
@@ -1,6 +1,7 @@
 package com.fishercoder;
 
 import com.fishercoder.solutions._355;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -12,29 +13,103 @@ import static org.junit.Assert.assertEquals;
  * Created by fishercoder on 5/10/17.
  */
 public class _355Test {
-    private static _355.Twitter twitter;
+    private static _355.Solution1.Twitter solution1Twitter;
+    private static _355.Solution2.Twitter solution2Twitter;
 
     @BeforeClass
     public static void setup() {
-        twitter = new _355.Twitter();
+        solution1Twitter = new _355.Solution1.Twitter();
+        solution2Twitter = new _355.Solution2.Twitter();
+    }
+
+    @Before
+    public void cleanUp() {
+        solution1Twitter = new _355.Solution1.Twitter();
+        solution2Twitter = new _355.Solution2.Twitter();
     }
 
     @Test
     public void test1() {
-        twitter.postTweet(1, 5);
-        List<Integer> user1newsFeed = twitter.getNewsFeed(1);
+        solution1Twitter.postTweet(1, 5);
+        List<Integer> user1newsFeed = solution1Twitter.getNewsFeed(1);
         assertEquals(1, user1newsFeed.size());
         assertEquals(5, (int) user1newsFeed.get(0));
 
-        twitter.follow(1, 2);
-        twitter.postTweet(2, 6);
-        user1newsFeed = twitter.getNewsFeed(1);
+        solution1Twitter.follow(1, 2);
+        solution1Twitter.postTweet(2, 6);
+        user1newsFeed = solution1Twitter.getNewsFeed(1);
         assertEquals(2, user1newsFeed.size());
         assertEquals(6, (int) user1newsFeed.get(0));
         assertEquals(5, (int) user1newsFeed.get(1));
 
-        twitter.unfollow(1, 2);
-        user1newsFeed = twitter.getNewsFeed(1);
+        solution1Twitter.unfollow(1, 2);
+        user1newsFeed = solution1Twitter.getNewsFeed(1);
         assertEquals(1, user1newsFeed.size());
+    }
+
+    @Test
+    public void test2() {
+        solution2Twitter.postTweet(1, 5);
+        List<Integer> user1newsFeed = solution2Twitter.getNewsFeed(1);
+        assertEquals(1, user1newsFeed.size());
+        assertEquals(5, (int) user1newsFeed.get(0));
+
+        solution2Twitter.follow(1, 2);
+        solution2Twitter.postTweet(2, 6);
+        user1newsFeed = solution2Twitter.getNewsFeed(1);
+        assertEquals(2, user1newsFeed.size());
+        assertEquals(6, (int) user1newsFeed.get(0));
+        assertEquals(5, (int) user1newsFeed.get(1));
+
+        solution2Twitter.unfollow(1, 2);
+        user1newsFeed = solution2Twitter.getNewsFeed(1);
+        assertEquals(1, user1newsFeed.size());
+    }
+
+    @Test
+    public void test3() {
+        solution2Twitter.postTweet(1, 1);
+        List<Integer> user1newsFeed = solution2Twitter.getNewsFeed(1);
+        assertEquals(1, user1newsFeed.size());
+        assertEquals(1, (int) user1newsFeed.get(0));
+
+        solution2Twitter.follow(2, 1);
+        user1newsFeed = solution2Twitter.getNewsFeed(2);
+        assertEquals(1, user1newsFeed.size());
+        assertEquals(1, (int) user1newsFeed.get(0));
+
+        solution2Twitter.unfollow(2, 1);
+        user1newsFeed = solution2Twitter.getNewsFeed(2);
+        assertEquals(0, user1newsFeed.size());
+    }
+
+    @Test
+    public void test4() {
+        solution1Twitter.postTweet(1, 1);
+        List<Integer> user1newsFeed = solution1Twitter.getNewsFeed(1);
+        assertEquals(1, user1newsFeed.size());
+        assertEquals(1, (int) user1newsFeed.get(0));
+
+        solution1Twitter.follow(2, 1);
+        user1newsFeed = solution1Twitter.getNewsFeed(2);
+        assertEquals(1, user1newsFeed.size());
+        assertEquals(1, (int) user1newsFeed.get(0));
+
+        solution1Twitter.unfollow(2, 1);
+        user1newsFeed = solution1Twitter.getNewsFeed(2);
+        assertEquals(0, user1newsFeed.size());
+    }
+
+    @Test
+    public void test5() {
+        List<Integer> user1newsFeed = solution2Twitter.getNewsFeed(1);
+        assertEquals(0, user1newsFeed.size());
+    }
+
+    @Test
+    public void test6() {
+        solution2Twitter.follow(1, 5);
+        List<Integer> user1newsFeed = solution2Twitter.getNewsFeed(1);
+        assertEquals(0, user1newsFeed.size());
     }
 }
