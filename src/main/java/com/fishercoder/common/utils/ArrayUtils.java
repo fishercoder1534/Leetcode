f6098f5a26a4676b45f73af5e4a472823a6106cb
stevesun <fishercoder@gmail.com>
2017-09-12 10:35:37 -0700

    add ArrayUtils

---
+++ src/main/java/com/fishercoder/common/utils/ArrayUtils.java
@@ -0,0 +1,23 @@
+package com.fishercoder.common.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by stevesun on 9/12/17.
+ */
+public class ArrayUtils {
+    public static List<List<Integer>> buildList(int[][] nums) {
+        List<List<Integer>> result = new ArrayList<>(nums.length);
+        int row = nums.length;
+        int col = nums[0].length;
+        for (int i = 0; i < row; i++) {
+            List<Integer> thisRow = new ArrayList<>();
+            for (int j = 0; j < col; j++) {
+                thisRow.add(nums[i][j]);
+            }
+            result.add(thisRow);
+        }
+        return result;
+    }
+}
