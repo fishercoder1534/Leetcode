39f12b79296c6ecc603e3a259d8b74e5428c146d
stevesun <fishercoder@gmail.com>
2017-09-13 07:24:49 -0700

    refactor 206

--- src/test/java/com/fishercoder/_206Test.java
+++ src/test/java/com/fishercoder/_206Test.java
@@ -1,31 +1,33 @@
 package com.fishercoder;
 
 import com.fishercoder.common.classes.ListNode;
-import com.fishercoder.common.utils.CommonUtils;
+import com.fishercoder.common.utils.LinkedListUtils;
 import com.fishercoder.solutions._206;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
 
 /**
  * Created by stevesun on 6/5/17.
  */
 public class _206Test {
-    private static _206 test;
-    private static ListNode actual;
+    private static _206.Solution1 solution1;
+    private static _206.Solution2 solution2;
     private static ListNode head;
 
     @BeforeClass
     public static void setup() {
-        test = new _206();
+        solution1 = new _206.Solution1();
+        solution2 = new _206.Solution2();
     }
 
     @Test
     public void test1() {
-        head = new ListNode(1);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
-        CommonUtils.printList(head);
-        actual = test.reverseList_iterative(head);
-        CommonUtils.printList(actual);
+        head = LinkedListUtils.contructLinkedList(new int[]{1,2,3});
+        assertEquals(LinkedListUtils.contructLinkedList(new int[]{3,2,1}), solution1.reverseList(head));
+
+        head = LinkedListUtils.contructLinkedList(new int[]{1,2,3});
+        assertEquals(LinkedListUtils.contructLinkedList(new int[]{3,2,1}), solution2.reverseList(head));
     }
 }
