4f4b77fb4283d3bb73538858ec1c24c255f39c21
stevesun <fishercoder@gmail.com>
2017-09-14 07:25:31 -0700

    refactor 48

--- src/main/java/com/fishercoder/solutions/_48.java
+++ src/main/java/com/fishercoder/solutions/_48.java
@@ -12,57 +12,68 @@ package com.fishercoder.solutions;
  */
 public class _48 {
 
-    public void rotate_O1(int[][] matrix) {
-        /**First swap the elements on the diagonal, then reverse each row:
-         * 1, 2, 3                    1, 4, 7                      7, 4, 1
-         * 4, 5, 6         becomes    2, 5, 8           becomes    8, 5, 2
-         * 7, 8, 9                    3, 6, 9                      9, 6, 3
-         This is done in O(1) space!
-         **/
-        int m = matrix.length;
-        int n = matrix[0].length;
-        for (int i = 0; i < m; i++) {
-            for (int j = i; j < n; j++) {
-                /**ATTN: j starts from i, so that the diagonal changes with itself, no change.*/
-                int tmp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = tmp;
-            }
-        }
+    /**Note: this is an n*n matrix, in other words, it's a square, this makes it easier as well.*/
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n / 2; j++) {
-                int tmp = matrix[i][j];
-                matrix[i][j] = matrix[i][n - 1 - j];
-                matrix[i][n - 1 - j] = tmp;
+    public static class Solution1 {
+        public void rotate(int[][] matrix) {
+            /**First swap the elements on the diagonal, then reverse each row:
+             * 1, 2, 3                    1, 4, 7                      7, 4, 1
+             * 4, 5, 6         becomes    2, 5, 8           becomes    8, 5, 2
+             * 7, 8, 9                    3, 6, 9                      9, 6, 3
+             This is done in O(1) space!
+             **/
+            int m = matrix.length;
+            for (int i = 0; i < m; i++) {
+                for (int j = i; j < m; j++) {
+                    /**ATTN: j starts from i, so that the diagonal changes with itself, results in no change.*/
+                    int tmp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = tmp;
+                }
+            }
+
+            /**then reverse*/
+            for (int i = 0; i < m; i++) {
+                int left = 0;
+                int right = m - 1;
+                while (left < right) {
+                    int tmp = matrix[i][left];
+                    matrix[i][left] = matrix[i][right];
+                    matrix[i][right] = tmp;
+                    left++;
+                    right--;
+                }
             }
         }
     }
 
-    /**First swap the rows bottom up, then swap the element on the diagonal:
-     * 1, 2, 3                         7, 8, 9                           7, 4, 1
-     * 4, 5, 6           becomes       4, 5, 6           becomes         8, 5, 2
-     * 7, 8, 9                         1, 2, 3                           9, 6, 3
-     * */
-    /**This is using O(n) of extra space*/
-    public void rotate_On(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int top = 0;
-        int bottom = n - 1;
-        while (top < bottom) {
-            int[] tmp = matrix[top];
-            matrix[top] = matrix[bottom];
-            matrix[bottom] = tmp;
-            top++;
-            bottom--;
-        }
+    public static class Solution2 {
+        /**First swap the rows bottom up, then swap the element on the diagonal:
+         * 1, 2, 3                         7, 8, 9                           7, 4, 1
+         * 4, 5, 6           becomes       4, 5, 6           becomes         8, 5, 2
+         * 7, 8, 9                         1, 2, 3                           9, 6, 3
+         *
+         *  This is using O(n) of extra space
+         */
+        public void rotate(int[][] matrix) {
+            int m = matrix.length;
+            int n = matrix[0].length;
+            int top = 0;
+            int bottom = n - 1;
+            while (top < bottom) {
+                int[] tmp = matrix[top];
+                matrix[top] = matrix[bottom];
+                matrix[bottom] = tmp;
+                top++;
+                bottom--;
+            }
 
-        for (int i = 0; i < m; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int tmp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = tmp;
+            for (int i = 0; i < m; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    int tmp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = tmp;
+                }
             }
         }
     }
--- src/test/java/com/fishercoder/_48Test.java
+++ src/test/java/com/fishercoder/_48Test.java
@@ -1,5 +1,6 @@
 package com.fishercoder;
 
+import com.fishercoder.common.utils.CommonUtils;
 import com.fishercoder.solutions._48;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -8,12 +9,14 @@ import org.junit.Test;
  * Created by fishercoder on 5/8/17.
  */
 public class _48Test {
-    private static _48 test;
+    private static _48.Solution1 solution1;
+    private static _48.Solution2 solution2;
     private static int[][] matrix;
 
     @BeforeClass
     public static void setup() {
-        test = new _48();
+        solution1 = new _48.Solution1();
+        solution2 = new _48.Solution2();
     }
 
     @Test
@@ -23,6 +26,18 @@ public class _48Test {
                 {4, 5, 6},
                 {7, 8, 9},
         };
-        test.rotate_On(matrix);
+        solution1.rotate(matrix);
+        CommonUtils.print2DIntArray(matrix);
+    }
+
+    @Test
+    public void test2() {
+        matrix = new int[][]{
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9},
+        };
+        solution2.rotate(matrix);
+        CommonUtils.print2DIntArray(matrix);
     }
 }
