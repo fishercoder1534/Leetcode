6051432dc05ab53ac56308148035f6f2763d451c
stevesun <fishercoder@gmail.com>
2017-09-17 07:46:01 -0700

    add 680

--- src/main/java/com/fishercoder/solutions/_680.java
+++ src/main/java/com/fishercoder/solutions/_680.java
@@ -57,4 +57,28 @@ public class _680 {
             return diff < 2;
         }
     }
+
+    public static class Solution2 {
+        public boolean validPalindrome(String s) {
+            int left = 0;
+            int right = s.length() - 1;
+            while (left < right) {
+                if (s.charAt(left) != s.charAt(right)) {
+                    return isValid(s, left + 1, right) || isValid(s, left, right - 1);
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+
+        private boolean isValid(String s, int left, int right) {
+            while (left < right) {
+                if (s.charAt(left++) != s.charAt(right--)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
 }
