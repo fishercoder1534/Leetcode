b8ffb94d594fc3c2e3cad6db36beb0d2e083018d
stevesun <fishercoder@gmail.com>
2017-09-27 09:28:20 -0700

    [N-0] refactor 5

--- src/main/java/com/fishercoder/solutions/_5.java
+++ src/main/java/com/fishercoder/solutions/_5.java
@@ -16,31 +16,32 @@ package com.fishercoder.solutions;
  */
 public class _5 {
 
-    private int low;
-    private int maxLen;
+    public static class Solution1 {
+        private int low;
+        private int maxLen;
 
-    public String longestPalindrome(String s) {
-        int len = s.length();
-        if (len < 2) {
-            return s;
+        public String longestPalindrome(String s) {
+            int len = s.length();
+            if (len < 2) {
+                return s;
+            }
+
+            for (int i = 0; i < len - 1; i++) {
+                extendPalindrome(s, i, i); // assume odd length, try to extend Palindrome as possible
+                extendPalindrome(s, i, i + 1); // assume even length.
+            }
+            return s.substring(low, low + maxLen);
         }
 
-        for (int i = 0; i < len - 1; i++) {
-            extendPalindrome(s, i, i); // assume odd length, try to extend Palindrome as possible
-            extendPalindrome(s, i, i + 1); // assume even length.
+        private void extendPalindrome(String s, int left, int right) {
+            while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+                left--;
+                right++;
+            }
+            if (maxLen < right - left - 1) {
+                low = left + 1;
+                maxLen = right - left - 1;
+            }
         }
-        return s.substring(low, low + maxLen);
     }
-
-    private void extendPalindrome(String s, int left, int right) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            left--;
-            right++;
-        }
-        if (maxLen < right - left - 1) {
-            low = left + 1;
-            maxLen = right - left - 1;
-        }
-    }
-
 }
