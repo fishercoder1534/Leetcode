4e3dd0f9186f936c8e44dd537dc0b153fb91e6a7
stevesun <fishercoder@gmail.com>
2017-09-27 09:32:20 -0700

    [N-0] refactor 121

--- src/main/java/com/fishercoder/solutions/_121.java
+++ src/main/java/com/fishercoder/solutions/_121.java
@@ -2,29 +2,27 @@ package com.fishercoder.solutions;
 
 /**
  * 121. Best Time to Buy and Sell Stock
- * <p>
+ *
  * Say you have an array for which the ith element is the price of a given stock on day i.
- * <p>
+ *
  * If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock),
  * design an algorithm to find the maximum profit.
- * <p>
+ *
  * Example 1:
  * Input: [7, 1, 5, 3, 6, 4]
  * Output: 5
- * <p>
+ *
  * max. difference = 6-1 = 5 (not 7-1 = 6, as selling price needs to be larger than buying price)
- * <p>
- * <p>
+ *
+ *
  * Example 2:
  * Input: [7, 6, 4, 3, 1]
  * Output: 0
- * <p>
+ *
  * In this case, no transaction is done, i.e. max profit = 0.
  */
 
 public class _121 {
-
-    /**Pretty straightforward, sell before you buy, keep a global maxProfit variable, update it along the way if necessary.*/
 
     /**
      * The key here is that you'll have to buy first, before you can sell.
