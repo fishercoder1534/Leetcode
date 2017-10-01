e215f78a38efe2c480b06a12a0181b783054ad90
stevesun <fishercoder@gmail.com>
2017-10-01 09:10:01 -0700

    [N-0] refactor 549

--- src/main/java/com/fishercoder/solutions/_549.java
+++ src/main/java/com/fishercoder/solutions/_549.java
@@ -3,6 +3,8 @@ package com.fishercoder.solutions;
 import com.fishercoder.common.classes.TreeNode;
 
 /**
+ * 549. Binary Tree Longest Consecutive Sequence II
+ *
  * Given a binary tree, you need to find the length of Longest Consecutive Path in Binary Tree.
  Especially, this path can be either increasing or decreasing. For example, [1,2,3,4] and [4,3,2,1] are both considered valid,
  but the path [1,2,4,3] is not valid. On the other hand, the path can be in the child-Parent-child order, where not necessarily be parent-child order.
@@ -26,39 +28,40 @@ import com.fishercoder.common.classes.TreeNode;
  */
 public class _549 {
 
-    int max = 0;
+    public static class Solution1 {
+        int max = 0;
 
-    public int longestConsecutive(TreeNode root) {
-        longestPath(root);
-        return max;
-    }
-
-    private int[] longestPath(TreeNode root) {
-        if (root == null) {
-            return new int[]{0, 0};
+        public int longestConsecutive(TreeNode root) {
+            longestPath(root);
+            return max;
         }
-        int increasing = 1;
-        int decreasing = 1;
-        if (root.left != null) {
-            int[] left = longestPath(root.left);
-            if (root.val == root.left.val + 1) {
-                decreasing = left[1] + 1;
-            } else if (root.val == root.left.val - 1) {
-                increasing = left[0] + 1;
+
+        private int[] longestPath(TreeNode root) {
+            if (root == null) {
+                return new int[]{0, 0};
             }
-        }
-
-        if (root.right != null) {
-            int[] right = longestPath(root.right);
-            if (root.val == root.right.val + 1) {
-                decreasing = Math.max(right[1] + 1, decreasing);
-            } else if (root.val == root.right.val - 1) {
-                increasing = Math.max(right[0] + 1, increasing);
+            int increasing = 1;
+            int decreasing = 1;
+            if (root.left != null) {
+                int[] left = longestPath(root.left);
+                if (root.val == root.left.val + 1) {
+                    decreasing = left[1] + 1;
+                } else if (root.val == root.left.val - 1) {
+                    increasing = left[0] + 1;
+                }
             }
+
+            if (root.right != null) {
+                int[] right = longestPath(root.right);
+                if (root.val == root.right.val + 1) {
+                    decreasing = Math.max(right[1] + 1, decreasing);
+                } else if (root.val == root.right.val - 1) {
+                    increasing = Math.max(right[0] + 1, increasing);
+                }
+            }
+
+            max = Math.max(max, decreasing + increasing - 1);
+            return new int[]{increasing, decreasing};
         }
-
-        max = Math.max(max, decreasing + increasing - 1);
-        return new int[]{increasing, decreasing};
     }
-
 }
--- src/test/java/com/fishercoder/_549Test.java
+++ src/test/java/com/fishercoder/_549Test.java
@@ -1,68 +1,62 @@
 package com.fishercoder;
 
 import com.fishercoder.common.classes.TreeNode;
+import com.fishercoder.common.utils.TreeUtils;
 import com.fishercoder.solutions._549;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 import static junit.framework.Assert.assertEquals;
 
 public class _549Test {
-    private static _549 test;
+    private static _549.Solution1 solution1;
     private static int expected;
     private static int actual;
     private static TreeNode root;
 
     @BeforeClass
     public static void setup() {
-        test = new _549();
     }
 
     @Before
     public void setupForEachTest() {
         root = null;
         actual = 0;
+        solution1 = new _549.Solution1();
     }
 
     @Test
     public void test1() {
-        root = new TreeNode(1);
-        root.left = new TreeNode(2);
-        root.right = new TreeNode(3);
-        actual = test.longestConsecutive(root);
+        root = TreeUtils.constructBinaryTree(Arrays.asList(1, 2, 3));
+        actual = solution1.longestConsecutive(root);
         expected = 2;
         assertEquals(expected, actual);
     }
 
     @Test
     public void test2() {
-        root = new TreeNode(2);
-        root.left = new TreeNode(1);
-        root.right = new TreeNode(3);
-        actual = test.longestConsecutive(root);
+        root = TreeUtils.constructBinaryTree(Arrays.asList(2, 1, 3));
+        actual = solution1.longestConsecutive(root);
         expected = 3;
         assertEquals(expected, actual);
     }
 
     @Test
-    @Ignore
-//NOTE: somehow it's always returning wrong when running with other tests, even if it passes on Leetcode OJ, so ignore this case
     public void test3() {
-        root = new TreeNode(1);
-        actual = test.longestConsecutive(root);
+        root = TreeUtils.constructBinaryTree(Arrays.asList(1));
+        actual = solution1.longestConsecutive(root);
         expected = 1;
         assertEquals(expected, actual);
     }
 
     @Test
     public void test4() {
-        root = new TreeNode(1);
-        root.left = new TreeNode(2);
-        root.left.left = new TreeNode(3);
-        root.left.left.left = new TreeNode(4);
-        actual = test.longestConsecutive(root);
+        root = TreeUtils.constructBinaryTree(Arrays.asList(1, 2, null, 3, null, 4));
+        TreeUtils.printBinaryTree(root);
+        actual = solution1.longestConsecutive(root);
         expected = 4;
         assertEquals(expected, actual);
     }
