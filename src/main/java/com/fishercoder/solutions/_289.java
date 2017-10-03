185d8797c8b3ff979dc0f01516bff9c309656d53
stevesun <fishercoder@gmail.com>
2017-10-03 07:33:11 -0700

    [N-0] refactor 289

--- src/main/java/com/fishercoder/solutions/_289.java
+++ src/main/java/com/fishercoder/solutions/_289.java
@@ -28,41 +28,23 @@ public class _289 {
         int height = board.length;
         int width = board[0].length;
         int[][] next = new int[height][width];
+        int[][] directions = {{-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}};
 
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
                 int liveCellsCount = 0;
                 //count all its live cells
 
-                if (j + 1 < width && board[i][j + 1] == 1) {
-                    liveCellsCount++;//right cell
-                }
-                if (j - 1 >= 0 && board[i][j - 1] == 1) {
-                    liveCellsCount++;//left cell
-                }
-                if (i + 1 < height && board[i + 1][j] == 1) {
-                    liveCellsCount++;//down cell
-                }
-                if (i - 1 >= 0 && board[i - 1][j] == 1) {
-                    liveCellsCount++;//up cell
-                }
-                if (i - 1 >= 0 && j - 1 >= 0 && board[i - 1][j - 1] == 1) {
-                    liveCellsCount++;//up left cell
-                }
-                if (i - 1 >= 0 && j + 1 < width && board[i - 1][j + 1] == 1) {
-                    liveCellsCount++;//up right cell
-                }
-                if (i + 1 < height && j - 1 >= 0 && board[i + 1][j - 1] == 1) {
-                    liveCellsCount++;//down left cell
-                }
-                if (i + 1 < height && j + 1 < width && board[i + 1][j + 1] == 1) {
-                    liveCellsCount++;//down right cell
+                for (int[] dir : directions) {
+                    int x = i + dir[0];
+                    int y = j + dir[1];
+                    if (x >= 0 && y >= 0 && x < height && y < width && board[x][y] == 1) {
+                        liveCellsCount++;
+                    }
                 }
 
                 if (board[i][j] == 1) {
-                    if (liveCellsCount > 3 || liveCellsCount < 2) {
-                        next[i][j] = 0;
-                    } else {
+                    if (liveCellsCount <= 3 && liveCellsCount >= 2) {
                         next[i][j] = 1;
                     }
                 } else if (board[i][j] == 0) {
