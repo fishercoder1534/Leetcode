b0525be628a4bf8f44aaf37b78472be31d2847d6
stevesun <fishercoder@gmail.com>
2017-10-06 07:25:19 -0700

    [N-0] refactor 554

--- src/main/java/com/fishercoder/solutions/_554.java
+++ src/main/java/com/fishercoder/solutions/_554.java
@@ -5,13 +5,17 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * There is a brick wall in front of you. The wall is rectangular and has several rows of bricks. The bricks have the same height but different width. You want to draw a vertical line from the top to the bottom and cross the least bricks.
-
- The brick wall is represented by a list of rows. Each row is a list of integers representing the width of each brick in this row from left to right.
-
- If your line go through the edge of a brick, then the brick is not considered as crossed. You need to find out how to draw the line to cross the least bricks and return the number of crossed bricks.
-
- You cannot draw a line just along one of the two vertical edges of the wall, in which case the line will obviously cross no bricks.
+ * 554. Brick Wall
+ *
+ * There is a brick wall in front of you. The wall is rectangular and has several rows of bricks.
+ * The bricks have the same height but different width.
+ * You want to draw a vertical line from the top to the bottom and cross the least bricks.
+ * The brick wall is represented by a list of rows.
+ * Each row is a list of integers representing the width of each brick in this row from left to right.
+ * If your line go through the edge of a brick,
+ * then the brick is not considered as crossed.
+ * You need to find out how to draw the line to cross the least bricks and return the number of crossed bricks.
+ * You cannot draw a line just along one of the two vertical edges of the wall, in which case the line will obviously cross no bricks.
 
  Example:
  Input:
@@ -21,6 +25,7 @@ import java.util.Map;
  [2,4],
  [3,1,2],
  [1,3,1,1]]
+
  Output: 2
  Explanation:
 
