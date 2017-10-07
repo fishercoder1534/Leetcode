0deea1abcfd3b8b904e16c3ad49a85c52758c14b
stevesun <fishercoder@gmail.com>
2017-10-07 08:06:56 -0700

    refactor 556

--- src/main/java/com/fishercoder/solutions/_556.java
+++ src/main/java/com/fishercoder/solutions/_556.java
@@ -1,13 +1,15 @@
 package com.fishercoder.solutions;
 
 /**
+ * 556. Next Greater Element III
+ *
  * Given a positive 32-bit integer n, you need to find the smallest 32-bit integer which has exactly the same digits existing
  * in the integer n and is greater in value than n. If no such positive 32-bit integer exists, you need to return -1.
- * <p>
+ *
  * Example 1:
  * Input: 12
  * Output: 21
- * <p>
+ *
  * Example 2:
  * Input: 21
  * Output: -1
