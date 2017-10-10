a25695d1faaffc0b3ae511816bcd1b758eb7b156
stevesun <fishercoder@gmail.com>
2017-10-10 14:58:18 -0700

    [N-0] refactor 458

--- src/main/java/com/fishercoder/solutions/_458.java
+++ src/main/java/com/fishercoder/solutions/_458.java
@@ -1,13 +1,19 @@
 package com.fishercoder.solutions;
 
 /**
- * There are 1000 buckets, one and only one of them contains poison, the rest are filled with water. They all look the same. If a pig drinks that poison it will die within 15 minutes. What is the minimum amount of pigs you need to figure out which bucket contains the poison within one hour.
-
- Answer this question, and write an algorithm for the follow-up general case.
+ * 458. Poor Pigs
+ *
+ * There are 1000 buckets, one and only one of them contains poison,
+ * the rest are filled with water.
+ * They all look the same.
+ * If a pig drinks that poison it will die within 15 minutes.
+ * What is the minimum amount of pigs you need to figure out which bucket contains the poison within one hour.
+ * Answer this question, and write an algorithm for the follow-up general case.
 
  Follow-up:
-
- If there are n buckets and a pig drinking poison will die within m minutes, how many pigs (x) you need to figure out the "poison" bucket within p minutes? There is exact one bucket with poison.
+ If there are n buckets and a pig drinking poison will die within m minutes,
+ how many pigs (x) you need to figure out the "poison" bucket within p minutes? There is exact one bucket with poison.
+ 
  */
 public class _458 {
 
