41083c660b1bb94e0520a00204fa86806662c3a6
stevesun <fishercoder@gmail.com>
2017-10-10 10:03:29 -0700

    [N-0] refactor 567

--- src/main/java/com/fishercoder/solutions/_567.java
+++ src/main/java/com/fishercoder/solutions/_567.java
@@ -1,6 +1,8 @@
 package com.fishercoder.solutions;
 
 /**
+ * 567. Permutation in String
+ *
  * Given two strings s1 and s2, write a function to return true if s2 contains the permutation of s1.
  * In other words, one of the first string's permutations is the substring of the second string.
 
