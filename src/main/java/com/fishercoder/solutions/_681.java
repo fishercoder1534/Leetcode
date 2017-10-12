928d0cda0f9764265c1449b9b30852954c12693a
stevesun <fishercoder@gmail.com>
2017-10-12 16:31:45 -0700

    [N-0] add 681

--- src/main/java/com/fishercoder/solutions/_681.java
+++ src/main/java/com/fishercoder/solutions/_681.java
@@ -1,5 +1,8 @@
 package com.fishercoder.solutions;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 681. Next Closest Time
  *
@@ -27,7 +30,27 @@ package com.fishercoder.solutions;
 public class _681 {
     public static class Solution1 {
         public String nextClosestTime(String time) {
-            return "";
+            int cur = 60 * Integer.parseInt(time.substring(0, 2));
+            cur += Integer.parseInt(time.substring(3));
+            Set<Integer> allowed = new HashSet();
+            for (char c : time.toCharArray())
+                if (c != ':') {
+                    allowed.add(c - '0');
+                }
+
+            while (true) {
+                cur = (cur + 1) % (24 * 60);
+                int[] digits = new int[]{cur / 60 / 10, cur / 60 % 10, cur % 60 / 10, cur % 60 % 10};
+                search:
+                {
+                    for (int d : digits) {
+                        if (!allowed.contains(d)) {
+                            break search;
+                        }
+                    }
+                    return String.format("%02d:%02d", cur / 60, cur % 60);
+                }
+            }
         }
     }
 }
