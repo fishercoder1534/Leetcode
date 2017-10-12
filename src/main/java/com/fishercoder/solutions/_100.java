ef0b1b09a67b2e04af28bd2d740de59c07082ac3
stevesun <fishercoder@gmail.com>
2017-10-12 07:55:50 -0700

    [N-0] minor refactor

--- src/main/java/com/fishercoder/solutions/_100.java
+++ src/main/java/com/fishercoder/solutions/_100.java
@@ -4,6 +4,7 @@ import com.fishercoder.common.classes.TreeNode;
 
 /**
  * 100. Same Tree
+ *
  * Given two binary trees, write a function to check if they are equal or not.
  * Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
  */
--- src/main/java/com/fishercoder/solutions/_235.java
+++ src/main/java/com/fishercoder/solutions/_235.java
@@ -4,6 +4,7 @@ import com.fishercoder.common.classes.TreeNode;
 
 /**
  * 235. Lowest Common Ancestor of a Binary Search Tree
+ *
  * Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
  * According to the definition of LCA on Wikipedia:
  * “The lowest common ancestor is defined between two nodes v and w as the lowest node in T that has both v and w as descendants
