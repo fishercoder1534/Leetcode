34ecd69f4f24cfad554ea078f5c252aa9da6d050
stevesun <fishercoder@gmail.com>
2017-10-15 08:18:50 -0700

    [N-0] refactor 40

--- src/main/java/com/fishercoder/solutions/_40.java
+++ src/main/java/com/fishercoder/solutions/_40.java
@@ -5,9 +5,11 @@ import com.fishercoder.common.utils.CommonUtils;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-/***Given a collection of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
-
- Each number in C may only be used once in the combination.
+/**
+ * 40. Combination Sum II
+ *
+ * Given a collection of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+ * Each number in C may only be used once in the combination.
 
  Note:
  All numbers (including target) will be positive integers.
@@ -19,24 +21,25 @@ import java.util.List;
  [1, 2, 5],
  [2, 6],
  [1, 1, 6]
- ]*/
+ ]
+ */
 public class _40 {
 
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList();
         Arrays.sort(candidates);
-        helper(candidates, target, 0, new ArrayList(), result);
+        backtracking(candidates, target, 0, new ArrayList(), result);
         return result;
     }
 
-    void helper(int[] candidates, int target, int start, List<Integer> curr, List<List<Integer>> result) {
+    void backtracking(int[] candidates, int target, int start, List<Integer> curr, List<List<Integer>> result) {
         if (target > 0) {
             for (int i = start; i < candidates.length && target >= candidates[i]; i++) {
                 if (i > start && candidates[i] == candidates[i - 1]) {
                     continue;//skip duplicates, this is one difference from Combination Sum I
                 }
                 curr.add(candidates[i]);
-                helper(candidates, target - candidates[i], i + 1, curr, result);//i+1 is the other difference from Combination Sum I
+                backtracking(candidates, target - candidates[i], i + 1, curr, result);//i+1 is the other difference from Combination Sum I
                 curr.remove(curr.size() - 1);
             }
         } else if (target == 0) {
@@ -52,6 +55,5 @@ public class _40 {
         List<List<Integer>> result = test.combinationSum2(candidates, target);
         CommonUtils.printListList(result);
     }
-
 
 }
