592add307e6b61bca0fa4bccc504fc5d6644ab62
stevesun <fishercoder@gmail.com>
2017-10-18 08:41:32 -0700

    [N-0] refactor 21

--- src/main/java/com/fishercoder/solutions/_21.java
+++ src/main/java/com/fishercoder/solutions/_21.java
@@ -4,6 +4,7 @@ import com.fishercoder.common.classes.ListNode;
 
 /**
  * 21. Merge Two Sorted Lists
+ *
  * Merge two sorted linked lists and return it as a new list.
  * The new list should be made by splicing together the nodes of the first two lists.*/
 
