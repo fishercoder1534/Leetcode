a58e05c601b22379dcdb525eaa54b918ecfd7c59
stevesun <fishercoder@gmail.com>
2017-10-18 11:42:41 -0700

    [N-0] fix build

--- src/main/java/com/fishercoder/solutions/_33.java
+++ src/main/java/com/fishercoder/solutions/_33.java
@@ -62,7 +62,9 @@ public class _33 {
             int hi = nums.length - 1;
             while (lo < hi) {
                 int mid = (lo + hi) / 2;
-                if (nums[mid] == target) return mid;
+                if (nums[mid] == target) {
+                    return mid;
+                }
 
                 if (nums[lo] <= nums[mid]) {
                     if (target >= nums[lo] && target < nums[mid]) {
