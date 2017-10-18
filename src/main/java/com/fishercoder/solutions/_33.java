3c99748aea334a3ecb474ee3f01a9a3b4834b16e
stevesun <fishercoder@gmail.com>
2017-10-18 11:36:07 -0700

    [N-0] refactor 33

--- src/main/java/com/fishercoder/solutions/_33.java
+++ src/main/java/com/fishercoder/solutions/_33.java
@@ -1,54 +1,84 @@
 package com.fishercoder.solutions;
 
 /**
+ * 33. Search in Rotated Sorted Array
+ *
  * Suppose a sorted array is rotated at some pivot unknown to you beforehand.
-
- (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
-
- You are given a target value to search. If found in the array return its index, otherwise return -1.
-
- You may assume no duplicate exists in the array.
+ * (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+ * You are given a target value to search. If found in the array return its index, otherwise return -1.
+ * You may assume no duplicate exists in the array.
  */
 public class _33 {
 
-    public int search(int[] nums, int target) {
-        if (nums == null || nums.length == 0) {
+    public static class Solution1 {
+
+        public int search(int[] nums, int target) {
+            if (nums == null || nums.length == 0) {
+                return -1;
+            }
+            int minIdx = findMinIdx(nums);
+            if (target == nums[minIdx]) {
+                return minIdx;
+            }
+            int m = nums.length;
+            int start = (target <= nums[m - 1]) ? minIdx : 0;
+            int end = (target > nums[m - 1]) ? minIdx : m - 1;
+
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+                if (nums[mid] == target) {
+                    return mid;
+                } else if (target > nums[mid]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
             return -1;
         }
-        int minIdx = findMinIdx(nums);
-        if (target == nums[minIdx]) {
-            return minIdx;
-        }
-        int m = nums.length;
-        int start = (target <= nums[m - 1]) ? minIdx : 0;
-        int end = (target > nums[m - 1]) ? minIdx : m - 1;
 
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (nums[mid] == target) {
-                return mid;
-            } else if (target > nums[mid]) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
+        private int findMinIdx(int[] nums) {
+            int start = 0;
+            int end = nums.length - 1;
+
+            while (start < end) {
+                int mid = start + (end - start) / 2;
+                if (nums[mid] > nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid;
+                }
             }
+            return start;
         }
-        return -1;
     }
 
-    public int findMinIdx(int[] nums) {
-        int start = 0;
-        int end = nums.length - 1;
-
-        while (start < end) {
-            int mid = start + (end -  start) / 2;
-            if (nums[mid] > nums[end]) {
-                start = mid + 1;
-            } else {
-                end = mid;
+    public static class Solution2 {
+        public int search(int[] nums, int target) {
+            if (nums == null || nums.length == 0) {
+                return -1;
             }
-        }
-        return start;
-    }
+            int lo = 0;
+            int hi = nums.length - 1;
+            while (lo < hi) {
+                int mid = (lo + hi) / 2;
+                if (nums[mid] == target) return mid;
 
+                if (nums[lo] <= nums[mid]) {
+                    if (target >= nums[lo] && target < nums[mid]) {
+                        hi = mid - 1;
+                    } else {
+                        lo = mid + 1;
+                    }
+                } else {
+                    if (target > nums[mid] && target <= nums[hi]) {
+                        lo = mid + 1;
+                    } else {
+                        hi = mid - 1;
+                    }
+                }
+            }
+            return nums[lo] == target ? lo : -1;
+        }
+    }
 }
