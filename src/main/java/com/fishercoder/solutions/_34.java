e8f6f68927ded3692e818e5891ee61e4b8d411b8
stevesun <fishercoder@gmail.com>
2017-10-18 08:57:45 -0700

    [N-0] refactor 34

--- src/main/java/com/fishercoder/solutions/_34.java
+++ src/main/java/com/fishercoder/solutions/_34.java
@@ -1,25 +1,28 @@
 package com.fishercoder.solutions;
 
-import com.fishercoder.common.utils.CommonUtils;
-
-/**Given an array of integers sorted in ascending order, find the starting and ending position of a given target value.
-
- Your algorithm's runtime complexity must be in the order of O(log n).
-
- If the target is not found in the array, return [-1, -1].
+/**
+ * 34. Search for a Range
+ *
+ * Given an array of integers sorted in ascending order, find the starting and ending position of a given target value.
+ * Your algorithm's runtime complexity must be in the order of O(log n).
+ * If the target is not found in the array, return [-1, -1].
 
  For example,
  Given [5, 7, 7, 8, 8, 10] and target value 8,
  return [3, 4].
+
  */
 public class _34 {
 
-    public static int[] searchRange(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length - 1;
+    public int[] searchRange(int[] nums, int target) {
         int[] range = new int[2];
         range[0] = -1;
         range[1] = -1;
+        if (nums == null || nums.length == 0) {
+            return range;
+        }
+        int start = 0;
+        int end = nums.length - 1;
         while (start + 1 < end) {
             int mid = start + (end - start) / 2;
             if (nums[mid] == target) {
@@ -40,14 +43,14 @@ public class _34 {
                 end = mid;
             }
         }
+
         if (nums[start] == target) {
             range[0] = start;
             while (start + 1 < nums.length && nums[start] == nums[start + 1]) {
                 start++;
             }
             range[1] = start;
-        }
-        if (nums[end] == target) {
+        } else if (nums[end] == target) {
             range[1] = end;
             while (end - 1 >= 0 && nums[end] == nums[end - 1]) {
                 end--;
@@ -55,12 +58,5 @@ public class _34 {
             range[0] = end;
         }
         return range;
-    }
-
-    public static void main(String... strings) {
-        int[] nums = new int[]{1, 2, 3};
-        int target = 2;
-        int[] result = searchRange(nums, target);
-        CommonUtils.printArray(result);
     }
 }
