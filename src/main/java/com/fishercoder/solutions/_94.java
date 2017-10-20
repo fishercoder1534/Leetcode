ef6e410e478a60e5ef63cb81ec65420373405dca
stevesun <fishercoder@gmail.com>
2017-10-20 09:27:13 -0700

    [N-0] refactor 94

--- src/main/java/com/fishercoder/solutions/_94.java
+++ src/main/java/com/fishercoder/solutions/_94.java
@@ -1,6 +1,5 @@
 package com.fishercoder.solutions;
 
-
 import com.fishercoder.common.classes.TreeNode;
 
 import java.util.ArrayList;
@@ -25,38 +24,36 @@ import java.util.Stack;
 
 public class _94 {
 
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList();
-        return inorder(root, result);
+    public static class Solution1 {
+        public List<Integer> inorderTraversal(TreeNode root) {
+            return inorder(root, new ArrayList());
+        }
+
+        List<Integer> inorder(TreeNode root, List<Integer> result) {
+            if (root == null) {
+                return result;
+            }
+            inorder(root.left, result);
+            result.add(root.val);
+            return inorder(root.right, result);
+        }
     }
 
-    List<Integer> inorder(TreeNode root, List<Integer> result) {
-        if (root == null) {
+    public static class Solution2 {
+        //iterative approach
+        public List<Integer> inorderTraversal(TreeNode root) {
+            List<Integer> result = new ArrayList();
+            Stack<TreeNode> stack = new Stack();
+            while (root != null || !stack.isEmpty()) {
+                while (root != null) {
+                    stack.push(root);
+                    root = root.left;
+                }
+                root = stack.pop();
+                result.add(root.val);
+                root = root.right;
+            }
             return result;
         }
-        if (root.left != null) {
-            inorder(root.left, result);
-        }
-        result.add(root.val);
-        if (root.right != null) {
-            inorder(root.right, result);
-        }
-        return result;
     }
-
-    public List<Integer> inorderTraversal_iterative(TreeNode root) {
-        List<Integer> result = new ArrayList();
-        Stack<TreeNode> stack = new Stack();
-        while (root != null || !stack.isEmpty()) {
-            while (root != null) {
-                stack.push(root);
-                root = root.left;
-            }
-            root = stack.pop();
-            result.add(root.val);
-            root = root.right;
-        }
-        return result;
-    }
-
 }
