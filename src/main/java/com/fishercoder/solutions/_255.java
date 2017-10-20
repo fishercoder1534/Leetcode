c7ac6325a72f365eaf36dd12fe2e98653eba0f83
stevesun <fishercoder@gmail.com>
2017-10-20 08:20:15 -0700

    [N-0] refactor 255

--- src/main/java/com/fishercoder/solutions/_255.java
+++ src/main/java/com/fishercoder/solutions/_255.java
@@ -3,9 +3,9 @@ package com.fishercoder.solutions;
 import java.util.Stack;
 
 /**
+ * 255. Verify Preorder Sequence in Binary Search Tree
  * Given an array of numbers, verify whether it is the correct preorder traversal sequence of a binary search tree.
-
- You may assume each number in the sequence is unique.
+ * You may assume each number in the sequence is unique.
 
  Follow up:
  Could you do it using only constant space complexity?
@@ -14,15 +14,15 @@ public class _255 {
 
     public boolean verifyPreorder(int[] preorder) {
         int low = Integer.MIN_VALUE;
-        Stack<Integer> path = new Stack();
+        Stack<Integer> stack = new Stack();
         for (int p : preorder) {
             if (p < low) {
                 return false;
             }
-            while (!path.empty() && p > path.peek()) {
-                low = path.pop();
+            while (!stack.empty() && p > stack.peek()) {
+                low = stack.pop();
             }
-            path.push(p);
+            stack.push(p);
         }
         return true;
     }
