811c57f71ada5577eabcac07209ae37a8425d9b1
stevesun <fishercoder@gmail.com>
2017-10-21 08:02:44 -0700

    [N-0] update random question

--- src/main/java/com/fishercoder/solutions/_99999RandomQuestions.java
+++ src/main/java/com/fishercoder/solutions/_99999RandomQuestions.java
@@ -190,44 +190,6 @@ public class _99999RandomQuestions {
         }
     }
 
-    /**
-     * Problem: count binary substrings:
-     * The 0's and 1's are grouped consecutively and their numbers are equal
-     * e.g.
-     * 00110 => 3 because there are 3 substrings that have equal number of consecutive 1's and 0's: 0011, 01, 10
-     * 10101 => 4, there are 4 substrings: 10, 01, 10, 01
-     */
-    static int counting(String s) {
-        int n = s.length();
-        /**a[i][0] denotes from most left up to i (inclusive), how many consecutive 0's
-         * a[i][1] denotes from most left up to i (inclusive), how many consecutive 1's*/
-        int[][] a = new int[n][2];
-        /**a[i][0] denotes from i (inclusive) to the most right, how many consecutive 0's
-         * b[i][0] denotes from i (inclusive) to the most right, how many consecutive 1's*/
-        int[][] b = new int[n][2];
-        for (int i = 0; i < n; i++) {
-            if (s.charAt(i) == '0') {
-                a[i][0] = 1 + (i - 1 >= 0 ? a[i - 1][0] : 0);
-            } else {
-                a[i][1] = 1 + (i - 1 >= 0 ? a[i - 1][1] : 0);
-            }
-        }
-        for (int i = n - 1; i >= 0; i--) {
-            if (s.charAt(i) == '0') {
-                b[i][0] = 1 + (i + 1 < n ? b[i + 1][0] : 0);
-            } else {
-                b[i][1] = 1 + (i + 1 < n ? b[i + 1][1] : 0);
-            }
-
-        }
-        long ans = 0;
-        for (int i = 0; i + 1 < n; i++) {
-            ans += Math.min(a[i][0], b[i + 1][1]);
-            ans += Math.min(a[i][1], b[i + 1][0]);
-        }
-        return (int) ans;
-    }
-
     public static class SubArraySum {
         /**
          * Given an array, return the start/end indices of the contiguous subarray that have the largest sum.
