2009875dcfc424d15b3e8f900494715c915393e7
stevesun <fishercoder@gmail.com>
2017-10-25 11:27:17 -0700

    [N-0] refactor 340

--- src/main/java/com/fishercoder/solutions/_340.java
+++ src/main/java/com/fishercoder/solutions/_340.java
@@ -1,5 +1,8 @@
 package com.fishercoder.solutions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 340. Longest Substring with At Most K Distinct Characters
  *
@@ -12,23 +15,53 @@ package com.fishercoder.solutions;
  */
 public class _340 {
 
-    /**credit: https://discuss.leetcode.com/topic/41671/15-lines-java-solution-using-slide-window*/
-    public int lengthOfLongestSubstringKDistinct(String s, int k) {
-        int[] count = new int[256];
-        int left = 0;
-        int result = 0;
-        int num = 0;
-        for (int right = 0; right < s.length(); right++) {
-            if (count[s.charAt(right)]++ == 0) {
-                num++;
+    public static class Solution1 {
+        /**
+         * credit: https://discuss.leetcode.com/topic/41671/15-lines-java-solution-using-slide-window
+         */
+        public int lengthOfLongestSubstringKDistinct(String s, int k) {
+            int[] count = new int[256];
+            int left = 0;
+            int result = 0;
+            int num = 0;
+            for (int right = 0; right < s.length(); right++) {
+                if (count[s.charAt(right)]++ == 0) {
+                    num++;
+                }
+                if (num > k) {
+                    while (--count[s.charAt(left++)] > 0) {
+                    }
+                    ;
+                    num--;
+                }
+                result = Math.max(result, right - left + 1);
             }
-            if (num > k) {
-                while (--count[s.charAt(left++)] > 0) {};
-                num--;
-            }
-            result = Math.max(result, right - left + 1);
+            return result;
         }
-        return result;
+    }
+
+    public static class Solution2 {
+        public int lengthOfLongestSubstringKDistinct(String s, int k) {
+            Map<Character, Integer> map = new HashMap<>();
+            int longest = 0;
+            int left = 0;
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                map.put(c, map.getOrDefault(c, 0) + 1);
+                while (map.size() > k) {
+                    char leftChar = s.charAt(left);
+                    if (map.containsKey(leftChar)) {
+                        map.put(leftChar, map.get(leftChar) - 1);
+                        if (map.get(leftChar) == 0) {
+                            map.remove(leftChar);
+                        }
+                    }
+                    left++;
+                }
+                longest = Math.max(longest, i - left + 1);
+            }
+            return longest;
+        }
     }
 
 }
--- src/test/java/com/fishercoder/_340Test.java
+++ src/test/java/com/fishercoder/_340Test.java
@@ -10,46 +10,50 @@ import static org.junit.Assert.assertEquals;
  * Created by stevesun on 6/10/17.
  */
 public class _340Test {
-    private static _340 test;
+    private static _340.Solution1 solution1;
+    private static _340.Solution2 solution2;
 
     @BeforeClass
     public static void setup() {
-        test = new _340();
+        solution1 = new _340.Solution1();
+        solution2 = new _340.Solution2();
     }
 
     @Test
     public void test1() {
-        assertEquals(3, test.lengthOfLongestSubstringKDistinct("eceba", 2));
+        assertEquals(3, solution1.lengthOfLongestSubstringKDistinct("eceba", 2));
+        assertEquals(3, solution2.lengthOfLongestSubstringKDistinct("eceba", 2));
     }
 
     @Test
     public void test2() {
-        assertEquals(0, test.lengthOfLongestSubstringKDistinct("", 0));
+        assertEquals(0, solution1.lengthOfLongestSubstringKDistinct("", 0));
+        assertEquals(0, solution2.lengthOfLongestSubstringKDistinct("", 0));
     }
 
     @Test
     public void test3() {
-        assertEquals(0, test.lengthOfLongestSubstringKDistinct("a", 0));
+        assertEquals(0, solution1.lengthOfLongestSubstringKDistinct("a", 0));
     }
 
     @Test
     public void test4() {
-        assertEquals(1, test.lengthOfLongestSubstringKDistinct("a", 1));
+        assertEquals(1, solution1.lengthOfLongestSubstringKDistinct("a", 1));
     }
 
     @Test
     public void test5() {
-        assertEquals(1, test.lengthOfLongestSubstringKDistinct("a", 2));
+        assertEquals(1, solution1.lengthOfLongestSubstringKDistinct("a", 2));
     }
 
     @Test
     public void test6() {
-        assertEquals(2, test.lengthOfLongestSubstringKDistinct("aa", 1));
+        assertEquals(2, solution1.lengthOfLongestSubstringKDistinct("aa", 1));
     }
 
     @Test
     public void test7() {
-        assertEquals(3, test.lengthOfLongestSubstringKDistinct("bacc", 2));
+        assertEquals(3, solution1.lengthOfLongestSubstringKDistinct("bacc", 2));
     }
 
 }
