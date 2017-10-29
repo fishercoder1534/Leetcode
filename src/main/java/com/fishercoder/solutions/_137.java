b7e6342a25b019822011a54a2fc32d2df897634f
stevesun <fishercoder@gmail.com>
2017-10-29 16:16:29 -0700

    [N-0] refactor 137

--- src/main/java/com/fishercoder/solutions/_137.java
+++ src/main/java/com/fishercoder/solutions/_137.java
@@ -12,23 +12,36 @@ Your algorithm should have a linear runtime complexity. Could you implement it w
 */
 public class _137 {
 
-    public int singleNumber(int[] nums) {
-        Map<Integer, Integer> map = new HashMap();
-        for (int i : nums) {
-            map.put(i, map.getOrDefault(i, 0) + 1);
-        }
-        for (int key : map.keySet()) {
-            if (map.get(key) != 3) {
-                return key;
+    public static class Solution1 {
+        public int singleNumber(int[] nums) {
+            Map<Integer, Integer> map = new HashMap();
+            for (int i : nums) {
+                map.put(i, map.getOrDefault(i, 0) + 1);
             }
+            for (int key : map.keySet()) {
+                if (map.get(key) != 3) {
+                    return key;
+                }
+            }
+            return 0;
         }
-        return 0;
     }
 
-    public static void main(String... strings) {
-        int[] nums = new int[]{2, 2, 3, 2};
-        _137 test = new _137();
-        System.out.println(test.singleNumber(nums));
+    public static class Solution2 {
+        /**Credit: https://discuss.leetcode.com/topic/11877/detailed-explanation-and-generalization-of-the-bitwise-operation-method-for-single-numbers/2*/
+        public int singleNumber(int[] nums) {
+            int counter1 = 0;
+            int counter2 = 0;
+            int mask = 0;
+            for (int num : nums) {
+                counter2 ^= counter1 & num;
+                counter1 ^= num;
+                mask = ~(counter1 & counter2);
+                counter1 &= mask;
+                counter2 &= mask;
+            }
+            return counter1;
+        }
     }
 
 }
