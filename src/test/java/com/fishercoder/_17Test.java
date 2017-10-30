94dcbcc3c2511aa23beb0e17ab98bc1a34d43c0e
stevesun <fishercoder@gmail.com>
2017-10-29 21:52:09 -0700

    [N-0] refactor 17

--- src/test/java/com/fishercoder/_17Test.java
+++ src/test/java/com/fishercoder/_17Test.java
@@ -12,20 +12,20 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class _17Test {
-    private static _17 test;
+    private static _17.Solution1 solution1;
     private static String digits;
     private static List<String> expected;
     private static List<String> actual;
 
     @BeforeClass
     public static void setup() {
-        test = new _17();
+        solution1 = new _17.Solution1();
     }
 
     @Test
     public void test1() {
         digits = "2";
-        actual = test.letterCombinations(digits);
+        actual = solution1.letterCombinations(digits);
         expected = new ArrayList<>(Arrays.asList("a", "b", "c"));
         assertEquals(expected, actual);
     }
@@ -33,7 +33,7 @@ public class _17Test {
     @Test
     public void test2() {
         digits = "23";
-        actual = test.letterCombinations(digits);
+        actual = solution1.letterCombinations(digits);
         expected = new ArrayList<>(Arrays.asList("ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"));
         /**order doesn't matter, so we check like below*/
         assertTrue(expected.containsAll(actual) && actual.containsAll(expected));
