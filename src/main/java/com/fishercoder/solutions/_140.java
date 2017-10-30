eae1dde80a83c48794c06e9665673e790a226e36
stevesun <fishercoder@gmail.com>
2017-10-29 20:47:24 -0700

    [N-0] refactor 140

--- src/main/java/com/fishercoder/solutions/_140.java
+++ src/main/java/com/fishercoder/solutions/_140.java
@@ -2,11 +2,11 @@ package com.fishercoder.solutions;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 /**
+ * 140. Word Break II
+ *
  * Given a string s and a dictionary of words dict, add spaces in s to construct a sentence where each word is a valid dictionary word.
 
  Return all such possible sentences.
@@ -19,54 +19,31 @@ import java.util.Set;
 
  */
 public class _140 {
-    public List<String> wordBreak(String s, Set<String> wordDict) {
-        return dfs(s, wordDict, new HashMap<String, ArrayList<String>>());
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        return dfs(s, wordDict, new HashMap<>());
     }
 
-    private List<String> dfs(String s, Set<String> wordDict,
-                             HashMap<String, ArrayList<String>> map) {
+    List<String> dfs(String s, List<String> wordDict, HashMap<String, List<String>> map) {
         if (map.containsKey(s)) {
             return map.get(s);
         }
 
-        ArrayList<String> res = new ArrayList<String>();
+        ArrayList<String> result = new ArrayList<>();
         if (s.length() == 0) {
-            res.add("");
-            return res;
+            result.add("");
+            return result;
         }
 
         for (String word : wordDict) {
             if (s.startsWith(word)) {
                 List<String> subList = dfs(s.substring(word.length()), wordDict, map);
                 for (String sub : subList) {
-                    res.add(word + (sub.length() == 0 ? "" : " ") + sub);
+                    result.add(word + (sub.length() == 0 ? "" : " ") + sub);
                 }
             }
         }
-        map.put(s, res);
-        return res;
+        map.put(s, result);
+        return result;
     }
 
-    public static void main(String... strings) {
-        List<String> temp = new ArrayList<String>();
-        System.out.println(temp);
-        List<String> temp2 = new ArrayList<String>(temp);
-        temp2.add("");
-        System.out.println(temp2);
-
-        _140 test = new _140();
-        Set<String> wordDict = new HashSet();
-        wordDict.add("cat");
-        wordDict.add("cats");
-        wordDict.add("sand");
-        wordDict.add("and");
-        wordDict.add("dog");
-        String s = "catsanddog";
-//        List<String> list = test.wordBreak(s, wordDict);
-        List<String> list = test.wordBreak(s, wordDict);
-        for (String word : list) {
-            System.out.print(word + ", ");
-        }
-        System.out.println();
-    }
 }
