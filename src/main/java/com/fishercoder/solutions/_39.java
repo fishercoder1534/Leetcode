2357bbe1c504b9eabaf5df5031e144019f8c78f6
stevesun <fishercoder@gmail.com>
2017-10-29 21:37:26 -0700

    [N-0] refactor 39

--- src/main/java/com/fishercoder/solutions/_39.java
+++ src/main/java/com/fishercoder/solutions/_39.java
@@ -1,7 +1,5 @@
 package com.fishercoder.solutions;
 
-import com.fishercoder.common.utils.CommonUtils;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -25,39 +23,27 @@ import java.util.List;
  ]*/
 public class _39 {
 
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> result = new ArrayList();
-        Arrays.sort(candidates);
-        backtracking(candidates, target, 0, new ArrayList(), result);
-        return result;
-    }
+    public static class Solution1 {
+        public List<List<Integer>> combinationSum(int[] candidates, int target) {
+            List<List<Integer>> result = new ArrayList();
+            Arrays.sort(candidates);
+            backtracking(candidates, target, 0, new ArrayList(), result);
+            return result;
+        }
 
-    private void backtracking(int[] candidates, int target, int startIndex, List<Integer> curr, List<List<Integer>> result) {
-        if (target > 0) {
-            int prev = -1;
-            for (int i = startIndex; i < candidates.length; i++) {
-                if (candidates[i] > target) {
-                    return;//this is one very important step to optimize this algorithm: pruning
+        void backtracking(int[] candidates, int target, int start, List<Integer> curr, List<List<Integer>> result) {
+            if (target > 0) {
+                for (int i = start; i < candidates.length; i++) {
+                    if (candidates[i] > target) {
+                        return;//pruning
+                    }
+                    curr.add(candidates[i]);
+                    backtracking(candidates, target - candidates[i], i, curr, result);
+                    curr.remove(curr.size() - 1);
                 }
-                if (prev != -1 && prev == candidates[i]) {
-                    continue;
-                }
-                curr.add(candidates[i]);
-                backtracking(candidates, target - candidates[i], i, curr, result);
-                curr.remove(curr.size() - 1);
+            } else if (target == 0) {
+                result.add(new ArrayList(curr));
             }
-        } else if (target == 0) {
-            result.add(new ArrayList(curr));
         }
     }
-
-
-    public static void main(String... args) {
-        _39 test = new _39();
-        int[] candidates = new int[]{2, 3, 6, 7};
-        int target = 7;
-        List<List<Integer>> result = test.combinationSum(candidates, target);
-        CommonUtils.printListList(result);
-    }
-
 }
