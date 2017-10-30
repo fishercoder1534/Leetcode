67091d214171ac85c36d03a07cc8f475c98d0d64
stevesun <fishercoder@gmail.com>
2017-10-29 21:58:07 -0700

    [N-0] refactor 40

--- src/main/java/com/fishercoder/solutions/_40.java
+++ src/main/java/com/fishercoder/solutions/_40.java
@@ -1,7 +1,5 @@
 package com.fishercoder.solutions;
 
-import com.fishercoder.common.utils.CommonUtils;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -25,35 +23,28 @@ import java.util.List;
  */
 public class _40 {
 
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> result = new ArrayList();
-        Arrays.sort(candidates);
-        backtracking(candidates, target, 0, new ArrayList(), result);
-        return result;
-    }
-
-    void backtracking(int[] candidates, int target, int start, List<Integer> curr, List<List<Integer>> result) {
-        if (target > 0) {
-            for (int i = start; i < candidates.length && target >= candidates[i]; i++) {
-                if (i > start && candidates[i] == candidates[i - 1]) {
-                    continue;//skip duplicates, this is one difference from Combination Sum I
-                }
-                curr.add(candidates[i]);
-                backtracking(candidates, target - candidates[i], i + 1, curr, result);//i+1 is the other difference from Combination Sum I
-                curr.remove(curr.size() - 1);
-            }
-        } else if (target == 0) {
-            List<Integer> temp = new ArrayList(curr);
-            result.add(temp);
+    public static class Solution1 {
+        public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+            List<List<Integer>> result = new ArrayList();
+            Arrays.sort(candidates);
+            backtracking(candidates, target, 0, new ArrayList(), result);
+            return result;
         }
-    }
 
-    public static void main(String... args) {
-        _40 test = new _40();
-        int[] candidates = new int[]{10, 1, 2, 7, 6, 1, 5};
-        int target = 8;
-        List<List<Integer>> result = test.combinationSum2(candidates, target);
-        CommonUtils.printListList(result);
+        void backtracking(int[] candidates, int target, int start, List<Integer> curr, List<List<Integer>> result) {
+            if (target > 0) {
+                for (int i = start; i < candidates.length && target >= candidates[i]; i++) {
+                    if (i > start && candidates[i] == candidates[i - 1]) {
+                        continue;//skip duplicates, this is one difference from Combination Sum I
+                    }
+                    curr.add(candidates[i]);
+                    backtracking(candidates, target - candidates[i], i + 1, curr, result);//i+1 is the other difference from Combination Sum I
+                    curr.remove(curr.size() - 1);
+                }
+            } else if (target == 0) {
+                result.add(new ArrayList(curr));
+            }
+        }
     }
 
 }
