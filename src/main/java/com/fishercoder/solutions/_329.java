459eadc3beec2aa3604724e25a8fa978e9281db5
stevesun <fishercoder@gmail.com>
2017-10-29 20:22:28 -0700

    [N-0] refactor 329

--- src/main/java/com/fishercoder/solutions/_329.java
+++ src/main/java/com/fishercoder/solutions/_329.java
@@ -2,6 +2,7 @@ package com.fishercoder.solutions;
 
 /**
  * 329. Longest Increasing Path in a Matrix
+ *
  * Given an integer matrix, find the length of the longest increasing path.
  * From each cell, you can either move to four directions: left, right, up or down.
  * You may NOT move diagonally or move outside of the boundary (i.e. wrap-around is not allowed).
