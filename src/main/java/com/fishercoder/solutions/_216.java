c5614ab47256365a470f4510ad3282e0754a1c28
stevesun <fishercoder@gmail.com>
2017-10-29 22:02:09 -0700

    [N-0] refactor 216

--- src/main/java/com/fishercoder/solutions/_216.java
+++ src/main/java/com/fishercoder/solutions/_216.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 /**
  * 216. Combination Sum III
+ *
  * Find all possible combinations of k numbers that add up to a number n,
  * given that only numbers from 1 to 9 can be used and each combination should be a unique set of numbers.
 
@@ -29,23 +30,22 @@ public class _216 {
     public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> result = new ArrayList();
         int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
-        helper(k, n, nums, 0, new ArrayList(), result);
+        backtracking(k, n, nums, 0, new ArrayList(), result);
         return result;
     }
 
-    void helper(int k, int n, int[] nums, int start, List<Integer> curr, List<List<Integer>> result) {
+    void backtracking(int k, int n, int[] nums, int start, List<Integer> curr, List<List<Integer>> result) {
         if (n > 0) {
             for (int i = start; i < nums.length; i++) {
                 curr.add(nums[i]);
                 /** it needs to be a unique set of numbers, so we need to set it
                  as i+1 here: each number is used only once in this array: [1,2,3,4,5,6,7,8,9]*/
-                helper(k, n - nums[i], nums, i + 1, curr, result);
+                backtracking(k, n - nums[i], nums, i + 1, curr, result);
                 curr.remove(curr.size() - 1);
             }
         } else if (n == 0 && curr.size() == k) {
             //this is the major difference here: check size of curr list is of k before adding it
-            List<Integer> temp = new ArrayList(curr);
-            result.add(temp);
+            result.add(new ArrayList(curr));
         }
     }
 }
