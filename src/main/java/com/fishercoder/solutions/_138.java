40a1e073f92057aa45aac3f8a9e985d754aeb8d5
stevesun <fishercoder@gmail.com>
2017-10-30 06:45:36 -0700

    [N-0] refactor 138

--- src/main/java/com/fishercoder/solutions/_138.java
+++ src/main/java/com/fishercoder/solutions/_138.java
@@ -4,9 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**138. Copy List with Random Pointer
+ *
  * A linked list is given such that each node contains an additional random
  * pointer which could point to any node in the list or null.
- * Return a deep copy of the list.*/
+ * Return a deep copy of the list.
+ * */
 
 public class _138 {
 
