5a79793f8b6a3e7c44b1a31053eb246b91cad300
stevesun <fishercoder@gmail.com>
2017-10-31 07:44:47 -0700

    [N-0] refactor 141

--- src/main/java/com/fishercoder/solutions/_141.java
+++ src/main/java/com/fishercoder/solutions/_141.java
@@ -2,12 +2,13 @@ package com.fishercoder.solutions;
 
 import com.fishercoder.common.classes.ListNode;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * 141. Linked List Cycle
-Given a linked list, determine if it has a cycle in it.
+ *
+ * Given a linked list, determine if it has a cycle in it.
 
 Follow up:
 Can you solve it without using extra space?
@@ -16,6 +17,19 @@ Can you solve it without using extra space?
 public class _141 {
 
 	public static class Solution1 {
+		public boolean hasCycle(ListNode head) {
+			Set<ListNode> set = new HashSet();
+			while (head != null) {
+				if (!set.add(head)) {
+					return true;
+				}
+				head = head.next;
+			}
+			return false;
+		}
+	}
+
+	public static class Solution2 {
 		public boolean hasCycle(ListNode head) {
 			ListNode slow = head;
 			ListNode fast = head;
@@ -29,20 +43,4 @@ public class _141 {
 			return false;
 		}
 	}
-
-	public static class Solution2 {
-		public boolean hasCycle(ListNode head) {
-			Map<ListNode, Boolean> visited = new HashMap();
-			ListNode temp = head;
-			while (temp != null) {
-				if (visited.containsKey(temp)) {
-					return true;
-				}
-				visited.put(temp, true);
-				temp = temp.next;
-			}
-			return false;
-		}
-	}
-
 }
