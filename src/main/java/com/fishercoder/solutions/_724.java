56f74d0fddcabf0e222e66026b42306d017b6f03
stevesun <fishercoder@gmail.com>
2017-11-12 08:48:38 -0500

    [N-0] refactor 724

--- src/main/java/com/fishercoder/solutions/_724.java
+++ src/main/java/com/fishercoder/solutions/_724.java
@@ -38,14 +38,12 @@ public class _724 {
             for (int i = 1; i < nums.length; i++) {
                 sums[i] = sums[i - 1] + nums[i];
             }
-            int result = -1;
             for (int i = 0; i < nums.length; i++) {
                 if (i == 0 && 0 == sums[nums.length - 1] - sums[i] || (i > 0 && sums[i - 1] == sums[nums.length - 1] - sums[i])) {
-                    result = i;
-                    break;
+                    return i;
                 }
             }
-            return result;
+            return -1;
         }
     }
 }
