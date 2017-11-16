d65cd7af58f3252e36671706bbe5e1ea2d86de85
stevesun <fishercoder@gmail.com>
2017-11-16 07:08:17 -0800

    [N-0] refactor 14

--- src/main/java/com/fishercoder/solutions/_14.java
+++ src/main/java/com/fishercoder/solutions/_14.java
@@ -1,43 +1,42 @@
 package com.fishercoder.solutions;
 
 /**
+ * 14. Longest Common Prefix
+ *
  * Write a function to find the longest common prefix string amongst an array of strings.
  */
 
 public class _14 {
 
-    public static String longestCommonPrefix(String[] strs) {
-        if (strs.length == 0) {
-            return "";
-        }
-
-        int i = 0;
-        String prefix = "";
-        String result = "";
-        boolean broken = false;
-        while (true) {
-            i++;
-            result = prefix;
-            if (i > strs[0].length()) {
-                break;//this will break out the while loop
+    public static class Solution1 {
+        public String longestCommonPrefix(String[] strs) {
+            if (strs.length == 0) {
+                return "";
             }
-            prefix = strs[0].substring(0, i);
-            for (String word : strs) {
-                if (i > word.length() || !word.startsWith(prefix)) {
-                    broken = true;
-                    break;//this will only break out of the for loop
+
+            int i = 0;
+            String prefix = "";
+            String result = "";
+            boolean broken = false;
+            while (true) {
+                i++;
+                result = prefix;
+                if (i > strs[0].length()) {
+                    break;//this will break out the while loop
+                }
+                prefix = strs[0].substring(0, i);
+                for (String word : strs) {
+                    if (i > word.length() || !word.startsWith(prefix)) {
+                        broken = true;
+                        break;//this will only break out of the for loop
+                    }
+                }
+                if (broken) {
+                    break;//this will break out the while loop
                 }
             }
-            if (broken) {
-                break;//this will break out the while loop
-            }
+            return result;
         }
-        return result;
     }
 
-    public static void main(String... strings) {
-//        String[] strs = new String[]{"a"};
-        String[] strs = new String[]{"a", "b"};
-        System.out.println(longestCommonPrefix(strs));
-    }
 }
