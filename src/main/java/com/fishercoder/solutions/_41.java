ad3c21566a7f590736d79b424a58746a47fd0f27
stevesun <fishercoder@gmail.com>
2017-11-18 06:59:53 -0800

    [N-0] refactor 41

--- src/main/java/com/fishercoder/solutions/_41.java
+++ src/main/java/com/fishercoder/solutions/_41.java
@@ -2,7 +2,7 @@ package com.fishercoder.solutions;
 
 /**
  *41. First Missing Positive
- * Given an unsorted integer array, find the first missing positive integer.
+ *Given an unsorted integer array, find the first missing positive integer.
 
  For example,
  Given [1,2,0] return 3,
@@ -13,33 +13,35 @@ package com.fishercoder.solutions;
 
 public class _41 {
 
-    public int firstMissingPositive(int[] nums) {
-        int i = 0;
-        while (i < nums.length) {
-            if (nums[i] > 0 && nums[i] != i + 1
-                    && nums[i] - 1 < nums.length
-                    && nums[i] != nums[nums[i] - 1]) {
-                swap(nums, i, nums[i] - 1);
-            } else {
-                i++;
+    public static class Solution1 {
+        public int firstMissingPositive(int[] nums) {
+            int i = 0;
+            while (i < nums.length) {
+                if (nums[i] > 0 && nums[i] != i + 1
+                        && nums[i] - 1 < nums.length
+                        && nums[i] != nums[nums[i] - 1]) {
+                    swap(nums, i, nums[i] - 1);
+                } else {
+                    i++;
+                }
             }
+
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] != j + 1) {
+                    return j + 1;
+                }
+            }
+
+            return nums.length + 1;
+            /** if all values are in the correct position, then we return the length + 1.
+             * This also takes care of corner case: [], we return 1 for it.*/
         }
 
-        for (int j = 0; j < nums.length; j++) {
-            if (nums[j] != j + 1) {
-                return j + 1;
-            }
+        public void swap(int[] nums, int i, int j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
         }
-
-        return nums.length + 1;
-        /** if all values are in the correct position, then we return the length + 1.
-         * This also takes care of corner case: [], we return 1 for it.*/
-    }
-
-    public void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
     }
 
 }
