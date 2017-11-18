ff1898c3fc1d395ba46c4130cb9471d35167adb1
stevesun <fishercoder@gmail.com>
2017-11-18 14:30:01 -0800

    [N-0] refactor 117

--- src/main/java/com/fishercoder/solutions/_117.java
+++ src/main/java/com/fishercoder/solutions/_117.java
@@ -3,9 +3,10 @@ package com.fishercoder.solutions;
 import com.fishercoder.common.classes.TreeLinkNode;
 
 /**
- * /* Follow up for problem "Populating Next Right Pointers in Each Node".
-
- What if the given tree could be any binary tree? Would your previous solution still work?
+ * 117. Populating Next Right Pointers in Each Node II
+ *
+ * Follow up for problem "Populating Next Right Pointers in Each Node".
+ * What if the given tree could be any binary tree? Would your previous solution still work?
 
  Note:
 
@@ -25,56 +26,47 @@ import com.fishercoder.common.classes.TreeLinkNode;
  4-> 5 -> 7 -> NULL */
 
 public class _117 {
-    //copied this post: https://discuss.leetcode.com/topic/1106/o-1-space-o-n-complexity-iterative-solution
-    //very clever and concise to make it in O(1) space
+    public static class Solution1 {
+        //copied this post: https://discuss.leetcode.com/topic/1106/o-1-space-o-n-complexity-iterative-solution
+        //very clever and concise to make it in O(1) space
 
-    //based on level order traversal
-    public static void connect(TreeLinkNode root) {
+        //based on level order traversal
+        public void connect(TreeLinkNode root) {
 
-        TreeLinkNode head = null; //head of the next level
-        TreeLinkNode prev = null; //the leading node on the next level
-        TreeLinkNode cur = root;  //current node of current level
+            TreeLinkNode head = null; //head of the next level
+            TreeLinkNode prev = null; //the leading node on the next level
+            TreeLinkNode cur = root;  //current node of current level
 
-        while (cur != null) {
+            while (cur != null) {
 
-            while (cur != null) { //iterate on the current level
-                //left child
-                if (cur.left != null) {
-                    if (prev != null) {
-                        prev.next = cur.left;
-                    } else {
-                        head = cur.left;
+                while (cur != null) { //iterate on the current level
+                    //left child
+                    if (cur.left != null) {
+                        if (prev != null) {
+                            prev.next = cur.left;
+                        } else {
+                            head = cur.left;
+                        }
+                        prev = cur.left;
                     }
-                    prev = cur.left;
-                }
-                //right child
-                if (cur.right != null) {
-                    if (prev != null) {
-                        prev.next = cur.right;
-                    } else {
-                        head = cur.right;
+                    //right child
+                    if (cur.right != null) {
+                        if (prev != null) {
+                            prev.next = cur.right;
+                        } else {
+                            head = cur.right;
+                        }
+                        prev = cur.right;
                     }
-                    prev = cur.right;
+                    //move to next node
+                    cur = cur.next;
                 }
-                //move to next node
-                cur = cur.next;
+
+                //move to next level
+                cur = head;
+                head = null;
+                prev = null;
             }
-
-            //move to next level
-            cur = head;
-            head = null;
-            prev = null;
         }
-
-    }
-
-    public static void main(String... args) {
-        TreeLinkNode root = new TreeLinkNode(1);
-        root.left = new TreeLinkNode(2);
-        root.right = new TreeLinkNode(3);
-        root.left.left = new TreeLinkNode(4);
-        root.left.right = new TreeLinkNode(5);
-        root.right.right = new TreeLinkNode(7);
-        connect(root);
     }
 }
