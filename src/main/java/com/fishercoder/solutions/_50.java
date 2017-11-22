1d0f00c4d3cbf9d54ef14fa5f0b9783ff8b57cca
stevesun <fishercoder@gmail.com>
2017-11-22 07:36:34 -0800

    [N-0] refactor 50

--- src/main/java/com/fishercoder/solutions/_50.java
+++ src/main/java/com/fishercoder/solutions/_50.java
@@ -1,25 +1,39 @@
 package com.fishercoder.solutions;
 
 /**
+ * 50. Pow(x, n)
+ *
  * Implement pow(x, n).
+
+ Example 1:
+
+ Input: 2.00000, 10
+ Output: 1024.00000
+
+ Example 2:
+
+ Input: 2.10000, 3
+ Output: 9.26100
  */
 public class _50 {
 
-    public double myPow(double x, int n) {
-        if (n == 0) {
-            return 1;
+    public static class Solution1 {
+        public double myPow(double x, int n) {
+            if (n == 0) {
+                return 1;
+            }
+            if (n == Integer.MIN_VALUE) {
+                ++n;
+                n = -n;
+                x = 1 / x;
+                return x * x * myPow(x * x, n / 2);
+            }
+            if (n < 0) {
+                n = -n;
+                x = 1 / x;
+            }
+            return (n % 2 == 0) ? myPow(x * x, n / 2) : x * myPow(x * x, n / 2);
         }
-        if (n == Integer.MIN_VALUE) {
-            ++n;
-            n = -n;
-            x = 1 / x;
-            return x * x * myPow(x * x, n / 2);
-        }
-        if (n < 0) {
-            n = -n;
-            x = 1 / x;
-        }
-        return (n % 2 == 0) ? myPow(x * x, n / 2) : x * myPow(x * x, n / 2);
     }
 
 }
