3d2295ca8c4705637a72f3987ab6710e815090e8
Steve Sun <stevesun@squareup.com>
2017-12-12 18:30:04 -0800

    refactor 18

--- src/main/java/com/fishercoder/solutions/_18.java
+++ src/main/java/com/fishercoder/solutions/_18.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-/**Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
+/**
+ * 18. 4Sum
+ *
+ * Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target?
+ * Find all unique quadruplets in the array which gives the sum of target.
 
  Note: The solution set must not contain duplicate quadruplets.
 
