f3ba9d7feed40ced95977a26f756a91fd649a72b
Steve Sun <fishercoder@gmail.com>
2017-12-12 18:34:36 -0800

    refactor 19

--- src/main/java/com/fishercoder/solutions/_19.java
+++ src/main/java/com/fishercoder/solutions/_19.java
@@ -4,18 +4,20 @@ import com.fishercoder.common.classes.ListNode;
 import com.fishercoder.common.utils.CommonUtils;
 
 /**19. Remove Nth Node From End of List
-Given a linked list, remove the nth node from the end of list and return its head.
+ *
+ * Given a linked list, remove the nth node from the end of list and return its head.
 
-For example,
+    For example,
 
    Given linked list: 1->2->3->4->5, and n = 2.
 
    After removing the second node from the end, the linked list becomes 1->2->3->5.
+
 Note:
 Given n will always be valid.
 Try to do this in one pass.*/
 public class _19 {
-    
+
     /**Naive/most straightforward approach:
      * go through the list, find its total length, then go through the list a second time:
      * this time, pause at the delta point, then assign its next.next pointer to next.
