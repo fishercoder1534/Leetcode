d28fdce56811c1d4c9266643c7992aef395b79e6
Steve Sun <fishercoder@gmail.com>
2017-12-14 07:55:36 -0800

    refactor 22

--- src/main/java/com/fishercoder/solutions/_22.java
+++ src/main/java/com/fishercoder/solutions/_22.java
@@ -5,9 +5,12 @@ import com.fishercoder.common.utils.CommonUtils;
 import java.util.ArrayList;
 import java.util.List;
 
-/**Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
-
- For example, given n = 3, a solution set is:
+/**
+ * 22. Generate Parentheses
+ *
+ * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ *
+ * For example, given n = 3, a solution set is:
 
  [
  "((()))",
@@ -18,39 +21,30 @@ import java.util.List;
  ]*/
 
 public class _22 {
-    public List<String> generateParenthesis(int n) {
-        List<String> result = new ArrayList();
-        backtrack(result, "", 0, 0, n);
-        return result;
-    }
-
-    void backtrack(List<String> result, String str, int left, int right, int max) {
-        if (str.length() == max * 2) {
-            result.add(str);
-            return;
+    public static class Solution1 {
+        public List<String> generateParenthesis(int n) {
+            List<String> result = new ArrayList();
+            backtrack(result, "", 0, 0, n);
+            return result;
         }
 
-        if (left < max) {
-            backtrack(result, str + "(", left + 1, right, max);
-        }
+        void backtrack(List<String> result, String str, int left, int right, int max) {
+            if (str.length() == max * 2) {
+                result.add(str);
+                return;
+            }
 
-        if (right < left) {
-            backtrack(result, str + ")", left, right + 1, max);
+            if (left < max) {
+                backtrack(result, str + "(", left + 1, right, max);
+            }
+
+            if (right < left) {
+                backtrack(result, str + ")", left, right + 1, max);
+            }
         }
     }
 
-    public static void main(String... args) {
-        _22 test = new _22();
-        int n = 3;
-        List<String> result = test.generateParenthesis(n);
-        CommonUtils.print(result);
-
-        Solution2 sol2 = new Solution2();
-        List<String> result2 = sol2.generateParenthesis(n);
-        CommonUtils.print(result2);
-    }
-
-    static class Solution2 {
+    public static class Solution2 {
         public List<String> generateParenthesis(int n) {
             List<String> result = new ArrayList();
             if (n == 0) {
