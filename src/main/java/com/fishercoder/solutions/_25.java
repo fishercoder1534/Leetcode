7ebbf14f3fb18b2878075726a973ecdba9489875
Steve Sun <fishercoder@gmail.com>
2017-12-15 08:00:07 -0800

    refactor 25

--- src/main/java/com/fishercoder/solutions/_25.java
+++ src/main/java/com/fishercoder/solutions/_25.java
@@ -4,6 +4,7 @@ import com.fishercoder.common.classes.ListNode;
 
 /**
  * 25. Reverse Nodes in k-Group
+ *
  * Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
  * k is a positive integer and is less than or equal to the length of the linked list.
  * If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
