4323cc8a2a3e616c3aacf7f8a917ea52e3c4b623
Steve Sun <fishercoder@gmail.com>
2017-12-16 09:43:35 -0800

    refactor 26

--- src/main/java/com/fishercoder/solutions/_26.java
+++ src/main/java/com/fishercoder/solutions/_26.java
@@ -1,59 +1,58 @@
 package com.fishercoder.solutions;
 
-/** Given a sorted array, remove the duplicates
+/**
+ * 26. Remove Duplicates from Sorted Array
+ *
+ * Given a sorted array, remove the duplicates
  * in place such that each element appear only once and return the new length.
  * Do not allocate extra space for another array, you must do this in place with constant memory.
-
- For example,
- Given input array A = [1,1,2],
-
- Your function should return length = 2, and A is now [1,2].*/
+ *
+ * For example,
+ * Given input array A = [1,1,2],
+ * Your function should return length = 2, and A is now [1,2].
+ * */
 
 public class _26 {
 
-    public static int removeDuplicates_editorial_solution(int[] nums) {
-        int i = 0;
-        for (int j = 1; j < nums.length; j++) {
-            if (nums[i] != nums[j]) {
-                i++;
-                nums[i] = nums[j];
-            }
+  public static class Solution1 {
+    public int removeDuplicates(int[] nums) {
+      int i = 0;
+      for (int j = 1; j < nums.length; j++) {
+        if (nums[i] != nums[j]) {
+          i++;
+          nums[i] = nums[j];
         }
-        return i + 1;
+      }
+      return i + 1;
     }
+  }
 
-
-    public static void main(String... strings) {
-        int[] nums = new int[]{1, 1, 2};
-//        int[] nums = new int[]{1,1,2,2,3};
-//        int[] nums = new int[]{1,1};
-        System.out.println(removeDuplicates_editorial_solution(nums));
-    }
-
+  public static class Solution2 {
     /**
      * Same idea as the editorial solution, mine just got more verbose.
      */
-    public static int removeDuplicates_my_original(int[] nums) {
-        int i = 0;
-        for (int j = i + 1; i < nums.length && j < nums.length; ) {
-            while (j < nums.length && nums[i] == nums[j]) {
-                j++;
-            }
-            if (j == nums.length) {
-                j--;
-            }
-            int temp = nums[j];
-            nums[j] = nums[i + 1];
-            nums[i + 1] = temp;
-            if (nums[i] != nums[i + 1]) {
-                i++;
-            }
-            if (j == nums.length) {
-                break;
-            }
-            j++;
+    public static int removeDuplicates(int[] nums) {
+      int i = 0;
+      for (int j = i + 1; i < nums.length && j < nums.length; ) {
+        while (j < nums.length && nums[i] == nums[j]) {
+          j++;
         }
-        return i + 1;
+        if (j == nums.length) {
+          j--;
+        }
+        int temp = nums[j];
+        nums[j] = nums[i + 1];
+        nums[i + 1] = temp;
+        if (nums[i] != nums[i + 1]) {
+          i++;
+        }
+        if (j == nums.length) {
+          break;
+        }
+        j++;
+      }
+      return i + 1;
     }
+  }
 
 }
