c012b215da61bc6ce5aa12e8b401eb637043ecd7
Steve Sun <fishercoder@gmail.com>
2017-12-17 10:41:11 -0800

    fix build

--- src/main/java/com/fishercoder/solutions/_746.java
+++ src/main/java/com/fishercoder/solutions/_746.java
@@ -29,9 +29,9 @@ public class _746 {
       dp[0] = cost[0];
       dp[1] = Math.min(cost[1], cost[0] + cost[1]);
       for (int i = 2; i < cost.length; i++) {
-        dp[i] = Math.min(dp[i-1] + cost[i], dp[i-2] + cost[i]);
+        dp[i] = Math.min(dp[i - 1] + cost[i], dp[i - 2] + cost[i]);
       }
-      return Math.min(dp[cost.length-1], dp[cost.length-2]);
+      return Math.min(dp[cost.length - 1], dp[cost.length - 2]);
     }
   }
 }
