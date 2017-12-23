1207a1e593a70db84c2e640274a7f3c3cc8901c2
Steve Sun <fishercoder@gmail.com>
2017-12-23 08:07:15 -0800

    refactor 32

--- src/main/java/com/fishercoder/solutions/_32.java
+++ src/main/java/com/fishercoder/solutions/_32.java
@@ -3,29 +3,31 @@ package com.fishercoder.solutions;
 import java.util.Stack;
 
 /**
+ * 32. Longest Valid Parentheses
+ *
  * Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
-
- For "(()", the longest valid parentheses substring is "()", which has length = 2.
-
- Another example is ")()())", where the longest valid parentheses substring is "()()", which has length = 4.
+ * For "(()", the longest valid parentheses substring is "()", which has length = 2.
+ * Another example is ")()())", where the longest valid parentheses substring is "()()", which has length = 4.
  */
 public class _32 {
-	public int longestValidParentheses(String s) {
-		int result = 0;
-		Stack<Integer> stack = new Stack();
-		stack.push(-1);
-		for (int i = 0; i < s.length(); i++) {
-			if (s.charAt(i) == '(') {
-				stack.push(i);
-			} else {
-				stack.pop();
-				if (stack.isEmpty()) {
-					stack.push(i);
-				} else {
-					result = Math.max(result, i - stack.peek());
-				}
-			}
-		}
-		return result;
-	}
+  public static class Solution1 {
+    public int longestValidParentheses(String s) {
+      int result = 0;
+      Stack<Integer> stack = new Stack();
+      stack.push(-1);
+      for (int i = 0; i < s.length(); i++) {
+        if (s.charAt(i) == '(') {
+          stack.push(i);
+        } else {
+          stack.pop();
+          if (stack.isEmpty()) {
+            stack.push(i);
+          } else {
+            result = Math.max(result, i - stack.peek());
+          }
+        }
+      }
+      return result;
+    }
+  }
 }
