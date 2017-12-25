9301b26793075aff429f17b0adff9ea33b83f8b1
Steve Sun <fishercoder@gmail.com>
2017-12-25 08:33:45 -0800

    refactor 30

---
+++ src/test/java/com/fishercoder/_30Test.java
@@ -0,0 +1,29 @@
+package com.fishercoder;
+
+import com.fishercoder.solutions._30;
+import com.fishercoder.solutions._735;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+public class _30Test {
+  private static _30.Solution1 solution1;
+  private static String[] words;
+  private static List<Integer> expected;
+
+  @BeforeClass
+  public static void setup() {
+    solution1 = new _30.Solution1();
+  }
+
+  @Test
+  public void test1() {
+    words = new String[] {"foo", "bar"};
+    expected = Arrays.asList(0, 9);
+    assertEquals(expected, solution1.findSubstring("barfoothefoobarman", words));
+  }
+}
