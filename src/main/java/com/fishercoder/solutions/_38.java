9aa640a3c7d52e74407e11894c2b8c30605c5d9c
Steve Sun <fishercoder@gmail.com>
2017-12-26 13:35:56 -0800

    refactor 38

--- src/main/java/com/fishercoder/solutions/_38.java
+++ src/main/java/com/fishercoder/solutions/_38.java
@@ -1,7 +1,9 @@
 package com.fishercoder.solutions;
 
-public class _38 {
-/**The count-and-say sequence is the sequence of integers beginning as follows:
+/**
+ * 38. Count and Say
+ *
+ * The count-and-say sequence is the sequence of integers beginning as follows:
  1, 11, 21, 1211, 111221, ...
 
  1 is read off as "one 1" or 11.
@@ -10,29 +12,33 @@ public class _38 {
  Given an integer n, generate the nth sequence.
 
  Note: The sequence of integers will be represented as a string.*/
-public String countAndSay(int n) {
-    StringBuilder curr = new StringBuilder("1");
-    StringBuilder prev;
-    int count;
-    char say;
-    for (int i = 1; i < n; i++) {
+
+public class _38 {
+  public static class Solution1 {
+    public String countAndSay(int n) {
+      StringBuilder curr = new StringBuilder("1");
+      StringBuilder prev;
+      int count;
+      char say;
+      for (int i = 1; i < n; i++) {
         prev = curr;
         curr = new StringBuilder();
         count = 1;
         say = prev.charAt(0);
 
         for (int j = 1, len = prev.length(); j < len; j++) {
-            if (prev.charAt(j) != say) {
-                curr.append(count).append(say);
-                count = 1;
-                say = prev.charAt(j);
-            } else {
-                count++;
-            }
+          if (prev.charAt(j) != say) {
+            curr.append(count).append(say);
+            count = 1;
+            say = prev.charAt(j);
+          } else {
+            count++;
+          }
         }
         curr.append(count).append(say);
+      }
+      return curr.toString();
     }
-    return curr.toString();
-}
+  }
 
 }
