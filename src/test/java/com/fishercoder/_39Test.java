ba98d8eaa0f21c4322c5e41bcba29bc57c1a1cd2
Steve Sun <fishercoder@gmail.com>
2017-12-27 15:06:22 -0800

    refactor 39

---
+++ src/test/java/com/fishercoder/_39Test.java
@@ -0,0 +1,31 @@
+package com.fishercoder;
+
+import com.fishercoder.solutions._30;
+import com.fishercoder.solutions._39;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class _39Test {
+  private static _39.Solution1 solution1;
+  private static int[] candidates;
+  private static List<List<Integer>> expected;
+
+  @BeforeClass
+  public static void setup() {
+    solution1 = new _39.Solution1();
+  }
+
+  @Test
+  public void test1() {
+    candidates = new int[] {2, 3, 6, 7};
+    expected = new ArrayList<>();
+    expected.add(Arrays.asList(2, 2, 3));
+    expected.add(Arrays.asList(7));
+    assertEquals(expected, solution1.combinationSum(candidates, 7));
+  }
+}
