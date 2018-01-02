c550ab305ad75ec93e5dc133d2822d52571349b0
Steve Sun <fishercoder@gmail.com>
2018-01-01 20:28:37 -0800

    refactor 45 again

--- src/main/java/com/fishercoder/solutions/_45.java
+++ src/main/java/com/fishercoder/solutions/_45.java
@@ -18,12 +18,12 @@ package com.fishercoder.solutions;
 public class _45 {
 
   public static class Solution1 {
-    public int jump(int[] A) {
+    public int jump(int[] nums) {
       int stepCount = 0;
       int lastJumpMax = 0;
       int currentJumpMax = 0;
-      for (int i = 0; i < A.length - 1; i++) {
-        currentJumpMax = Math.max(currentJumpMax, i + A[i]);
+      for (int i = 0; i < nums.length - 1; i++) {
+        currentJumpMax = Math.max(currentJumpMax, i + nums[i]);
         if (i == lastJumpMax) {
           stepCount++;
           lastJumpMax = currentJumpMax;
