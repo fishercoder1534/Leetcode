47d46731149828996746cbdf9a64422cda0ff307
Steve Sun <fishercoder@gmail.com>
2018-01-04 15:00:12 -0800

    refactor 49

--- src/main/java/com/fishercoder/solutions/_49.java
+++ src/main/java/com/fishercoder/solutions/_49.java
@@ -25,18 +25,19 @@ import java.util.Map;
 
 public class _49 {
 
+  public static class Solution1 {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> map = new HashMap<>();
-        for (String word : strs) {
-            char[] c = word.toCharArray();
-            Arrays.sort(c);
-            String key = new String(c);
-            if (!map.containsKey(key)) {
-                map.put(key, new ArrayList<>());
-            }
-            map.get(key).add(word);
+      Map<String, List<String>> map = new HashMap<>();
+      for (String word : strs) {
+        char[] c = word.toCharArray();
+        Arrays.sort(c);
+        String key = new String(c);
+        if (!map.containsKey(key)) {
+          map.put(key, new ArrayList<>());
         }
-        return new ArrayList<>(map.values());
+        map.get(key).add(word);
+      }
+      return new ArrayList<>(map.values());
     }
-
+  }
 }
