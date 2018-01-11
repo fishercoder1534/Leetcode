3180cd6307da1d7053ae3ae0ed4648fc91441ff9
Steve Sun <fishercoder@gmail.com>
2018-01-11 08:16:42 -0800

    refactor 53

--- src/main/java/com/fishercoder/solutions/_53.java
+++ src/main/java/com/fishercoder/solutions/_53.java
@@ -1,19 +1,24 @@
 package com.fishercoder.solutions;
 
 /**
+ * 53. Maximum Subarray
+ *
  * Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
-
- For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
- the contiguous subarray [4,-1,2,1] has the largest sum = 6.
+ * For example, given the array [-2,1,-3,4,-1,2,1,-5,4], the contiguous subarray [4,-1,2,1] has the largest sum = 6.
  */
+
 public class _53 {
+
+  public static class Solution1 {
+    /**credit: https://discuss.leetcode.com/topic/5000/accepted-o-n-solution-in-java*/
     public int maxSubArray(int[] nums) {
-        int maxSum = nums[0];
-        int currentSum = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            currentSum = Math.max(nums[i], currentSum + nums[i]);
-            maxSum = Math.max(currentSum, maxSum);
-        }
-        return maxSum;
+      int maxSoFar = nums[0];
+      int maxEndingHere = nums[0];
+      for (int i = 1; i < nums.length; i++) {
+        maxEndingHere = Math.max(nums[i], maxEndingHere + nums[i]);
+        maxSoFar = Math.max(maxEndingHere, maxSoFar);
+      }
+      return maxSoFar;
     }
+  }
 }
---
+++ src/test/java/com/fishercoder/_53Test.java
@@ -0,0 +1,29 @@
+package com.fishercoder;
+
+import com.fishercoder.solutions._53;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class _53Test {
+  private static _53.Solution1 solution1;
+  private static int[] nums;
+
+  @BeforeClass
+  public static void setup() {
+    solution1 = new _53.Solution1();
+  }
+
+  @Before
+  public void clear() {
+    solution1 = new _53.Solution1();
+  }
+
+  @Test
+  public void test1() {
+    nums = new int[] {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    assertEquals(6, solution1.maxSubArray(nums));
+  }
+}
