fb403cbeb9f9c69d3000a44a4e5beee85ea02766
Steve Sun <fishercoder@gmail.com>
2018-01-20 07:21:57 -0800

    refactor 59

--- src/main/java/com/fishercoder/solutions/_59.java
+++ src/main/java/com/fishercoder/solutions/_59.java
@@ -1,6 +1,8 @@
 package com.fishercoder.solutions;
 
 /**
+ * 59. Spiral Matrix II
+ *
  * Given an integer n, generate a square matrix filled with elements from 1 to n2 in spiral order.
 
  For example,
@@ -15,103 +17,101 @@ package com.fishercoder.solutions;
  */
 public class _59 {
 
+  public static class Solution1 {
     public int[][] generateMatrix(int num) {
-        int temp = num;
-        int[][] fourEdges = new int[num][num];
-        int value = 1;
-        int i = 0;
-        int j = 0;
-        if (num % 2 == 0) {
-            //when num is even
-            while (i < num / 2 && j < num / 2 && temp >= 0) {
+      int temp = num;
+      int[][] fourEdges = new int[num][num];
+      int value = 1;
+      int i = 0;
+      int j = 0;
+      if (num % 2 == 0) {
+        //when num is even
+        while (i < num / 2 && j < num / 2 && temp >= 0) {
                 /* Assign the top row */
-                while (j < temp) {
-                    fourEdges[i][j] = value;
-                    j++;
-                    value++;
-
-                }
+          while (j < temp) {
+            fourEdges[i][j] = value;
+            j++;
+            value++;
+          }
 
 				/* Assign the right column */
-                while (i < temp - 1) {
-                    i++;
-                    fourEdges[i][j - 1] = value;
-                    value++;
-                }
-                j = j - 2;
+          while (i < temp - 1) {
+            i++;
+            fourEdges[i][j - 1] = value;
+            value++;
+          }
+          j = j - 2;
 
 				/* Assign the bottom row */
-                while (j >= num - temp) {
-                    fourEdges[i][j] = value;
-                    j--;
-                    value++;
-                }
-                i--;
-                j++;
+          while (j >= num - temp) {
+            fourEdges[i][j] = value;
+            j--;
+            value++;
+          }
+          i--;
+          j++;
 
 				/* Assign the left column */
-                while (i > num - temp) {
-                    fourEdges[i][j] = value;
-                    i--;
-                    value++;
-                }
-                //}
-                i++;
-                j++;
-                temp--;
-            }
-
-        } else {
-            //when num is odd
-            while (i < num / 2 && j < num / 2 && temp >= 0) {
+          while (i > num - temp) {
+            fourEdges[i][j] = value;
+            i--;
+            value++;
+          }
+          //}
+          i++;
+          j++;
+          temp--;
+        }
+      } else {
+        //when num is odd
+        while (i < num / 2 && j < num / 2 && temp >= 0) {
                 /* Assign the top row */
-                while (j < temp) {
-                    fourEdges[i][j] = value;
-                    j++;
-                    value++;
-
-                }
+          while (j < temp) {
+            fourEdges[i][j] = value;
+            j++;
+            value++;
+          }
 
 				/* Assign the right column */
-                while (i < temp - 1) {
-                    i++;
-                    fourEdges[i][j - 1] = value;
-                    value++;
-                }
-                j = j - 2;
+          while (i < temp - 1) {
+            i++;
+            fourEdges[i][j - 1] = value;
+            value++;
+          }
+          j = j - 2;
 
 				/* Assign the bottom row */
-                while (j >= num - temp) {
-                    fourEdges[i][j] = value;
-                    j--;
-                    value++;
-                }
-                i--;
-                j++;
+          while (j >= num - temp) {
+            fourEdges[i][j] = value;
+            j--;
+            value++;
+          }
+          i--;
+          j++;
 
 				/* Assign the left column */
-                while (i > num - temp) {
-                    fourEdges[i][j] = value;
-                    i--;
-                    value++;
-                }
-                //}
-                i++;
-                j++;
-                temp--;
-            }
-            fourEdges[num / 2][num / 2] = num * num;
+          while (i > num - temp) {
+            fourEdges[i][j] = value;
+            i--;
+            value++;
+          }
+          //}
+          i++;
+          j++;
+          temp--;
         }
+        fourEdges[num / 2][num / 2] = num * num;
+      }
 
-        for (int m = 0; m < num; m++) {
-            for (int n = 0; n < num; n++) {
-                System.out.print(fourEdges[m][n] + "\t");
-                if ((n + 1) % num == 0) {
-                    System.out.println();
-                }
-            }
+      for (int m = 0; m < num; m++) {
+        for (int n = 0; n < num; n++) {
+          System.out.print(fourEdges[m][n] + "\t");
+          if ((n + 1) % num == 0) {
+            System.out.println();
+          }
         }
-        return fourEdges;
+      }
+      return fourEdges;
     }
-
+  }
 }
