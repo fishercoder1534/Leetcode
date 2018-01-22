cdb9da04ba51521dccdc5546eb90d9c8f9f6ab52
Steve Sun <fishercoder@gmail.com>
2018-01-22 07:23:51 -0800

    refactor 61

--- src/test/java/com/fishercoder/_61Test.java
+++ src/test/java/com/fishercoder/_61Test.java
@@ -7,37 +7,34 @@ import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
 
-/**
- * Created by fishercoder on 4/30/17.
- */
 public class _61Test {
-    private static _61 test;
-    private static ListNode expected;
-    private static ListNode actual;
-    private static ListNode head;
-    private static int k;
+  private static _61.Solution1 solution1;
+  private static ListNode expected;
+  private static ListNode actual;
+  private static ListNode head;
+  private static int k;
 
-    @BeforeClass
-    public static void setup() {
-        test = new _61();
-    }
+  @BeforeClass
+  public static void setup() {
+    solution1 = new _61.Solution1();
+  }
 
-    @Test
-    public void test1() {
-        k = 2;
-        expected = new ListNode(4);
-        expected.next = new ListNode(5);
-        expected.next.next = new ListNode(1);
-        expected.next.next.next = new ListNode(2);
-        expected.next.next.next.next = new ListNode(3);
+  @Test
+  public void test1() {
+    k = 2;
+    expected = new ListNode(4);
+    expected.next = new ListNode(5);
+    expected.next.next = new ListNode(1);
+    expected.next.next.next = new ListNode(2);
+    expected.next.next.next.next = new ListNode(3);
 
-        head = new ListNode(1);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
-        head.next.next.next = new ListNode(4);
-        head.next.next.next.next = new ListNode(5);
+    head = new ListNode(1);
+    head.next = new ListNode(2);
+    head.next.next = new ListNode(3);
+    head.next.next.next = new ListNode(4);
+    head.next.next.next.next = new ListNode(5);
 
-        actual = test.rotateRight(head, k);
-        assertEquals(expected, actual);
-    }
+    actual = solution1.rotateRight(head, k);
+    assertEquals(expected, actual);
+  }
 }
