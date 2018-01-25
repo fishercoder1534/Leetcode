a9be0758955d3b5abff702868ed1c55bd883dc6c
Steve Sun <fishercoder@gmail.com>
2018-01-25 08:00:21 -0800

    refactor 244

--- src/main/java/com/fishercoder/solutions/_244.java
+++ src/main/java/com/fishercoder/solutions/_244.java
@@ -24,13 +24,13 @@ public class _244 {
     private Map<String, List<Integer>> map;
 
     public _244(String[] words) {
-        map = new HashMap<String, List<Integer>>();
+        map = new HashMap<>();
         for (int i = 0; i < words.length; i++) {
             String w = words[i];
             if (map.containsKey(w)) {
                 map.get(w).add(i);
             } else {
-                List<Integer> list = new ArrayList<Integer>();
+                List<Integer> list = new ArrayList<>();
                 list.add(i);
                 map.put(w, list);
             }
@@ -40,19 +40,19 @@ public class _244 {
     public int shortest(String word1, String word2) {
         List<Integer> list1 = map.get(word1);
         List<Integer> list2 = map.get(word2);
-        int ret = Integer.MAX_VALUE;
+        int result = Integer.MAX_VALUE;
         for (int i = 0, j = 0; i < list1.size() && j < list2.size(); ) {
             int index1 = list1.get(i);
             int index2 = list2.get(j);
             if (index1 < index2) {
-                ret = Math.min(ret, index2 - index1);
+                result = Math.min(result, index2 - index1);
                 i++;
             } else {
-                ret = Math.min(ret, index1 - index2);
+                result = Math.min(result, index1 - index2);
                 j++;
             }
         }
-        return ret;
+        return result;
     }
 
 }
