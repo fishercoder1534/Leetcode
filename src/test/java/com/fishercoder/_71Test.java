a7b6e1fbd8d4d118801f42e9eb0a125e23d7f3fe
Steve Sun <fishercoder@gmail.com>
2018-02-03 07:42:55 -0800

    refactor 71

---
+++ src/test/java/com/fishercoder/_71Test.java
@@ -0,0 +1,26 @@
+package com.fishercoder;
+
+import com.fishercoder.solutions._71;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class _71Test {
+  private static _71.Solution1 solution1;
+
+  @BeforeClass
+  public static void setup() {
+    solution1 = new _71.Solution1();
+  }
+
+  @Test
+  public void test1() {
+    assertEquals("/home", solution1.simplifyPath("/home/"));
+  }
+
+  @Test
+  public void test2() {
+    assertEquals("/c", solution1.simplifyPath("/a/./b/../../c/"));
+  }
+}
