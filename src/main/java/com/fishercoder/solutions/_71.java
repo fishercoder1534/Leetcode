9a8ce12ebd2d867527701417baaf27057d195615
Steve Sun <fishercoder@gmail.com>
2018-02-03 07:42:55 -0800

    refactor 71

--- src/main/java/com/fishercoder/solutions/_71.java
+++ src/main/java/com/fishercoder/solutions/_71.java
@@ -1,6 +1,5 @@
 package com.fishercoder.solutions;
 
-
 import java.util.Arrays;
 import java.util.Deque;
 import java.util.HashSet;
@@ -8,7 +7,9 @@ import java.util.LinkedList;
 import java.util.Set;
 
 /**
- * Given an absolute path for a file (Unix-style), simplify it.
+ * 70. Climbing Stairs
+
+ Given an absolute path for a file (Unix-style), simplify it.
 
  For example,
  path = "/home/", => "/home"
@@ -22,21 +23,22 @@ import java.util.Set;
  */
 public class _71 {
 
+  public static class Solution1 {
     public String simplifyPath(String path) {
-        Deque<String> stack = new LinkedList<>();
-        Set<String> skipSet = new HashSet<>(Arrays.asList("..", ".", ""));
-        for (String dir : path.split("/")) {
-            if (dir.equals("..") && !stack.isEmpty()) {
-                stack.pop();
-            } else if (!skipSet.contains(dir)) {
-                stack.push(dir);
-            }
+      Deque<String> stack = new LinkedList<>();
+      Set<String> skipSet = new HashSet<>(Arrays.asList("..", ".", ""));
+      for (String dir : path.split("/")) {
+        if (dir.equals("..") && !stack.isEmpty()) {
+          stack.pop();
+        } else if (!skipSet.contains(dir)) {
+          stack.push(dir);
         }
-        String result = "";
-        for (String dir : stack) {
-            result = "/" + dir + result;
-        }
-        return result.isEmpty() ? "/" : result;
+      }
+      String result = "";
+      for (String dir : stack) {
+        result = "/" + dir + result;
+      }
+      return result.isEmpty() ? "/" : result;
     }
-
+  }
 }
