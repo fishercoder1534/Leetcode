a7000fa9a89addf2af25beb72b5599d5e80df42d
Steve Sun <fishercoder@gmail.com>
2018-02-08 07:33:05 -0800

    refactor 73

--- src/main/java/com/fishercoder/solutions/_73.java
+++ src/main/java/com/fishercoder/solutions/_73.java
@@ -6,11 +6,11 @@ package com.fishercoder.solutions;
  * Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in place.
 
  Follow up:
+
  Did you use extra space?
  A straight forward solution using O(mn) space is probably a bad idea.
  A simple improvement uses O(m + n) space, but still not the best solution.
  Could you devise a constant space solution?
-
  */
 public class _73 {
 
