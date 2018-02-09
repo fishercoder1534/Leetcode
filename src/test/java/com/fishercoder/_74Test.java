a0862774a526facb9997983c4870ed8c0e9a1aca
Steve Sun <fishercoder@gmail.com>
2018-02-09 07:32:23 -0800

    refactor 74

--- src/test/java/com/fishercoder/_74Test.java
+++ src/test/java/com/fishercoder/_74Test.java
@@ -1,38 +1,29 @@
 package com.fishercoder;
 
 import com.fishercoder.solutions._74;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
 
 public class _74Test {
-    private static _74 test;
-    private static boolean actual;
-    private static boolean expected;
-    private static int target;
-    private static int[][] matrix;
+  private static _74.Solution1 solution1;
+  private static int target;
+  private static int[][] matrix;
 
-    @BeforeClass
-    public static void setup() {
-        test = new _74();
-    }
+  @BeforeClass
+  public static void setup() {
+    solution1 = new _74.Solution1();
+  }
 
-    @Before
-    public void setupForEachTest() {
-    }
-
-    @Test
-    public void test1() {
-        target = 3;
-        matrix = new int[][]{
-                {1, 3, 5, 7},
-                {10, 11, 16, 20},
-                {23, 30, 34, 50},
-        };
-        expected = true;
-        actual = test.searchMatrix(matrix, target);
-        assertEquals(expected, actual);
-    }
+  @Test
+  public void test1() {
+    target = 3;
+    matrix = new int[][] {
+        {1, 3, 5, 7},
+        {10, 11, 16, 20},
+        {23, 30, 34, 50},
+    };
+    assertEquals(true, solution1.searchMatrix(matrix, target));
+  }
 }
