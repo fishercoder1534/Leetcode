e8e8a5979eba23e7b2ad54616405018910b2045b
Steve Sun <fishercoder@gmail.com>
2018-02-16 08:29:27 -0800

    refactor 80

--- src/main/java/com/fishercoder/solutions/_80.java
+++ src/main/java/com/fishercoder/solutions/_80.java
@@ -1,8 +1,11 @@
 package com.fishercoder.solutions;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
+ * 80. Remove Duplicates from Sorted Array II
+ *
  * Follow up for "Remove Duplicates":
  What if duplicates are allowed at most twice?
 
@@ -14,36 +17,37 @@ import java.util.ArrayList;
  */
 public class _80 {
 
+  public static class Solution1 {
     public int removeDuplicates(int[] nums) {
-        int counter = 0;
-        int len = nums.length;
-        if (len == 0) {
-            return 0;
-        }
-        if (len == 1) {
-            return 1;
-        }
-        if (len == 2) {
-            return 2;
-        }
+      int counter = 0;
+      int len = nums.length;
+      if (len == 0) {
+        return 0;
+      }
+      if (len == 1) {
+        return 1;
+      }
+      if (len == 2) {
+        return 2;
+      }
 
-        ArrayList<Integer> a = new ArrayList();
-        a.add(nums[0]);
-        a.add(nums[1]);
-        for (int i = 2; i < len; i++) {
-            if (nums[i] != nums[i - 1]) {
-                a.add(nums[i]);
-            } else if (nums[i] != nums[i - 2]) {
-                a.add(nums[i]);
-            }
+      List<Integer> a = new ArrayList();
+      a.add(nums[0]);
+      a.add(nums[1]);
+      for (int i = 2; i < len; i++) {
+        if (nums[i] != nums[i - 1]) {
+          a.add(nums[i]);
+        } else if (nums[i] != nums[i - 2]) {
+          a.add(nums[i]);
         }
+      }
 
-        counter = a.size();
-        for (int i = 0; i < counter; i++) {
-            nums[i] = a.get(i);
-        }
-
-        return counter;
+      counter = a.size();
+      for (int i = 0; i < counter; i++) {
+        nums[i] = a.get(i);
+      }
+      return counter;
     }
+  }
 
 }
