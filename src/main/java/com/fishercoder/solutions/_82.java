099d8ace3489f16e38d1a29a815141db3b35a90c
Steve Sun <fishercoder@gmail.com>
2018-02-19 07:30:05 -0800

    refactor 82

--- src/main/java/com/fishercoder/solutions/_82.java
+++ src/main/java/com/fishercoder/solutions/_82.java
@@ -13,27 +13,28 @@ import com.fishercoder.common.classes.ListNode;
 
  */
 public class _82 {
-
+  public static class Solution1 {
     public ListNode deleteDuplicates(ListNode head) {
-        if (head == null) {
-            return head;
+      if (head == null) {
+        return head;
+      }
+      ListNode fakeHead = new ListNode(-1);
+      fakeHead.next = head;
+      ListNode pre = fakeHead;
+      ListNode curr = head;
+      while (curr != null) {
+        while (curr.next != null && curr.val == curr.next.val) {
+          curr = curr.next;
         }
-        ListNode fakeHead = new ListNode(-1);
-        fakeHead.next = head;
-        ListNode pre = fakeHead;
-        ListNode curr = head;
-        while (curr != null) {
-            while (curr.next != null && curr.val == curr.next.val) {
-                curr = curr.next;
-            }
-            if (pre.next == curr) {
-                pre = pre.next;
-            } else {
-                pre.next = curr.next;
-            }
-            curr = curr.next;
+        if (pre.next == curr) {
+          pre = pre.next;
+        } else {
+          pre.next = curr.next;
         }
-        return fakeHead.next;
+        curr = curr.next;
+      }
+      return fakeHead.next;
     }
+  }
 
 }
--- src/test/java/com/fishercoder/_82Test.java
+++ src/test/java/com/fishercoder/_82Test.java
@@ -1,58 +1,34 @@
 package com.fishercoder;
 
 import com.fishercoder.common.classes.ListNode;
+import com.fishercoder.common.utils.LinkedListUtils;
 import com.fishercoder.solutions._82;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-/**
- * Created by fishercoder on 5/1/17.
- */
 public class _82Test {
 
-    @Test
-    public void test1() {
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
-        head.next.next.next = new ListNode(3);
-        head.next.next.next.next = new ListNode(4);
-        head.next.next.next.next.next = new ListNode(4);
-        head.next.next.next.next.next.next = new ListNode(5);
+  private static _82.Solution1 solution1;
+  private static ListNode head;
+  private static ListNode expected;
 
-        _82 test = new _82();
+  @BeforeClass
+  public static void setup() {
+    solution1 = new _82.Solution1();
+  }
 
-        ListNode expected = new ListNode(1);
-        expected.next = new ListNode(2);
-        expected.next.next = new ListNode(5);
+  @Test
+  public void test1() {
+    head = LinkedListUtils.contructLinkedList(new int[] {1, 2, 3, 3, 4, 4, 5});
+    expected = LinkedListUtils.contructLinkedList(new int[] {1, 2, 5});
+    Assert.assertEquals(expected, solution1.deleteDuplicates(head));
+  }
 
-        Assert.assertEquals(expected, test.deleteDuplicates(head));
-    }
-
-    @Test
-    public void test2() {
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(1);
-        head.next.next = new ListNode(1);
-        head.next.next.next = new ListNode(2);
-        head.next.next.next.next = new ListNode(3);
-
-        _82 test = new _82();
-
-        ListNode expected = new ListNode(2);
-        expected.next = new ListNode(3);
-
-        Assert.assertEquals(expected, test.deleteDuplicates(head));
-    }
-
-    @Test
-    public void test3() {
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(1);
-
-        _82 test = new _82();
-
-        ListNode expected = null;
-        Assert.assertEquals(expected, test.deleteDuplicates(head));
-    }
+  @Test
+  public void test2() {
+    head = LinkedListUtils.contructLinkedList(new int[] {1, 1, 1, 2, 3});
+    expected = LinkedListUtils.contructLinkedList(new int[] {2, 3});
+    Assert.assertEquals(expected, solution1.deleteDuplicates(head));
+  }
 }
