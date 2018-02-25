addbe74b4a294aa669eafd3b3d618124be47d4e1
Steve Sun <fishercoder@gmail.com>
2018-02-25 14:10:43 -0800

    add 788

---
+++ src/test/java/com/fishercoder/_788Test.java
@@ -0,0 +1,21 @@
+package com.fishercoder;
+
+import com.fishercoder.solutions._788;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class _788Test {
+  private static _788.Solution1 solution1;
+
+  @BeforeClass
+  public static void setup() {
+    solution1 = new _788.Solution1();
+  }
+
+  @Test
+  public void test1() {
+    assertEquals(4, solution1.rotatedDigits(10));
+  }
+}
