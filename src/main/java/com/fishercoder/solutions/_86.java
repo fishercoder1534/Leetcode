b9d75b73142d1d54dd988b16ff59f45ac0993006
Steve Sun <fishercoder@gmail.com>
2018-02-27 07:22:15 -0800

    refactor 86

--- src/main/java/com/fishercoder/solutions/_86.java
+++ src/main/java/com/fishercoder/solutions/_86.java
@@ -15,27 +15,28 @@ import com.fishercoder.common.classes.ListNode;
  */
 public class _86 {
 
+  public static class Solution1 {
     public ListNode partition(ListNode head, int x) {
-        if (head == null || head.next == null) {
-            return head;
+      if (head == null || head.next == null) {
+        return head;
+      }
+      ListNode left = new ListNode(0);
+      ListNode right = new ListNode(0);
+      ListNode less = left;
+      ListNode greater = right;
+      while (head != null) {
+        if (head.val < x) {
+          less.next = head;
+          less = less.next;
+        } else {
+          greater.next = head;
+          greater = greater.next;
         }
-        ListNode left = new ListNode(0);
-        ListNode right = new ListNode(0);
-        ListNode less = left;
-        ListNode greater = right;
-        while (head != null) {
-            if (head.val < x) {
-                less.next = head;
-                less = less.next;
-            } else {
-                greater.next = head;
-                greater = greater.next;
-            }
-            head = head.next;
-        }
-        greater.next = null;
-        less.next = right.next;
-        return left.next;
+        head = head.next;
+      }
+      greater.next = null;
+      less.next = right.next;
+      return left.next;
     }
-
+  }
 }
