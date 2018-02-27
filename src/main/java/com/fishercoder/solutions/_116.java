f96f054f675bf80cf7189962603a0d42f58f5439
David <10097zhou@gmail.com>
2018-02-27 10:24:25 -0800

    Update 116

--- src/main/java/com/fishercoder/solutions/_116.java
+++ src/main/java/com/fishercoder/solutions/_116.java
@@ -39,40 +39,18 @@ public class _116 {
         //credit: https://discuss.leetcode.com/topic/1106/o-1-space-o-n-complexity-iterative-solution
         //based on level order traversal
         public void connect(TreeLinkNode root) {
+          if(root == null)
+           return;
 
-            TreeLinkNode head = null; //head of the next level
-            TreeLinkNode prev = null; //the leading node on the next level
-            TreeLinkNode curr = root;  //current node of current level
-
-            while (curr != null) {
-                while (curr != null) { //iterate on the current level
-                    //left child
-                    if (curr.left != null) {
-                        if (prev != null) {
-                            prev.next = curr.left;
-                        } else {
-                            head = curr.left;
-                        }
-                        prev = curr.left;
-                    }
-                    //right child
-                    if (curr.right != null) {
-                        if (prev != null) {
-                            prev.next = curr.right;
-                        } else {
-                            head = curr.right;
-                        }
-                        prev = curr.right;
-                    }
-                    //move to next node
-                    curr = curr.next;
-                }
-                //move to next level
-                curr = head;
-                head = null;
-                prev = null;
-            }
+          if(root.left != null){
+            root.left.next = root.right;
+          if(root.next != null)
+            root.right.next = root.next.left;
         }
+
+       connect(root.left);
+       connect(root.right);
+}
     }
 
 }
