2d01f2fbd09fb77a09c1e5b6ed46b2ed30a8ab98
Steve Sun <fishercoder@gmail.com>
2018-02-28 08:15:17 -0800

    refactor 87

--- src/test/java/com/fishercoder/_87Test.java
+++ src/test/java/com/fishercoder/_87Test.java
@@ -6,30 +6,26 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by stevesun on 6/4/17.
- */
 public class _87Test {
-    private static _87 test;
+  private static _87.Solution1 solution1;
 
-    @BeforeClass
-    public static void setup() {
-        test = new _87();
-    }
+  @BeforeClass
+  public static void setup() {
+    solution1 = new _87.Solution1();
+  }
 
-    @Test
-    public void test1() {
-        assertEquals(true, test.isScramble("great", "rgeat"));
-    }
+  @Test
+  public void test1() {
+    assertEquals(true, solution1.isScramble("great", "rgeat"));
+  }
 
-    @Test
-    public void test2() {
-        assertEquals(true, test.isScramble("great", "rgtae"));
-    }
+  @Test
+  public void test2() {
+    assertEquals(true, solution1.isScramble("great", "rgtae"));
+  }
 
-    @Test
-    public void test3() {
-        assertEquals(true, test.isScramble("abc", "bca"));
-    }
-
+  @Test
+  public void test3() {
+    assertEquals(true, solution1.isScramble("abc", "bca"));
+  }
 }
