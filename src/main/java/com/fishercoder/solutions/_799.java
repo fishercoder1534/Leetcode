0672e9664491397a630cfb6d3b7fbb2ea017d4de
Steve Sun <fishercoder@gmail.com>
2018-03-14 10:47:00 -0700

    fix build

--- src/main/java/com/fishercoder/solutions/_799.java
+++ src/main/java/com/fishercoder/solutions/_799.java
@@ -37,10 +37,10 @@ public class _799 {
       dp[0][0] = poured;
       for (int row = 0; row <= query_row; row++) {
         for (int col = 0; col <= row; col++) {
-          double quantity = (dp[row][col] - 1.0)/2.0;
+          double quantity = (dp[row][col] - 1.0) / 2.0;
           if (quantity > 0) {
-            dp[row+1][col] += quantity;
-            dp[row+1][col+1] += quantity;
+            dp[row + 1][col] += quantity;
+            dp[row + 1][col + 1] += quantity;
           }
         }
       }
