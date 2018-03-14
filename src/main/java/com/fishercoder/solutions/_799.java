dca0dd76fbd8ea4127633823a8f004b09d66ed35
Steve Sun <fishercoder@gmail.com>
2018-03-14 10:56:18 -0700

    fix build

--- src/main/java/com/fishercoder/solutions/_799.java
+++ src/main/java/com/fishercoder/solutions/_799.java
@@ -32,10 +32,10 @@ package com.fishercoder.solutions;
  */
 public class _799 {
   public static class Solution1 {
-    public double champagneTower(int poured, int query_row, int query_glass) {
+    public double champagneTower(int poured, int queryRow, int queryGlass) {
       double[][] dp = new double[101][101];
       dp[0][0] = poured;
-      for (int row = 0; row <= query_row; row++) {
+      for (int row = 0; row <= queryRow; row++) {
         for (int col = 0; col <= row; col++) {
           double quantity = (dp[row][col] - 1.0) / 2.0;
           if (quantity > 0) {
@@ -44,7 +44,7 @@ public class _799 {
           }
         }
       }
-      return Math.min(dp[query_row][query_glass], 1.0);
+      return Math.min(dp[queryRow][queryGlass], 1.0);
     }
   }
 }
