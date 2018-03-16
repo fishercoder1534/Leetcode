f8728b1b83d0a2f3a908eb46f96138a2e35b5186
Steve Sun <fishercoder@gmail.com>
2018-03-16 08:09:27 -0700

    refactor 103

--- src/main/java/com/fishercoder/solutions/_103.java
+++ src/main/java/com/fishercoder/solutions/_103.java
@@ -8,10 +8,9 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-
 /**
- * 103. Binary Tree Zigzag Level Order Traversal  
- * 
+ * 103. Binary Tree Zigzag Level Order Traversal
+ *
 Given a binary tree, return the zigzag level order traversal of its nodes' values.
  (ie, from left to right, then right to left for the next level and alternate between).
 
@@ -30,36 +29,38 @@ return its zigzag level order traversal as:
 ]
 */
 public class _103 {
-    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        Queue<TreeNode> q = new LinkedList();
-        List<List<Integer>> levels = new ArrayList();
-        if (root == null) {
+    public static class Solution1 {
+        public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+            Queue<TreeNode> q = new LinkedList();
+            List<List<Integer>> levels = new ArrayList();
+            if (root == null) {
+                return levels;
+            }
+            q.offer(root);
+            boolean forward = true;
+            while (!q.isEmpty()) {
+                int size = q.size();
+                List<Integer> level = new ArrayList();
+                for (int i = 0; i < size; i++) {
+                    TreeNode curr = q.poll();
+                    level.add(curr.val);
+                    if (curr.left != null) {
+                        q.offer(curr.left);
+                    }
+                    if (curr.right != null) {
+                        q.offer(curr.right);
+                    }
+                }
+                if (forward) {
+                    forward = false;
+                    levels.add(level);
+                } else {
+                    Collections.reverse(level);
+                    levels.add(level);
+                    forward = true;
+                }
+            }
             return levels;
         }
-        q.offer(root);
-        boolean forward = true;
-        while (!q.isEmpty()) {
-            int size = q.size();
-            List<Integer> level = new ArrayList();
-            for (int i = 0; i < size; i++) {
-                TreeNode curr = q.poll();
-                level.add(curr.val);
-                if (curr.left != null) {
-                    q.offer(curr.left);
-                }
-                if (curr.right != null) {
-                    q.offer(curr.right);
-                }
-            }
-            if (forward) {
-                forward = false;
-                levels.add(level);
-            } else {
-                Collections.reverse(level);
-                levels.add(level);
-                forward = true;
-            }
-        }
-        return levels;
     }
 }
