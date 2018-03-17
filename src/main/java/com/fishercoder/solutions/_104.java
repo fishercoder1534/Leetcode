836454935b8831fbab32c244389ad767faf5c715
Steve Sun <fishercoder@gmail.com>
2018-03-17 08:22:51 -0700

    refactor 104

--- src/main/java/com/fishercoder/solutions/_104.java
+++ src/main/java/com/fishercoder/solutions/_104.java
@@ -4,16 +4,19 @@ import com.fishercoder.common.classes.TreeNode;
 
 /**
  * 104. Maximum Depth of Binary Tree
+ *
  * Given a binary tree, find its maximum depth.
  * The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
  */
 public class _104 {
 
+  public static class Solution1 {
     public int maxDepth(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+      if (root == null) {
+        return 0;
+      }
+      return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
+  }
 
 }
