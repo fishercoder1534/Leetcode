4b61aea93224c6e4df34846e549946877386b950
Steve Sun <fishercoder@gmail.com>
2018-03-19 16:49:43 -0700

    refactor 491

--- src/main/java/com/fishercoder/solutions/_491.java
+++ src/main/java/com/fishercoder/solutions/_491.java
@@ -1,6 +1,5 @@
 package com.fishercoder.solutions;
 
-
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -23,28 +22,29 @@ import java.util.Set;
  */
 public class _491 {
 
-    /**I made it by myself this time! Cheers!*/
-    public List<List<Integer>> findSubsequences(int[] nums) {
-        if (nums == null || nums.length == 1) {
-            return new ArrayList<>();
-        }
-        Set<List<Integer>> answer = new HashSet<>();
-        List<Integer> list = new ArrayList<>();
-        return new ArrayList<>(backtracking(nums, 0, list, answer));
-    }
-
-    private Set<List<Integer>> backtracking(int[] nums, int start, List<Integer> currList, Set<List<Integer>> answer) {
-        if (currList.size() >= 2) {
-            answer.add(new ArrayList<>(currList));
-        }
-        for (int i = start; i < nums.length; i++) {
-            if (currList.size() == 0 || currList.get(currList.size() - 1) <= nums[i]) {
-                currList.add(nums[i]);
-                backtracking(nums, i + 1, currList, answer);
-                currList.remove(currList.size() - 1);
+    public static class Solution1 {
+        public List<List<Integer>> findSubsequences(int[] nums) {
+            if (nums == null || nums.length == 1) {
+                return new ArrayList<>();
             }
+            Set<List<Integer>> answer = new HashSet<>();
+            List<Integer> list = new ArrayList<>();
+            return new ArrayList<>(backtracking(nums, 0, list, answer));
         }
-        return answer;
-    }
 
+        private Set<List<Integer>> backtracking(int[] nums, int start, List<Integer> currList,
+            Set<List<Integer>> answer) {
+            if (currList.size() >= 2) {
+                answer.add(new ArrayList<>(currList));
+            }
+            for (int i = start; i < nums.length; i++) {
+                if (currList.size() == 0 || currList.get(currList.size() - 1) <= nums[i]) {
+                    currList.add(nums[i]);
+                    backtracking(nums, i + 1, currList, answer);
+                    currList.remove(currList.size() - 1);
+                }
+            }
+            return answer;
+        }
+    }
 }
--- src/test/java/com/fishercoder/_491Test.java
+++ src/test/java/com/fishercoder/_491Test.java
@@ -7,22 +7,19 @@ import org.junit.Test;
 
 import java.util.List;
 
-/**
- * Created by stevesun on 5/30/17.
- */
 public class _491Test {
-    private static _491 test;
-    private static int[] nums;
+  private static _491.Solution1 solution1;
+  private static int[] nums;
 
-    @BeforeClass
-    public static void setup() {
-        test = new _491();
-    }
+  @BeforeClass
+  public static void setup() {
+    solution1 = new _491.Solution1();
+  }
 
-    @Test
-    public void test1() {
-        nums = new int[]{4, 6, 7, 7};
-        List<List<Integer>> actual = test.findSubsequences(nums);
-        CommonUtils.printListList(actual);
-    }
+  @Test
+  public void test1() {
+    nums = new int[] {4, 6, 7, 7};
+    List<List<Integer>> actual = solution1.findSubsequences(nums);
+    CommonUtils.printListList(actual);
+  }
 }
