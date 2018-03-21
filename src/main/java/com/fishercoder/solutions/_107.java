7e42238ecb7286fe2a1d48f89eb748c09fb8257d
Steve Sun <fishercoder@gmail.com>
2018-03-21 07:36:12 -0700

    refactor 107

--- src/main/java/com/fishercoder/solutions/_107.java
+++ src/main/java/com/fishercoder/solutions/_107.java
@@ -8,7 +8,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-
 /**107. Binary Tree Level Order Traversal II
 
 Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
@@ -32,30 +31,32 @@ return its bottom-up level order traversal as:
 */
 
 public class _107 {
-	public List<List<Integer>> levelOrder(TreeNode root) {
-		List<List<Integer>> result = new ArrayList();
-		if (root == null) {
+	public static class Solution1 {
+		public List<List<Integer>> levelOrder(TreeNode root) {
+			List<List<Integer>> result = new ArrayList();
+			if (root == null) {
+				return result;
+			}
+
+			Queue<TreeNode> q = new LinkedList();
+			q.offer(root);
+			while (!q.isEmpty()) {
+				List<Integer> thisLevel = new ArrayList<Integer>();
+				int qSize = q.size();
+				for (int i = 0; i < qSize; i++) {
+					TreeNode curr = q.poll();
+					thisLevel.add(curr.val);
+					if (curr.left != null) {
+						q.offer(curr.left);
+					}
+					if (curr.right != null) {
+						q.offer(curr.right);
+					}
+				}
+				result.add(thisLevel);
+			}
+			Collections.reverse(result);
 			return result;
 		}
-
-		Queue<TreeNode> q = new LinkedList();
-		q.offer(root);
-		while (!q.isEmpty()) {
-			List<Integer> thisLevel = new ArrayList<Integer>();
-			int qSize = q.size();
-			for (int i = 0; i < qSize; i++) {
-				TreeNode curr = q.poll();
-				thisLevel.add(curr.val);
-				if (curr.left != null) {
-					q.offer(curr.left);
-				}
-				if (curr.right != null) {
-					q.offer(curr.right);
-				}
-			}
-			result.add(thisLevel);
-		}
-		Collections.reverse(result);//this is the only line that gets added/changed to the previous solution.
-		return result;
 	}
 }
