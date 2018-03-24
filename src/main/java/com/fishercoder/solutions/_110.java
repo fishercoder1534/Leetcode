66e640ec09dba940da54ea498bd02a4804e59b1a
Steve Sun <fishercoder@gmail.com>
2018-03-24 08:15:29 -0700

    refactor 110

--- src/main/java/com/fishercoder/solutions/_110.java
+++ src/main/java/com/fishercoder/solutions/_110.java
@@ -3,13 +3,39 @@ package com.fishercoder.solutions;
 import com.fishercoder.common.classes.TreeNode;
 
 /**
+ * 110. Balanced Binary Tree
+ *
  * Given a binary tree, determine if it is height-balanced.
+ * For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
 
- For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.*/
+ Example 1:
+ Given the following tree [3,9,20,null,null,15,7]:
+
+    3
+   / \
+  9  20
+  /  \
+ 15  7
+
+ Return true.
+
+ Example 2:
+ Given the following tree [1,2,2,3,3,null,null,4,4]:
+
+     1
+    / \
+   2   2
+  / \
+ 3   3
+/ \
+4 4
+
+ Return false.
+ */
 
 public class _110 {
 
-    class Solution1 {
+    public static class Solution1 {
         //recursively get the height of each subtree of each node, compare their difference, if greater than 1, then return false
         //although this is working, but it's not efficient, since it repeatedly computes the heights of each node every time
         //Its time complexity is O(n^2).
@@ -34,7 +60,7 @@ public class _110 {
         }
     }
 
-    class Solution2 {
+    public static class Solution2 {
 
         public boolean isBalanced(TreeNode root) {
             return getH(root) != -1;
