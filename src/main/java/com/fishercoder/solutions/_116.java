f57f8605f3dc23248846e426f2be406e862a738e
Steve Sun <fishercoder@gmail.com>
2018-03-30 07:26:53 -0700

    refactor 116

--- src/main/java/com/fishercoder/solutions/_116.java
+++ src/main/java/com/fishercoder/solutions/_116.java
@@ -4,8 +4,8 @@ import com.fishercoder.common.classes.TreeLinkNode;
 
 /**
  * 116. Populating Next Right Pointers in Each Node
- *
- * Given a binary tree
+
+ Given a binary tree
 
  struct TreeLinkNode {
      TreeLinkNode *left;
@@ -27,52 +27,55 @@ import com.fishercoder.common.classes.TreeLinkNode;
    2    3
   / \  / \
  4  5  6  7
+
  After calling your function, the tree should look like:
       1 -> NULL
     /  \
    2 -> 3 -> NULL
   / \  / \
- 4->5->6->7 -> NULL */
+ 4->5->6->7 -> NULL
+ */
 
 public class _116 {
-    public static class Solution1 {
-        //credit: https://discuss.leetcode.com/topic/1106/o-1-space-o-n-complexity-iterative-solution
-        //based on level order traversal
-        public void connect(TreeLinkNode root) {
+  public static class Solution1 {
+    /**
+     * credit: https://discuss.leetcode.com/topic/1106/o-1-space-o-n-complexity-iterative-solution
+     * based on level order traversal
+     */
+    public void connect(TreeLinkNode root) {
 
-            TreeLinkNode head = null; //head of the next level
-            TreeLinkNode prev = null; //the leading node on the next level
-            TreeLinkNode curr = root;  //current node of current level
+      TreeLinkNode head = null; //head of the next level
+      TreeLinkNode prev = null; //the leading node on the next level
+      TreeLinkNode curr = root;  //current node of current level
 
-            while (curr != null) {
-                while (curr != null) { //iterate on the current level
-                    //left child
-                    if (curr.left != null) {
-                        if (prev != null) {
-                            prev.next = curr.left;
-                        } else {
-                            head = curr.left;
-                        }
-                        prev = curr.left;
-                    }
-                    //right child
-                    if (curr.right != null) {
-                        if (prev != null) {
-                            prev.next = curr.right;
-                        } else {
-                            head = curr.right;
-                        }
-                        prev = curr.right;
-                    }
-                    //move to next node
-                    curr = curr.next;
-                }
-                //move to next level
-                curr = head;
-                head = null;
-                prev = null;
+      while (curr != null) {
+        while (curr != null) { //iterate on the current level
+          //left child
+          if (curr.left != null) {
+            if (prev != null) {
+              prev.next = curr.left;
+            } else {
+              head = curr.left;
             }
+            prev = curr.left;
+          }
+          //right child
+          if (curr.right != null) {
+            if (prev != null) {
+              prev.next = curr.right;
+            } else {
+              head = curr.right;
+            }
+            prev = curr.right;
+          }
+          //move to next node
+          curr = curr.next;
         }
+        //move to next level
+        curr = head;
+        head = null;
+        prev = null;
+      }
     }
-
+  }
 }
