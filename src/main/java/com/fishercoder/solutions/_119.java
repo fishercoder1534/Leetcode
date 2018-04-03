78004df65bd5e6c127785a6773682e4668d9c5d6
Steve Sun <fishercoder@gmail.com>
2018-04-03 08:18:53 -0700

    refactor 119

--- src/main/java/com/fishercoder/solutions/_119.java
+++ src/main/java/com/fishercoder/solutions/_119.java
@@ -1,6 +1,5 @@
 package com.fishercoder.solutions;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,40 +16,40 @@ import java.util.List;
 
 public class _119 {
 
-    public static class Solution1 {
-        public List<Integer> getRow(int rowIndex) {
-            if (rowIndex < 0) {
-                return new ArrayList();
-            }
-            List<List<Integer>> result = new ArrayList();
-            List<Integer> row = new ArrayList();
-            row.add(1);
-            result.add(row);
-            for (int i = 1; i <= rowIndex; i++) {
-                List<Integer> newRow = new ArrayList();
-                newRow.add(1);
-                List<Integer> lastRow = result.get(i - 1);
-                for (int j = 1; j < lastRow.size(); j++) {
-                    newRow.add(lastRow.get(j - 1) + lastRow.get(j));
-                }
-                newRow.add(1);
-                result.add(newRow);
-            }
-            return result.get(result.size() - 1);
+  public static class Solution1 {
+    public List<Integer> getRow(int rowIndex) {
+      if (rowIndex < 0) {
+        return new ArrayList();
+      }
+      List<List<Integer>> result = new ArrayList();
+      List<Integer> row = new ArrayList();
+      row.add(1);
+      result.add(row);
+      for (int i = 1; i <= rowIndex; i++) {
+        List<Integer> newRow = new ArrayList();
+        newRow.add(1);
+        List<Integer> lastRow = result.get(i - 1);
+        for (int j = 1; j < lastRow.size(); j++) {
+          newRow.add(lastRow.get(j - 1) + lastRow.get(j));
         }
+        newRow.add(1);
+        result.add(newRow);
+      }
+      return result.get(result.size() - 1);
     }
+  }
 
-    public static class SolutionOkSpace {
-        public List<Integer> getRow(int rowIndex) {
-            List<Integer> row = new ArrayList<>();
-            for (int i = 0; i < rowIndex + 1; i++) {
-                row.add(0, 1);
-                for (int j = 1; j < row.size() - 1; j++) {
-                    row.set(j, row.get(j) + row.get(j + 1));
-                }
-            }
-            return row;
+  public static class Solution2 {
+    /** O(k) space */
+    public List<Integer> getRow(int rowIndex) {
+      List<Integer> row = new ArrayList<>();
+      for (int i = 0; i < rowIndex + 1; i++) {
+        row.add(0, 1);
+        for (int j = 1; j < row.size() - 1; j++) {
+          row.set(j, row.get(j) + row.get(j + 1));
         }
+      }
+      return row;
     }
-
+  }
 }
