d7da56d120ecc5eb77ea7fc40633027d03e58233
Steve Sun <fishercoder@gmail.com>
2018-04-05 08:14:54 -0700

    refactor 121

--- src/main/java/com/fishercoder/solutions/_121.java
+++ src/main/java/com/fishercoder/solutions/_121.java
@@ -24,24 +24,26 @@ package com.fishercoder.solutions;
 
 public class _121 {
 
-    /**
-     * The key here is that you'll have to buy first, before you can sell.
-     * That means, if the lower price comes after a higher price, their combination won't work! Since you cannot sell first
-     * before you buy it.
-     */
-    public int maxProfit(int[] prices) {
-        if (prices == null || prices.length == 0) {
-            return 0;
-        }
-        int buy = prices[0];
-        int maxProfit = 0;
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] < buy) {
-                buy = prices[i];
-            } else {
-                maxProfit = Math.max(maxProfit, prices[i] - buy);
+    public static class Solution1 {
+        /**
+         * The key here is that you'll have to buy first, before you can sell. That means, if the lower
+         * price comes after a higher price, their combination won't work! Since you cannot sell first
+         * before you buy it.
+         */
+        public int maxProfit(int[] prices) {
+            if (prices == null || prices.length == 0) {
+                return 0;
             }
+            int buy = prices[0];
+            int maxProfit = 0;
+            for (int i = 1; i < prices.length; i++) {
+                if (prices[i] < buy) {
+                    buy = prices[i];
+                } else {
+                    maxProfit = Math.max(maxProfit, prices[i] - buy);
+                }
+            }
+            return maxProfit;
         }
-        return maxProfit;
     }
 }
