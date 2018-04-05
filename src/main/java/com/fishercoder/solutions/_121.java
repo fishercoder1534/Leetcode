046075aba347acc8e11f84161ccd949699f409c2
Steve Sun <fishercoder@gmail.com>
2018-04-05 08:15:27 -0700

    refactor 121 again

--- src/main/java/com/fishercoder/solutions/_121.java
+++ src/main/java/com/fishercoder/solutions/_121.java
@@ -14,7 +14,6 @@ package com.fishercoder.solutions;
  *
  * max. difference = 6-1 = 5 (not 7-1 = 6, as selling price needs to be larger than buying price)
  *
- *
  * Example 2:
  * Input: [7, 6, 4, 3, 1]
  * Output: 0
