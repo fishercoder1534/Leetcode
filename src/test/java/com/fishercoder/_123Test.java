281d511a61bb976b7816e28f23a3a33ab1b72dab
Steve Sun <fishercoder@gmail.com>
2018-04-07 06:58:06 -0700

    refactor 123

--- src/test/java/com/fishercoder/_123Test.java
+++ src/test/java/com/fishercoder/_123Test.java
@@ -7,18 +7,17 @@ import org.junit.Test;
 import static junit.framework.Assert.assertEquals;
 
 public class _123Test {
-    private static _123.Solution1 solution1;
-    private static int[] prices;
+  private static _123.Solution1 solution1;
+  private static int[] prices;
 
-    @BeforeClass
-    public static void setup() {
-        solution1 = new _123.Solution1();
-    }
+  @BeforeClass
+  public static void setup() {
+    solution1 = new _123.Solution1();
+  }
 
-    @Test
-    public void test1() {
-        prices = new int[]{1};
-        assertEquals(0, solution1.maxProfit(prices));
-    }
-
+  @Test
+  public void test1() {
+    prices = new int[] {1};
+    assertEquals(0, solution1.maxProfit(prices));
+  }
 }
