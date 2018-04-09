3351fb7658b327f1e8daceec6c2c2b91f0e57924
Steve Sun <fishercoder@gmail.com>
2018-04-09 07:32:55 -0700

    refactor 125

--- src/main/java/com/fishercoder/solutions/_125.java
+++ src/main/java/com/fishercoder/solutions/_125.java
@@ -1,6 +1,9 @@
 package com.fishercoder.solutions;
 
-/**Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+/**
+ * 125. Valid Palindrome
+
+ Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
 
  For example,
  "A man, a plan, a canal: Panama" is a palindrome.
@@ -8,28 +11,30 @@ package com.fishercoder.solutions;
 
  Note:
  Have you consider that the string might be empty? This is a good question to ask during an interview.
+ For the purpose of this problem, we define empty string as valid palindrome.
+ */
 
- For the purpose of this problem, we define empty string as valid palindrome.*/
 public class _125 {
 
+  public static class Solution1 {
     public boolean isPalindrome(String s) {
-        int i = 0;
-        int j = s.length() - 1;
-        char[] chars = s.toCharArray();
-        while (i < j) {
-            while (i < j && !Character.isLetterOrDigit(chars[i])) {
-                i++;
-            }
-            while (i < j && !Character.isLetterOrDigit(chars[j])) {
-                j--;
-            }
-            if (Character.toLowerCase(chars[i]) != Character.toLowerCase(chars[j])) {
-                return false;
-            }
-            i++;
-            j--;
+      int i = 0;
+      int j = s.length() - 1;
+      char[] chars = s.toCharArray();
+      while (i < j) {
+        while (i < j && !Character.isLetterOrDigit(chars[i])) {
+          i++;
         }
-        return true;
+        while (i < j && !Character.isLetterOrDigit(chars[j])) {
+          j--;
+        }
+        if (Character.toLowerCase(chars[i]) != Character.toLowerCase(chars[j])) {
+          return false;
+        }
+        i++;
+        j--;
+      }
+      return true;
     }
-
+  }
 }
