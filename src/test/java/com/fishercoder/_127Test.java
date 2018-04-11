02d94cc6bed35d85a92feb8ffcf9fb5c39004168
Steve Sun <fishercoder@gmail.com>
2018-04-11 08:23:10 -0700

    refactor 127

--- src/test/java/com/fishercoder/_127Test.java
+++ src/test/java/com/fishercoder/_127Test.java
@@ -10,27 +10,24 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by stevesun on 6/5/17.
- */
 public class _127Test {
-    private static _127 test;
-    private static List<String> wordList;
+  private static _127.Solution1 solution1;
+  private static List<String> wordList;
 
-    @BeforeClass
-    public static void setup() {
-        test = new _127();
-    }
+  @BeforeClass
+  public static void setup() {
+    solution1 = new _127.Solution1();
+  }
 
-    @Test
-    public void test1() {
-        wordList = new ArrayList<>(Arrays.asList("hot", "dot", "dog", "lot", "log"));
-        assertEquals(0, test.ladderLength("hit", "cog", wordList));
-    }
+  @Test
+  public void test1() {
+    wordList = new ArrayList<>(Arrays.asList("hot", "dot", "dog", "lot", "log"));
+    assertEquals(0, solution1.ladderLength("hit", "cog", wordList));
+  }
 
-    @Test
-    public void test2() {
-        wordList = new ArrayList<>(Arrays.asList("hot", "dot", "dog", "lot", "log", "cog"));
-        assertEquals(5, test.ladderLength("hit", "cog", wordList));
-    }
+  @Test
+  public void test2() {
+    wordList = new ArrayList<>(Arrays.asList("hot", "dot", "dog", "lot", "log", "cog"));
+    assertEquals(5, solution1.ladderLength("hit", "cog", wordList));
+  }
 }
