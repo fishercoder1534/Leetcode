c52c12457031a0109a78466df2dabc9c25e537bb
Steve Sun <fishercoder@gmail.com>
2018-04-13 16:23:37 -0700

    refactor 129

--- src/main/java/com/fishercoder/solutions/_129.java
+++ src/main/java/com/fishercoder/solutions/_129.java
@@ -5,7 +5,10 @@ import com.fishercoder.common.classes.TreeNode;
 import java.util.ArrayList;
 import java.util.List;
 
-/**Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
+/**
+ * 129. Sum Root to Leaf Numbers
+ *
+ * Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
 
  An example is the root-to-leaf path 1->2->3 which represents the number 123.
 
@@ -23,47 +26,49 @@ import java.util.List;
 
  */
 public class _129 {
+  public static class Solution1 {
     public int sumNumbers(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        List<Integer> allNumbers = new ArrayList();
-        dfs(root, new StringBuilder(), allNumbers);
-        int sum = 0;
-        for (int i : allNumbers) {
-            sum += i;
-        }
-        return sum;
+      if (root == null) {
+        return 0;
+      }
+      List<Integer> allNumbers = new ArrayList();
+      dfs(root, new StringBuilder(), allNumbers);
+      int sum = 0;
+      for (int i : allNumbers) {
+        sum += i;
+      }
+      return sum;
     }
 
     private void dfs(TreeNode root, StringBuilder sb, List<Integer> allNumbers) {
-        sb.append(root.val);
-        if (root.left != null) {
-            dfs(root.left, sb, allNumbers);
-        }
-        if (root.right != null) {
-            dfs(root.right, sb, allNumbers);
-        }
-        if (root.left == null && root.right == null) {
-            allNumbers.add(Integer.parseInt(sb.toString()));
-        }
-        sb.deleteCharAt(sb.length() - 1);
+      sb.append(root.val);
+      if (root.left != null) {
+        dfs(root.left, sb, allNumbers);
+      }
+      if (root.right != null) {
+        dfs(root.right, sb, allNumbers);
+      }
+      if (root.left == null && root.right == null) {
+        allNumbers.add(Integer.parseInt(sb.toString()));
+      }
+      sb.deleteCharAt(sb.length() - 1);
+    }
+  }
+
+  public static class Solution2 {
+    public int sumNumbers(TreeNode root) {
+      return dfs(root, 0);
     }
 
-    class MoreConciseVersion {
-        public int sumNumbers(TreeNode root) {
-            return dfs(root, 0);
-        }
-
-        private int dfs(TreeNode root, int sum) {
-            if (root == null) {
-                return 0;
-            }
-            if (root.left == null && root.right == null) {
-                return sum * 10 + root.val;
-            }
-            return dfs(root.left, sum * 10 + root.val) + dfs(root.right, sum * 10 + root.val);
-        }
+    private int dfs(TreeNode root, int sum) {
+      if (root == null) {
+        return 0;
+      }
+      if (root.left == null && root.right == null) {
+        return sum * 10 + root.val;
+      }
+      return dfs(root.left, sum * 10 + root.val) + dfs(root.right, sum * 10 + root.val);
     }
+  }
 
 }
