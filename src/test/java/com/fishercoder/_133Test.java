73e79a8cf02838164da4bc35e8a389a47854b4ff
Steve Sun <fishercoder@gmail.com>
2018-04-22 16:27:40 -0700

    refactor 133

--- src/test/java/com/fishercoder/_133Test.java
+++ src/test/java/com/fishercoder/_133Test.java
@@ -8,36 +8,33 @@ import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
 
-/**
- * Created by fishercoder on 1/15/17.
- */
 public class _133Test {
-    private static _133 test;
-    private static UndirectedGraphNode expected;
-    private static UndirectedGraphNode actual;
+  private static _133.Solution1 solution1;
+  private static UndirectedGraphNode expected;
+  private static UndirectedGraphNode actual;
 
-    @BeforeClass
-    public static void setup() {
-        test = new _133();
-    }
+  @BeforeClass
+  public static void setup() {
+    solution1 = new _133.Solution1();
+  }
 
-    @Before
-    public void setupForEachTest() {
-        expected = null;
-        actual = null;
-    }
+  @Before
+  public void setupForEachTest() {
+    expected = null;
+    actual = null;
+  }
 
-    @Test
-    public void test1() {
-        UndirectedGraphNode node0 = new UndirectedGraphNode(0);
-        UndirectedGraphNode node1 = new UndirectedGraphNode(1);
-        UndirectedGraphNode node2 = new UndirectedGraphNode(2);
-        node0.neighbors.add(node1);
-        node0.neighbors.add(node2);
-        node1.neighbors.add(node2);
+  @Test
+  public void test1() {
+    UndirectedGraphNode node0 = new UndirectedGraphNode(0);
+    UndirectedGraphNode node1 = new UndirectedGraphNode(1);
+    UndirectedGraphNode node2 = new UndirectedGraphNode(2);
+    node0.neighbors.add(node1);
+    node0.neighbors.add(node2);
+    node1.neighbors.add(node2);
 
-        expected = node0;
-        actual = test.cloneGraph(expected);
-        assertEquals(expected, actual);
-    }
+    expected = node0;
+    actual = solution1.cloneGraph(expected);
+    assertEquals(expected, actual);
+  }
 }
