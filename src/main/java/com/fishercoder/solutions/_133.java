d84a1e2a667d16f55340a48caf5875e7af7ccad9
Steve Sun <fishercoder@gmail.com>
2018-04-22 16:27:40 -0700

    refactor 133

--- src/main/java/com/fishercoder/solutions/_133.java
+++ src/main/java/com/fishercoder/solutions/_133.java
@@ -8,7 +8,10 @@ import java.util.Map;
 import java.util.Queue;
 
 /**
- * Clone an undirected graph. Each node in the graph contains a label and a list of its neighbors.
+ * 133. Clone Graph
+
+ Clone an undirected graph.
+ Each node in the graph contains a label and a list of its neighbors.
 
 
  OJ's undirected graph serialization:
@@ -34,28 +37,29 @@ import java.util.Queue;
  */
 public class _133 {
 
+  public static class Solution1 {
     public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
-        if (node == null) {
-            return node;
-        }
+      if (node == null) {
+        return node;
+      }
 
-        Map<Integer, UndirectedGraphNode> map = new HashMap();
-        Queue<UndirectedGraphNode> queue = new LinkedList();
-        UndirectedGraphNode root = new UndirectedGraphNode(node.label);
-        map.put(root.label, root);
-        queue.offer(node);
-        //remember to offer the original input node into the queue which contains all the information
-        while (!queue.isEmpty()) {
-            UndirectedGraphNode curr = queue.poll();
-            for (UndirectedGraphNode eachNode : curr.neighbors) {
-                if (!map.containsKey(eachNode.label)) {
-                    map.put(eachNode.label, new UndirectedGraphNode(eachNode.label));
-                    queue.offer(eachNode);
-                }
-                map.get(curr.label).neighbors.add(map.get(eachNode.label));
-            }
+      Map<Integer, UndirectedGraphNode> map = new HashMap();
+      Queue<UndirectedGraphNode> queue = new LinkedList();
+      UndirectedGraphNode root = new UndirectedGraphNode(node.label);
+      map.put(root.label, root);
+      queue.offer(node);
+      //remember to offer the original input node into the queue which contains all the information
+      while (!queue.isEmpty()) {
+        UndirectedGraphNode curr = queue.poll();
+        for (UndirectedGraphNode eachNode : curr.neighbors) {
+          if (!map.containsKey(eachNode.label)) {
+            map.put(eachNode.label, new UndirectedGraphNode(eachNode.label));
+            queue.offer(eachNode);
+          }
+          map.get(curr.label).neighbors.add(map.get(eachNode.label));
         }
-        return root;
+      }
+      return root;
     }
-
+  }
 }
