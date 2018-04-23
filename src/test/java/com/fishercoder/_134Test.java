538d103d899f56cf01a9ab2e083cf1f6543b7184
Steve Sun <fishercoder@gmail.com>
2018-04-23 00:28:24 -0700

    refactor 134

--- src/test/java/com/fishercoder/_134Test.java
+++ src/test/java/com/fishercoder/_134Test.java
@@ -6,37 +6,34 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by fishercoder on 5/27/17.
- */
 public class _134Test {
-    private static _134 test;
-    private static int[] gas;
-    private static int[] cost;
+  private static _134.Solution1 solution1;
+  private static int[] gas;
+  private static int[] cost;
 
-    @BeforeClass
-    public static void setup() {
-        test = new _134();
-    }
+  @BeforeClass
+  public static void setup() {
+    solution1 = new _134.Solution1();
+  }
 
-    @Test
-    public void test1() {
-        gas = new int[]{4};
-        cost = new int[]{5};
-        assertEquals(-1, test.canCompleteCircuit(gas, cost));
-    }
+  @Test
+  public void test1() {
+    gas = new int[] {4};
+    cost = new int[] {5};
+    assertEquals(-1, solution1.canCompleteCircuit(gas, cost));
+  }
 
-    @Test
-    public void test2() {
-        gas = new int[]{5};
-        cost = new int[]{4};
-        assertEquals(0, test.canCompleteCircuit(gas, cost));
-    }
+  @Test
+  public void test2() {
+    gas = new int[] {5};
+    cost = new int[] {4};
+    assertEquals(0, solution1.canCompleteCircuit(gas, cost));
+  }
 
-    @Test
-    public void test3() {
-        gas = new int[]{2};
-        cost = new int[]{2};
-        assertEquals(0, test.canCompleteCircuit(gas, cost));
-    }
+  @Test
+  public void test3() {
+    gas = new int[] {2};
+    cost = new int[] {2};
+    assertEquals(0, solution1.canCompleteCircuit(gas, cost));
+  }
 }
