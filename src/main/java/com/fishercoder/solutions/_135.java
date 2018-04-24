1f62825bd00533078077fffbce83213ef9e00667
Steve Sun <fishercoder@gmail.com>
2018-04-24 08:45:33 -0700

    refactor 135

--- src/main/java/com/fishercoder/solutions/_135.java
+++ src/main/java/com/fishercoder/solutions/_135.java
@@ -1,7 +1,9 @@
 package com.fishercoder.solutions;
 
 /**
- * There are N children standing in a line. Each child is assigned a rating value.
+ * 135. Candy
+
+ There are N children standing in a line. Each child is assigned a rating value.
 
  You are giving candies to these children subjected to the following requirements:
 
@@ -11,30 +13,31 @@ package com.fishercoder.solutions;
  */
 public class _135 {
 
+  public static class Solution1 {
     public int candy(int[] ratings) {
-        int[] candy = new int[ratings.length];
-        for (int i = 0; i < ratings.length; i++) {
-            candy[i] = 1;
-        }
+      int[] candy = new int[ratings.length];
+      for (int i = 0; i < ratings.length; i++) {
+        candy[i] = 1;
+      }
 
-        for (int i = 0; i < ratings.length - 1; i++) {
-            if (ratings[i] < ratings[i + 1]) {
-                candy[i + 1] = candy[i] + 1;
-            }
+      for (int i = 0; i < ratings.length - 1; i++) {
+        if (ratings[i] < ratings[i + 1]) {
+          candy[i + 1] = candy[i] + 1;
         }
+      }
 
-        for (int i = ratings.length - 1; i > 0; i--) {
-            if (ratings[i] < ratings[i - 1]) {
-                candy[i - 1] = Math.max(candy[i - 1], candy[i] + 1);
-            }
+      for (int i = ratings.length - 1; i > 0; i--) {
+        if (ratings[i] < ratings[i - 1]) {
+          candy[i - 1] = Math.max(candy[i - 1], candy[i] + 1);
         }
+      }
 
-        int sum = 0;
-        for (int i : candy) {
-            sum += i;
-        }
+      int sum = 0;
+      for (int i : candy) {
+        sum += i;
+      }
 
-        return sum;
+      return sum;
     }
-
+  }
 }
