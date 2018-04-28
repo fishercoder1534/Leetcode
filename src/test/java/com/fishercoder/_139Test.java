5a41238b49178b1b78dede433fab3eb646c2eb31
Steve Sun <fishercoder@gmail.com>
2018-04-28 07:54:25 -0700

    refactor 139

--- src/test/java/com/fishercoder/_139Test.java
+++ src/test/java/com/fishercoder/_139Test.java
@@ -11,19 +11,25 @@ import java.util.List;
 import static junit.framework.Assert.assertEquals;
 
 public class _139Test {
-    private static _139.Solution2 solution2;
-    private static String s;
-    private static List<String> wordDict;
+  private static _139.Solution1 solution1;
+  private static _139.Solution2 solution2;
+  private static _139.Solution3 solution3;
+  private static String s;
+  private static List<String> wordDict;
 
-    @BeforeClass
-    public static void setup() {
-        solution2 = new _139.Solution2();
-    }
+  @BeforeClass
+  public static void setup() {
+    solution1 = new _139.Solution1();
+    solution2 = new _139.Solution2();
+    solution3 = new _139.Solution3();
+  }
 
-    @Test
-    public void test1() {
-        s = "leetcode";
-        wordDict = new ArrayList<>(Arrays.asList("leet", "code"));
-        assertEquals(true, solution2.wordBreak(s, wordDict));
-    }
+  @Test
+  public void test1() {
+    s = "leetcode";
+    wordDict = new ArrayList<>(Arrays.asList("leet", "code"));
+    assertEquals(true, solution1.wordBreak(s, wordDict));
+    assertEquals(true, solution2.wordBreak(s, wordDict));
+    assertEquals(true, solution3.wordBreak(s, wordDict));
+  }
 }
