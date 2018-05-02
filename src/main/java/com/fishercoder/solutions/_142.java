c47141099a38bd0296a83a542e324d5680ce8fb8
Steve Sun <fishercoder@gmail.com>
2018-05-02 07:31:30 -0700

    refactor 142

--- src/main/java/com/fishercoder/solutions/_142.java
+++ src/main/java/com/fishercoder/solutions/_142.java
@@ -2,29 +2,35 @@ package com.fishercoder.solutions;
 
 import com.fishercoder.common.classes.ListNode;
 
-/**Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+/**
+ * 142. Linked List Cycle II
+
+ Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
 
  Note: Do not modify the linked list.
 
  Follow up:
- Can you solve it without using extra space?*/
+ Can you solve it without using extra space?
+ */
 public class _142 {
 
+  public static class Solution1 {
     public ListNode detectCycle(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-        while (fast != null && fast.next != null) {
+      ListNode slow = head;
+      ListNode fast = head;
+      while (fast != null && fast.next != null) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow == fast) {
+          ListNode slow2 = head;
+          while (slow2 != slow) {
             slow = slow.next;
-            fast = fast.next.next;
-            if ( slow == fast) {
-                ListNode slow2 = head;
-                while (slow2 != slow) {
-                    slow = slow.next;
-                    slow2 = slow2.next;
-                }
-                return slow;
-            }
+            slow2 = slow2.next;
+          }
+          return slow;
         }
-        return null;
+      }
+      return null;
     }
+  }
 }
