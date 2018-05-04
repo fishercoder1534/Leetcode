bba04c204ce7db6381fbf38f45ffc82129351bc4
Steve Sun <fishercoder@gmail.com>
2018-05-04 06:32:11 -0700

    refactor 144

--- src/main/java/com/fishercoder/solutions/_144.java
+++ src/main/java/com/fishercoder/solutions/_144.java
@@ -7,10 +7,10 @@ import java.util.ArrayList;
 import java.util.Deque;
 import java.util.List;
 
-
 /**
  * 144. Binary Tree Preorder Traversal
- * Given a binary tree, return the preorder traversal of its nodes' values.
+
+ Given a binary tree, return the preorder traversal of its nodes' values.
 
  For example:
  Given binary tree {1,#,2,3},
@@ -24,42 +24,44 @@ import java.util.List;
  Note: Recursive solution is trivial, could you do it iteratively?*/
 
 public class _144 {
-
-    public List<Integer> preorderTraversal_iterative(TreeNode root) {
-        List<Integer> list = new ArrayList();
-        if (root == null) {
-            return list;
-        }
-        Deque<TreeNode> stack = new ArrayDeque<>();
-        stack.push(root);
-        while (!stack.isEmpty()) {
-            TreeNode curr = stack.pop();
-            list.add(curr.val);
-            /**We push right nodes onto the stack first, since they'll be popped out later than
-             * the left nodes, to meet the preorder: root -> left -> right. */
-            if (curr.right != null) {
-                stack.push(curr.right);
-            }
-            if (curr.left != null) {
-                stack.push(curr.left);
-            }
-        }
+  public static class Solution1 {
+    public List<Integer> preorderTraversal(TreeNode root) {
+      List<Integer> list = new ArrayList();
+      if (root == null) {
         return list;
+      }
+      Deque<TreeNode> stack = new ArrayDeque<>();
+      stack.push(root);
+      while (!stack.isEmpty()) {
+        TreeNode curr = stack.pop();
+        list.add(curr.val);
+        /**We push right nodes onto the stack first, since they'll be popped out later than
+         * the left nodes, to meet the preorder: root -> left -> right. */
+        if (curr.right != null) {
+          stack.push(curr.right);
+        }
+        if (curr.left != null) {
+          stack.push(curr.left);
+        }
+      }
+      return list;
     }
+  }
 
-    public List<Integer> preorderTraversal_recursive(TreeNode root) {
-        List<Integer> list = new ArrayList();
-        return pre(root, list);
+  public static class Solution2 {
+    public List<Integer> preorderTraversal(TreeNode root) {
+      List<Integer> list = new ArrayList();
+      return pre(root, list);
     }
 
     List<Integer> pre(TreeNode root, List<Integer> list) {
-        if (root == null) {
-            return list;
-        }
-        list.add(root.val);
-        pre(root.left, list);
-        pre(root.right, list);
+      if (root == null) {
         return list;
+      }
+      list.add(root.val);
+      pre(root.left, list);
+      pre(root.right, list);
+      return list;
     }
-
+  }
 }
