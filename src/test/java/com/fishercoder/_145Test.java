20a66fd799e3421b326c0706adb956f51c1b37a9
Steve Sun <fishercoder@gmail.com>
2018-05-05 06:46:54 -0700

    refactor 145

--- src/test/java/com/fishercoder/_145Test.java
+++ src/test/java/com/fishercoder/_145Test.java
@@ -12,21 +12,24 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 public class _145Test {
-    private static _145 test;
-    private static TreeNode root;
-    private static List<Integer> expected;
+  private static _145.Solution1 solution1;
+  private static _145.Solution2 solution2;
+  private static TreeNode root;
+  private static List<Integer> expected;
 
-    @BeforeClass
-    public static void setup() {
-        test = new _145();
-    }
+  @BeforeClass
+  public static void setup() {
+    solution1 = new _145.Solution1();
+    solution2 = new _145.Solution2();
+  }
 
-    @Test
-    public void test1() {
-        root = new TreeNode(1);
-        root.left = new TreeNode(2);
-        root.right = new TreeNode(3);
-        expected = new ArrayList<>(Arrays.asList(2, 3, 1));
-        assertEquals(expected, test.postorderTraversal_recursive(root));
-    }
+  @Test
+  public void test1() {
+    root = new TreeNode(1);
+    root.left = new TreeNode(2);
+    root.right = new TreeNode(3);
+    expected = new ArrayList<>(Arrays.asList(2, 3, 1));
+    assertEquals(expected, solution1.postorderTraversal(root));
+    assertEquals(expected, solution2.postorderTraversal(root));
+  }
 }
