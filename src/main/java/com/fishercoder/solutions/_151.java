f759cd34a01609cb7d8ee7b98c2adc0d4eb9d634
Steve Sun <fishercoder@gmail.com>
2018-05-10 07:16:10 -0700

    refactor 151

--- src/main/java/com/fishercoder/solutions/_151.java
+++ src/main/java/com/fishercoder/solutions/_151.java
@@ -1,57 +1,48 @@
 package com.fishercoder.solutions;
 
-import com.fishercoder.common.utils.CommonUtils;
-
 import java.util.ArrayDeque;
 import java.util.Deque;
 
 /**
  * 151. Reverse Words in a String
- * Given an input string, reverse the string word by word.
-For example,
-Given s = "the sky is blue",
-return "blue is sky the".
+
+  Given an input string, reverse the string word by word.
+  For example,
+  Given s = "the sky is blue",
+  return "blue is sky the".
 
  Clarification:
+
  What constitutes a word?
  A sequence of non-space characters constitutes a word.
  Could the input string contain leading or trailing spaces?
  Yes. However, your reversed string should not contain leading or trailing spaces.
  How about multiple spaces between two words?
  Reduce them to a single space in the reversed string.
-
  */
 
 public class _151 {
-
+  public static class Solution1 {
     public String reverseWords(String s) {
-        s.trim();
-        if (s == null || s.length() == 0) {
-            return "";
+      s.trim();
+      if (s == null || s.length() == 0) {
+        return "";
+      }
+      String[] words = s.split(" ");
+      if (words == null || words.length == 0) {
+        return "";
+      }
+      Deque<String> stack = new ArrayDeque<>();
+      for (String word : words) {
+        if (!word.equals("")) {
+          stack.offer(word);
         }
-        String[] words = s.split(" ");
-        if (words == null || words.length == 0) {
-            return "";
-        }
-        Deque<String> stack = new ArrayDeque<>();
-        for (String word : words) {
-            if (!word.equals("")) {
-                stack.offer(word);
-            }
-        }
-        StringBuilder stringBuilder = new StringBuilder();
-        while (!stack.isEmpty()) {
-            stringBuilder.append(stack.pollLast()).append(" ");
-        }
-        return stringBuilder.substring(0, stringBuilder.length() - 1).toString();
+      }
+      StringBuilder stringBuilder = new StringBuilder();
+      while (!stack.isEmpty()) {
+        stringBuilder.append(stack.pollLast()).append(" ");
+      }
+      return stringBuilder.substring(0, stringBuilder.length() - 1).toString();
     }
-
-    public static void main(String... args) {
-        /**This main program is to demo:
-         * a string that contains consecutive empty spaces when splitting by delimiter " ",
-         * it'll produce a an "" as an element.*/
-        String s = "a b  c";
-        String[] strs = s.split(" ");
-        CommonUtils.printArray_generic_type(strs);
-    }
+  }
 }
--- src/test/java/com/fishercoder/_151Test.java
+++ src/test/java/com/fishercoder/_151Test.java
@@ -7,30 +7,35 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class _151Test {
-    private static _151 test;
-    private static String s;
+  private static _151.Solution1 solution1;
+  private static String s;
 
-    @BeforeClass
-    public static void setup() {
-        test = new _151();
-    }
+  @BeforeClass
+  public static void setup() {
+    solution1 = new _151.Solution1();
+  }
 
-    @Test
-    public void test1() {
-        s = " ";
-        assertEquals("", test.reverseWords(s));
-    }
+  @Test
+  public void test1() {
+    s = " ";
+    assertEquals("", solution1.reverseWords(s));
+  }
 
-    @Test
-    public void test2() {
-        s = " 1";
-        assertEquals("1", test.reverseWords(s));
-    }
+  @Test
+  public void test2() {
+    s = " 1";
+    assertEquals("1", solution1.reverseWords(s));
+  }
 
-    @Test
-    public void test3() {
-        s = "   a   b ";
-        assertEquals("b a", test.reverseWords(s));
-    }
+  @Test
+  public void test3() {
+    s = "   a   b ";
+    assertEquals("b a", solution1.reverseWords(s));
+  }
 
+  @Test
+  public void test4() {
+    s = "a b  c";
+    assertEquals("c b a", solution1.reverseWords(s));
+  }
 }
