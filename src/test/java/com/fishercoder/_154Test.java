90d0a21b65144181b137c3cda43bbd8710f9251f
Steve Sun <fishercoder@gmail.com>
2018-05-14 07:28:58 -0700

    refactor 154

--- src/test/java/com/fishercoder/_154Test.java
+++ src/test/java/com/fishercoder/_154Test.java
@@ -6,21 +6,18 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by fishercoder on 5/25/17.
- */
 public class _154Test {
-    private static _154 test;
-    private static int[] nums;
+  private static _154.Solution1 solution1;
+  private static int[] nums;
 
-    @BeforeClass
-    public static void setup() {
-        test = new _154();
-    }
+  @BeforeClass
+  public static void setup() {
+    solution1 = new _154.Solution1();
+  }
 
-    @Test
-    public void test1() {
-        nums = new int[]{1, 1, 1};
-        assertEquals(1, test.findMin(nums));
-    }
+  @Test
+  public void test1() {
+    nums = new int[] {1, 1, 1};
+    assertEquals(1, solution1.findMin(nums));
+  }
 }
