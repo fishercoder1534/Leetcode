e5f578f113f367627ed560c104f2f177e508a2d2
Steve Sun <fishercoder@gmail.com>
2018-05-15 06:50:26 -0700

    refactor 155

--- src/main/java/com/fishercoder/solutions/_155.java
+++ src/main/java/com/fishercoder/solutions/_155.java
@@ -1,6 +1,5 @@
 package com.fishercoder.solutions;
 
-
 import java.util.Stack;
 
 /**
@@ -25,47 +24,48 @@ import java.util.Stack;
 
 public class _155 {
 
-    public static class MinStack {
-        private Stack<Integer> stack;
-        private int min;
+    public static class Solution1 {
+        class MinStack {
+            private Stack<Integer> stack;
+            private int min;
 
-        /**
-         * initialize your data structure here.
-         */
-        public MinStack() {
-            stack = new Stack();
-            min = Integer.MAX_VALUE;
-        }
-
-        public void push(int x) {
-            /**All the trick happens here, we push the second minimum number onto the stack before we push the newer one,
-             * this way, when popping, we could always get the next minimum one in constant time.*/
-            if (x <= min) {
-                stack.push(min);
-                min = x;
-            }
-            stack.push(x);
-        }
-
-        public void pop() {
-            if (min == stack.peek()) {
-                stack.pop();
-                min = stack.pop();
-            } else {
-                stack.pop();
-            }
-            if (stack.isEmpty()) {
+            /**
+             * initialize your data structure here.
+             */
+            public MinStack() {
+                stack = new Stack();
                 min = Integer.MAX_VALUE;
             }
-        }
 
-        public int top() {
-            return stack.peek();
-        }
+            public void push(int x) {
+                /**All the trick happens here, we push the second minimum number onto the stack before we push the newer one,
+                 * this way, when popping, we could always get the next minimum one in constant time.*/
+                if (x <= min) {
+                    stack.push(min);
+                    min = x;
+                }
+                stack.push(x);
+            }
 
-        public int getMin() {
-            return min;
+            public void pop() {
+                if (min == stack.peek()) {
+                    stack.pop();
+                    min = stack.pop();
+                } else {
+                    stack.pop();
+                }
+                if (stack.isEmpty()) {
+                    min = Integer.MAX_VALUE;
+                }
+            }
+
+            public int top() {
+                return stack.peek();
+            }
+
+            public int getMin() {
+                return min;
+            }
         }
     }
-
 }
