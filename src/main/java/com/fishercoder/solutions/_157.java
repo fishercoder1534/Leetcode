6f48f3487743e899ce7f82813b633b24cb80d990
Steve Sun <fishercoder@gmail.com>
2018-05-16 07:20:23 -0700

    refactor 157

--- src/main/java/com/fishercoder/solutions/_157.java
+++ src/main/java/com/fishercoder/solutions/_157.java
@@ -1,36 +1,46 @@
 package com.fishercoder.solutions;
 
 /**
+ * 157. Read N Characters Given Read4
+ *
  * The API: int read4(char *buf) reads 4 characters at a time from a file.
- * 
- * The return value is the actual number of characters read. For example, it returns 3 if there is
- * only 3 characters left in the file.
- * 
- * By using the read4 API, implement the function int read(char *buf, int n) that reads n characters
- * from the file.
- * 
- * Note: The read function will only be called once for each test case.
- */
-
-/**
- * The problem description is pretty ambiguous, actually the problem means to Keep reading until
- * either you have gotten n characters or there is no more characters to read.
+ *
+ * The return value is the actual number of characters read. For example, it returns 3 if there is only 3 characters left in the file.
+ *
+ * By using the read4 API, implement the function int read(char *buf, int n) that reads n characters from the file.
+ *
+ * Example 1:
+ *
+ * Input: buf = "abc", n = 4
+ * Output: "abc"
+ * Explanation: The actual number of characters read is 3, which is "abc".
+ *
+ * Example 2:
+ *
+ * Input: buf = "abcde", n = 5
+ * Output: "abcde"
+ *
+ * Note:
+ * The read function will only be called once for each test case.
+ *
  */
 public class _157 {
-    public int read(char[] buf, int n) {
-        int index = 0;
-        int next = 0;
-        char[] buffer = new char[4];
-        while (index < n && (next = read4(buffer)) != 0) {
-            for (int i = 0; i < next && index < n; index++, i++) {
-                buf[index] = buffer[i];
+    public static class Solution1 {
+        public int read(char[] buf, int n) {
+            int index = 0;
+            int next = 0;
+            char[] buffer = new char[4];
+            while (index < n && (next = read4(buffer)) != 0) {
+                for (int i = 0; i < next && index < n; index++, i++) {
+                    buf[index] = buffer[i];
+                }
             }
+            return index;
         }
-        return index;
-    }
 
-    private int read4(char[] buffer) {
-        //this is a fake method to make Eclipse happy
-        return 0;
+        private int read4(char[] buffer) {
+            //this is a fake method to make Eclipse happy
+            return 0;
+        }
     }
 }
