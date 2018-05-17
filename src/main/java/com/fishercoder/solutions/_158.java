f219e027d379cf0f1e05211964c64b22603bf641
Steve Sun <fishercoder@gmail.com>
2018-05-17 06:54:05 -0700

    refactor 158

--- src/main/java/com/fishercoder/solutions/_158.java
+++ src/main/java/com/fishercoder/solutions/_158.java
@@ -1,7 +1,8 @@
 package com.fishercoder.solutions;
 
 /**
- * The API: int read4(char *buf) reads 4 characters at a time from a file.
+ 158. Read N Characters Given Read4 II - Call multiple times
+ The API: int read4(char *buf) reads 4 characters at a time from a file.
 
  The return value is the actual number of characters read. For example, it returns 3 if there is only 3 characters left in the file.
 
@@ -9,40 +10,54 @@ package com.fishercoder.solutions;
 
  Note:
  The read function may be called multiple times.
+
+ Example 1:
+
+ Given buf = "abc"
+ read("abc", 1) // returns "a"
+ read("abc", 2); // returns "bc"
+ read("abc", 1); // returns ""
+
+ Example 2:
+
+ Given buf = "abc"
+ read("abc", 4) // returns "abc"
+ read("abc", 1); // returns ""
  */
 public class _158 {
 
-	/**
-	 * @param buf Destination buffer
-	 * @param n   Maximum number of characters to read
-	 * @return    The number of characters read
-	 */
-	private int buffPtr = 0;
-	private int buffCnt = 0;
-	private char[] buff = new char[4];
+  public static class Solution1 {
+    /**
+     * @param buf Destination buffer
+     * @param n   Maximum number of characters to read
+     * @return The number of characters read
+     */
+    private int buffPtr = 0;
+    private int buffCnt = 0;
+    private char[] buff = new char[4];
 
-	public int read(char[] buf, int n) {
-		int ptr = 0;
-		while (ptr < n) {
-			if (buffPtr == 0) {
-				buffCnt = read4(buff);
-			}
-			if (buffCnt == 0) {
-				break;
-			}
-			while (ptr < n && buffPtr < buffCnt) {
-				buf[ptr++] = buff[buffPtr++];
-			}
-			if (buffPtr >= buffCnt) {
-				buffPtr = 0;
-			}
-		}
-		return ptr;
-	}
+    public int read(char[] buf, int n) {
+      int ptr = 0;
+      while (ptr < n) {
+        if (buffPtr == 0) {
+          buffCnt = read4(buff);
+        }
+        if (buffCnt == 0) {
+          break;
+        }
+        while (ptr < n && buffPtr < buffCnt) {
+          buf[ptr++] = buff[buffPtr++];
+        }
+        if (buffPtr >= buffCnt) {
+          buffPtr = 0;
+        }
+      }
+      return ptr;
+    }
 
-	//This is a fake method to make IDE happy.
-	private int read4(char[] buff) {
-		return 1;
-	}
-
+    //This is a fake method to make IDE happy.
+    private int read4(char[] buff) {
+      return 1;
+    }
+  }
 }
