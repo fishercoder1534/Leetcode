d6836ad456d4853a5c194ce14673fa1069c001c4
Steve Sun <fishercoder@gmail.com>
2018-05-26 07:08:01 -0700

    refactor 167

--- src/main/java/com/fishercoder/solutions/_167.java
+++ src/main/java/com/fishercoder/solutions/_167.java
@@ -15,24 +15,24 @@ package com.fishercoder.solutions;
  */
 
 public class _167 {
-
-    public int[] twoSum(int[] numbers, int target) {
-        int left = 0;
-        int right = numbers.length - 1;
-        while (left < right) {
-            long sum = numbers[left] + numbers[right];
-            if (sum > target) {
-                right--;
-            } else if (sum < target) {
-                left++;
-            } else {
-                int[] res = new int[2];
-                res[0] = left + 1;
-                res[1] = right + 1;
-                return res;
+    public static class Solution1 {
+        public int[] twoSum(int[] numbers, int target) {
+            int left = 0;
+            int right = numbers.length - 1;
+            while (left < right) {
+                long sum = numbers[left] + numbers[right];
+                if (sum > target) {
+                    right--;
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    int[] res = new int[2];
+                    res[0] = left + 1;
+                    res[1] = right + 1;
+                    return res;
+                }
             }
+            return new int[] {-1, -1};
         }
-        return new int[]{-1, -1};
     }
-
 }
