9105d99486b20db1ce2f0c546d9aed0d9ee52d6c
Steve Sun <fishercoder@gmail.com>
2018-05-31 07:35:28 -0700

    refactor 171

--- src/main/java/com/fishercoder/solutions/_171.java
+++ src/main/java/com/fishercoder/solutions/_171.java
@@ -1,6 +1,9 @@
 package com.fishercoder.solutions;
 
-/**Given a column title as appear in an Excel sheet, return its corresponding column number.
+/**
+ * 170. Two Sum III - Data structure design
+ *
+ * Given a column title as appear in an Excel sheet, return its corresponding column number.
 
  For example:
 
@@ -11,16 +14,19 @@ package com.fishercoder.solutions;
  Z -> 26
  AA -> 27
  AB -> 28
+
  */
 public class _171 {
 
+  public static class Solution1 {
     public int titleToNumber(String s) {
-        char[] c = s.toCharArray();
-        int result = 0;
-        for (int i = s.length() - 1; i >= 0; i--) {
-            result += (c[i] - 64) * ((int) Math.pow(26, s.length() - i - 1));//The ASCII value of A is 65
-        }
-        return result;
+      char[] c = s.toCharArray();
+      int result = 0;
+      for (int i = s.length() - 1; i >= 0; i--) {
+        result +=
+            (c[i] - 64) * ((int) Math.pow(26, s.length() - i - 1));//The ASCII value of A is 65
+      }
+      return result;
     }
-
+  }
 }
--- src/test/java/com/fishercoder/_171Test.java
+++ src/test/java/com/fishercoder/_171Test.java
@@ -6,19 +6,16 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by fishercoder on 5/13/17.
- */
 public class _171Test {
-    private static _171 test;
+  private static _171.Solution1 solution1;
 
-    @BeforeClass
-    public static void setup() {
-        test = new _171();
-    }
+  @BeforeClass
+  public static void setup() {
+    solution1 = new _171.Solution1();
+  }
 
-    @Test
-    public void test1() {
-        assertEquals(28, test.titleToNumber("AB"));
-    }
+  @Test
+  public void test1() {
+    assertEquals(28, solution1.titleToNumber("AB"));
+  }
 }
