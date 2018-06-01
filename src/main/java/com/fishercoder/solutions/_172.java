8ecb7de1f0b9850fa103711d4f2ccc12836ba61c
Steve Sun <fishercoder@gmail.com>
2018-06-01 07:26:21 -0700

    refactor 172

--- src/main/java/com/fishercoder/solutions/_172.java
+++ src/main/java/com/fishercoder/solutions/_172.java
@@ -1,16 +1,34 @@
 package com.fishercoder.solutions;
-/**Given an integer n, return the number of trailing zeroes in n!.
+/**
+ * 172. Factorial Trailing Zeroes
 
- Note: Your solution should be in logarithmic time complexity.*/
+ Given an integer n, return the number of trailing zeroes in n!.
+
+ Example 1:
+
+ Input: 3
+ Output: 0
+ Explanation: 3! = 6, no trailing zero.
+
+ Example 2:
+
+ Input: 5
+ Output: 1
+ Explanation: 5! = 120, one trailing zero.
+
+ Note: Your solution should be in logarithmic time complexity.
+
+ */
 public class _172 {
 
+  public static class Solution1 {
     public int trailingZeroes(int n) {
-        int result = 0;
-        while (n > 4) {
-            n /= 5;
-            result += n;
-        }
-        return result;
+      int result = 0;
+      while (n > 4) {
+        n /= 5;
+        result += n;
+      }
+      return result;
     }
-
+  }
 }
