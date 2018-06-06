b314490d7878f27345e9b37ffff0bdd4165fc581
Steve Sun <fishercoder@gmail.com>
2018-06-06 07:27:09 -0700

    refactor 187

--- src/main/java/com/fishercoder/solutions/_187.java
+++ src/main/java/com/fishercoder/solutions/_187.java
@@ -20,20 +20,20 @@ import java.util.Map;
  ["AAAAACCCCC", "CCCCCAAAAA"].
  */
 public class _187 {
-
-    public List<String> findRepeatedDnaSequences(String s) {
-        Map<String, Integer> map = new HashMap();
-        for (int i = 0; i < s.length() - 9; i++) {
-            String sequence = s.substring(i, i + 10);
-            map.put(sequence, map.getOrDefault(sequence, 0) + 1);
-        }
-        List<String> repeatedSequences = new ArrayList<>();
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            if (entry.getValue() > 1) {
-                repeatedSequences.add(entry.getKey());
+    public static class Solution1 {
+        public List<String> findRepeatedDnaSequences(String s) {
+            Map<String, Integer> map = new HashMap();
+            for (int i = 0; i < s.length() - 9; i++) {
+                String sequence = s.substring(i, i + 10);
+                map.put(sequence, map.getOrDefault(sequence, 0) + 1);
             }
+            List<String> repeatedSequences = new ArrayList<>();
+            for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                if (entry.getValue() > 1) {
+                    repeatedSequences.add(entry.getKey());
+                }
+            }
+            return repeatedSequences;
         }
-        return repeatedSequences;
     }
-
 }
