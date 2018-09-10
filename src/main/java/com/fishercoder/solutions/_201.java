ecebead16756d58a1f9925388459e7a8d3d08d2e
Fisher Coder <fishercoder@gmail.com>
2018-09-10 07:50:43 -0700

    refactor 201 comment

--- src/main/java/com/fishercoder/solutions/_201.java
+++ src/main/java/com/fishercoder/solutions/_201.java
@@ -10,10 +10,12 @@ package com.fishercoder.solutions;
 public class _201 {
 
     public static class Solution1 {
-        //credit: https://discuss.leetcode.com/topic/28538/java-python-easy-solution-with-explanation
-        //Bitwise AND operation within range actually turns out to be doing some operations with just these two boundary numbers: m and n
-        //e.g. 5 and 7, in binary, they are 101 and 111, the result for [5,7] is 5&6&7 which is 101&110&111
-        //this we can understand it to be shifting the digits of m and n from left to right until they become the same, then we pad that number with zeroes on the right side
+        /**
+         * credit: https://discuss.leetcode.com/topic/28538/java-python-easy-solution-with-explanation
+         * Bitwise AND operation within range actually turns out to be doing some operations with just these two boundary numbers: m and n
+         * e.g. 5 and 7, in binary, they are 101 and 111, the result for [5,7] is 5&6&7 which is 101&110&111
+         * this we can understand it to be shifting the digits of m and n from left to right until they become the same, then we pad that number with zeroes on the right side
+         */
         public int rangeBitwiseAnd(int m, int n) {
             int i = 0;
             while (m != n) {
