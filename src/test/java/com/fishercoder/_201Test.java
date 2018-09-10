03cc97e94c61948a6b9ddf315933e917e5339880
Fisher Coder <fishercoder@gmail.com>
2018-09-10 07:50:09 -0700

    refactor 201

--- src/test/java/com/fishercoder/_201Test.java
+++ src/test/java/com/fishercoder/_201Test.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.assertEquals;
  * Created by fishercoder on 5/3/17.
  */
 public class _201Test {
-    private static _201 test;
+    private static _201.Solution1 solution1;
     private static int m;
     private static int n;
     private static int actual;
@@ -18,17 +18,17 @@ public class _201Test {
 
     @BeforeClass
     public static void setup() {
-        test = new _201();
+        solution1 = new _201.Solution1();
     }
 
     @Test
     public void test1() {
         m = 5;
         n = 7;
-        actual = test.rangeBitwiseAnd_TLE(m, n);
+        actual = solution1.rangeBitwiseAnd(m, n);
         expected = 4;
         assertEquals(expected, actual);
-        actual = test.rangeBitwiseAnd(m, n);
+        actual = solution1.rangeBitwiseAnd(m, n);
         assertEquals(expected, actual);
     }
 
@@ -36,10 +36,10 @@ public class _201Test {
     public void test2() {
         m = 1;
         n = 2;
-        actual = test.rangeBitwiseAnd_TLE(m, n);
+        actual = solution1.rangeBitwiseAnd(m, n);
         expected = 0;
         assertEquals(expected, actual);
-        actual = test.rangeBitwiseAnd(m, n);
+        actual = solution1.rangeBitwiseAnd(m, n);
         assertEquals(expected, actual);
     }
 
@@ -47,12 +47,10 @@ public class _201Test {
     public void test3() {
         m = 0;
         n = 2147483647;
-        long start = System.currentTimeMillis();
-        actual = test.rangeBitwiseAnd_TLE(m, n);
-        System.out.println("It took " + (System.currentTimeMillis() - start) + " ms to finish.");
+        actual = solution1.rangeBitwiseAnd(m, n);
         expected = 0;
         assertEquals(expected, actual);
-        actual = test.rangeBitwiseAnd(m, n);
+        actual = solution1.rangeBitwiseAnd(m, n);
         assertEquals(expected, actual);
     }
 
@@ -60,12 +58,10 @@ public class _201Test {
     public void test4() {
         m = 20000;
         n = 2147483647;
-        long start = System.currentTimeMillis();
-//        actual = test.rangeBitwiseAnd_TLE(m, n);
-        System.out.println("It took " + (System.currentTimeMillis() - start) + " ms to finish.");
+        actual = solution1.rangeBitwiseAnd(m, n);
         expected = 0;
         assertEquals(expected, actual);
-        actual = test.rangeBitwiseAnd(m, n);
+        actual = solution1.rangeBitwiseAnd(m, n);
         assertEquals(expected, actual);
     }
 }
