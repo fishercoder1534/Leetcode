4a6f7cc6ab6535bed6baa21e8c060e7a77d63a9d
Fisher Coder <fishercoder@gmail.com>
2018-09-12 07:48:36 -0700

    refactor 203

--- src/test/java/com/fishercoder/_203Test.java
+++ src/test/java/com/fishercoder/_203Test.java
@@ -9,20 +9,20 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class _203Test {
-    private static _203 test;
+    private static _203.Solution1 solution1;
     private static ListNode head;
     private static ListNode expected;
 
     @BeforeClass
     public static void setup() {
-        test = new _203();
+        solution1 = new _203.Solution1();
     }
 
     @Test
     public void test1() {
         head = LinkedListUtils.contructLinkedList(new int[]{1, 2, 6, 3, 4, 5, 6});
         expected = LinkedListUtils.contructLinkedList(new int[]{1, 2, 3, 4, 5});
-        assertEquals(expected, test.removeElements(head, 6));
+        assertEquals(expected, solution1.removeElements(head, 6));
     }
 
 }
