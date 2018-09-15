8ea27548b2da7fd5cef0d62190b8b8e7bb7f8afb
Fisher Coder <fishercoder@gmail.com>
2018-09-15 07:41:34 -0700

    refactor 206

--- src/test/java/com/fishercoder/_206Test.java
+++ src/test/java/com/fishercoder/_206Test.java
@@ -8,9 +8,6 @@ import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
 
-/**
- * Created by stevesun on 6/5/17.
- */
 public class _206Test {
     private static _206.Solution1 solution1;
     private static _206.Solution2 solution2;
@@ -24,10 +21,10 @@ public class _206Test {
 
     @Test
     public void test1() {
-        head = LinkedListUtils.contructLinkedList(new int[]{1,2,3});
-        assertEquals(LinkedListUtils.contructLinkedList(new int[]{3,2,1}), solution1.reverseList(head));
+        head = LinkedListUtils.contructLinkedList(new int[]{1, 2, 3});
+        assertEquals(LinkedListUtils.contructLinkedList(new int[]{3, 2, 1}), solution1.reverseList(head));
 
-        head = LinkedListUtils.contructLinkedList(new int[]{1,2,3});
-        assertEquals(LinkedListUtils.contructLinkedList(new int[]{3,2,1}), solution2.reverseList(head));
+        head = LinkedListUtils.contructLinkedList(new int[]{1, 2, 3});
+        assertEquals(LinkedListUtils.contructLinkedList(new int[]{3, 2, 1}), solution2.reverseList(head));
     }
 }
