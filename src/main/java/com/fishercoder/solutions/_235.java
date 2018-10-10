a08a9fd696ae495563add349af6cf880a8feb157
Fisher Coder <fishercoder@gmail.com>
2018-10-10 07:21:16 -0700

    refactor 235

--- src/main/java/com/fishercoder/solutions/_235.java
+++ src/main/java/com/fishercoder/solutions/_235.java
@@ -24,16 +24,18 @@ import com.fishercoder.common.classes.TreeNode;
  */
 public class _235 {
 
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null || p == root || q == root) {
+    public static class Solution1 {
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            if (root == null || p == root || q == root) {
+                return root;
+            }
+            if ((root.val - p.val) * (root.val - q.val) > 0) {
+                if (root.val - p.val > 0) {
+                    return lowestCommonAncestor(root.left, p, q);
+                }
+                return lowestCommonAncestor(root.right, p, q);
+            }
             return root;
         }
-        if ((root.val - p.val) * (root.val - q.val) > 0) {
-            if (root.val - p.val > 0) {
-                return lowestCommonAncestor(root.left, p, q);
-            }
-            return lowestCommonAncestor(root.right, p, q);
-        }
-        return root;
     }
 }
