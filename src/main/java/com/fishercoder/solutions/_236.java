730adee3efe470a6f8c90ee5a0b894f2c6491e7c
Fisher Coder <fishercoder@gmail.com>
2018-10-11 06:33:34 -0700

    refactor 236

--- src/main/java/com/fishercoder/solutions/_236.java
+++ src/main/java/com/fishercoder/solutions/_236.java
@@ -22,23 +22,27 @@ import com.fishercoder.common.classes.TreeNode;
  Another example is LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.*/
 
 public class _236 {
+    public static class Solution1 {
 
-    /**We need to find TWO nodes in the tree,
-     * so we'll have to divide and conquer this tree,
-     * we need to have two nodes to as the intermediate result,
-     * also, refer to my earlier drawings:http://www.fishercoder.com/2016/06/23/lowest-common-ancestor-of-a-binary-tree/
-     * I'm really impressed with myself at that time!*/
+        /**
+         * We need to find TWO nodes in the tree,
+         * so we'll have to divide and conquer this tree,
+         * we need to have two nodes to as the intermediate result,
+         * also, refer to my earlier drawings:http://www.fishercoder.com/2016/06/23/lowest-common-ancestor-of-a-binary-tree/
+         * I'm really impressed with myself at that time!
+         */
 
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null || root == p || root == q) {
-            return root;
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            if (root == null || root == p || root == q) {
+                return root;
+            }
+            TreeNode left = lowestCommonAncestor(root.left, p, q);
+            TreeNode right = lowestCommonAncestor(root.right, p, q);
+            if (left != null && right != null) {
+                return root;
+            }
+            return left != null ? left : right;
         }
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
-        if (left != null && right != null) {
-            return root;
-        }
-        return left != null ? left : right;
     }
 
 }
