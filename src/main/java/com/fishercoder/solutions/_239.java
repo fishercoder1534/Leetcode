f5c89a01b836e24f5a604e6048e41998b6971730
Fisher Coder <fishercoder@gmail.com>
2018-10-13 08:11:57 -0700

    refactor 239

--- src/main/java/com/fishercoder/solutions/_239.java
+++ src/main/java/com/fishercoder/solutions/_239.java
@@ -35,24 +35,26 @@ import java.util.PriorityQueue;
  */
 public class _239 {
 
-    public int[] maxSlidingWindow(int[] nums, int k) {
-        if (nums == null || nums.length == 0 || k == 0) {
-            return new int[0];
-        }
-        PriorityQueue<Integer> heap = new PriorityQueue<>((a, b) -> b - a);
-        int[] res = new int[nums.length - k + 1];
-        for (int i = 0; i < nums.length; i++) {
-            if (i < k) {
-                heap.offer(nums[i]);
-                if (i == k - 1) {
-                    res[0] = heap.peek();
-                }
-            } else {
-                heap.remove(nums[i - k]);
-                heap.offer(nums[i]);
-                res[i - k + 1] = heap.peek();
+    public static class Solution1 {
+        public int[] maxSlidingWindow(int[] nums, int k) {
+            if (nums == null || nums.length == 0 || k == 0) {
+                return new int[0];
             }
+            PriorityQueue<Integer> heap = new PriorityQueue<>((a, b) -> b - a);
+            int[] res = new int[nums.length - k + 1];
+            for (int i = 0; i < nums.length; i++) {
+                if (i < k) {
+                    heap.offer(nums[i]);
+                    if (i == k - 1) {
+                        res[0] = heap.peek();
+                    }
+                } else {
+                    heap.remove(nums[i - k]);
+                    heap.offer(nums[i]);
+                    res[i - k + 1] = heap.peek();
+                }
+            }
+            return res;
         }
-        return res;
     }
 }
