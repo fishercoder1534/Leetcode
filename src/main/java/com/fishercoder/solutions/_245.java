8335262fef79bb40ce251ebe6d629835ff98a789
Fisher Coder <fishercoder@gmail.com>
2018-10-20 09:01:36 -0700

    refactor 245

--- src/main/java/com/fishercoder/solutions/_245.java
+++ src/main/java/com/fishercoder/solutions/_245.java
@@ -1,11 +1,11 @@
 package com.fishercoder.solutions;
 
 /**
+ * 245. Shortest Word Distance III
+ *
  * This is a follow up of Shortest Word Distance. The only difference is now word1 could be the same as word2.
-
- Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
-
- word1 and word2 may be the same and they represent two individual words in the list.
+ * Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
+ * word1 and word2 may be the same and they represent two individual words in the list.
 
  For example,
  Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
@@ -18,31 +18,32 @@ package com.fishercoder.solutions;
  */
 public class _245 {
 
-    public int shortestWordDistance(String[] words, String word1, String word2) {
-        int p1 = -1;
-        int p2 = -1;
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < words.length; i++) {
-            if (words[i].equals(word1)) {
-                if (word1.equals(word2)) {
-                    if (p1 != -1 && i - p1 < min) {
-                        min = i - p1;
+    public static class Solution1 {
+        public int shortestWordDistance(String[] words, String word1, String word2) {
+            int p1 = -1;
+            int p2 = -1;
+            int min = Integer.MAX_VALUE;
+            for (int i = 0; i < words.length; i++) {
+                if (words[i].equals(word1)) {
+                    if (word1.equals(word2)) {
+                        if (p1 != -1 && i - p1 < min) {
+                            min = i - p1;
+                        }
+                        p1 = i;
+                    } else {
+                        p1 = i;
+                        if (p2 != -1 && p1 - p2 < min) {
+                            min = p1 - p2;
+                        }
                     }
-                    p1 = i;
-                } else {
-                    p1 = i;
-                    if (p2 != -1 && p1 - p2 < min) {
-                        min = p1 - p2;
+                } else if (words[i].equals(word2)) {
+                    p2 = i;
+                    if (p1 != -1 && p2 - p1 < min) {
+                        min = p2 - p1;
                     }
-                }
-            } else if (words[i].equals(word2)) {
-                p2 = i;
-                if (p1 != -1 && p2 - p1 < min) {
-                    min = p2 - p1;
                 }
             }
+            return min;
         }
-        return min;
     }
-
 }
