9b985cbdfb3eebe3bf3df9d67971fc2ec7c9a5db
Fisher Coder <fishercoder@gmail.com>
2018-10-20 17:16:05 -0700

    add 709

---
+++ src/main/java/com/fishercoder/solutions/_709.java
@@ -0,0 +1,25 @@
+package com.fishercoder.solutions;
+
+/**709. To Lower Case
+ *
+ * Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+ *
+ *  Example 1:
+ *
+ * Input: "Hello"
+ * Output: "hello"
+ * Example 2:
+ *
+ * Input: "here"
+ * Output: "here"
+ * Example 3:
+ *
+ * Input: "LOVELY"
+ * Output: "lovely"*/
+public class _709 {
+    public static class Solution1 {
+        public String toLowerCase(String str) {
+            return str.toLowerCase();
+        }
+    }
+}
