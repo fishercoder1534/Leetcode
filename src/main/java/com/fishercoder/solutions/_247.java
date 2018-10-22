356e4482b1a2d1988e52050eb9481539c040a28f
Fisher Coder <fishercoder@gmail.com>
2018-10-22 07:59:57 -0700

    refactor 247

--- src/main/java/com/fishercoder/solutions/_247.java
+++ src/main/java/com/fishercoder/solutions/_247.java
@@ -4,44 +4,34 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-/**
- * A strobogrammatic number is a number that looks the same when rotated 180 degrees (looked at upside down).
-
- Find all strobogrammatic numbers that are of length = n.
-
- For example,
- Given n = 2, return ["11","69","88","96"].
-
- Hint:
-
- Try to use recursion and notice that it should recurse with n - 2 instead of n - 1.
- */
 public class _247 {
-    public List<String> findStrobogrammatic(int n) {
-        return recursiveHelper(n, n);
-    }
-
-    private List<String> recursiveHelper(int n, int m) {
-        if (n == 0) {
-            return new ArrayList<>(Arrays.asList(""));
-        }
-        if (n == 1) {
-            return new ArrayList<>(Arrays.asList("0", "1", "8"));
+    public static class Solution1 {
+        public List<String> findStrobogrammatic(int n) {
+            return recursiveHelper(n, n);
         }
 
-        List<String> list = recursiveHelper(n - 2, m);
-        List<String> res = new ArrayList<String>();
-
-        for (int i = 0; i < list.size(); i++) {
-            String s = list.get(i);
-            if (n != m) {
-                res.add("0" + s + "0");
+        private List<String> recursiveHelper(int n, int m) {
+            if (n == 0) {
+                return new ArrayList<>(Arrays.asList(""));
             }
-            res.add("1" + s + "1");
-            res.add("6" + s + "9");
-            res.add("8" + s + "8");
-            res.add("9" + s + "6");
+            if (n == 1) {
+                return new ArrayList<>(Arrays.asList("0", "1", "8"));
+            }
+
+            List<String> list = recursiveHelper(n - 2, m);
+            List<String> res = new ArrayList<>();
+
+            for (int i = 0; i < list.size(); i++) {
+                String s = list.get(i);
+                if (n != m) {
+                    res.add("0" + s + "0");
+                }
+                res.add("1" + s + "1");
+                res.add("6" + s + "9");
+                res.add("8" + s + "8");
+                res.add("9" + s + "6");
+            }
+            return res;
         }
-        return res;
     }
 }
---
+++ src/test/java/com/fishercoder/_247Test.java
@@ -0,0 +1,26 @@
+package com.fishercoder;
+
+import com.fishercoder.solutions._247;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class _247Test {
+  private static _247.Solution1 solution1;
+  private static List<String> expected;
+
+  @BeforeClass
+  public static void setup() {
+    solution1 = new _247.Solution1();
+  }
+
+  @Test
+  public void test1() {
+    expected = List.of("11","69","88","96");
+    assertEquals(expected, solution1.findStrobogrammatic(2));
+  }
+
+}
