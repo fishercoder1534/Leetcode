bafccafff0670f9bc508164df57197d6dbae0b5b
Fisher Coder <fishercoder@gmail.com>
2018-10-26 07:19:17 -0700

    refactor 250

--- src/main/java/com/fishercoder/solutions/_250.java
+++ src/main/java/com/fishercoder/solutions/_250.java
@@ -2,45 +2,32 @@ package com.fishercoder.solutions;
 
 import com.fishercoder.common.classes.TreeNode;
 
-/**Given a binary tree, count the number of uni-value subtrees.
-
- A Uni-value subtree means all nodes of the subtree have the same value.
-
- For example:
- Given binary tree,
-     5
-    / \
-   1   5
-  / \   \
- 5  5   5
- return 4.
-
- */
 public class _250 {
 
-    public int countUnivalSubtrees(TreeNode root) {
-        int[] count = new int[1];
-        helper(root, count);
-        return count[0];
-    }
-
-    private boolean helper(TreeNode node, int[] count) {
-        if (node == null) {
-            return true;
+    public static class Solution1 {
+        public int countUnivalSubtrees(TreeNode root) {
+            int[] count = new int[1];
+            helper(root, count);
+            return count[0];
         }
-        boolean left = helper(node.left, count);
-        boolean right = helper(node.right, count);
-        if (left && right) {
-            if (node.left != null && node.val != node.left.val) {
-                return false;
-            }
-            if (node.right != null && node.val != node.right.val) {
-                return false;
-            }
-            count[0]++;
-            return true;
-        }
-        return false;
-    }
 
+        private boolean helper(TreeNode node, int[] count) {
+            if (node == null) {
+                return true;
+            }
+            boolean left = helper(node.left, count);
+            boolean right = helper(node.right, count);
+            if (left && right) {
+                if (node.left != null && node.val != node.left.val) {
+                    return false;
+                }
+                if (node.right != null && node.val != node.right.val) {
+                    return false;
+                }
+                count[0]++;
+                return true;
+            }
+            return false;
+        }
+    }
 }
