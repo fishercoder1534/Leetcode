53ac3365f2543ff63e61c1a1499aa51a9339dae7
Fisher Coder <fishercoder@gmail.com>
2018-11-04 07:23:08 -0800

    refactor 258

--- src/main/java/com/fishercoder/solutions/_258.java
+++ src/main/java/com/fishercoder/solutions/_258.java
@@ -1,34 +1,17 @@
 package com.fishercoder.solutions;
 
-/**
- * Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
-
- For example:
-
- Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
-
- Follow up:
- Could you do it without any loop/recursion in O(1) runtime?
-
- Hint:
-
- A naive implementation of the above process is trivial. Could you come up with other methods?
- What are all the possible results?
- How do they occur, periodically or randomly?
- You may find this Wikipedia article: https://en.wikipedia.org/wiki/Digital_root useful.
- * */
-
 public class _258 {
 
-    //only three cases as the code shows
-    public int addDigits(int num) {
-        if (num == 0) {
-            return 0;
+    public static class Solution1 {
+        //only three cases as the code shows
+        public int addDigits(int num) {
+            if (num == 0) {
+                return 0;
+            }
+            if (num % 9 == 0) {
+                return 9;
+            }
+            return num % 9;
         }
-        if (num % 9 == 0) {
-            return 9;
-        }
-        return num % 9;
     }
-
 }
