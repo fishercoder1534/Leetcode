29e52e620de77b443f0561a0d71564c48daf898e
Fisher Coder <fishercoder@gmail.com>
2018-11-13 07:41:04 -0800

    refactor 269

---
+++ src/test/java/com/fishercoder/_269Test.java
@@ -0,0 +1,24 @@
+package com.fishercoder;
+
+import com.fishercoder.solutions._269;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class _269Test {
+    private static _269.Solution1 solution1;
+    private static String[] words;
+
+    @BeforeClass
+    public static void setup() {
+        solution1 = new _269.Solution1();
+    }
+
+    @Test
+    public void test1() {
+        words = new String[]{"wrt", "wrf", "er", "ett", "rftt"};
+        assertEquals("wertf", solution1.alienOrder(words));
+    }
+
+}
