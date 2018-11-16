815908bfda9ecbc408932dbe7310c80d5687f1ec
Fisher Coder <fishercoder@gmail.com>
2018-11-16 08:05:10 -0800

    refactor 272

--- src/main/java/com/fishercoder/solutions/_272.java
+++ src/main/java/com/fishercoder/solutions/_272.java
@@ -7,6 +7,8 @@ import java.util.List;
 import java.util.Stack;
 
 /**
+ * 272. Closest Binary Search Tree Value II
+ *
  * Given a non-empty binary search tree and a target value, find k values in the BST that are closest to the target.
 
  Note:
@@ -28,44 +30,45 @@ import java.util.Stack;
  */
 public class _272 {
 
-	public List<Integer> closestKValues(TreeNode root, double target, int k) {
-		List<Integer> res = new ArrayList();
+    public static class Solution1 {
+        public List<Integer> closestKValues(TreeNode root, double target, int k) {
+            List<Integer> res = new ArrayList();
 
-		Stack<Integer> s1 = new Stack(); // predecessors
-		Stack<Integer> s2 = new Stack(); // successors
+            Stack<Integer> s1 = new Stack(); // predecessors
+            Stack<Integer> s2 = new Stack(); // successors
 
-		inorder(root, target, false, s1);
-		inorder(root, target, true, s2);
+            inorder(root, target, false, s1);
+            inorder(root, target, true, s2);
 
-		while (k-- > 0) {
-			if (s1.isEmpty()) {
-				res.add(s2.pop());
-			} else if (s2.isEmpty()) {
-				res.add(s1.pop());
-			} else if (Math.abs(s1.peek() - target) < Math.abs(s2.peek() - target)) {
-				res.add(s1.pop());
-			} else {
-				res.add(s2.pop());
-			}
-		}
+            while (k-- > 0) {
+                if (s1.isEmpty()) {
+                    res.add(s2.pop());
+                } else if (s2.isEmpty()) {
+                    res.add(s1.pop());
+                } else if (Math.abs(s1.peek() - target) < Math.abs(s2.peek() - target)) {
+                    res.add(s1.pop());
+                } else {
+                    res.add(s2.pop());
+                }
+            }
 
-		return res;
-	}
+            return res;
+        }
 
-	// inorder traversal
-	void inorder(TreeNode root, double target, boolean reverse, Stack<Integer> stack) {
-		if (root == null) {
-			return;
-		}
+        // inorder traversal
+        void inorder(TreeNode root, double target, boolean reverse, Stack<Integer> stack) {
+            if (root == null) {
+                return;
+            }
 
-		inorder(reverse ? root.right : root.left, target, reverse, stack);
-		// early terminate, no need to traverse the whole tree
-		if ((reverse && root.val <= target) || (!reverse && root.val > target)) {
-			return;
-		}
-		// track the value of current node
-		stack.push(root.val);
-		inorder(reverse ? root.left : root.right, target, reverse, stack);
-	}
-
+            inorder(reverse ? root.right : root.left, target, reverse, stack);
+            // early terminate, no need to traverse the whole tree
+            if ((reverse && root.val <= target) || (!reverse && root.val > target)) {
+                return;
+            }
+            // track the value of current node
+            stack.push(root.val);
+            inorder(reverse ? root.left : root.right, target, reverse, stack);
+        }
+    }
 }
