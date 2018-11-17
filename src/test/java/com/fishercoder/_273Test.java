ec473e60668a6246fcde5d245d052e4e7e16778d
Fisher Coder <fishercoder@gmail.com>
2018-11-17 08:30:43 -0800

    refactor 273

--- src/test/java/com/fishercoder/_273Test.java
+++ src/test/java/com/fishercoder/_273Test.java
@@ -6,33 +6,30 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by fishercoder on 5/13/17.
- */
 public class _273Test {
-    private static _273 test;
+    private static _273.Solution1 solution1;
     private static int num;
 
     @BeforeClass
     public static void setup() {
-        test = new _273();
+        solution1 = new _273.Solution1();
     }
 
     @Test
     public void test1() {
         num = 123;
-        assertEquals("One Hundred Twenty Three", test.numberToWords(num));
+        assertEquals("One Hundred Twenty Three", solution1.numberToWords(num));
     }
 
     @Test
     public void test2() {
         num = 12345;
-        assertEquals("Twelve Thousand Three Hundred Forty Five", test.numberToWords(num));
+        assertEquals("Twelve Thousand Three Hundred Forty Five", solution1.numberToWords(num));
     }
 
     @Test
     public void test3() {
         num = 1234567;
-        assertEquals("One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven", test.numberToWords(num));
+        assertEquals("One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven", solution1.numberToWords(num));
     }
 }
