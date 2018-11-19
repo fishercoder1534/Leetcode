ff793c66c8bf2c09bd451c71903e4490c2f2f791
Fisher Coder <fishercoder@gmail.com>
2018-11-19 08:18:59 -0800

    refactor 275

--- src/main/java/com/fishercoder/solutions/_275.java
+++ src/main/java/com/fishercoder/solutions/_275.java
@@ -2,23 +2,25 @@ package com.fishercoder.solutions;
 
 /**
  * 275. H-Index II
- * Follow up for H-Index: What if the citations array is sorted in ascending order? Could you optimize your algorithm?
+ *
+ * Follow up for H-Index: What if the citations array is sorted in ascending order?
+ * Could you optimize your algorithm?
  */
 public class _275 {
-
-    public int hIndex(int[] citations) {
-        int left = 0;
-        int len = citations.length;
-        int right = len - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (citations[mid] >= (len - mid)) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
+    public static class Solution1 {
+        public int hIndex(int[] citations) {
+            int left = 0;
+            int len = citations.length;
+            int right = len - 1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (citations[mid] >= (len - mid)) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
             }
+            return len - left;
         }
-        return len - left;
     }
-
 }
