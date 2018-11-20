f31ac9b4ef7e83fef5ea7c423a5aa22032a05732
Fisher Coder <fishercoder@gmail.com>
2018-11-20 07:50:37 -0800

    refactor 276

--- src/main/java/com/fishercoder/solutions/_276.java
+++ src/main/java/com/fishercoder/solutions/_276.java
@@ -1,6 +1,9 @@
 package com.fishercoder.solutions;
 
-/**There is a fence with n posts, each post can be painted with one of the k colors.
+/**
+ * 276. Paint Fence
+
+ There is a fence with n posts, each post can be painted with one of the k colors.
 
  You have to paint all the posts such that no more than two adjacent fence posts have the same color.
 
@@ -10,19 +13,21 @@ package com.fishercoder.solutions;
  n and k are non-negative integers.*/
 
 public class _276 {
-    public int numWays(int n, int k) {
-        if (n == 0) {
-            return 0;
-        } else if (n == 1) {
-            return k;
+    public static class Solution1 {
+        public int numWays(int n, int k) {
+            if (n == 0) {
+                return 0;
+            } else if (n == 1) {
+                return k;
+            }
+            int sameColorCnt = k;
+            int diffColorCnt = k * (k - 1);
+            for (int i = 2; i < n; i++) {
+                int temp = diffColorCnt;
+                diffColorCnt = (diffColorCnt + sameColorCnt) * (k - 1);
+                sameColorCnt = temp;
+            }
+            return sameColorCnt + diffColorCnt;
         }
-        int sameColorCnt = k;
-        int diffColorCnt = k * (k - 1);
-        for (int i = 2; i < n; i++) {
-            int temp = diffColorCnt;
-            diffColorCnt = (diffColorCnt + sameColorCnt) * (k - 1);
-            sameColorCnt = temp;
-        }
-        return sameColorCnt + diffColorCnt;
     }
 }
