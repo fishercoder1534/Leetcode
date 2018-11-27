d24e60b2cef67e8ca364de8f3e6b9750fdaa03ba
Fisher Coder <fishercoder@gmail.com>
2018-11-27 08:36:03 -0800

    refactor 286

--- src/main/java/com/fishercoder/solutions/_286.java
+++ src/main/java/com/fishercoder/solutions/_286.java
@@ -25,7 +25,7 @@ import java.util.Queue;
 
  */
 public class _286 {
-    class BFSSolutionWithoutQueue {
+    public static class Solution1 {
 
         int[] dirs = new int[]{0, 1, 0, -1, 0};
 
@@ -57,7 +57,7 @@ public class _286 {
 
     }
 
-    class BFSSolutionWithAQueue {
+    public static class Solution2 {
 
         //push all gates into the queue first, and then put all its neighbours into the queue with one distance to the gate, then continue to push the rest of the nodes into the queue, and put all their neighbours into the queue with the nodes' value plus one until the queue is empty
         int[] dirs = new int[]{0, 1, 0, -1, 0};
