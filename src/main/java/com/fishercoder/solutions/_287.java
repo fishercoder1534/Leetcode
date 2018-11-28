3297cdcd8e58cbf39c6cf68a36f0007b2dde3cf7
Fisher Coder <fishercoder@gmail.com>
2018-11-28 07:28:25 -0800

    refactor 287

--- src/main/java/com/fishercoder/solutions/_287.java
+++ src/main/java/com/fishercoder/solutions/_287.java
@@ -4,9 +4,11 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
+ * 287. Find the Duplicate Number
+ *
  * Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive),
  * prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
-
+ *
  Note:
  You must not modify the array (assume the array is read only).
  You must use only constant, O(1) extra space.
@@ -16,20 +18,23 @@ import java.util.Set;
  */
 public class _287 {
 
-    //no-brainer, used O(n) space
-    public int findDuplicate(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        int dup = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (!set.add(nums[i])) {
-                dup = nums[i];
-                break;
+    public static class Solution1 {
+        /**no-brainer, used O(n) space*/
+        public int findDuplicate(int[] nums) {
+            Set<Integer> set = new HashSet<>();
+            int dup = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (!set.add(nums[i])) {
+                    dup = nums[i];
+                    break;
+                }
             }
+            return dup;
         }
-        return dup;
     }
 
-    class SolutionO1 {
+    public static class Solution2 {
+        /** O(1) space */
         public int findDuplicate(int[] nums) {
             int slow = 0;
             int fast = 0;
