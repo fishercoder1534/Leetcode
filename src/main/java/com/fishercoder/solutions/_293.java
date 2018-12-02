dcafb850f86ee80cb2ba16eacac57b4bfdb88947
Fisher Coder <fishercoder@gmail.com>
2018-12-02 07:18:44 -0800

    refactor 293

--- src/main/java/com/fishercoder/solutions/_293.java
+++ src/main/java/com/fishercoder/solutions/_293.java
@@ -19,14 +19,15 @@ import java.util.List;
  If there is no valid move, return an empty list [].*/
 public class _293 {
 
-    public List<String> generatePossibleNextMoves(String s) {
-        List<String> result = new ArrayList<String>();
-        for (int i = 1; i < s.length(); i++) {
-            if (s.charAt(i) == '+' && s.charAt(i - 1) == '+') {
-                result.add(s.substring(0, i - 1) + "--" + s.substring(i + 1));
+    public static class Solutoin1 {
+        public List<String> generatePossibleNextMoves(String s) {
+            List<String> result = new ArrayList<>();
+            for (int i = 1; i < s.length(); i++) {
+                if (s.charAt(i) == '+' && s.charAt(i - 1) == '+') {
+                    result.add(s.substring(0, i - 1) + "--" + s.substring(i + 1));
+                }
             }
+            return result;
         }
-        return result;
     }
-
 }
