8c59f175c6e65858bafe4d527b0020eecc103683
Fisher Coder <fishercoder@gmail.com>
2018-12-04 07:21:12 -0800

    refactor 296

--- src/main/java/com/fishercoder/solutions/_296.java
+++ src/main/java/com/fishercoder/solutions/_296.java
@@ -5,7 +5,11 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * A group of two or more people wants to meet and minimize the total travel distance. You are given a 2D grid of values 0 or 1, where each 1 marks the home of someone in the group. The distance is calculated using Manhattan Distance, where distance(p1, p2) = |p2.x - p1.x| + |p2.y - p1.y|.
+ * 296: Best Meeting Point
+ *
+ * A group of two or more people wants to meet and minimize the total travel distance.
+ * You are given a 2D grid of values 0 or 1, where each 1 marks the home of someone in the group.
+ * The distance is calculated using Manhattan Distance, where distance(p1, p2) = |p2.x - p1.x| + |p2.y - p1.y|.
 
  For example, given three people living at (0,0), (0,4), and (2,2):
 
@@ -17,36 +21,38 @@ import java.util.List;
  The point (0,2) is an ideal meeting point, as the total travel distance of 2+2+2=6 is minimal. So return 6.
  */
 public class _296 {
-	public int minTotalDistance(int[][] grid) {
-		int m = grid.length;
-		int n = grid[0].length;
+	public static class Solution1 {
+		public int minTotalDistance(int[][] grid) {
+			int m = grid.length;
+			int n = grid[0].length;
 
-		List<Integer> I = new ArrayList(m);
-		List<Integer> J = new ArrayList(n);
+			List<Integer> I = new ArrayList(m);
+			List<Integer> J = new ArrayList(n);
 
-		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < n; j++) {
-				if (grid[i][j] == 1) {
-					I.add(i);
-					J.add(j);
+			for (int i = 0; i < m; i++) {
+				for (int j = 0; j < n; j++) {
+					if (grid[i][j] == 1) {
+						I.add(i);
+						J.add(j);
+					}
 				}
 			}
+
+			return getMin(I) + getMin(J);
 		}
 
-		return getMin(I) + getMin(J);
-	}
+		private int getMin(List<Integer> list) {
+			int ret = 0;
 
-	private int getMin(List<Integer> list) {
-		int ret = 0;
+			Collections.sort(list);
 
-		Collections.sort(list);
+			int i = 0;
+			int j = list.size() - 1;
+			while (i < j) {
+				ret += list.get(j--) - list.get(i++);
+			}
 
-		int i = 0;
-		int j = list.size() - 1;
-		while (i < j) {
-			ret += list.get(j--) - list.get(i++);
+			return ret;
 		}
-
-		return ret;
 	}
 }
