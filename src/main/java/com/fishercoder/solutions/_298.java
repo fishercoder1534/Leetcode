6558a8a2f25ea515bdae6ee1786b2f3c80e0762e
Fisher Coder <fishercoder@gmail.com>
2018-12-05 07:43:13 -0800

    refactor 298

--- src/main/java/com/fishercoder/solutions/_298.java
+++ src/main/java/com/fishercoder/solutions/_298.java
@@ -29,28 +29,29 @@ import com.fishercoder.common.classes.TreeNode;
  */
 public class _298 {
 
-    private int max = 1;
+    public static class Solution1 {
+        private int max = 1;
 
-    public int longestConsecutive(TreeNode root) {
-        if (root == null) {
-            return 0;
+        public int longestConsecutive(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            dfs(root, 0, root.val);
+            return max;
         }
-        dfs(root, 0, root.val);
-        return max;
+
+        private void dfs(TreeNode root, int curr, int target) {
+            if (root == null) {
+                return;
+            }
+            if (root.val == target) {
+                curr++;
+            } else {
+                curr = 1;
+            }
+            max = Math.max(max, curr);
+            dfs(root.left, curr, root.val + 1);
+            dfs(root.right, curr, root.val + 1);
+        }
     }
-
-    private void dfs(TreeNode root, int curr, int target) {
-        if (root == null) {
-            return;
-        }
-        if (root.val == target) {
-            curr++;
-        } else {
-            curr = 1;
-        }
-        max = Math.max(max, curr);
-        dfs(root.left, curr, root.val + 1);
-        dfs(root.right, curr, root.val + 1);
-    }
-
 }
