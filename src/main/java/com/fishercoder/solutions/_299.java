829ee6f614e184576b86c2fbb5b0f731e99c9c0e
Fisher Coder <fishercoder@gmail.com>
2018-12-06 07:28:33 -0800

    refactor 299

--- src/main/java/com/fishercoder/solutions/_299.java
+++ src/main/java/com/fishercoder/solutions/_299.java
@@ -1,6 +1,13 @@
 package com.fishercoder.solutions;
 
-/**You are playing the following Bulls and Cows game with your friend: You write down a number and ask your friend to guess what the number is. Each time your friend makes a guess, you provide a hint that indicates how many digits in said guess match your secret number exactly in both digit and position (called "bulls") and how many digits match the secret number but locate in the wrong position (called "cows"). Your friend will use successive guesses and hints to eventually derive the secret number.
+/**
+ * 299. Bulls and Cows
+ *
+ * You are playing the following Bulls and Cows game with your friend:
+ * You write down a number and ask your friend to guess what the number is.
+ * Each time your friend makes a guess, you provide a hint that indicates how many digits in said guess match your secret number exactly in
+ * both digit and position (called "bulls") and how many digits match the secret number but locate in the wrong position (called "cows").
+ * Your friend will use successive guesses and hints to eventually derive the secret number.
 
  For example:
 
@@ -16,24 +23,24 @@ package com.fishercoder.solutions;
  In this case, the 1st 1 in friend's guess is a bull, the 2nd or 3rd 1 is a cow, and your function should return "1A1B".
  You may assume that the secret number and your friend's guess only contain digits, and their lengths are always equal.*/
 public class _299 {
-
-    public String getHint(String secret, String guess) {
-        int[] secretCows = new int[10];
-        int[] guessCows = new int[10];
-        int bulls = 0;
-        for (int i = 0; i < secret.length(); i++) {
-            if (guess.charAt(i) == secret.charAt(i)) {
-                bulls++;
-            } else {
-                secretCows[Character.getNumericValue(secret.charAt(i))]++;
-                guessCows[Character.getNumericValue(guess.charAt(i))]++;
+    public static class Solution1 {
+        public String getHint(String secret, String guess) {
+            int[] secretCows = new int[10];
+            int[] guessCows = new int[10];
+            int bulls = 0;
+            for (int i = 0; i < secret.length(); i++) {
+                if (guess.charAt(i) == secret.charAt(i)) {
+                    bulls++;
+                } else {
+                    secretCows[Character.getNumericValue(secret.charAt(i))]++;
+                    guessCows[Character.getNumericValue(guess.charAt(i))]++;
+                }
             }
+            int cows = 0;
+            for (int i = 0; i < 11; i++) {
+                cows += Math.min(secretCows[i], guessCows[i]);
+            }
+            return bulls + "A" + cows + "B";
         }
-        int cows = 0;
-        for (int i = 0; i < 11; i++) {
-            cows += Math.min(secretCows[i], guessCows[i]);
-        }
-        return bulls + "A" + cows + "B";
     }
-
 }
