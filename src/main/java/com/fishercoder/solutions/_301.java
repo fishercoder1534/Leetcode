6a19059e31aa349372ec10f11d88502cb468e3b9
Fisher Coder <fishercoder@gmail.com>
2018-12-08 07:16:07 -0800

    refactor 301

--- src/main/java/com/fishercoder/solutions/_301.java
+++ src/main/java/com/fishercoder/solutions/_301.java
@@ -8,6 +8,8 @@ import java.util.Queue;
 import java.util.Set;
 
 /**
+ * 301. Remove Invalid Parentheses
+ *
  * Remove the minimum number of invalid parentheses in order to make the input string valid. Return all possible results.
 
  Note: The input string may contain letters other than the parentheses ( and ).
@@ -19,62 +21,65 @@ import java.util.Set;
  */
 public class _301 {
 
-	public List<String> removeInvalidParentheses(String s) {
-		List<String> result = new ArrayList<>();
-		if (s == null) {
+	public static class Solution1 {
+
+		public List<String> removeInvalidParentheses(String s) {
+			List<String> result = new ArrayList<>();
+			if (s == null) {
+				return result;
+			}
+
+			Set<String> visited = new HashSet();
+			Queue<String> q = new LinkedList();
+
+			q.offer(s);
+			visited.add(s);
+
+			boolean found = false;
+
+			while (!q.isEmpty()) {
+				String curr = q.poll();
+				if (isValid(curr)) {
+					found = true;
+					result.add(curr);
+				}
+
+				if (found) {
+					continue;//this means if the initial input is already a valid one, we'll just directly return it and there's actually only one valid result
+				}
+
+				for (int i = 0; i < curr.length(); i++) {
+					if (curr.charAt(i) != '(' && curr.charAt(i) != ')') {
+						continue;//this is to rule out those non-parentheses characters
+					}
+
+					String next = curr.substring(0, i) + curr.substring(i + 1);
+					if (!visited.contains(next)) {
+						q.offer(next);
+						visited.add(next);
+					}
+				}
+
+			}
 			return result;
 		}
 
-		Set<String> visited = new HashSet();
-		Queue<String> q = new LinkedList();
-
-		q.offer(s);
-		visited.add(s);
-
-		boolean found = false;
-
-		while (!q.isEmpty()) {
-			String curr = q.poll();
-			if (isValid(curr)) {
-				found = true;
-				result.add(curr);
-			}
-
-			if (found) {
-				continue;//this means if the initial input is already a valid one, we'll just directly return it and there's actually only one valid result
-			}
-
-			for (int i = 0; i < curr.length(); i++) {
-				if (curr.charAt(i) != '(' && curr.charAt(i) != ')') {
-					continue;//this is to rule out those non-parentheses characters
+		private boolean isValid(String str) {
+			char[] chars = str.toCharArray();
+			int count = 0;
+			for (int i = 0; i < chars.length; i++) {
+				char c = chars[i];
+				if (c == '(') {
+					count++;
 				}
-
-				String next = curr.substring(0, i) + curr.substring(i + 1);
-				if (!visited.contains(next)) {
-					q.offer(next);
-					visited.add(next);
+				if (c == ')') {
+					count--;
+					if (count == -1) {
+						return false;
+					}
 				}
 			}
-
+			return count == 0;
 		}
-		return result;
-	}
-
-	private boolean isValid(String str) {
-		char[] chars = str.toCharArray();
-		int count = 0;
-		for (int i = 0; i < chars.length; i++) {
-			char c = chars[i];
-			if (c == '(') {
-				count++;
-			}
-			if (c == ')') {
-				count--;
-				if (count == -1) {
-					return false;
-				}
-			}
-		}
-		return count == 0;
 	}
 }
