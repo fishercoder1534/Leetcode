29f1f002fa8e3092d49c06a29cc64614b98655e8
Fisher Coder <fishercoder@gmail.com>
2018-12-10 07:46:25 -0800

    refactor 302

--- src/main/java/com/fishercoder/solutions/_302.java
+++ src/main/java/com/fishercoder/solutions/_302.java
@@ -2,7 +2,7 @@ package com.fishercoder.solutions;
 
 /**
  * 302. Smallest Rectangle Enclosing Black Pixels
- * 
+ *
  * An image is represented by a binary matrix with 0 as a white pixel and 1 as a black pixel.
  * The black pixels are connected, i.e., there is only one black region.
  * Pixels are connected horizontally and vertically.
