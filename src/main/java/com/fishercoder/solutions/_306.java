3e83d4be84e14a398b8a60bd4e9bff70c5366746
Fisher Coder <fishercoder@gmail.com>
2018-12-13 07:44:42 -0800

    refactor 306

--- src/main/java/com/fishercoder/solutions/_306.java
+++ src/main/java/com/fishercoder/solutions/_306.java
@@ -1,9 +1,12 @@
 package com.fishercoder.solutions;
 
 /**
+ * 306. Additive Number
+ *
  * Additive number is a string whose digits can form additive sequence.
 
- A valid additive sequence should contain at least three numbers. Except for the first two numbers, each subsequent number in the sequence must be the sum of the preceding two.
+ A valid additive sequence should contain at least three numbers.
+ Except for the first two numbers, each subsequent number in the sequence must be the sum of the preceding two.
 
  For example:
  "112358" is an additive number because the digits can form an additive sequence: 1, 1, 2, 3, 5, 8.
@@ -19,38 +22,40 @@ package com.fishercoder.solutions;
  How would you handle overflow for very large input integers?
  */
 public class _306 {
-    /**Credit: https://discuss.leetcode.com/topic/29856/java-recursive-and-iterative-solutions/2*/
-    public boolean isAdditiveNumber(String num) {
-        int n = num.length();
-        for (int i = 1; i <= n / 2; ++i) {
-            for (int j = 1; Math.max(j, i) <= n - i - j; ++j) {
-                if (isValid(i, j, num)) {
-                    return true;
+    public static class Solution1 {
+        /** Credit: https://discuss.leetcode.com/topic/29856/java-recursive-and-iterative-solutions/2 */
+        public boolean isAdditiveNumber(String num) {
+            int n = num.length();
+            for (int i = 1; i <= n / 2; ++i) {
+                for (int j = 1; Math.max(j, i) <= n - i - j; ++j) {
+                    if (isValid(i, j, num)) {
+                        return true;
+                    }
                 }
             }
+            return false;
         }
-        return false;
-    }
 
-    private boolean isValid(int i, int j, String num) {
-        if (num.charAt(0) == '0' && i > 1) {
-            return false;
-        }
-        if (num.charAt(i) == '0' && j > 1) {
-            return false;
-        }
-        String sum;
-        Long x1 = Long.parseLong(num.substring(0, i));
-        Long x2 = Long.parseLong(num.substring(i, i + j));
-        for (int start = i + j; start != num.length(); start += sum.length()) {
-            x2 = x2 + x1;
-            x1 = x2 - x1;
-            sum = x2.toString();
-            if (!num.startsWith(sum, start)) {
+        private boolean isValid(int i, int j, String num) {
+            if (num.charAt(0) == '0' && i > 1) {
                 return false;
             }
+            if (num.charAt(i) == '0' && j > 1) {
+                return false;
+            }
+            String sum;
+            Long x1 = Long.parseLong(num.substring(0, i));
+            Long x2 = Long.parseLong(num.substring(i, i + j));
+            for (int start = i + j; start != num.length(); start += sum.length()) {
+                x2 = x2 + x1;
+                x1 = x2 - x1;
+                sum = x2.toString();
+                if (!num.startsWith(sum, start)) {
+                    return false;
+                }
+            }
+            return true;
         }
-        return true;
     }
 
 }
--- src/test/java/com/fishercoder/_306Test.java
+++ src/test/java/com/fishercoder/_306Test.java
@@ -6,33 +6,30 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by fishercoder on 5/27/17.
- */
 public class _306Test {
-    private static _306 test;
-    private static String num;
+  private static _306.Solution1 solution1;
+  private static String num;
 
-    @BeforeClass
-    public static void setup() {
-        test = new _306();
-    }
+  @BeforeClass
+  public static void setup() {
+    solution1 = new _306.Solution1();
+  }
 
-    @Test
-    public void test1() {
-        num = "0235813";
-        assertEquals(false, test.isAdditiveNumber(num));
-    }
+  @Test
+  public void test1() {
+    num = "0235813";
+    assertEquals(false, solution1.isAdditiveNumber(num));
+  }
 
-    @Test
-    public void test2() {
-        num = "000";
-        assertEquals(true, test.isAdditiveNumber(num));
-    }
+  @Test
+  public void test2() {
+    num = "000";
+    assertEquals(true, solution1.isAdditiveNumber(num));
+  }
 
-    @Test
-    public void test3() {
-        num = "011235";
-        assertEquals(true, test.isAdditiveNumber(num));
-    }
+  @Test
+  public void test3() {
+    num = "011235";
+    assertEquals(true, solution1.isAdditiveNumber(num));
+  }
 }
