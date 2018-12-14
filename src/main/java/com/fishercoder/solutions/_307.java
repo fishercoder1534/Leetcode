0869aab06eaedab25b69c8e901ff38de16d79a4e
Fisher Coder <fishercoder@gmail.com>
2018-12-14 08:15:44 -0800

    refactor 307

--- src/main/java/com/fishercoder/solutions/_307.java
+++ src/main/java/com/fishercoder/solutions/_307.java
@@ -1,6 +1,9 @@
 package com.fishercoder.solutions;
 
-/**Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
+/**
+ * 307. Range Sum Query - Mutable
+ *
+ * Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
 
  The update(i, val) function modifies nums by updating the element at index i to val.
  Example:
@@ -11,92 +14,85 @@ package com.fishercoder.solutions;
  sumRange(0, 2) -> 8
  Note:
  The array is only modifiable by the update function.
- You may assume the number of calls to update and sumRange function is distributed evenly.*/
+ You may assume the number of calls to update and sumRange function is distributed evenly.
+ */
 public class _307 {
 
-	public static void main(String... strings) {
-		//        int[] nums = new int[]{1,3,5};
+	public static class Solution1 {
+		class NumArray {
+			class SegmentTreeNode {
+				SegmentTreeNode left;
+				SegmentTreeNode right;
+				int start;
+				int end;
+				int sum;
 
-		int[] nums = new int[] { 7, 2, 7, 2, 0 };
-		NumArray test = new NumArray(nums);
-		test.update(4, 6);
-		test.update(0, 2);
-		test.update(0, 9);
-	}
-
-	private static class NumArray {
-		class SegmentTreeNode {
-			SegmentTreeNode left;
-			SegmentTreeNode right;
-			int start;
-			int end;
-			int sum;
-
-			public SegmentTreeNode(int start, int end) {
-				this.start = start;
-				this.end = end;
-				this.left = null;
-				this.right = null;
-				this.sum = 0;
+				public SegmentTreeNode(int start, int end) {
+					this.start = start;
+					this.end = end;
+					this.left = null;
+					this.right = null;
+					this.sum = 0;
+				}
 			}
-		}
 
-		private SegmentTreeNode root = null;
+			private SegmentTreeNode root = null;
 
-		public NumArray(int[] nums) {
-			root = buildSegmentTree(nums, 0, nums.length - 1);
-		}
+			public NumArray(int[] nums) {
+				root = buildSegmentTree(nums, 0, nums.length - 1);
+			}
 
-		SegmentTreeNode buildSegmentTree(int[] nums, int start, int end) {
-			if (start > end) {
-				return null;
-			} else {
-				SegmentTreeNode root = new SegmentTreeNode(start, end);
-				if (start == end) {
-					root.sum = nums[start];
+			SegmentTreeNode buildSegmentTree(int[] nums, int start, int end) {
+				if (start > end) {
+					return null;
 				} else {
-					int mid = start + (end - start) / 2;
-					root.left = buildSegmentTree(nums, start, mid);
-					root.right = buildSegmentTree(nums, mid + 1, end);
+					SegmentTreeNode root = new SegmentTreeNode(start, end);
+					if (start == end) {
+						root.sum = nums[start];
+					} else {
+						int mid = start + (end - start) / 2;
+						root.left = buildSegmentTree(nums, start, mid);
+						root.right = buildSegmentTree(nums, mid + 1, end);
+						root.sum = root.left.sum + root.right.sum;
+					}
+					return root;
+				}
+			}
+
+			void update(int i, int val) {
+				update(root, i, val);
+			}
+
+			void update(SegmentTreeNode root, int pos, int val) {
+				if (root.start == root.end) {
+					root.sum = val;
+				} else {
+					int mid = root.start + (root.end - root.start) / 2;
+					if (pos <= mid) {
+						update(root.left, pos, val);
+					} else {
+						update(root.right, pos, val);
+					}
 					root.sum = root.left.sum + root.right.sum;
 				}
-				return root;
 			}
-		}
 
-		void update(int i, int val) {
-			update(root, i, val);
-		}
-
-		void update(SegmentTreeNode root, int pos, int val) {
-			if (root.start == root.end) {
-				root.sum = val;
-			} else {
-				int mid = root.start + (root.end - root.start) / 2;
-				if (pos <= mid) {
-					update(root.left, pos, val);
-				} else {
-					update(root.right, pos, val);
-				}
-				root.sum = root.left.sum + root.right.sum;
+			public int sumRange(int i, int j) {
+				return sumRange(root, i, j);
 			}
-		}
 
-		public int sumRange(int i, int j) {
-			return sumRange(root, i, j);
-		}
-
-		int sumRange(SegmentTreeNode root, int start, int end) {
-			if (root.end == end && root.start == start) {
-				return root.sum;
-			} else {
-				int mid = root.start + (root.end - root.start) / 2;
-				if (end <= mid) {
-					return sumRange(root.left, start, end);
-				} else if (start >= mid + 1) {
-					return sumRange(root.right, start, end);
+			int sumRange(SegmentTreeNode root, int start, int end) {
+				if (root.end == end && root.start == start) {
+					return root.sum;
 				} else {
-					return sumRange(root.right, mid + 1, end) + sumRange(root.left, start, mid);
+					int mid = root.start + (root.end - root.start) / 2;
+					if (end <= mid) {
+						return sumRange(root.left, start, end);
+					} else if (start >= mid + 1) {
+						return sumRange(root.right, start, end);
+					} else {
+						return sumRange(root.right, mid + 1, end) + sumRange(root.left, start, mid);
+					}
 				}
 			}
 		}
