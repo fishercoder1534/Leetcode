db420285ecfaec6c6a60a309e361813fe45c78a3
Fisher Coder <fishercoder@gmail.com>
2018-12-17 08:30:49 -0800

    refactor 310

--- src/main/java/com/fishercoder/solutions/_310.java
+++ src/main/java/com/fishercoder/solutions/_310.java
@@ -8,7 +8,11 @@ import java.util.List;
 import java.util.Set;
 
 /**
- * For a undirected graph with tree characteristics, we can choose any node as the root. The result graph is then a rooted tree. Among all possible rooted trees, those with minimum height are called minimum height trees (MHTs). Given such a graph, write a function to find all the MHTs and return a list of their root labels.
+ * 310. Minimum Height Trees
+ *
+ * For a undirected graph with tree characteristics, we can choose any node as the root.
+ * The result graph is then a rooted tree. Among all possible rooted trees, those with minimum height are called minimum height trees (MHTs).
+ * Given such a graph, write a function to find all the MHTs and return a list of their root labels.
 
  Format
  The graph contains n nodes which are labeled from 0 to n - 1. You will be given the number n and a list of undirected edges (each edge is a pair of labels).
@@ -19,7 +23,7 @@ import java.util.Set;
 
  Given n = 4, edges = [[1, 0], [1, 2], [1, 3]]
 
-  0
+   0
    |
    1
   / \
@@ -50,40 +54,41 @@ import java.util.Set;
  */
 public class _310 {
 
-    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-        if (n == 1) {
-            return Collections.singletonList(0);
-        }
-
-        List<Set<Integer>> adj = new ArrayList<>(n);
-        for (int i = 0; i < n; ++i) {
-            adj.add(new HashSet<>());
-        }
-        for (int[] edge : edges) {
-            adj.get(edge[0]).add(edge[1]);
-            adj.get(edge[1]).add(edge[0]);
-        }
-
-        List<Integer> leaves = new ArrayList<>();
-        for (int i = 0; i < n; ++i) {
-            if (adj.get(i).size() == 1) {
-                leaves.add(i);
+    public static class Solution1 {
+        public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+            if (n == 1) {
+                return Collections.singletonList(0);
             }
-        }
 
-        while (n > 2) {
-            n -= leaves.size();
-            List<Integer> newLeaves = new ArrayList<>();
-            for (int i : leaves) {
-                int j = adj.get(i).iterator().next();
-                adj.get(j).remove(i);
-                if (adj.get(j).size() == 1) {
-                    newLeaves.add(j);
+            List<Set<Integer>> adj = new ArrayList<>(n);
+            for (int i = 0; i < n; ++i) {
+                adj.add(new HashSet<>());
+            }
+            for (int[] edge : edges) {
+                adj.get(edge[0]).add(edge[1]);
+                adj.get(edge[1]).add(edge[0]);
+            }
+
+            List<Integer> leaves = new ArrayList<>();
+            for (int i = 0; i < n; ++i) {
+                if (adj.get(i).size() == 1) {
+                    leaves.add(i);
                 }
             }
-            leaves = newLeaves;
-        }
-        return leaves;
-    }
 
+            while (n > 2) {
+                n -= leaves.size();
+                List<Integer> newLeaves = new ArrayList<>();
+                for (int i : leaves) {
+                    int j = adj.get(i).iterator().next();
+                    adj.get(j).remove(i);
+                    if (adj.get(j).size() == 1) {
+                        newLeaves.add(j);
+                    }
+                }
+                leaves = newLeaves;
+            }
+            return leaves;
+        }
+    }
 }
