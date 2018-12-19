8df1dc0a93e7f49d4c4e2e65ca9cb3de6c6bd20f
Fisher Coder <fishercoder@gmail.com>
2018-12-19 08:16:14 -0800

    refactor 312

--- src/main/java/com/fishercoder/solutions/_312.java
+++ src/main/java/com/fishercoder/solutions/_312.java
@@ -1,7 +1,13 @@
 package com.fishercoder.solutions;
 
 /**
- * Given n balloons, indexed from 0 to n-1. Each balloon is painted with a number on it represented by array nums. You are asked to burst all the balloons. If the you burst balloon i you will get nums[left] * nums[i] * nums[right] coins. Here left and right are adjacent indices of i. After the burst, the left and right then becomes adjacent.
+ * 312. Burst Balloons
+ *
+ * Given n balloons, indexed from 0 to n-1.
+ * Each balloon is painted with a number on it represented by array nums.
+ * You are asked to burst all the balloons.
+ * If the you burst balloon i you will get nums[left] * nums[i] * nums[right] coins.
+ * Here left and right are adjacent indices of i. After the burst, the left and right then becomes adjacent.
 
  Find the maximum coins you can collect by bursting the balloons wisely.
 
@@ -20,28 +26,28 @@ package com.fishercoder.solutions;
  */
 public class _312 {
 
-    public int maxCoins(int[] iNums) {
-        int[] nums = new int[iNums.length + 2];
-        int n = 1;
-        for (int x : iNums) {
-            if (x > 0) {
-                nums[n++] = x;
-            }
-        }
-        nums[0] = nums[n++] = 1;
-
-
-        int[][] dp = new int[n][n];
-        for (int k = 2; k < n; ++k) {
-            for (int left = 0; left < n - k; ++left) {
-                int right = left + k;
-                for (int i = left + 1; i < right; ++i) {
-                    dp[left][right] = Math.max(dp[left][right],
-                            nums[left] * nums[i] * nums[right] + dp[left][i] + dp[i][right]);
+    public static class Solution1 {
+        public int maxCoins(int[] iNums) {
+            int[] nums = new int[iNums.length + 2];
+            int n = 1;
+            for (int x : iNums) {
+                if (x > 0) {
+                    nums[n++] = x;
                 }
             }
-        }
-        return dp[0][n - 1];
-    }
+            nums[0] = nums[n++] = 1;
 
+            int[][] dp = new int[n][n];
+            for (int k = 2; k < n; ++k) {
+                for (int left = 0; left < n - k; ++left) {
+                    int right = left + k;
+                    for (int i = left + 1; i < right; ++i) {
+                        dp[left][right] = Math.max(dp[left][right],
+                            nums[left] * nums[i] * nums[right] + dp[left][i] + dp[i][right]);
+                    }
+                }
+            }
+            return dp[0][n - 1];
+        }
+    }
 }
