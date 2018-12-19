eb1fc6426fc191b4261796072c615af0e5482da6
David <10097zhou@gmail.com>
2018-12-18 22:04:03 -0800

    Update _177.sql

--- database/_177.sql
+++ database/_177.sql
@@ -1,9 +1,12 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-declare m int;
-set m=N-1;
+  SET N=N-1;
   RETURN (
       # Write your MySQL query statement below.
-    select distinct Salary from Employee order by Salary desc limit m, 1  
+      SELECT Salary
+      FROM Employee
+      GROUP BY Salary
+      ORDER BY Salary DESC
+      LIMIT N, 1
   );
 END
