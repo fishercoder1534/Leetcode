4e47f8b6e346c0ec77dcf4663d686ee7f153dd75
Fisher Coder <fishercoder@gmail.com>
2018-12-25 08:44:51 -0800

    refactor 316

--- src/test/java/com/fishercoder/_316Test.java
+++ src/test/java/com/fishercoder/_316Test.java
@@ -6,24 +6,25 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by stevesun on 6/2/17.
- */
 public class _316Test {
-    private static _316 test;
+  private static _316.Solution1 solution1;
+  private static _316.Solution2 solution2;
 
-    @BeforeClass
-    public static void setup() {
-        test = new _316();
-    }
+  @BeforeClass
+  public static void setup() {
+    solution1 = new _316.Solution1();
+    solution2 = new _316.Solution2();
+  }
 
-    @Test
-    public void test1() {
-        assertEquals("abc", test.removeDuplicateLetters("bcabc"));
-    }
+  @Test
+  public void test1() {
+    assertEquals("abc", solution1.removeDuplicateLetters("bcabc"));
+    assertEquals("abc", solution2.removeDuplicateLetters("bcabc"));
+  }
 
-    @Test
-    public void test2() {
-        assertEquals("acdb", test.removeDuplicateLetters("cbacdcbc"));
-    }
+  @Test
+  public void test2() {
+    assertEquals("acdb", solution1.removeDuplicateLetters("cbacdcbc"));
+    assertEquals("acdb", solution2.removeDuplicateLetters("cbacdcbc"));
+  }
 }
