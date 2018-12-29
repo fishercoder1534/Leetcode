004d4c38357186279f5e91916d757f1d23f1e1bd
Fisher Coder <fishercoder@gmail.com>
2018-12-29 07:54:37 -0800

    refactor 320

--- src/test/java/com/fishercoder/_320Test.java
+++ src/test/java/com/fishercoder/_320Test.java
@@ -11,31 +11,30 @@ import java.util.List;
 
 import static org.junit.Assert.assertTrue;
 
-/**
- * Created by fishercoder on 2/10/17.
- */
 public class _320Test {
-    private static _320 test;
-    private static List<String> expected;
-    private static List<String> actual;
-    private static String word;
+  private static _320.Solution1 solution1;
+  private static List<String> expected;
+  private static List<String> actual;
+  private static String word;
 
-    @BeforeClass
-    public static void setup() {
-        test = new _320();
-    }
+  @BeforeClass
+  public static void setup() {
+    solution1 = new _320.Solution1();
+  }
 
-    @Before
-    public void setupForEachTest() {
-        expected = new ArrayList<>();
-        actual = new ArrayList<>();
-    }
+  @Before
+  public void setupForEachTest() {
+    expected = new ArrayList<>();
+    actual = new ArrayList<>();
+  }
 
-    @Test
-    public void test1() {
-        word = "word";
-        expected = Arrays.asList("word", "1ord", "w1rd", "wo1d", "wor1", "2rd", "w2d", "wo2", "1o1d", "1or1", "w1r1", "1o2", "2r1", "3d", "w3", "4");
-        actual = test.generateAbbreviations(word);
-        assertTrue(expected.containsAll(actual) && actual.containsAll(expected));
-    }
+  @Test
+  public void test1() {
+    word = "word";
+    expected =
+        Arrays.asList("word", "1ord", "w1rd", "wo1d", "wor1", "2rd", "w2d", "wo2", "1o1d", "1or1",
+            "w1r1", "1o2", "2r1", "3d", "w3", "4");
+    actual = solution1.generateAbbreviations(word);
+    assertTrue(expected.containsAll(actual) && actual.containsAll(expected));
+  }
 }
