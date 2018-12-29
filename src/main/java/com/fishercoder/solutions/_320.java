adb5683b0a071b91c03e40387c8fce935b028f51
Fisher Coder <fishercoder@gmail.com>
2018-12-29 07:54:37 -0800

    refactor 320

--- src/main/java/com/fishercoder/solutions/_320.java
+++ src/main/java/com/fishercoder/solutions/_320.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * 320. Generalized Abbreviation
+ *
  * Write a function to generate the generalized abbreviations of a word.
 
  Example:
@@ -13,21 +15,25 @@ import java.util.List;
  */
 public class _320 {
 
-    public List<String> generateAbbreviations(String word) {
-        List<String> result = new ArrayList<>();
-        backtrack(word, result, 0, "", 0);
-        return result;
-    }
+    public static class Solution1 {
+        public List<String> generateAbbreviations(String word) {
+            List<String> result = new ArrayList<>();
+            backtrack(word, result, 0, "", 0);
+            return result;
+        }
 
-    private void backtrack(String word, List<String> result, int position, String current, int count) {
-        if (position == word.length()) {
-            if (count > 0) {
-                current += count;
+        private void backtrack(String word, List<String> result, int position, String current,
+            int count) {
+            if (position == word.length()) {
+                if (count > 0) {
+                    current += count;
+                }
+                result.add(current);
+            } else {
+                backtrack(word, result, position + 1, current, count + 1);
+                backtrack(word, result, position + 1,
+                    current + (count > 0 ? count : "") + word.charAt(position), 0);
             }
-            result.add(current);
-        } else {
-            backtrack(word, result, position + 1, current, count + 1);
-            backtrack(word, result, position + 1, current + (count > 0 ? count : "") + word.charAt(position), 0);
         }
     }
 
