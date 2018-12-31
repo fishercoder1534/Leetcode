71aa0c18fc50a57966628a65a21c7c9422c817a4
Fisher Coder <fishercoder@gmail.com>
2018-12-31 07:54:04 -0800

    refactor 323

--- src/main/java/com/fishercoder/solutions/_323.java
+++ src/main/java/com/fishercoder/solutions/_323.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.Queue;
 
 /**
+ * 323. Number of Connected Components in an Undirected Graph
+ *
  *  Given n nodes labeled from 0 to n - 1 and a list of undirected edges (each edge is a pair of nodes), write a function to find the number of connected components in an undirected graph.
 
  Example 1:
@@ -28,49 +30,51 @@ import java.util.Queue;
  You can assume that no duplicate edges will appear in edges. Since all edges are undirected, [0, 1] is the same as [1, 0] and thus will not appear together in edges.
  */
 public class _323 {
+    public static class Solution1 {
 
-    public int countComponents(int n, int[][] edges) {
-        if (n <= 1) {
-            return n;
-        }
-
-        List<List<Integer>> adList = new ArrayList<List<Integer>>();
-        for (int i = 0; i < n; i++) {
-            adList.add(new ArrayList<Integer>());
-        }
-
-        for (int[] edge : edges) {
-            adList.get(edge[0]).add(edge[1]);
-            adList.get(edge[1]).add(edge[0]);
-        }
-
-        for (List<Integer> list : adList) {
-            for (int i : list) {
-                System.out.print(i + ", ");
+        public int countComponents(int n, int[][] edges) {
+            if (n <= 1) {
+                return n;
             }
-            System.out.println();
-        }
 
-        boolean[] visited = new boolean[n];
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                count++;
-                Queue<Integer> q = new LinkedList<Integer>();
-                q.offer(i);
-                while (!q.isEmpty()) {
-                    int index = q.poll();
-                    visited[index] = true;
-                    for (int j : adList.get(index)) {
-                        if (!visited[j]) {
-                            q.offer(j);
+            List<List<Integer>> adList = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                adList.add(new ArrayList<>());
+            }
+
+            for (int[] edge : edges) {
+                adList.get(edge[0]).add(edge[1]);
+                adList.get(edge[1]).add(edge[0]);
+            }
+
+            for (List<Integer> list : adList) {
+                for (int i : list) {
+                    System.out.print(i + ", ");
+                }
+                System.out.println();
+            }
+
+            boolean[] visited = new boolean[n];
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                if (!visited[i]) {
+                    count++;
+                    Queue<Integer> q = new LinkedList<>();
+                    q.offer(i);
+                    while (!q.isEmpty()) {
+                        int index = q.poll();
+                        visited[index] = true;
+                        for (int j : adList.get(index)) {
+                            if (!visited[j]) {
+                                q.offer(j);
+                            }
                         }
                     }
                 }
             }
-        }
 
-        return count;
+            return count;
+        }
     }
 
 }
