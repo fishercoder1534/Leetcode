fe2c7df5207b08c6c8556d3f7675f1799e6abb32
Fisher Coder <fishercoder@gmail.com>
2019-01-02 07:22:39 -0800

    refactor 328

--- src/test/java/com/fishercoder/_328Test.java
+++ src/test/java/com/fishercoder/_328Test.java
@@ -7,32 +7,29 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by stevesun on 5/29/17.
- */
 public class _328Test {
-    private static _328 test;
-    private static ListNode expected;
-    private static ListNode node;
+  private static _328.Solution1 solution1;
+  private static ListNode expected;
+  private static ListNode node;
 
-    @BeforeClass
-    public static void setup() {
-        test = new _328();
-    }
+  @BeforeClass
+  public static void setup() {
+    solution1 = new _328.Solution1();
+  }
 
-    @Test
-    public void test1() {
-        node = new ListNode(1);
-        node.next = new ListNode(2);
-        node.next.next = new ListNode(3);
-        node.next.next.next = new ListNode(4);
-        node.next.next.next.next = new ListNode(5);
+  @Test
+  public void test1() {
+    node = new ListNode(1);
+    node.next = new ListNode(2);
+    node.next.next = new ListNode(3);
+    node.next.next.next = new ListNode(4);
+    node.next.next.next.next = new ListNode(5);
 
-        expected = new ListNode(1);
-        expected.next = new ListNode(3);
-        expected.next.next = new ListNode(5);
-        expected.next.next.next = new ListNode(2);
-        expected.next.next.next.next = new ListNode(4);
-        assertEquals(expected, test.oddEvenList(node));
-    }
+    expected = new ListNode(1);
+    expected.next = new ListNode(3);
+    expected.next.next = new ListNode(5);
+    expected.next.next.next = new ListNode(2);
+    expected.next.next.next.next = new ListNode(4);
+    assertEquals(expected, solution1.oddEvenList(node));
+  }
 }
