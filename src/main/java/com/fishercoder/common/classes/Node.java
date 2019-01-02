4b4354904f91f10072785bac7ac7455f3016745c
Fisher Coder <fishercoder@gmail.com>
2019-01-01 21:46:04 -0800

    add 429

--- src/main/java/com/fishercoder/common/classes/Node.java
+++ src/main/java/com/fishercoder/common/classes/Node.java
@@ -1,5 +1,6 @@
 package com.fishercoder.common.classes;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Node {
@@ -12,6 +13,11 @@ public class Node {
   public Node(int val, List<Node> children) {
     this.val = val;
     this.children = children;
+  }
+
+  public Node(int val) {
+    this.val = val;
+    this.children = new ArrayList<>();
   }
 
   //todo: implement this method
