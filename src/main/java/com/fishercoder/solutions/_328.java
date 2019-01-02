9ad3f9c316119cac8d89db022b7570a8b73bfff5
Fisher Coder <fishercoder@gmail.com>
2019-01-02 07:22:39 -0800

    refactor 328

--- src/main/java/com/fishercoder/solutions/_328.java
+++ src/main/java/com/fishercoder/solutions/_328.java
@@ -19,22 +19,23 @@ import com.fishercoder.common.classes.ListNode;
  */
 public class _328 {
 
-    public ListNode oddEvenList(ListNode head) {
-        if (head != null) {
-            ListNode odd = head;
-            ListNode even = head.next;
-            ListNode evenHead = even;
+    public static class Solution1 {
+        public ListNode oddEvenList(ListNode head) {
+            if (head != null) {
+                ListNode odd = head;
+                ListNode even = head.next;
+                ListNode evenHead = even;
 
-            while (even != null && even.next != null) {
-                odd.next = odd.next.next;
-                even.next = even.next.next;
-                odd = odd.next;
-                even = even.next;
+                while (even != null && even.next != null) {
+                    odd.next = odd.next.next;
+                    even.next = even.next.next;
+                    odd = odd.next;
+                    even = even.next;
+                }
+
+                odd.next = evenHead;
             }
-
-            odd.next = evenHead;
+            return head;
         }
-        return head;
     }
-
 }
