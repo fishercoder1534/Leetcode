03e6857cfce78eab6a5061fbba201d2893a90544
Fisher Coder <fishercoder@gmail.com>
2019-01-05 07:08:42 -0800

    fix build

--- src/test/java/com/fishercoder/_331Test.java
+++ src/test/java/com/fishercoder/_331Test.java
@@ -8,41 +8,34 @@ import static org.junit.Assert.assertEquals;
 
 public class _331Test {
   private static _331.Solution1 solution1;
-  private static _331.Solution2 solution2;
 
   @BeforeClass
   public static void setup() {
     solution1 = new _331.Solution1();
-    solution2 = new _331.Solution2();
   }
 
   @Test
   public void test1() {
     assertEquals(true, solution1.isValidSerialization("9,3,4,#,#,1,#,#,2,#,6,#,#"));
-    assertEquals(true, solution2.isValidSerialization("9,3,4,#,#,1,#,#,2,#,6,#,#"));
   }
 
   @Test
   public void test2() {
     assertEquals(false, solution1.isValidSerialization("1,#"));
-    assertEquals(false, solution2.isValidSerialization("1,#"));
   }
 
   @Test
   public void test3() {
     assertEquals(false, solution1.isValidSerialization("9,#,#,1"));
-    assertEquals(false, solution2.isValidSerialization("9,#,#,1"));
   }
 
   @Test
   public void test4() {
     assertEquals(false, solution1.isValidSerialization("1"));
-    assertEquals(false, solution2.isValidSerialization("1"));
   }
 
   @Test
   public void test5() {
-    assertEquals(true, solution1.isValidSerialization("#,7,6,9,#,#,#"));
-    assertEquals(true, solution2.isValidSerialization("#,7,6,9,#,#,#"));
+    assertEquals(false, solution1.isValidSerialization("#,7,6,9,#,#,#"));
   }
 }
