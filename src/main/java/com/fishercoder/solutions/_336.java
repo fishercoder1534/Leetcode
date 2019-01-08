28c35ff077413ab1b04dd9f2b5d3c79c117c93a8
Fisher Coder <fishercoder@gmail.com>
2019-01-08 07:14:37 -0800

    refactor 336

--- src/main/java/com/fishercoder/solutions/_336.java
+++ src/main/java/com/fishercoder/solutions/_336.java
@@ -6,7 +6,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
+/**336. Palindrome Pairs
+ *
  * Given a list of unique words, find all pairs of distinct indices (i, j) in the given list, so that the concatenation of the two words, i.e. words[i] + words[j] is a palindrome.
 
  Example 1:
@@ -20,39 +21,42 @@ import java.util.Map;
  */
 public class _336 {
 
-    public List<List<Integer>> palindromePairs(String[] words) {
-        List<List<Integer>> pairs = new ArrayList();
-        Map<String, Integer> map = new HashMap();
-        for (int i = 0; i < words.length; i++) {
-            map.put(words[i], i);
-        }
+    public static class Solution1 {
+        public List<List<Integer>> palindromePairs(String[] words) {
+            List<List<Integer>> pairs = new ArrayList();
+            Map<String, Integer> map = new HashMap();
+            for (int i = 0; i < words.length; i++) {
+                map.put(words[i], i);
+            }
 
-        for (int i = 0; i < words.length; i++) {
-            int l = 0;
-            int r = 0;
-            while (l <= r) {
-                String s = words[i].substring(l, r);
-                Integer j = map.get(new StringBuilder(s).reverse().toString());
-                if (j != null && j != i && isPalindrome(words[i].substring(l == 0 ? r : 0, l == 0 ? words[i].length() : l))) {
-                    pairs.add(Arrays.asList(l == 0 ? new Integer[]{i, j} : new Integer[]{j, i}));
-                }
-                if (r < words[i].length()) {
-                    r++;
-                } else {
-                    l++;
+            for (int i = 0; i < words.length; i++) {
+                int l = 0;
+                int r = 0;
+                while (l <= r) {
+                    String s = words[i].substring(l, r);
+                    Integer j = map.get(new StringBuilder(s).reverse().toString());
+                    if (j != null && j != i && isPalindrome(
+                        words[i].substring(l == 0 ? r : 0, l == 0 ? words[i].length() : l))) {
+                        pairs.add(
+                            Arrays.asList(l == 0 ? new Integer[] {i, j} : new Integer[] {j, i}));
+                    }
+                    if (r < words[i].length()) {
+                        r++;
+                    } else {
+                        l++;
+                    }
                 }
             }
+            return pairs;
         }
-        return pairs;
-    }
 
-    private boolean isPalindrome(String s) {
-        for (int i = 0; i < s.length() / 2; i++) {
-            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
-                return false;
+        private boolean isPalindrome(String s) {
+            for (int i = 0; i < s.length() / 2; i++) {
+                if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+                    return false;
+                }
             }
+            return true;
         }
-        return true;
     }
-
 }
