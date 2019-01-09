e565826979c1267f937dfbef97775c91dd968dc5
Fisher Coder <fishercoder@gmail.com>
2019-01-09 07:27:28 -0800

    refactor 337

--- src/main/java/com/fishercoder/solutions/_337.java
+++ src/main/java/com/fishercoder/solutions/_337.java
@@ -19,7 +19,7 @@ Example 1:
      3
     / \
    2   3
-    \   \ 
+    \   \
      3   1
 Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
 
@@ -27,54 +27,59 @@ Example 2:
      3
     / \
    4   5
-  / \   \ 
+  / \   \
  1   3   1
 Maximum amount of money the thief can rob = 4 + 5 = 9.
 */
 
 public class _337 {
 
-    //same idea, but with cacheing via a hashmap: 8 ms
-    public int rob_dp(TreeNode root) {
-        Map<TreeNode, Integer> map = new HashMap<>();
-        return getMaxValue(root, map);
+    public static class Solution1 {
+        //simple recursion without cacheing: 1189 ms
+        public int rob(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+
+            int val = 0;
+            if (root.left != null) {
+                val += rob(root.left.left) + rob(root.left.right);
+            }
+            if (root.right != null) {
+                val += rob(root.right.left) + rob(root.right.right);
+            }
+            val = Math.max(val + root.val, rob(root.left) + rob(root.right));
+            return val;
+        }
     }
 
-    private int getMaxValue(TreeNode root, Map<TreeNode, Integer> map) {
-        if (root == null) {
-            return 0;
-        }
-        if (map.containsKey(root)) {
-            return map.get(root);
-        }
-
-        int val = 0;
-        if (root.left != null) {
-            val += getMaxValue(root.left.left, map) + getMaxValue(root.left.right, map);
-        }
-        if (root.right != null) {
-            val += getMaxValue(root.right.left, map) + getMaxValue(root.right.right, map);
-        }
-        int max = Math.max(root.val + val, getMaxValue(root.left, map) + getMaxValue(root.right, map));
-        map.put(root, max);
-        return max;
-    }
-
-    //simple recursion without cacheing: 1189 ms
-    public int rob(TreeNode root) {
-        if (root == null) {
-            return 0;
+    public static class Solution2 {
+        //same idea, but with cacheing via a hashmap: 8 ms
+        public int rob_dp(TreeNode root) {
+            Map<TreeNode, Integer> map = new HashMap<>();
+            return getMaxValue(root, map);
         }
 
-        int val = 0;
-        if (root.left != null) {
-            val += rob(root.left.left) + rob(root.left.right);
+        private int getMaxValue(TreeNode root, Map<TreeNode, Integer> map) {
+            if (root == null) {
+                return 0;
+            }
+            if (map.containsKey(root)) {
+                return map.get(root);
+            }
+
+            int val = 0;
+            if (root.left != null) {
+                val += getMaxValue(root.left.left, map) + getMaxValue(root.left.right, map);
+            }
+            if (root.right != null) {
+                val += getMaxValue(root.right.left, map) + getMaxValue(root.right.right, map);
+            }
+            int max = Math.max(root.val + val,
+                getMaxValue(root.left, map) + getMaxValue(root.right, map));
+            map.put(root, max);
+            return max;
         }
-        if (root.right != null) {
-            val += rob(root.right.left) + rob(root.right.right);
-        }
-        val = Math.max(val + root.val, rob(root.left) + rob(root.right));
-        return val;
     }
 
 }
