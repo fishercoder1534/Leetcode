a01662c85fd06fc6a885789876ec1c5e21187efd
Fisher Coder <fishercoder@gmail.com>
2019-01-10 19:42:06 -0800

    fix build

--- src/test/java/com/fishercoder/_394Test.java
+++ src/test/java/com/fishercoder/_394Test.java
@@ -1,6 +1,7 @@
 package com.fishercoder;
 
 import com.fishercoder.solutions._394;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -10,24 +11,25 @@ import static org.junit.Assert.assertEquals;
  */
 
 public class _394Test {
-    private static _394.Solution1 test;
+  private static _394.Solution1 test;
 
-    public static void setUp() {
-        test = new _394.Solution1();
-    }
+  @BeforeClass
+  public static void setUp() {
+    test = new _394.Solution1();
+  }
 
-    @Test
-    public void test1() {
-        assertEquals("aaabcbc", test.decodeString("3[a]2[bc]"));
-    }
+  @Test
+  public void test1() {
+    assertEquals("aaabcbc", test.decodeString("3[a]2[bc]"));
+  }
 
-    @Test
-    public void test2() {
-        assertEquals("accaccacc", test.decodeString("3[a2[c]]"));
-    }
+  @Test
+  public void test2() {
+    assertEquals("accaccacc", test.decodeString("3[a2[c]]"));
+  }
 
-    @Test
-    public void test3() {
-        assertEquals("abcabccdcdcdef", test.decodeString("2[abc]3[cd]ef"));
-    }
+  @Test
+  public void test3() {
+    assertEquals("abcabccdcdcdef", test.decodeString("2[abc]3[cd]ef"));
+  }
 }
