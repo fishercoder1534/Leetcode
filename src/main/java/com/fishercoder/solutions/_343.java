242a21dd23754572ebb1e92de336e7390313511d
Fisher Coder <fishercoder@gmail.com>
2019-01-15 07:26:17 -0800

    refactor 343

--- src/main/java/com/fishercoder/solutions/_343.java
+++ src/main/java/com/fishercoder/solutions/_343.java
@@ -1,6 +1,8 @@
 package com.fishercoder.solutions;
 
 /**
+ * 343. Integer Break
+ *
  * Given a positive integer n, break it into the sum of at least two positive integers and maximize the product of those integers. Return the maximum product you can get.
 
  For example, given n = 2, return 1 (2 = 1 + 1); given n = 10, return 36 (10 = 3 + 3 + 4).
@@ -13,17 +15,19 @@ package com.fishercoder.solutions;
  You may check the breaking results of n ranging from 7 to 10 to discover the regularities.
  */
 public class _343 {
-    public int integerBreak(int n) {
-        if (n == 2) {
-            return 1;
-        } else if (n == 3) {
-            return 2;
-        } else if (n % 3 == 0) {
-            return (int) Math.pow(3, n / 3);
-        } else if (n % 3 == 1) {
-            return 2 * 2 * (int) Math.pow(3, (n - 4) / 3);
-        } else {
-            return 2 * (int) Math.pow(3, n / 3);
+    public static class Solution1 {
+        public int integerBreak(int n) {
+            if (n == 2) {
+                return 1;
+            } else if (n == 3) {
+                return 2;
+            } else if (n % 3 == 0) {
+                return (int) Math.pow(3, n / 3);
+            } else if (n % 3 == 1) {
+                return 2 * 2 * (int) Math.pow(3, (n - 4) / 3);
+            } else {
+                return 2 * (int) Math.pow(3, n / 3);
+            }
         }
     }
 }
