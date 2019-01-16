b4fac2345c047153129bf166ed493aa9b8c25e48
Fisher Coder <fishercoder@gmail.com>
2019-01-16 07:32:17 -0800

    refactor 344

--- src/main/java/com/fishercoder/solutions/_344.java
+++ src/main/java/com/fishercoder/solutions/_344.java
@@ -6,22 +6,20 @@ package com.fishercoder.solutions;
  * Write a function that takes a string as input and returns the string reversed.
 
  Example:
- Given s = "hello", return "olleh".*/
+ Given s = "hello", return "olleh".
+ */
 public class _344 {
-    public String reverseString_cheating(String s) {
-        return new StringBuilder(s).reverse().toString();
-    }
-
-    public String reverseString(String s) {
-        int i = 0;
-        int j = s.length() - 1;
-        char[] chars = s.toCharArray();
-        while (i < j) {
-            char temp = chars[i];
-            chars[i++] = chars[j];
-            chars[j--] = temp;
-
+    public static class Solution1 {
+        public String reverseString(String s) {
+            int i = 0;
+            int j = s.length() - 1;
+            char[] chars = s.toCharArray();
+            while (i < j) {
+                char temp = chars[i];
+                chars[i++] = chars[j];
+                chars[j--] = temp;
+            }
+            return new String(chars);
         }
-        return new String(chars);
     }
 }
