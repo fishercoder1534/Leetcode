2b6b2c6e58ed8ee46ae8e84e3cdc2e9e7db8f4de
Fisher Coder <fishercoder@gmail.com>
2019-01-17 08:03:36 -0800

    refactor 345

--- src/main/java/com/fishercoder/solutions/_345.java
+++ src/main/java/com/fishercoder/solutions/_345.java
@@ -1,9 +1,13 @@
 package com.fishercoder.solutions;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-/**Write a function that takes a string as input and reverse only the vowels of a string.
+/**
+ * 345. Reverse Vowels of a String
+ *
+ * Write a function that takes a string as input and reverse only the vowels of a string.
 
  Example 1:
  Given s = "hello", return "holle".
@@ -14,45 +18,31 @@ import java.util.Set;
  Note:
  The vowels does not include the letter "y".*/
 public class _345 {
-	public String reverseVowels(String s) {
-		StringBuilder sb = new StringBuilder(s);
-		Set<Character> vowels = new HashSet();
-		vowels.add('a');
-		vowels.add('e');
-		vowels.add('i');
-		vowels.add('o');
-		vowels.add('u');
-		vowels.add('A');
-		vowels.add('E');
-		vowels.add('I');
-		vowels.add('O');
-		vowels.add('U');
-		//use two pointers approach would be the fastest
-		int i = 0;
-		int j = s.length() - 1;
-		while (i < j) {
-			char left = s.charAt(i);
-			char right = s.charAt(j);
-			while (i < j && !vowels.contains(left)) {
+	public static class Solution1 {
+		public String reverseVowels(String s) {
+			StringBuilder sb = new StringBuilder(s);
+			Set<Character> vowels = new HashSet(Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
+			//use two pointers approach would be the fastest
+			int i = 0;
+			int j = s.length() - 1;
+			while (i < j) {
+				char left = s.charAt(i);
+				char right = s.charAt(j);
+				while (i < j && !vowels.contains(left)) {
+					i++;
+					left = s.charAt(i);
+				}
+				while (i < j && !vowels.contains(right)) {
+					j--;
+					right = s.charAt(j);
+				}
+				char temp = left;
+				sb.setCharAt(i, right);
+				sb.setCharAt(j, temp);
 				i++;
-				left = s.charAt(i);
-			}
-			while (i < j && !vowels.contains(right)) {
 				j--;
-				right = s.charAt(j);
 			}
-			char temp = left;
-			sb.setCharAt(i, right);
-			sb.setCharAt(j, temp);
-			i++;
-			j--;
+			return sb.toString();
 		}
-		return sb.toString();
-	}
-
-	public static void main(String... strings) {
-		_345 test = new _345();
-		String s = "leetcode";
-		System.out.println(test.reverseVowels(s));
 	}
 }
