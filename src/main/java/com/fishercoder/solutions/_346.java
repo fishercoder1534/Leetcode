603bc20974a4012daa84585a62403107864d2c77
Fisher Coder <fishercoder@gmail.com>
2019-01-18 07:27:48 -0800

    refactor 346

--- src/main/java/com/fishercoder/solutions/_346.java
+++ src/main/java/com/fishercoder/solutions/_346.java
@@ -3,7 +3,10 @@ package com.fishercoder.solutions;
 import java.util.LinkedList;
 import java.util.Queue;
 
-/**Given a stream of integers and a window size, calculate the moving average of all integers in the sliding window.
+/**
+ * 346. Moving Average from Data Stream
+ *
+ * Given a stream of integers and a window size, calculate the moving average of all integers in the sliding window.
 
  For example,
  MovingAverage m = new MovingAverage(3);
@@ -14,32 +17,34 @@ import java.util.Queue;
  */
 public class _346 {
 
-    class MovingAverage {
+    public static class Solution1 {
+        class MovingAverage {
 
-        private Queue<Integer> q;
-        private Long sum;
-        private int max;
+            private Queue<Integer> q;
+            private Long sum;
+            private int max;
 
-        /**
-         * Initialize your data structure here.
-         */
-        public MovingAverage(int size) {
-            q = new LinkedList();
-            sum = 0L;
-            max = size;
-        }
+            /**
+             * Initialize your data structure here.
+             */
+            public MovingAverage(int size) {
+                q = new LinkedList();
+                sum = 0L;
+                max = size;
+            }
 
-        public double next(int val) {
-            if (q.size() < max) {
-                q.offer(val);
-                sum += val;
-                return (double) sum / q.size();
-            } else {
-                int first = q.poll();
-                sum -= first;
-                q.offer(val);
-                sum += val;
-                return (double) sum / q.size();
+            public double next(int val) {
+                if (q.size() < max) {
+                    q.offer(val);
+                    sum += val;
+                    return (double) sum / q.size();
+                } else {
+                    int first = q.poll();
+                    sum -= first;
+                    q.offer(val);
+                    sum += val;
+                    return (double) sum / q.size();
+                }
             }
         }
     }
