fa914b8a33a074f2f4f134078485c34bbb7253d6
Fisher Coder <fishercoder@gmail.com>
2019-01-25 08:18:03 -0800

    refactor 356

--- src/main/java/com/fishercoder/solutions/_356.java
+++ src/main/java/com/fishercoder/solutions/_356.java
@@ -4,6 +4,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
+ * 356. Line Reflection
+ *
  * Given n points on a 2D plane, find if there is such a line parallel to y-axis that reflect the given points.
 
  Example 1:
@@ -22,24 +24,26 @@ import java.util.Set;
  For each point, make sure that it has a reflected point in the opposite side.
  */
 public class _356 {
-    /**credit: https://discuss.leetcode.com/topic/48172/simple-java-hashset-solution*/
-    public boolean isReflected(int[][] points) {
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        Set<String> set = new HashSet<>();
-        for (int[] point : points) {
-            max = Math.max(max, point[0]);
-            min = Math.min(min, point[0]);
-            String str = point[0] + "a" + point[1];
-            set.add(str);
-        }
-        int sum = max + min;
-        for (int[] p : points) {
-            String str = (sum - p[0]) + "a" + p[1];
-            if (!set.contains(str)) {
-                return false;
+    public static class Solution1 {
+        /** credit: https://discuss.leetcode.com/topic/48172/simple-java-hashset-solution */
+        public boolean isReflected(int[][] points) {
+            int max = Integer.MIN_VALUE;
+            int min = Integer.MAX_VALUE;
+            Set<String> set = new HashSet<>();
+            for (int[] point : points) {
+                max = Math.max(max, point[0]);
+                min = Math.min(min, point[0]);
+                String str = point[0] + "a" + point[1];
+                set.add(str);
             }
+            int sum = max + min;
+            for (int[] p : points) {
+                String str = (sum - p[0]) + "a" + p[1];
+                if (!set.contains(str)) {
+                    return false;
+                }
+            }
+            return true;
         }
-        return true;
     }
 }
