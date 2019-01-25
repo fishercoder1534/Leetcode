1feac43a0db13f3511d501abd9e408aabd7a6b7b
Fisher Coder <fishercoder@gmail.com>
2019-01-25 08:18:03 -0800

    refactor 356

--- src/test/java/com/fishercoder/_356Test.java
+++ src/test/java/com/fishercoder/_356Test.java
@@ -6,33 +6,30 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by stevesun on 6/1/17.
- */
 public class _356Test {
-    private static _356 test;
-    private static int[][] points;
+  private static _356.Solution1 solution1;
+  private static int[][] points;
 
-    @BeforeClass
-    public static void setup() {
-        test = new _356();
-    }
+  @BeforeClass
+  public static void setup() {
+    solution1 = new _356.Solution1();
+  }
 
-    @Test
-    public void test1() {
-        points = new int[][]{
-                {1, 1},
-                {-1, 1},
-        };
-        assertEquals(true, test.isReflected(points));
-    }
+  @Test
+  public void test1() {
+    points = new int[][] {
+        {1, 1},
+        {-1, 1},
+    };
+    assertEquals(true, solution1.isReflected(points));
+  }
 
-    @Test
-    public void test2() {
-        points = new int[][]{
-                {1, 1},
-                {-1, -1},
-        };
-        assertEquals(false, test.isReflected(points));
-    }
+  @Test
+  public void test2() {
+    points = new int[][] {
+        {1, 1},
+        {-1, -1},
+    };
+    assertEquals(false, solution1.isReflected(points));
+  }
 }
