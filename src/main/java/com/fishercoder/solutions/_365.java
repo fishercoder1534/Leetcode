8e1b24f4e5d5f09fbedc12fefaed8120fe2b6ca0
Fisher Coder <fishercoder@gmail.com>
2019-02-03 08:30:04 -0800

    refactor 365

--- src/main/java/com/fishercoder/solutions/_365.java
+++ src/main/java/com/fishercoder/solutions/_365.java
@@ -26,23 +26,24 @@ package com.fishercoder.solutions;
  */
 public class _365 {
 
-    public boolean canMeasureWater(int x, int y, int z) {
-        if (x + y < z) {
-            return false;
+    public static class Solution1 {
+        public boolean canMeasureWater(int x, int y, int z) {
+            if (x + y < z) {
+                return false;
+            }
+            if (x == z || y == z || x + y == z) {
+                return true;
+            }
+            return z % gcd(x, y) == 0;
         }
-        if (x == z || y == z || x + y == z) {
-            return true;
-        }
-        return z % gcd(x, y) == 0;
-    }
 
-    int gcd(int x, int y) {
-        while (y != 0) {
-            int temp = y;
-            y = x % y;
-            x = temp;
+        int gcd(int x, int y) {
+            while (y != 0) {
+                int temp = y;
+                y = x % y;
+                x = temp;
+            }
+            return x;
         }
-        return x;
     }
-
 }
