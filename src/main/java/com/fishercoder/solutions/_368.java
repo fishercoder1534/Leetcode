fbf59c42516699562367aba6e95f184bbdc1edac
Fisher Coder <fishercoder@gmail.com>
2019-02-06 09:31:08 -0800

    refactor 368

--- src/main/java/com/fishercoder/solutions/_368.java
+++ src/main/java/com/fishercoder/solutions/_368.java
@@ -24,36 +24,38 @@ import java.util.List;
  */
 public class _368 {
 
-    /**Credit: https://discuss.leetcode.com/topic/49652/classic-dp-solution-similar-to-lis-o-n-2*/
-    public List<Integer> largestDivisibleSubset(int[] nums) {
-        int len = nums.length;
-        int[] count = new int[len];
-        int[] pre = new int[len];
-        Arrays.sort(nums);
-        int max = 0;
-        int index = -1;
-        for (int i = 0; i < len; i++) {
-            count[i] = 1;
-            pre[i] = -1;
-            for (int j = i - 1; j >= 0; j--) {
-                if (nums[i] % nums[j] == 0) {
-                    if (1 + count[j] > count[i]) {
-                        count[i] = count[j] + 1;
-                        pre[i] = j;
+    public static class Solution1 {
+        /** Credit: https://discuss.leetcode.com/topic/49652/classic-dp-solution-similar-to-lis-o-n-2 */
+        public List<Integer> largestDivisibleSubset(int[] nums) {
+            int len = nums.length;
+            int[] count = new int[len];
+            int[] pre = new int[len];
+            Arrays.sort(nums);
+            int max = 0;
+            int index = -1;
+            for (int i = 0; i < len; i++) {
+                count[i] = 1;
+                pre[i] = -1;
+                for (int j = i - 1; j >= 0; j--) {
+                    if (nums[i] % nums[j] == 0) {
+                        if (1 + count[j] > count[i]) {
+                            count[i] = count[j] + 1;
+                            pre[i] = j;
+                        }
                     }
                 }
+                if (count[i] > max) {
+                    max = count[i];
+                    index = i;
+                }
             }
-            if (count[i] > max) {
-                max = count[i];
-                index = i;
+            List<Integer> res = new ArrayList<>();
+            while (index != -1) {
+                res.add(nums[index]);
+                index = pre[index];
             }
+            return res;
         }
-        List<Integer> res = new ArrayList<>();
-        while (index != -1) {
-            res.add(nums[index]);
-            index = pre[index];
-        }
-        return res;
     }
 
 }
