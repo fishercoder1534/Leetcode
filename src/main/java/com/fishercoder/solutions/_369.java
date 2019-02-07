b9ba53b075f876893c12dd1cdc734ad331528e4c
Fisher Coder <fishercoder@gmail.com>
2019-02-07 08:04:38 -0800

    refactor 369

--- src/main/java/com/fishercoder/solutions/_369.java
+++ src/main/java/com/fishercoder/solutions/_369.java
@@ -3,6 +3,8 @@ package com.fishercoder.solutions;
 import com.fishercoder.common.classes.ListNode;
 
 /**
+ * 369. Plus One Linked List
+ *
  * Given a non-negative integer represented as non-empty a singly linked list of digits, plus one to the integer.
 
  You may assume the integer do not contain any leading zero, except the number 0 itself.
@@ -18,54 +20,55 @@ import com.fishercoder.common.classes.ListNode;
  */
 public class _369 {
 
-    public ListNode plusOne(ListNode head) {
-        //get the length of the list and take out the value of each node and store them into an array
-        ListNode temp = head;
-        int len = 0;
-        while (temp != null) {
-            len++;
-            temp = temp.next;
-        }
+    public static class Solution1 {
+        public ListNode plusOne(ListNode head) {
+            //get the length of the list and take out the value of each node and store them into an array
+            ListNode temp = head;
+            int len = 0;
+            while (temp != null) {
+                len++;
+                temp = temp.next;
+            }
 
-        int[] nums = new int[len];
-        temp = head;
-        int j = 0;
-        while (temp != null) {
-            nums[j++] = temp.val;
-            temp = temp.next;
-        }
+            int[] nums = new int[len];
+            temp = head;
+            int j = 0;
+            while (temp != null) {
+                nums[j++] = temp.val;
+                temp = temp.next;
+            }
 
-        //plus one into this array: nums
-        for (int i = len - 1; i >= 0; i--) {
-            if (nums[i] != 9) {
-                nums[i]++;
-                break;
+            //plus one into this array: nums
+            for (int i = len - 1; i >= 0; i--) {
+                if (nums[i] != 9) {
+                    nums[i]++;
+                    break;
+                } else {
+                    nums[i] = 0;
+                }
+            }
+
+            //still assuming the first value in the list should not be zero as it's representing a valid number, although it's in a list
+            ListNode pre = new ListNode(-1);
+            if (nums[0] == 0) {
+                //in this case, let's just construct a new linked list and return: only first node value is 1, all the rest is 0
+                ListNode newHead = new ListNode(1);
+                ListNode result = newHead;
+                int count = 0;
+                while (count++ < len) {
+                    newHead.next = new ListNode(0);
+                    newHead = newHead.next;
+                }
+                return result;
             } else {
-                nums[i] = 0;
+                pre.next = head;
+                for (int i = 0; i < len; i++) {
+                    head.val = nums[i];
+                    head = head.next;
+                }
+                return pre.next;
             }
         }
-
-        //still assuming the first value in the list should not be zero as it's representing a valid number, although it's in a list
-        ListNode pre = new ListNode(-1);
-        if (nums[0] == 0) {
-            //in this case, let's just construct a new linked list and return: only first node value is 1, all the rest is 0
-            ListNode newHead = new ListNode(1);
-            ListNode result = newHead;
-            int count = 0;
-            while (count++ < len) {
-                newHead.next = new ListNode(0);
-                newHead = newHead.next;
-            }
-            return result;
-        } else {
-            pre.next = head;
-            for (int i = 0; i < len; i++) {
-                head.val = nums[i];
-                head = head.next;
-            }
-            return pre.next;
-        }
-
     }
 
 }
