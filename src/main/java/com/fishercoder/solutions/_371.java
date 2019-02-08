34107744d95d706a9a7da7eb31ce23beaebd5086
Fisher Coder <fishercoder@gmail.com>
2019-02-08 07:58:17 -0800

    refactor 371

--- src/main/java/com/fishercoder/solutions/_371.java
+++ src/main/java/com/fishercoder/solutions/_371.java
@@ -1,18 +1,24 @@
 package com.fishercoder.solutions;
 
-/**Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+/**
+ * 371. Sum of Two Integers
+ *
+ * Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
 
  Example:
- Given a = 1 and b = 2, return 3.*/
+ Given a = 1 and b = 2, return 3.
+ */
 public class _371 {
 
-    /**reference: http://stackoverflow.com/questions/9070937/adding-two-numbers-without-operator-clarification*/
-    public int getSum(int a, int b) {
-        if (b == 0) {
-            return a;
+    public static class Solution1 {
+        /** reference: http://stackoverflow.com/questions/9070937/adding-two-numbers-without-operator-clarification */
+        public int getSum(int a, int b) {
+            if (b == 0) {
+                return a;
+            }
+            int sum = a ^ b;
+            int carry = (a & b) << 1;
+            return getSum(sum, carry);
         }
-        int sum = a ^ b;
-        int carry = (a & b) << 1;
-        return getSum(sum, carry);
     }
 }
