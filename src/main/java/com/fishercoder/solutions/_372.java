e1f70c982ab5463ab2a306ac757beae658413513
Fisher Coder <fishercoder@gmail.com>
2019-02-09 08:23:50 -0800

    refactor 372

--- src/main/java/com/fishercoder/solutions/_372.java
+++ src/main/java/com/fishercoder/solutions/_372.java
@@ -1,6 +1,8 @@
 package com.fishercoder.solutions;
 
 /**
+ * 372. Super Pow
+ *
  * Your task is to calculate ab mod 1337 where a is a positive integer and b is an extremely large positive integer given in the form of an array.
 
  Example1:
@@ -17,33 +19,32 @@ package com.fishercoder.solutions;
  Result: 1024
  */
 public class _372 {
-    /**Reference: https://discuss.leetcode.com/topic/50586/math-solusion-based-on-euler-s-theorem-power-called-only-once-c-java-1-line-python*/
-
-    public int superPow(int a, int[] b) {
-        if (a % 1337 == 0) {
-            return 0;
-        }
-        int p = 0;
-        for (int i : b) {
-            p = (p * 10 + i) % 1140;
-        }
-        if (p == 0) {
-            p += 1140;
-        }
-        return power(a, p, 1337);
-    }
-
-    private int power(int a, int n, int mod) {
-        a %= mod;
-        int result = 1;
-        while (n != 0) {
-            if ((n & 1) != 0) {
-                result = result * a % mod;
+    public static class Solution1 {
+        public int superPow(int a, int[] b) {
+            if (a % 1337 == 0) {
+                return 0;
             }
-            a = a * a % mod;
-            n >>= 1;
+            int p = 0;
+            for (int i : b) {
+                p = (p * 10 + i) % 1140;
+            }
+            if (p == 0) {
+                p += 1140;
+            }
+            return power(a, p, 1337);
         }
-        return result;
-    }
 
+        private int power(int a, int n, int mod) {
+            a %= mod;
+            int result = 1;
+            while (n != 0) {
+                if ((n & 1) != 0) {
+                    result = result * a % mod;
+                }
+                a = a * a % mod;
+                n >>= 1;
+            }
+            return result;
+        }
+    }
 }
