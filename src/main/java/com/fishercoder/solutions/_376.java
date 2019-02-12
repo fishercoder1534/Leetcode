27791a3e8a7b2d3c3de212b9fc7dbd1e22a6d489
Fisher Coder <fishercoder@gmail.com>
2019-02-12 10:30:30 -0800

    refactor 376

--- src/main/java/com/fishercoder/solutions/_376.java
+++ src/main/java/com/fishercoder/solutions/_376.java
@@ -34,23 +34,23 @@ package com.fishercoder.solutions;
  */
 public class _376 {
 
-    /**credit: https://leetcode.com/articles/wiggle-subsequence/#approach-5-greedy-approach-accepted*/
-    public int wiggleMaxLength(int[] nums) {
-        if (nums.length < 2) {
-            return nums.length;
-        }
-        int prevDiff = nums[1] - nums[0];
-        int count = (prevDiff != 0) ? 2 : 1;
-        for (int i = 2; i < nums.length; i++) {
-            int diff = nums[i] - nums[i - 1];
-            /**ATTN: prevDiff could be zero. e.g. [3,3,3,2,5]
-             * but diff needs to exactly greater than zero*/
-            if ((prevDiff <= 0 && diff > 0) || (prevDiff >= 0) && diff < 0) {
-                count++;
-                prevDiff = diff;
+    public static class Solution1 {
+        public int wiggleMaxLength(int[] nums) {
+            if (nums.length < 2) {
+                return nums.length;
             }
+            int prevDiff = nums[1] - nums[0];
+            int count = (prevDiff != 0) ? 2 : 1;
+            for (int i = 2; i < nums.length; i++) {
+                int diff = nums[i] - nums[i - 1];
+                /**ATTN: prevDiff could be zero. e.g. [3,3,3,2,5]
+                 * but diff needs to exactly greater than zero*/
+                if ((prevDiff <= 0 && diff > 0) || (prevDiff >= 0) && diff < 0) {
+                    count++;
+                    prevDiff = diff;
+                }
+            }
+            return count;
         }
-        return count;
     }
-
 }
