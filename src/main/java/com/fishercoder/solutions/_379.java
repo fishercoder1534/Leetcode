cd295c0d470f559ad2dbaa02d2e05f4dabbe3573
Fisher Coder <fishercoder@gmail.com>
2019-02-13 07:06:19 -0800

    refactor 379

--- src/main/java/com/fishercoder/solutions/_379.java
+++ src/main/java/com/fishercoder/solutions/_379.java
@@ -5,7 +5,10 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Set;
 
-/**Design a Phone Directory which supports the following operations:
+/**
+ * 379. Design Phone Directory
+ *
+ * Design a Phone Directory which supports the following operations:
 
  get: Provide a number which is not assigned to anyone.
  check: Check if a number is available or not.
@@ -38,53 +41,54 @@ import java.util.Set;
  */
 public class _379 {
 
-    private class PhoneDirectory {
-        private Queue<Integer> phoneDir;
-        private Set<Integer> used;
+    public static class Solution1 {
+        private class PhoneDirectory {
+            private Queue<Integer> phoneDir;
+            private Set<Integer> used;
 
-        /**
-         * Initialize your data structure here
-         *
-         * @param maxNumbers - The maximum numbers that can be stored in the phone directory.
-         */
-        public PhoneDirectory(int maxNumbers) {
-            phoneDir = new LinkedList();
-            int number = 0;
-            while (maxNumbers-- > 0) {
-                phoneDir.add(number++);
+            /**
+             * Initialize your data structure here
+             *
+             * @param maxNumbers - The maximum numbers that can be stored in the phone directory.
+             */
+            public PhoneDirectory(int maxNumbers) {
+                phoneDir = new LinkedList();
+                int number = 0;
+                while (maxNumbers-- > 0) {
+                    phoneDir.add(number++);
+                }
+                used = new HashSet();
             }
-            used = new HashSet();
-        }
 
-        /**
-         * Provide a number which is not assigned to anyone.
-         *
-         * @return - Return an available number. Return -1 if none is available.
-         */
-        public int get() {
-            if (phoneDir.peek() == null) {
-                return -1;
+            /**
+             * Provide a number which is not assigned to anyone.
+             *
+             * @return - Return an available number. Return -1 if none is available.
+             */
+            public int get() {
+                if (phoneDir.peek() == null) {
+                    return -1;
+                }
+                int newNumber = phoneDir.poll();
+                used.add(newNumber);
+                return newNumber;
             }
-            int newNumber = phoneDir.poll();
-            used.add(newNumber);
-            return newNumber;
-        }
 
-        /**
-         * Check if a number is available or not.
-         */
-        public boolean check(int number) {
-            return !used.contains(number);
-        }
+            /**
+             * Check if a number is available or not.
+             */
+            public boolean check(int number) {
+                return !used.contains(number);
+            }
 
-        /**
-         * Recycle or release a number.
-         */
-        public void release(int number) {
-            if (used.remove(number)) {
-                phoneDir.add(number);
+            /**
+             * Recycle or release a number.
+             */
+            public void release(int number) {
+                if (used.remove(number)) {
+                    phoneDir.add(number);
+                }
             }
         }
     }
-
 }
