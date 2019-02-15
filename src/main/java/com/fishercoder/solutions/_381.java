ba6b18fe815d4770d23c24803c7261fda6ba23d3
Fisher Coder <fishercoder@gmail.com>
2019-02-15 07:49:38 -0800

    refactor 381

--- src/main/java/com/fishercoder/solutions/_381.java
+++ src/main/java/com/fishercoder/solutions/_381.java
@@ -35,78 +35,73 @@ collection.remove(1);
 // getRandom should return 1 and 2 both equally likely.
 collection.getRandom();*/
 public class _381 {
+    public static class Solution1 {
 
-    Map<Integer, Integer> forwardMap;//key is the to-be-inserted number, value is its auto-incremented index
-    Map<Integer, Integer> reverseMap;//the other way around
-    int index;
-    Random rand;
+        Map<Integer, Integer> forwardMap;
+        //key is the to-be-inserted number, value is its auto-incremented index
+        Map<Integer, Integer> reverseMap;//the other way around
+        int index;
+        Random rand;
 
-    /**
-     * Initialize your data structure here.
-     */
-    public _381() {
-        forwardMap = new HashMap();
-        reverseMap = new HashMap();
-        index = 0;
-        rand = new Random();
-    }
-
-    /**
-     * Inserts a value to the collection. Returns true if the collection did not already contain the specified element.
-     */
-    public boolean insert(int val) {
-        boolean contains;
-        if (reverseMap.containsValue(val)) {
-            contains = true;
-        } else {
-            contains = false;
+        /**
+         * Initialize your data structure here.
+         */
+        public _381() {
+            forwardMap = new HashMap();
+            reverseMap = new HashMap();
+            index = 0;
+            rand = new Random();
         }
-        forwardMap.put(val, index);//this will overwrite the preivous key with a new index if the key already exists
-        reverseMap.put(index, val);
-        index++;
-        return contains;
-    }
 
-    /**
-     * Removes a value from the collection. Returns true if the collection contained the specified element.
-     */
-    public boolean remove(int val) {
-        boolean contains;
-        if (reverseMap.containsValue(val)) {
-            contains = true;
-            if (forwardMap.containsKey(val)) {
-                int i = forwardMap.get(val);
-                forwardMap.remove(val);
-                reverseMap.remove(i);
+        /**
+         * Inserts a value to the collection. Returns true if the collection did not already contain the
+         * specified element.
+         */
+        public boolean insert(int val) {
+            boolean contains;
+            if (reverseMap.containsValue(val)) {
+                contains = true;
             } else {
-                //remove the entry in revserve map that has val as its value
-                reverseMap.values().remove(val);
+                contains = false;
             }
-        } else {
-            contains = false;
+            forwardMap.put(val,
+                index);//this will overwrite the preivous key with a new index if the key already exists
+            reverseMap.put(index, val);
+            index++;
+            return contains;
         }
-        return contains;
-    }
 
-    /**
-     * Get a random element from the collection.
-     */
-    public int getRandom() {
-        int randNum = rand.nextInt(index);
-        while (!reverseMap.containsKey(randNum)) {
-            randNum = rand.nextInt(index);
+        /**
+         * Removes a value from the collection. Returns true if the collection contained the specified
+         * element.
+         */
+        public boolean remove(int val) {
+            boolean contains;
+            if (reverseMap.containsValue(val)) {
+                contains = true;
+                if (forwardMap.containsKey(val)) {
+                    int i = forwardMap.get(val);
+                    forwardMap.remove(val);
+                    reverseMap.remove(i);
+                } else {
+                    //remove the entry in revserve map that has val as its value
+                    reverseMap.values().remove(val);
+                }
+            } else {
+                contains = false;
+            }
+            return contains;
         }
-        return reverseMap.get(randNum);
-    }
 
-    public static void main(String... strings) {
-        _381 test = new _381();
-        System.out.println(test.insert(1));
-        System.out.println(test.insert(1));
-        System.out.println(test.insert(2));
-        System.out.println(test.getRandom());
-        System.out.println(test.remove(1));
-        System.out.println(test.getRandom());
+        /**
+         * Get a random element from the collection.
+         */
+        public int getRandom() {
+            int randNum = rand.nextInt(index);
+            while (!reverseMap.containsKey(randNum)) {
+                randNum = rand.nextInt(index);
+            }
+            return reverseMap.get(randNum);
+        }
     }
-
 }
