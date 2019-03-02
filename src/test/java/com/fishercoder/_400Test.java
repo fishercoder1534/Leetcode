d7c07281c88b7e304cc7a6b0a628cdb6177e3193
Fisher Coder <fishercoder@gmail.com>
2019-03-02 08:32:41 -0800

    refactor 400

--- src/test/java/com/fishercoder/_400Test.java
+++ src/test/java/com/fishercoder/_400Test.java
@@ -6,25 +6,22 @@ import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
 
-/**
- * Created by fishercoder on 4/26/17.
- */
 public class _400Test {
-    private static _400 test;
-    private static int expected;
-    private static int actual;
-    private static int n;
+  private static _400.Solution1 solution1;
+  private static int expected;
+  private static int actual;
+  private static int n;
 
-    @BeforeClass
-    public static void setup() {
-        test = new _400();
-    }
+  @BeforeClass
+  public static void setup() {
+    solution1 = new _400.Solution1();
+  }
 
-    @Test
-    public void test1() {
-        n = 11;
-        expected = 0;
-        actual = test.findNthDigit(n);
-        assertEquals(expected, actual);
-    }
+  @Test
+  public void test1() {
+    n = 11;
+    expected = 0;
+    actual = solution1.findNthDigit(n);
+    assertEquals(expected, actual);
+  }
 }
