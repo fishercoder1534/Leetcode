ac30da0d972d8ed2d6ed91e9f680e7edd718839e
Jian GU <ycaxgjd@gmail.com>
2019-03-03 01:12:36 +0100

    Update _35.java (#36)

--- src/main/java/com/fishercoder/solutions/_35.java
+++ src/main/java/com/fishercoder/solutions/_35.java
@@ -18,24 +18,14 @@ package com.fishercoder.solutions;
 
 public class _35 {
 
-    public int searchInsert(int[] A, int target) {
-        int len = A.length;
-        if (len == 0) {
-            return 0;
-        } else {
-            for (int i = 0; i < len; i++) {
-                if (A[0] > target) {
-                    return 0;
-                } else if (A[len - 1] < target) {
-                    return len;
-                } else if (A[i] == target) {
-                    return i;
-                } else if (A[i] < target && A[i + 1] > target) {
-                    return i + 1;
-                }
+    public int searchInsert(int[] nums, int target) {
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            if (target <= nums[i]) {
+                return i;
             }
-            return len;
         }
+        return len;
     }
 
 }
