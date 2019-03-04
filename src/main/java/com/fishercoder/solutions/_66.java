26c7e48e8fa6c1890efe8669ac58fd808d223f5d
Jian GU <ycaxgjd@gmail.com>
2019-03-04 02:21:04 +0100

    Update _66.java (#37)

--- src/main/java/com/fishercoder/solutions/_66.java
+++ src/main/java/com/fishercoder/solutions/_66.java
@@ -10,35 +10,24 @@ public class _66 {
 
   public static class Solution1 {
     public int[] plusOne(int[] digits) {
-      boolean carry = false;
-      int len = digits.length;
-      int[] temp = digits;
-      //process the last digit at first, to get carry value
-      if (digits[len - 1] + 1 == 10) {
-        carry = true;
-        temp[len - 1] = 0;
-      } else {
-        temp[len - 1] += 1;
-        return temp;
-      }
-
-      //start from the second last element
-      for (int i = len - 2; i >= 0; i--) {
-        if (carry && temp[i] + 1 == 10) {
-          temp[i] = 0;
-          carry = true;
-        } else if (carry) {
-          temp[i] += 1;
-          carry = false;
+        int len = digits.length;
+        int[] temp = digits;
+        
+        for (int i = len - 1; i >= 0; i--) {
+            if (temp[i] + 1 == 10) {
+                temp[i] = 0;
+            } else {
+                temp[i] += 1;
+                return temp;
+            }
         }
-      }
-      if (carry && temp[0] == 0) {
-        int[] res = new int[len + 1];
-        res[0] = 1;
-        //all the rest of the numbers should all be zeroes, so we don't need to copy from the original array
-        return res;
-      }
-      return temp;
+        if (temp[0] == 0) {
+            int[] res = new int[len + 1];
+            res[0] = 1; //all the rest of the numbers should all be zeroes, so we don't need to copy from the original array
+            return res;
+        } else {
+            return temp;
+        }
     }
   }
 }
