443cdb57a33ebf1b6961f8d57ccf56c0f4763d81
Fisher Coder <fishercoder@gmail.com>
2019-03-04 06:55:56 -0800

    refactor 402

--- src/test/java/com/fishercoder/_402Test.java
+++ src/test/java/com/fishercoder/_402Test.java
@@ -6,19 +6,16 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by stevesun on 6/3/17.
- */
 public class _402Test {
-    private static _402 test;
+  private static _402.Solution1 solution1;
 
-    @BeforeClass
-    public static void setup() {
-        test = new _402();
-    }
+  @BeforeClass
+  public static void setup() {
+    solution1 = new _402.Solution1();
+  }
 
-    @Test
-    public void test1() {
-        assertEquals("1219", test.removeKdigits("1432219", 3));
-    }
+  @Test
+  public void test1() {
+    assertEquals("1219", solution1.removeKdigits("1432219", 3));
+  }
 }
