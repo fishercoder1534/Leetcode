de01fa7eadea622fe6f2b9e423fff5f9ba00c505
Fisher Coder <fishercoder@gmail.com>
2019-03-08 07:15:54 -0800

    refactor 401

--- src/main/java/com/fishercoder/solutions/_401.java
+++ src/main/java/com/fishercoder/solutions/_401.java
@@ -29,7 +29,7 @@ public class _401 {
             List<String> times = new ArrayList<>();
             for (int h = 0; h < 12; h++) {
                 for (int m = 0; m < 60; m++) {
-                    if (Integer.bitCount(h * 60 + m) == num) {
+                    if (Integer.bitCount(h * 64 + m) == num) {
                         times.add(String.format("%d:%02d", h,
                             m));//%02 means to pad this two-digit decimal number on the left with zeroes
                     }
