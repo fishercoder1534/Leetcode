2d6df377d59f86245e56a6dd0cae7ce3f7e7d312
Fisher Coder <fishercoder@gmail.com>
2019-03-08 07:10:20 -0800

    refactor 406

--- src/test/java/com/fishercoder/_406Test.java
+++ src/test/java/com/fishercoder/_406Test.java
@@ -5,30 +5,27 @@ import com.fishercoder.solutions._406;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-/**
- * Created by stevesun on 6/6/17.
- */
 public class _406Test {
-    private static _406 test;
-    private static int[][] people;
-    private static int[][] actual;
+  private static _406.Solution1 solution1;
+  private static int[][] people;
+  private static int[][] actual;
 
-    @BeforeClass
-    public static void setup() {
-        test = new _406();
-    }
+  @BeforeClass
+  public static void setup() {
+    solution1 = new _406.Solution1();
+  }
 
-    @Test
-    public void test1() {
-        people = new int[][]{
-                {7, 0},
-                {4, 4},
-                {7, 1},
-                {5, 0},
-                {6, 1},
-                {5, 2}
-        };
-        actual = test.reconstructQueue(people);
-        CommonUtils.printArrayArray(actual);
-    }
+  @Test
+  public void test1() {
+    people = new int[][] {
+        {7, 0},
+        {4, 4},
+        {7, 1},
+        {5, 0},
+        {6, 1},
+        {5, 2}
+    };
+    actual = solution1.reconstructQueue(people);
+    CommonUtils.printArrayArray(actual);
+  }
 }
