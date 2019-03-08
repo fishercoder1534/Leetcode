c50f1eee1d593d88b1d26fb392fd12127c5743ab
Fisher Coder <fishercoder@gmail.com>
2019-03-08 07:10:20 -0800

    refactor 406

--- src/main/java/com/fishercoder/solutions/_406.java
+++ src/main/java/com/fishercoder/solutions/_406.java
@@ -27,20 +27,22 @@ import java.util.List;
  */
 public class _406 {
 
-    /**
-     * Credit: https://discuss.leetcode.com/topic/60437/java-solution-using-priorityqueue-and-linkedlist
-     */
-    public int[][] reconstructQueue(int[][] people) {
-        Arrays.sort(people, new Comparator<int[]>() {
-            public int compare(int[] p1, int[] p2) {
-                return p1[0] != p2[0] ? Integer.compare(p2[0], p1[0]) : Integer.compare(p1[1], p2[1]);
+    public static class Solution1 {
+        /**
+         * Credit: https://discuss.leetcode.com/topic/60437/java-solution-using-priorityqueue-and-linkedlist
+         */
+        public int[][] reconstructQueue(int[][] people) {
+            Arrays.sort(people, new Comparator<int[]>() {
+                public int compare(int[] p1, int[] p2) {
+                    return p1[0] != p2[0] ? Integer.compare(p2[0], p1[0])
+                        : Integer.compare(p1[1], p2[1]);
+                }
+            });
+            List<int[]> list = new LinkedList();
+            for (int[] ppl : people) {
+                list.add(ppl[1], ppl);
             }
-        });
-        List<int[]> list = new LinkedList();
-        for (int[] ppl : people) {
-            list.add(ppl[1], ppl);
+            return list.toArray(new int[people.length][]);
         }
-        return list.toArray(new int[people.length][]);
     }
-
 }
