8e90ba09e6dd355172667381da308d8176806c5a
Fisher Coder <fishercoder@gmail.com>
2019-03-11 07:27:59 -0700

    refactor 410

--- src/test/java/com/fishercoder/_410Test.java
+++ src/test/java/com/fishercoder/_410Test.java
@@ -6,21 +6,18 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by stevesun on 6/6/17.
- */
 public class _410Test {
-    private static _410 test;
-    private static int[] nums;
+  private static _410.Solution1 test;
+  private static int[] nums;
 
-    @BeforeClass
-    public static void setup() {
-        test = new _410();
-    }
+  @BeforeClass
+  public static void setup() {
+    test = new _410.Solution1();
+  }
 
-    @Test
-    public void test1() {
-        nums = new int[]{7, 2, 5, 10, 8};
-        assertEquals(18, test.splitArray(nums, 2));
-    }
+  @Test
+  public void test1() {
+    nums = new int[] {7, 2, 5, 10, 8};
+    assertEquals(18, test.splitArray(nums, 2));
+  }
 }
