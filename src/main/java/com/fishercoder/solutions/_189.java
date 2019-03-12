bc3bcf16e6ba30f6be0e4f7fcfc720273336ef54
Jian GU <ycaxgjd@gmail.com>
2019-03-12 04:29:26 +0100

    Update _189.java (#40)

--- src/main/java/com/fishercoder/solutions/_189.java
+++ src/main/java/com/fishercoder/solutions/_189.java
@@ -77,4 +77,18 @@ public class _189 {
       }
     }
   }
+ 
+   public static class Solution3 {
+    public void rotate(int[] nums, int k) {
+        int tmp = 0;
+        for (int i = 0; i < k; i++) {
+            tmp = nums[nums.length - 1];
+            for (int j = nums.length - 1; j > 0; j--) {
+                nums[j] = nums[j-1];
+            }
+            nums[0] = tmp;
+        }
+    }
+  }
+ 
 }
