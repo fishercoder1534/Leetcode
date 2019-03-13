38753de7befbfcb8a74d0d7956d850ff0ada69b1
Fisher Coder <fishercoder@gmail.com>
2019-03-13 07:27:16 -0700

    refactor 412

--- src/main/java/com/fishercoder/solutions/_412.java
+++ src/main/java/com/fishercoder/solutions/_412.java
@@ -35,20 +35,21 @@ import java.util.List;
  ]*/
 public class _412 {
 
-    public List<String> fizzBuzz(int n) {
-        List<String> result = new ArrayList();
-        for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                result.add("_412");
-            } else if (i % 3 == 0) {
-                result.add("Fizz");
-            } else if (i % 5 == 0) {
-                result.add("Buzz");
-            } else {
-                result.add(Integer.toString(i));
+    public static class Solution1 {
+        public List<String> fizzBuzz(int n) {
+            List<String> result = new ArrayList();
+            for (int i = 1; i <= n; i++) {
+                if (i % 3 == 0 && i % 5 == 0) {
+                    result.add("_412");
+                } else if (i % 3 == 0) {
+                    result.add("Fizz");
+                } else if (i % 5 == 0) {
+                    result.add("Buzz");
+                } else {
+                    result.add(Integer.toString(i));
+                }
             }
+            return result;
         }
-        return result;
     }
-
 }
