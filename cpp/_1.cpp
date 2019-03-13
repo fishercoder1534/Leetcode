9e585968d0ed8f0b5c4b5ebc3ffa2eae454e2f39
Fisher Coder <fishercoder@gmail.com>
2019-03-12 21:40:00 -0700

    refactor 1

--- cpp/_1.cpp
+++ cpp/_1.cpp
@@ -23,7 +23,7 @@ public:
             if (m.find(nums[i]) == m.end() ) {
             // store the first one poisition into the second one's key
                 m[target - nums[i]] = i;
-            }else {
+            } else {
             // found the second one
                 result.push_back(m[nums[i]]);
                 result.push_back(i);
