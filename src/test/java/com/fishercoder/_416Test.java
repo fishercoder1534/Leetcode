7df616ba0b8ddada4355ef51c4b753bd9a7c74c4
Fisher Coder <fishercoder@gmail.com>
2019-03-17 08:05:21 -0700

    refactor 416

--- src/test/java/com/fishercoder/_416Test.java
+++ src/test/java/com/fishercoder/_416Test.java
@@ -6,34 +6,31 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by stevesun on 6/5/17.
- */
 public class _416Test {
-    private static _416 test;
+    private static _416.Solution1 solution1;
     private static int[] nums;
 
     @BeforeClass
     public static void setup() {
-        test = new _416();
+        solution1 = new _416.Solution1();
     }
 
     @Test
     public void test1() {
         nums = new int[]{1, 5, 11, 5};
-        assertEquals(true, test.canPartition(nums));
+        assertEquals(true, solution1.canPartition(nums));
     }
 
     @Test
     public void test2() {
         nums = new int[]{1, 2, 3, 5};
-        assertEquals(false, test.canPartition(nums));
+        assertEquals(false, solution1.canPartition(nums));
     }
 
     @Test
     public void test3() {
         nums = new int[]{1, 2, 3, 4, 5, 6, 7};
-        assertEquals(true, test.canPartition(nums));
+        assertEquals(true, solution1.canPartition(nums));
     }
 
 }
