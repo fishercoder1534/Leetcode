8b59f717ee3ea5653b2851c498c3b64cf76a4c84
Fisher Coder <fishercoder@gmail.com>
2019-03-22 15:24:29 -0700

    minor refactor

--- src/main/java/com/fishercoder/solutions/_420.java
+++ src/main/java/com/fishercoder/solutions/_420.java
@@ -10,6 +10,7 @@ package com.fishercoder.solutions;
  It must NOT contain three repeating characters in a row ("...aaa..." is weak, but "...aa...a..." is strong, assuming other conditions are met).
  Write a function strongPasswordChecker(s), that takes a string s as input, and return the MINIMUM
  change required to make s a strong password. If s is already strong, return 0.
+
  Insertion, deletion or replace of any one character are all considered as one change.
  */
 public class _420 {
