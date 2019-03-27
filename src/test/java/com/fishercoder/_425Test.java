639bc1a9a73f83834ecfe3b4e0a84ee887232c8a
Fisher Coder <fishercoder@gmail.com>
2019-03-27 07:36:49 -0700

    refactor 425

--- src/test/java/com/fishercoder/_425Test.java
+++ src/test/java/com/fishercoder/_425Test.java
@@ -1,26 +1,25 @@
 package com.fishercoder;
 
+import com.fishercoder.common.utils.CommonUtils;
 import com.fishercoder.solutions._425;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.List;
 
-/**
- * Created by stevesun on 6/3/17.
- */
 public class _425Test {
-    private static _425 test;
+    private static _425.Solution1 solution1;
     private static String[] words;
 
     @BeforeClass
     public static void setup() {
-        test = new _425();
+        solution1 = new _425.Solution1();
     }
 
     @Test
     public void test1() {
         words = new String[]{"area", "lead", "wall", "lady", "ball"};
-        List<List<String>> result = test.wordSquares(words);
+        List<List<String>> result = solution1.wordSquares(words);
+        CommonUtils.printListList(result);
     }
 }
