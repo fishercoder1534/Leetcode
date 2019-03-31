09b63cac230400a691e0577fc6ff8c8d26dabf46
Fisher Coder <fishercoder@gmail.com>
2019-03-31 07:05:17 -0700

    refactor 437

--- src/main/java/com/fishercoder/solutions/_437.java
+++ src/main/java/com/fishercoder/solutions/_437.java
@@ -3,6 +3,8 @@ package com.fishercoder.solutions;
 import com.fishercoder.common.classes.TreeNode;
 
 /**
+ * 437. Path Sum III
+ *
  * You are given a binary tree in which each node contains an integer value.
 
  Find the number of paths that sum to a given value.
@@ -32,18 +34,20 @@ import com.fishercoder.common.classes.TreeNode;
 
 public class _437 {
 
-    public int pathSum(TreeNode root, int sum) {
-        if (root == null) {
-            return 0;
+    public static class Solution1 {
+        public int pathSum(TreeNode root, int sum) {
+            if (root == null) {
+                return 0;
+            }
+            return pathSumFrom(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
         }
-        return pathSumFrom(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
-    }
 
-    private int pathSumFrom(TreeNode root, int sum) {
-        if (root == null) {
-            return 0;
+        private int pathSumFrom(TreeNode root, int sum) {
+            if (root == null) {
+                return 0;
+            }
+            return (root.val == sum ? 1 : 0) + pathSumFrom(root.left, sum - root.val) + pathSumFrom(root.right, sum - root.val);
         }
-        return (root.val == sum ? 1 : 0) + pathSumFrom(root.left, sum - root.val) + pathSumFrom(root.right, sum - root.val);
     }
 
 }
