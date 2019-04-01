c2cb4138a6088b80d4407222d62d9141e8f35f6f
Fisher Coder <fishercoder@gmail.com>
2019-04-01 08:21:14 -0700

    refactor 439

--- src/test/java/com/fishercoder/_439Test.java
+++ src/test/java/com/fishercoder/_439Test.java
@@ -10,41 +10,40 @@ import static org.junit.Assert.assertEquals;
  * Created by fishercoder on 5/18/17.
  */
 public class _439Test {
-    private static _439 test;
-    private static String expression;
-    private static String expected;
-    private static String actual;
+  private static _439.Solution1 solution1;
+  private static String expression;
+  private static String expected;
 
-    @BeforeClass
-    public static void setup() {
-        test = new _439();
-    }
+  @BeforeClass
+  public static void setup() {
+    solution1 = new _439.Solution1();
+  }
 
-    @Test
-    public void test1() {
-        expression = "T?2:3";
-        expected = "2";
-        assertEquals(expected, test.parseTernary(expression));
-    }
+  @Test
+  public void test1() {
+    expression = "T?2:3";
+    expected = "2";
+    assertEquals(expected, solution1.parseTernary(expression));
+  }
 
-    @Test
-    public void test2() {
-        expression = "F?1:T?4:5";
-        expected = "4";
-        assertEquals(expected, test.parseTernary(expression));
-    }
+  @Test
+  public void test2() {
+    expression = "F?1:T?4:5";
+    expected = "4";
+    assertEquals(expected, solution1.parseTernary(expression));
+  }
 
-    @Test
-    public void test3() {
-        expression = "T?T?F:5:3";
-        expected = "F";
-        assertEquals(expected, test.parseTernary(expression));
-    }
+  @Test
+  public void test3() {
+    expression = "T?T?F:5:3";
+    expected = "F";
+    assertEquals(expected, solution1.parseTernary(expression));
+  }
 
-    @Test
-    public void test4() {
-        expression = "T?T:F?T?1:2:F?3:4";
-        expected = "T";
-        assertEquals(expected, test.parseTernary(expression));
-    }
+  @Test
+  public void test4() {
+    expression = "T?T:F?T?1:2:F?3:4";
+    expected = "T";
+    assertEquals(expected, solution1.parseTernary(expression));
+  }
 }
