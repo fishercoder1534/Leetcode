c935ae3bdd7482391b43a9f8b74c86f866258639
Fisher Coder <fishercoder@gmail.com>
2019-04-03 07:50:13 -0700

    refactor 441

--- src/main/java/com/fishercoder/solutions/_441.java
+++ src/main/java/com/fishercoder/solutions/_441.java
@@ -1,6 +1,8 @@
 package com.fishercoder.solutions;
 
 /**
+ * 441. Arranging Coins
+ *
  * You have a total of n coins that you want to form in a staircase shape, where every k-th row must have exactly k coins.
 
  Given n, find the total number of full staircase rows that can be formed.
@@ -32,26 +34,24 @@ package com.fishercoder.solutions;
  Because the 4th row is incomplete, we return 3.*/
 public class _441 {
 
-    public static int arrangeCoins(int n) {
-        if (n < 2) {
-            return n;
+    public static class Solution1 {
+        public int arrangeCoins(int n) {
+            if (n < 2) {
+                return n;
+            }
+            int row = 0;
+            int count = 0;
+            long sum = 0;
+            while (sum < n) {
+                row += 1;
+                sum += row;
+                count++;
+            }
+            if (sum == n) {
+                return count;
+            }
+            return count - 1;
         }
-        int row = 0;
-        int count = 0;
-        long sum = 0;
-        while (sum < n) {
-            row += 1;
-            sum += row;
-            count++;
-        }
-        if (sum == n) {
-            return count;
-        }
-        return count - 1;
     }
 
-    public static void main(String... args) {
-        int n = 3;//should be 2
-        System.out.println(arrangeCoins(n));
-    }
 }
