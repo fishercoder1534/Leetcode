381a444fda2851d20599c34df26336e058949a11
Fisher Coder <fishercoder@gmail.com>
2019-04-04 07:54:15 -0700

    refactor 442

--- src/main/java/com/fishercoder/solutions/_442.java
+++ src/main/java/com/fishercoder/solutions/_442.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.Set;
 
 /**
+ * 442. Find All Duplicates in an Array
+ *
  * Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
 
  Find all the elements that appear twice in this array.
@@ -21,31 +23,35 @@ import java.util.Set;
  */
 public class _442 {
 
-    //O(n) space
-    //O(n) time
-    public List<Integer> findDuplicates_solution1(int[] nums) {
-        Set<Integer> set = new HashSet();
-        List<Integer> result = new ArrayList();
-        for (int i : nums) {
-            if (!set.add(i)) {
-                result.add(i);
+    public static class Solution1 {
+        //O(n) space
+        //O(n) time
+        public List<Integer> findDuplicates(int[] nums) {
+            Set<Integer> set = new HashSet();
+            List<Integer> result = new ArrayList();
+            for (int i : nums) {
+                if (!set.add(i)) {
+                    result.add(i);
+                }
             }
+            return result;
         }
-        return result;
     }
 
-    //O(1) space
-    //O(n) time
-    public List<Integer> findDuplicates_solution2(int[] nums) {
-        List<Integer> result = new ArrayList();
-        for (int i = 0; i < nums.length; i++) {
-            int index = Math.abs(nums[i]) - 1;
-            if (nums[index] < 0) {
-                result.add(Math.abs(index + 1));
+    public static class Solution2 {
+        //O(1) space
+        //O(n) time
+        public List<Integer> findDuplicates(int[] nums) {
+            List<Integer> result = new ArrayList();
+            for (int i = 0; i < nums.length; i++) {
+                int index = Math.abs(nums[i]) - 1;
+                if (nums[index] < 0) {
+                    result.add(Math.abs(index + 1));
+                }
+                nums[index] = -nums[index];
             }
-            nums[index] = -nums[index];
+            return result;
         }
-        return result;
     }
 
 }
