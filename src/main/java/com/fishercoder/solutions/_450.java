fb7064f8f8387eae373f191f1d7cb9776a35d572
Fisher Coder <fishercoder@gmail.com>
2019-04-12 08:23:02 -0700

    refactor 450

--- src/main/java/com/fishercoder/solutions/_450.java
+++ src/main/java/com/fishercoder/solutions/_450.java
@@ -44,42 +44,46 @@ import com.fishercoder.common.classes.TreeNode;
    4   7
  */
 public class _450 {
+    public static class Solution1 {
 
-    /**credit: https://discuss.leetcode.com/topic/65792/recursive-easy-to-understand-java-solution
-     * Steps:
-     1. Recursively find the node that has the same value as the key, while setting the left/right nodes equal to the returned subtree
-     2. Once the node is found, have to handle the below 4 cases
-        a. node doesn't have left or right - return null
-        b. node only has left subtree- return the left subtree
-        c. node only has right subtree- return the right subtree
-        d. node has both left and right - find the minimum value in the right subtree, set that value to the currently found node, then recursively delete the minimum value in the right subtree*/
-    public TreeNode deleteNode(TreeNode root, int key) {
-        if (root == null) {
+        /**
+         * credit: https://discuss.leetcode.com/topic/65792/recursive-easy-to-understand-java-solution
+         * Steps:
+         * 1. Recursively find the node that has the same value as the key, while setting the left/right nodes equal to the returned subtree
+         * 2. Once the node is found, have to handle the below 4 cases
+         * a. node doesn't have left or right - return null
+         * b. node only has left subtree- return the left subtree
+         * c. node only has right subtree- return the right subtree
+         * d. node has both left and right - find the minimum value in the right subtree, set that value to the currently found node, then recursively delete the minimum value in the right subtree
+         */
+        public TreeNode deleteNode(TreeNode root, int key) {
+            if (root == null) {
+                return root;
+            }
+            if (root.val > key) {
+                root.left = deleteNode(root.left, key);
+            } else if (root.val < key) {
+                root.right = deleteNode(root.right, key);
+            } else {
+                if (root.left == null) {
+                    return root.right;
+                } else if (root.right == null) {
+                    return root.left;
+                }
+
+                TreeNode minNode = getMin(root.right);
+                root.val = minNode.val;
+                root.right = deleteNode(root.right, root.val);
+            }
             return root;
         }
-        if (root.val > key) {
-            root.left = deleteNode(root.left, key);
-        } else if (root.val < key) {
-            root.right = deleteNode(root.right, key);
-        } else {
-            if (root.left == null) {
-                return root.right;
-            } else if (root.right == null) {
-                return root.left;
+
+        private TreeNode getMin(TreeNode node) {
+            while (node.left != null) {
+                node = node.left;
             }
-
-            TreeNode minNode = getMin(root.right);
-            root.val = minNode.val;
-            root.right = deleteNode(root.right, root.val);
+            return node;
         }
-        return root;
-    }
-
-    private TreeNode getMin(TreeNode node) {
-        while (node.left != null) {
-            node = node.left;
-        }
-        return node;
     }
 
 }
