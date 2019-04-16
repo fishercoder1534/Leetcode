7fdadb16fd109802bc93b98c3d4471aba353b2c1
Fisher Coder <fishercoder@gmail.com>
2019-04-16 07:53:51 -0700

    refactor 454

--- src/main/java/com/fishercoder/solutions/_454.java
+++ src/main/java/com/fishercoder/solutions/_454.java
@@ -3,9 +3,13 @@ package com.fishercoder.solutions;
 import java.util.HashMap;
 import java.util.Map;
 
-/**Given four lists A, B, C, D of integer values, compute how many tuples (i, j, k, l) there are such that A[i] + B[j] + C[k] + D[l] is zero.
-
- To make problem a bit easier, all A, B, C, D have same length of N where 0 ≤ N ≤ 500. All integers are in the range of -228 to 228 - 1 and the result is guaranteed to be at most 231 - 1.
+/**
+ * 454. 4Sum II
+ *
+ * Given four lists A, B, C, D of integer values,
+ * compute how many tuples (i, j, k, l) there are such that A[i] + B[j] + C[k] + D[l] is zero.
+ * To make problem a bit easier, all A, B, C, D have same length of N where 0 ≤ N ≤ 500.
+ * All integers are in the range of -228 to 228 - 1 and the result is guaranteed to be at most 231 - 1.
 
  Example:
 
@@ -25,30 +29,32 @@ import java.util.Map;
 
 public class _454 {
 
-    public static int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
-        Map<Integer, Integer> map = new HashMap();
-        int result = 0;
-        int len = A.length;
-        for (int i = 0; i < len; i++) {
-            for (int j = 0; j < len; j++) {
-                int sum = A[i] + B[j];
-                if (map.containsKey(sum)) {
-                    map.put(sum, map.get(sum) + 1);
-                } else {
-                    map.put(sum, 1);
+    public static class Solution1 {
+        public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+            Map<Integer, Integer> map = new HashMap();
+            int result = 0;
+            int len = A.length;
+            for (int i = 0; i < len; i++) {
+                for (int j = 0; j < len; j++) {
+                    int sum = A[i] + B[j];
+                    if (map.containsKey(sum)) {
+                        map.put(sum, map.get(sum) + 1);
+                    } else {
+                        map.put(sum, 1);
+                    }
                 }
             }
-        }
 
-        for (int i = 0; i < len; i++) {
-            for (int j = 0; j < len; j++) {
-                int sum = -(C[i] + D[j]);
-                if (map.containsKey(sum)) {
-                    result += map.get(sum);
+            for (int i = 0; i < len; i++) {
+                for (int j = 0; j < len; j++) {
+                    int sum = -(C[i] + D[j]);
+                    if (map.containsKey(sum)) {
+                        result += map.get(sum);
+                    }
                 }
             }
-        }
 
-        return result;
+            return result;
+        }
     }
 }
