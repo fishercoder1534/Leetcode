1cfd56cc8d444a498431c518dd60c162bb036fe0
Fisher Coder <fishercoder@gmail.com>
2019-04-16 07:53:51 -0700

    refactor 454

--- src/test/java/com/fishercoder/_454Test.java
+++ src/test/java/com/fishercoder/_454Test.java
@@ -1,14 +1,13 @@
 package com.fishercoder;
 
 import com.fishercoder.solutions._454;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
 
 public class _454Test {
-    private static _454 test;
+    private static _454.Solution1 solution1;
     private static int expected;
     private static int actual;
     private static int[] A;
@@ -18,28 +17,17 @@ public class _454Test {
 
     @BeforeClass
     public static void setup() {
-        test = new _454();
-    }
-
-    @Before
-    public void setupForEachTest() {
-        expected = 0;
-        actual = 0;
-        A = new int[1000];
-        B = new int[1000];
-        C = new int[1000];
-        D = new int[1000];
+        solution1 = new _454.Solution1();
     }
 
     @Test
     public void test1() {
-
         A = new int[]{1, 2};
         B = new int[]{-2, -1};
         C = new int[]{-1, 2};
         D = new int[]{0, 2};
         expected = 2;
-        actual = test.fourSumCount(A, B, C, D);
+        actual = solution1.fourSumCount(A, B, C, D);
         assertEquals(expected, actual);
 
     }
