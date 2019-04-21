0c191fe1346f2053a61be835b6a714d1de17483b
Fisher Coder <fishercoder@gmail.com>
2019-04-21 07:47:59 -0700

    refactor 461

--- src/main/java/com/fishercoder/solutions/_461.java
+++ src/main/java/com/fishercoder/solutions/_461.java
@@ -1,6 +1,8 @@
 package com.fishercoder.solutions;
 
 /**
+ * 461. Hamming Distance
+ *
  * The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
 
  Given two integers x and y, calculate the Hamming distance.
@@ -22,13 +24,15 @@ package com.fishercoder.solutions;
  The above arrows point to positions where the corresponding bits are different.
  */
 public class _461 {
-	public int hammingDistance(int x, int y) {
-		int n = x ^ y;
-		int count = 0;
-		while (n != 0) {
-			count++;
-			n &= (n - 1);
+	public static class Solution1 {
+		public int hammingDistance(int x, int y) {
+			int n = x ^ y;
+			int count = 0;
+			while (n != 0) {
+				count++;
+				n &= (n - 1);
+			}
+			return count;
 		}
-		return count;
 	}
 }
