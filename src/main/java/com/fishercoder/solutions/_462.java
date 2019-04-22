4a0ecd28a0354051a3bb7ea926fb24ff560fd04c
Fisher Coder <fishercoder@gmail.com>
2019-04-22 07:32:23 -0700

    refactor 462

--- src/main/java/com/fishercoder/solutions/_462.java
+++ src/main/java/com/fishercoder/solutions/_462.java
@@ -2,12 +2,14 @@ package com.fishercoder.solutions;
 
 import java.util.Arrays;
 
-/**462. Minimum Moves to Equal Array Elements II
+/**
+ * 462. Minimum Moves to Equal Array Elements II
+ *
  * Given a non-empty integer array,
  * find the minimum number of moves required to make all array elements equal,
  * where a move is incrementing a selected element by 1 or decrementing a selected element by 1.
-
- You may assume the array's length is at most 10,000.
+ *
+ * You may assume the array's length is at most 10,000.
 
  Example:
 
@@ -20,40 +22,37 @@ import java.util.Arrays;
  Explanation:
  Only two moves are needed (remember each move increments or decrements one element):
 
- [1,2,3]  =>  [2,2,3]  =>  [2,2,2]*/
+ [1,2,3]  =>  [2,2,3]  =>  [2,2,2]
+ */
 
 public class _462 {
 
-    public static int minMoves2(int[] nums) {
-        /**sort this array, find the median of this array as the pivot*/
-        Arrays.sort(nums);
-        int result = 0;
-        int result1 = 0;
-        if (nums.length % 2 != 0) {
-            int median = nums[nums.length / 2];
-            for (int n : nums) {
-                result += Math.abs(n - median);
+    public static class Solution1 {
+        public int minMoves2(int[] nums) {
+            /**sort this array, find the median of this array as the pivot*/
+            Arrays.sort(nums);
+            int result = 0;
+            int result1 = 0;
+            if (nums.length % 2 != 0) {
+                int median = nums[nums.length / 2];
+                for (int n : nums) {
+                    result += Math.abs(n - median);
+                }
+                return result;
+            } else {
+                int median1 = nums[nums.length / 2];
+                for (int n : nums) {
+                    result1 += Math.abs(n - median1);
+                }
+                int result2 = 0;
+                int median2 = nums[nums.length / 2 - 1];
+                for (int n : nums) {
+                    result2 += Math.abs(n - median2);
+                }
+                result1 = Math.min(result1, result2);
+                return result1;
             }
-            return result;
-        } else {
-            int median1 = nums[nums.length / 2];
-            for (int n : nums) {
-                result1 += Math.abs(n - median1);
-            }
-            int result2 = 0;
-            int median2 = nums[nums.length / 2 - 1];
-            for (int n : nums) {
-                result2 += Math.abs(n - median2);
-            }
-            result1 = Math.min(result1, result2);
-            return result1;
         }
-
-    }
-
-    public static void main(String... args) {
-        int[] nums = new int[]{1, 2, 3};
-        System.out.println(minMoves2(nums));
     }
 
 }
---
+++ src/test/java/com/fishercoder/_462Test.java
@@ -0,0 +1,22 @@
+package com.fishercoder;
+
+import com.fishercoder.solutions._462;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class _462Test {
+    private static _462.Solution1 solution1;
+
+    @BeforeClass
+    public static void setup() {
+        solution1 = new _462.Solution1();
+    }
+
+    @Test
+    public void test1() {
+        assertEquals(2, solution1.minMoves2(new int[]{1, 2, 3}));
+    }
+
+}
