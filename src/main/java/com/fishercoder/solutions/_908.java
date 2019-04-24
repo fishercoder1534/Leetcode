a3606535d834a2ecadc3550daa91e722e95cc504
Havan Agrawal <havanagrawal@gmail.com>
2019-04-24 07:36:37 -0700

    Improve solution for 908 (#48)

--- src/main/java/com/fishercoder/solutions/_908.java
+++ src/main/java/com/fishercoder/solutions/_908.java
@@ -37,17 +37,31 @@ import java.util.Arrays;
  * 0 <= K <= 10000
  */
 public class _908 {
-  public static class Solution1 {
-    public int smallestRangeI(int[] A, int K) {
-      Arrays.sort(A);
-      int smallestPlus = A[0] + K;
-      int biggestMinus = A[A.length - 1] - K;
-      int diff = biggestMinus - smallestPlus;
-      if (diff > 0) {
-        return diff;
-      } else {
-        return 0;
+    public static class Solution1 {
+        public int smallestRangeI(int[] A, int K) {
+          Arrays.sort(A);
+          int smallestPlus = A[0] + K;
+          int biggestMinus = A[A.length - 1] - K;
+          int diff = biggestMinus - smallestPlus;
+          if (diff > 0) {
+              return diff;
+          } else {
+              return 0;
+          }
       }
-    }
+  }
+
+  public static class Solution2 {
+      public int smallestRangeI(int[] A, int K) {
+          int min = A[0];
+          int max = A[0];
+
+          for (int k : A) {
+            min = Math.min(min, k);
+            max = Math.max(max, k);
+          }
+
+          return Math.max(max - min - 2*K, 0);
+      }
   }
 }
