928a60a7c02a06c00bbffd9d1ce955462c03533c
Fisher Coder <fishercoder@gmail.com>
2019-04-26 18:06:37 -0700

    fix build for 914

--- src/main/java/com/fishercoder/solutions/_914.java
+++ src/main/java/com/fishercoder/solutions/_914.java
@@ -4,10 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * 914. X of a Kind in a Deck of Cards
+ *
  * In a deck of cards, each card has an integer written on it.
  * Return true if and only if you can choose X >= 2 such that it is possible to split the entire deck into 1 or more groups of cards, where:
- *  Each group has exactly X cards.
- *  All the cards in each group have the same integer.
+ * Each group has exactly X cards.
+ * All the cards in each group have the same integer.
  *
  * Example 1:
  * Input: [1,2,3,4,4,3,2,1]
@@ -38,22 +40,24 @@ public class _914 {
     public static class Solution1 {
         public boolean hasGroupsSizeX(int[] deck) {
             //Size too small for partitions
-            if (deck.length < 2)
+            if (deck.length < 2) {
                 return false;
+            }
 
             //Track repetitions of values in deck array
             Map<Integer, Integer> mapReps = new HashMap<>();
             for (int card : deck) {
-                if (!mapReps.containsKey(card))
-                    mapReps.put(card,1);
-                else
-                    mapReps.put(card,mapReps.get(card)+1);
+                if (!mapReps.containsKey(card)) {
+                    mapReps.put(card, 1);
+                } else {
+                    mapReps.put(card, mapReps.get(card) + 1);
+                }
             }
 
             //Create array of map values
             int num = 0;
             int[] arrReps = new int[mapReps.size()];
-            for (Map.Entry<Integer,Integer> e : mapReps.entrySet()){
+            for (Map.Entry<Integer, Integer> e : mapReps.entrySet()) {
                 arrReps[num++] = e.getValue();
             }
 
@@ -64,14 +68,15 @@ public class _914 {
             return num > 1;
         }
 
-        private int gcd(int a, int b){
+        private int gcd(int a, int b) {
             return b == 0 ? a : gcd(b, a % b);
         }
 
-        private int arrGCD(int[] arr, int n){
+        private int arrGCD(int[] arr, int n) {
             int result = arr[0];
-            for (int i = 1; i < n; i++)
+            for (int i = 1; i < n; i++) {
                 result = gcd(arr[i], result);
+            }
 
             return result;
         }
