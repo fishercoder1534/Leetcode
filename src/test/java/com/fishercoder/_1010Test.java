958992abd33f39c5ce101bb3c74944ecf0ec46bf
Teresa C <31744254+teresachenec@users.noreply.github.com>
2019-04-26 20:22:07 -0400

    Renamed p1013 to 1010 (#54)

--- src/test/java/com/fishercoder/_1010Test.java
+++ src/test/java/com/fishercoder/_1010Test.java
@@ -1,18 +1,18 @@
 package com.fishercoder;
 
-import com.fishercoder.solutions._1013;
+import com.fishercoder.solutions._1010;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class _1013Test {
-    private static _1013.Solution1 solution1;
+public class _1010Test {
+    private static _1010.Solution1 solution1;
     private static int[] time;
 
     @BeforeClass
     public static void setup() {
-        solution1 = new _1013.Solution1();
+        solution1 = new _1010.Solution1();
     }
 
     @Test
