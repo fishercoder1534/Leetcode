c24e2646aad802eb5063aa3061211cf5b17cf6d7
Fisher Coder <fishercoder@gmail.com>
2019-05-01 15:58:23 -0700

    refactor 473

--- src/test/java/com/fishercoder/_473Test.java
+++ src/test/java/com/fishercoder/_473Test.java
@@ -10,17 +10,17 @@ import static org.junit.Assert.assertEquals;
  * Created by stevesun on 5/30/17.
  */
 public class _473Test {
-    private static _473 test;
+    private static _473.Solution1 solution1;
     private static int[] nums;
 
     @BeforeClass
     public static void setup() {
-        test = new _473();
+        solution1 = new _473.Solution1();
     }
 
     @Test
     public void test1() {
         nums = new int[]{1, 1, 2, 2, 2};
-        assertEquals(true, test.makesquare(nums));
+        assertEquals(true, solution1.makesquare(nums));
     }
 }
