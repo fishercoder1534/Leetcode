e4bbaaeb692873137d961a340386659ed75b5be2
Fisher Coder <fishercoder@gmail.com>
2019-05-04 07:40:56 -0700

    refactor 476

--- src/main/java/com/fishercoder/solutions/_476.java
+++ src/main/java/com/fishercoder/solutions/_476.java
@@ -1,6 +1,8 @@
 package com.fishercoder.solutions;
 
 /**
+ * 476. Number Complement
+ *
  * Given a positive integer, output its complement number. The complement strategy is to flip the bits of its binary representation.
 
  Note:
@@ -17,21 +19,25 @@ package com.fishercoder.solutions;
  */
 public class _476 {
 
-    public int findComplement_oneLiner(int num) {
-        return ~num & ((Integer.highestOneBit(num) << 1) - 1);
+    public static class Solution1 {
+        public int findComplement(int num) {
+            return ~num & ((Integer.highestOneBit(num) << 1) - 1);
+        }
     }
 
-    public int findComplement(int num) {
-        String str = Integer.toBinaryString(num);
-        StringBuilder sb = new StringBuilder();
-        char[] chars = str.toCharArray();
-        for (int i = 0; i < chars.length; i++) {
-            if (chars[i] == '0') {
-                sb.append("1");
-            } else {
-                sb.append("0");
+    public static class Solution2 {
+        public int findComplement(int num) {
+            String str = Integer.toBinaryString(num);
+            StringBuilder sb = new StringBuilder();
+            char[] chars = str.toCharArray();
+            for (int i = 0; i < chars.length; i++) {
+                if (chars[i] == '0') {
+                    sb.append("1");
+                } else {
+                    sb.append("0");
+                }
             }
+            return Integer.parseInt(sb.toString(), 2);
         }
-        return Integer.parseInt(sb.toString(), 2);
     }
 }
