16f3398eff6d0aa89e24c995ee252e9e03757720
Fisher Coder <fishercoder@gmail.com>
2019-05-07 09:12:30 -0700

    refactor 480

--- src/main/java/com/fishercoder/solutions/_480.java
+++ src/main/java/com/fishercoder/solutions/_480.java
@@ -35,69 +35,73 @@ import java.util.PriorityQueue;
  You may assume k is always valid, ie: 1 ≤ k ≤ input array's size for non-empty array.
  */
 public class _480 {
+    public static class Solution1 {
 
-    /**You cannot simply use minus sign '-' to denote the descending order, because e.g. 3 and -3 might both exist in this array,
-     * so we'll have to use the original numbers themselves to store in the heaps.*/
-    private PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-    private PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        /**
+         * You cannot simply use minus sign '-' to denote the descending order, because e.g. 3 and -3 might both exist in this array,
+         * so we'll have to use the original numbers themselves to store in the heaps.
+         */
+        private PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        private PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
 
-    public double[] medianSlidingWindow(int[] nums, int k) {
-        int n = nums.length - k + 1;
-        if (n <= 0) {
-            return new double[0];
-        }
-        double[] result = new double[n];
-
-        for (int i = 0; i <= nums.length; i++) {
-            if (i >= k) {
-                result[i - k] = getMedian();
-                remove(nums[i - k]);
+        public double[] medianSlidingWindow(int[] nums, int k) {
+            int n = nums.length - k + 1;
+            if (n <= 0) {
+                return new double[0];
             }
-            if (i < nums.length) {
-                add(nums[i]);
+            double[] result = new double[n];
+
+            for (int i = 0; i <= nums.length; i++) {
+                if (i >= k) {
+                    result[i - k] = getMedian();
+                    remove(nums[i - k]);
+                }
+                if (i < nums.length) {
+                    add(nums[i]);
+                }
+            }
+
+            return result;
+        }
+
+        private double getMedian() {
+            if (maxHeap.isEmpty() && minHeap.isEmpty()) {
+                return 0;
+            }
+
+            if (maxHeap.size() == minHeap.size()) {
+                return ((double) maxHeap.peek() + (double) minHeap.peek()) / 2.0;
+            } else {
+                return (double) minHeap.peek();
             }
         }
 
-        return result;
-    }
-
-    private double getMedian() {
-        if (maxHeap.isEmpty() && minHeap.isEmpty()) {
-            return 0;
+        private void remove(int num) {
+            if (num < getMedian()) {
+                maxHeap.remove(num);
+            } else {
+                minHeap.remove(num);
+            }
+            if (maxHeap.size() > minHeap.size()) {
+                minHeap.add(maxHeap.poll());
+            }
+            if (minHeap.size() - maxHeap.size() > 1) {
+                maxHeap.add(minHeap.poll());
+            }
         }
 
-        if (maxHeap.size() == minHeap.size()) {
-            return ((double)maxHeap.peek() + (double)minHeap.peek()) / 2.0;
-        } else {
-            return (double)minHeap.peek();
-        }
-    }
-
-    private void remove(int num) {
-        if (num < getMedian()) {
-            maxHeap.remove(num);
-        } else {
-            minHeap.remove(num);
-        }
-        if (maxHeap.size() > minHeap.size()) {
-            minHeap.add(maxHeap.poll());
-        }
-        if (minHeap.size() - maxHeap.size() > 1) {
-            maxHeap.add(minHeap.poll());
-        }
-    }
-
-    private void add(int num) {
-        if (num < getMedian()) {
-            maxHeap.add(num);
-        } else {
-            minHeap.add(num);
-        }
-        if (maxHeap.size() > minHeap.size()) {
-            minHeap.add(maxHeap.poll());
-        }
-        if (minHeap.size() - maxHeap.size() > 1) {
-            maxHeap.add(minHeap.poll());
+        private void add(int num) {
+            if (num < getMedian()) {
+                maxHeap.add(num);
+            } else {
+                minHeap.add(num);
+            }
+            if (maxHeap.size() > minHeap.size()) {
+                minHeap.add(maxHeap.poll());
+            }
+            if (minHeap.size() - maxHeap.size() > 1) {
+                maxHeap.add(minHeap.poll());
+            }
         }
     }
 
