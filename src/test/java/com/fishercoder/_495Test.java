ff59d5e7a4557bb17ce67bc4b6ad33ca7c2202f7
Fisher Coder <fishercoder@gmail.com>
2019-05-16 07:13:21 -0700

    refactor 495

--- src/test/java/com/fishercoder/_495Test.java
+++ src/test/java/com/fishercoder/_495Test.java
@@ -10,6 +10,7 @@ import static org.junit.Assert.assertEquals;
  * Created by fishercoder on 5/8/17.
  */
 public class _495Test {
+    _495.Solution1 solution1 = new _495.Solution1();
     private static int actual = 0;
     private static int expected = 0;
     private static int[] timeSeries;
@@ -25,30 +26,27 @@ public class _495Test {
 
     @Test
     public void test1() {
-        _495 test = new _495();
-        timeSeries = new int[]{1,4};
+        timeSeries = new int[]{1, 4};
         duration = 2;
-        actual = test.findPoisonedDuration(timeSeries, duration);
+        actual = solution1.findPoisonedDuration(timeSeries, duration);
         expected = 4;
         assertEquals(expected, actual);
     }
 
     @Test
     public void test2() {
-        _495 test = new _495();
-        timeSeries = new int[]{1,2};
+        timeSeries = new int[]{1, 2};
         duration = 2;
-        actual = test.findPoisonedDuration(timeSeries, duration);
+        actual = solution1.findPoisonedDuration(timeSeries, duration);
         expected = 3;
         assertEquals(expected, actual);
     }
 
     @Test
     public void test3() {
-        _495 test = new _495();
         timeSeries = new int[]{};
         duration = 100000;
-        actual = test.findPoisonedDuration(timeSeries, duration);
+        actual = solution1.findPoisonedDuration(timeSeries, duration);
         expected = 0;
         assertEquals(expected, actual);
     }
