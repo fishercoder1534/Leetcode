5ee00e14721f2b03be939aa616887270c9c7b9a4
Fisher Coder <fishercoder@gmail.com>
2019-05-18 09:42:26 -0700

    refactor 617

--- src/main/java/com/fishercoder/solutions/_617.java
+++ src/main/java/com/fishercoder/solutions/_617.java
@@ -39,28 +39,6 @@ public class _617 {
             if (t2 == null) {
                 return t1;
             }
-            TreeNode mergedNode = null;
-            if (t1 != null && t2 != null) {
-                mergedNode = new TreeNode(t1.val + t2.val);
-            } else if (t1 != null) {
-                mergedNode = t1;
-            } else if (t2 != null) {
-                mergedNode = t2;
-            }
-            mergedNode.left = mergeTrees(t1.left, t2.left);
-            mergedNode.right = mergeTrees(t1.right, t2.right);
-            return mergedNode;
-        }
-    }
-
-    public static class Solution2 {
-        public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
-            if (t1 == null) {
-                return t2;
-            }
-            if (t2 == null) {
-                return t1;
-            }
             TreeNode mergedNode = new TreeNode(t1.val + t2.val);
             mergedNode.left = mergeTrees(t1.left, t2.left);
             mergedNode.right = mergeTrees(t1.right, t2.right);
