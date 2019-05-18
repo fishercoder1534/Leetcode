a6f75dc9f157eb530a91c2cfa7b7bbc91447ab4e
Fisher Coder <fishercoder@gmail.com>
2019-05-18 11:02:11 -0700

    refactor 167

--- src/test/java/com/fishercoder/_167Test.java
+++ src/test/java/com/fishercoder/_167Test.java
@@ -7,19 +7,19 @@ import org.junit.Test;
 import static org.junit.Assert.assertArrayEquals;
 
 public class _167Test {
-  private static _167.Solution1 solution1;
-  private static int[] numbers;
-  private static int[] expected;
+    private static _167.Solution1 solution1;
+    private static int[] numbers;
+    private static int[] expected;
 
-  @BeforeClass
-  public static void setup() {
-    solution1 = new _167.Solution1();
-  }
+    @BeforeClass
+    public static void setup() {
+        solution1 = new _167.Solution1();
+    }
 
-  @Test
-  public void test1() {
-    numbers = new int[] {-3, 3, 4, 90};
-    expected = new int[] {1, 2};
-    assertArrayEquals(expected, solution1.twoSum(numbers, 0));
-  }
+    @Test
+    public void test1() {
+        numbers = new int[]{-3, 3, 4, 90};
+        expected = new int[]{1, 2};
+        assertArrayEquals(expected, solution1.twoSum(numbers, 0));
+    }
 }
