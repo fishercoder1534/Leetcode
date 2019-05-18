822de7fe29bc4aa062cc548520ef30b9aa29b8b0
Fisher Coder <fishercoder@gmail.com>
2019-05-18 11:35:36 -0700

    refactor 746

--- src/main/java/com/fishercoder/solutions/_746.java
+++ src/main/java/com/fishercoder/solutions/_746.java
@@ -27,7 +27,7 @@ public class _746 {
     public int minCostClimbingStairs(int[] cost) {
       int[] dp = new int[cost.length];
       dp[0] = cost[0];
-      dp[1] = Math.min(cost[1], cost[0] + cost[1]);
+      dp[1] = cost[1];
       for (int i = 2; i < cost.length; i++) {
         dp[i] = Math.min(dp[i - 1] + cost[i], dp[i - 2] + cost[i]);
       }
