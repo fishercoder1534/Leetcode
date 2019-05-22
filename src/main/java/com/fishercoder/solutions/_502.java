24ceed5971a06d74c1d8ffb3be01559f3ccdcba3
Fisher Coder <fishercoder@gmail.com>
2019-05-22 08:11:47 -0700

    refactor 502

--- src/main/java/com/fishercoder/solutions/_502.java
+++ src/main/java/com/fishercoder/solutions/_502.java
@@ -29,23 +29,27 @@ import java.util.PriorityQueue;
  */
 public class _502 {
 
-    /**credit: https://discuss.leetcode.com/topic/77768/very-simple-greedy-java-solution-using-two-priorityqueues*/
-    public int findMaximizedCapital(int k, int W, int[] Profits, int[] Capital) {
-        PriorityQueue<int[]> capitalHeap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        PriorityQueue<int[]> profitHeap = new PriorityQueue<>((a,b) -> b[1] - a[1]);
-        for (int i = 0; i < Profits.length; i++) {
-            capitalHeap.add(new int[]{Capital[i], Profits[i]});
-        }
-        while (k-- > 0) {
-            while (!capitalHeap.isEmpty() && capitalHeap.peek()[0] <= W) {
-                profitHeap.add(capitalHeap.poll());
+    public static class Solution1 {
+        /**
+         * credit: https://discuss.leetcode.com/topic/77768/very-simple-greedy-java-solution-using-two-priorityqueues
+         */
+        public int findMaximizedCapital(int k, int W, int[] Profits, int[] Capital) {
+            PriorityQueue<int[]> capitalHeap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+            PriorityQueue<int[]> profitHeap = new PriorityQueue<>((a, b) -> b[1] - a[1]);
+            for (int i = 0; i < Profits.length; i++) {
+                capitalHeap.add(new int[]{Capital[i], Profits[i]});
             }
-            if (profitHeap.isEmpty()) {
-                break;
+            while (k-- > 0) {
+                while (!capitalHeap.isEmpty() && capitalHeap.peek()[0] <= W) {
+                    profitHeap.add(capitalHeap.poll());
+                }
+                if (profitHeap.isEmpty()) {
+                    break;
+                }
+                W += profitHeap.poll()[1];
             }
-            W += profitHeap.poll()[1];
+            return W;
         }
-        return W;
     }
 
 }
--- src/test/java/com/fishercoder/_502Test.java
+++ src/test/java/com/fishercoder/_502Test.java
@@ -6,23 +6,20 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by stevesun on 6/2/17.
- */
 public class _502Test {
-    private static _502 test;
+    private static _502.Solution1 solution1;
     private static int[] Profits;
     private static int[] Capital;
 
     @BeforeClass
     public static void setup() {
-        test = new _502();
+        solution1 = new _502.Solution1();
     }
 
     @Test
     public void test1() {
         Profits = new int[]{1, 2, 3};
         Capital = new int[]{0, 1, 1};
-        assertEquals(4, test.findMaximizedCapital(2, 0, Profits, Capital));
+        assertEquals(4, solution1.findMaximizedCapital(2, 0, Profits, Capital));
     }
 }
