0c3f6518755b871b88e25a7a7be9c7345cdb325f
Fisher Coder <fishercoder@gmail.com>
2019-05-22 19:21:06 -0700

    fix build

--- src/main/java/com/fishercoder/solutions/_99999RandomQuestions.java
+++ src/main/java/com/fishercoder/solutions/_99999RandomQuestions.java
@@ -9,7 +9,12 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.*;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.TreeMap;
 
 public class _99999RandomQuestions {
 
