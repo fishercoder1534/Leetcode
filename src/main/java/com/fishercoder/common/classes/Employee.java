9aab5e04f57283481bfc235ccfd7c7540328718a
Fisher Coder <fishercoder@gmail.com>
2019-06-07 15:21:55 -0700

    minor refactor

--- src/main/java/com/fishercoder/common/classes/Employee.java
+++ src/main/java/com/fishercoder/common/classes/Employee.java
@@ -2,16 +2,19 @@ package com.fishercoder.common.classes;
 
 import java.util.List;
 
-/**
- * Created by stevesun on 9/29/17.
- */
 public class Employee {
-    // It's the unique id of each node;
-    // unique id of this employee
+    /**
+     * It's the unique id of each node;
+     * unique id of this employee
+     */
     public int id;
-    // the importance value of this employee
+    /**
+     * the importance value of this employee
+     */
     public int importance;
-    // the id of direct subordinates
+    /**
+     * the id of direct subordinates
+     */
     public List<Integer> subordinates;
 
     public Employee(int id, int importance, List<Integer> subordinates) {
