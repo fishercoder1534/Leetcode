fc7014370c48f7c5133aa20ee9babffc9dc9ff2d
Fisher Coder <fishercoder@gmail.com>
2019-06-09 12:51:01 -0700

    refactor 47

---
+++ src/test/java/com/fishercoder/_47Test.java
@@ -0,0 +1,25 @@
+package com.fishercoder;
+
+import com.fishercoder.common.utils.CommonUtils;
+import com.fishercoder.solutions._47;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.List;
+
+public class _47Test {
+    private static _47.Solution1 solution1;
+    private static List<List<Integer>> actual;
+
+    @BeforeClass
+    public static void setup() {
+        solution1 = new _47.Solution1();
+    }
+
+    @Test
+    public void test1() {
+        actual = solution1.permuteUnique(new int[]{1, 1, 2});
+        CommonUtils.printListList(actual);
+    }
+
+}
