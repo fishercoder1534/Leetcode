5cebad40a7e408f661536188c09d66d5650eb3f9
Fisher Coder <fishercoder@gmail.com>
2019-06-11 06:10:33 -0700

    refactor 525

--- src/test/java/com/fishercoder/_525Test.java
+++ src/test/java/com/fishercoder/_525Test.java
@@ -1,5 +1,6 @@
 package com.fishercoder;
 
+import com.fishercoder.solutions._525;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -7,14 +8,14 @@ import org.junit.Test;
 import static junit.framework.Assert.assertEquals;
 
 public class _525Test {
-    private static com.fishercoder.solutions._525 test;
+    private static _525.Solution1 solution1;
     private static int expected;
     private static int actual;
     private static int[] nums;
 
     @BeforeClass
     public static void setup() {
-        test = new com.fishercoder.solutions._525();
+        solution1 = new _525.Solution1();
     }
 
     @Before
@@ -25,7 +26,7 @@ public class _525Test {
     public void test1() {
         nums = new int[]{0, 1};
         expected = 2;
-        actual = test.findMaxLength(nums);
+        actual = solution1.findMaxLength(nums);
         assertEquals(expected, actual);
     }
 
@@ -33,7 +34,7 @@ public class _525Test {
     public void test2() {
         nums = new int[]{0, 1, 0};
         expected = 2;
-        actual = test.findMaxLength(nums);
+        actual = solution1.findMaxLength(nums);
         assertEquals(expected, actual);
     }
 
@@ -41,7 +42,7 @@ public class _525Test {
     public void test3() {
         nums = new int[]{0, 0, 1, 0, 0, 0, 1, 1};
         expected = 6;
-        actual = test.findMaxLength(nums);
+        actual = solution1.findMaxLength(nums);
         assertEquals(expected, actual);
     }
 
