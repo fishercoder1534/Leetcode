d159d7de9dfdb837544b441d15e8e3754fc6da6a
Fisher Coder <fishercoder@gmail.com>
2019-06-16 09:55:58 -0700

    refactor 533

--- src/test/java/com/fishercoder/_533Test.java
+++ src/test/java/com/fishercoder/_533Test.java
@@ -6,16 +6,13 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by fishercoder on 5/25/17.
- */
 public class _533Test {
-    private static _533 test;
+    private static _533.Solution1 solution1;
     private static char[][] picture;
 
     @BeforeClass
     public static void setup() {
-        test = new _533();
+        solution1 = new _533.Solution1();
     }
 
     @Test
@@ -25,12 +22,12 @@ public class _533Test {
                 {'W', 'B', 'W', 'B', 'B', 'W'},
                 {'W', 'B', 'W', 'B', 'B', 'W'},
                 {'W', 'W', 'B', 'W', 'B', 'W'}};
-        assertEquals(6, test.findBlackPixel(picture, 3));
+        assertEquals(6, solution1.findBlackPixel(picture, 3));
     }
 
     @Test
     public void test2() {
         picture = new char[][]{{'B'}};
-        assertEquals(1, test.findBlackPixel(picture, 1));
+        assertEquals(1, solution1.findBlackPixel(picture, 1));
     }
 }
