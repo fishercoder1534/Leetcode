abb25fdb48779b2bdaf691ed22897ea71d1fd4f4
Fisher Coder <fishercoder@gmail.com>
2019-06-25 07:38:23 -0700

    refactor 551

--- src/main/java/com/fishercoder/solutions/_551.java
+++ src/main/java/com/fishercoder/solutions/_551.java
@@ -1,6 +1,8 @@
 package com.fishercoder.solutions;
 
 /**
+ * 551. Student Attendance Record I
+ *
  * You are given a string representing an attendance record for a student. The record only contains the following three characters:
 
  'A' : Absent.
@@ -21,27 +23,29 @@ package com.fishercoder.solutions;
  */
 public class _551 {
 
-    public boolean checkRecord(String s) {
-        int aCount = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if ( s.charAt(i) == 'A') {
-                aCount++;
-                if (aCount > 1) {
-                    return false;
-                }
-            } else if (s.charAt(i) == 'L') {
-                int continuousLCount = 0;
-                while (i < s.length() && s.charAt(i) == 'L') {
-                    i++;
-                    continuousLCount++;
-                    if (continuousLCount > 2) {
+    public static class Solution1 {
+        public boolean checkRecord(String s) {
+            int aCount = 0;
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == 'A') {
+                    aCount++;
+                    if (aCount > 1) {
                         return false;
                     }
+                } else if (s.charAt(i) == 'L') {
+                    int continuousLCount = 0;
+                    while (i < s.length() && s.charAt(i) == 'L') {
+                        i++;
+                        continuousLCount++;
+                        if (continuousLCount > 2) {
+                            return false;
+                        }
+                    }
+                    i--;
                 }
-                i--;
             }
+            return true;
         }
-        return true;
     }
 
 }
--- src/test/java/com/fishercoder/_551Test.java
+++ src/test/java/com/fishercoder/_551Test.java
@@ -1,25 +1,20 @@
 package com.fishercoder;
 
 import com.fishercoder.solutions._551;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
 
 public class _551Test {
-    private static _551 test;
+    private static _551.Solution1 test;
     private static boolean expected;
     private static boolean actual;
     private static String s;
 
     @BeforeClass
     public static void setup() {
-        test = new _551();
-    }
-
-    @Before
-    public void setupForEachTest() {
+        test = new _551.Solution1();
     }
 
     @Test
