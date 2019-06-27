1dbc03fd40ecbaca1e53de557f17d765f08a2e7a
Fisher Coder <fishercoder@gmail.com>
2019-06-27 07:23:56 -0700

    refactor 553

--- src/test/java/com/fishercoder/_553Test.java
+++ src/test/java/com/fishercoder/_553Test.java
@@ -10,23 +10,23 @@ import static org.junit.Assert.assertEquals;
  * Created by fishercoder on 5/25/17.
  */
 public class _553Test {
-    private static _553 test;
+    private static _553.Solution1 solution1;
     private static int[] nums;
 
     @BeforeClass
     public static void setup() {
-        test = new _553();
+        solution1 = new _553.Solution1();
     }
 
     @Test
     public void test1() {
         nums = new int[]{1000, 100, 10, 2};
-        assertEquals("1000/(100/10/2)", test.optimalDivision(nums));
+        assertEquals("1000/(100/10/2)", solution1.optimalDivision(nums));
     }
 
     @Test
     public void test2() {
         nums = new int[]{1000, 100, 40, 10, 2};
-        assertEquals("1000/(100/40/10/2)", test.optimalDivision(nums));
+        assertEquals("1000/(100/40/10/2)", solution1.optimalDivision(nums));
     }
 }
