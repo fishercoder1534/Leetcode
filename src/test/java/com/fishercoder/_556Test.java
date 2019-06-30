6f9c5b9d6a0c9b10940c8678442d436b61db7619
Fisher Coder <fishercoder@gmail.com>
2019-06-30 09:33:17 -0700

    refactor 556

--- src/test/java/com/fishercoder/_556Test.java
+++ src/test/java/com/fishercoder/_556Test.java
@@ -1,7 +1,6 @@
 package com.fishercoder;
 
 import com.fishercoder.solutions._556;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -9,25 +8,21 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class _556Test {
-    private static _556 test;
+    private static _556.Solution1 solution1;
     private static int n;
     private static int expected;
     private static int actual;
 
     @BeforeClass
     public static void setup() {
-        test = new _556();
-    }
-
-    @Before
-    public void setupForEachTest() {
+        solution1 = new _556.Solution1();
     }
 
     @Test
     public void test1() {
         n = 12;
         expected = 21;
-        actual = test.nextGreaterElement(n);
+        actual = solution1.nextGreaterElement(n);
         assertEquals(expected, actual);
     }
 
@@ -35,7 +30,7 @@ public class _556Test {
     public void test2() {
         n = 21;
         expected = -1;
-        actual = test.nextGreaterElement(n);
+        actual = solution1.nextGreaterElement(n);
         assertEquals(expected, actual);
         assertTrue(Integer.MAX_VALUE > 1999999999);
     }
@@ -44,7 +39,7 @@ public class _556Test {
     public void test3() {
         n = 1999999999;
         expected = -1;
-        actual = test.nextGreaterElement(n);
+        actual = solution1.nextGreaterElement(n);
         assertEquals(expected, actual);
     }
 
@@ -52,7 +47,7 @@ public class _556Test {
     public void test4() {
         n = 12222333;
         expected = 12223233;
-        actual = test.nextGreaterElement(n);
+        actual = solution1.nextGreaterElement(n);
         assertEquals(expected, actual);
     }
 
@@ -60,7 +55,7 @@ public class _556Test {
     public void test5() {
         n = 12443322;
         expected = 13222344;
-        actual = test.nextGreaterElement(n);
+        actual = solution1.nextGreaterElement(n);
         assertEquals(expected, actual);
     }
 
