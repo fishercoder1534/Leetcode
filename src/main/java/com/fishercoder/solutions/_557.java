e5f030b2ae47b32ecd2ad51bc018650256af4653
Fisher Coder <fishercoder@gmail.com>
2019-07-01 07:17:07 -0700

    refactor 557

--- src/main/java/com/fishercoder/solutions/_557.java
+++ src/main/java/com/fishercoder/solutions/_557.java
@@ -12,17 +12,18 @@ package com.fishercoder.solutions;
  */
 public class _557 {
 
-    public String reverseWords(String s) {
-        StringBuilder stringBuilder = new StringBuilder();
-        StringBuilder sbUtils = new StringBuilder();
-        for (String word : s.split(" ")) {
-            sbUtils.setLength(0);
-            sbUtils.append(word);
-            stringBuilder.append(sbUtils.reverse().toString());
-            stringBuilder.append(" ");
+    public static class Solution1 {
+        public String reverseWords(String s) {
+            StringBuilder stringBuilder = new StringBuilder();
+            StringBuilder sbUtils = new StringBuilder();
+            for (String word : s.split(" ")) {
+                sbUtils.setLength(0);
+                sbUtils.append(word);
+                stringBuilder.append(sbUtils.reverse().toString());
+                stringBuilder.append(" ");
+            }
+            stringBuilder.setLength(stringBuilder.length() - 1);
+            return stringBuilder.toString();
         }
-        stringBuilder.setLength(stringBuilder.length() - 1);
-        return stringBuilder.toString();
     }
-
 }
