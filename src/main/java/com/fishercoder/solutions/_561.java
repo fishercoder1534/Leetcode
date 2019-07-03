0e360df1739603e2902ce6fd08c9a987ea9c5f6e
Fisher Coder <fishercoder@gmail.com>
2019-07-03 09:19:00 -0700

    refactor 561

--- src/main/java/com/fishercoder/solutions/_561.java
+++ src/main/java/com/fishercoder/solutions/_561.java
@@ -24,13 +24,15 @@ import java.util.Arrays;
  All the integers in the array will be in the range of [-10000, 10000].*/
 public class _561 {
 
-    public int arrayPairSum(int[] nums) {
-        Arrays.sort(nums);
-        int sum = 0;
-        for (int i = 0; i < nums.length; i += 2) {
-            sum += nums[i];
+    public static class Solution1 {
+        public int arrayPairSum(int[] nums) {
+            Arrays.sort(nums);
+            int sum = 0;
+            for (int i = 0; i < nums.length; i += 2) {
+                sum += nums[i];
+            }
+            return sum;
         }
-        return sum;
     }
 
 }
--- src/test/java/com/fishercoder/_561Test.java
+++ src/test/java/com/fishercoder/_561Test.java
@@ -10,21 +10,21 @@ import static junit.framework.Assert.assertEquals;
  * Created by fishercoder on 4/23/17.
  */
 public class _561Test {
-    private static _561 test;
+    private static _561.Solution1 solution1;
     private static int expected;
     private static int actual;
     private static int[] nums;
 
     @BeforeClass
     public static void setup() {
-        test = new _561();
+        solution1 = new _561.Solution1();
     }
 
     @Test
     public void test1() {
         nums = new int[]{1, 4, 3, 2};
         expected = 4;
-        actual = test.arrayPairSum(nums);
+        actual = solution1.arrayPairSum(nums);
         assertEquals(expected, actual);
     }
 
