53c1e8de98ac0da7000869e7bbe1d91d3fd85f17
Fisher Coder <fishercoder@gmail.com>
2019-07-07 07:11:15 -0700

    refactor 565

--- src/main/java/com/fishercoder/solutions/_565.java
+++ src/main/java/com/fishercoder/solutions/_565.java
@@ -28,23 +28,25 @@ package com.fishercoder.solutions;
  */
 public class _565 {
 
-    public int arrayNesting(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-        boolean[] visited = new boolean[nums.length];
-        int answer = 0;
-        for (int i : nums) {
-            int count = 0;
-            int j = i;
-            while (j >= 0 && j < nums.length && !visited[j]) {
-                count++;
-                visited[j] = true;
-                j = nums[j];
+    public static class Solution1 {
+        public int arrayNesting(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return 0;
             }
-            answer = Math.max(answer, count);
+            boolean[] visited = new boolean[nums.length];
+            int answer = 0;
+            for (int i : nums) {
+                int count = 0;
+                int j = i;
+                while (j >= 0 && j < nums.length && !visited[j]) {
+                    count++;
+                    visited[j] = true;
+                    j = nums[j];
+                }
+                answer = Math.max(answer, count);
+            }
+            return answer;
         }
-        return answer;
     }
 
 }
