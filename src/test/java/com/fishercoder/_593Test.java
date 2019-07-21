12be2ffc284c08ca3e8d2cf3607c717cc74f7058
Fisher Coder <fishercoder@gmail.com>
2019-07-21 08:01:10 -0700

    refactor 593

--- src/test/java/com/fishercoder/_593Test.java
+++ src/test/java/com/fishercoder/_593Test.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.assertEquals;
  * Created by fishercoder on 5/22/17.
  */
 public class _593Test {
-    private static _593 test;
+    private static _593.Solution1 solution1;
     private static int[] p1;
     private static int[] p2;
     private static int[] p3;
@@ -18,7 +18,7 @@ public class _593Test {
 
     @BeforeClass
     public static void setup() {
-        test = new _593();
+        solution1 = new _593.Solution1();
     }
 
     @Test
@@ -27,7 +27,7 @@ public class _593Test {
         p2 = new int[]{1, 1};
         p3 = new int[]{1, 0};
         p4 = new int[]{0, 1};
-        assertEquals(true, test.validSquare(p1, p2, p3, p4));
+        assertEquals(true, solution1.validSquare(p1, p2, p3, p4));
     }
 
     @Test
@@ -36,7 +36,7 @@ public class _593Test {
         p2 = new int[]{5, 3};
         p3 = new int[]{3, 5};
         p4 = new int[]{7, 7};
-        assertEquals(false, test.validSquare(p1, p2, p3, p4));
+        assertEquals(false, solution1.validSquare(p1, p2, p3, p4));
     }
 
     @Test
@@ -45,7 +45,7 @@ public class _593Test {
         p2 = new int[]{0, 0};
         p3 = new int[]{0, 0};
         p4 = new int[]{0, 0};
-        System.out.println(test.noDuplicate(p1, p2, p3, p4));
-        assertEquals(false, test.validSquare(p1, p2, p3, p4));
+        System.out.println(solution1.noDuplicate(p1, p2, p3, p4));
+        assertEquals(false, solution1.validSquare(p1, p2, p3, p4));
     }
 }
