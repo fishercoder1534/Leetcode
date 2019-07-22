c7acf34002f0c5af513242063a186d0259a23a03
Fisher Coder <fishercoder@gmail.com>
2019-07-22 07:20:15 -0700

    refactor 594

--- src/main/java/com/fishercoder/solutions/_594.java
+++ src/main/java/com/fishercoder/solutions/_594.java
@@ -19,18 +19,19 @@ import java.util.Map;
 
  */
 public class _594 {
-    public int findLHS(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i : nums) {
-            map.put(i, map.getOrDefault(i, 0) + 1);
-        }
-        int max = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i] + 1)) {
-                max = Math.max(max, map.get(nums[i]) + map.get(nums[i] + 1));
+    public static class Solution1 {
+        public int findLHS(int[] nums) {
+            Map<Integer, Integer> map = new HashMap<>();
+            for (int i : nums) {
+                map.put(i, map.getOrDefault(i, 0) + 1);
             }
+            int max = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (map.containsKey(nums[i] + 1)) {
+                    max = Math.max(max, map.get(nums[i]) + map.get(nums[i] + 1));
+                }
+            }
+            return max;
         }
-        return max;
     }
-
 }
--- src/test/java/com/fishercoder/_594Test.java
+++ src/test/java/com/fishercoder/_594Test.java
@@ -10,17 +10,17 @@ import static org.junit.Assert.assertEquals;
  * Created by fishercoder on 5/20/17.
  */
 public class _594Test {
-    private static _594 test;
+    private static _594.Solution1 solution1;
     private static int[] nums;
 
     @BeforeClass
     public static void setup() {
-        test = new _594();
+        solution1 = new _594.Solution1();
     }
 
     @Test
     public void test1() {
         nums = new int[]{1, 3, 2, 2, 5, 2, 3, 7};
-        assertEquals(5, test.findLHS(nums));
+        assertEquals(5, solution1.findLHS(nums));
     }
 }
