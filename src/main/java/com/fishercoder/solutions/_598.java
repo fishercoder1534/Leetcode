72e6a3fe493590e7cc62bafffab73cdb475bc9fd
Fisher Coder <fishercoder@gmail.com>
2019-07-25 07:14:32 -0700

    refactor 598

--- src/main/java/com/fishercoder/solutions/_598.java
+++ src/main/java/com/fishercoder/solutions/_598.java
@@ -41,13 +41,15 @@ package com.fishercoder.solutions;
 
 public class _598 {
 
-    public int maxCount(int m, int n, int[][] ops) {
-        int x = m;
-        int y = n;
-        for (int[] op : ops) {
-            x = Math.min(x, op[0]);
-            y = Math.min(y, op[1]);
+    public static class Solution1 {
+        public int maxCount(int m, int n, int[][] ops) {
+            int x = m;
+            int y = n;
+            for (int[] op : ops) {
+                x = Math.min(x, op[0]);
+                y = Math.min(y, op[1]);
+            }
+            return x * y;
         }
-        return x * y;
     }
 }
