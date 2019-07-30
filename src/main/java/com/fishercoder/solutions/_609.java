8cf9fd3ed47ddf84cd9d5a72499d117e2260077c
Fisher Coder <fishercoder@gmail.com>
2019-07-30 07:05:51 -0700

    refactor 609

--- src/main/java/com/fishercoder/solutions/_609.java
+++ src/main/java/com/fishercoder/solutions/_609.java
@@ -52,32 +52,34 @@ import java.util.Map;
  */
 public class _609 {
 
-    public List<List<String>> findDuplicate(String[] paths) {
-        Map<String, List<String>> map = new HashMap<>();//key is the file content, value is the list of directories that has this directory/file
-        for (String path : paths) {
-            String[] dirAndFiles = path.split(" ");
-            for (int i = 1; i < dirAndFiles.length; i++) {
-                String content = dirAndFiles[i].substring(dirAndFiles[i].indexOf("(") + 1, dirAndFiles[i].indexOf(")"));
-                if (!map.containsKey(content)) {
-                    map.put(content, new ArrayList<>());
+    public static class Solution1 {
+        public List<List<String>> findDuplicate(String[] paths) {
+            Map<String, List<String>> map = new HashMap<>();//key is the file content, value is the list of directories that has this directory/file
+            for (String path : paths) {
+                String[] dirAndFiles = path.split(" ");
+                for (int i = 1; i < dirAndFiles.length; i++) {
+                    String content = dirAndFiles[i].substring(dirAndFiles[i].indexOf("(") + 1, dirAndFiles[i].indexOf(")"));
+                    if (!map.containsKey(content)) {
+                        map.put(content, new ArrayList<>());
+                    }
+                    List<String> dirs = map.get(content);
+                    dirs.add(dirAndFiles[0] + "/" + dirAndFiles[i].substring(0, dirAndFiles[i].indexOf("(")));
+                    map.put(content, dirs);
                 }
-                List<String> dirs = map.get(content);
-                dirs.add(dirAndFiles[0] + "/" + dirAndFiles[i].substring(0, dirAndFiles[i].indexOf("(")));
-                map.put(content, dirs);
             }
-        }
 
-        List<List<String>> result = new ArrayList<>();
-        for (String content : map.keySet()) {
-            if (map.get(content).size() > 1) {
-                List<String> dupFile = new ArrayList<>();
-                for (String dir : map.get(content)) {
-                    dupFile.add(dir);
+            List<List<String>> result = new ArrayList<>();
+            for (String content : map.keySet()) {
+                if (map.get(content).size() > 1) {
+                    List<String> dupFile = new ArrayList<>();
+                    for (String dir : map.get(content)) {
+                        dupFile.add(dir);
+                    }
+                    result.add(dupFile);
                 }
-                result.add(dupFile);
             }
+            return result;
         }
-        return result;
     }
 
     /**Answers to follow-up questions:
--- src/test/java/com/fishercoder/_609Test.java
+++ src/test/java/com/fishercoder/_609Test.java
@@ -8,19 +8,19 @@ import org.junit.Test;
 import java.util.List;
 
 public class _609Test {
-    private static _609 test;
+    private static _609.Solution1 solution1;
     private static String[] paths;
     private static List<List<String>> actual;
 
     @BeforeClass
     public static void setup() {
-        test = new _609();
+        solution1 = new _609.Solution1();
     }
 
     @Test
     public void test1() {
         paths = new String[]{"root/a 1.txt(abcd) 2.txt(efgh)", "root/c 3.txt(abcd)", "root/c/d 4.txt(efgh)", "root 4.txt(efgh)"};
-        actual = test.findDuplicate(paths);
+        actual = solution1.findDuplicate(paths);
         CommonUtils.printListList(actual);
     }
 }
