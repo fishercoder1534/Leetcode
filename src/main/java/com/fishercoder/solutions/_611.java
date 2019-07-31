05b44192b974cc8fb0998d595363a307515fb1a7
Fisher Coder <fishercoder@gmail.com>
2019-07-31 10:08:44 -0700

    refactor 611

--- src/main/java/com/fishercoder/solutions/_611.java
+++ src/main/java/com/fishercoder/solutions/_611.java
@@ -23,28 +23,32 @@ import java.util.Arrays;
  The integers in the given array are in the range of [0, 1000].
  */
 public class _611 {
-    /**Rule: among three sides, we need to find whether the longest of the three is smaller than the sum of the two shorter one.
-     * If so, then these three could form a valid triangle.*/
+    public static class Solution1 {
+        /**
+         * Rule: among three sides, we need to find whether the longest of the three is smaller than the sum of the two shorter one.
+         * If so, then these three could form a valid triangle.
+         */
 
-    public int triangleNumber(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-        Arrays.sort(nums);
-        int triplets = 0;
-        for (int i = 2; i < nums.length; i++) {
-            int left = 0;
-            int right = i - 1;
-            while (left < right) {
-                if (nums[i] < nums[left] + nums[right]) {
-                    triplets += (right - left);
-                    right--;
-                } else {
-                    left++;
+        public int triangleNumber(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return 0;
+            }
+            Arrays.sort(nums);
+            int triplets = 0;
+            for (int i = 2; i < nums.length; i++) {
+                int left = 0;
+                int right = i - 1;
+                while (left < right) {
+                    if (nums[i] < nums[left] + nums[right]) {
+                        triplets += (right - left);
+                        right--;
+                    } else {
+                        left++;
+                    }
                 }
             }
+            return triplets;
         }
-        return triplets;
     }
 
 }
