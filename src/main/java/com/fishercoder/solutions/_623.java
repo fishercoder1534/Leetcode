066f232675579315ce1aa34b0877b07b60748c1f
Fisher Coder <fishercoder@gmail.com>
2019-08-03 07:03:54 -0700

    refactor 623

--- src/main/java/com/fishercoder/solutions/_623.java
+++ src/main/java/com/fishercoder/solutions/_623.java
@@ -70,31 +70,34 @@ import com.fishercoder.common.classes.TreeNode;
  */
 public class _623 {
 
-    public TreeNode addOneRow(TreeNode root, int v, int d) {
-        if (d == 1) {
-            TreeNode newRoot = new TreeNode(v);
-            newRoot.left = root;
-            return newRoot;
-        } else {
-            dfs(root, v, d);
-            return root;
-        }
-    }
+    public static class Solution1 {
 
-    private void dfs(TreeNode root, int v, int d) {
-        if (root == null) {
-            return;
+        public TreeNode addOneRow(TreeNode root, int v, int d) {
+            if (d == 1) {
+                TreeNode newRoot = new TreeNode(v);
+                newRoot.left = root;
+                return newRoot;
+            } else {
+                dfs(root, v, d);
+                return root;
+            }
         }
-        if (d == 2) {
-            TreeNode newLeft = new TreeNode(v);
-            TreeNode newRight = new TreeNode(v);
-            newLeft.left = root.left;
-            newRight.right = root.right;
-            root.left = newLeft;
-            root.right = newRight;
-        } else {
-            dfs(root.left, v, d - 1);
-            dfs(root.right, v, d - 1);
+
+        private void dfs(TreeNode root, int v, int d) {
+            if (root == null) {
+                return;
+            }
+            if (d == 2) {
+                TreeNode newLeft = new TreeNode(v);
+                TreeNode newRight = new TreeNode(v);
+                newLeft.left = root.left;
+                newRight.right = root.right;
+                root.left = newLeft;
+                root.right = newRight;
+            } else {
+                dfs(root.left, v, d - 1);
+                dfs(root.right, v, d - 1);
+            }
         }
     }
 
