6a9e0432a920b03d9b5983417b77101109b3b17b
Fisher Coder <fishercoder@gmail.com>
2019-08-04 07:52:57 -0700

    refactor 624

--- src/main/java/com/fishercoder/solutions/_624.java
+++ src/main/java/com/fishercoder/solutions/_624.java
@@ -31,18 +31,19 @@ import java.util.List;
  */
 public class _624 {
 
-    public int maxDistance(int[][] arrays) {
-        List<Integer> max = new ArrayList<>();
-        for (int[] array : arrays) {
-            max.add(array[array.length - 1]);
+    public static class Solution1 {
+        public int maxDistance(int[][] arrays) {
+            List<Integer> max = new ArrayList<>();
+            for (int[] array : arrays) {
+                max.add(array[array.length - 1]);
+            }
+            Collections.sort(max);
+            int ans = Integer.MIN_VALUE;
+            for (int[] array : arrays) {
+                int big = array[array.length - 1] == max.get(max.size() - 1) ? max.get(max.size() - 2) : max.get(max.size() - 1);
+                ans = Math.max(ans, big - array[0]);
+            }
+            return ans;
         }
-        Collections.sort(max);
-        int ans = Integer.MIN_VALUE;
-        for (int[] array : arrays) {
-            int big = array[array.length - 1] == max.get(max.size() - 1) ? max.get(max.size() - 2) : max.get(max.size() - 1);
-            ans = Math.max(ans, big - array[0]);
-        }
-        return ans;
     }
-
 }
