34c07bcedc2c93aa38d4a404411eba3fbb782db5
Fisher Coder <fishercoder@gmail.com>
2019-08-09 06:16:01 -0700

    refactor 630

--- src/main/java/com/fishercoder/solutions/_630.java
+++ src/main/java/com/fishercoder/solutions/_630.java
@@ -28,32 +28,23 @@ import java.util.PriorityQueue;
  You can't take two courses simultaneously.
  */
 public class _630 {
-    /**Reference: https://discuss.leetcode.com/topic/93790/short-java-code-using-priorityqueue
-     * Sort by finish date!!! This is greedy! We should take those classes that finish early first.*/
-    public int scheduleCourse(int[][] courses) {
-        Arrays.sort(courses, (a,b) -> a[1] - b[1]);
-        int day = 0;
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
-        for (int[] course : courses) {
-            day += course[0];
-            maxHeap.offer(course[0]);
-            if (day > course[1]) {
-                day -= maxHeap.poll();//drop the previous courses that took the most time
+    public static class Solution1 {
+        /**
+         * Reference: https://discuss.leetcode.com/topic/93790/short-java-code-using-priorityqueue
+         * Sort by finish date!!! This is greedy! We should take those classes that finish early first.
+         */
+        public int scheduleCourse(int[][] courses) {
+            Arrays.sort(courses, (a, b) -> a[1] - b[1]);
+            int day = 0;
+            PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+            for (int[] course : courses) {
+                day += course[0];
+                maxHeap.offer(course[0]);
+                if (day > course[1]) {
+                    day -= maxHeap.poll();//drop the previous courses that took the most time
+                }
             }
+            return maxHeap.size();
         }
-        return maxHeap.size();
-    }
-
-    public static void main(String... args) {
-        _630 test = new _630();
-        int[][] courses = new int[][]{
-                {100, 200},
-                {200, 1300},
-                {1000, 1250},
-                {2000, 3200},
-                {300, 1200}
-        };
-        test.scheduleCourse(courses);
-        System.out.println("Finished..");
     }
 }
