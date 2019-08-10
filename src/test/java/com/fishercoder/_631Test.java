5eba67c647994bb97242f40748dcecf783aa91fa
Fisher Coder <fishercoder@gmail.com>
2019-08-10 07:35:22 -0700

    refactor 631

--- src/test/java/com/fishercoder/_631Test.java
+++ src/test/java/com/fishercoder/_631Test.java
@@ -6,11 +6,11 @@ import org.junit.Test;
 import static junit.framework.TestCase.assertEquals;
 
 public class _631Test {
-    private static _631.Excel excel;
+    private static _631.Solution1.Excel excel;
 
     @Test
     public void test1() {
-        excel = new _631.Excel(3, 'C');
+        excel = new _631.Solution1.Excel(3, 'C');
         assertEquals(0, excel.get(1, 'A'));
         excel.set(1, 'A', 1);
         assertEquals(1, excel.get(1, 'A'));
@@ -18,7 +18,7 @@ public class _631Test {
 
     @Test
     public void test2() {
-        excel = new _631.Excel(3, 'C');
+        excel = new _631.Solution1.Excel(3, 'C');
         assertEquals(0, excel.sum(1, 'A', new String[]{"A2"}));
         excel.set(2, 'A', 1);
         assertEquals(1, excel.get(1, 'A'));
