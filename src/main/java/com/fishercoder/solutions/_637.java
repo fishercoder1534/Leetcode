9ed098f12fc400bd359769f70df7dd83720cbc5f
Fisher Coder <fishercoder@gmail.com>
2019-08-16 07:54:20 -0700

    refactor 637

--- src/main/java/com/fishercoder/solutions/_637.java
+++ src/main/java/com/fishercoder/solutions/_637.java
@@ -29,31 +29,33 @@ import java.util.Queue;
  */
 public class _637 {
 
-    public List<Double> averageOfLevels(TreeNode root) {
-        List<Double> result = new ArrayList<>();
-        if (root == null) {
+    public static class Solution1 {
+        public List<Double> averageOfLevels(TreeNode root) {
+            List<Double> result = new ArrayList<>();
+            if (root == null) {
+                return result;
+            }
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                double sum = 0.0;
+                for (int i = 0; i < size; i++) {
+                    TreeNode curr = queue.poll();
+                    if (curr != null) {
+                        sum += curr.val;
+                    }
+                    if (curr.left != null) {
+                        queue.offer(curr.left);
+                    }
+                    if (curr.right != null) {
+                        queue.offer(curr.right);
+                    }
+                }
+                result.add(sum / size);
+            }
             return result;
         }
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            double sum = 0.0;
-            for (int i = 0; i < size; i++) {
-                TreeNode curr = queue.poll();
-                if (curr != null) {
-                    sum += curr.val;
-                }
-                if (curr.left != null) {
-                    queue.offer(curr.left);
-                }
-                if (curr.right != null) {
-                    queue.offer(curr.right);
-                }
-            }
-            result.add(sum / size);
-        }
-        return result;
     }
 
 }
