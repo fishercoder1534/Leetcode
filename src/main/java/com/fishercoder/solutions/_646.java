f21b14873589189d051db555f5522b2fdafa9055
Fisher Coder <fishercoder@gmail.com>
2019-08-23 06:58:34 -0700

    refactor 646

--- src/main/java/com/fishercoder/solutions/_646.java
+++ src/main/java/com/fishercoder/solutions/_646.java
@@ -21,24 +21,26 @@ import java.util.Arrays;
  */
 public class _646 {
 
-    /**
-     * credit: https://discuss.leetcode.com/topic/96804/java-o-nlog-n-time-o-1-space
-     */
-    public int findLongestChain(int[][] pairs) {
-        Arrays.sort(pairs, (o1, o2) -> o1[1] - o2[1]);
-        int result = 0;
-        int n = pairs.length;
-        int i = -1;
-        while (++i < n) {
-            result++;
-            int curEnd = pairs[i][1];
-            while (i + 1 < n && pairs[i + 1][0] <= curEnd) {
-                /**This means, we'll keep incrementing i until pairs[i+1][0] is
-                 * exactly greater than curEnd.*/
-                i++;
+    public static class Solution1 {
+        /**
+         * credit: https://discuss.leetcode.com/topic/96804/java-o-nlog-n-time-o-1-space
+         */
+        public int findLongestChain(int[][] pairs) {
+            Arrays.sort(pairs, (o1, o2) -> o1[1] - o2[1]);
+            int result = 0;
+            int n = pairs.length;
+            int i = -1;
+            while (++i < n) {
+                result++;
+                int curEnd = pairs[i][1];
+                while (i + 1 < n && pairs[i + 1][0] <= curEnd) {
+                    /**This means, we'll keep incrementing i until pairs[i+1][0] is
+                     * exactly greater than curEnd.*/
+                    i++;
+                }
             }
+            return result;
         }
-        return result;
     }
 
 }
--- src/test/java/com/fishercoder/_646Test.java
+++ src/test/java/com/fishercoder/_646Test.java
@@ -8,38 +8,38 @@ import static org.junit.Assert.assertEquals;
 
 public class _646Test {
 
-    private static _646 test;
+    private static _646.Solution1 solution1;
     private static int[][] pairs;
 
     @BeforeClass
     public static void setup() {
-        test = new _646();
+        solution1 = new _646.Solution1();
     }
 
     @Test
     public void test1() {
         pairs = new int[][]{
-                {1,2},
-                {2,3},
-                {5,6},
-                {3,4}
+                {1, 2},
+                {2, 3},
+                {5, 6},
+                {3, 4}
         };
-        assertEquals(3, test.findLongestChain(pairs));
+        assertEquals(3, solution1.findLongestChain(pairs));
     }
 
     @Test
     public void test2() {
         pairs = new int[][]{
-                {9,10},
-                {-9,9},
-                {-6,1},
-                {-4,1},
-                {8,10},
-                {7,10},
-                {9,10},
-                {2,10}
+                {9, 10},
+                {-9, 9},
+                {-6, 1},
+                {-4, 1},
+                {8, 10},
+                {7, 10},
+                {9, 10},
+                {2, 10}
         };
-        assertEquals(2, test.findLongestChain(pairs));
+        assertEquals(2, solution1.findLongestChain(pairs));
     }
 
 }
