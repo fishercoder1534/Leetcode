23e60edbe229743156acd31c889e4b4fad78e5ba
Fisher Coder <fishercoder@gmail.com>
2019-08-25 07:53:52 -0700

    refactor 649

--- src/main/java/com/fishercoder/solutions/_649.java
+++ src/main/java/com/fishercoder/solutions/_649.java
@@ -51,27 +51,29 @@ import java.util.Queue;
  */
 public class _649 {
 
-    public String predictPartyVictory(String senate) {
-        Queue<Integer> radiantQ = new LinkedList<>();
-        Queue<Integer> direQ = new LinkedList<>();
-        int len = senate.length();
-        for (int i = 0; i < len; i++) {
-            if (senate.charAt(i) == 'R') {
-                radiantQ.offer(i);
-            } else {
-                direQ.offer(i);
+    public static class Solution1 {
+        public String predictPartyVictory(String senate) {
+            Queue<Integer> radiantQ = new LinkedList<>();
+            Queue<Integer> direQ = new LinkedList<>();
+            int len = senate.length();
+            for (int i = 0; i < len; i++) {
+                if (senate.charAt(i) == 'R') {
+                    radiantQ.offer(i);
+                } else {
+                    direQ.offer(i);
+                }
             }
-        }
-        while (!radiantQ.isEmpty() && !direQ.isEmpty()) {
-            int radiantIndex = radiantQ.poll();
-            int direIndex = direQ.poll();
-            if (radiantIndex < direIndex) {
-                /**Radiant will ban Dire in this case, so we'll add radiant index back to the queue plus n*/
-                radiantQ.offer(radiantIndex + len);
-            } else {
-                direQ.offer(direIndex + len);
+            while (!radiantQ.isEmpty() && !direQ.isEmpty()) {
+                int radiantIndex = radiantQ.poll();
+                int direIndex = direQ.poll();
+                if (radiantIndex < direIndex) {
+                    /**Radiant will ban Dire in this case, so we'll add radiant index back to the queue plus n*/
+                    radiantQ.offer(radiantIndex + len);
+                } else {
+                    direQ.offer(direIndex + len);
+                }
             }
+            return radiantQ.isEmpty() ? "Dire" : "Radiant";
         }
-        return radiantQ.isEmpty() ? "Dire" : "Radiant";
     }
 }
--- src/test/java/com/fishercoder/_649Test.java
+++ src/test/java/com/fishercoder/_649Test.java
@@ -10,45 +10,45 @@ import static org.junit.Assert.assertEquals;
  * Created by fishercoder on 5/8/17.
  */
 public class _649Test {
-    private static _649 test;
+    private static _649.Solution1 solution1;
 
     @BeforeClass
     public static void setup() {
-        test = new _649();
+        solution1 = new _649.Solution1();
     }
 
     @Test
     public void test1() {
-        assertEquals("Dire", test.predictPartyVictory("RDDDR"));
+        assertEquals("Dire", solution1.predictPartyVictory("RDDDR"));
     }
 
     @Test
     public void test2() {
-        assertEquals("Radiant", test.predictPartyVictory("RD"));
+        assertEquals("Radiant", solution1.predictPartyVictory("RD"));
     }
 
     @Test
     public void test3() {
-        assertEquals("Dire", test.predictPartyVictory("RDD"));
+        assertEquals("Dire", solution1.predictPartyVictory("RDD"));
     }
 
     @Test
     public void test4() {
-        assertEquals("Radiant", test.predictPartyVictory("RDDR"));
+        assertEquals("Radiant", solution1.predictPartyVictory("RDDR"));
     }
 
     @Test
     public void test5() {
-        assertEquals("Dire", test.predictPartyVictory("RDDRD"));
+        assertEquals("Dire", solution1.predictPartyVictory("RDDRD"));
     }
 
     @Test
     public void test6() {
-        assertEquals("Dire", test.predictPartyVictory("RDDDDDRR"));
+        assertEquals("Dire", solution1.predictPartyVictory("RDDDDDRR"));
     }
 
     @Test
     public void test7() {
-        assertEquals("Dire", test.predictPartyVictory("RDDDDRR"));
+        assertEquals("Dire", solution1.predictPartyVictory("RDDDDRR"));
     }
 }
