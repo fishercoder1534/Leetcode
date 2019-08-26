955b4d9a1a573322720bdc75aa96f88766fd1465
Fisher Coder <fishercoder@gmail.com>
2019-08-26 07:44:52 -0700

    refactor 650

--- src/test/java/com/fishercoder/_650Test.java
+++ src/test/java/com/fishercoder/_650Test.java
@@ -7,35 +7,35 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class _650Test {
-    private static _650 test;
+    private static _650.Solution1 solution1;
 
     @BeforeClass
     public static void setup() {
-        test = new _650();
+        solution1 = new _650.Solution1();
     }
 
     @Test
     public void test1() {
-        assertEquals(3, test.minSteps(3));
+        assertEquals(3, solution1.minSteps(3));
     }
 
     @Test
     public void test2() {
-        assertEquals(9, test.minSteps(20));
+        assertEquals(9, solution1.minSteps(20));
     }
 
     @Test
     public void test3() {
-        assertEquals(19, test.minSteps(19));
+        assertEquals(19, solution1.minSteps(19));
     }
 
     @Test
     public void test4() {
-        assertEquals(0, test.minSteps(1));
+        assertEquals(0, solution1.minSteps(1));
     }
 
     @Test
     public void test5() {
-        assertEquals(35, test.minSteps(741));
+        assertEquals(35, solution1.minSteps(741));
     }
 }
