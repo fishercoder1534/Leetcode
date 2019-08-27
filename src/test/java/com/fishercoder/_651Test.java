50f9186526dbfd2be0fef56c5f2650cd7a1be420
Fisher Coder <fishercoder@gmail.com>
2019-08-27 07:12:11 -0700

    refactor 651

--- src/test/java/com/fishercoder/_651Test.java
+++ src/test/java/com/fishercoder/_651Test.java
@@ -7,40 +7,40 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class _651Test {
-    private static _651 test;
+    private static _651.Solution1 solution1;
 
     @BeforeClass
     public static void setup() {
-        test = new _651();
+        solution1 = new _651.Solution1();
     }
 
     @Test
     public void test1() {
-        assertEquals(3, test.maxA(3));
+        assertEquals(3, solution1.maxA(3));
     }
 
     @Test
     public void test2() {
-        assertEquals(324, test.maxA(20));
+        assertEquals(324, solution1.maxA(20));
     }
 
     @Test
     public void test3() {
-        assertEquals(256, test.maxA(19));
+        assertEquals(256, solution1.maxA(19));
     }
 
     @Test
     public void test4() {
-        assertEquals(1, test.maxA(1));
+        assertEquals(1, solution1.maxA(1));
     }
 
     @Test
     public void test5() {
-        assertEquals(1327104, test.maxA(50));
+        assertEquals(1327104, solution1.maxA(50));
     }
 
     @Test
     public void test6() {
-        assertEquals(9, test.maxA(7));
+        assertEquals(9, solution1.maxA(7));
     }
 }
