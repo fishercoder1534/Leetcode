36183c841fbbd45e0baad0b63e2d514189affd4d
Fisher Coder <fishercoder@gmail.com>
2019-08-31 08:30:49 -0700

    refactor 656

--- src/test/java/com/fishercoder/_656Test.java
+++ src/test/java/com/fishercoder/_656Test.java
@@ -14,19 +14,19 @@ import static org.junit.Assert.assertEquals;
  * Created by fishercoder on 5/25/17.
  */
 public class _656Test {
-    private static _656 test;
+    private static _656.Solution1 solution1;
     private static int[] A;
     private static List<Integer> expected;
 
     @BeforeClass
     public static void setup() {
-        test = new _656();
+        solution1 = new _656.Solution1();
     }
 
     @Test
     public void test1() {
         A = new int[]{1, 2, 4, -1, 2};
         expected = new ArrayList<>(Arrays.asList(1, 3, 5));
-        assertEquals(expected, test.cheapestJump(A, 2));
+        assertEquals(expected, solution1.cheapestJump(A, 2));
     }
 }
