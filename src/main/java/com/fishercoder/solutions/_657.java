1b7a1fde5f338ef9135f75c0f745d6b9f72002b6
Fisher Coder <fishercoder@gmail.com>
2019-09-01 08:18:10 -0700

    refactor 657

--- src/main/java/com/fishercoder/solutions/_657.java
+++ src/main/java/com/fishercoder/solutions/_657.java
@@ -22,20 +22,22 @@ package com.fishercoder.solutions;
  */
 
 public class _657 {
-    public boolean judgeCircle(String moves) {
-        int hori = 0;
-        int verti = 0;
-        for (char c : moves.toCharArray()) {
-            if (c == 'U') {
-                verti++;
-            } else if (c == 'D') {
-                verti--;
-            } else if (c == 'L') {
-                hori--;
-            } else if (c == 'R') {
-                hori++;
+    public static class Solution1 {
+        public boolean judgeCircle(String moves) {
+            int hori = 0;
+            int verti = 0;
+            for (char c : moves.toCharArray()) {
+                if (c == 'U') {
+                    verti++;
+                } else if (c == 'D') {
+                    verti--;
+                } else if (c == 'L') {
+                    hori--;
+                } else if (c == 'R') {
+                    hori++;
+                }
             }
+            return verti == 0 && hori == 0;
         }
-        return verti == 0 && hori == 0;
     }
 }
