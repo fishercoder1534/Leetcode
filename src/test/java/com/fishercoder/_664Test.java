6cb67241dffe336812036141fef1c5a85b1a07de
Fisher Coder <fishercoder@gmail.com>
2019-09-05 06:22:38 -0700

    refactor 664

--- src/test/java/com/fishercoder/_664Test.java
+++ src/test/java/com/fishercoder/_664Test.java
@@ -7,21 +7,21 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class _664Test {
-    private static _664 test;
+    private static _664.Solution1 solution1;
 
     @BeforeClass
     public static void setup() {
-        test = new _664();
+        solution1 = new _664.Solution1();
     }
 
     @Test
     public void test1() {
-        assertEquals(2, test.strangePrinter("aaabbb"));
+        assertEquals(2, solution1.strangePrinter("aaabbb"));
     }
 
     @Test
     public void test2() {
-        assertEquals(2, test.strangePrinter("aba"));
+        assertEquals(2, solution1.strangePrinter("aba"));
     }
 
 }
