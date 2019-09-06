6b063821ee67440d3117e4d07d21dc7471319413
Fisher Coder <fishercoder@gmail.com>
2019-09-06 07:34:52 -0700

    refactor 695

--- src/main/java/com/fishercoder/solutions/_695.java
+++ src/main/java/com/fishercoder/solutions/_695.java
@@ -32,35 +32,37 @@ package com.fishercoder.solutions;
 
 public class _695 {
 
-    public int maxAreaOfIsland(int[][] grid) {
-        if (grid == null || grid.length == 0) {
-            return 0;
-        }
-        int m = grid.length;
-        int n = grid[0].length;
-        int max = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {
-                    int area = dfs(grid, i, j, m, n, 0);
-                    max = Math.max(area, max);
+    public static class Solution1 {
+        public int maxAreaOfIsland(int[][] grid) {
+            if (grid == null || grid.length == 0) {
+                return 0;
+            }
+            int m = grid.length;
+            int n = grid[0].length;
+            int max = 0;
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (grid[i][j] == 1) {
+                        int area = dfs(grid, i, j, m, n, 0);
+                        max = Math.max(area, max);
+                    }
                 }
             }
+            return max;
         }
-        return max;
-    }
 
-    int dfs(int[][] grid, int i, int j, int m, int n, int area) {
-        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0) {
+        int dfs(int[][] grid, int i, int j, int m, int n, int area) {
+            if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0) {
+                return area;
+            }
+            grid[i][j] = 0;
+            area++;
+            area = dfs(grid, i + 1, j, m, n, area);
+            area = dfs(grid, i, j + 1, m, n, area);
+            area = dfs(grid, i - 1, j, m, n, area);
+            area = dfs(grid, i, j - 1, m, n, area);
             return area;
         }
-        grid[i][j] = 0;
-        area++;
-        area = dfs(grid, i + 1, j, m, n, area);
-        area = dfs(grid, i, j + 1, m, n, area);
-        area = dfs(grid, i - 1, j, m, n, area);
-        area = dfs(grid, i, j - 1, m, n, area);
-        return area;
     }
 
 }
--- src/test/java/com/fishercoder/_695Test.java
+++ src/test/java/com/fishercoder/_695Test.java
@@ -7,12 +7,12 @@ import org.junit.Test;
 import static junit.framework.TestCase.assertEquals;
 
 public class _695Test {
-    private static _695 test;
+    private static _695.Solution1 solution1;
     private static int[][] grid;
 
     @Before
     public void setup() {
-        test = new _695();
+        solution1 = new _695.Solution1();
     }
 
     @Test
@@ -27,7 +27,7 @@ public class _695Test {
                 {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
                 {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}
         };
-        assertEquals(6, test.maxAreaOfIsland(grid));
+        assertEquals(6, solution1.maxAreaOfIsland(grid));
     }
 
 }
