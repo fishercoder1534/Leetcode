0644f0c465a86020838132e20a7788f9cc305441
Fisher Coder <fishercoder@gmail.com>
2019-09-18 07:21:04 -0700

    refactor 5083

--- src/main/java/com/fishercoder/solutions/_5083.java
+++ src/main/java/com/fishercoder/solutions/_5083.java
@@ -1,44 +1,40 @@
 package com.fishercoder.solutions;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * 5083. Occurrences After Bigram
- *
+ * <p>
  * Given words first and second, consider occurrences in some text of the form "first second third",
  * where second comes immediately after first, and third comes immediately after second.
  * For each such occurrence, add "third" to the answer, and return the answer.
- *
+ * <p>
  * Example 1:
  * Input: text = "alice is a good girl she is a good student", first = "a", second = "good"
  * Output: ["girl","student"]
- *
+ * <p>
  * Example 2:
  * Input: text = "we will we will rock you", first = "we", second = "will"
  * Output: ["we","rock"]
- *
+ * <p>
  * Note:
  * 1 <= text.length <= 1000
  * text consists of space separated words, where each word consists of lowercase English letters.
  * 1 <= first.length, second.length <= 10
  * first and second consist of lowercase English letters.
- * */
+ */
 public class _5083 {
     public static class Solution1 {
         public String[] findOcurrences(String text, String first, String second) {
-            List<String> result = new ArrayList<>();
             String[] words = text.split(" ");
-            for (int i = 0; i < words.length - 2; i++) {
-                if (words[i].equals(first) && words[i + 1].equals(second)) {
-                    result.add(words[i + 2]);
-                }
-            }
-            String[] occ = new String[result.size()];
-            for (int i = 0; i < result.size(); i++) {
-                occ[i] = result.get(i);
-            }
-            return occ;
+            return IntStream
+                    .range(0, words.length - 2)
+                    .filter(i -> words[i].equals(first) && words[i + 1].equals(second))
+                    .mapToObj(i -> words[i + 2])
+                    .collect(Collectors.toList())
+                    .stream()
+                    .toArray(String[]::new);
         }
     }
 }
