0dd40b3d29ab609f4ca5763373f144937e27a1bf
Fisher Coder <fishercoder@gmail.com>
2019-09-25 06:57:47 -0700

    refactor 1

--- src/main/java/com/fishercoder/solutions/_1.java
+++ src/main/java/com/fishercoder/solutions/_1.java
@@ -8,6 +8,7 @@ import java.util.Map;
  *
  * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
  * Example:
  * Given nums = [2, 7, 11, 15], target = 9,
  * Because nums[0] + nums[1] = 2 + 7 = 9,
