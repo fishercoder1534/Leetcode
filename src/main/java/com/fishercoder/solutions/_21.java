a229b588220474a93f1fde9ca0f91ac3151ce3bf
Fisher Coder <fishercoder@gmail.com>
2019-10-07 07:28:42 -0700

    refactor 21

--- src/main/java/com/fishercoder/solutions/_21.java
+++ src/main/java/com/fishercoder/solutions/_21.java
@@ -9,20 +9,21 @@ import com.fishercoder.common.classes.ListNode;
  * The new list should be made by splicing together the nodes of the first two lists.*/
 
 public class _21 {
-
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null) {
-            return l2;
-        }
-        if (l2 == null) {
-            return l1;
-        }
-        if (l1.val < l2.val) {
-            l1.next = mergeTwoLists(l1.next, l2);
-            return l1;
-        } else {
-            l2.next = mergeTwoLists(l1, l2.next);
-            return l2;
+    public static class Solution1 {
+        public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+            if (l1 == null) {
+                return l2;
+            }
+            if (l2 == null) {
+                return l1;
+            }
+            if (l1.val < l2.val) {
+                l1.next = mergeTwoLists(l1.next, l2);
+                return l1;
+            } else {
+                l2.next = mergeTwoLists(l1, l2.next);
+                return l2;
+            }
         }
     }
 
