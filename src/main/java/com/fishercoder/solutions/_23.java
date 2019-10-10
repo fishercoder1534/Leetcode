cabf5ae0e2f956827280e61c2d03c75e99af90fe
Fisher Coder <fishercoder@gmail.com>
2019-10-10 07:45:29 -0700

    refactor 23

--- src/main/java/com/fishercoder/solutions/_23.java
+++ src/main/java/com/fishercoder/solutions/_23.java
@@ -12,27 +12,28 @@ import java.util.PriorityQueue;
  * Analyze and describe its complexity.*/
 
 public class _23 {
+    public static class Solution1 {
+        public ListNode mergeKLists(ListNode[] lists) {
+            PriorityQueue<ListNode> heap = new PriorityQueue((Comparator<ListNode>) (o1, o2) -> o1.val - o2.val);
 
-    public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<ListNode> heap = new PriorityQueue((Comparator<ListNode>) (o1, o2) -> o1.val - o2.val);
-
-        for (ListNode node : lists) {
-            if (node != null) {
-                heap.offer(node);
+            for (ListNode node : lists) {
+                if (node != null) {
+                    heap.offer(node);
+                }
             }
-        }
 
-        ListNode pre = new ListNode(-1);
-        ListNode temp = pre;
-        while (!heap.isEmpty()) {
-            ListNode curr = heap.poll();
-            temp.next = new ListNode(curr.val);
-            if (curr.next != null) {
-                heap.offer(curr.next);
+            ListNode pre = new ListNode(-1);
+            ListNode temp = pre;
+            while (!heap.isEmpty()) {
+                ListNode curr = heap.poll();
+                temp.next = new ListNode(curr.val);
+                if (curr.next != null) {
+                    heap.offer(curr.next);
+                }
+                temp = temp.next;
             }
-            temp = temp.next;
+            return pre.next;
         }
-        return pre.next;
     }
 
 }
