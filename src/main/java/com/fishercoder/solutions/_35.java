a9f4e84c320c2941baabec1b89c1f56a6c665c49
Fisher Coder <fishercoder@gmail.com>
2019-10-18 06:40:59 -0700

    refactor 35

--- src/main/java/com/fishercoder/solutions/_35.java
+++ src/main/java/com/fishercoder/solutions/_35.java
@@ -18,14 +18,16 @@ package com.fishercoder.solutions;
 
 public class _35 {
 
-    public int searchInsert(int[] nums, int target) {
-        int len = nums.length;
-        for (int i = 0; i < len; i++) {
-            if (target <= nums[i]) {
-                return i;
+    public static class Solution1 {
+        public int searchInsert(int[] nums, int target) {
+            int len = nums.length;
+            for (int i = 0; i < len; i++) {
+                if (target <= nums[i]) {
+                    return i;
+                }
             }
+            return len;
         }
-        return len;
     }
 
 }
