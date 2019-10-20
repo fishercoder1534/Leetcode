65f2f114e4e7ec5123b678f6368c76e565b7fdce
Fisher Coder <fishercoder@gmail.com>
2019-10-20 07:08:21 -0700

    refactor 37

--- src/main/java/com/fishercoder/solutions/_37.java
+++ src/main/java/com/fishercoder/solutions/_37.java
@@ -3,9 +3,53 @@ package com.fishercoder.solutions;
 /**
  * 37. Sudoku Solver
  *
- * Write a program to solve a Sudoku puzzle by filling the empty cells.
- * Empty cells are indicated by the character '.'.
- * You may assume that there will be only one unique solution.
+ * Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+ *
+ * Each row must contain the digits 1-9 without repetition.
+ * Each column must contain the digits 1-9 without repetition.
+ * Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+ *
+ * The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+ *
+ * Example 1:
+ *
+ * Input:
+ * [
+ *   ["5","3",".",".","7",".",".",".","."],
+ *   ["6",".",".","1","9","5",".",".","."],
+ *   [".","9","8",".",".",".",".","6","."],
+ *   ["8",".",".",".","6",".",".",".","3"],
+ *   ["4",".",".","8",".","3",".",".","1"],
+ *   ["7",".",".",".","2",".",".",".","6"],
+ *   [".","6",".",".",".",".","2","8","."],
+ *   [".",".",".","4","1","9",".",".","5"],
+ *   [".",".",".",".","8",".",".","7","9"]
+ * ]
+ * Output: true
+ * Example 2:
+ *
+ * Input:
+ * [
+ *   ["8","3",".",".","7",".",".",".","."],
+ *   ["6",".",".","1","9","5",".",".","."],
+ *   [".","9","8",".",".",".",".","6","."],
+ *   ["8",".",".",".","6",".",".",".","3"],
+ *   ["4",".",".","8",".","3",".",".","1"],
+ *   ["7",".",".",".","2",".",".",".","6"],
+ *   [".","6",".",".",".",".","2","8","."],
+ *   [".",".",".","4","1","9",".",".","5"],
+ *   [".",".",".",".","8",".",".","7","9"]
+ * ]
+ * Output: false
+ * Explanation: Same as Example 1, except with the 5 in the top left corner being
+ *     modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+ * Note:
+ *
+ * A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+ * Only the filled cells need to be validated according to the mentioned rules.
+ * The given board contain only digits 1-9 and the character '.'.
+ * The given board size is always 9x9.
+ *
  */
 public class _37 {
 
