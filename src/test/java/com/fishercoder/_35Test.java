391e843cf7193fa57b737142e7ecb5ecdbaf2eec
Fisher Coder <fishercoder@gmail.com>
2019-10-19 17:15:33 -0700

    add test for 35

---
+++ src/test/java/com/fishercoder/_35Test.java
@@ -0,0 +1,23 @@
+package com.fishercoder;
+
+import com.fishercoder.solutions._35;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class _35Test {
+    private static _35.Solution1 solution1;
+    private static int[] nums;
+
+    @BeforeClass
+    public static void setup() {
+        solution1 = new _35.Solution1();
+    }
+
+    @Test
+    public void test1() {
+        nums = new int[]{1, 3, 5, 6};
+        assertEquals(2, solution1.searchInsert(nums, 5));
+    }
+}
