74330232f31733081498c9615bb423071346639f
Fisher Coder <fishercoder@gmail.com>
2019-10-28 06:52:08 -0700

    add test for 72

---
+++ src/test/java/com/fishercoder/_72Test.java
@@ -0,0 +1,21 @@
+package com.fishercoder;
+
+import com.fishercoder.solutions._72;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class _72Test {
+    private static _72.Solution1 solution1;
+
+    @BeforeClass
+    public static void setup() {
+        solution1 = new _72.Solution1();
+    }
+
+    @Test
+    public void test1() {
+        assertEquals(1, solution1.minDistance("Ada", "Adam"));
+    }
+}
