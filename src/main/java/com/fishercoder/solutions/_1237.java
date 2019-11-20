142890adc6d90dbc049501220ddb107e3bcdeb1e
Fisher Coder <fishercoder@gmail.com>
2019-11-20 07:29:10 -0800

    add one brute force solution for 1237

--- src/main/java/com/fishercoder/solutions/_1237.java
+++ src/main/java/com/fishercoder/solutions/_1237.java
@@ -1,5 +1,7 @@
 package com.fishercoder.solutions;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -41,16 +43,26 @@ public class _1237 {
 
     // This is the custom function interface.
     // You should not implement it, or speculate about its implementation
-    interface CustomFunction {
+    abstract class CustomFunction {
         // Returns f(x, y) for any given positive integers x and y.
         // Note that f(x, y) is increasing with respect to both x and y.
         // i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
-        public int f(int x, int y);
+        public abstract int f(int x, int y);
     }
 
     public static class Solution1 {
+        /**Time: O(1) since it's bound by 1 <= x, y <= 1000
+         * Space: O(1)*/
         public List<List<Integer>> findSolution(CustomFunction customfunction, int z) {
-            return null;
+            List<List<Integer>> result = new ArrayList<>();
+            for (int x = 1; x <= 1000; x++) {
+                for (int y = 1; y <= 1000; y++) {
+                    if (customfunction.f(x, y) == z) {
+                        result.add(Arrays.asList(x, y));
+                    }
+                }
+            }
+            return result;
         }
     }
 
