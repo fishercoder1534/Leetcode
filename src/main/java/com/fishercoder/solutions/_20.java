e22405bb1b5899c6cd9514a547f652a647a90abd
Fisher Coder <fishercoder@gmail.com>
2019-11-24 15:49:26 -0800

    refactor 20

--- src/main/java/com/fishercoder/solutions/_20.java
+++ src/main/java/com/fishercoder/solutions/_20.java
@@ -1,18 +1,43 @@
 package com.fishercoder.solutions;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.Stack;
 
 /**
  * 20. Valid Parentheses
  *
  * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
- * The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.*/
+ *
+ * An input string is valid if:
+ *
+ * 1. Open brackets must be closed by the same type of brackets.
+ * 2. Open brackets must be closed in the correct order.
+ *
+ * Note that an empty string is also considered valid.
+ *
+ * Example 1:
+ * Input: "()"
+ * Output: true
+ *
+ * Example 2:
+ * Input: "()[]{}"
+ * Output: true
+ *
+ * Example 3:
+ * Input: "(]"
+ * Output: false
+ *
+ * Example 4:
+ * Input: "([)]"
+ * Output: false
+ *
+ * Example 5:
+ * Input: "{[]}"
+ * Output: true
+ * */
 public class _20 {
-
     public static class Solution1 {
         public boolean isValid(String s) {
-            Deque<Character> stack = new ArrayDeque<>();
+            Stack<Character> stack = new Stack<>();
             for (int i = 0; i < s.length(); i++) {
                 if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
                     stack.push(s.charAt(i));
