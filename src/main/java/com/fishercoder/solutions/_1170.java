89c55d13c82d9cb92a5dced3fcf944c179a9564c
Fisher Coder <fishercoder@gmail.com>
2019-11-26 07:44:12 -0800

    add binary search solution for 1170

--- src/main/java/com/fishercoder/solutions/_1170.java
+++ src/main/java/com/fishercoder/solutions/_1170.java
@@ -81,4 +81,59 @@ public class _1170 {
             return frequency;
         }
     }
+
+    public static class Solution2 {
+        /**
+         * Use binary search when finding counts
+         * Time: O(n^logn) where m is the size of queries and n is the size of words
+         * Space: O(max(m, n) where m is the size of queries and n is the size of words)
+         * */
+        public int[] numSmallerByFrequency(String[] queries, String[] words) {
+            int[] queriesMinFrequecies = new int[queries.length];
+            for (int i = 0; i < queries.length; i++) {
+                queriesMinFrequecies[i] = computeLowestFrequency(queries[i]);
+            }
+
+            int[] wordsMinFrequecies = new int[words.length];
+            for (int i = 0; i < words.length; i++) {
+                wordsMinFrequecies[i] = computeLowestFrequency(words[i]);
+            }
+            Arrays.sort(wordsMinFrequecies);
+
+            int[] result = new int[queries.length];
+            for (int i = 0; i < result.length; i++) {
+                result[i] = binarySearch(wordsMinFrequecies, queriesMinFrequecies[i]);
+            }
+            return result;
+        }
+
+        private int binarySearch(int[] nums, int target) {
+            int left = 0;
+            int right = nums.length - 1;
+            while (left <= right) {
+                int mid = (left + right) / 2;
+                if (nums[mid] <= target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            return nums.length - left;
+        }
+
+        private int computeLowestFrequency(String string) {
+            char[] str = string.toCharArray();
+            Arrays.sort(str);
+            String sortedString = new String(str);
+            int frequency = 1;
+            for (int i = 1; i < sortedString.length(); i++) {
+                if (sortedString.charAt(i) == sortedString.charAt(0)) {
+                    frequency++;
+                } else {
+                    break;
+                }
+            }
+            return frequency;
+        }
+    }
 }
--- src/test/java/com/fishercoder/_1170Test.java
+++ src/test/java/com/fishercoder/_1170Test.java
@@ -8,12 +8,14 @@ import static org.junit.Assert.assertArrayEquals;
 
 public class _1170Test {
     private static _1170.Solution1 solution1;
+    private static _1170.Solution2 solution2;
     private static String[] queries;
     private static String[] words;
 
     @BeforeClass
     public static void setup() {
         solution1 = new _1170.Solution1();
+        solution2 = new _1170.Solution2();
     }
 
     @Test
@@ -28,6 +30,20 @@ public class _1170Test {
         queries = new String[]{"bbb", "cc"};
         words = new String[]{"a", "aa", "aaa", "aaaa"};
         assertArrayEquals(new int[]{1, 2}, solution1.numSmallerByFrequency(queries, words));
+    }
+
+    @Test
+    public void test3() {
+        queries = new String[]{"cbd"};
+        words = new String[]{"zaaaz"};
+        assertArrayEquals(new int[]{1}, solution2.numSmallerByFrequency(queries, words));
+    }
+
+    @Test
+    public void test4() {
+        queries = new String[]{"bbb", "cc"};
+        words = new String[]{"a", "aa", "aaa", "aaaa"};
+        assertArrayEquals(new int[]{1, 2}, solution2.numSmallerByFrequency(queries, words));
     }
 
 }
