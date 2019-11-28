95ef264708335f643e05ade83a80c0ff3de64bc1
Fisher Coder <fishercoder@gmail.com>
2019-11-28 12:01:22 -0800

    add solution for 1134

--- src/main/java/com/fishercoder/solutions/_1134.java
+++ src/main/java/com/fishercoder/solutions/_1134.java
@@ -24,7 +24,20 @@ package com.fishercoder.solutions;
 public class _1134 {
     public static class Solution1 {
         public boolean isArmstrong(int N) {
-            return false;
+            int numOfDigits = 0;
+            int copyN = N;
+            while (copyN > 0) {
+                copyN /= 10;
+                numOfDigits++;
+            }
+            int sum = 0;
+            copyN = N;
+            while (N > 0) {
+                int digit = N % 10;
+                sum += Math.pow(digit, numOfDigits);
+                N /= 10;
+            }
+            return sum == copyN;
         }
     }
 }
