5a8f5cec2483d8c242e7aa658492122f0f58911c
Fisher Coder <fishercoder@gmail.com>
2019-11-29 07:36:45 -0800

    add another solution for 1099

--- src/main/java/com/fishercoder/solutions/_1099.java
+++ src/main/java/com/fishercoder/solutions/_1099.java
@@ -1,5 +1,7 @@
 package com.fishercoder.solutions;
 
+import java.util.Arrays;
+
 /**
  * 1099. Two Sum Less Than K
  *
@@ -26,6 +28,10 @@ package com.fishercoder.solutions;
  * */
 public class _1099 {
     public static class Solution1 {
+        /**
+         * Time: O(n^2)
+         * Space: O(1)
+         * */
         public int twoSumLessThanK(int[] A, int K) {
             int maxSum = Integer.MIN_VALUE;
             for (int i = 0; i < A.length - 1; i++) {
@@ -36,6 +42,30 @@ public class _1099 {
                 }
             }
             return maxSum == Integer.MIN_VALUE ? -1 : maxSum;
+        }
+    }
+
+    public static class Solution2 {
+        /**
+         * Time: O(nlogn)
+         * Space: O(1)
+         * */
+        public int twoSumLessThanK(int[] A, int K) {
+            Arrays.sort(A);
+            int left = 0;
+            int right = A.length - 1;
+            int sum = Integer.MIN_VALUE;
+            while (left < right) {
+                int newSum = A[left] + A[right];
+                if (newSum < K && newSum > sum) {
+                    sum = newSum;
+                } else if (newSum >= K) {
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+            return sum == Integer.MIN_VALUE ? -1 : sum;
         }
     }
 }
--- src/test/java/com/fishercoder/_1099Test.java
+++ src/test/java/com/fishercoder/_1099Test.java
@@ -8,10 +8,12 @@ import static org.junit.Assert.assertEquals;
 
 public class _1099Test {
     private static _1099.Solution1 solution1;
+    private static _1099.Solution2 solution2;
 
     @BeforeClass
     public static void setup() {
         solution1 = new _1099.Solution1();
+        solution2 = new _1099.Solution2();
     }
 
     @Test
@@ -22,6 +24,16 @@ public class _1099Test {
     @Test
     public void test2() {
         assertEquals(-1, solution1.twoSumLessThanK(new int[]{10, 20, 30}, 15));
+    }
+
+    @Test
+    public void test3() {
+        assertEquals(58, solution2.twoSumLessThanK(new int[]{34, 23, 1, 24, 75, 33, 54, 8}, 60));
+    }
+
+    @Test
+    public void test4() {
+        assertEquals(-1, solution2.twoSumLessThanK(new int[]{10, 20, 30}, 15));
     }
 
 }
