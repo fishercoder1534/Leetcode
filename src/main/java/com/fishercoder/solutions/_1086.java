75c597a0b62cb63dddd1cdee4c5f4cf6c5c3bab7
Fisher Coder <fishercoder@gmail.com>
2019-11-30 07:53:05 -0800

    add one more solution for 1086

--- src/main/java/com/fishercoder/solutions/_1086.java
+++ src/main/java/com/fishercoder/solutions/_1086.java
@@ -3,6 +3,7 @@ package com.fishercoder.solutions;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.TreeMap;
 
 /**
@@ -48,6 +49,37 @@ public class _1086 {
                     sum += scores.get(i);
                 }
                 result[id - 1][0] = id;
+                result[id - 1][1] = sum / 5;
+            }
+            return result;
+        }
+    }
+
+    public static class Solution2 {
+        public int[][] highFive(int[][] items) {
+            TreeMap<Integer, PriorityQueue<Integer>> treeMap = new TreeMap<>();
+            for (int[] studentToScores : items) {
+                if (treeMap.containsKey(studentToScores[0])) {
+                    PriorityQueue<Integer> maxHeap = treeMap.get(studentToScores[0]);
+                    maxHeap.offer(studentToScores[1]);
+                    if (maxHeap.size() > 5) {
+                        maxHeap.poll();
+                    }
+                    treeMap.put(studentToScores[0], maxHeap);
+                } else {
+                    PriorityQueue<Integer> maxHeap = new PriorityQueue<>();
+                    maxHeap.offer(studentToScores[1]);
+                    treeMap.put(studentToScores[0], maxHeap);
+                }
+            }
+            int[][] result = new int[treeMap.size()][2];
+            for (int id : treeMap.keySet()) {
+                result[id - 1][0] = id;
+                int sum = 0;
+                PriorityQueue<Integer> maxHeap = treeMap.get(id);
+                while (!maxHeap.isEmpty()) {
+                    sum += maxHeap.poll();
+                }
                 result[id - 1][1] = sum / 5;
             }
             return result;
