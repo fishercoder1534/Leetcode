abae1a429c08b60a9cdd69b9f6e8506e992e6eca
Fisher Coder <fishercoder@gmail.com>
2019-12-07 07:18:03 -0800

    refactor 15

--- src/main/java/com/fishercoder/solutions/_15.java
+++ src/main/java/com/fishercoder/solutions/_15.java
@@ -26,6 +26,7 @@ public class _15 {
     public static class Solution1 {
         public List<List<Integer>> threeSum(int[] nums) {
             Arrays.sort(nums);
+            
             List<List<Integer>> result = new ArrayList<>();
             for (int i = 0; i < nums.length - 2; i++) {
                 if (i >= 1 && nums[i] == nums[i - 1]) {
