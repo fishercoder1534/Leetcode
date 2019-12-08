46e285cbce708fc0510751d1a66ead5b06b0e17e
Fisher Coder <fishercoder@gmail.com>
2019-12-08 07:40:45 -0800

    fix build

--- src/main/java/com/fishercoder/solutions/_1232.java
+++ src/main/java/com/fishercoder/solutions/_1232.java
@@ -33,8 +33,8 @@ public class _1232 {
          * */
         public boolean checkStraightLine(int[][] coordinates) {
             for (int i = 2; i < coordinates.length - 1; i++) {
-                if ((coordinates[1][0] - coordinates[0][0]) * (coordinates[i + 1][1] - coordinates[i][1]) !=
-                        (coordinates[1][1] - coordinates[0][1]) * (coordinates[i + 1][0] - coordinates[i][0])) {
+                if ((coordinates[1][0] - coordinates[0][0]) * (coordinates[i + 1][1] - coordinates[i][1])
+                        != (coordinates[1][1] - coordinates[0][1]) * (coordinates[i + 1][0] - coordinates[i][0])) {
                     return false;
                 }
             }
