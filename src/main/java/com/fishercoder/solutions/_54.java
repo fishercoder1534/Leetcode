06a1283313759d5588775fb7eea13f88b9c1083d
Fisher Coder <fishercoder@gmail.com>
2019-12-21 12:50:11 -0800

    refactor 54

--- src/main/java/com/fishercoder/solutions/_54.java
+++ src/main/java/com/fishercoder/solutions/_54.java
@@ -22,47 +22,35 @@ import java.util.List;
 public class _54 {
 
   public static class Solution1 {
+    /**credit: https://leetcode.com/problems/spiral-matrix/discuss/20599/Super-Simple-and-Easy-to-Understand-Solution/185257*/
     public List<Integer> spiralOrder(int[][] matrix) {
-      List<Integer> result = new ArrayList();
-      int row = matrix.length;
-
-      if (row == 0) {
-        return result;
+      if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+        return new ArrayList<>();
       }
-      int rowStart = 0;
-      int rowEnd = matrix.length - 1;
-      int colStart = 0;
-      int colEnd = matrix[0].length - 1;
-      while (rowStart <= rowEnd && colStart <= colEnd) {
-        //traverse to the right
-        for (int j = colStart; j <= colEnd; j++) {
-          result.add(matrix[rowStart][j]);
+      int m = matrix.length;
+      int n = matrix[0].length;
+      List<Integer> result = new ArrayList();
+      int left = 0;
+      int right = n - 1;
+      int top = 0;
+      int bottom = m - 1;
+      while (result.size() < m * n) {
+        for (int j = left; j <= right && result.size() < m * n; j++) {
+          result.add(matrix[top][j]);
         }
-        rowStart++;
-
-        //traverse to the bottom
-        for (int i = rowStart; i <= rowEnd; i++) {
-          result.add(matrix[i][colEnd]);
+        top++;
+        for (int i = top; i <= bottom && result.size() < m * n; i++) {
+          result.add(matrix[i][right]);
         }
-        colEnd--;
-
-        //only when rowStart <= rowEnd
-        //we'll traverse to the left
-        if (rowStart <= rowEnd) {
-          for (int j = colEnd; j >= colStart; j--) {
-            result.add(matrix[rowEnd][j]);
-          }
+        right--;
+        for (int j = right; j >= left && result.size() < m * n; j--) {
+          result.add(matrix[bottom][j]);
         }
-        rowEnd--;
-
-        //only when colStart <= colEnd
-        //we'll traverse to the top
-        if (colStart <= colEnd) {
-          for (int i = rowEnd; i >= rowStart; i--) {
-            result.add(matrix[i][colStart]);
-          }
+        bottom--;
+        for (int i = bottom; i >= top && result.size() < m * n; i--) {
+          result.add(matrix[i][left]);
         }
-        colStart++;
+        left++;
       }
       return result;
     }
--- src/test/java/com/fishercoder/_54Test.java
+++ src/test/java/com/fishercoder/_54Test.java
@@ -1,7 +1,9 @@
 package com.fishercoder;
 
 import com.fishercoder.solutions._54;
+
 import java.util.Arrays;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -9,21 +11,27 @@ import static org.junit.Assert.assertEquals;
 
 public class _54Test {
 
-  private static _54.Solution1 solution1;
-  private static int[][] matrix;
+    private static _54.Solution1 solution1;
+    private static int[][] matrix;
 
-  @BeforeClass
-  public static void setup() {
-    solution1 = new _54.Solution1();
-  }
+    @BeforeClass
+    public static void setup() {
+        solution1 = new _54.Solution1();
+    }
 
-  @Test
-  public void test1() {
-    matrix = new int[][] {
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9},
-    };
-    assertEquals(Arrays.asList(1, 2, 3, 6, 9, 8, 7, 4, 5), solution1.spiralOrder(matrix));
-  }
+    @Test
+    public void test1() {
+        matrix = new int[][]{
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9},
+        };
+        assertEquals(Arrays.asList(1, 2, 3, 6, 9, 8, 7, 4, 5), solution1.spiralOrder(matrix));
+    }
+
+    @Test
+    public void test2() {
+        matrix = new int[][]{};
+        assertEquals(Arrays.asList(), solution1.spiralOrder(matrix));
+    }
 }
