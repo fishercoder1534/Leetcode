fa9de70f2361ef85b116d44d2d478c598350a142
Fisher Coder <fishercoder@gmail.com>
2019-12-21 18:17:17 -0800

    refactor 885

--- src/main/java/com/fishercoder/solutions/_885.java
+++ src/main/java/com/fishercoder/solutions/_885.java
@@ -28,27 +28,22 @@ public class _885 {
     public static class Solution1 {
         /**credit: https://leetcode.com/problems/spiral-matrix-iii/discuss/158977/Java-15-lines-concise-solution-with-comments*/
         public int[][] spiralMatrixIII(int R, int C, int r0, int c0) {
-            int[][] directions = new int[][]{
-                    {0, 1},
-                    {1, 0},
-                    {0, -1},
-                    {-1, 0}
-            };
+            int[] directions = new int[]{0, 1, 0, -1, 0};
             int[][] result = new int[R * C][2];
-            int j = 0;
-            result[j++] = new int[]{r0, c0};
+            int i = 0;
+            result[i++] = new int[]{r0, c0};
             int len = 0;
             int d = 0;
-            while (j < R * C) {
+            while (i < R * C) {
                 if (d == 0 || d == 2) {
                     //plus one when moving east or west
                     len++;
                 }
-                for (int i = 0; i < len; i++) {
-                    r0 += directions[d][0];
-                    c0 += directions[d][1];
+                for (int k = 0; k < len; k++) {
+                    r0 += directions[d];
+                    c0 += directions[d + 1];
                     if (r0 >= 0 && r0 < R && c0 >= 0 && c0 < C) {
-                        result[j++] = new int[]{r0, c0};
+                        result[i++] = new int[]{r0, c0};
                     }
                 }
                 d = (d + 1) % 4;
