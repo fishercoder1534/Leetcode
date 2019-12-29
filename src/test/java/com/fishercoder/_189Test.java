727dd51255dc0ffb6609b51565d3cef5cbaa24fa
Fisher Coder <fishercoder@gmail.com>
2019-12-29 10:53:22 -0800

    refactor 189

--- src/test/java/com/fishercoder/_189Test.java
+++ src/test/java/com/fishercoder/_189Test.java
@@ -8,14 +8,12 @@ import org.junit.Test;
 public class _189Test {
     private static _189.Solution1 solution1;
     private static _189.Solution2 solution2;
-    private static _189.Solution3 solution3;
     private static int[] nums;
 
     @BeforeClass
     public static void setup() {
         solution1 = new _189.Solution1();
         solution2 = new _189.Solution2();
-        solution3 = new _189.Solution3();
     }
 
     @Test
@@ -34,11 +32,4 @@ public class _189Test {
         CommonUtils.printArray(nums);
     }
 
-    @Test
-    public void test3() {
-        nums = new int[]{1, 2, 3};
-
-        solution3.rotate(nums, 1);
-        CommonUtils.printArray(nums);
-    }
 }
