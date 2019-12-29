fe63f53c220b10534eca62e3c303957a25af98f6
Fisher Coder <fishercoder@gmail.com>
2019-12-29 09:26:00 -0800

    refactor 26

--- src/test/java/com/fishercoder/_26Test.java
+++ src/test/java/com/fishercoder/_26Test.java
@@ -7,49 +7,29 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class _26Test {
-  private static _26.Solution1 solution1;
-  private static _26.Solution2 solution2;
-  private static int[] nums;
+    private static _26.Solution1 solution1;
+    private static int[] nums;
 
-  @BeforeClass
-  public static void setup() {
-    solution1 = new _26.Solution1();
-    solution2 = new _26.Solution2();
-  }
+    @BeforeClass
+    public static void setup() {
+        solution1 = new _26.Solution1();
+    }
 
-  @Test
-  public void test1() {
-    nums = new int[] {1, 1, 2};
-    assertEquals(2, solution1.removeDuplicates(nums));
-  }
+    @Test
+    public void test1() {
+        nums = new int[]{1, 1, 2};
+        assertEquals(2, solution1.removeDuplicates(nums));
+    }
 
-  @Test
-  public void test2() {
-    nums = new int[] {1, 1, 2};
-    assertEquals(2, solution2.removeDuplicates(nums));
-  }
+    @Test
+    public void test2() {
+        nums = new int[]{1, 1, 2, 2, 3};
+        assertEquals(3, solution1.removeDuplicates(nums));
+    }
 
-  @Test
-  public void test3() {
-    nums = new int[] {1,1,2,2,3};
-    assertEquals(3, solution1.removeDuplicates(nums));
-  }
-
-  @Test
-  public void test4() {
-    nums = new int[] {1,1,2,2,3};
-    assertEquals(3, solution2.removeDuplicates(nums));
-  }
-
-  @Test
-  public void test5() {
-    nums = new int[] {1, 1};
-    assertEquals(1, solution1.removeDuplicates(nums));
-  }
-
-  @Test
-  public void test6() {
-    nums = new int[] {1, 1};
-    assertEquals(1, solution2.removeDuplicates(nums));
-  }
+    @Test
+    public void test3() {
+        nums = new int[]{1, 1};
+        assertEquals(1, solution1.removeDuplicates(nums));
+    }
 }
