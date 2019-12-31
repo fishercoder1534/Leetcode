bb6bf1d6e21a3288984385e5bd0e69dccff3859b
Fisher Coder <fishercoder@gmail.com>
2019-12-31 08:39:09 -0800

    refactor 219

--- src/main/java/com/fishercoder/solutions/_219.java
+++ src/main/java/com/fishercoder/solutions/_219.java
@@ -1,13 +1,27 @@
 package com.fishercoder.solutions;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
-/**219. Contains Duplicate II
-
+/**
+ * 219. Contains Duplicate II
  Given an array of integers and an integer k,
- find out whether there are two distinct indices i and j in
- the array such that nums[i] = nums[j] and the difference between i and j is at most k.
+ find out whether there are two distinct indices i and j in the array
+ such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
+
+ Example 1:
+ Input: nums = [1,2,3,1], k = 3
+ Output: true
+
+ Example 2:
+ Input: nums = [1,0,1,1], k = 1
+ Output: true
+
+ Example 3:
+ Input: nums = [1,2,3,1,2,3], k = 2
+ Output: false
  */
 public class _219 {
 
@@ -23,6 +37,21 @@ public class _219 {
                     }
                 } else {
                     map.put(nums[i], i);
+                }
+            }
+            return false;
+        }
+    }
+
+    public static class Solution2 {
+        public boolean containsNearbyDuplicate(int[] nums, int k) {
+            Set<Integer> set = new HashSet<>();
+            for (int i = 0; i < nums.length; i++) {
+                if (!set.add(nums[i])) {
+                    return true;
+                }
+                if (set.size() > k) {
+                    set.remove(nums[i - k]);
                 }
             }
             return false;
