492de8c50432b43135511cbd95cce849a1e05ad7
Fisher Coder <fishercoder@gmail.com>
2020-01-04 08:53:34 -0800

    refactor 350

--- src/main/java/com/fishercoder/solutions/_350.java
+++ src/main/java/com/fishercoder/solutions/_350.java
@@ -1,6 +1,10 @@
 package com.fishercoder.solutions;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Arrays;
 
 /**
  * 350. Intersection of Two Arrays II
