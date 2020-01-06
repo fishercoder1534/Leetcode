3b579573048c829c634e7de532bbfd7a194674ba
Fisher Coder <fishercoder@gmail.com>
2020-01-06 07:59:02 -0800

    refactor 189

--- src/main/java/com/fishercoder/solutions/_189.java
+++ src/main/java/com/fishercoder/solutions/_189.java
@@ -23,12 +23,28 @@ package com.fishercoder.solutions;
  Note:
  Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
  Could you do it in-place with O(1) extra space?
-
  * */
 
 public class _189 {
 
   public static class Solution1 {
+    /**
+     * O(n*k) time
+     * O(1) space
+     */
+    public void rotate(int[] nums, int k) {
+      int tmp;
+      for (int i = 0; i < k; i++) {
+        tmp = nums[nums.length - 1];
+        for (int j = nums.length - 1; j > 0; j--) {
+          nums[j] = nums[j - 1];
+        }
+        nums[0] = tmp;
+      }
+    }
+  }
+
+  public static class Solution2 {
     /**
      * using an extra array of the same size to copy it
      * O(n) time
@@ -46,19 +62,27 @@ public class _189 {
     }
   }
 
-  public static class Solution2 {
+  public static class Solution3 {
     /**
+     * reverse three times
      * O(n) time
      * O(1) space
      */
     public void rotate(int[] nums, int k) {
-      int tmp;
-      for (int i = 0; i < k; i++) {
-        tmp = nums[nums.length - 1];
-        for (int j = nums.length - 1; j > 0; j--) {
-          nums[j] = nums[j - 1];
-        }
-        nums[0] = tmp;
+      int len = nums.length;
+      k %= len;
+      reverse(nums, 0, len - 1);
+      reverse(nums, 0, k - 1);
+      reverse(nums, k, len - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+      while (start < end) {
+        int tmp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = tmp;
+        start++;
+        end--;
       }
     }
   }
--- src/test/java/com/fishercoder/_189Test.java
+++ src/test/java/com/fishercoder/_189Test.java
@@ -8,18 +8,19 @@ import org.junit.Test;
 public class _189Test {
     private static _189.Solution1 solution1;
     private static _189.Solution2 solution2;
+    private static _189.Solution3 solution3;
     private static int[] nums;
 
     @BeforeClass
     public static void setup() {
         solution1 = new _189.Solution1();
         solution2 = new _189.Solution2();
+        solution3 = new _189.Solution3();
     }
 
     @Test
     public void test1() {
         nums = new int[]{1, 2, 3};
-
         solution1.rotate(nums, 1);
         CommonUtils.printArray(nums);
     }
@@ -27,8 +28,14 @@ public class _189Test {
     @Test
     public void test2() {
         nums = new int[]{1, 2, 3};
-
         solution2.rotate(nums, 1);
+        CommonUtils.printArray(nums);
+    }
+
+    @Test
+    public void test3() {
+        nums = new int[]{1, 2, 3};
+        solution3.rotate(nums, 1);
         CommonUtils.printArray(nums);
     }
 
