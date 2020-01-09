2ee3708a32be97ac1aeb274b2bca836d6b62feee
Fisher Coder <fishercoder@gmail.com>
2020-01-09 09:44:50 -0800

    refactor 48

--- src/main/java/com/fishercoder/solutions/_48.java
+++ src/main/java/com/fishercoder/solutions/_48.java
@@ -80,4 +80,28 @@ public class _48 {
         }
     }
 
+    public static class Solution3 {
+        public void rotate(int[][] matrix) {
+            int n = matrix.length;
+            for (int i = 0; i < n / 2; i++) {
+                for (int j = i; j < n - i - 1; j++) {
+                    //save the top
+                    int top = matrix[i][j];
+
+                    //move left to top
+                    matrix[i][j] = matrix[n - 1 - j][i];
+
+                    //move bottom to left
+                    matrix[n - 1 - j][i] = matrix[n - i - 1][n - 1 - j];
+
+                    //move right to bottom
+                    matrix[n - i - 1][n - 1 - j] = matrix[j][n - i - 1];
+
+                    //move top to right
+                    matrix[j][n - i - 1] = top;
+                }
+            }
+        }
+    }
+
 }
