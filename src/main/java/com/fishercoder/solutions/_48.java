fea8f1d3467174bc96c3cb8c7406b1541cd705c6
Fisher Coder <fishercoder@gmail.com>
2020-01-09 19:58:10 -0800

    refactor 48

--- src/main/java/com/fishercoder/solutions/_48.java
+++ src/main/java/com/fishercoder/solutions/_48.java
@@ -2,13 +2,44 @@ package com.fishercoder.solutions;
 
 /**
  * 48. Rotate Image
- *
- * You are given an n x n 2D matrix representing an image.
+
+ You are given an n x n 2D matrix representing an image.
 
  Rotate the image by 90 degrees (clockwise).
 
- Follow up:
- Could you do this in-place?
+ Note:
+ You have to rotate the image in-place, which means you have to modify the
+ input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+ Example 1:
+ Given input matrix =
+ [
+ [1,2,3],
+ [4,5,6],
+ [7,8,9]
+ ],
+ rotate the input matrix in-place such that it becomes:
+ [
+ [7,4,1],
+ [8,5,2],
+ [9,6,3]
+ ]
+
+ Example 2:
+ Given input matrix =
+ [
+ [ 5, 1, 9,11],
+ [ 2, 4, 8,10],
+ [13, 3, 6, 7],
+ [15,14,12,16]
+ ],
+ rotate the input matrix in-place such that it becomes:
+ [
+ [15,13, 2, 5],
+ [14, 3, 4, 1],
+ [12, 6, 8, 9],
+ [16, 7,10,11]
+ ]
  */
 public class _48 {
 
@@ -22,7 +53,6 @@ public class _48 {
              * 1, 2, 3                    1, 4, 7                      7, 4, 1
              * 4, 5, 6         becomes    2, 5, 8           becomes    8, 5, 2
              * 7, 8, 9                    3, 6, 9                      9, 6, 3
-             This is done in O(1) space!
              **/
             int m = matrix.length;
             for (int i = 0; i < m; i++) {
@@ -51,11 +81,13 @@ public class _48 {
 
     public static class Solution2 {
         /**First swap the rows bottom up, then swap the element on the diagonal:
+         *
          * 1, 2, 3                         7, 8, 9                           7, 4, 1
          * 4, 5, 6           becomes       4, 5, 6           becomes         8, 5, 2
          * 7, 8, 9                         1, 2, 3                           9, 6, 3
          *
-         *  This is using O(n) of extra space
+         * Time: O(n^2)
+         * Space: O(1)
          */
         public void rotate(int[][] matrix) {
             int m = matrix.length;
