448e3a928c02808a14c2cf93129c23939b48b5bf
Fisher Coder <fishercoder@gmail.com>
2020-01-11 07:59:29 -0800

    refactor 7

--- src/main/java/com/fishercoder/solutions/_7.java
+++ src/main/java/com/fishercoder/solutions/_7.java
@@ -31,11 +31,30 @@ public class _7 {
             while (x != 0) {
                 result = result * 10 + x % 10;
                 x /= 10;
+                System.out.println("result = " + result);
                 if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
+                    System.out.println("break out..");
                     return 0;
                 }
             }
             return (int) result;
+        }
+    }
+
+    public static class Solution2 {
+        /**credit: https://leetcode.com/problems/reverse-integer/discuss/4060/My-accepted-15-lines-of-code-for-Java*/
+        public int reverse(int x) {
+            int result = 0;
+            while (x != 0) {
+                int lastDigit = x % 10;
+                int newResult = result * 10 + lastDigit;
+                if (result != (newResult - lastDigit) / 10) {
+                    return 0;
+                }
+                x /= 10;
+                result = newResult;
+            }
+            return result;
         }
     }
 }
