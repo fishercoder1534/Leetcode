98a399e68a7cae15bcc758f3b000281352a3097d
Fisher Coder <fishercoder@gmail.com>
2020-01-10 17:29:52 -0800

    refactor 7

--- src/main/java/com/fishercoder/solutions/_7.java
+++ src/main/java/com/fishercoder/solutions/_7.java
@@ -2,27 +2,38 @@ package com.fishercoder.solutions;
 
 /**
  * 7. Reverse Integer
- *
- * Reverse digits of an integer.
 
- Example1: x = 123, return 321
+ Given a 32-bit signed integer, reverse digits of an integer.
 
- Example2: x = -123, return -321
+ Example 1:
+ Input: 123
+ Output: 321
 
+ Example 2:
+ Input: -123
+ Output: -321
+
+ Example 3:
+ Input: 120
+ Output: 21
+
+ Note:
+ Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1].
+ For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
  */
 public class _7 {
 
     public static class Solution1 {
         public int reverse(int x) {
-            long rev = 0;
+            long result = 0;
             while (x != 0) {
-                rev = rev * 10 + x % 10;
+                result = result * 10 + x % 10;
                 x /= 10;
-                if (rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE) {
+                if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
                     return 0;
                 }
             }
-            return (int) rev;
+            return (int) result;
         }
     }
 }
