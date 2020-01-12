7978d30300c1ee2add0074d302a99c026469d0d8
Fisher Coder <fishercoder@gmail.com>
2020-01-12 15:10:18 -0800

    refactor 237

--- src/main/java/com/fishercoder/solutions/_237.java
+++ src/main/java/com/fishercoder/solutions/_237.java
@@ -6,8 +6,24 @@ import com.fishercoder.common.classes.ListNode;
  * 237. Delete Node in a Linked List
  * 
  * Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
- * Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3,
- * the linked list should become 1 -> 2 -> 4 after calling your function.
+ *
+ * Given linked list -- head = [4,5,1,9], which looks like following:
+ *
+ * Example 1:
+ * Input: head = [4,5,1,9], node = 5
+ * Output: [4,1,9]
+ * Explanation: You are given the second node with value 5, the linked list should become 4 -> 1 -> 9 after calling your function.
+ *
+ * Example 2:
+ * Input: head = [4,5,1,9], node = 1
+ * Output: [4,5,9]
+ * Explanation: You are given the third node with value 1, the linked list should become 4 -> 5 -> 9 after calling your function.
+ *
+ * Note:
+ * The linked list will have at least two elements.
+ * All of the nodes' values will be unique.
+ * The given node will not be the tail and it will always be a valid node of the linked list.
+ * Do not return anything from your function.
 */
 public class _237 {
 
