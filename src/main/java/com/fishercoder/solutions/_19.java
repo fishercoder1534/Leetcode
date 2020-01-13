011b393765266b11529f79aef7df6440dca63e6a
Fisher Coder <fishercoder@gmail.com>
2020-01-12 21:28:23 -0800

    refactor 19

--- src/main/java/com/fishercoder/solutions/_19.java
+++ src/main/java/com/fishercoder/solutions/_19.java
@@ -10,7 +10,6 @@ import com.fishercoder.common.utils.CommonUtils;
  * After removing the second node from the end, the linked list becomes 1->2->3->5.
 
 Note:
-
 Given n will always be valid.
 Try to do this in one pass.
  */
--- src/test/java/com/fishercoder/_19Test.java
+++ src/test/java/com/fishercoder/_19Test.java
@@ -10,12 +10,14 @@ import static org.junit.Assert.assertEquals;
 
 public class _19Test {
     private static _19.Solution1 solution1;
+    private static _19.Solution3 solution3;
     private static ListNode head;
     private static ListNode expected;
 
     @BeforeClass
     public static void setup() {
         solution1 = new _19.Solution1();
+        solution3 = new _19.Solution3();
     }
 
     @Test
@@ -23,6 +25,20 @@ public class _19Test {
         head = LinkedListUtils.contructLinkedList(new int[]{1, 2, 3, 4, 5});
         expected = LinkedListUtils.contructLinkedList(new int[]{1, 2, 3, 5});
         assertEquals(expected, solution1.removeNthFromEnd(head, 2));
+    }
+
+    @Test
+    public void test2() {
+        head = LinkedListUtils.contructLinkedList(new int[]{1, 2, 3, 4, 5});
+        expected = LinkedListUtils.contructLinkedList(new int[]{1, 2, 3, 5});
+        assertEquals(expected, solution3.removeNthFromEnd(head, 2));
+    }
+
+    @Test
+    public void test3() {
+        head = LinkedListUtils.contructLinkedList(new int[]{1});
+        expected = LinkedListUtils.contructLinkedList(new int[]{});
+        assertEquals(expected, solution1.removeNthFromEnd(head, 1));
     }
 
 }
