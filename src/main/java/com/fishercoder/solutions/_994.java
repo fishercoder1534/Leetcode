9d14dadb6e047a8a9e88486568848f29219df410
Fisher Coder <fishercoder@gmail.com>
2020-01-17 16:40:34 -0800

    refactor 2994

--- src/main/java/com/fishercoder/solutions/_994.java
+++ src/main/java/com/fishercoder/solutions/_994.java
@@ -11,10 +11,8 @@ import java.util.Queue;
  * the value 0 representing an empty cell;
  * the value 1 representing a fresh orange;
  * the value 2 representing a rotten orange.
- *
  * Every minute, any fresh orange that is adjacent (4-directionally) to a
  * rotten orange becomes rotten.
- *
  * Return the minimum number of minutes that must elapse until no cell has a fresh orange.
  * If this is impossible, return -1 instead.
  *
@@ -33,52 +31,51 @@ import java.util.Queue;
  * Explanation:  Since there are already no fresh oranges at minute 0, the answer is just 0.
  *
  * Note:
- *
  * 1 <= grid.length <= 10
  * 1 <= grid[0].length <= 10
  * grid[i][j] is only 0, 1, or 2.
  */
 public class _994 {
-  public static class Solution1 {
-    int[] directions = new int[] {0, 1, 0, -1, 0};
+    public static class Solution1 {
+        int[] directions = new int[]{0, 1, 0, -1, 0};
 
-    public int orangesRotting(int[][] grid) {
-      Queue<int[]> rottens = new LinkedList<>();
-      for (int i = 0; i < grid.length; i++) {
-        for (int j = 0; j < grid[0].length; j++) {
-          if (grid[i][j] == 2) {
-            rottens.add(new int[] {i, j});
-          }
-        }
-      }
-      int times = 0;
-      while (!rottens.isEmpty()) {
-        int size = rottens.size();
-        boolean counted = false;
-        for (int k = 0; k < size; k++) {
-          int[] rotten = rottens.poll();
-          for (int i = 0; i < 4; i++) {
-            int x = rotten[0] + directions[i];
-            int y = rotten[1] + directions[i + 1];
-            if (x >= 0 && x < grid.length && y >= 0 && y < grid[0].length && grid[x][y] == 1) {
-              grid[x][y] = 2;
-              if (!counted) {
-                times++;
-              }
-              counted = true;
-              rottens.add(new int[] {x, y});
+        public int orangesRotting(int[][] grid) {
+            Queue<int[]> rottens = new LinkedList<>();
+            for (int i = 0; i < grid.length; i++) {
+                for (int j = 0; j < grid[0].length; j++) {
+                    if (grid[i][j] == 2) {
+                        rottens.add(new int[]{i, j});
+                    }
+                }
             }
-          }
+            int times = 0;
+            while (!rottens.isEmpty()) {
+                int size = rottens.size();
+                boolean counted = false;
+                for (int k = 0; k < size; k++) {
+                    int[] rotten = rottens.poll();
+                    for (int i = 0; i < 4; i++) {
+                        int x = rotten[0] + directions[i];
+                        int y = rotten[1] + directions[i + 1];
+                        if (x >= 0 && x < grid.length && y >= 0 && y < grid[0].length && grid[x][y] == 1) {
+                            grid[x][y] = 2;
+                            if (!counted) {
+                                times++;
+                            }
+                            counted = true;
+                            rottens.add(new int[]{x, y});
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < grid.length; i++) {
+                for (int j = 0; j < grid[0].length; j++) {
+                    if (grid[i][j] == 1) {
+                        return -1;
+                    }
+                }
+            }
+            return times;
         }
-      }
-      for (int i = 0; i < grid.length; i++) {
-        for (int j = 0; j < grid[0].length; j++) {
-          if (grid[i][j] == 1) {
-            return -1;
-          }
-        }
-      }
-      return times;
     }
-  }
 }
