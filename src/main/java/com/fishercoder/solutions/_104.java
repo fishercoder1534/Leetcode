2a37bb09dd0e3d2c646a0be5b4f03f524800fbb6
Fisher Coder <fishercoder@gmail.com>
2020-01-25 13:34:45 -0800

    refactor 104

--- src/main/java/com/fishercoder/solutions/_104.java
+++ src/main/java/com/fishercoder/solutions/_104.java
@@ -2,6 +2,8 @@ package com.fishercoder.solutions;
 
 import com.fishercoder.common.classes.TreeNode;
 
+import java.util.LinkedList;
+
 /**
  * 104. Maximum Depth of Binary Tree
  *
@@ -25,6 +27,7 @@ public class _104 {
 
   public static class Solution1 {
     /**
+     * Recursive solution:
      * Time: O(n)
      * Space: O(n)
      */
@@ -33,6 +36,37 @@ public class _104 {
         return 0;
       }
       return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+  }
+
+  public static class Solution2 {
+    /**
+     * Iterative solution:
+     * Time: O(n)
+     * Space: O(n)
+     */
+    public int maxDepth(TreeNode root) {
+      if (root == null) {
+        return 0;
+      }
+      LinkedList<TreeNode> stack = new LinkedList<>();
+      LinkedList<Integer> depths = new LinkedList<>();
+      stack.add(root);
+      depths.add(1);
+
+      int depth = 0;
+      while (!stack.isEmpty()) {
+        TreeNode currentNode = stack.pollLast();
+        int currentDepth = depths.pollLast();
+        if (currentNode != null) {
+          depth = Math.max(depth, currentDepth);
+          stack.add(currentNode.right);
+          depths.add(currentDepth + 1);
+          stack.add(currentNode.left);
+          depths.add(currentDepth + 1);
+        }
+      }
+      return depth;
     }
   }
 
--- src/test/java/com/fishercoder/_104Test.java
+++ src/test/java/com/fishercoder/_104Test.java
@@ -13,16 +13,25 @@ import static org.junit.Assert.assertEquals;
 
 public class _104Test {
     private static _104.Solution1 solution1;
+    private static _104.Solution2 solution2;
     private static TreeNode root;
 
     @BeforeClass
     public static void setup() {
         solution1 = new _104.Solution1();
+        solution2 = new _104.Solution2();
     }
 
     @Test
     public void test1() {
         root = TreeUtils.constructBinaryTree(Arrays.asList(3, 9, 20, null, null, 15, 7));
         assertEquals(3, solution1.maxDepth(root));
+    }
+
+    @Test
+    public void test2() {
+        root = TreeUtils.constructBinaryTree(Arrays.asList(3, 9, 20, null, null, 15, 7));
+        TreeUtils.printBinaryTree(root);
+        assertEquals(3, solution2.maxDepth(root));
     }
 }
