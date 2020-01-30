75654d35a8f66cce83dad78e6158c8f644dac701
Fisher Coder <fishercoder@gmail.com>
2020-01-29 17:26:26 -0800

    add a solution to 70 and tests

--- src/main/java/com/fishercoder/solutions/_70.java
+++ src/main/java/com/fishercoder/solutions/_70.java
@@ -4,26 +4,21 @@ package com.fishercoder.solutions;
  * 70. Climbing Stairs
 
  You are climbing a stair case. It takes n steps to reach to the top.
-
  Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 
  Note: Given n will be a positive integer.
 
  Example 1:
-
  Input: 2
  Output:  2
  Explanation:  There are two ways to climb to the top.
-
  1. 1 step + 1 step
  2. 2 steps
 
  Example 2:
-
  Input: 3
  Output:  3
  Explanation:  There are three ways to climb to the top.
-
  1. 1 step + 1 step + 1 step
  2. 1 step + 2 steps
  3. 2 steps + 1 step
@@ -32,6 +27,10 @@ package com.fishercoder.solutions;
 
 public class _70 {
   public static class Solution1 {
+    /**
+     * O(n) time
+     * O(n) space
+     * */
     public int climbStairs(int n) {
       if (n == 1) {
         return n;
@@ -43,6 +42,26 @@ public class _70 {
         dp[i] = dp[i - 1] + dp[i - 2];
       }
       return dp[n];
+    }
+  }
+
+  public static class Solution2 {
+    /**
+     * O(n) time
+     * O(1) space
+     * */
+    public int climbStairs(int n) {
+      if (n == 1) {
+        return n;
+      }
+      int stepOne = 1;
+      int stepTwo = 2;
+      for (int i = 3; i <= n; i++) {
+        int tmp = stepTwo;
+        stepTwo = stepOne + stepTwo;
+        stepOne = tmp;
+      }
+      return stepTwo;
     }
   }
 }
--- src/test/java/com/fishercoder/_70Test.java
+++ src/test/java/com/fishercoder/_70Test.java
@@ -8,14 +8,21 @@ import static org.junit.Assert.assertEquals;
 
 public class _70Test {
     private static _70.Solution1 solution1;
+    private static _70.Solution2 solution2;
 
     @BeforeClass
     public static void setup() {
         solution1 = new _70.Solution1();
+        solution2 = new _70.Solution2();
     }
 
     @Test
     public void test1() {
         assertEquals(3, solution1.climbStairs(3));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(3, solution2.climbStairs(3));
     }
 }
