fa0f32db79250e87ad3a9fd4ba84c059b7cbb88c
Fisher Coder <fishercoder@gmail.com>
2020-01-31 11:23:41 -0800

    refactor 53

--- src/main/java/com/fishercoder/solutions/_53.java
+++ src/main/java/com/fishercoder/solutions/_53.java
@@ -2,9 +2,15 @@ package com.fishercoder.solutions;
 
 /**
  * 53. Maximum Subarray
+ * Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
  *
- * Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
- * For example, given the array [-2,1,-3,4,-1,2,1,-5,4], the contiguous subarray [4,-1,2,1] has the largest sum = 6.
+ * Example:
+ * Input: [-2,1,-3,4,-1,2,1,-5,4],
+ * Output: 6
+ * Explanation: [4,-1,2,1] has the largest sum = 6.
+ *
+ * Follow up:
+ * If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
  */
 
 public class _53 {
