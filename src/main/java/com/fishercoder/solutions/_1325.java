17a7ebbd48a5966e6bed360b56d0bcdc0899610b
Fisher Coder <fishercoder@gmail.com>
2020-02-07 07:26:54 -0800

    add one solution for 1325

--- src/main/java/com/fishercoder/solutions/_1325.java
+++ src/main/java/com/fishercoder/solutions/_1325.java
@@ -66,8 +66,43 @@ import com.fishercoder.common.classes.TreeNode;
  * */
 public class _1325 {
     public static class Solution1 {
+        /**
+         * my original but verbose solution
+         */
         public TreeNode removeLeafNodes(TreeNode root, int target) {
-            return null;
+            while (hasTargetLeafNodes(root, target)) {
+                root = removeLeafNodes(target, root);
+            }
+            return root;
+        }
+
+        private boolean hasTargetLeafNodes(TreeNode root, int target) {
+            if (root == null) {
+                return false;
+            }
+            if (root.left == null && root.right == null && root.val == target) {
+                return true;
+            }
+            return hasTargetLeafNodes(root.left, target) || hasTargetLeafNodes(root.right, target);
+        }
+
+        private TreeNode removeLeafNodes(int target, TreeNode root) {
+            if (root == null) {
+                return root;
+            }
+            if (root.val == target && root.left == null && root.right == null) {
+                root = null;
+                return root;
+            }
+            if (root.left != null && root.left.val == target && root.left.left == null && root.left.right == null) {
+                root.left = null;
+            }
+            if (root.right != null && root.right.val == target && root.right.left == null && root.right.right == null) {
+                root.right = null;
+            }
+            removeLeafNodes(target, root.left);
+            removeLeafNodes(target, root.right);
+            return root;
         }
     }
 }
