a9b1bcb0956d91ba3ce4dd4580e9908bcd60ada2
Fisher Coder <fishercoder@gmail.com>
2020-02-07 07:30:15 -0800

    add one more solution for 1325

--- src/main/java/com/fishercoder/solutions/_1325.java
+++ src/main/java/com/fishercoder/solutions/_1325.java
@@ -105,4 +105,19 @@ public class _1325 {
             return root;
         }
     }
+
+    public static class Solution2 {
+        /**A much more concise and efficient solution.*/
+        public TreeNode removeLeafNodes(TreeNode root, int target) {
+            if (root == null) {
+                return root;
+            }
+            root.left = removeLeafNodes(root.left, target);
+            root.right = removeLeafNodes(root.right, target);
+            if (root.left == null && root.right == null && root.val == target) {
+                return null;
+            }
+            return root;
+        }
+    }
 }
--- src/test/java/com/fishercoder/_1325Test.java
+++ src/test/java/com/fishercoder/_1325Test.java
@@ -12,12 +12,14 @@ import static org.junit.Assert.assertEquals;
 
 public class _1325Test {
     private static _1325.Solution1 solution1;
+    private static _1325.Solution2 solution2;
     private static TreeNode root;
     private static TreeNode expected;
 
     @BeforeClass
     public static void setup() {
         solution1 = new _1325.Solution1();
+        solution2 = new _1325.Solution2();
     }
 
     @Test
@@ -63,6 +65,51 @@ public class _1325Test {
         expected = TreeUtils.constructBinaryTree(Arrays.asList(1, 2, 3));
         TreeUtils.printBinaryTree(expected);
         assertEquals(expected, solution1.removeLeafNodes(root, 1));
+    }
+
+    @Test
+    public void test6() {
+        root = TreeUtils.constructBinaryTree(Arrays.asList(1, 2, 3, 2, null, 2, 4));
+        TreeUtils.printBinaryTree(root);
+        expected = TreeUtils.constructBinaryTree(Arrays.asList(1, null, 3, null, 4));
+        TreeUtils.printBinaryTree(expected);
+        assertEquals(expected, solution2.removeLeafNodes(root, 2));
+    }
+
+    @Test
+    public void test7() {
+        root = TreeUtils.constructBinaryTree(Arrays.asList(1, 3, 3, 3, 2));
+        TreeUtils.printBinaryTree(root);
+        expected = TreeUtils.constructBinaryTree(Arrays.asList(1, 3, null, null, 2));
+        TreeUtils.printBinaryTree(expected);
+        assertEquals(expected, solution2.removeLeafNodes(root, 3));
+    }
+
+    @Test
+    public void test8() {
+        root = TreeUtils.constructBinaryTree(Arrays.asList(1, 2, null, 2, null, 2));
+        TreeUtils.printBinaryTree(root);
+        expected = TreeUtils.constructBinaryTree(Arrays.asList(1));
+        TreeUtils.printBinaryTree(expected);
+        assertEquals(expected, solution2.removeLeafNodes(root, 2));
+    }
+
+    @Test
+    public void test9() {
+        root = TreeUtils.constructBinaryTree(Arrays.asList(1, 1, 1));
+        TreeUtils.printBinaryTree(root);
+        expected = null;
+        TreeUtils.printBinaryTree(expected);
+        assertEquals(expected, solution2.removeLeafNodes(root, 1));
+    }
+
+    @Test
+    public void test10() {
+        root = TreeUtils.constructBinaryTree(Arrays.asList(1, 2, 3));
+        TreeUtils.printBinaryTree(root);
+        expected = TreeUtils.constructBinaryTree(Arrays.asList(1, 2, 3));
+        TreeUtils.printBinaryTree(expected);
+        assertEquals(expected, solution2.removeLeafNodes(root, 1));
     }
 
 }
