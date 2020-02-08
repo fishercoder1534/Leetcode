e36ccb145dc918238fe593738e0032fd32ad5d0f
Fisher Coder <fishercoder@gmail.com>
2020-02-08 09:59:03 -0800

    add more tests for 1343

--- src/test/java/com/fishercoder/_1343Test.java
+++ src/test/java/com/fishercoder/_1343Test.java
@@ -19,4 +19,19 @@ public class _1343Test {
         assertEquals(3, solution1.numOfSubarrays(new int[]{2, 2, 2, 2, 5, 5, 5, 8}, 3, 4));
     }
 
+    @Test
+    public void test2() {
+        assertEquals(5, solution1.numOfSubarrays(new int[]{1, 1, 1, 1, 1}, 1, 0));
+    }
+
+    @Test
+    public void test3() {
+        assertEquals(6, solution1.numOfSubarrays(new int[]{11, 13, 17, 23, 29, 31, 7, 5, 2, 3}, 3, 5));
+    }
+
+    @Test
+    public void test4() {
+        assertEquals(1, solution1.numOfSubarrays(new int[]{7, 7, 7, 7, 7, 7, 7}, 7, 7));
+    }
+
 }
