0e739de1a1c8e233cdb3db5c8583e392e8d1cb93
Fisher Coder <fishercoder@gmail.com>
2020-02-08 10:12:04 -0800

    fix build

--- src/main/java/com/fishercoder/solutions/_1342.java
+++ src/main/java/com/fishercoder/solutions/_1342.java
@@ -35,7 +35,7 @@ package com.fishercoder.solutions;
  * */
 public class _1342 {
     public static class Solution1 {
-        public int numberOfSteps (int num) {
+        public int numberOfSteps(int num) {
             int steps = 0;
             while (num != 0) {
                 if (num % 2 == 0) {
