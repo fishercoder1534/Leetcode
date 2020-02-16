c27c7318808cfe3083e114b6bbdeeabc96adfcd4
Fisher Coder <fishercoder@gmail.com>
2020-02-16 08:02:32 -0800

    add 1352

--- src/main/java/com/fishercoder/solutions/_1352.java
+++ src/main/java/com/fishercoder/solutions/_1352.java
@@ -1,5 +1,8 @@
 package com.fishercoder.solutions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 1352. Product of the Last K Numbers
  *
@@ -37,18 +40,27 @@ package com.fishercoder.solutions;
  * */
 public class _1352 {
     public static class Solution1 {
+        /**credit: https://leetcode.com/problems/product-of-the-last-k-numbers/discuss/510260/JavaC%2B%2BPython-Prefix-Product*/
         public static class ProductOfNumbers {
 
-            public ProductOfNumbers() {
+            List<Integer> list;
 
+            public ProductOfNumbers() {
+                add(0);
             }
 
             public void add(int num) {
-
+                if (num > 0) {
+                    list.add(list.get(list.size() - 1) * num);
+                } else {
+                    list = new ArrayList<>();
+                    list.add(1);
+                }
             }
 
             public int getProduct(int k) {
-                return -1;
+                int size = list.size();
+                return k >= size ? 0 : (list.get(size - 1) / list.get(size - k - 1));
             }
         }
     }
