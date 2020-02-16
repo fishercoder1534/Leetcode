aeb3ae9972faf77c973ef9cb4ee6fe65eec17e52
Fisher Coder <fishercoder@gmail.com>
2020-02-16 13:00:51 -0800

    refactor 278

--- src/main/java/com/fishercoder/solutions/_278.java
+++ src/main/java/com/fishercoder/solutions/_278.java
@@ -26,23 +26,15 @@ public class _278 {
 		public int firstBadVersion(int n) {
 			int left = 1;
 			int right = n;
-			if (isBadVersion(left)) {
-				return left;
-			}
-
-			while (left + 1 < right) {
-				int mid = left + (right - left) / 2;
+			while (left < right) {
+				int mid = left + (right - left)/2;
 				if (isBadVersion(mid)) {
 					right = mid;
 				} else {
-					left = mid;
+					left = mid + 1;
 				}
 			}
-
-			if (isBadVersion(left)) {
-				return left;
-			}
-			return right;
+			return left;
 		}
 
 		private boolean isBadVersion(int left) {
