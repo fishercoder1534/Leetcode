51436836388e279f951d50cdb520c123f37148b0
Fisher Coder <fishercoder@gmail.com>
2020-02-16 07:54:11 -0800

    refactor 1182, remove unused imports

--- src/main/java/com/fishercoder/solutions/_1182.java
+++ src/main/java/com/fishercoder/solutions/_1182.java
@@ -1,8 +1,6 @@
 package com.fishercoder.solutions;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
