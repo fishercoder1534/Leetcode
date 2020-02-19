d8f8c2fb5e1680af0dcd909f54737a4348476514
Fisher Coder <fishercoder@gmail.com>
2020-02-18 16:40:39 -0800

    refactor 985

--- src/test/java/com/fishercoder/_985Test.java
+++ src/test/java/com/fishercoder/_985Test.java
@@ -7,23 +7,23 @@ import org.junit.Test;
 import static org.junit.Assert.assertArrayEquals;
 
 public class _985Test {
-  private static _985.Solution1 solution1;
-  private static int[] expected;
-  private static int[] actual;
-  private static int[] A;
-  private static int[][] queries;
+    private static _985.Solution1 solution1;
+    private static int[] expected;
+    private static int[] actual;
+    private static int[] A;
+    private static int[][] queries;
 
-  @BeforeClass
-  public static void setup() {
-    solution1 = new _985.Solution1();
-  }
+    @BeforeClass
+    public static void setup() {
+        solution1 = new _985.Solution1();
+    }
 
-  @Test
-  public void test1() {
-    A = new int[] {1, 2, 3, 4};
-    queries = new int[][] {{1, 0}, {-3, 1}, {-4, 0}, {2, 3}};
-    expected = new int[] {8, 6, 2, 4};
-    actual = solution1.sumEvenAfterQueries(A, queries);
-    assertArrayEquals(expected, actual);
-  }
+    @Test
+    public void test1() {
+        A = new int[]{1, 2, 3, 4};
+        queries = new int[][]{{1, 0}, {-3, 1}, {-4, 0}, {2, 3}};
+        expected = new int[]{8, 6, 2, 4};
+        actual = solution1.sumEvenAfterQueries(A, queries);
+        assertArrayEquals(expected, actual);
+    }
 }
