d5d2fda15351bd0d657893ad9b7ec3b8b5980d33
Fisher Coder <fishercoder@gmail.com>
2020-02-22 10:55:48 -0800

    refactor 1358

--- src/main/java/com/fishercoder/solutions/_1358.java
+++ src/main/java/com/fishercoder/solutions/_1358.java
@@ -1,11 +1,5 @@
 package com.fishercoder.solutions;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 /**
  * 1358. Number of Substrings Containing All Three Characters
  *
