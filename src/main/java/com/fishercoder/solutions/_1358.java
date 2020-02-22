2665132c313f0143db851a2c77b1ae8fdb832414
Fisher Coder <fishercoder@gmail.com>
2020-02-22 10:55:19 -0800

    add solution for 1358

--- src/main/java/com/fishercoder/solutions/_1358.java
+++ src/main/java/com/fishercoder/solutions/_1358.java
@@ -1,5 +1,11 @@
 package com.fishercoder.solutions;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * 1358. Number of Substrings Containing All Three Characters
  *
@@ -26,9 +32,23 @@ package com.fishercoder.solutions;
  * */
 public class _1358 {
     public static class Solution1 {
+        /**A classic sliding window problem, no dp or backtracking, just sliding window: use two pointers.
+         * my new favorite queustion!
+         * */
         public int numberOfSubstrings(String s) {
-            //TODO: implement it
-            return -1;
+            int[] counts = new int[3];
+            int i = 0;
+            int n = s.length();
+            int result = 0;
+            for (int j = 0; j < n; j++) {
+                counts[s.charAt(j) - 'a']++;
+                while (counts[0] > 0 && counts[1] > 0 && counts[2] > 0) {
+                    counts[s.charAt(i++) - 'a']--;
+                }
+                result += i;
+            }
+            return result;
         }
+
     }
 }
