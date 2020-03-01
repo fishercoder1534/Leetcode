df7c3b144cff7c2cf487be1bc8d2dccf8b2218c2
Fisher Coder <fishercoder@gmail.com>
2020-03-01 11:31:01 -0800

    add a test for 94

--- src/test/java/com/fishercoder/_94Test.java
+++ src/test/java/com/fishercoder/_94Test.java
@@ -12,12 +12,14 @@ import java.util.List;
 
 public class _94Test {
     private static _94.Solution1 solution1;
+    private static _94.Solution2 solution2;
     private static TreeNode root;
     private static List<Integer> inorder;
 
     @BeforeClass
     public static void setup() {
         solution1 = new _94.Solution1();
+        solution2 = new _94.Solution2();
     }
 
     @Test
@@ -32,6 +34,14 @@ public class _94Test {
         root = TreeUtils.constructBinaryTree(Arrays.asList(1, 2, 3, 4, null, 5, 6, null, 7, null, null, null, null, 8, 9));
         TreeUtils.printBinaryTree(root);
         inorder = solution1.inorderTraversal(root);
+        CommonUtils.printList(inorder);
+    }
+
+    @Test
+    public void test3() {
+        root = TreeUtils.constructBinaryTree(Arrays.asList(1, 2, 3, 4, null, 5, null, null, 6));
+        TreeUtils.printBinaryTree(root);
+        inorder = solution2.inorderTraversal(root);
         CommonUtils.printList(inorder);
     }
 
