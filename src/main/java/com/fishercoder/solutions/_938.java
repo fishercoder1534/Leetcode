42fe1173dd77f295b08bfb10ce88a6e2e77fabba
Fisher Coder <fishercoder@gmail.com>
2020-03-11 06:58:35 -0700

    refactor 938

--- src/main/java/com/fishercoder/solutions/_938.java
+++ src/main/java/com/fishercoder/solutions/_938.java
@@ -12,18 +12,14 @@ import java.util.List;
  * The binary search tree is guaranteed to have unique values.
  *
  * Example 1:
- *
  * Input: root = [10,5,15,3,7,null,18], L = 7, R = 15
  * Output: 32
  *
- *
  * Example 2:
- *
  * Input: root = [10,5,15,3,7,13,18,1,null,6], L = 6, R = 10
  * Output: 23
  *
  * Note:
- *
  * The number of nodes in the tree is at most 10000.
  * The final answer is guaranteed to be less than 2^31.
  */
