a9b790c50dd8063c5c06709e2cd48411842a797c
Fisher Coder <fishercoder@gmail.com>
2020-03-12 09:05:01 -0700

    refactor 339

--- src/main/java/com/fishercoder/solutions/_339.java
+++ src/main/java/com/fishercoder/solutions/_339.java
@@ -4,6 +4,22 @@ import com.fishercoder.common.classes.NestedInteger;
 
 import java.util.List;
 
+/**
+ * 339. Nested List Weight Sum
+ *
+ * Given a nested list of integers, return the sum of all integers in the list weighted by their depth.
+ * Each element is either an integer, or a list -- whose elements may also be integers or other lists.
+ *
+ * Example 1:
+ * Input: [[1,1],2,[1,1]]
+ * Output: 10
+ * Explanation: Four 1's at depth 2, one 2 at depth 1.
+ *
+ * Example 2:
+ * Input: [1,[4,[6]]]
+ * Output: 27
+ * Explanation: One 1 at depth 1, one 4 at depth 2, and one 6 at depth 3; 1 + 4*2 + 6*3 = 27.
+ * */
 public class _339 {
   public static class Solution1 {
     private int sum = 0;
