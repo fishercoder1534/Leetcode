abe2401c3aca4939ecc47c8d2799df847013d2d4
Fisher Coder <fishercoder@gmail.com>
2020-03-14 07:16:16 -0700

    refactor 88

--- src/main/java/com/fishercoder/solutions/_88.java
+++ src/main/java/com/fishercoder/solutions/_88.java
@@ -12,21 +12,21 @@ package com.fishercoder.solutions;
 
 public class _88 {
 
-  public static class Solution1 {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-      int i = m - 1;
-      int j = n - 1;
-      int k = m + n - 1;
-      while (i >= 0 && j >= 0) {
-        if (nums1[i] > nums2[j]) {
-          nums1[k--] = nums1[i--];
-        } else {
-          nums1[k--] = nums2[j--];
+    public static class Solution1 {
+        public void merge(int[] nums1, int m, int[] nums2, int n) {
+            int i = m - 1;
+            int j = n - 1;
+            int k = m + n - 1;
+            while (i >= 0 && j >= 0) {
+                if (nums1[i] > nums2[j]) {
+                    nums1[k--] = nums1[i--];
+                } else {
+                    nums1[k--] = nums2[j--];
+                }
+            }
+            while (j >= 0) {
+                nums1[k--] = nums2[j--];
+            }
         }
-      }
-      while (j >= 0) {
-        nums1[k--] = nums2[j--];
-      }
     }
-  }
 }
