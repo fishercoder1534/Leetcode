2e663c56c2bfa6ff4b8aa79576f9dd8593449af6
Fisher Coder <fishercoder@gmail.com>
2020-03-20 08:00:47 -0700

    refactor 102

--- src/main/java/com/fishercoder/solutions/_102.java
+++ src/main/java/com/fishercoder/solutions/_102.java
@@ -9,52 +9,51 @@ import java.util.Queue;
 
 /**
  * 102. Binary Tree Level Order Traversal
-
-Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
-
-For example:
-Given binary tree [3,9,20,null,null,15,7],
-
-    3
-   / \
-  9  20
-    /  \
-   15   7
-
-return its level order traversal as:
-
-[
-  [3],
-  [9,20],
-  [15,7]
-]
-*/
+ *
+ * Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+ *
+ * For example:
+ * Given binary tree [3,9,20,null,null,15,7],
+ *    3
+ *   / \
+ *  9  20
+ *    /  \
+ *   15   7
+ *
+ * return its level order traversal as:
+ *
+ * [
+ * [3],
+ * [9,20],
+ * [15,7]
+ * ]
+ */
 public class _102 {
 
-  public static class Solution1 {
-    public List<List<Integer>> levelOrder(TreeNode root) {
-      List<List<Integer>> result = new ArrayList<>();
-      if (root == null) {
-        return result;
-      }
-      Queue<TreeNode> queue = new LinkedList();
-      queue.offer(root);
-      while (!queue.isEmpty()) {
-        List<Integer> thisLevel = new ArrayList();
-        int size = queue.size();
-        for (int i = 0; i < size; i++) {
-          TreeNode curr = queue.poll();
-          thisLevel.add(curr.val);
-          if (curr.left != null) {
-            queue.offer(curr.left);
-          }
-          if (curr.right != null) {
-            queue.offer(curr.right);
-          }
+    public static class Solution1 {
+        public List<List<Integer>> levelOrder(TreeNode root) {
+            List<List<Integer>> result = new ArrayList<>();
+            if (root == null) {
+                return result;
+            }
+            Queue<TreeNode> queue = new LinkedList();
+            queue.offer(root);
+            while (!queue.isEmpty()) {
+                List<Integer> thisLevel = new ArrayList();
+                int size = queue.size();
+                for (int i = 0; i < size; i++) {
+                    TreeNode curr = queue.poll();
+                    thisLevel.add(curr.val);
+                    if (curr.left != null) {
+                        queue.offer(curr.left);
+                    }
+                    if (curr.right != null) {
+                        queue.offer(curr.right);
+                    }
+                }
+                result.add(thisLevel);
+            }
+            return result;
         }
-        result.add(thisLevel);
-      }
-      return result;
     }
-  }
 }
