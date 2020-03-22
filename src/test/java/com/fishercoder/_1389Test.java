df58c5439a584a6c029d4faf51f98a7bf9bf11ca
Fisher Coder <fishercoder@gmail.com>
2020-03-22 07:47:29 -0700

    fix build

--- src/test/java/com/fishercoder/_1389Test.java
+++ src/test/java/com/fishercoder/_1389Test.java
@@ -21,12 +21,12 @@ public class _1389Test {
 
     @Test
     public void test2() {
-        assertArrayEquals(new int[]{1, 2, 3, 4, 0}, solution1.createTargetArray(new int[]{0, 1, 2, 3, 0}, new int[]{0, 1, 2, 3, 4}));
+        assertArrayEquals(new int[]{0, 1, 2, 3, 4}, solution1.createTargetArray(new int[]{1, 2, 3, 4, 0}, new int[]{0, 1, 2, 3, 0}));
     }
 
     @Test
     public void test3() {
-        assertArrayEquals(new int[]{1}, solution1.createTargetArray(new int[]{0}, new int[]{1}));
+        assertArrayEquals(new int[]{1}, solution1.createTargetArray(new int[]{1}, new int[]{0}));
     }
 
 }
