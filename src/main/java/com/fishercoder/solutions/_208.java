51c21e0937dcd2c78f1fd1c665503c7f7ebd0f7e
Fisher Coder <fishercoder@gmail.com>
2020-03-23 07:43:38 -0700

    refactor 208

--- src/main/java/com/fishercoder/solutions/_208.java
+++ src/main/java/com/fishercoder/solutions/_208.java
@@ -4,9 +4,21 @@ package com.fishercoder.solutions;
  * 208. Implement Trie (Prefix Tree)
  *
  * Implement a trie with insert, search, and startsWith methods.
-
- Note:
- You may assume that all inputs are consist of lowercase letters a-z.
+ *
+ * Example:
+ *
+ * Trie trie = new Trie();
+ *
+ * trie.insert("apple");
+ * trie.search("apple");   // returns true
+ * trie.search("app");     // returns false
+ * trie.startsWith("app"); // returns true
+ * trie.insert("app");
+ * trie.search("app");     // returns true
+ * Note:
+ *
+ * You may assume that all inputs are consist of lowercase letters a-z.
+ * All inputs are guaranteed to be non-empty strings.
  */
 
 public class _208 {
