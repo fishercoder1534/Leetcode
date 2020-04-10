3d241cce78e7fd65ed250ead59891fc763455c95
Fisher Coder <fishercoder@gmail.com>
2020-04-10 10:20:37 -0700

    refactor 49

--- src/main/java/com/fishercoder/solutions/_49.java
+++ src/main/java/com/fishercoder/solutions/_49.java
@@ -10,34 +10,35 @@ import java.util.Map;
  * 49. Group Anagrams
  *
  * Given an array of strings, group anagrams together.
-
- For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"],
- Return:
-
- [
- ["ate", "eat","tea"],
- ["nat","tan"],
- ["bat"]
- ]
-
- Note: All inputs will be in lower-case.
+ *
+ * For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"],
+ * Return:
+ * [
+ * ["ate", "eat","tea"],
+ * ["nat","tan"],
+ * ["bat"]
+ * ]
+ *
+ * Note:
+ *  All inputs will be in lowercase.
+ *  The order of your output does not matter.
  */
 
 public class _49 {
 
-  public static class Solution1 {
-    public List<List<String>> groupAnagrams(String[] strs) {
-      Map<String, List<String>> map = new HashMap<>();
-      for (String word : strs) {
-        char[] c = word.toCharArray();
-        Arrays.sort(c);
-        String key = new String(c);
-        if (!map.containsKey(key)) {
-          map.put(key, new ArrayList<>());
+    public static class Solution1 {
+        public List<List<String>> groupAnagrams(String[] strs) {
+            Map<String, List<String>> map = new HashMap<>();
+            for (String word : strs) {
+                char[] c = word.toCharArray();
+                Arrays.sort(c);
+                String key = new String(c);
+                if (!map.containsKey(key)) {
+                    map.put(key, new ArrayList<>());
+                }
+                map.get(key).add(word);
+            }
+            return new ArrayList<>(map.values());
         }
-        map.get(key).add(word);
-      }
-      return new ArrayList<>(map.values());
     }
-  }
 }
--- src/test/java/com/fishercoder/_49Test.java
+++ src/test/java/com/fishercoder/_49Test.java
@@ -1,34 +1,51 @@
 package com.fishercoder;
 
 import com.fishercoder.solutions._49;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import org.apache.commons.collections4.CollectionUtils;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
 
 public class _49Test {
-  private static _49.Solution1 solution1;
-  private static String[] words;
-  private static List<List<String>> expected;
-  private static List<List<String>> actual;
+    private static _49.Solution1 solution1;
+    private static String[] words;
+    private static List<List<String>> expected;
+    private static List<List<String>> actual;
 
-  @BeforeClass
-  public static void setup() {
-    solution1 = new _49.Solution1();
-  }
+    @BeforeClass
+    public static void setup() {
+        solution1 = new _49.Solution1();
+    }
 
-  @Test
-  public void test1() {
-    words = new String[] {"eat", "tea", "tan", "ate", "nat", "bat"};
-    expected = new ArrayList<>();
-    expected.add(Arrays.asList("ate", "eat", "tea"));
-    expected.add(Arrays.asList("nat", "tan"));
-    expected.add(Arrays.asList("bat"));
-    actual = solution1.groupAnagrams(words);
-    assertEquals(expected.size(), actual.size());
-    assertEquals(expected.containsAll(actual), actual.containsAll(expected));
-  }
+    @Test
+    public void test1() {
+        words = new String[]{"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<String> e1 = Arrays.asList("bat");
+        List<String> e2 = Arrays.asList("tan", "nat");
+        List<String> e3 = Arrays.asList("ate", "eat", "tea");
+        expected = Arrays.asList(e1, e2, e3);
+        actual = solution1.groupAnagrams(words);
+        assertEquals(expected.size(), actual.size());
+        assertEquals(expected.containsAll(actual), actual.containsAll(expected));
+        for (List<String> a : actual) {
+            switch (a.size()) {
+                case 1:
+                    assertTrue(CollectionUtils.isEqualCollection(e1, a));
+                    break;
+                case 2:
+                    assertTrue(CollectionUtils.isEqualCollection(e2, a));
+                    break;
+                case 3:
+                    assertTrue(CollectionUtils.isEqualCollection(e3, a));
+                    break;
+            }
+        }
+    }
 }
