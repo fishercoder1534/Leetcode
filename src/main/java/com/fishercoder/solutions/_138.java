9c593b94ccb189f2e72463b9df09ba7ed182946c
Fisher Coder <fishercoder@gmail.com>
2020-04-10 19:07:48 -0700

    refactor 138

--- src/main/java/com/fishercoder/solutions/_138.java
+++ src/main/java/com/fishercoder/solutions/_138.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 /**
  * 138. Copy List with Random Pointer
- * 
+ *
  * A linked list is given such that each node contains an additional random
  * pointer which could point to any node in the list or null.
  * Return a deep copy of the list.
