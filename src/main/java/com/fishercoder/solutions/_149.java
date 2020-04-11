1f687bb9bd671e0e31a743cab1048cebf3744135
Fisher Coder <fishercoder@gmail.com>
2020-04-11 10:13:35 -0700

    fix build

--- src/main/java/com/fishercoder/solutions/_149.java
+++ src/main/java/com/fishercoder/solutions/_149.java
@@ -42,24 +42,29 @@ public class _149 {
    */
   public static class Solution1 {
     public int maxPoints(int[][] points) {
-      if (points.length < 3) return points.length;
+      if (points.length < 3) {
+        return points.length;
+      }
       int max = 0;
       Map<Long, Integer> map = new HashMap<>();
       for (int i = 0; i < points.length; i++) {
         int dup = 1;
         map.clear();
         for (int j = i + 1; j < points.length; j++) {
-          int dx = points[j][0] - points[i][0], dy = points[j][1] - points[i][1];
-          if (dx == 0 && dy == 0) dup++;
-          else {
+          int dx = points[j][0] - points[i][0];
+          int dy = points[j][1] - points[i][1];
+          if (dx == 0 && dy == 0) {
+            dup++;
+          } else {
             int gcd = getGcd(dx, dy);
             long slope = ((long) (dy / gcd) << 32) + (dx / gcd);
             map.put(slope, map.getOrDefault(slope, 0) + 1);
           }
         }
         max = Math.max(max, dup);
-        for (Map.Entry<Long, Integer> entry : map.entrySet())
+        for (Map.Entry<Long, Integer> entry : map.entrySet()) {
           max = Math.max(max, entry.getValue() + dup);
+        }
       }
       return max;
     }
