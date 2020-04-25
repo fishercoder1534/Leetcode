de8d5f574dfbad58698c4b742f8727ac4831efa1
Fisher Coder <fishercoder@gmail.com>
2020-04-25 06:31:23 -0700

    refactor 4

--- src/main/java/com/fishercoder/solutions/_4.java
+++ src/main/java/com/fishercoder/solutions/_4.java
@@ -3,23 +3,6 @@ package com.fishercoder.solutions;
 import static java.lang.Math.max;
 import static java.lang.Math.min;
 
-/**
- 4. Median of Two Sorted Arrays
-
- There are two sorted arrays nums1 and nums2 of size m and n respectively.
-
- Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
-
- Example 1:
- nums1 = [1, 3]
- nums2 = [2]
- The median is 2.0
-
- Example 2:
- nums1 = [1, 2]
- nums2 = [3, 4]
- The median is (2 + 3)/2 = 2.5
- */
 public class _4 {
 
     public static class Solution1 {
