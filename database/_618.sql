ec92b18ba90de8a9a1751e77370a44a4a19f7b95
Fisher Coder <fishercoder@gmail.com>
2020-04-25 13:48:08 -0700

    refactor 618

--- database/_618.sql
+++ database/_618.sql
@@ -1,21 +1,3 @@
---618. Students Report By Geography
---
---A U.S graduate school has students from Asia, Europe and America. The students' location information are stored in table student as below.
---
---| name   | continent |
---|--------|-----------|
---| Jack   | America   |
---| Pascal | Europe    |
---| Xi     | Asia      |
---| Jane   | America   |
---Pivot the continent column in this table so that each name is sorted alphabetically and displayed underneath its corresponding continent. The output headers should be America, Asia and Europe respectively. It is guaranteed that the student number from America is no less than either Asia or Europe.
---For the sample input, the output is:
---| America | Asia | Europe |
---|---------|------|--------|
---| Jack    | Xi   | Pascal |
---| Jane    |      |        |
---Follow-up: If it is unknown which continent has the most students, can you write a query to generate the student report?
-
 --reference: https://discuss.leetcode.com/topic/92752/accept-solution and https://leetcode.com/articles/students-report-by-geography/#approach-using-session-variables-and-join-accepted
 
 set @a = 0;
