9a2debee43b2bab95454c27ca00fb9804cc88a50
Fisher Coder <fishercoder@gmail.com>
2020-04-25 15:18:32 -0700

    refactor 954

--- src/main/java/com/fishercoder/solutions/_954.java
+++ src/main/java/com/fishercoder/solutions/_954.java
@@ -1,41 +1,34 @@
 package com.fishercoder.solutions;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 
 public class _954 {
     public static class Solution1 {
+        /**
+         * credit: https://leetcode.com/problems/array-of-doubled-pairs/solution/
+         */
         public boolean canReorderDoubled(int[] A) {
-            for (int i = 0; i < A.length; i++) {
-                if (A[i] < 0) {
-                    A[i] = -A[i];
-                }
-            }
-            Arrays.sort(A);
             Map<Integer, Integer> map = new HashMap<>();
-            for (int num : A) {
-                map.put(num, map.getOrDefault(num, 0) + 1);
+            for (int i : A) {
+                map.put(i, map.getOrDefault(i, 0) + 1);
             }
-            for (int num : A) {
-                if (num != 0) {
-                    if (map.get(num) < 0) {
-                        return false;
-                    } else if (map.get(num) == 0) {
-                        continue;
-                    } else {
-                        int count = map.get(num);
-                        map.put(num, 0);
-                        int doubleNum = num * 2;
-                        if (!map.containsKey(doubleNum)) {
-                            return false;
-                        } else {
-                            map.put(doubleNum, map.get(doubleNum) - count);
-                        }
-                    }
-                } else if (map.get(num) % 2 != 0) {
+            Integer[] sorted = new Integer[A.length];
+            for (int i = 0; i < A.length; i++) {
+                sorted[i] = A[i];
+            }
+            Arrays.sort(sorted, Comparator.comparingInt(Math::abs));
+            for (int num : sorted) {
+                if (map.get(num) == 0) {
+                    continue;
+                }
+                if (!map.containsKey(2 * num) || map.get(2 * num) <= 0) {
                     return false;
                 }
+                map.put(num, map.get(num) - 1);
+                map.put(2 * num, map.get(2 * num) - 1);
             }
             return true;
         }
