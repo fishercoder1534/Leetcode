3c1b061569a30b42fc6c5a166fd39ddf6a86f3d9
Fisher Coder <fishercoder@gmail.com>
2020-04-26 11:34:22 -0700

    refactor 14

--- src/main/java/com/fishercoder/solutions/_14.java
+++ src/main/java/com/fishercoder/solutions/_14.java
@@ -1,25 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 14. Longest Common Prefix
- *
- * Write a function to find the longest common prefix string amongst an array of strings.
- *
- * If there is no common prefix, return an empty string "".
- *
- * Example 1:
- * Input: ["flower","flow","flight"]
- * Output: "fl"
- *
- * Example 2:
- * Input: ["dog","racecar","car"]
- * Output: ""
- * Explanation: There is no common prefix among the input strings.
- *
- * Note:
- * All given inputs are in lowercase letters a-z.
- */
-
 public class _14 {
 
     public static class Solution1 {
