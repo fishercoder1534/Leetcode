85a188333ced26e62e29aaa6ce2be85b97c55315
Fisher Coder <fishercoder@gmail.com>
2020-04-26 11:33:38 -0700

    refactor 12

--- src/main/java/com/fishercoder/solutions/_12.java
+++ src/main/java/com/fishercoder/solutions/_12.java
@@ -1,11 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 12. Integer to Roman
- *
- * Given an integer, convert it to a roman numeral.
- * Input is guaranteed to be within the range from 1 to 3999.
- */
 public class _12 {
 
     public static class Solution1 {
