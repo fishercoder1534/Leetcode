5833f20037271ce015b941f95ff85480032899a6
Fisher Coder <fishercoder@gmail.com>
2020-04-27 07:24:01 -0700

    refactor 20

--- src/main/java/com/fishercoder/solutions/_20.java
+++ src/main/java/com/fishercoder/solutions/_20.java
@@ -2,38 +2,6 @@ package com.fishercoder.solutions;
 
 import java.util.Stack;
 
-/**
- * 20. Valid Parentheses
- *
- * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
- *
- * An input string is valid if:
- *
- * 1. Open brackets must be closed by the same type of brackets.
- * 2. Open brackets must be closed in the correct order.
- *
- * Note that an empty string is also considered valid.
- *
- * Example 1:
- * Input: "()"
- * Output: true
- *
- * Example 2:
- * Input: "()[]{}"
- * Output: true
- *
- * Example 3:
- * Input: "(]"
- * Output: false
- *
- * Example 4:
- * Input: "([)]"
- * Output: false
- *
- * Example 5:
- * Input: "{[]}"
- * Output: true
- * */
 public class _20 {
     public static class Solution1 {
         public boolean isValid(String s) {
