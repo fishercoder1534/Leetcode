731c958192dc8636978b0d1aa3eb1f349ab7a5dd
Fisher Coder <fishercoder@gmail.com>
2020-04-27 07:33:59 -0700

    refactor 24

--- src/main/java/com/fishercoder/solutions/_24.java
+++ src/main/java/com/fishercoder/solutions/_24.java
@@ -2,17 +2,6 @@ package com.fishercoder.solutions;
 
 import com.fishercoder.common.classes.ListNode;
 
-/**
- * 24. Swap Nodes in Pairs
- *
- * Given a linked list, swap every two adjacent nodes and return its head.
-
- For example,
- Given 1->2->3->4, you should return the list as 2->1->4->3.
-
- Your algorithm should use only constant space. You may not modify the values in the list, only nodes itself can be changed.
-
- */
 public class _24 {
     public static class Solution1 {
         public ListNode swapPairs(ListNode head) {
