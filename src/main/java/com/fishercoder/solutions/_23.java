28342748b483530c5978c81d9c850b69d7b8e057
Fisher Coder <fishercoder@gmail.com>
2020-04-27 07:25:28 -0700

    refactor 23

--- src/main/java/com/fishercoder/solutions/_23.java
+++ src/main/java/com/fishercoder/solutions/_23.java
@@ -5,23 +5,6 @@ import com.fishercoder.common.classes.ListNode;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
-/**
- * 23. Merge k Sorted Lists
- *
- * Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
- *
- * Example:
- *
- * Input:
- * [
- *   1->4->5,
- *   1->3->4,
- *   2->6
- * ]
- * Output: 1->1->2->3->4->4->5->6
- *
- * */
-
 public class _23 {
     public static class Solution1 {
         public ListNode mergeKLists(ListNode[] lists) {
