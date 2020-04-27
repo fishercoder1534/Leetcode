bba34100f54cfe36a61ae675f952ce11c8d5ee2c
Fisher Coder <fishercoder@gmail.com>
2020-04-27 06:21:34 -0700

    refactor 221

--- src/main/java/com/fishercoder/solutions/_221.java
+++ src/main/java/com/fishercoder/solutions/_221.java
@@ -1,18 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 221. Maximal Square
- *
- * Given a 2D binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
-
- For example, given the following matrix:
-
- 1 0 1 0 0
- 1 0 1 1 1
- 1 1 1 1 1
- 1 0 0 1 0
- Return 4.
- */
 public class _221 {
 
     public static class Solution1 {
