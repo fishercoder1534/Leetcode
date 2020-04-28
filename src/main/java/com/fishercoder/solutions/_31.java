eb803661f02b68ca89c8e66e50ac784cc9344d10
Fisher Coder <fishercoder@gmail.com>
2020-04-28 08:25:46 -0700

    refactor 31

--- src/main/java/com/fishercoder/solutions/_31.java
+++ src/main/java/com/fishercoder/solutions/_31.java
@@ -1,20 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 31. Next Permutation
-
-Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
-
-If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
-
-The replacement must be in-place, do not allocate extra memory.
-
-Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
-1,2,3 → 1,3,2
-3,2,1 → 1,2,3
-1,1,5 → 1,5,1
- */
-
 public class _31 {
   public static class Solution1 {
     /**
