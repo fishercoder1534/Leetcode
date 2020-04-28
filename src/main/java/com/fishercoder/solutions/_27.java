0c07ff61caff003824683119955756f159e66e36
Fisher Coder <fishercoder@gmail.com>
2020-04-28 07:56:44 -0700

    refactor 27

--- src/main/java/com/fishercoder/solutions/_27.java
+++ src/main/java/com/fishercoder/solutions/_27.java
@@ -1,19 +1,5 @@
 package com.fishercoder.solutions;
 
-/**27. Remove Element
-
-Given an array and a value, remove all instances of that value in place and return the new length.
-
-Do not allocate extra space for another array, you must do this in place with constant memory.
-
-The order of elements can be changed. It doesn't matter what you leave beyond the new length.
-
-Example:
-Given input array nums = [3,2,2,3], val = 3
-
-Your function should return length = 2, with the first two elements of nums being 2.
- */
-
 public class _27 {
 
   public static class Solution1 {
