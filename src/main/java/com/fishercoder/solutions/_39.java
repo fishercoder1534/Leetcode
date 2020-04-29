459b755d99a3663b793e526a915de1e5ea82dcab
Fisher Coder <fishercoder@gmail.com>
2020-04-29 09:50:42 -0700

    refactor 39

--- src/main/java/com/fishercoder/solutions/_39.java
+++ src/main/java/com/fishercoder/solutions/_39.java
@@ -4,23 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-/**
- * 39. Combination Sum
- *
- * Given a set of candidate numbers (C) (without duplicates) and a target number (T),
- * find all unique combinations in C where the candidate numbers sums to T.
-
- The same repeated number may be chosen from C unlimited number of times.
-
- Note:
- All numbers (including target) will be positive integers.
- The solution set must not contain duplicate combinations.
- For example, given candidate set [2, 3, 6, 7] and target 7,
- A solution set is:
- [
- [7],
- [2, 2, 3]
- ]*/
 public class _39 {
 
     public static class Solution1 {
