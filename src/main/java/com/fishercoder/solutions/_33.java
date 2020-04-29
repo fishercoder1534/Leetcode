f4bec1a3798cc45555a647094435fc3f2684d2d1
Fisher Coder <fishercoder@gmail.com>
2020-04-29 06:54:24 -0700

    refactor 33

--- src/main/java/com/fishercoder/solutions/_33.java
+++ src/main/java/com/fishercoder/solutions/_33.java
@@ -1,13 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 33. Search in Rotated Sorted Array
- *
- * Suppose a sorted array is rotated at some pivot unknown to you beforehand.
- * (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
- * You are given a target value to search. If found in the array return its index, otherwise return -1.
- * You may assume no duplicate exists in the array.
- */
 public class _33 {
 
     public static class Solution1 {
