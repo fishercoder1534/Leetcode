15af8a615f7368794ecea347a116f6074aaf73d8
Fisher Coder <fishercoder@gmail.com>
2020-04-29 06:53:21 -0700

    refactor 124

--- src/main/java/com/fishercoder/solutions/_124.java
+++ src/main/java/com/fishercoder/solutions/_124.java
@@ -5,24 +5,6 @@ import com.fishercoder.common.classes.TreeNode;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * 124. Binary Tree Maximum Path Sum
- *
- * Given a binary tree, find the maximum path sum.
- * For this problem, a path is defined as any sequence of nodes from some starting node to any node
- * in the tree along the parent-child connections.
- *
- * The path must contain at least one node and does not need to go through the root.
- *
- * For example:
- * Given the below binary tree,
- *
- *     1
- *    / \
- *   2   3
- *
- * Return 6.
- */
 public class _124 {
 
     public static class Solution1 {
