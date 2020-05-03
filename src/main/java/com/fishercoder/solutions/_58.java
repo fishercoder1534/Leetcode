ce4c2aaf91dd29ccf006ad62d230d7dc5ca53991
Fisher Coder <fishercoder@gmail.com>
2020-05-03 07:53:28 -0700

    refactor 58

--- src/main/java/com/fishercoder/solutions/_58.java
+++ src/main/java/com/fishercoder/solutions/_58.java
@@ -1,17 +1,5 @@
 package com.fishercoder.solutions;
-/**
- * 58. Length of Last Word
- *
- * Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
- * If the last word does not exist, return 0.
 
- Note: A word is defined as a character sequence consists of non-space characters only.
-
- For example,
- Given s = "Hello World",
- return 5.
-
- */
 public class _58 {
 
     public static class Solution1 {
