d948a61b6c419540ce037f669ac3722f3d2df8d4
Fisher Coder <fishercoder@gmail.com>
2020-05-03 10:20:56 -0700

    refactor 60

--- src/main/java/com/fishercoder/solutions/_60.java
+++ src/main/java/com/fishercoder/solutions/_60.java
@@ -1,23 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 60. Permutation Sequence
- *
- * The set [1,2,3,…,n] contains a total of n! unique permutations.
-
- By listing and labeling all of the permutations in order,
- We get the following sequence (ie, for n = 3):
-
- "123"
- "132"
- "213"
- "231"
- "312"
- "321"
- Given n and k, return the kth permutation sequence.
-
- Note: Given n will be between 1 and 9 inclusive.
- */
 public class _60 {
 
     public static class Solution1 {
