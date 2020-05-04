d3c3babd17c356c232c2e13c775da45f22126739
Fisher Coder <fishercoder@gmail.com>
2020-05-04 06:39:45 -0700

    refactor 476

--- src/main/java/com/fishercoder/solutions/_476.java
+++ src/main/java/com/fishercoder/solutions/_476.java
@@ -1,22 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 476. Number Complement
- *
- * Given a positive integer, output its complement number. The complement strategy is to flip the bits of its binary representation.
-
- Note:
- The given integer is guaranteed to fit within the range of a 32-bit signed integer.
- You could assume no leading zero bit in the integer’s binary representation.
- Example 1:
- Input: 5
- Output: 2
- Explanation: The binary representation of 5 is 101 (no leading zero bits), and its complement is 010. So you need to output 2.
- Example 2:
- Input: 1
- Output: 0
- Explanation: The binary representation of 1 is 1 (no leading zero bits), and its complement is 0. So you need to output 0.
- */
 public class _476 {
 
     public static class Solution1 {
