3242ae9e5c40afb9b098b1e59920e83479aa965f
Fisher Coder <fishercoder@gmail.com>
2020-05-03 18:18:11 -0700

    refactor 74

--- src/main/java/com/fishercoder/solutions/_74.java
+++ src/main/java/com/fishercoder/solutions/_74.java
@@ -1,34 +1,13 @@
 package com.fishercoder.solutions;
 
-/**
- * 74. Search a 2D Matrix
- *
- * Write an efficient algorithm that searches for a value in an m x n matrix.
- * This matrix has the following properties:
-
- Integers in each row are sorted from left to right.
- The first integer of each row is greater than the last integer of the previous row.
- For example,
-
- Consider the following matrix:
-
- [
- [1,   3,  5,  7],
- [10, 11, 16, 20],
- [23, 30, 34, 50]
- ]
-
- Given target = 3, return true.
-
- */
 public class _74 {
 
     public static class Solution1 {
         public boolean searchMatrix(int[][] matrix, int target) {
             if (matrix == null || matrix.length == 0
-                || matrix[0].length == 0
-                || matrix[0][0] > target
-                || matrix[matrix.length - 1][matrix[0].length - 1] < target) {
+                    || matrix[0].length == 0
+                    || matrix[0][0] > target
+                    || matrix[matrix.length - 1][matrix[0].length - 1] < target) {
                 return false;
             }
             int m = matrix.length;
