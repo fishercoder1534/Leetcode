2e9bbb189880ca26820ff7f3ab2057f766e684a6
Fisher Coder <fishercoder@gmail.com>
2020-05-03 18:12:40 -0700

    refactor 64

--- src/main/java/com/fishercoder/solutions/_64.java
+++ src/main/java/com/fishercoder/solutions/_64.java
@@ -1,12 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 64. Minimum Path Sum
- *
- * Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
- *
- * Note: You can only move either down or right at any point in time.
- */
 public class _64 {
     public static class Solution1 {
         /**
