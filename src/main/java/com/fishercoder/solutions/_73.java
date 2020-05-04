c2856a846db6f10a7b6282d2add771ac0ebd8d75
Fisher Coder <fishercoder@gmail.com>
2020-05-03 18:17:10 -0700

    refactor 73

--- src/main/java/com/fishercoder/solutions/_73.java
+++ src/main/java/com/fishercoder/solutions/_73.java
@@ -1,17 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 73. Set Matrix Zeroes
- *
- * Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in place.
-
- Follow up:
-
- Did you use extra space?
- A straight forward solution using O(mn) space is probably a bad idea.
- A simple improvement uses O(m + n) space, but still not the best solution.
- Could you devise a constant space solution?
- */
 public class _73 {
 
     public static class Solution1 {
