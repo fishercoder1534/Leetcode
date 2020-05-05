73d21c346cbe554d9e988de863997b1ab3067f0b
Fisher Coder <fishercoder@gmail.com>
2020-05-05 07:10:07 -0700

    refactor 77

--- src/main/java/com/fishercoder/solutions/_77.java
+++ src/main/java/com/fishercoder/solutions/_77.java
@@ -3,24 +3,6 @@ package com.fishercoder.solutions;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 77. Combinations
- *
- * Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
-
- For example,
- If n = 4 and k = 2, a solution is:
-
- [
- [2,4],
- [3,4],
- [2,3],
- [1,2],
- [1,3],
- [1,4],
- ]
- */
-
 public class _77 {
 
     public static class Solution1 {
