b52ac60161d9141860a5b8b77a04cd3be83d1d18
Fisher Coder <fishercoder@gmail.com>
2020-05-09 07:19:00 -0700

    refactor 90

--- src/main/java/com/fishercoder/solutions/_90.java
+++ src/main/java/com/fishercoder/solutions/_90.java
@@ -6,23 +6,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-/**
- * 90. Subsets II
- *
- *Given a collection of integers that might contain duplicates, nums, return all possible subsets.
- Note: The solution set must not contain duplicate subsets.
-
- For example,
- If nums = [1,2,2], a solution is:
- [
- [2],
- [1],
- [1,2,2],
- [2,2],
- [1,2],
- []
- ]
- */
 public class _90 {
 
     public static class Solution1 {
