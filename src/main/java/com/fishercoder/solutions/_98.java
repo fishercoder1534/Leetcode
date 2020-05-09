e446fa2f0a4cb4e413245b320b608261d1680ba4
Fisher Coder <fishercoder@gmail.com>
2020-05-09 10:12:43 -0700

    refactor 98

--- src/main/java/com/fishercoder/solutions/_98.java
+++ src/main/java/com/fishercoder/solutions/_98.java
@@ -2,29 +2,6 @@ package com.fishercoder.solutions;
 
 import com.fishercoder.common.classes.TreeNode;
 
-/**
- * 98. Validate Binary Search Tree
- *
- * Given a binary tree, determine if it is a valid binary search tree (BST).
-
- Assume a BST is defined as follows:
-
- The left subtree of a node contains only nodes with keys less than the node's key.
- The right subtree of a node contains only nodes with keys greater than the node's key.
- Both the left and right subtrees must also be binary search trees.
-
- Example 1:
-   2
-  / \
- 1   3
- Binary tree [2,1,3], return true.
-
- Example 2:
-   1
-  / \
- 2   3
- Binary tree [1,2,3], return false.
- */
 public class _98 {
 
     public static class Solution1 {
