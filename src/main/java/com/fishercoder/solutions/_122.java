96203c94fdffe2eb1e8779109cbed872d1e7e568
Fisher Coder <fishercoder@gmail.com>
2020-05-13 13:18:04 -0700

    refactor 122

--- src/main/java/com/fishercoder/solutions/_122.java
+++ src/main/java/com/fishercoder/solutions/_122.java
@@ -1,14 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 122. Best Time to Buy and Sell Stock II
- *
- * Say you have an array for which the ith element is the price of a given stock on day i.
- * Design an algorithm to find the maximum profit. You may complete as many transactions as you like
- * (ie, buy one and sell one share of the stock multiple times).
- * However, you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
- * */
-
 public class _122 {
     public static class Solution1 {
         //peak and valley approach
