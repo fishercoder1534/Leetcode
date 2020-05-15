a08235d1a1d033846e35c43cbb57f568d258d12e
Fisher Coder <fishercoder@gmail.com>
2020-05-15 07:19:50 -0700

    refactor 222

--- src/main/java/com/fishercoder/solutions/_222.java
+++ src/main/java/com/fishercoder/solutions/_222.java
@@ -2,16 +2,6 @@ package com.fishercoder.solutions;
 
 import com.fishercoder.common.classes.TreeNode;
 
-/**
- * 222. Count Complete Tree Nodes
- *
- * Given a complete binary tree, count the number of nodes.
- * Definition of a complete binary tree from Wikipedia:
- * In a complete binary tree every level,
- * except possibly the last, is completely filled,
- * and all nodes in the last level are as far left as possible.
- * It can have between 1 and 2h nodes inclusive at the last level h.
- */
 public class _222 {
 
     public static class Solution1 {
