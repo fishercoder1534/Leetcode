831289db971087f35a92deca96f87526f3ab3821
Fisher Coder <fishercoder@gmail.com>
2020-05-15 07:18:45 -0700

    refactor 219

--- src/main/java/com/fishercoder/solutions/_219.java
+++ src/main/java/com/fishercoder/solutions/_219.java
@@ -5,24 +5,6 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-/**
- * 219. Contains Duplicate II
- Given an array of integers and an integer k,
- find out whether there are two distinct indices i and j in the array
- such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
-
- Example 1:
- Input: nums = [1,2,3,1], k = 3
- Output: true
-
- Example 2:
- Input: nums = [1,0,1,1], k = 1
- Output: true
-
- Example 3:
- Input: nums = [1,2,3,1,2,3], k = 2
- Output: false
- */
 public class _219 {
 
     public static class Solution1 {
