c2677c98b44c553b503536869d5fbe8d1375e5ed
Fisher Coder <fishercoder@gmail.com>
2020-05-15 07:15:38 -0700

    refactor 213

--- src/main/java/com/fishercoder/solutions/_213.java
+++ src/main/java/com/fishercoder/solutions/_213.java
@@ -1,19 +1,5 @@
 package com.fishercoder.solutions;
-/**
- * 213. House Robber II
 
- Note: This is an extension of House Robber.
-
- After robbing those houses on that street,
- the thief has found himself a new place for his thievery
- so that he will not get too much attention.
- This time, all houses at this place are arranged in a circle.
- That means the first house is the neighbor of the last one.
- Meanwhile, the security system for these houses remain the same as for those in the previous street.
-
-Given a list of non-negative integers representing the amount of money of each house,
- determine the maximum amount of money you can rob tonight without alerting the police.
-*/
 public class _213 {
     public static class Solution1 {
         /**
