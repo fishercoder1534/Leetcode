cf9c2c97242d75ba3480337ba672f36812a44312
Fisher Coder <fishercoder@gmail.com>
2020-05-15 07:20:12 -0700

    refactor 223

--- src/main/java/com/fishercoder/solutions/_223.java
+++ src/main/java/com/fishercoder/solutions/_223.java
@@ -1,18 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 223. Rectangle Area
- *
- * Find the total area covered by two rectilinear rectangles in a 2D plane.
- * Each rectangle is defined by its bottom left corner and top right corner as shown in the figure.
- * Rectangle Area
-
- Example:
-
- Input: A = -3, B = 0, C = 3, D = 4, E = 0, F = -1, G = 9, H = 2
- Output: 45
-
- Note: Assume that the total area is never beyond the maximum possible value of int.*/
 public class _223 {
 
     public static class Solution1 {
