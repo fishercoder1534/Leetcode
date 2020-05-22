cacc4aa95f3cb4f2e93f1feefa67297d0fecd78f
Fisher Coder <fishercoder@gmail.com>
2020-05-22 10:38:02 -0700

    refactor 229

--- src/main/java/com/fishercoder/solutions/_229.java
+++ src/main/java/com/fishercoder/solutions/_229.java
@@ -3,30 +3,14 @@ package com.fishercoder.solutions;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 229. Majority Element II
- *
- * Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.
- * The algorithm should run in linear time and in O(1) space.
- *
- * Example 1:
- * Input: [3,2,3]
- * Output: [3]
- *
- * Example 2:
- * Input: [1,1,1,3,3,2,2,2]
- * Output: [1,2]
-
- Hint:
- How many majority elements could it possibly have?
- Do you have a better hint? Suggest it!
- */
 public class _229 {
 
     public static class Solution1 {
-        /**Moore Voting algorithm:
+        /**
+         * Moore Voting algorithm:
          * This is an extension of Majority Element I, instead of one one majority, there could be a max of two majority elements,
-         * so we'll just use two counters to do the job.*/
+         * so we'll just use two counters to do the job.
+         */
         public List<Integer> majorityElement(int[] nums) {
             List<Integer> result = new ArrayList<>();
             if (nums == null || nums.length == 0) {
