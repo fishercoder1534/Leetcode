fd081e6291c1cf9e1c35091b18d21b644757b0ff
Fisher Coder <fishercoder@gmail.com>
2020-05-22 06:59:35 -0700

    refactor 451

--- src/main/java/com/fishercoder/solutions/_451.java
+++ src/main/java/com/fishercoder/solutions/_451.java
@@ -6,48 +6,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * 451. Sort Characters By Frequency
- *
- * Given a string, sort it in decreasing order based on the frequency of characters.
-
- Example 1:
-
- Input:
- "tree"
-
- Output:
- "eert"
-
- Explanation:
- 'e' appears twice while 'r' and 't' both appear once.
- So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
-
- Example 2:
-
- Input:
- "cccaaa"
-
- Output:
- "cccaaa"
-
- Explanation:
- Both 'c' and 'a' appear three times, so "aaaccc" is also a valid answer.
- Note that "cacaca" is incorrect, as the same characters must be together.
-
- Example 3:
-
- Input:
- "Aabb"
-
- Output:
- "bbAa"
-
- Explanation:
- "bbaA" is also a valid answer, but "Aabb" is incorrect.
- Note that 'A' and 'a' are treated as two different characters.
-
- */
 public class _451 {
 
     public static class Solution1 {
