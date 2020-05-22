e85cbb568c0cbcf0e216f41b6b773c444dcf2870
Fisher Coder <fishercoder@gmail.com>
2020-05-22 10:39:09 -0700

    refactor 233

--- src/main/java/com/fishercoder/solutions/_233.java
+++ src/main/java/com/fishercoder/solutions/_233.java
@@ -1,16 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 233. Number of Digit One
- *
- * Given an integer n, count the total number of digit 1 appearing in all non-negative integers less than or equal to n.
-
- For example:
- Given n = 13,
- Return 6, because digit 1 occurred in the following numbers: 1, 10, 11, 12, 13.
-
- Hint: Beware of overflow.
- */
 public class _233 {
     public static class Solution1 {
         public int countDigitOne(int n) {
