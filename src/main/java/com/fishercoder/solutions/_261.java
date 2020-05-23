e631d17d95ce3733607310c21bf98547279d53f8
Fisher Coder <fishercoder@gmail.com>
2020-05-23 08:20:53 -0700

    refactor 261

--- src/main/java/com/fishercoder/solutions/_261.java
+++ src/main/java/com/fishercoder/solutions/_261.java
@@ -1,28 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- *261. Graph Valid Tree
- *
- * Given n nodes labeled from 0 to n - 1 and a list of undirected edges
- * (each edge is a pair of nodes),
- * write a function to check whether these edges make up a valid tree.
-
- For example:
-
- Given n = 5 and edges = [[0, 1], [0, 2], [0, 3], [1, 4]], return true.
-
- Given n = 5 and edges = [[0, 1], [1, 2], [2, 3], [1, 3], [1, 4]], return false.
-
- Hint:
-
- Given n = 5 and edges = [[0, 1], [1, 2], [3, 4]], what should your return? Is this case a valid tree?
- According to the definition of tree on Wikipedia:
- “a tree is an undirected graph in which any two vertices are connected by exactly one path.
- In other words, any connected graph without simple cycles is a tree.”
-
- Note: you can assume that no duplicate edges will appear in edges.
- Since all edges are undirected, [0, 1] is the same as [1, 0] and thus will not appear together in edges.
- */
 public class _261 {
 
     public static class Solution1 {
