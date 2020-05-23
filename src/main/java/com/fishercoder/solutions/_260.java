36f810cfb3cf0fc0d887d418cbec24c86c7902a7
Fisher Coder <fishercoder@gmail.com>
2020-05-23 08:20:28 -0700

    refactor 260

--- src/main/java/com/fishercoder/solutions/_260.java
+++ src/main/java/com/fishercoder/solutions/_260.java
@@ -3,22 +3,6 @@ package com.fishercoder.solutions;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * 260. Single Number III
- *
- * Given an array of numbers nums,
- * in which exactly two elements appear only once and all the other elements appear exactly twice.
- * Find the two elements that appear only once.
-
-For example:
-
-Given nums = [1, 2, 1, 3, 2, 5], return [3, 5].
-
-Note:
-The order of the result is not important. So in the above example, [5, 3] is also correct.
-Your algorithm should run in linear runtime complexity. Could you implement it using only constant space complexity?
-*/
-
 public class _260 {
 
     public static class Solution1 {
