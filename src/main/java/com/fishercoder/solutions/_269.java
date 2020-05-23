d903a3de427a3815b15bd4b96f336c762dbcc669
Fisher Coder <fishercoder@gmail.com>
2020-05-23 08:32:19 -0700

    refactor 269

--- src/main/java/com/fishercoder/solutions/_269.java
+++ src/main/java/com/fishercoder/solutions/_269.java
@@ -7,31 +7,6 @@ import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
 
-/**
- * 269. Alien Dictionary
- *
- * There is a new alien language which uses the latin alphabet.
- * However, the order among letters are unknown to you.
- * You receive a list of words from the dictionary, where words are sorted lexicographically by the rules of this new language.
- * Derive the order of letters in this language.
-
- For example,
- Given the following words in dictionary,
-
- [
- "wrt",
- "wrf",
- "er",
- "ett",
- "rftt"
- ]
- The correct order is: "wertf".
-
- Note:
- You may assume all letters are in lowercase.
- If the order is invalid, return an empty string.
- There may be multiple valid order of letters, return any one of them is fine.
- */
 public class _269 {
     public static class Solution1 {
 
