09210997d57aa93e7a6ababc1b3fda2a3f13b73d
Fisher Coder <fishercoder@gmail.com>
2020-05-23 08:18:47 -0700

    refactor 244

--- src/main/java/com/fishercoder/solutions/_244.java
+++ src/main/java/com/fishercoder/solutions/_244.java
@@ -5,21 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * 244. Shortest Word Distance II
- *
- * This is a follow up of Shortest Word Distance. The only difference is now you are given the list of words and your method will be called repeatedly many times with different parameters. How would you optimize it?
- * Design a class which receives a list of words in the constructor, and implements a method that takes two words word1 and word2 and return the shortest distance between these two words in the list.
-
- For example,
- Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
-
- Given word1 = “coding”, word2 = “practice”, return 3.
- Given word1 = "makes", word2 = "coding", return 1.
-
- Note:
- You may assume that word1 does not equal to word2, and word1 and word2 are both in the list.
- */
 public class _244 {
     public static class Solution1 {
         class WordDistance {
