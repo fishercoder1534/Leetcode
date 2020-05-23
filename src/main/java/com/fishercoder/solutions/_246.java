09c3a7ab907faf00706de72f8a9ea5a96102ab9b
Fisher Coder <fishercoder@gmail.com>
2020-05-23 08:19:26 -0700

    refactor 246

--- src/main/java/com/fishercoder/solutions/_246.java
+++ src/main/java/com/fishercoder/solutions/_246.java
@@ -5,14 +5,6 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-/**
- * 246. Strobogrammatic Number
- *
- * A strobogrammatic number is a number that looks the same when rotated 180 degrees (looked at upside down).
- * Write a function to determine if a number is strobogrammatic. The number is represented as a string.
- * For example, the numbers "69", "88", and "818" are all strobogrammatic.
- */
-
 public class _246 {
 
     public static class Solution1 {
