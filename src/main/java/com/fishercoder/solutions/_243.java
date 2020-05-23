3888a18f7c50d4d0749ef86dbaabae75e147190d
Fisher Coder <fishercoder@gmail.com>
2020-05-23 08:18:23 -0700

    refactor 243

--- src/main/java/com/fishercoder/solutions/_243.java
+++ src/main/java/com/fishercoder/solutions/_243.java
@@ -1,18 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 243. Shortest Word Distance
- *
- * Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
-
- For example,
- Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
-
- Given word1 = “coding”, word2 = “practice”, return 3.
- Given word1 = "makes", word2 = "coding", return 1.
-
- Note:
- You may assume that word1 does not equal to word2, and word1 and word2 are both in the list.*/
 public class _243 {
     public static class Solution1 {
         public int shortestDistance(String[] words, String word1, String word2) {
