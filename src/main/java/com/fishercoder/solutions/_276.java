8ff40746137dff3efb257442ce100720626ad7c0
Fisher Coder <fishercoder@gmail.com>
2020-05-24 08:54:31 -0700

    refactor 276

--- src/main/java/com/fishercoder/solutions/_276.java
+++ src/main/java/com/fishercoder/solutions/_276.java
@@ -1,17 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 276. Paint Fence
-
- There is a fence with n posts, each post can be painted with one of the k colors.
-
- You have to paint all the posts such that no more than two adjacent fence posts have the same color.
-
- Return the total number of ways you can paint the fence.
-
- Note:
- n and k are non-negative integers.*/
-
 public class _276 {
     public static class Solution1 {
         public int numWays(int n, int k) {
