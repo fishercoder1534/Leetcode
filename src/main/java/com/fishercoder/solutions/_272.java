7fec7d5022fa73444a7bbf0f6ae2d0caa08983cd
Fisher Coder <fishercoder@gmail.com>
2020-05-24 08:53:14 -0700

    refactor 272

--- src/main/java/com/fishercoder/solutions/_272.java
+++ src/main/java/com/fishercoder/solutions/_272.java
@@ -6,28 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
-/**
- * 272. Closest Binary Search Tree Value II
- *
- * Given a non-empty binary search tree and a target value, find k values in the BST that are closest to the target.
-
- Note:
- Given target value is a floating point.
- You may assume k is always valid, that is: k ≤ total nodes.
- You are guaranteed to have only one unique set of k values in the BST that are closest to the target.
- Follow up:
- Assume that the BST is balanced, could you solve it in less than O(n) runtime (where n = total nodes)?
-
- Hint:
-
- Consider implement these two helper functions:
- getPredecessor(N), which returns the next smaller node to N.
- getSuccessor(N), which returns the next larger node to N.
- Try to assume that each node has a parent pointer, it makes the problem much easier.
- Without parent pointer we just need to keep track of the path from the root to the current node using a stack.
- You would need two stacks to track the path in finding predecessor and successor node separately.
-
- */
 public class _272 {
 
     public static class Solution1 {
