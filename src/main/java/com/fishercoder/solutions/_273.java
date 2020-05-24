1387c0fa085edbe40a1b743cd1be8f22c28d7d72
Fisher Coder <fishercoder@gmail.com>
2020-05-24 08:53:37 -0700

    refactor 273

--- src/main/java/com/fishercoder/solutions/_273.java
+++ src/main/java/com/fishercoder/solutions/_273.java
@@ -1,20 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 273. Integer to English Words
- *
- * Convert a non-negative integer to its english words representation. Given input is guaranteed to be less than 231 - 1.
-
- For example,
- 123 -> "One Hundred Twenty Three"
- 12345 -> "Twelve Thousand Three Hundred Forty Five"
- 1234567 -> "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"
-
- Hint:
- Did you see a pattern in dividing the number into chunk of words? For example, 123 and 123000.
- Group the number by thousands (3 digits). You can write a helper function that takes a number less than 1000 and convert just that chunk to words.
- There are many edge cases. What are some good test cases? Does your code work with input such as 0? Or 1000010? (middle chunk is zero and should not be printed out)
- */
 public class _273 {
 
     public static class Solution1 {
