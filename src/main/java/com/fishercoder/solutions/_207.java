f179db82e58d0dd8681b0cf8d759d6d768f18d7a
Fisher Coder <fishercoder@gmail.com>
2020-05-26 07:52:32 -0700

    refactor 207

--- src/main/java/com/fishercoder/solutions/_207.java
+++ src/main/java/com/fishercoder/solutions/_207.java
@@ -2,11 +2,14 @@ package com.fishercoder.solutions;
 
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Set;
 
 public class _207 {
 
     public static class Solution1 {
+        /**Kahn's algorithm for topological sorting*/
         public boolean canFinish(int numCourses, int[][] prerequisites) {
             int[] indegree = new int[numCourses];
             for (int[] prereq : prerequisites) {
@@ -38,6 +41,44 @@ public class _207 {
 
             for (int i : indegree) {
                 if (i != 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    public static class Solution2 {
+        /**
+         * BFS
+         */
+        public boolean canFinish(int numCourses, int[][] prerequisites) {
+            int[] indegree = new int[numCourses];
+            for (int[] pre : prerequisites) {
+                indegree[pre[0]]++;
+            }
+            Queue<Integer> queue = new LinkedList();
+            for (int i = 0; i < numCourses; i++) {
+                if (indegree[i] == 0) {
+                    queue.offer(i);
+                }
+            }
+            if (queue.isEmpty()) {
+                return false;
+            }
+            while (!queue.isEmpty()) {
+                int course = queue.poll();
+                for (int[] pre : prerequisites) {
+                    if (pre[1] == course) {
+                        indegree[pre[0]]--;
+                        if (indegree[pre[0]] == 0) {
+                            queue.offer(pre[0]);
+                        }
+                    }
+                }
+            }
+            for (int degree : indegree) {
+                if (degree != 0) {
                     return false;
                 }
             }
