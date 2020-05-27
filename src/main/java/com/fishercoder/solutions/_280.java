84e901a9126d29a301d79c6bb2cc33523d192d00
Fisher Coder <fishercoder@gmail.com>
2020-05-27 09:18:16 -0700

    refactor 280

--- src/main/java/com/fishercoder/solutions/_280.java
+++ src/main/java/com/fishercoder/solutions/_280.java
@@ -1,8 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * Given an unsorted array nums, reorder it in-place such that nums[0] <= nums[1] >= nums[2] <= nums[3]....
-For example, given nums = [3, 5, 2, 1, 6, 4], one possible answer is [1, 6, 2, 5, 3, 4]*/
 public class _280 {
     public static class Solution1 {
         public void wiggleSort(int[] nums) {
