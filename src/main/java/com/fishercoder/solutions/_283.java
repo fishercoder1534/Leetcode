cdee4748083fc04605374c3bc37f145ac15663e7
Fisher Coder <fishercoder@gmail.com>
2020-05-28 10:21:29 -0700

    refactor 283

--- src/main/java/com/fishercoder/solutions/_283.java
+++ src/main/java/com/fishercoder/solutions/_283.java
@@ -1,15 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 283. Move Zeroes
- *
- * Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
- *
- * For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
- *
- * Note:
- * You must do this in-place without making a copy of the array.
- * Minimize the total number of operations.*/
 public class _283 {
     public static class Solution1 {
         public void moveZeroes(int[] nums) {
