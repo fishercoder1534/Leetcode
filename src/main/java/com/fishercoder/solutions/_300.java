47d99b6e09718628fd6fd1ed9ba4b64963b2a1c3
Fisher Coder <fishercoder@gmail.com>
2020-05-28 10:29:14 -0700

    refactor 300

--- src/main/java/com/fishercoder/solutions/_300.java
+++ src/main/java/com/fishercoder/solutions/_300.java
@@ -2,19 +2,6 @@ package com.fishercoder.solutions;
 
 import java.util.Arrays;
 
-/**
- * 300. Longest Increasing Subsequence
- *
- * Given an unsorted array of integers, find the length of longest increasing subsequence.
- For example,
- Given [10, 9, 2, 5, 3, 7, 101, 18],
- The longest increasing subsequence is [2, 3, 7, 101], therefore the length is 4.
- Note that there may be more than one LIS combination, it is only necessary for you to return the length.
-
- Your algorithm should run in O(n2) complexity.
-
- Follow up: Could you improve it to O(nlogn) time complexity?
- */
 public class _300 {
 
     public static class Solution1 {
