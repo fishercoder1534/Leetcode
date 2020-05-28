a9640a3c651f840224c1c498e18c9b55161681ed
Fisher Coder <fishercoder@gmail.com>
2020-05-28 10:27:53 -0700

    refactor 297

--- src/main/java/com/fishercoder/solutions/_297.java
+++ src/main/java/com/fishercoder/solutions/_297.java
@@ -5,31 +5,6 @@ import com.fishercoder.common.classes.TreeNode;
 import java.util.LinkedList;
 import java.util.Queue;
 
-/**
- * 297. Serialize and Deserialize Binary Tree
- *
- * Serialization is the process of converting a data structure or object into a sequence of bits
- * so that it can be stored in a file or memory buffer,
- * or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
- * Design an algorithm to serialize and deserialize a binary tree.
- * There is no restriction on how your serialization/deserialization algorithm should work.
- * You just need to ensure that a binary tree can be serialized to a string and this string can
- * be deserialized to the original tree structure.
-
- For example, you may serialize the following tree
-
-     1
-    / \
-   2   3
-  / \
- 4   5
-
- as "[1,2,3,null,null,4,5]",
- just the same as how LeetCode OJ serializes a binary tree.
- You do not necessarily need to follow this format, so please be creative and come up with different approaches yourself.
-
- Note: Do not use class member/global/static variables to store states. Your serialize and deserialize algorithms should be stateless.
- */
 public class _297 {
 
     public static class Solution1 {
