b2a77f9f51e174f3bf643ba74f03d3decdb7b9e1
Fisher Coder <fishercoder@gmail.com>
2020-05-28 10:26:17 -0700

    refactor 293

--- src/main/java/com/fishercoder/solutions/_293.java
+++ src/main/java/com/fishercoder/solutions/_293.java
@@ -2,21 +2,7 @@ package com.fishercoder.solutions;
 
 import java.util.ArrayList;
 import java.util.List;
-/**
- * 293. Flip Game
- *
- * You are playing the following Flip Game with your friend: Given a string that contains only
- * these two characters: + and -, you and your friend take turns to flip two consecutive "++" into "--".
- * The game ends when a person can no longer make a move and therefore the other person will be the winner.
- Write a function to compute all possible states of the string after one valid move.
- For example, given s = "++++", after one move, it may become one of the following states:
 
- [
- "--++",
- "+--+",
- "++--"
- ]
- If there is no valid move, return an empty list [].*/
 public class _293 {
 
     public static class Solutoin1 {
