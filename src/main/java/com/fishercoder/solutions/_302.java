9e87c6877d56561d5329e308477517485f1c8c89
Fisher Coder <fishercoder@gmail.com>
2020-05-28 18:24:38 -0700

    refactor 302

--- src/main/java/com/fishercoder/solutions/_302.java
+++ src/main/java/com/fishercoder/solutions/_302.java
@@ -1,23 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 302. Smallest Rectangle Enclosing Black Pixels
- *
- * An image is represented by a binary matrix with 0 as a white pixel and 1 as a black pixel.
- * The black pixels are connected, i.e., there is only one black region.
- * Pixels are connected horizontally and vertically.
- * Given the location (x, y) of one of the black pixels, return the area of the smallest (axis-aligned) rectangle that encloses all black pixels.
-
- For example, given the following image:
-
- [
- "0010",
- "0110",
- "0100"
- ]
- and x = 0, y = 2,
- Return 6.
- */
 public class _302 {
     public static class Solution1 {
         private char[][] image;
