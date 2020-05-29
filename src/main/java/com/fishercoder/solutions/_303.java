b9f3ffaf4b0d7a9ba814480833a5012d21ac8410
Fisher Coder <fishercoder@gmail.com>
2020-05-28 18:25:01 -0700

    refactor 303

--- src/main/java/com/fishercoder/solutions/_303.java
+++ src/main/java/com/fishercoder/solutions/_303.java
@@ -1,20 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 303. Range Sum Query - Immutable
- * Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
-
- Example:
- Given nums = [-2, 0, 3, -5, 2, -1]
-
- sumRange(0, 2) -> 1
- sumRange(2, 5) -> -1
- sumRange(0, 5) -> -3
-
- Note:
- You may assume that the array does not change.
- There are many calls to sumRange function.
- */
 public class _303 {
     public static class NumArray {
         int[] sums;
