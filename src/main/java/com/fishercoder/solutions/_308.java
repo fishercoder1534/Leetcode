d493ee5ce6f15b418fa44b5269dac512ebc76b9c
Fisher Coder <fishercoder@gmail.com>
2020-06-01 07:31:13 -0700

    refactor 308

--- src/main/java/com/fishercoder/solutions/_308.java
+++ src/main/java/com/fishercoder/solutions/_308.java
@@ -1,31 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 308. Range Sum Query 2D - Mutable
- *
- * Given a 2D matrix matrix,
- * find the sum of the elements inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
-
- Range Sum Query 2D
- The above rectangle (with the red border) is defined by (row1, col1) = (2, 1) and (row2, col2) = (4, 3), which contains sum = 8.
-
- Example:
- Given matrix = [
- [3, 0, 1, 4, 2],
- [5, 6, 3, 2, 1],
- [1, 2, 0, 1, 5],
- [4, 1, 0, 1, 7],
- [1, 0, 3, 0, 5]
- ]
-
- sumRegion(2, 1, 4, 3) -> 8
- update(3, 2, 2)
- sumRegion(2, 1, 4, 3) -> 10
- Note:
- The matrix is only modifiable by the update function.
- You may assume the number of calls to update and sumRegion function is distributed evenly.
- You may assume that row1 ≤ row2 and col1 ≤ col2.
- */
 public class _308 {
     public static class Solution1 {
         public class NumMatrix {
@@ -67,7 +41,7 @@ public class _308 {
                     return 0;
                 }
                 return sum(row2 + 1, col2 + 1) + sum(row1, col1) - sum(row1, col2 + 1) - sum(
-                    row2 + 1, col1);
+                        row2 + 1, col1);
             }
 
             private int sum(int row, int col) {
