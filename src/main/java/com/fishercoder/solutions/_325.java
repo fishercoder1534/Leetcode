20327498f29ec026e1e0d3e546ec23a656435cb0
Fisher Coder <fishercoder@gmail.com>
2020-06-03 07:29:28 -0700

    refactor 325

--- src/main/java/com/fishercoder/solutions/_325.java
+++ src/main/java/com/fishercoder/solutions/_325.java
@@ -3,25 +3,6 @@ package com.fishercoder.solutions;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * 325. Maximum Size Subarray Sum Equals k
- *
- * Given an array nums and a target value k, find the maximum length of a subarray that sums to k. If there isn't one, return 0 instead.
- *
- * Note:
- * The sum of the entire nums array is guaranteed to fit within the 32-bit signed integer range.
- *
- * Example 1:
- * Given nums = [1, -1, 5, -2, 3], k = 3,
- * return 4. (because the subarray [1, -1, 5, -2] sums to 3 and is the longest)
- *
- * Example 2:
- * Given nums = [-2, -1, 2, 1], k = 1,
- * return 2. (because the subarray [-1, 2] sums to 1 and is the longest)
- *
- * Follow Up:
- * Can you do it in O(n) time?*/
-
 public class _325 {
 
     public static class Solution1 {
