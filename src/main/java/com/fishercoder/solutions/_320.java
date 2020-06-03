eadf3f9eff88ef659575aff9a1d3118bae3703ed
Fisher Coder <fishercoder@gmail.com>
2020-06-03 07:27:35 -0700

    refactor 320

--- src/main/java/com/fishercoder/solutions/_320.java
+++ src/main/java/com/fishercoder/solutions/_320.java
@@ -3,16 +3,6 @@ package com.fishercoder.solutions;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 320. Generalized Abbreviation
- *
- * Write a function to generate the generalized abbreviations of a word.
-
- Example:
-
- Given word = "word", return the following list (order does not matter):
- ["word", "1ord", "w1rd", "wo1d", "wor1", "2rd", "w2d", "wo2", "1o1d", "1or1", "w1r1", "1o2", "2r1", "3d", "w3", "4"]
- */
 public class _320 {
 
     public static class Solution1 {
@@ -23,7 +13,7 @@ public class _320 {
         }
 
         private void backtrack(String word, List<String> result, int position, String current,
-            int count) {
+                               int count) {
             if (position == word.length()) {
                 if (count > 0) {
                     current += count;
@@ -32,7 +22,7 @@ public class _320 {
             } else {
                 backtrack(word, result, position + 1, current, count + 1);
                 backtrack(word, result, position + 1,
-                    current + (count > 0 ? count : "") + word.charAt(position), 0);
+                        current + (count > 0 ? count : "") + word.charAt(position), 0);
             }
         }
     }
