00f7656a3bcdc85f5cc24068cc5630f9b1ef1ca3
Fisher Coder <fishercoder@gmail.com>
2020-06-03 07:28:16 -0700

    refactor 322

--- src/main/java/com/fishercoder/solutions/_322.java
+++ src/main/java/com/fishercoder/solutions/_322.java
@@ -2,24 +2,6 @@ package com.fishercoder.solutions;
 
 import java.util.Arrays;
 
-/**
- * 322. Coin Change
- *
- * You are given coins of different denominations and a total amount of money amount.
- * Write a function to compute the fewest number of coins that you need to make up that amount.
- * If that amount of money cannot be made up by any combination of the coins, return -1.
-
- Example 1:
- coins = [1, 2, 5], amount = 11
- return 3 (11 = 5 + 5 + 1)
-
- Example 2:
- coins = [2], amount = 3
- return -1.
-
- Note:
- You may assume that you have an infinite number of each kind of coin.
- */
 public class _322 {
 
     public static class Solution1 {
