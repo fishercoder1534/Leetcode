0ac6f6908ac9e7dcc98af6734f25fdd101b585a4
Fisher Coder <fishercoder@gmail.com>
2020-06-04 07:50:25 -0700

    refactor 334

--- src/main/java/com/fishercoder/solutions/_334.java
+++ src/main/java/com/fishercoder/solutions/_334.java
@@ -1,27 +1,12 @@
 package com.fishercoder.solutions;
 
-/**
- * 334. Increasing Triplet Subsequence
- *
- * Given an unsorted array return whether an increasing subsequence of length 3 exists or not in the array.
- *
- * Formally the function should:
- * Return true if there exists i, j, k
- * such that arr[i] < arr[j] < arr[k] given 0 ≤ i < j < k ≤ n-1 else return false.
- * Your algorithm should run in O(n) time complexity and O(1) space complexity.
-
- Examples:
- Given [1, 2, 3, 4, 5],
- return true.
-
- Given [5, 4, 3, 2, 1],
- return false.
- */
 public class _334 {
 
     public static class Solution1 {
-        /**Time: O(n^2)
-         * Space: O(1)*/
+        /**
+         * Time: O(n^2)
+         * Space: O(1)
+         */
         public boolean increasingTriplet(int[] nums) {
             if (nums == null || nums.length == 0) {
                 return false;
