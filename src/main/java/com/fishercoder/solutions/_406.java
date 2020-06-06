03957d26a2f8e723ba5ef72335d3931651d2a443
Fisher Coder <fishercoder@gmail.com>
2020-06-06 08:16:03 -0700

    refactor 406

--- src/main/java/com/fishercoder/solutions/_406.java
+++ src/main/java/com/fishercoder/solutions/_406.java
@@ -5,26 +5,6 @@ import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 
-/**
- * 406. Queue Reconstruction by Height
- *
- * Suppose you have a random list of people standing in a queue.
- * Each person is described by a pair of integers (h, k),
- * where h is the height of the person and k is the number of people in
- * front of this person who have a height greater than or equal to h. Write an algorithm to reconstruct the queue.
-
- Note:
- The number of people is less than 1,100.
-
- Example
-
- Input:
- [[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
-
- Output:
- [[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
-
- */
 public class _406 {
 
     public static class Solution1 {
@@ -35,7 +15,7 @@ public class _406 {
             Arrays.sort(people, new Comparator<int[]>() {
                 public int compare(int[] p1, int[] p2) {
                     return p1[0] != p2[0] ? Integer.compare(p2[0], p1[0])
-                        : Integer.compare(p1[1], p2[1]);
+                            : Integer.compare(p1[1], p2[1]);
                 }
             });
             List<int[]> list = new LinkedList();
