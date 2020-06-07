13a25a952ebf46efa3f5799397b3a1d78b7b6af1
fishercoder1534 <fishercoder@gmail.com>
2020-06-06 17:46:57 -0700

    refactor 976

--- src/main/java/com/fishercoder/solutions/_976.java
+++ src/main/java/com/fishercoder/solutions/_976.java
@@ -2,15 +2,6 @@ package com.fishercoder.solutions;
 
 import java.util.Arrays;
 
-/**
- * 976. Largest Perimeter Triangle
- *
- * Given an array A of positive lengths, return the largest perimeter of a triangle with non-zero area, formed from 3
- * of these lengths.
- *
- * If it is impossible to form any triangle of non-zero area, return 0.
- * */
-
 public class _976 {
 
     public static class Solution1 {
