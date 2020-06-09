279ed91d8fd7fa13e318ab8fff63f997e5b726b9
Fisher Coder <fishercoder@gmail.com>
2020-06-09 06:34:08 -0700

    refactor 346

--- src/main/java/com/fishercoder/solutions/_346.java
+++ src/main/java/com/fishercoder/solutions/_346.java
@@ -3,18 +3,6 @@ package com.fishercoder.solutions;
 import java.util.LinkedList;
 import java.util.Queue;
 
-/**
- * 346. Moving Average from Data Stream
- *
- * Given a stream of integers and a window size, calculate the moving average of all integers in the sliding window.
-
- For example,
- MovingAverage m = new MovingAverage(3);
- m.next(1) = 1
- m.next(10) = (1 + 10) / 2
- m.next(3) = (1 + 10 + 3) / 3
- m.next(5) = (10 + 3 + 5) / 3
- */
 public class _346 {
 
     public static class Solution1 {
