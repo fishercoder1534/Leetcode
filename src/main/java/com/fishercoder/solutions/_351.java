7e15f7c31e65fc46a1b0283d6217a67a04004330
Fisher Coder <fishercoder@gmail.com>
2020-06-10 11:50:47 -0700

    refactor 351

--- src/main/java/com/fishercoder/solutions/_351.java
+++ src/main/java/com/fishercoder/solutions/_351.java
@@ -1,37 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 351. Android Unlock Patterns
- *
- * Given an Android 3x3 key lock screen and two integers m and n, where 1 ≤ m ≤ n ≤ 9, count the total number of unlock patterns of the Android lock screen, which consist of minimum of m keys and maximum n keys.
-
- Rules for a valid pattern:
- Each pattern must connect at least m keys and at most n keys.
- All the keys must be distinct.
- If the line connecting two consecutive keys in the pattern passes through any other keys, the other keys must have previously selected in the pattern. No jumps through non selected key is allowed.
- The order of keys used matters.
-
- Explanation:
- | 1 | 2 | 3 |
- | 4 | 5 | 6 |
- | 7 | 8 | 9 |
- Invalid move: 4 - 1 - 3 - 6
- Line 1 - 3 passes through key 2 which had not been selected in the pattern.
-
- Invalid move: 4 - 1 - 9 - 2
- Line 1 - 9 passes through key 5 which had not been selected in the pattern.
-
- Valid move: 2 - 4 - 1 - 3 - 6
- Line 1 - 3 is valid because it passes through key 2, which had been selected in the pattern
-
- Valid move: 6 - 5 - 4 - 1 - 9 - 2
- Line 1 - 9 is valid because it passes through key 5, which had been selected in the pattern.
-
- Example:
- Given m = 1, n = 1, return 9.
-
-
- */
 public class _351 {
 
     public static class Solution1 {
@@ -50,9 +18,9 @@ public class _351 {
             visited = new boolean[10];
             int count = 0;
             count += dfs(1, 1, 0, m, n)
-                * 4;//1,3,7,9 are symmetric, so we only need to use 1 to do it once and then multiply the result by 4
+                    * 4;//1,3,7,9 are symmetric, so we only need to use 1 to do it once and then multiply the result by 4
             count += dfs(2, 1, 0, m, n)
-                * 4;//2,4,6,8 are symmetric, so we only need to use 1 to do it once and then multiply the result by 4
+                    * 4;//2,4,6,8 are symmetric, so we only need to use 1 to do it once and then multiply the result by 4
             count += dfs(5, 1, 0, m, n);
             return count;
         }
