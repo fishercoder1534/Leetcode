7da4e384e9a3a2c823dcc0022d94cf1e2303814e
Fisher Coder <fishercoder@gmail.com>
2020-06-10 11:53:29 -0700

    refactor 358

--- src/main/java/com/fishercoder/solutions/_358.java
+++ src/main/java/com/fishercoder/solutions/_358.java
@@ -6,28 +6,6 @@ import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
-/**
- * 358. Rearrange String k Distance Apart
- *
- * Given a non-empty string s and an integer k, rearrange the string such that the same characters are at least distance k from each other.
-
- All input strings are given in lowercase letters. If it is not possible to rearrange the string, return an empty string "".
- Example 1:
- s = "aabbcc", k = 3
- Result: "abcabc"
- The same letters are at least distance 3 from each other.
-
- Example 2:
- s = "aaabc", k = 3
- Answer: ""
- It is not possible to rearrange the string.
-
- Example 3:
- s = "aaadbbcc", k = 2
- Answer: "abacabcd"
- Another possible answer is: "abcabcda"
- The same letters are at least distance 2 from each other.
- */
 public class _358 {
 
     public static class Solution1 {
@@ -38,7 +16,7 @@ public class _358 {
             }
 
             PriorityQueue<Map.Entry<Character, Integer>> heap =
-                new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+                    new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
             heap.addAll(count.entrySet());
 
             Queue<Map.Entry<Character, Integer>> waitQueue = new LinkedList<>();
