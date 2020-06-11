91052c5485575f1878fe21e91fa03ddc41b11cc3
Fisher Coder <fishercoder@gmail.com>
2020-06-11 06:42:34 -0700

    refactor 363

--- src/main/java/com/fishercoder/solutions/_363.java
+++ src/main/java/com/fishercoder/solutions/_363.java
@@ -2,27 +2,11 @@ package com.fishercoder.solutions;
 
 import java.util.TreeSet;
 
-/**
- * 363. Max Sum of Rectangle No Larger Than K
- *
- * Given a non-empty 2D matrix matrix and an integer k,
- * find the max sum of a rectangle in the matrix such that its sum is no larger than k.
-
- Example:
- Given matrix = [
- [1,  0, 1],
- [0, -2, 3]
- ]
- k = 2
- The answer is 2. Because the sum of rectangle [[0, 1], [-2, 3]] is 2 and 2 is the max number no larger than k (k = 2).
-
- Note:
- The rectangle inside the matrix must have an area > 0.
- What if the number of rows is much larger than the number of columns?
- */
 public class _363 {
     public static class Solution1 {
-        /** reference: https://discuss.leetcode.com/topic/48854/java-binary-search-solution-time-complexity-min-m-n-2-max-m-n-log-max-m-n */
+        /**
+         * reference: https://discuss.leetcode.com/topic/48854/java-binary-search-solution-time-complexity-min-m-n-2-max-m-n-log-max-m-n
+         */
         public int maxSumSubmatrix(int[][] matrix, int k) {
             int row = matrix.length;
             if (row == 0) {
