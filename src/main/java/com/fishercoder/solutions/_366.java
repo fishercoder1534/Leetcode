5bdcfd71733bee8decfbf4a1c169fa047e69b5cd
Fisher Coder <fishercoder@gmail.com>
2020-06-11 06:43:42 -0700

    refactor 366

--- src/main/java/com/fishercoder/solutions/_366.java
+++ src/main/java/com/fishercoder/solutions/_366.java
@@ -5,36 +5,6 @@ import com.fishercoder.common.classes.TreeNode;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 366. Find Leaves of Binary Tree
- *
- * Given a binary tree, collect a tree's nodes as if you were doing this:
- * Collect and remove all leaves, repeat until the tree is empty.
-
- Example:
- Given binary tree
-     1
-    / \
-   2   3
-  / \
- 4  5
- Returns [4, 5, 3], [2], [1].
-
- Explanation:
- 1. Removing the leaves [4, 5, 3] would result in this tree:
-
-   1
-  /
- 2
- 2. Now removing the leaf [2] would result in this tree:
-
- 1
- 3. Now removing the leaf [1] would result in the empty tree:
-
- []
- Returns [4, 5, 3], [2], [1].
-
- */
 public class _366 {
 
     public static class Solution1 {
