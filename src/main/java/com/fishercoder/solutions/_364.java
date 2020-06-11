8dd97d1b308652b601acb2f1e4bb50b7381161fb
Fisher Coder <fishercoder@gmail.com>
2020-06-11 06:42:56 -0700

    refactor 364

--- src/main/java/com/fishercoder/solutions/_364.java
+++ src/main/java/com/fishercoder/solutions/_364.java
@@ -6,20 +6,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-/**
- * 364. Nested List Weight Sum II
- *
- * Given a nested list of integers, return the sum of all integers in the list weighted by their depth.
-
- Each element is either an integer, or a list -- whose elements may also be integers or other lists.
-
- Different from the previous question where weight is increasing from root to leaf, now the weight is defined from bottom up. i.e., the leaf level integers have weight 1, and the root level integers have the largest weight.
-
- Example 1:
- Given the list [[1,1],2,[1,1]], return 8. (four 1's at depth 1, one 2 at depth 2)
-
- Example 2:
- Given the list [1,[4,[6]]], return 17. (one 1 at depth 3, one 4 at depth 2, and one 6 at depth 1; 1*3 + 4*2 + 6*1 = 17)*/
 public class _364 {
 
     public static class Solution1 {
