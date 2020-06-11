88ba6b655373bbdba5282cd533125e85ff89d169
Fisher Coder <fishercoder@gmail.com>
2020-06-11 06:41:10 -0700

    refactor 361

--- src/main/java/com/fishercoder/solutions/_361.java
+++ src/main/java/com/fishercoder/solutions/_361.java
@@ -1,21 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 361. Bomb Enemy
- *
- * Given a 2D grid, each cell is either a wall 'W', an enemy 'E' or empty '0' (the number zero), return the maximum enemies you can kill using one bomb.
- The bomb kills all the enemies in the same row and column from the planted point until it hits the wall since the wall is too strong to be destroyed.
- Note that you can only put the bomb at an empty cell.
-
- Example:
- For the given grid
-
- 0 E 0 0
- E 0 W E
- 0 E 0 0
-
- return 3. (Placing a bomb at (1,1) kills 3 enemies)
- */
 public class _361 {
 
     public static class Solution1 {
