2ce1c6e29d334f89c47e2eeb63a555907545c984
Fisher Coder <fishercoder@gmail.com>
2020-06-13 06:13:04 -0700

    refactor 369

--- src/main/java/com/fishercoder/solutions/_369.java
+++ src/main/java/com/fishercoder/solutions/_369.java
@@ -2,22 +2,6 @@ package com.fishercoder.solutions;
 
 import com.fishercoder.common.classes.ListNode;
 
-/**
- * 369. Plus One Linked List
- *
- * Given a non-negative integer represented as non-empty a singly linked list of digits, plus one to the integer.
-
- You may assume the integer do not contain any leading zero, except the number 0 itself.
-
- The digits are stored such that the most significant digit is at the head of the list.
-
- Example:
- Input:
- 1->2->3
-
- Output:
- 1->2->4
- */
 public class _369 {
 
     public static class Solution1 {
