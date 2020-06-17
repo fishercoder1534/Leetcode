5544855171bcac62f5525e7e619d2fda8eeb9546
Fisher Coder <fishercoder@gmail.com>
2020-06-17 05:36:17 -0700

    refactor 130

--- src/main/java/com/fishercoder/solutions/_130.java
+++ src/main/java/com/fishercoder/solutions/_130.java
@@ -3,26 +3,6 @@ package com.fishercoder.solutions;
 import java.util.LinkedList;
 import java.util.Queue;
 
-/**
- * 130. Surrounded Regions
- *
- * Given a 2D board containing 'X' and 'O' (the letter O), capture all regions surrounded by 'X'.
-
- A region is captured by flipping all 'O's into 'X's in that surrounded region.
-
- For example,
- X X X X
- X O O X
- X X O X
- X O X X
-
- After running your function, the board should be:
-
- X X X X
- X X X X
- X X X X
- X O X X
- */
 public class _130 {
 
     public static class Solution1 {
