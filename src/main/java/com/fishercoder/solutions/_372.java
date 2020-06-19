4a1631a2d6ed8c257789cd7d30532f3f1925b9df
Fisher Coder <fishercoder@gmail.com>
2020-06-19 06:19:05 -0700

    refactor 372

--- src/main/java/com/fishercoder/solutions/_372.java
+++ src/main/java/com/fishercoder/solutions/_372.java
@@ -1,23 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 372. Super Pow
- *
- * Your task is to calculate ab mod 1337 where a is a positive integer and b is an extremely large positive integer given in the form of an array.
-
- Example1:
-
- a = 2
- b = [3]
-
- Result: 8
- Example2:
-
- a = 2
- b = [1,0]
-
- Result: 1024
- */
 public class _372 {
     public static class Solution1 {
         public int superPow(int a, int[] b) {
