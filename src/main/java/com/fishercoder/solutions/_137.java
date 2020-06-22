9dd87cb3ca9e967f8127443d7adedc60e3c18eaf
Fisher Coder <fishercoder@gmail.com>
2020-06-22 07:04:45 -0700

    refactor 137

--- src/main/java/com/fishercoder/solutions/_137.java
+++ src/main/java/com/fishercoder/solutions/_137.java
@@ -3,45 +3,39 @@ package com.fishercoder.solutions;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * 137. Single Number II
-
-Given an array of integers, every element appears three times except for one. Find that single one.
-
-Note:
-Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
-*/
 public class _137 {
 
-  public static class Solution1 {
-    public int singleNumber(int[] nums) {
-      Map<Integer, Integer> map = new HashMap();
-      for (int i : nums) {
-        map.put(i, map.getOrDefault(i, 0) + 1);
-      }
-      for (int key : map.keySet()) {
-        if (map.get(key) != 3) {
-          return key;
+    public static class Solution1 {
+        public int singleNumber(int[] nums) {
+            Map<Integer, Integer> map = new HashMap();
+            for (int i : nums) {
+                map.put(i, map.getOrDefault(i, 0) + 1);
+            }
+            for (int key : map.keySet()) {
+                if (map.get(key) != 3) {
+                    return key;
+                }
+            }
+            return 0;
         }
-      }
-      return 0;
     }
-  }
 
-  public static class Solution2 {
-    /** Credit: https://discuss.leetcode.com/topic/11877/detailed-explanation-and-generalization-of-the-bitwise-operation-method-for-single-numbers/2 */
-    public int singleNumber(int[] nums) {
-      int counter1 = 0;
-      int counter2 = 0;
-      int mask = 0;
-      for (int num : nums) {
-        counter2 ^= counter1 & num;
-        counter1 ^= num;
-        mask = ~(counter1 & counter2);
-        counter1 &= mask;
-        counter2 &= mask;
-      }
-      return counter1;
+    public static class Solution2 {
+        /**
+         * Credit: https://discuss.leetcode.com/topic/11877/detailed-explanation-and-generalization-of-the-bitwise-operation-method-for-single-numbers/2
+         */
+        public int singleNumber(int[] nums) {
+            int counter1 = 0;
+            int counter2 = 0;
+            int mask = 0;
+            for (int num : nums) {
+                counter2 ^= counter1 & num;
+                counter1 ^= num;
+                mask = ~(counter1 & counter2);
+                counter1 &= mask;
+                counter2 &= mask;
+            }
+            return counter1;
+        }
     }
-  }
 }
