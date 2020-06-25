04da4df428445d67d96339529073da3fdbd1d11e
Fisher Coder <fishercoder@gmail.com>
2020-06-25 07:52:58 -0700

    refactor 139

--- src/main/java/com/fishercoder/solutions/_139.java
+++ src/main/java/com/fishercoder/solutions/_139.java
@@ -2,28 +2,12 @@ package com.fishercoder.solutions;
 
 import java.util.List;
 
-/**
- * 139. Word Break
- *
- * Given a non-empty string s and a dictionary wordDict containing a list of non-empty words,
- * determine if s can be segmented into a space-separated sequence of one or more dictionary words.
- * You may assume the dictionary does not contain duplicate words.
-
- For example, given
- s = "leetcode",
- dict = ["leet", "code"].
-
- Return true because "leetcode" can be segmented as "leet code".
-
- UPDATE (2017/1/4):
- The wordDict parameter had been changed to a list of strings (instead of a set of strings).
- Please reload the code definition to get the latest changes.
- */
-
 public class _139 {
 
     public static class Solution1 {
-        /** this beats 70.46% submission. */
+        /**
+         * this beats 70.46% submission.
+         */
         public boolean wordBreak(String s, List<String> wordDict) {
             int n = s.length();
             boolean[] dp = new boolean[n + 1];
@@ -85,7 +69,7 @@ public class _139 {
             dp[0] = true;
             for (int i = 1; i <= n; i++) {
                 for (int lastWordLength = 1; lastWordLength <= i && lastWordLength <= maxLen;
-                    lastWordLength++) {
+                     lastWordLength++) {
                     if (!dp[i - lastWordLength]) {
                         continue;
                     }
