da70c71a0b027f5f1007a34130d3f86ced126573
Fisher Coder <fishercoder@gmail.com>
2020-06-26 07:42:51 -0700

    refactor 383

--- src/main/java/com/fishercoder/solutions/_383.java
+++ src/main/java/com/fishercoder/solutions/_383.java
@@ -1,20 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 383 Ransom Note
- *
- * Given an arbitrary ransom note string and another string containing letters
- * from all the magazines, write a function that will return true if
- * the ransom note can be constructed from the magazines ; otherwise, it will return false.
- * Each letter in the magazine string can only be used once in your ransom note.
-
- Note:
-
- You may assume that both strings contain only lowercase letters.
- canConstruct("a", "b") -> false
- canConstruct("aa", "ab") -> false
- canConstruct("aa", "aab") -> true
-*/
 public class _383 {
 
     public static class Solution1 {
