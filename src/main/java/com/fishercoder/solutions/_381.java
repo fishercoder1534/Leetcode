74a8534f0028d0b6604a910eb4c88e349c5e2a74
Fisher Coder <fishercoder@gmail.com>
2020-06-26 07:42:12 -0700

    refactor 381

--- src/main/java/com/fishercoder/solutions/_381.java
+++ src/main/java/com/fishercoder/solutions/_381.java
@@ -4,36 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
-/**381. Insert Delete GetRandom O(1) - Duplicates allowed
- *
-Design a data structure that supports all following operations in average O(1) time.
-
-Note: Duplicate elements are allowed.
-insert(val): Inserts an item val to the collection.
-remove(val): Removes an item val from the collection if present.
-getRandom: Returns a random element from current collection of elements. The probability of each element being returned is linearly related to the number of same value the collection contains.
-Example:
-
-// Init an empty collection.
-_381 collection = new _381();
-
-// Inserts 1 to the collection. Returns true as the collection did not contain 1.
-collection.insert(1);
-
-// Inserts another 1 to the collection. Returns false as the collection contained 1. Collection now contains [1,1].
-collection.insert(1);
-
-// Inserts 2 to the collection, returns true. Collection now contains [1,1,2].
-collection.insert(2);
-
-// getRandom should return 1 with the probability 2/3, and returns 2 with the probability 1/3.
-collection.getRandom();
-
-// Removes 1 from the collection, returns true. Collection now contains [1,2].
-collection.remove(1);
-
-// getRandom should return 1 and 2 both equally likely.
-collection.getRandom();*/
 public class _381 {
     public static class Solution1 {
 
@@ -65,7 +35,7 @@ public class _381 {
                 contains = false;
             }
             forwardMap.put(val,
-                index);//this will overwrite the preivous key with a new index if the key already exists
+                    index);//this will overwrite the preivous key with a new index if the key already exists
             reverseMap.put(index, val);
             index++;
             return contains;
