d185bd79afe8e7e66609dea9a7504448785ef04f
Fisher Coder <fishercoder@gmail.com>
2020-06-26 07:43:25 -0700

    refactor 384

--- src/main/java/com/fishercoder/solutions/_384.java
+++ src/main/java/com/fishercoder/solutions/_384.java
@@ -1,23 +1,5 @@
 package com.fishercoder.solutions;
-/**
- * 384. Shuffle an Array
-Shuffle a set of numbers without duplicates.
 
-Example:
-
-// Init an array with set 1, 2, and 3.
-int[] nums = {1,2,3};
-Solution solution = new Solution(nums);
-
-// Shuffle the array [1,2,3] and return its result.
- Any permutation of [1,2,3] must equally likely to be returned.
-solution.shuffle();
-
-// Resets the array back to its original configuration [1,2,3].
-solution.reset();
-
-// Returns the random shuffling of array [1,2,3].
-solution.shuffle();*/
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
