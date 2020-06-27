b4f2bb1ba91a15cac16f5a4a3acaf6dee1a483d1
Fisher Coder <fishercoder@gmail.com>
2020-06-27 06:55:53 -0700

    refactor 390

--- src/main/java/com/fishercoder/solutions/_390.java
+++ src/main/java/com/fishercoder/solutions/_390.java
@@ -1,29 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 390. Elimination Game
- *
- * There is a list of sorted integers from 1 to n.
- * Starting from left to right,
- * remove the first number and every other number afterward until you reach the end of the list.
- * Repeat the previous step again, but this time from right to left,
- * remove the right most number and every other number from the remaining numbers.
- * We keep repeating the steps again, alternating left to right and right to left, until a single number remains.
- * Find the last number that remains starting with a list of length n.
-
- Example:
-
- Input:
- n = 9,
- 1 2 3 4 5 6 7 8 9
- 2 4 6 8
- 2 6
- 6
-
- Output:
- 6
-
- */
 public class _390 {
 
     public static class Solution1 {
