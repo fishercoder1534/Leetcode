4682f283ee895c07df54643efbbbbc1f5c986218
Fisher Coder <fishercoder@gmail.com>
2020-06-27 06:53:55 -0700

    refactor 385

--- src/main/java/com/fishercoder/solutions/_385.java
+++ src/main/java/com/fishercoder/solutions/_385.java
@@ -4,36 +4,6 @@ import com.fishercoder.common.classes.NestedInteger;
 
 import java.util.Stack;
 
-/**
- * 385. Mini Parser
- *
- * Given a nested list of integers represented as a string,
- * implement a parser to deserialize it.
-
- Each element is either an integer, or a list --
- whose elements may also be integers or other lists.
-
- Note: You may assume that the string is well-formed:
-
- String is non-empty.
- String does not contain white spaces.
- String contains only digits 0-9, [, - ,, ].
- Example 1:
-
- Given s = "324",
-
- You should return a NestedInteger object which contains a single integer 324.
- Example 2:
-
- Given s = "[123,[456,[789]]]",
-
- Return a NestedInteger object containing a nested list with 2 elements:
-
- 1. An integer containing value 123.
- 2. A nested list containing two elements:
- i.  An integer containing value 456.
- ii. A nested list with one element:
- a. An integer containing value 789.*/
 public class _385 {
 
     public static class Solution1 {
@@ -58,7 +28,7 @@ public class _385 {
             if (s.charAt(i) != '[') {
                 sb.setLength(0);
                 while (i < s.length() && ((Character.getNumericValue(s.charAt(i)) < 10
-                    && Character.getNumericValue(s.charAt(i)) >= 0) || s.charAt(i) == '-')) {
+                        && Character.getNumericValue(s.charAt(i)) >= 0) || s.charAt(i) == '-')) {
                     sb.append(s.charAt(i));
                     i++;
                 }
@@ -87,8 +57,8 @@ public class _385 {
                         // then it must be a number
                         sb.setLength(0);
                         while (i < s.length()
-                            && ((Character.getNumericValue(s.charAt(i)) < 10 && Character
-                            .getNumericValue(s.charAt(i)) >= 0) || s.charAt(i) == '-')) {
+                                && ((Character.getNumericValue(s.charAt(i)) < 10 && Character
+                                .getNumericValue(s.charAt(i)) >= 0) || s.charAt(i) == '-')) {
                             sb.append(s.charAt(i));
                             i++;
                         }
