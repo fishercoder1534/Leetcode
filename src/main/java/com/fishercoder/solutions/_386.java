24e24b20a0d94c7df6f0fdff25a7fe068bb5f293
Fisher Coder <fishercoder@gmail.com>
2020-06-27 06:54:24 -0700

    refactor 386

--- src/main/java/com/fishercoder/solutions/_386.java
+++ src/main/java/com/fishercoder/solutions/_386.java
@@ -3,38 +3,28 @@ package com.fishercoder.solutions;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 386. Lexicographical Numbers
- *
- * Given an integer n, return 1 - n in lexicographical order.
- *
- * For example, given 13, return: [1,10,11,12,13,2,3,4,5,6,7,8,9].
- *
- * Please optimize your algorithm to use less time and space. The input size may be as large as
- * 5,000,000.
- */
 public class _386 {
-  public static class Solution1 {
-    //Radix sort doesn't apply here! Don't confuse yourself!
+    public static class Solution1 {
+        //Radix sort doesn't apply here! Don't confuse yourself!
 
-    //rewrote their solution from Python to Java:https://discuss.leetcode.com/topic/54986/python-memory-limit-exceeded-for-problem-386/17
-    public List<Integer> lexicalOrder(int n) {
-      List<Integer> result = new ArrayList();
-      int i = 1;
-      while (true) {
-        result.add(i);
-        if (i * 10 <= n) {
-          i *= 10;
-        } else {
-          while (i % 10 == 9 || i == n) {
-            i /= 10;
-          }
-          if (i == 0) {
-            return result;
-          }
-          i++;
+        //rewrote their solution from Python to Java:https://discuss.leetcode.com/topic/54986/python-memory-limit-exceeded-for-problem-386/17
+        public List<Integer> lexicalOrder(int n) {
+            List<Integer> result = new ArrayList();
+            int i = 1;
+            while (true) {
+                result.add(i);
+                if (i * 10 <= n) {
+                    i *= 10;
+                } else {
+                    while (i % 10 == 9 || i == n) {
+                        i /= 10;
+                    }
+                    if (i == 0) {
+                        return result;
+                    }
+                    i++;
+                }
+            }
         }
-      }
     }
-  }
 }
