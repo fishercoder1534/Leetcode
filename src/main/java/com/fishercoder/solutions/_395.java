37de384693227d05a6c53e9be43fc04b9c40a2d8
Fisher Coder <fishercoder@gmail.com>
2020-06-29 06:58:06 -0700

    refactor 395

--- src/main/java/com/fishercoder/solutions/_395.java
+++ src/main/java/com/fishercoder/solutions/_395.java
@@ -1,35 +1,10 @@
 package com.fishercoder.solutions;
 
-/**
- * 395. Longest Substring with At Least K Repeating Characters
- *
- *  Find the length of the longest substring T of a given string
- *  (consists of lowercase letters only)
- *  such that every character in T appears no less than k times.
-
- Example 1:
- Input:
- s = "aaabb", k = 3
-
- Output:
- 3
-
- The longest substring is "aaa", as 'a' is repeated 3 times.
-
-
- Example 2:
- Input:
- s = "ababbc", k = 2
-
- Output:
- 5
-
- The longest substring is "ababb", as 'a' is repeated 2 times and 'b' is repeated 3 times.
- */
-
 public class _395 {
     public static class Solution1 {
-        /**Reference: https://discuss.leetcode.com/topic/57372/java-divide-and-conquer-recursion-solution*/
+        /**
+         * Reference: https://discuss.leetcode.com/topic/57372/java-divide-and-conquer-recursion-solution
+         */
         public int longestSubstring(String s, int k) {
             return findLongestSubstring(s.toCharArray(), 0, s.length(), k);
         }
