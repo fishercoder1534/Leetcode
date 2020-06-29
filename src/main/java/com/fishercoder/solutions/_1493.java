0c767f0c07ead8d30454afd8ae6294fb3c567c0b
Fisher Coder <fishercoder@gmail.com>
2020-06-28 19:16:20 -0700

    refactor 1493

--- src/main/java/com/fishercoder/solutions/_1493.java
+++ src/main/java/com/fishercoder/solutions/_1493.java
@@ -39,4 +39,37 @@ public class _1493 {
             return longest;
         }
     }
+
+    public static class Solution2 {
+        /**
+         * Sliding window solution
+         * Credit: https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/discuss/708112/JavaC%2B%2BPython-Sliding-Window-at-most-one-0
+         * <p>
+         * we initialize k to be one, meaning we could have at most one zero in the sliding window
+         * i is the left pointer
+         * j is the right pointer
+         * when j encounters a zero, we'll decrement k by one;
+         * and once k becomes negative, that means we have more than one zeroes in the sliding window,
+         * so we'll have to move left pointer to the right until k becomes not negative;
+         * along this process, we use result to hold the max length of this sliding window
+         */
+        public int longestSubarray(int[] nums) {
+            int i = 0;
+            int k = 1;
+            int result = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] == 0) {
+                    k--;
+                }
+                while (k < 0) {
+                    if (nums[i] == 0) {
+                        k++;
+                    }
+                    i++;
+                }
+                result = Math.max(result, j - i);
+            }
+            return result;
+        }
+    }
 }
