7401afeed6afaa5ee6d6d77b948550a3e434e659
Fisher Coder <fishercoder@gmail.com>
2020-06-30 06:21:05 -0700

    refactor 408

--- src/main/java/com/fishercoder/solutions/_408.java
+++ src/main/java/com/fishercoder/solutions/_408.java
@@ -1,28 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 408. Valid Word Abbreviation
- *
- * Given a non-empty string s and an abbreviation abbr,
- * return whether the string matches with the given abbreviation.
- * A string such as "word" contains only the following valid abbreviations:
-
- ["word", "1ord", "w1rd", "wo1d", "wor1", "2rd", "w2d", "wo2", "1o1d", "1or1", "w1r1", "1o2", "2r1", "3d", "w3", "4"]
- Notice that only the above abbreviations are valid abbreviations of the string "word".
- Any other string is not a valid abbreviation of "word".
-
- Note:
- Assume s contains only lowercase letters and abbr contains only lowercase letters and digits.
-
- Example 1:
- Given s = "internationalization", abbr = "i12iz4n":
-
- Return true.
- Example 2:
- Given s = "apple", abbr = "a2e":
-
- Return false.
- */
 public class _408 {
 
     public static class Solution1 {
@@ -68,12 +45,12 @@ public class _408 {
                             stringBuilder.setLength(0);
                         }
                         if ((i >= abbrChars.length && j < wordChars.length) || (i < abbrChars.length
-                            && j >= wordChars.length)) {
+                                && j >= wordChars.length)) {
                             return false;
                         }
                         if (i < abbrChars.length
-                            && j < wordChars.length
-                            && abbrChars[i] != wordChars[j]) {
+                                && j < wordChars.length
+                                && abbrChars[i] != wordChars[j]) {
                             return false;
                         }
                         if (j > wordChars.length && i <= abbrChars.length) {
