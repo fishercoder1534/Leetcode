17c977327bfa49fc5ca866b14cec04e797058219
Fisher Coder <fishercoder@gmail.com>
2020-06-30 06:17:58 -0700

    refactor 397

--- src/main/java/com/fishercoder/solutions/_397.java
+++ src/main/java/com/fishercoder/solutions/_397.java
@@ -6,37 +6,6 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Set;
 
-/**
- * 397. Integer Replacement
- *
- * Given a positive integer n and you can do operations as follow:
-
- If n is even, replace n with n/2.
- If n is odd, you can replace n with either n + 1 or n - 1.
- What is the minimum number of replacements needed for n to become 1?
-
- Example 1:
-
- Input:
- 8
-
- Output:
- 3
-
- Explanation:
- 8 -> 4 -> 2 -> 1
- Example 2:
-
- Input:
- 7
-
- Output:
- 4
-
- Explanation:
- 7 -> 8 -> 4 -> 2 -> 1
- or
- 7 -> 6 -> 3 -> 2 -> 1*/
 public class _397 {
 
     public static class Solution1 {
@@ -44,7 +13,7 @@ public class _397 {
             long min = Long.MAX_VALUE;
             Set<long[]> set = new HashSet();
             Queue<long[]> q = new LinkedList();
-            long[] pair = new long[] {n, 0};
+            long[] pair = new long[]{n, 0};
             q.offer(pair);
             while (!q.isEmpty()) {
                 int size = q.size();
