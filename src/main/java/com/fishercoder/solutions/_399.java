58f1227ad4513df62e203e9f62038520c173290e
Fisher Coder <fishercoder@gmail.com>
2020-07-01 06:56:36 -0700

    refaactor 399

--- src/main/java/com/fishercoder/solutions/_399.java
+++ src/main/java/com/fishercoder/solutions/_399.java
@@ -6,34 +6,12 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
-/**
- * 399. Evaluate Division
- *
- * Equations are given in the format A / B = k, where A and B are variables represented as strings, and k is a real number (floating point number).
- * Given some queries, return the answers. If the answer does not exist, return -1.0.
-
- Example:
- Given a / b = 2.0, b / c = 3.0.
- queries are: a / c = ?, b / a = ?, a / e = ?, a / a = ?, x / x = ? .
- return [6.0, 0.5, -1.0, 1.0, -1.0 ].
-
- The input is: vector<pair<string, string>> equations, vector<double>& values, vector<pair<string, string>> queries ,
- where equations.size() == values.size(), and the values are positive. This represents the equations. Return vector<double>.
-
- According to the example above:
-
- equations = [ ["a", "b"], ["b", "c"] ],
- values = [2.0, 3.0],
- queries = [ ["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"] ].
-
- The input is always valid. You may assume that evaluating the queries will result in no division by zero and there is no contradiction.
- */
 public class _399 {
 
     public static class Solution1 {
         /**
          * Credit: https://discuss.leetcode.com/topic/59146/java-ac-solution-using-graph
-         *
+         * <p>
          * Image a/b = k as a link between node a and b, the weight from a to b is k, the reverse link
          * is 1/k. Query is to find a path between two nodes.
          */
@@ -68,7 +46,7 @@ public class _399 {
         }
 
         private double dfs(String start, String end, Map<String, List<String>> pairs,
-            Map<String, List<Double>> valuePairs, HashSet<String> set, double value) {
+                           Map<String, List<Double>> valuePairs, HashSet<String> set, double value) {
             if (set.contains(start)) {
                 return 0.0;
             }
