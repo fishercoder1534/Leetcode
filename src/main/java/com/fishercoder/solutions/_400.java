020594868a59b83edef0fc488dc2b9c920eea1d3
Fisher Coder <fishercoder@gmail.com>
2020-07-01 06:57:02 -0700

    refaactor 400

--- src/main/java/com/fishercoder/solutions/_400.java
+++ src/main/java/com/fishercoder/solutions/_400.java
@@ -1,34 +1,11 @@
 package com.fishercoder.solutions;
 
-/**
- * 400. Nth Digit
- *
- * Find the nth digit of the infinite integer sequence 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ...
-
- Note:
- n is positive and will fit within the range of a 32-bit signed integer (n < 231).
-
- Example 1:
- Input:
- 3
- Output:
- 3
-
- Example 2:
- Input:
- 11
- Output:
- 0
-
- Explanation:
- The 11th digit of the sequence 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ... is a 0, which is part of the number 10.
- */
 public class _400 {
 
     public static class Solution1 {
         /**
          * credit: https://discuss.leetcode.com/topic/59314/java-solution:
-         *
+         * <p>
          * 1. find the length of the number where the nth digit is from 2. find the actual number where
          * the nth digit is from 3. find the nth digit and return
          */
