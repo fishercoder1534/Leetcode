24bdcdbe0b487245d5f0cda9bb944625bf0bcc45
Fisher Coder <fishercoder@gmail.com>
2020-07-01 06:56:12 -0700

    refaactor 398

--- src/main/java/com/fishercoder/solutions/_398.java
+++ src/main/java/com/fishercoder/solutions/_398.java
@@ -3,25 +3,6 @@ package com.fishercoder.solutions;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 398. Random Pick Index
- *
- * Given an array of integers with possible duplicates,
- * randomly output the index of a given target number. You can assume that the given target number must exist in the array.
-
- Note:
- The array size can be very large. Solution that uses too much extra space will not pass the judge.
-
- Example:
-
- int[] nums = new int[] {1,2,3,3,3};
- Solution solution = new Solution(nums);
-
- // pick(3) should return either index 2, 3, or 4 randomly. Each index should have equal probability of returning.
- solution.pick(3);
-
- // pick(1) should return 0. Since in the array only nums[0] is equal to 1.
- solution.pick(1);*/
 public class _398 {
 
     //TODO: use reservoir sampling to solve it again
