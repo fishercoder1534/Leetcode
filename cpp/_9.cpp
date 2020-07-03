131ecfaf3438d61ef32bcd6140ed62dd759b5a2e
Fisher Coder <fishercoder@gmail.com>
2020-07-03 10:12:12 -0700

    refaactor 9

--- cpp/_9.cpp
+++ cpp/_9.cpp
@@ -1,27 +1,3 @@
-/**
- * 9. Palindrome Number
- * 
- * Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
-
-Example 1:
-
-Input: 121
-Output: true
-Example 2:
-
-Input: -121
-Output: false
-Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
-Example 3:
-
-Input: 10
-Output: false
-Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
-Follow up:
-
-Coud you solve it without converting the integer to a string?
-*/
-
 class Solution1 {
     /**This is a Java translation, while its Java counterpart could be accepted perfectly, this one failed due to
      * Char 21: runtime error: signed integer overflow: 746384741 * 10 cannot be represented in type 'int' (solution.cpp)
