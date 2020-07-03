61f247483177cd4c9b9ca531575c5328120f2f7a
Fisher Coder <fishercoder@gmail.com>
2020-07-03 10:11:55 -0700

    refaactor 1

--- cpp/_1.cpp
+++ cpp/_1.cpp
@@ -1,19 +1,3 @@
-/**
-1. Two Sum
-
-Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-Example:
-
-Given nums = [2, 7, 11, 15], target = 9,
-
-Because nums[0] + nums[1] = 2 + 7 = 9,
-return [0, 1].
-
-*/
-
 #include <vector>
 #include <unordered_map>
 using namespace std;
