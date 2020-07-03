34969d859d90204255d244c710f4dd4dfb393638
Fisher Coder <fishercoder@gmail.com>
2020-07-03 10:14:07 -0700

    refaactor 416

--- src/main/java/com/fishercoder/solutions/_416.java
+++ src/main/java/com/fishercoder/solutions/_416.java
@@ -2,34 +2,6 @@ package com.fishercoder.solutions;
 
 import java.util.Arrays;
 
-/**
- * 416. Partition Equal Subset Sum
- *
- * Given a non-empty array containing only positive integers,
- * find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
-
- Note:
-
- Each of the array element will not exceed 100.
- The array size will not exceed 200.
-
- Example 1:
-
- Input: [1, 5, 11, 5]
-
- Output: true
-
- Explanation: The array can be partitioned as [1, 5, 5] and [11].
-
-
- Example 2:
-
- Input: [1, 2, 3, 5]
-
- Output: false
-
- Explanation: The array cannot be partitioned into equal sum subsets.
- */
 public class _416 {
     public static class Solution1 {
         /**
