786564c50ad643fe77066b1a571461257ee85b0e
Fisher Coder <fishercoder@gmail.com>
2020-07-07 06:44:55 -0700

    refactor 412

--- src/main/java/com/fishercoder/solutions/_412.java
+++ src/main/java/com/fishercoder/solutions/_412.java
@@ -3,36 +3,6 @@ package com.fishercoder.solutions;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 412. Fizz Buzz
- *
- * Write a program that outputs the string representation of numbers from 1 to n.
-
- But for multiples of three it should output “Fizz” instead of the number and for
- the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “_412”.
-
- Example:
-
- n = 15,
-
- Return:
- [
- "1",
- "2",
- "Fizz",
- "4",
- "Buzz",
- "Fizz",
- "7",
- "8",
- "Fizz",
- "Buzz",
- "11",
- "Fizz",
- "13",
- "14",
- "_412"
- ]*/
 public class _412 {
 
     public static class Solution1 {
