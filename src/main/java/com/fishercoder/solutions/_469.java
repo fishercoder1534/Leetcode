d55b388aad2e424b9dc97c88465acc7fb9af93ea
Fisher Coder <fishercoder@gmail.com>
2020-07-11 08:55:52 -0700

    refactor 469

--- src/main/java/com/fishercoder/solutions/_469.java
+++ src/main/java/com/fishercoder/solutions/_469.java
@@ -2,31 +2,6 @@ package com.fishercoder.solutions;
 
 import java.util.List;
 
-/**
- * 469. Convex Polygon
- *
- * Given a list of points that form a polygon when joined sequentially, find if this polygon is convex (Convex polygon definition).
-
- Note:
-
- There are at least 3 and at most 10,000 points.
- Coordinates are in the range -10,000 to 10,000.
- You may assume the polygon formed by given points is always a simple polygon (Simple polygon definition).
- In other words, we ensure that exactly two edges intersect at each vertex, and that edges otherwise don't intersect each other.
-
- Example 1:
-
- [[0,0],[0,1],[1,1],[1,0]]
-
- Answer: True
-
- Example 2:
-
- [[0,0],[0,10],[10,10],[10,0],[5,5]]
-
- Answer: False
-
- */
 public class _469 {
 
     public static class Solution1 {
