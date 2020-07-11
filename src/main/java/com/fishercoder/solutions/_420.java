8613b2d722b33becf5e127918cd38adb7ab86a93
Fisher Coder <fishercoder@gmail.com>
2020-07-11 08:33:24 -0700

    refactor 420

--- src/main/java/com/fishercoder/solutions/_420.java
+++ src/main/java/com/fishercoder/solutions/_420.java
@@ -1,18 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 420. Strong Password Checker
- *
- * A password is considered strong if below conditions are all met:
-
- It has at least 6 characters and at most 20 characters.
- It must contain at least one lowercase letter, at least one uppercase letter, and at least one digit.
- It must NOT contain three repeating characters in a row ("...aaa..." is weak, but "...aa...a..." is strong, assuming other conditions are met).
- Write a function strongPasswordChecker(s), that takes a string s as input, and return the MINIMUM
- change required to make s a strong password. If s is already strong, return 0.
-
- Insertion, deletion or replace of any one character are all considered as one change.
- */
 public class _420 {
     public static class Solution1 {
         /**
