7db7376624aae58caa845f3b0f96d02951867397
Fisher Coder <fishercoder@gmail.com>
2020-07-11 08:46:49 -0700

    refactor 445

--- src/main/java/com/fishercoder/solutions/_445.java
+++ src/main/java/com/fishercoder/solutions/_445.java
@@ -6,22 +6,6 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.Stack;
 
-/**
- * 445. Add Two Numbers II
- *
- * You are given two non-empty linked lists representing two non-negative integers.
- * The most significant digit comes first and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
-
- You may assume the two numbers do not contain any leading zero, except the number 0 itself.
-
- Follow up:
- What if you cannot modify the input lists? In other words, reversing the lists is not allowed.
-
- Example:
-
- Input: (7 -> 2 -> 4 -> 3) + (5 -> 6 -> 4)
- Output: 7 -> 8 -> 0 -> 7
- */
 public class _445 {
 
     public static class Solution1 {
