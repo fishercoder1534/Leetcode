9a605b598cf398dd25d11695c435e65fcc178c79
Fisher Coder <fishercoder@gmail.com>
2020-07-11 07:05:20 -0700

    refactor 417

--- src/main/java/com/fishercoder/solutions/_417.java
+++ src/main/java/com/fishercoder/solutions/_417.java
@@ -2,22 +2,6 @@ package com.fishercoder.solutions;
 
 import java.util.ArrayList;
 import java.util.List;
-/**
- * 417. Pacific Atlantic Water Flow
- *
- * Given the following 5x5 matrix:
-
-  Pacific ~   ~   ~   ~   ~ 
-       ~  1   2   2   3  (5) *
-       ~  3   2   3  (4) (4) *
-       ~  2   4  (5)  3   1  *
-       ~ (6) (7)  1   4   5  *
-       ~ (5)  1   1   2   4  *
-          *   *   *   *   * Atlantic
-
-Return:
-
-[[0, 4], [1, 3], [1, 4], [2, 2], [3, 0], [3, 1], [4, 0]] (positions with parentheses in above matrix).*/
 
 public class _417 {
     public static class Solution1 {
