43cec01de05b0c5044d20fc93f1352a209cec313
Fisher Coder <fishercoder@gmail.com>
2020-07-11 08:44:13 -0700

    refactor 437

--- src/main/java/com/fishercoder/solutions/_437.java
+++ src/main/java/com/fishercoder/solutions/_437.java
@@ -2,36 +2,6 @@ package com.fishercoder.solutions;
 
 import com.fishercoder.common.classes.TreeNode;
 
-/**
- * 437. Path Sum III
- *
- * You are given a binary tree in which each node contains an integer value.
-
- Find the number of paths that sum to a given value.
-
- The path does not need to start or end at the root or a leaf, but it must go downwards (traveling only from parent nodes to child nodes).
-
- The tree has no more than 1,000 nodes and the values are in the range -1,000,000 to 1,000,000.
-
- Example:
-
- root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
-
-      10
-     /  \
-    5   -3
-   / \    \
-  3   2   11
- / \   \
-3  -2  1
-
- Return 3. The paths that sum to 8 are:
-
- 1.  5 -> 3
- 2.  5 -> 2 -> 1
- 3. -3 -> 11
- */
-
 public class _437 {
 
     public static class Solution1 {
