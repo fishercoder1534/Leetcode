07cb7b3fde2c135f9b3f4aa93e1675ba95884e6f
Fisher Coder <fishercoder@gmail.com>
2020-07-11 08:45:35 -0700

    refactor 440

--- src/main/java/com/fishercoder/solutions/_440.java
+++ src/main/java/com/fishercoder/solutions/_440.java
@@ -1,23 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 440. K-th Smallest in Lexicographical Order
- *
- * Given integers n and k, find the lexicographically k-th smallest integer in the range from 1 to n.
-
- Note: 1 ≤ k ≤ n ≤ 109.
-
- Example:
-
- Input:
- n: 13   k: 2
-
- Output:
- 10
-
- Explanation:
- The lexicographical order is [1, 10, 11, 12, 13, 2, 3, 4, 5, 6, 7, 8, 9], so the second smallest number is 10.
- */
 public class _440 {
 
     public static class Solution1 {
