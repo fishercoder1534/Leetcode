d078e877844b98c4b2263cb8af2bacd483d79959
Fisher Coder <fishercoder@gmail.com>
2020-07-11 08:36:29 -0700

    refactor 423

--- src/main/java/com/fishercoder/solutions/_423.java
+++ src/main/java/com/fishercoder/solutions/_423.java
@@ -1,25 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 423. Reconstruct Original Digits from English
- *
- * Given a non-empty string containing an out-of-order English representation of digits 0-9, output the digits in ascending order.
-
- Note:
- Input contains only lowercase English letters.
- Input is guaranteed to be valid and can be transformed to its original digits. That means invalid inputs such as "abc" or "zerone" are not permitted.
- Input length is less than 50,000.
-
- Example 1:
- Input: "owoztneoer"
-
- Output: "012"
- Example 2:
- Input: "fviefuro"
-
- Output: "45"
-
- */
 public class _423 {
 
     public static class Solution1 {
