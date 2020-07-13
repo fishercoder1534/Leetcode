162fa20b2b444ac6d7e63df8fbf76faee2c958c8
Fisher Coder <fishercoder@gmail.com>
2020-07-13 06:20:33 -0700

    refactor 479

--- src/main/java/com/fishercoder/solutions/_479.java
+++ src/main/java/com/fishercoder/solutions/_479.java
@@ -1,20 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 479. Largest Palindrome Product
- *
- * Find the largest palindrome made from the product of two n-digit numbers.
- Since the result could be very large, you should return the largest palindrome mod 1337.
-
- Example:
- Input: 2
- Output: 987
-
- Explanation: 99 x 91 = 9009, 9009 % 1337 = 987
-
- Note:
- The range of n is [1,8].
- */
 public class _479 {
     public static class Solution1 {
         /**
