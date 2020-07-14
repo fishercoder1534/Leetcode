fd9588112b9d1467c726c01ffb6944fc314092df
yaodehuang <sef1548@gmail.com>
2020-07-14 23:57:07 +0800

    feat: Add javascript Solution2 for 1

--- javascript/_1.js
+++ javascript/_1.js
@@ -1,6 +1,6 @@
 // Two sum
 
-var twoSum = function (nums, target) {
+var Solution1 = function (nums, target) {
   var sum = {}
   for (var i = 0; i < nums.length; i++) {
     if (!Object.prototype.hasOwnProperty.call(sum, (target - nums[i]))) {
@@ -11,4 +11,21 @@ var twoSum = function (nums, target) {
   }
 }
 
-console.log(twoSum([2, 7, 11, 15], 9))
+var Solution2 = function (nums, target) {
+  var  p1 = 0
+  var  p2 = nums.length - 1
+  while (p1 < p2){
+    if (nums[p1] + nums[p2] < target) {
+      p1 += 1
+    }
+    else if (nums[p1] + nums[p2] > target) {
+      p2 -= 1
+    }
+    else {
+      return [ p1, p2]  
+    }
+  }
+}
+
+console.log(Solution1([2, 7, 11, 15], 9))
+console.log(Solution2([2, 7, 11, 15], 9))
