58ccbce5d418786710ab2122c48f9ab01d0beef3
Fisher Coder <fishercoder@gmail.com>
2020-07-14 06:41:34 -0700

    refactor 483

--- src/main/java/com/fishercoder/solutions/_483.java
+++ src/main/java/com/fishercoder/solutions/_483.java
@@ -2,33 +2,6 @@ package com.fishercoder.solutions;
 
 import java.math.BigInteger;
 
-/**
- * 483. Smallest Good Base
- *
- * For an integer n, we call k>=2 a good base of n, if all digits of n base k are 1.
-
- Now given a string representing n, you should return the smallest good base of n in string format.
-
- Example 1:
- Input: "13"
- Output: "3"
- Explanation: 13 base 3 is 111.
-
- Example 2:
- Input: "4681"
- Output: "8"
- Explanation: 4681 base 8 is 11111.
-
- Example 3:
- Input: "1000000000000000000"
- Output: "999999999999999999"
- Explanation: 1000000000000000000 base 999999999999999999 is 11.
-
- Note:
- The range of n is [3, 10^18].
- The string representing n is always valid and will not have leading zeros.
-
- */
 public class _483 {
 
     public static class Solution1 {
