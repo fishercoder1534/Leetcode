09dc3b9bc2fda5b5d0baf5fe1971dee60455816e
Fisher Coder <fishercoder@gmail.com>
2020-07-15 09:01:11 -0700

    refactor 504

--- src/main/java/com/fishercoder/solutions/_504.java
+++ src/main/java/com/fishercoder/solutions/_504.java
@@ -1,19 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 504. Base 7
- *
- * Given an integer, return its base 7 string representation.
- Example 1:
- Input: 100
- Output: "202"
-
- Example 2:
- Input: -7
- Output: "-10"
-
- Note: The input will be in range of [-1e7, 1e7].
- */
 public class _504 {
 
     public static class Solution1 {
