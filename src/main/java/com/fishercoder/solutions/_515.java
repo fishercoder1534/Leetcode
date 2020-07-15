216d33eca6221ddd0a766883f00d5f3e89381a91
Fisher Coder <fishercoder@gmail.com>
2020-07-15 09:12:54 -0700

    refactor 515

--- src/main/java/com/fishercoder/solutions/_515.java
+++ src/main/java/com/fishercoder/solutions/_515.java
@@ -7,22 +7,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-/**
- * 515. Find Largest Value in Each Tree Row
- *
- * You need to find the largest value in each row of a binary tree.
-
- Example:
- Input:
-
-      1
-     / \
-    3   2
-   / \   \
-  5  3   9
-
- Output: [1, 3, 9]
- */
 public class _515 {
 
     public static class Solution1 {
