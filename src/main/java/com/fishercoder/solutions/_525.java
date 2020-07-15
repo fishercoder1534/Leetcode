a543e9b3916fac6b41481407949c25b0a232c7bb
Fisher Coder <fishercoder@gmail.com>
2020-07-15 09:15:00 -0700

    refactor 525

--- src/main/java/com/fishercoder/solutions/_525.java
+++ src/main/java/com/fishercoder/solutions/_525.java
@@ -3,23 +3,6 @@ package com.fishercoder.solutions;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * 525. Contiguous Array
- *
- * Given a binary array, find the maximum length of a contiguous subarray with equal number of 0 and 1.
-
- Example 1:
- Input: [0,1]
- Output: 2
- Explanation: [0, 1] is the longest contiguous subarray with equal number of 0 and 1.
-
- Example 2:
- Input: [0,1,0]
- Output: 2
- Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal number of 0 and 1.
-
- Note: The length of the given binary array will not exceed 50,000.
- */
 public class _525 {
 
     public static class Solution1 {
