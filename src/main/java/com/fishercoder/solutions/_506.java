30b8b91c6e27e9864ae285ab30d12e2121100dc3
Fisher Coder <fishercoder@gmail.com>
2020-07-15 09:01:46 -0700

    refactor 506

--- src/main/java/com/fishercoder/solutions/_506.java
+++ src/main/java/com/fishercoder/solutions/_506.java
@@ -4,22 +4,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * 506. Relative Ranks
- *
- * Given scores of N athletes, find their relative ranks and the people with the top three highest scores,
- * who will be awarded medals: "Gold Medal", "Silver Medal" and "Bronze Medal".
-
- Example 1:
- Input: [5, 4, 3, 2, 1]
- Output: ["Gold Medal", "Silver Medal", "Bronze Medal", "4", "5"]
- Explanation: The first three athletes got the top three highest scores, so they got "Gold Medal", "Silver Medal" and "Bronze Medal".
- For the left two athletes, you just need to output their relative ranks according to their scores.
-
- Note:
- N is a positive integer and won't exceed 10,000.
- All the scores of athletes are guaranteed to be unique.
- */
 public class _506 {
 
     public static class Solution1 {
