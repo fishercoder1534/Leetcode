fc16a8853dea9a6d949784e93d4b9cc1ca9828fb
Fisher Coder <fishercoder@gmail.com>
2020-07-15 09:02:52 -0700

    refactor 513

--- src/main/java/com/fishercoder/solutions/_513.java
+++ src/main/java/com/fishercoder/solutions/_513.java
@@ -5,36 +5,6 @@ import com.fishercoder.common.classes.TreeNode;
 import java.util.LinkedList;
 import java.util.Queue;
 
-/**
- * 513. Find Bottom Left Tree Value
- *
- * Given a binary tree, find the leftmost value in the last row of the tree.
-
- Example 1:
- Input:
-
-   2
-  / \
- 1   3
-
- Output:
- 1
- Example 2:
- Input:
-
-
-      1
-     / \
-    2   3
-   /   / \
-  4   5   6
-     /
-    7
-
- Output:
- 7
- Note: You may assume the tree (i.e., the given root node) is not NULL.
- */
 public class _513 {
 
     public static class Solution1 {
