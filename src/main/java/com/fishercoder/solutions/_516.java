28ec3321b139ea2f4550337bab8dfc11634a935e
Fisher Coder <fishercoder@gmail.com>
2020-07-15 09:13:10 -0700

    refactor 516

--- src/main/java/com/fishercoder/solutions/_516.java
+++ src/main/java/com/fishercoder/solutions/_516.java
@@ -1,25 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 516. Longest Palindromic Subsequence
- *
- * Given a string s, find the longest palindromic subsequence's length in s.
- * You may assume that the maximum length of s is 1000.
-
- Example 1:
- Input:
- "bbbab"
- Output:
- 4
- One possible longest palindromic subsequence is "bbbb".
-
- Example 2:
- Input:
- "cbbd"
- Output:
- 2
- One possible longest palindromic subsequence is "bb".
- */
 public class _516 {
 
     public static class Solution1 {
