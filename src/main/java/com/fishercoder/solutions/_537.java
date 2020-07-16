d1daf0caeda58574fb43f9fd7c6a26b1701b1348
Fisher Coder <fishercoder@gmail.com>
2020-07-16 06:56:01 -0700

    refactor 537

--- src/main/java/com/fishercoder/solutions/_537.java
+++ src/main/java/com/fishercoder/solutions/_537.java
@@ -2,27 +2,6 @@ package com.fishercoder.solutions;
 
 import java.util.stream.Stream;
 
-/**
- * 537. Complex Number Multiplication
- *
- * Given two strings representing two complex numbers.
-
- You need to return a string representing their multiplication. Note i2 = -1 according to the definition.
-
- Example 1:
- Input: "1+1i", "1+1i"
- Output: "0+2i"
- Explanation: (1 + i) * (1 + i) = 1 + i2 + 2 * i = 2i, and you need convert it to the form of 0+2i.
-
- Example 2:
- Input: "1+-1i", "1+-1i"
- Output: "0+-2i"
- Explanation: (1 - i) * (1 - i) = 1 + i2 - 2 * i = -2i, and you need convert it to the form of 0+-2i.
-
- Note:
- The input strings will not have extra blank.
- The input strings will be given in the form of a+bi, where the integer a and b will both belong to the range of [-100, 100]. And the output should be also in this form.
- */
 public class _537 {
 
     public static class Solution1 {
