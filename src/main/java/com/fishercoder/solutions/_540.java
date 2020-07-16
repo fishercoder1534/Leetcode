67873124d44965cdcd56453cdce5ef5b160925b7
Fisher Coder <fishercoder@gmail.com>
2020-07-16 06:56:59 -0700

    refactor 540

--- src/main/java/com/fishercoder/solutions/_540.java
+++ src/main/java/com/fishercoder/solutions/_540.java
@@ -1,20 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 540. Single Element in a Sorted Array
- *
- * Given a sorted array consisting of only integers where every
- * element appears twice except for one element which appears once.
- * Find this single element that appears only once.
-
- Example 1:
- Input: [1,1,2,3,3,4,4,8,8]
- Output: 2
- Example 2:
- Input: [3,3,7,7,10,11,11]
- Output: 10
- Note: Your solution should run in O(log n) time and O(1) space.
- */
 public class _540 {
     public static class Solution1 {
         public int singleNonDuplicate(int[] nums) {
