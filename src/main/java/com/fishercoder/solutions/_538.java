979e5766f01d1e852166c0bb735fa6005e780bd9
Fisher Coder <fishercoder@gmail.com>
2020-07-16 06:56:21 -0700

    refactor 538

--- src/main/java/com/fishercoder/solutions/_538.java
+++ src/main/java/com/fishercoder/solutions/_538.java
@@ -7,25 +7,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.TreeMap;
 
-/**
- * 538. Convert BST to Greater Tree
- *
- * Given a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed
- * to the original key plus sum of all keys greater than the original key in BST.
-
- Example:
-
- Input: The root of a Binary Search Tree like this:
-     5
-   /   \
-  2    13
-
- Output: The root of a Greater Tree like this:
-     18
-   /   \
-  20   13
- */
-
 public class _538 {
 
     public static class Solution1 {
