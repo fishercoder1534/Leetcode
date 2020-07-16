be2b5a379b4e083c090f956aa2f86ff5ab777af0
Fisher Coder <fishercoder@gmail.com>
2020-07-16 06:56:41 -0700

    refactor 539

--- src/main/java/com/fishercoder/solutions/_539.java
+++ src/main/java/com/fishercoder/solutions/_539.java
@@ -2,20 +2,6 @@ package com.fishercoder.solutions;
 
 import java.util.List;
 
-/**
- * 539. Minimum Time Difference
- *
- * Given a list of 24-hour clock time points in "Hour:Minutes" format,
- * find the minimum minutes difference between any two time points in the list.
-
- Example 1:
- Input: ["23:59","00:00"]
- Output: 1
-
- Note:
- The number of time points in the given list is at least 2 and won't exceed 20000.
- The input time is legal and ranges from 00:00 to 23:59.
- */
 public class _539 {
 
     public static class Soluiton1 {
