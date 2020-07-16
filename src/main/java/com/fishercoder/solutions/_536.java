f68720eaf46dd060022044ecbd74ae2f066f5d6f
Fisher Coder <fishercoder@gmail.com>
2020-07-16 06:55:45 -0700

    refactor 536

--- src/main/java/com/fishercoder/solutions/_536.java
+++ src/main/java/com/fishercoder/solutions/_536.java
@@ -5,29 +5,6 @@ import com.fishercoder.common.classes.TreeNode;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
-/**
- * 536. Construct Binary Tree from String
- *
- * You need to construct a binary tree from a string consisting of parenthesis and integers.
- The whole input represents a binary tree. It contains an integer followed by zero, one or two pairs of parenthesis.
- The integer represents the root's value and a pair of parenthesis contains a child binary tree with the same structure.
- You always start to construct the left child node of the parent first if it exists.
-
- Example:
- Input: "4(2(3)(1))(6(5))"
- Output: return the tree root node representing the following tree:
-
-       4
-     /   \
-    2     6
-   / \   /
-  3   1 5
-
- Note:
- There will only be '(', ')', '-' and '0' ~ '9' in the input string.
- An empty tree is represented by "" instead of "()".
- */
-
 public class _536 {
 
     public static class Solution1 {
