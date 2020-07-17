43a02af30f7c9cca89d464a7f2a0ea0d76b3d816
Fisher Coder <fishercoder@gmail.com>
2020-07-17 06:46:56 -0700

    refactor 573

--- src/main/java/com/fishercoder/solutions/_573.java
+++ src/main/java/com/fishercoder/solutions/_573.java
@@ -1,54 +1,23 @@
 package com.fishercoder.solutions;
 
-/**
- * 573. Squirrel Simulation
- *
- * There's a tree, a squirrel, and several nuts.
- * Positions are represented by the cells in a 2D grid.
- * Your goal is to find the minimal distance for the squirrel to collect all the nuts and
- * put them under the tree one by one.
- * The squirrel can only take at most one nut at one time and can move in four directions -
- * up, down, left and right, to the adjacent cell.
- * The distance is represented by the number of moves.
-
- Example 1:
-
- Input:
- Height : 5
- Width : 7
- Tree position : [2,2]
- Squirrel : [4,4]
- Nuts : [[3,0], [2,5]]
- Output: 12
-
- Explanation:
-
- Note:
-
- All given positions won't overlap.
- The squirrel can take at most one nut at one time.
- The given positions of nuts have no order.
- Height and width are positive integers. 3 <= height * width <= 10,000.
- The given positions contain at least one nut, only one tree and one squirrel.
- */
 public class _573 {
 
     public static class Solution1 {
 
         /**
          * reference: https://leetcode.com/articles/squirrel-simulation
-         *
+         * <p>
          * 1. The order in which to pick the nuts does not matter except the first one
          * because for all the other nuts, the squirrel needs to travel back and forth.
-         *
+         * <p>
          * 2. For the first nut to be picked, there's some distance we might be able to save, what is this distance?
          * It is the difference between the squirrel's original starting point to the first nut and that the distance from this
          * first nut to the tree.
          * This is because, only for the first nut, the squirrel does NOT need to travel back and forth, it only needs to travel from
          * its starting position to the nut position and then return to the tree.
-         *
+         * <p>
          * 3. For the rest of all other nuts, the squirrel always needs to go back and forth.
-         *
+         * <p>
          * 4. So how can we find the first nut to go to so that we could have the maximum saved distance?
          * We iterate through all of the nuts and keep updating the savedDist as below:
          */
