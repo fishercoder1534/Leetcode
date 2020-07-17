31af4010d159a1330964f5567cf148d5b73d0e71
Fisher Coder <fishercoder@gmail.com>
2020-07-16 17:18:55 -0700

    refactor 552

--- src/main/java/com/fishercoder/solutions/_552.java
+++ src/main/java/com/fishercoder/solutions/_552.java
@@ -1,31 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 552. Student Attendance Record II
- *
- * Given a positive integer n, return the number of all possible attendance records with length n,
- * which will be regarded as rewardable. The answer may be very large, return it after mod 109 + 7.
-
- A student attendance record is a string that only contains the following three characters:
-
- 'A' : Absent.
- 'L' : Late.
- 'P' : Present.
-
- A record is regarded as rewardable if it doesn't contain more than one 'A' (absent) or more than two continuous 'L' (late).
-
- Example 1:
-
- Input: n = 2
- Output: 8
-
- Explanation:
- There are 8 records with length 2 will be regarded as rewardable:
- "PP" , "AP", "PA", "LP", "PL", "AL", "LA", "LL"
- Only "AA" won't be regarded as rewardable owing to more than one absent times.
-
- Note: The value of n won't exceed 100,000.
- */
 public class _552 {
 
     public static class Solution1 {
