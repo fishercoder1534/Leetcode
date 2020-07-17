118277b550c64f5ce98c50e87706a51f74e6c0c2
Fisher Coder <fishercoder@gmail.com>
2020-07-16 17:18:24 -0700

    refactor 549

--- src/main/java/com/fishercoder/solutions/_549.java
+++ src/main/java/com/fishercoder/solutions/_549.java
@@ -2,30 +2,6 @@ package com.fishercoder.solutions;
 
 import com.fishercoder.common.classes.TreeNode;
 
-/**
- * 549. Binary Tree Longest Consecutive Sequence II
- *
- * Given a binary tree, you need to find the length of Longest Consecutive Path in Binary Tree.
- Especially, this path can be either increasing or decreasing. For example, [1,2,3,4] and [4,3,2,1] are both considered valid,
- but the path [1,2,4,3] is not valid. On the other hand, the path can be in the child-Parent-child order, where not necessarily be parent-child order.
-
- Example 1:
- Input:
-   1
-  / \
- 2   3
- Output: 2
-
- Explanation: The longest consecutive path is [1, 2] or [2, 1].
- Example 2:
- Input:
-   2
-  / \
- 1   3
- Output: 3
- Explanation: The longest consecutive path is [1, 2, 3] or [3, 2, 1].
- Note: All the values of tree nodes are in the range of [-1e7, 1e7].
- */
 public class _549 {
 
     public static class Solution1 {
