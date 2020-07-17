3250759006443dd8bd27c810e84107f0c4290c71
Fisher Coder <fishercoder@gmail.com>
2020-07-17 07:04:41 -0700

    refactor 588

--- src/main/java/com/fishercoder/solutions/_588.java
+++ src/main/java/com/fishercoder/solutions/_588.java
@@ -7,41 +7,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * 588. Design In-Memory File System
- *
- * Design an in-memory file system to simulate the following functions:
-
- ls: Given a path in string format. If it is a file path, return a list that only contains this file's name.
- If it is a directory path, return the list of file and directory names in this directory.
- Your output (file and directory names together) should in lexicographic order.
-
- mkdir: Given a directory path that does not exist,
- you should make a new directory according to the path.
- If the middle directories in the path don't exist either,
- you should create them as well. This function has void return type.
-
- addContentToFile: Given a file path and file content in string format.
- If the file doesn't exist, you need to create that file containing given content.
- If the file already exists, you need to append given content to original content.
- This function has void return type.
-
- readContentFromFile: Given a file path, return its content in string format.
-
- Example:
- Input:
- ["FileSystem","ls","mkdir","addContentToFile","ls","readContentFromFile"]
- [[],["/"],["/a/b/c"],["/a/b/c/d","hello"],["/"],["/a/b/c/d"]]
- Output:
- [null,[],null,null,["a"],"hello"]
-
- Note:
-
- You can assume all file or directory paths are absolute paths which begin with / and do not end with / except that the path is just "/".
- You can assume that all operations will be passed valid parameters and users will not attempt to retrieve file content or list a directory or file that does not exist.
- You can assume that all directory names and file names only contain lower-case letters, and same names won't exist in the same directory.
-
- */
 public class _588 {
     public static class Solution1 {
         /**
