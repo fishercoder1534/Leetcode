891b819fd857ad7a6e1e9469a8764069107f7445
Fisher Coder <fishercoder@gmail.com>
2020-07-16 17:13:07 -0700

    refactor 541

--- src/main/java/com/fishercoder/solutions/_541.java
+++ src/main/java/com/fishercoder/solutions/_541.java
@@ -1,20 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 541. Reverse String II
- *
- * Given a string and an integer k, you need to reverse the first k characters for every 2k characters counting from the start of the string.
- * If there are less than k characters left, reverse all of them.
- * If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and left the other as original.
-
- Example:
- Input: s = "abcdefg", k = 2
- Output: "bacdfeg"
-
- Restrictions:
- The string consists of lower English letters only.
- Length of the given string and k will in the range [1, 10000]
- */
 public class _541 {
 
     public static class Solution1 {
