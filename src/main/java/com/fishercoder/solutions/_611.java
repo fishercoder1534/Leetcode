d0d8155d3a4597146f67a29211ef6812b8f074de
Fisher Coder <fishercoder@gmail.com>
2020-07-17 07:08:59 -0700

    refactor 611

--- src/main/java/com/fishercoder/solutions/_611.java
+++ src/main/java/com/fishercoder/solutions/_611.java
@@ -2,26 +2,6 @@ package com.fishercoder.solutions;
 
 import java.util.Arrays;
 
-/**
- * 611. Valid Triangle Number
- * Given an array consists of non-negative integers,
- * your task is to count the number of triplets chosen
- * from the array that can make triangles if we take them as side lengths of a triangle.
-
- Example 1:
- Input: [2,2,3,4]
- Output: 3
-
- Explanation:
- Valid combinations are:
- 2,3,4 (using the first 2)
- 2,3,4 (using the second 2)
- 2,2,3
-
- Note:
- The length of the given array won't exceed 1000.
- The integers in the given array are in the range of [0, 1000].
- */
 public class _611 {
     public static class Solution1 {
         /**
