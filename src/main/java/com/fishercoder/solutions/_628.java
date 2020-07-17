a44cfd27c18cdfa09d415a15d340a3c1e674bf1a
Fisher Coder <fishercoder@gmail.com>
2020-07-17 11:23:16 -0700

    refactor 628

--- src/main/java/com/fishercoder/solutions/_628.java
+++ src/main/java/com/fishercoder/solutions/_628.java
@@ -2,22 +2,6 @@ package com.fishercoder.solutions;
 
 import java.util.Arrays;
 
-/**
- * 628. Maximum Product of Three Numbers
- *
- * Given an integer array, find three numbers whose product is maximum and output the maximum product.
- Example 1:
- Input: [1,2,3]
- Output: 6
-
- Example 2:
- Input: [1,2,3,4]
- Output: 24
-
- Note:
- The length of the given array will be in range [3,104] and all elements are in the range [-1000, 1000].
- Multiplication of any three numbers in the input won't exceed the range of 32-bit signed integer.
- */
 public class _628 {
 
     public static class Solution1 {
