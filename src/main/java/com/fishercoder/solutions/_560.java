a538feba1e92e39c2fef67168e2eb3452135eea9
Fisher Coder <fishercoder@gmail.com>
2020-07-17 06:41:53 -0700

    refactor 560

--- src/main/java/com/fishercoder/solutions/_560.java
+++ src/main/java/com/fishercoder/solutions/_560.java
@@ -3,18 +3,6 @@ package com.fishercoder.solutions;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * 560. Subarray Sum Equals K
- *
- * Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
-
- Example 1:
- Input:nums = [1,1,1], k = 2
- Output: 2
- Note:
- The length of the array is in range [1, 20,000].
- The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].
- */
 public class _560 {
 
     public static class Solution1 {
