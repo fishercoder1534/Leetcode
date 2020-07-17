96bf66ecc185328e25a56307b01aa41ea9e033d2
Fisher Coder <fishercoder@gmail.com>
2020-07-17 07:06:16 -0700

    refactor 593

--- src/main/java/com/fishercoder/solutions/_593.java
+++ src/main/java/com/fishercoder/solutions/_593.java
@@ -4,22 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-/**
- * 593. Valid Square
- *
- * Given the coordinates of four points in 2D space, return whether the four points could construct a square.
-
- The coordinate (x,y) of a point is represented by an integer array with two integers.
-
- Example:
- Input: p1 = [0,0], p2 = [1,1], p3 = [1,0], p4 = [0,1]
- Output: True
- Note:
- All the input integers are in the range [-10000, 10000].
- A valid square has four equal sides with positive length and four equal angles (90-degree angles).
- Input points have no order.
-
- */
 public class _593 {
     public static class Solution1 {
         /**
