10615607847496f67bb103a2c0f55bbbd5c73e07
Fisher Coder <fishercoder@gmail.com>
2020-07-17 07:09:15 -0700

    refactor 616

--- src/main/java/com/fishercoder/solutions/_616.java
+++ src/main/java/com/fishercoder/solutions/_616.java
@@ -1,32 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 616. Add Bold Tag in String
- *
- * Given a string s and a list of strings dict,
- * you need to add a closed pair of bold tag <b> and </b>
- * to wrap the substrings in s that exist in dict.
- * If two such substrings overlap, you need to wrap them together by only one pair of closed bold tag.
- * Also, if two substrings wrapped by bold tags are consecutive, you need to combine them.
-
- Example 1:
- Input:
- s = "abcxyz123"
- dict = ["abc","123"]
- Output:
- "<b>abc</b>xyz<b>123</b>"
-
- Example 2:
- Input:
- s = "aaabbcc"
- dict = ["aaa","aab","bc"]
- Output:
- "<b>aaabbc</b>c"
-
- Note:
- The given dict won't contain duplicates, and its length won't exceed 100.
- All the strings in input have length in range [1, 1000].
- */
 public class _616 {
 
     public static class Solution1 {
