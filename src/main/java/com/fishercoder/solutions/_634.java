f34b4ab775889b2f20417d9e3eb9f05817cf0053
Fisher Coder <fishercoder@gmail.com>
2020-07-17 11:25:10 -0700

    refactor 634

--- src/main/java/com/fishercoder/solutions/_634.java
+++ src/main/java/com/fishercoder/solutions/_634.java
@@ -1,22 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 634. Find the Derangement of An Array
- *
- * In combinatorial mathematics, a derangement is a permutation of the elements of a set, such that no element appears in its original position.
-
- There's originally an array consisting of n integers from 1 to n in ascending order, you need to find the number of derangement it can generate.
-
- Also, since the answer may be very large, you should return the output mod 109 + 7.
-
- Example 1:
- Input: 3
- Output: 2
- Explanation: The original array is [1,2,3]. The two derangements are [2,3,1] and [3,1,2].
-
- Note:
- n is in the range of [1, 106].
- */
 public class _634 {
     public static class Solution1 {
         /**
