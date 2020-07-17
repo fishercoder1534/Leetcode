b432ea36221149799e59b930c7fb10da6cc2eefa
Fisher Coder <fishercoder@gmail.com>
2020-07-17 07:09:48 -0700

    refactor 621

--- src/main/java/com/fishercoder/solutions/_621.java
+++ src/main/java/com/fishercoder/solutions/_621.java
@@ -6,26 +6,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 
-/**
- * 621. Task Scheduler
- *
- * Given a char array representing tasks CPU need to do.
- * It contains capital letters A to Z where different letters represent different tasks.
- * Tasks could be done without original order.
- * Each task could be done in one interval. For each interval, CPU could finish one task or just be idle.
- * However, there is a non-negative cooling interval n that means between two same tasks,
- * there must be at least n intervals that CPU are doing different tasks or just be idle.
- * You need to return the least number of intervals the CPU will take to finish all the given tasks.
-
- Example 1:
- Input: tasks = ['A','A','A','B','B','B'], n = 2
- Output: 8
- Explanation: A -> B -> idle -> A -> B -> idle -> A -> B.
-
- Note:
- The number of tasks is in the range [1, 10000].
- The integer n is in the range [0, 100].
- */
 public class _621 {
 
     public static class Solution1 {
