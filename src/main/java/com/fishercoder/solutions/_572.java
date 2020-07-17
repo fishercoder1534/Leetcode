2445622accc3cd7034138cd8abc7da0ac07cf4f4
Fisher Coder <fishercoder@gmail.com>
2020-07-17 06:46:39 -0700

    refactor 572

--- src/main/java/com/fishercoder/solutions/_572.java
+++ src/main/java/com/fishercoder/solutions/_572.java
@@ -2,44 +2,6 @@ package com.fishercoder.solutions;
 
 import com.fishercoder.common.classes.TreeNode;
 
-/**
- * 572. Subtree of Another Tree
- * 
- Given two non-empty binary trees s and t,
- check whether tree t has exactly the same structure and node values with a subtree of s.
- A subtree of s is a tree consists of a node in s and all of this node's descendants. The tree s could also be considered as a subtree of itself.
-
- Example 1:
- Given tree s:
-
-     3
-    / \
-   4   5
-  / \
- 1   2
- Given tree t:
-   4
-  / \
- 1   2
- Return true, because t has the same structure and node values with a subtree of s.
-
- Example 2:
- Given tree s:
-
-     3
-    / \
-   4   5
-  / \
- 1   2
-    /
-   0
-
- Given tree t:
-   4
-  / \
- 1   2
- Return false.
- */
 public class _572 {
 
     public static class Solution1 {
