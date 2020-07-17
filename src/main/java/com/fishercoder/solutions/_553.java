c29e1d611decef1412987cc53088fe9a6437673a
Fisher Coder <fishercoder@gmail.com>
2020-07-16 17:19:11 -0700

    refactor 553

--- src/main/java/com/fishercoder/solutions/_553.java
+++ src/main/java/com/fishercoder/solutions/_553.java
@@ -2,34 +2,6 @@ package com.fishercoder.solutions;
 
 import java.util.StringJoiner;
 
-/**
- * 553. Optimal Division
- *
- * Given a list of positive integers, the adjacent integers will perform the float division. For example, [2,3,4] -> 2 / 3 / 4.
- However, you can add any number of parenthesis at any position to change the priority of operations.
- You should find out how to add parenthesis to get the maximum result,
- and return the corresponding expression in string format.
- Your expression should NOT contain redundant parenthesis.
-
- Example:
- Input: [1000,100,10,2]
- Output: "1000/(100/10/2)"
- Explanation:
- 1000/(100/10/2) = 1000/((100/10)/2) = 200
- However, the bold parenthesis in "1000/((100/10)/2)" are redundant,
- since they don't influence the operation priority. So you should return "1000/(100/10/2)".
-
- Other cases:
- 1000/(100/10)/2 = 50
- 1000/(100/(10/2)) = 50
- 1000/100/10/2 = 0.5
- 1000/100/(10/2) = 2
- Note:
-
- The length of the input array is [1, 10].
- Elements in the given array will be in range [2, 1000].
- There is only one optimal division for each test case.
- */
 public class _553 {
     public static class Solution1 {
         /**
