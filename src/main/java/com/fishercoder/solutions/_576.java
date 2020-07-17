8b1a7be111e32968bf4e7a60fa4c3a11d8d03df1
Fisher Coder <fishercoder@gmail.com>
2020-07-17 07:03:20 -0700

    refactor 576

--- src/main/java/com/fishercoder/solutions/_576.java
+++ src/main/java/com/fishercoder/solutions/_576.java
@@ -1,33 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 576. Out of Boundary Paths
- *
- * There is an m by n grid with a ball.
- * Given the start coordinate (i,j) of the ball,
- * you can move the ball to adjacent cell or cross the grid boundary in four directions (up, down, left, right).
- * However, you can at most move N times. Find out the number of paths to move the ball out of grid boundary.
- * The answer may be very large, return it after mod 109 + 7.
-
- Example 1:
-
- Input:m = 2, n = 2, N = 2, i = 0, j = 0
- Output: 6
- Explanation:
-
- Example 2:
-
- Input:m = 1, n = 3, N = 3, i = 0, j = 1
- Output: 12
- Explanation:
-
- Note:
-
- Once you move the ball out of boundary, you cannot move it back.
- The length and height of the grid is in range [1,50].
- N is in range [0,50].
-
- */
 public class _576 {
     public static class Solution1 {
         /**
