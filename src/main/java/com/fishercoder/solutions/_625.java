d0be0dc7e3f660a581d2cd41cfdcaeb888ad6b03
Fisher Coder <fishercoder@gmail.com>
2020-07-17 11:23:01 -0700

    refactor 625

--- src/main/java/com/fishercoder/solutions/_625.java
+++ src/main/java/com/fishercoder/solutions/_625.java
@@ -3,25 +3,6 @@ package com.fishercoder.solutions;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 625. Minimum Factorization
- *
- * Given a positive integer a, find the smallest positive integer b whose multiplication of each digit equals to a.
-
- If there is no answer or the answer is not fit in 32-bit signed integer, then return 0.
-
- Example 1
- Input:
- 48
- Output:
- 68
-
- Example 2
- Input:
- 15
- Output:
- 35
- */
 public class _625 {
 
     public static class Solution1 {
