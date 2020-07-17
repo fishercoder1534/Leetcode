50a9f607bc3b7140a17d36e80d0ab1a0ee1adaf8
Fisher Coder <fishercoder@gmail.com>
2020-07-17 07:04:07 -0700

    refactor 583

--- src/main/java/com/fishercoder/solutions/_583.java
+++ src/main/java/com/fishercoder/solutions/_583.java
@@ -1,20 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 583. Delete Operation for Two Strings
- *
- * Given two words word1 and word2, find the minimum number of steps required to make word1 and word2 the same, where in each step you can delete one character in either string.
-
- Example 1:
- Input: "sea", "eat"
- Output: 2
-
- Explanation: You need one step to make "sea" to "ea" and another step to make "eat" to "ea".
-
- Note:
- The length of given words won't exceed 500.
- Characters in given words can only be lower-case letters.
- */
 public class _583 {
 
     public static class Solution1 {
