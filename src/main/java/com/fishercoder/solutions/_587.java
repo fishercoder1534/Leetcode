48962670da0594555f1fd4cefaf676d519147658
Fisher Coder <fishercoder@gmail.com>
2020-07-17 07:04:24 -0700

    refactor 587

--- src/main/java/com/fishercoder/solutions/_587.java
+++ src/main/java/com/fishercoder/solutions/_587.java
@@ -7,33 +7,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-/**
- * 587. Erect the Fence
- *
- * There are some trees, where each tree is represented by (x,y) coordinate in a two-dimensional garden.
- * Your job is to fence the entire garden using the minimum length of rope as it is expensive.
- * The garden is well fenced only if all the trees are enclosed.
- * Your task is to help find the coordinates of trees which are exactly located on the fence perimeter.
-
- Example 1:
- Input: [[1,1],[2,2],[2,0],[2,4],[3,3],[4,2]]
- Output: [[1,1],[2,0],[4,2],[3,3],[2,4]]
- Explanation:
-
- Example 2:
- Input: [[1,2],[2,2],[4,2]]
- Output: [[1,2],[2,2],[4,2]]
- Explanation:
-
- Even you only have trees in a line, you need to use rope to enclose them.
-
- Note:
- All trees should be enclosed together. You cannot cut the rope to enclose trees that will separate them in more than one group.
- All input integers will range from 0 to 100.
- The garden has at least one tree.
- All coordinates are distinct.
- Input points have NO order. No order required for output.
- */
 public class _587 {
     public static class Solution1 {
 
