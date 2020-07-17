332205d5e999d84b35e8b057b8c46735a16baeb5
Fisher Coder <fishercoder@gmail.com>
2020-07-17 07:07:06 -0700

    refactor 599

--- src/main/java/com/fishercoder/solutions/_599.java
+++ src/main/java/com/fishercoder/solutions/_599.java
@@ -5,34 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * 599. Minimum Index Sum of Two Lists
- *
- Suppose Andy and Doris want to choose a restaurant for dinner,
- and they both have a list of favorite restaurants represented by strings.
- You need to help them find out their common interest with the least list index sum.
- If there is a choice tie between answers, output all of them with no order requirement. You could assume there always exists an answer.
-
- Example 1:
- Input:
- ["Shogun", "Tapioca Express", "Burger King", "KFC"]
- ["Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"]
- Output: ["Shogun"]
- Explanation: The only restaurant they both like is "Shogun".
-
- Example 2:
- Input:
- ["Shogun", "Tapioca Express", "Burger King", "KFC"]
- ["KFC", "Shogun", "Burger King"]
- Output: ["Shogun"]
- Explanation: The restaurant they both like and have the least index sum is "Shogun" with index sum 1 (0+1).
-
- Note:
- The length of both lists will be in the range of [1, 1000].
- The length of strings in both lists will be in the range of [1, 30].
- The index is starting from 0 to the list length minus 1.
- No duplicates in both lists.
- */
 public class _599 {
     public static class Solution1 {
         public String[] findRestaurant(String[] list1, String[] list2) {
