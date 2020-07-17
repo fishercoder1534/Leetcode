07c3256a64be9b7be38c6d073f42268cb1370ec7
Fisher Coder <fishercoder@gmail.com>
2020-07-17 07:07:21 -0700

    refactor 600

--- src/main/java/com/fishercoder/solutions/_600.java
+++ src/main/java/com/fishercoder/solutions/_600.java
@@ -1,25 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 600. Non-negative Integers without Consecutive Ones
- *
- * Given a positive integer n, find the number of non-negative integers less than or equal to n, whose binary representations do NOT contain consecutive ones.
-
- Example 1:
- Input: 5
- Output: 5
- Explanation:
- Here are the non-negative integers <= 5 with their corresponding binary representations:
- 0 : 0
- 1 : 1
- 2 : 10
- 3 : 11
- 4 : 100
- 5 : 101
- Among them, only integer 3 disobeys the rule (two consecutive ones) and the other 5 satisfy the rule.
-
- Note: 1 <= n <= 109
- */
 public class _600 {
 
     public static class Solution1 {
