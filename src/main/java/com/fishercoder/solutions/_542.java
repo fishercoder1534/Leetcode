d2b3c804ef6c5670dd128e5f4e163e69aa8a216e
Fisher Coder <fishercoder@gmail.com>
2020-07-16 17:13:24 -0700

    refactor 542

--- src/main/java/com/fishercoder/solutions/_542.java
+++ src/main/java/com/fishercoder/solutions/_542.java
@@ -4,38 +4,6 @@ import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
 
-/**
- * 542. 01 Matrix
- *
- * Given a matrix consists of 0 and 1, find the distance of the nearest 0 for each cell.
-
- The distance between two adjacent cells is 1.
- Example 1:
- Input:
-
- 0 0 0
- 0 1 0
- 0 0 0
- Output:
- 0 0 0
- 0 1 0
- 0 0 0
- Example 2:
- Input:
-
- 0 0 0
- 0 1 0
- 1 1 1
- Output:
- 0 0 0
- 0 1 0
- 1 2 1
-
- Note:
- The number of elements of the given matrix will not exceed 10,000.
- There are at least one 0 in the given matrix.
- The cells are adjacent in only four directions: up, down, left and right.
- */
 public class _542 {
 
     public static class Solution1 {
