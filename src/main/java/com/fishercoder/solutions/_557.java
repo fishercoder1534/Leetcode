26356cb157a778e02f6cf7540907cb013218f3f5
Fisher Coder <fishercoder@gmail.com>
2020-07-16 17:20:10 -0700

    refactor 557

--- src/main/java/com/fishercoder/solutions/_557.java
+++ src/main/java/com/fishercoder/solutions/_557.java
@@ -1,15 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 557. Reverse Words in a String III
- *
- * Given a string, you need to reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
-
- Example 1:
- Input: "Let's take LeetCode contest"
- Output: "s'teL ekat edoCteeL tsetnoc"
- Note: In the string, each word is separated by single space and there will not be any extra space in the string.
- */
 public class _557 {
 
     public static class Solution1 {
