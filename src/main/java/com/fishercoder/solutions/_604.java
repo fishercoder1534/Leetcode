ea990189bd7eadda86d9da2051a53beef333c712
Fisher Coder <fishercoder@gmail.com>
2020-07-17 07:07:44 -0700

    refactor 604

--- src/main/java/com/fishercoder/solutions/_604.java
+++ src/main/java/com/fishercoder/solutions/_604.java
@@ -3,37 +3,6 @@ package com.fishercoder.solutions;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
-/**
- * 604. Design Compressed String Iterator
- *
- * Design and implement a data structure for a compressed string iterator. It should support the following operations: next and hasNext.
- The given compressed string will be in the form of each letter followed by a positive integer representing
- the number of this letter existing in the original uncompressed string.
-
- next() - if the original string still has uncompressed characters, return the next letter; Otherwise return a white space.
- hasNext() - Judge whether there is any letter needs to be uncompressed.
-
- Note:
- Please remember to RESET your class variables declared in StringIterator,
- as static/class variables are persisted across multiple test cases. Please see here for more details.
-
- Example:
-
- StringIterator iterator = new StringIterator("L1e2t1C1o1d1e1");
-
- iterator.next(); // return 'L'
- iterator.next(); // return 'e'
- iterator.next(); // return 'e'
- iterator.next(); // return 't'
- iterator.next(); // return 'C'
- iterator.next(); // return 'o'
- iterator.next(); // return 'd'
- iterator.hasNext(); // return true
- iterator.next(); // return 'e'
- iterator.hasNext(); // return false
- iterator.next(); // return ' '
-
- */
 public class _604 {
     public static class Solution1 {
         public static class StringIterator {
