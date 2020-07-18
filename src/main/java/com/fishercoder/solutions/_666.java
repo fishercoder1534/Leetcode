71af3d05ae3740fbf7aab41bc5599cb4e3d1c037
Fisher Coder <fishercoder@gmail.com>
2020-07-18 06:51:40 -0700

    refactor 666

--- src/main/java/com/fishercoder/solutions/_666.java
+++ src/main/java/com/fishercoder/solutions/_666.java
@@ -5,51 +5,12 @@ import com.fishercoder.common.classes.TreeNode;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * 666. Path Sum IV
- * If the depth of a tree is smaller than 5, then this tree can be represented by a list of three-digits integers.
-
- For each integer in this list:
-
- The hundreds digit represents the depth D of this node, 1 <= D <= 4.
-
- The tens digit represents the position P of this node in the level it belongs to, 1 <= P <= 8.
- The position is the same as that in a full binary tree.
-
- The units digit represents the value V of this node, 0 <= V <= 9.
-
- Given a list of ascending three-digits integers representing a binary with the depth smaller than 5.
- You need to return the totalSum of all paths from the root towards the leaves.
-
- Example 1:
-
- Input: [113, 215, 221]
- Output: 12
- Explanation:
- The tree that the list represents is:
-   3
-  / \
- 5   1
-
- The path totalSum is (3 + 5) + (3 + 1) = 12.
-
- Example 2:
-
- Input: [113, 221]
- Output: 4
- Explanation:
- The tree that the list represents is:
- 3
-  \
-  1
-
- The path totalSum is (3 + 1) = 4.
-
- */
 public class _666 {
     public static class Solution1 {
-        /**OMG, since it's no larger than depth 5, I've got a hardcoded solution here....
-         * By "harcoded", I mean the constructTree() method.*/
+        /**
+         * OMG, since it's no larger than depth 5, I've got a hardcoded solution here....
+         * By "harcoded", I mean the constructTree() method.
+         */
         public int totalSum = 0;
 
         public int pathSum(int[] nums) {
