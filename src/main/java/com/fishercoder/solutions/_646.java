ad398bc76214356350dce76efdb0ca0a5a88693d
Fisher Coder <fishercoder@gmail.com>
2020-07-17 20:11:41 -0700

    refactor 646

--- src/main/java/com/fishercoder/solutions/_646.java
+++ src/main/java/com/fishercoder/solutions/_646.java
@@ -2,23 +2,6 @@ package com.fishercoder.solutions;
 
 import java.util.Arrays;
 
-/**
- * 646. Maximum Length of Pair Chain
- *
- * You are given n pairs of numbers. In every pair, the first number is always smaller than the second number.
-
- Now, we define a pair (c, d) can follow another pair (a, b) if and only if b < c. Chain of pairs can be formed in this fashion.
-
- Given a set of pairs, find the length longest chain which can be formed. You needn't use up all the given pairs. You can select pairs in any order.
-
- Example 1:
- Input: [[1,2], [2,3], [3,4]]
- Output: 2
- Explanation: The longest chain is [1,2] -> [3,4]
-
- Note:
- The number of given pairs will be in the range [1, 1000].
- */
 public class _646 {
 
     public static class Solution1 {
