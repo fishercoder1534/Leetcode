ffef2c5ac6b62e20edcaa339dc252f240d3628d0
Fisher Coder <fishercoder@gmail.com>
2020-07-18 06:47:19 -0700

    refactor 651

--- src/main/java/com/fishercoder/solutions/_651.java
+++ src/main/java/com/fishercoder/solutions/_651.java
@@ -1,38 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 651. 4 Keys Keyboard
- *
- * Imagine you have a special keyboard with the following keys:
-
- Key 1: (A): Prints one 'A' on screen.
-
- Key 2: (Ctrl-A): Select the whole screen.
-
- Key 3: (Ctrl-C): Copy selection to buffer.
-
- Key 4: (Ctrl-V): Print buffer on screen appending it after what has already been printed.
-
- Now, you can only press the keyboard for N times (with the above four keys), find out the maximum numbers of 'A' you can print on screen.
-
- Example 1:
- Input: N = 3
- Output: 3
- Explanation:
- We can at most get 3 A's on screen by pressing following key sequence:
- A, A, A
-
- Example 2:
- Input: N = 7
- Output: 9
- Explanation:
- We can at most get 9 A's on screen by pressing following key sequence:
- A, A, A, Ctrl A, Ctrl C, Ctrl V, Ctrl V
-
- Note:
- 1 <= N <= 50
- Answers will be in the range of 32-bit signed integer.
- */
 public class _651 {
 
     public static class Solution1 {
