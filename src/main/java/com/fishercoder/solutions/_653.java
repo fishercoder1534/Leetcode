0e0ab5ed916bb1594b4f6371c1f46f97537f7c40
Fisher Coder <fishercoder@gmail.com>
2020-07-18 06:47:49 -0700

    refactor 653

--- src/main/java/com/fishercoder/solutions/_653.java
+++ src/main/java/com/fishercoder/solutions/_653.java
@@ -5,35 +5,6 @@ import com.fishercoder.common.classes.TreeNode;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 653. Two Sum IV - Input is a BST
- *
- * Given a Binary Search Tree and a target number,
- * return true if there exist two elements in the BST such that their sum is equal to the given target.
-
- Example 1:
- Input:
-    5
-   / \
-  3   6
- / \   \
-2   4   7
-
-Target = 9
-Output: True
-
-Example 2:
-Input:
-    5
-   / \
-  3   6
- / \   \
-2   4   7
-
-Target = 28
-Output: False
-
- */
 public class _653 {
 
     public static class Solution1 {
