0f5ae03c3434c2eea9dfed0cd1adcc304b32b8ab
Fisher Coder <fishercoder@gmail.com>
2020-07-18 06:49:27 -0700

    refactor 659

--- src/main/java/com/fishercoder/solutions/_659.java
+++ src/main/java/com/fishercoder/solutions/_659.java
@@ -3,37 +3,6 @@ package com.fishercoder.solutions;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * 659. Split Array into Consecutive Subsequences
- *
- * You are given an integer array sorted in ascending order (may contain duplicates),
- * you need to split them into several subsequences,
- * where each subsequences consist of at least 3 consecutive integers. Return whether you can make such a split.
-
- Example 1:
- Input: [1,2,3,3,4,5]
- Output: True
- Explanation:
- You can split them into two consecutive subsequences :
- 1, 2, 3
- 3, 4, 5
-
- Example 2:
- Input: [1,2,3,3,4,4,5,5]
- Output: True
- Explanation:
- You can split them into two consecutive subsequences :
- 1, 2, 3, 4, 5
- 3, 4, 5
-
- Example 3:
- Input: [1,2,3,4,4,5]
- Output: False
-
- Note:
- The length of the input is in range of [1, 10000]
- */
-
 public class _659 {
 
     public static class Solution1 {
