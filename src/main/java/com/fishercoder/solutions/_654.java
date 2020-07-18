236345758675f0dcb2969e776a5a926e27e02ff4
Fisher Coder <fishercoder@gmail.com>
2020-07-18 06:48:06 -0700

    refactor 654

--- src/main/java/com/fishercoder/solutions/_654.java
+++ src/main/java/com/fishercoder/solutions/_654.java
@@ -2,41 +2,17 @@ package com.fishercoder.solutions;
 
 import com.fishercoder.common.classes.TreeNode;
 
-/**
- * 654. Maximum Binary Tree
- *
- * Given an integer array with no duplicates. A maximum tree building on this array is defined as follow:
- *
- * The root is the maximum number in the array.
- * The left subtree is the maximum tree constructed from left part subarray divided by the maximum number.
- * The right subtree is the maximum tree constructed from right part subarray divided by the maximum number.
- * Construct the maximum tree by the given array and output the root node of this tree.
-
- Example 1:
- Input: [3,2,1,6,0,5]
- Output: return the tree root node representing the following tree:
-
-    6
-  /   \
- 3    5
-  \   /
-  2  0
-   \
-   1
-
- Note:
- The size of the given array will be in the range [1,1000].
- */
 public class _654 {
 
     public static class Solution1 {
         /**
          * Completely my original solution:
-         *
+         * <p>
          * As the problem states, I always broke the array into two halves and make notes
          * of current max node, then in the recursive call, we can recursively search
          * from its left part to construct its left subtree and its right part to construct its
-         * right subtree.*/
+         * right subtree.
+         */
         public TreeNode constructMaximumBinaryTree(int[] nums) {
             int max = Integer.MIN_VALUE;
             int maxIndex = -1;
