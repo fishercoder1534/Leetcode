02a3df5be869c3dca1d99cf11b5e383fb912f229
Fisher Coder <fishercoder@gmail.com>
2020-07-18 06:48:27 -0700

    refactor 655

--- src/main/java/com/fishercoder/solutions/_655.java
+++ src/main/java/com/fishercoder/solutions/_655.java
@@ -6,62 +6,6 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-/**
- * 655. Print Binary Tree
- *
- * Print a binary tree in an m*n 2D string array following these rules:
- * 1. The row number m should be equal to the height of the given binary tree.
- * 2. The column number n should always be an odd number.
- * 3. The root node's value (in string format) should be put in the exactly middle of the first row it can be put.
- *    The column and the row where the root node belongs will separate the rest space into two parts (left-bottom part and right-bottom part).
- *    You should print the left subtree in the left-bottom part and print the right subtree in the right-bottom part.
- *    The left-bottom part and the right-bottom part should have the same size.
- *    Even if one subtree is none while the other is not,
- *    you don't need to print anything for the none subtree but
- *    still need to leave the space as large as that for the other subtree.
- *    However, if two subtrees are none, then you don't need to leave space for both of them.
- * 4. Each unused space should contain an empty string "".
- * 5. Print the subtrees following the same rules.
-
- Example 1:
- Input:
-   1
-  /
- 2
- Output:
- [["", "1", ""],
- ["2", "", ""]]
-
- Example 2:
- Input:
-   1
-  / \
- 2   3
-  \
-  4
- Output:
- [["", "", "", "1", "", "", ""],
- ["", "2", "", "", "", "3", ""],
- ["", "", "4", "", "", "", ""]]
-
- Example 3:
- Input:
-       1
-      / \
-     2   5
-    /
-   3
-  /
- 4
- Output:
-
- [["",  "",  "", "",  "", "", "", "1", "",  "",  "",  "",  "", "", ""]
- ["",  "",  "", "2", "", "", "", "",  "",  "",  "",  "5", "", "", ""]
- ["",  "3", "", "",  "", "", "", "",  "",  "",  "",  "",  "", "", ""]
- ["4", "",  "", "",  "", "", "", "",  "",  "",  "",  "",  "", "", ""]]
-
- Note: The height of binary tree is in the range of [1, 10]
- */
 public class _655 {
 
     public static class Solution1 {
