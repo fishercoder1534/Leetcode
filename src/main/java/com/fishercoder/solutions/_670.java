f82c1a897d6b867308906aa33eb3f81884145f84
Fisher Coder <fishercoder@gmail.com>
2020-07-18 07:12:05 -0700

    refactor 670

--- src/main/java/com/fishercoder/solutions/_670.java
+++ src/main/java/com/fishercoder/solutions/_670.java
@@ -1,25 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 670. Maximum Swap
- *
- * Given a non-negative integer, you could swap two digits at most once to get the maximum valued number.
- * Return the maximum valued number you could get.
-
- Example 1:
- Input: 2736
- Output: 7236
- Explanation: Swap the number 2 and the number 7.
-
- Example 2:
- Input: 9973
- Output: 9973
- Explanation: No swap.
-
- Note:
- The given number is in the range [0, 108]
-
- */
 public class _670 {
     public static class Solution1 {
         public int maximumSwap(int num) {
