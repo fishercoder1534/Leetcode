3838f841ca14bc1c042ee2b211555be727eee41a
Fisher Coder <fishercoder@gmail.com>
2020-07-18 06:47:34 -0700

    refactor 652

--- src/main/java/com/fishercoder/solutions/_652.java
+++ src/main/java/com/fishercoder/solutions/_652.java
@@ -11,30 +11,6 @@ import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
 
-/**
- * 652. Find Duplicate Subtrees
- *
- * Given a binary tree, return all duplicate subtrees. For each kind of duplicate subtrees, you only need to return the root node of any one of them.
-
- Two trees are duplicate if they have the same structure with same node values.
-
- Example 1:
-     1
-    / \
-   2   3
-  /   / \
- 4   2   4
-    /
-   4
- The following are two duplicate subtrees:
-     2
-    /
-   4
- and
- 4
-
- Therefore, you need to return above trees' root in the form of a list.
- */
 public class _652 {
     public static class Solution1 {
 
