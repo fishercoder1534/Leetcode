e25ef9dc61a89ca346be43db1fc604b564fc9e5f
Fisher Coder <fishercoder@gmail.com>
2020-07-18 07:11:31 -0700

    refactor 668

--- src/main/java/com/fishercoder/solutions/_668.java
+++ src/main/java/com/fishercoder/solutions/_668.java
@@ -2,42 +2,6 @@ package com.fishercoder.solutions;
 
 import java.util.PriorityQueue;
 
-/**
- * 668. Kth Smallest Number in Multiplication Table
- *
- *  Nearly every one have used the Multiplication Table.
- *  But could you find out the k-th smallest number quickly from the multiplication table?
- *  Given the height m and the length n of a m * n Multiplication Table,
- *  and a positive integer k, you need to return the k-th smallest number in this table.
-
- Example 1:
- Input: m = 3, n = 3, k = 5
- Output:
- Explanation:
- The Multiplication Table:
- 1	2	3
- 2	4	6
- 3	6	9
-
- The 5-th smallest number is 3 (1, 2, 2, 3, 3).
-
-
- Example 2:
- Input: m = 2, n = 3, k = 6
- Output:
- Explanation:
- The Multiplication Table:
- 1	2	3
- 2	4	6
-
- The 6-th smallest number is 6 (1, 2, 2, 3, 4, 6).
-
- Note:
-
- The m and n will be in the range [1, 30000].
- The k will be in the range [1, m * n]
- */
-
 public class _668 {
     public static class Solution1 {
         /**
@@ -65,7 +29,9 @@ public class _668 {
     }
 
     public static class Solution2 {
-        /**reference: https://discuss.leetcode.com/topic/101132/java-solution-binary-search*/
+        /**
+         * reference: https://discuss.leetcode.com/topic/101132/java-solution-binary-search
+         */
         public int findKthNumber(int m, int n, int k) {
             int low = 1;
             int high = m * n + 1;
