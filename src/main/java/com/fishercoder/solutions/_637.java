1700eedc3ae3ab7c9e452d8fd88d95f927471639
Fisher Coder <fishercoder@gmail.com>
2020-07-17 18:40:39 -0700

    refactor 637

--- src/main/java/com/fishercoder/solutions/_637.java
+++ src/main/java/com/fishercoder/solutions/_637.java
@@ -7,26 +7,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-/**
- * 637. Average of Levels in Binary Tree
- *
- Given a non-empty binary tree, return the average value of the nodes on each level in the form of an array.
-
- Example 1:
- Input:
-  3
- / \
-9  20
-  /  \
- 15   7
-
- Output: [3, 14.5, 11]
- Explanation:
-
- The average value of nodes on level 0 is 3,  on level 1 is 14.5, and on level 2 is 11. Hence return [3, 14.5, 11].
- Note:
- The range of node's value is in the range of 32-bit signed integer.
- */
 public class _637 {
 
     public static class Solution1 {
