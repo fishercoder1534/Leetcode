38f160d3f19262d8ef8031d64c7b507aa69b1252
Fisher Coder <fishercoder@gmail.com>
2020-07-18 06:49:12 -0700

    refactor 658

--- src/main/java/com/fishercoder/solutions/_658.java
+++ src/main/java/com/fishercoder/solutions/_658.java
@@ -3,27 +3,6 @@ package com.fishercoder.solutions;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 658. Find K Closest Elements
- *
- * Given a sorted array, two integers k and x,
- * find the k closest elements to x in the array.
- * The result should also be sorted in ascending order.
- * If there is a tie, the smaller elements are always preferred.
-
- Example 1:
- Input: [1,2,3,4,5], k=4, x=3
- Output: [1,2,3,4]
-
- Example 2:
- Input: [1,2,3,4,5], k=4, x=-1
- Output: [1,2,3,4]
-
- Note:
- The value k is positive and will always be smaller than the length of the sorted array.
- Length of the given array is positive and will not exceed 104
- Absolute value of elements in the array and x will not exceed 104
- */
 public class _658 {
     public static class Solution1 {
         public List<Integer> findClosestElements(List<Integer> arr, int k, int x) {
