0903137ab782e9b6ac785dbe783905ef5e36dd02
Fisher Coder <fishercoder@gmail.com>
2020-07-17 20:11:58 -0700

    refactor 647

--- src/main/java/com/fishercoder/solutions/_647.java
+++ src/main/java/com/fishercoder/solutions/_647.java
@@ -1,27 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 647. Palindromic Substrings
- *
- *  Given a string, your task is to count how many palindromic substrings in this string.
- *  The substrings with different start indexes or end indexes are counted
- *  as different substrings even they consist of same characters.
-
- Example 1:
- Input: "abc"
- Output: 3
- Explanation: Three palindromic strings: "a", "b", "c".
-
- Example 2:
- Input: "aaa"
- Output: 6
- Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
-
- Note:
- The input string length won't exceed 1000.
-
- */
-
 public class _647 {
 
     public static class Solution1 {
