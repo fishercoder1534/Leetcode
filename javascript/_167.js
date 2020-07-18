fba55e9bd8ced1c53777d45bd770420f0a5764ed
yaodehuang <sef1548@gmail.com>
2020-07-18 23:36:54 +0800

    feat: Add javascript Solution for 167

---
+++ javascript/_167.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+  var p1 = 0
+  var p2 = numbers.length - 1
+
+  while (p1 < p2) {
+    if (numbers[p1] + numbers[p2] < target) {
+      p1 += 1
+    } else if (numbers[p1] + numbers[p2] > target) {
+      p2 -= 1
+    } else {
+      return [p1+1 , p2 + 1]
+    }
+  }
+  return [-1 , -1]
+};
