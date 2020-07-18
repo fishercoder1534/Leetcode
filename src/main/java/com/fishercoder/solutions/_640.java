d39b19e0a301b176630905ccc794f04c6b6eeb12
Fisher Coder <fishercoder@gmail.com>
2020-07-17 18:41:18 -0700

    refactor 640

--- src/main/java/com/fishercoder/solutions/_640.java
+++ src/main/java/com/fishercoder/solutions/_640.java
@@ -1,38 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 640. Solve the Equation
- *
- * Solve a given equation and return the value of x in the form of string "x=#value".
- * The equation contains only '+', '-' operation, the variable x and its coefficient.
-
- If there is no solution for the equation, return "No solution".
-
- If there are infinite solutions for the equation, return "Infinite solutions".
-
- If there is exactly one solution for the equation, we ensure that the value of x is an integer.
-
- Example 1:
- Input: "x+5-3+x=6+x-2"
- Output: "x=2"
-
- Example 2:
- Input: "x=x"
- Output: "Infinite solutions"
-
- Example 3:
- Input: "2x=x"
- Output: "x=0"
-
- Example 4:
- Input: "2x+3x-6x=x+2"
- Output: "x=-1"
-
- Example 5:
- Input: "x=x+2"
- Output: "No solution"
-
- */
 public class _640 {
     public static class Solution1 {
         /**
