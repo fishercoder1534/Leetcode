c56da1a51e948c2ed2ac9b3b3c83c04683d2ce8b
Fisher Coder <fishercoder@gmail.com>
2020-07-17 20:11:11 -0700

    refactor 644

--- src/main/java/com/fishercoder/solutions/_644.java
+++ src/main/java/com/fishercoder/solutions/_644.java
@@ -1,24 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 644. Maximum Average Subarray II
- * <p>
- * Given an array consisting of n integers, find the contiguous subarray whose length is greater than
- * or equal to k that has the maximum average value. And you need to output the maximum average value.
- * <p>
- * Example 1:
- * Input: [1,12,-5,-6,50,3], k = 4
- * Output: 12.75
- * Explanation:
- * when length is 5, maximum average value is 10.8,
- * when length is 6, maximum average value is 9.16667.
- * Thus return 12.75.
- * <p>
- * Note:
- * 1 <= k <= n <= 10,000.
- * Elements of the given array will be in range [-10,000, 10,000].
- * The answer with the calculation error less than 10-5 will be accepted.
- */
 public class _644 {
     /**reference: https://leetcode.com/articles/maximum-average-subarray-ii/#approach-2-using-binary-search-accepted
      * https://discuss.leetcode.com/topic/96123/java-solution-o-nlogm-binary-search-the-answer/13*/
