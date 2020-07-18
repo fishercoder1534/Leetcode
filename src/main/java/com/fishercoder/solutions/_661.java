8259293e2fc8dde27bcc01fd7aca0c52b5f98635
Fisher Coder <fishercoder@gmail.com>
2020-07-18 06:50:17 -0700

    refactor 661

--- src/main/java/com/fishercoder/solutions/_661.java
+++ src/main/java/com/fishercoder/solutions/_661.java
@@ -1,33 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 661. Image Smoother
- *
- * Given a 2D integer matrix m representing the gray scale of an image,
- * you need to design a smoother to make the gray scale of each cell becomes the average gray scale (rounding down) of
- * all the 8 surrounding cells and itself. If a cell has less than 8 surrounding cells, then use as many as you can.
-
- Example 1:
- Input:
- [[1,1,1],
- [1,0,1],
- [1,1,1]]
-
- Output:
- [[0, 0, 0],
- [0, 0, 0],
- [0, 0, 0]]
-
- Explanation:
- For the point (0,0), (0,2), (2,0), (2,2): floor(3/4) = floor(0.75) = 0
- For the point (0,1), (1,0), (1,2), (2,1): floor(5/6) = floor(0.83333333) = 0
- For the point (1,1): floor(8/9) = floor(0.88888889) = 0
-
- Note:
- The value in the given matrix is in the range of [0, 255].
- The length and width of the given matrix are in the range of [1, 150].
-
- */
 public class _661 {
     public static class Solution1 {
         public int[][] imageSmoother(int[][] M) {
