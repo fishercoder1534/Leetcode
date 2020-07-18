5aa30f642a91fa496f19ffc83473e027650a0dcf
Fisher Coder <fishercoder@gmail.com>
2020-07-18 06:50:53 -0700

    refactor 664

--- src/main/java/com/fishercoder/solutions/_664.java
+++ src/main/java/com/fishercoder/solutions/_664.java
@@ -1,25 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 664. Strange Printer
- *
- * There is a strange printer with the following two special requirements:
- * The printer can only print a sequence of the same character each time.
- * At each turn, the printer can print new characters starting from and ending at any places, and will cover the original existing characters.
- * Given a string consists of lower English letters only, your job is to count the minimum number of turns the printer needed in order to print it.
-
- Example 1:
- Input: "aaabbb"
- Output: 2
- Explanation: Print "aaa" first and then print "bbb".
-
- Example 2:
- Input: "aba"
- Output: 2
- Explanation: Print "aaa" first and then print "b" from the second place of the string, which will cover the existing character 'a'.
-
- Hint: Length of the given string will not exceed 100.
- */
 public class _664 {
     public static class Solution1 {
         /**
