e0fce4b82a6957f81908dc481529b3f45a181ec7
Fisher Coder <fishercoder@gmail.com>
2020-07-17 18:41:01 -0700

    refactor 639

--- src/main/java/com/fishercoder/solutions/_639.java
+++ src/main/java/com/fishercoder/solutions/_639.java
@@ -1,35 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 639. Decode Ways II
- *
- * A message containing letters from A-Z is being encoded to numbers using the following mapping way:
-
- 'A' -> 1
- 'B' -> 2
- ...
- 'Z' -> 26
- Beyond that, now the encoded string can also contain the character '*',
- which can be treated as one of the numbers from 1 to 9.
-
- Given the encoded message containing digits and the character '*',
- return the total number of ways to decode it.
-
- Also, since the answer may be very large, you should return the output mod 109 + 7.
-
- Example 1:
- Input: "*"
- Output: 9
- Explanation: The encoded message can be decoded to the string: "A", "B", "C", "D", "E", "F", "G", "H", "I".
- Example 2:
- Input: "1*"
- Output: 9 + 9 = 18
-
- Note:
- The length of the input string will fit in range [1, 105].
- The input string will only contain the character '*' and digits '0' - '9'.
-
- */
 public class _639 {
     public static class Solution1 {
         /**
