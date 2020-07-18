b5edf8c7fcbc8b781d31936489af7bf11d45476d
Fisher Coder <fishercoder@gmail.com>
2020-07-17 20:07:25 -0700

    refactor 643

--- src/main/java/com/fishercoder/solutions/_643.java
+++ src/main/java/com/fishercoder/solutions/_643.java
@@ -1,17 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 643. Maximum Average Subarray I
- * Given an array consisting of n integers, find the contiguous subarray of given length k that has the maximum average value.
- * And you need to output the maximum average value.
- * Example 1:
- * Input: [1,12,-5,-6,50,3], k = 4
- * Output: 12.75
- * Explanation: Maximum average is (12-5-6+50)/4 = 51/4 = 12.75
- * Note:
- * 1 <= k <= n <= 30,000.
- * Elements of the given array will be in the range [-10,000, 10,000].
- */
 public class _643 {
 
     public static class Solution1 {
