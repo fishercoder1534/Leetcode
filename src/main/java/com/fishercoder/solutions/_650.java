b9e082902faa7f45e7835829ed45a46c66d123c5
Fisher Coder <fishercoder@gmail.com>
2020-07-18 06:46:59 -0700

    refactor 650

--- src/main/java/com/fishercoder/solutions/_650.java
+++ src/main/java/com/fishercoder/solutions/_650.java
@@ -1,27 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 650. 2 Keys Keyboard
- *
- * Initially on a notepad only one character 'A' is present. You can perform two operations on this notepad for each step:
-
- Copy All: You can copy all the characters present on the notepad (partial copy is not allowed).
- Paste: You can paste the characters which are copied last time.
- Given a number n. You have to get exactly n 'A' on the notepad by performing the minimum number of steps permitted. Output the minimum number of steps to get n 'A'.
-
- Example 1:
- Input: 3
- Output: 3
-
- Explanation:
- Intitally, we have one character 'A'.
- In step 1, we use Copy All operation.
- In step 2, we use Paste operation to get 'AA'.
- In step 3, we use Paste operation to get 'AAA'.
-
- Note:
- The n will be in the range [1, 1000].
- */
 public class _650 {
 
     public static class Solution1 {
