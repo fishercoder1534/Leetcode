f97b9239aa1dd89462c9b220ec83548c4d451dd1
Fisher Coder <fishercoder@gmail.com>
2020-07-20 06:49:46 -0700

    refactor 679

--- src/main/java/com/fishercoder/solutions/_679.java
+++ src/main/java/com/fishercoder/solutions/_679.java
@@ -2,35 +2,14 @@ package com.fishercoder.solutions;
 
 import java.util.stream.IntStream;
 
-/**
- * 679. 24 Game
- *
- *  You have 4 cards each containing a number from 1 to 9.
- *  You need to judge whether they could operated through *, /, +, -, (, ) to get the value of 24.
-
- Example 1:
- Input: [4, 1, 8, 7]
- Output: True
- Explanation: (8-4) * (7-1) = 24
-
- Example 2:
- Input: [1, 2, 1, 2]
- Output: False
-
- Note:
- The division operator / represents real division, not integer division. For example, 4 / (1 - 2/3) = 12.
- Every operation done is between two numbers.
- In particular, we cannot use - as a unary operator. For example, with [1, 1, 1, 1] as input, the expression -1 - 1 - 1 - 1 is not allowed.
- You cannot concatenate numbers together. For example, if the input is [1, 2, 1, 2], we cannot write this as 12 + 12.
-
- */
-
 public class _679 {
     public static class Solution1 {
-        /**Since there are only 4 cards and only 4 operations, we can iterate through all possible combinations, there's a total of 9216 possibilities:
+        /**
+         * Since there are only 4 cards and only 4 operations, we can iterate through all possible combinations, there's a total of 9216 possibilities:
          * 1. we pick two out of four cards, with order (since order matters for division), 4 * 3 = 12, then pick one of four operations: 12 * 4 = 48;
          * 2. then we pick two from these three numbers: 12 * 4 * 3 * 4 * 2 = 1152
-         * 3. then we pick the remaining two: 1152 * 2 * 4 = 9216 (with order and out of 4 operations)*/
+         * 3. then we pick the remaining two: 1152 * 2 * 4 = 9216 (with order and out of 4 operations)
+         */
         public boolean judgePoint24(int[] nums) {
             return dfs(IntStream.of(nums).mapToDouble(num -> num).toArray());
         }
