eea73a00269ab8401d7e9b63526b0a9728415798
yaodehuang <sef1548@gmail.com>
2020-07-20 12:42:31 +0800

    fix: travis syntax

--- javascript/_1480.js
+++ javascript/_1480.js
@@ -1,7 +1,8 @@
+
 var runningSum = function (nums) {
 
   for (var i = 0 ; i < nums.length - 1 ; i++) {
-    nums[i + 1] = nums[i] + nums[i + 1]
+    nums [i + 1] = nums [i] + nums [i + 1]
   }
   return nums
 }
