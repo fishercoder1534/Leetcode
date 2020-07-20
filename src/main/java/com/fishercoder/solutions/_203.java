7f58f0e308e87511b00bafc9b518693ecf7c2c79
Fisher Coder <fishercoder@gmail.com>
2020-07-20 06:49:02 -0700

    refactor 203

--- src/main/java/com/fishercoder/solutions/_203.java
+++ src/main/java/com/fishercoder/solutions/_203.java
@@ -2,21 +2,12 @@ package com.fishercoder.solutions;
 
 import com.fishercoder.common.classes.ListNode;
 
-/**
- * 203. Remove Linked List Elements
- *
- * Remove all elements from a linked list of integers that have value val.
- *
- * Example
- * Given: 1 --> 2 --> 6 --> 3 --> 4 --> 5 --> 6, val = 6
- * Return: 1 --> 2 --> 3 --> 4 --> 5
- */
 public class _203 {
     public static class Solution1 {
         /**
          * This is a very good question to test your understanding of pointers/memory/addresses, although it's marked as EASY.
          * All the three nodes: dummy, curr and prev are indispensable.
-         *
+         * <p>
          * 1. Eventually, we should return dummy.next as the final result.
          * 2. we assign head to curr, dummy to prev
          * 3. and then we use prev and curr to traverse through the list and do the work
