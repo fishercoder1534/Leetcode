aa9dc13cc0383a87144fb1cf37d5d4151e4d153a
Fisher Coder <fishercoder@gmail.com>
2020-07-20 06:51:27 -0700

    refactor 685

--- src/main/java/com/fishercoder/solutions/_685.java
+++ src/main/java/com/fishercoder/solutions/_685.java
@@ -5,40 +5,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-/**
- * 685. Redundant Connection II
- *
- * In this problem, a rooted tree is a directed graph such that, there is exactly one node (the root) for which all other nodes are
- * descendants of this node, plus every node has exactly one parents, except for the root node which has no parents.
- * The given input is a directed graph that started as a rooted tree with N nodes (with distinct values 1, 2, ..., N),
- * with one additional directed edge added. The added edge has two different vertices chosen from 1 to N, and was not an edge that already existed.
- * The resulting graph is given as a 2D-array of edges. Each element of edges is a pair [u, v] that
- * represents a directed edge connecting nodes u and v, where u is a parents of child v.
- * Return an edge that can be removed so that the resulting graph is a rooted tree of N nodes.
- * If there are multiple answers, return the answer that occurs last in the given 2D-array.
-
- Example 1:
- Input: [[1,2], [1,3], [2,3]]
- Output: [2,3]
- Explanation: The given directed graph will be like this:
-   1
-  / \
- v   v
- 2-->3
-
- Example 2:
- Input: [[1,2], [2,3], [3,4], [4,1], [1,5]]
- Output: [4,1]
- Explanation: The given directed graph will be like this:
- 5 <- 1 -> 2
-      ^    |
-      |    v
-      4 <- 3
-
- Note:
- The size of the input 2D-array will be between 3 and 1000.
- Every integer represented in the 2D-array will be between 1 and N, where N is the size of the input array.
- */
 public class _685 {
     public static class Solution1 {
         /**
@@ -179,7 +145,9 @@ public class _685 {
     }
 
     public static class Solution2 {
-        /**credit: https://discuss.leetcode.com/topic/105108/c-java-union-find-with-explanation-o-n*/
+        /**
+         * credit: https://discuss.leetcode.com/topic/105108/c-java-union-find-with-explanation-o-n
+         */
         public int[] findRedundantDirectedConnection(int[][] edges) {
             int[] can1 = {-1, -1};
             int[] can2 = {-1, -1};
