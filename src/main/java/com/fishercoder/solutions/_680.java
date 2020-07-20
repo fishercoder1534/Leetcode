ab6a1521b8880ab6d6de4dc5580f62c41fcd18da
Fisher Coder <fishercoder@gmail.com>
2020-07-20 06:50:01 -0700

    refactor 680

--- src/main/java/com/fishercoder/solutions/_680.java
+++ src/main/java/com/fishercoder/solutions/_680.java
@@ -1,23 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 680. Valid Palindrome II
- *
- *  Given a non-empty string s, you may delete at most one character. Judge whether you can make it a palindrome.
-
- Example 1:
- Input: "aba"
- Output: True
-
- Example 2:
- Input: "abca"
- Output: True
- Explanation: You could delete the character 'c'.
-
- Note:
- The string will only contain lowercase characters a-z. The maximum length of the string is 50000.
-
- */
 public class _680 {
     public static class Solution1 {
         public boolean validPalindrome(String s) {
