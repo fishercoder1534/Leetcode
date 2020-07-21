b47f3cbb94ed191d18508d3ce0b6eb0de8419072
Fisher Coder <fishercoder@gmail.com>
2020-07-21 07:39:11 -0700

    refactor 689

--- src/main/java/com/fishercoder/solutions/_689.java
+++ src/main/java/com/fishercoder/solutions/_689.java
@@ -1,34 +1,17 @@
 package com.fishercoder.solutions;
 
-/**
- * 689. Maximum Sum of 3 Non-Overlapping Subarrays
- *
- * In a given array nums of positive integers, find three non-overlapping subarrays with maximum sum.
- * Each subarray will be of size k, and we want to maximize the sum of all 3*k entries.
- * Return the result as a list of indices representing the starting position of each interval (0-indexed).
- * If there are multiple answers, return the lexicographically smallest one.
-
- Example:
- Input: [1,2,1,2,6,7,5,1], 2
- Output: [0, 3, 5]
- Explanation: Subarrays [1, 2], [2, 6], [7, 5] correspond to the starting indices [0, 3, 5].
- We could have also taken [2, 1], but an answer of [1, 3, 5] would be lexicographically larger.
-
- Note:
- nums.length will be between 1 and 20000.
- nums[i] will be between 1 and 65535.
- k will be between 1 and floor(nums.length / 3).
- */
 public class _689 {
     public static class Solution1 {
-        /**we basically need to find the interval (i, i+k-1) as the middle interval, where k <= i <= n-2k
+        /**
+         * we basically need to find the interval (i, i+k-1) as the middle interval, where k <= i <= n-2k
          * then this interval (0, i-1) will be the left interval
          * the interval (i+k, n-1) will be the right interval.
-         *
+         * <p>
          * Please pay special attention to the variable name I use here: this `k` is not a random one, it's the `k`
          * from the passed in parameter.
-         *
-         * Credit: https://discuss.leetcode.com/topic/105577/c-java-dp-with-explanation-o-n/*/
+         * <p>
+         * Credit: https://discuss.leetcode.com/topic/105577/c-java-dp-with-explanation-o-n/
+         */
         public int[] maxSumOfThreeSubarrays(int[] nums, int k) {
             if (nums == null || nums.length == 0) {
                 return new int[]{};
