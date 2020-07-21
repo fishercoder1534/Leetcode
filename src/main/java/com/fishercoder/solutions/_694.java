db51fccfdc109db33db6b42b9820e450355a019f
Fisher Coder <fishercoder@gmail.com>
2020-07-21 09:49:35 -0700

    refactor 694

--- src/main/java/com/fishercoder/solutions/_694.java
+++ src/main/java/com/fishercoder/solutions/_694.java
@@ -6,47 +6,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-/**
- * 694. Number of Distinct Islands
- *
- * Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's (representing land)
- * connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
- * Count the number of distinct islands.
- * An island is considered to be the same as another if and only if
- * one island can be translated (and not rotated or reflected) to equal the other.
-
- Example 1:
-
- 11000
- 11000
- 00011
- 00011
-
- Given the above grid map, return 1.
-
- Example 2:
-
- 11011
- 10000
- 00001
- 11011
-
- Given the above grid map, return 3.
-
- Notice that:
-
- 11
- 1
-
- and
-
- 1
- 11
-
- are considered different island shapes, because we do not consider reflection / rotation.
-
- Note: The length of each dimension in the given grid does not exceed 50.
- */
 public class _694 {
     public static class Solution1 {
         /**
