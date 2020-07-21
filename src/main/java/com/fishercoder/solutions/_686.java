d724ebec7e0e4e15fc05aa451edb3eaccc360e63
Fisher Coder <fishercoder@gmail.com>
2020-07-21 06:19:53 -0700

    refactor 686

--- src/main/java/com/fishercoder/solutions/_686.java
+++ src/main/java/com/fishercoder/solutions/_686.java
@@ -3,19 +3,6 @@ package com.fishercoder.solutions;
 import java.util.HashSet;
 import java.util.Set;
 
-/**
- * 686. Repeated String Match
- *
- * Given two strings A and B, find the minimum number of times A has to be repeated such that B is a substring of it.
- * If no such solution, return -1.
- * For example, with A = "abcd" and B = "cdabcdab".
- * Return 3, because by repeating A three times (“abcdabcdabcd”),
- * B is a substring of it; and B is not a substring of A repeated two times ("abcdabcd").
-
- Note:
- The length of A and B will be between 1 and 10000.
- */
-
 public class _686 {
     public static class Solution1 {
         public int repeatedStringMatch(String A, String B) {
@@ -43,7 +30,7 @@ public class _686 {
         /**
          * Time: O(N(N+M))
          * Space: O(N + M)
-         * */
+         */
         public int repeatedStringMatch(String A, String B) {
             int count = 1;
             StringBuilder sb = new StringBuilder(A);
