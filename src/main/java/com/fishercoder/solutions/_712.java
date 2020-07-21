6356f103a27b11a82289fca99986c10b02537758
Fisher Coder <fishercoder@gmail.com>
2020-07-21 09:52:44 -0700

    refactor 712

--- src/main/java/com/fishercoder/solutions/_712.java
+++ src/main/java/com/fishercoder/solutions/_712.java
@@ -1,29 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 712. Minimum ASCII Delete Sum for Two Strings
- *
- * Given two strings s1, s2, find the lowest ASCII sum of deleted characters to make two strings equal.
-
- Example 1:
- Input: s1 = "sea", s2 = "eat"
- Output: 231
- Explanation: Deleting "s" from "sea" adds the ASCII value of "s" (115) to the sum.
- Deleting "t" from "eat" adds 116 to the sum.
- At the end, both strings are equal, and 115 + 116 = 231 is the minimum sum possible to achieve this.
-
- Example 2:
- Input: s1 = "delete", s2 = "leet"
- Output: 403
- Explanation: Deleting "dee" from "delete" to turn the string into "let",
- adds 100[d]+101[e]+101[e] to the sum.  Deleting "e" from "leet" adds 101[e] to the sum.
- At the end, both strings are equal to "let", and the answer is 100+101+101+101 = 403.
- If instead we turned both strings into "lee" or "eet", we would get answers of 433 or 417, which are higher.
-
- Note:
- 0 < s1.length, s2.length <= 1000.
- All elements of each string will have an ASCII value in [97, 122].
- */
 public class _712 {
     public static class Solution1 {
         //credit: https://leetcode.com/articles/minimum-ascii-delete-sum-for-two-strings/
