8af3b5ab82fa92941909fad8856857db25b00f91
Fisher Coder <fishercoder@gmail.com>
2020-07-21 09:50:35 -0700

    refactor 698

--- src/main/java/com/fishercoder/solutions/_698.java
+++ src/main/java/com/fishercoder/solutions/_698.java
@@ -1,21 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 698. Partition to K Equal Sum Subsets
- *
- * Given an array of integers nums and a positive integer k,
- * find whether it's possible to divide this array into k non-empty subsets whose sums are all equal.
-
- Example 1:
-
- Input: nums = [4, 3, 2, 3, 5, 2, 1], k = 4
- Output: True
- Explanation: It's possible to divide it into 4 subsets (5), (1, 4), (2,3), (2,3) with equal sums.
-
- Note:
- 1 <= k <= len(nums) <= 16.
- 0 < nums[i] < 10000.
- */
 public class _698 {
 
     public static class Solution1 {
