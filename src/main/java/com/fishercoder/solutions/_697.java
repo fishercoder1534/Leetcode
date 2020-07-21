bf3c70758c5322c525758298172fdfada6dae18f
Fisher Coder <fishercoder@gmail.com>
2020-07-21 09:50:21 -0700

    refactor 697

--- src/main/java/com/fishercoder/solutions/_697.java
+++ src/main/java/com/fishercoder/solutions/_697.java
@@ -6,31 +6,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * 697. Degree of an Array
- *
- * Given a non-empty array of non-negative integers nums,
- * the degree of this array is defined as the maximum frequency of any one of its elements.
- * Your task is to find the smallest possible length of a (contiguous) subarray of nums, that has the same degree as nums.
-
- Example 1:
-
- Input: [1, 2, 2, 3, 1]
- Output: 2
- Explanation:
- The input array has a degree of 2 because both elements 1 and 2 appear twice.
- Of the subarrays that have the same degree:
- [1, 2, 2, 3, 1], [1, 2, 2, 3], [2, 2, 3, 1], [1, 2, 2], [2, 2, 3], [2, 2]
- The shortest length is 2. So return 2.
-
- Example 2:
- Input: [1,2,2,3,1,4,2]
- Output: 6
-
- Note:
- nums.length will be between 1 and 50,000.
- nums[i] will be an integer between 0 and 49,999.
- */
 public class _697 {
     public static class Solution1 {
         public int findShortestSubArray(int[] nums) {
