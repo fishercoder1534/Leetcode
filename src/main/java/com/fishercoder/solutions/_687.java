0c37eab28426c725ad79d013c773c2f9ad0376e7
Fisher Coder <fishercoder@gmail.com>
2020-07-21 07:38:31 -0700

    refactor 687

--- src/main/java/com/fishercoder/solutions/_687.java
+++ src/main/java/com/fishercoder/solutions/_687.java
@@ -2,39 +2,6 @@ package com.fishercoder.solutions;
 
 import com.fishercoder.common.classes.TreeNode;
 
-/**
- * 687. Longest Univalue Path
- *
- * Given a binary tree, find the length of the longest path where each node in the path has the same value.
- * This path may or may not pass through the root.
-
- Note: The length of path between two nodes is represented by the number of edges between them.
-
- Example 1:
- Input:
-     5
-    / \
-   4   5
-  / \   \
- 1  1   5
-
- Output:
- 2
-
- Example 2:
- Input:
-
-     1
-    / \
-   4   5
-  / \   \
- 4  4   5
- Output:
- 2
-
- Note: The given binary tree has not more than 10000 nodes. The height of the tree is not more than 1000.
- */
-
 public class _687 {
     public static class Solution1 {
         /**
@@ -47,7 +14,7 @@ public class _687 {
             }
             return result[0];
         }
-        
+
         // calculate longest univalue path from root to leaves
         // In addition, the maximum univalue path cross the root node is calculated and then global maximum is udpated.
         private int dfs(TreeNode root, int[] result) {
