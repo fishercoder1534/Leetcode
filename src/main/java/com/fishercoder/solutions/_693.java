e20b0d06cf21e6f6125d31cf86066918afe723c3
Fisher Coder <fishercoder@gmail.com>
2020-07-21 09:49:19 -0700

    refactor 693

--- src/main/java/com/fishercoder/solutions/_693.java
+++ src/main/java/com/fishercoder/solutions/_693.java
@@ -1,35 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 693. Binary Number with Alternating Bits
- *
- * Given a positive integer, check whether it has alternating bits: namely, if two adjacent bits will always have different values.
-
- Example 1:
- Input: 5
- Output: True
- Explanation:
- The binary representation of 5 is: 101
-
- Example 2:
- Input: 7
- Output: False
- Explanation:
- The binary representation of 7 is: 111.
-
- Example 3:
- Input: 11
- Output: False
- Explanation:
- The binary representation of 11 is: 1011.
-
- Example 4:
- Input: 10
- Output: True
- Explanation:
- The binary representation of 10 is: 1010.
- */
-
 public class _693 {
     public static class Solution1 {
         public boolean hasAlternatingBits(int n) {
