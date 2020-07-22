9f87dfc9cc024e742977bc1097a5af5bab50c8cc
Fisher Coder <fishercoder@gmail.com>
2020-07-22 15:59:35 -0700

    refactor 763

--- src/main/java/com/fishercoder/solutions/_763.java
+++ src/main/java/com/fishercoder/solutions/_763.java
@@ -3,48 +3,29 @@ package com.fishercoder.solutions;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 763. Partition Labels
- *
- * A string S of lowercase letters is given.
- * We want to partition this string into as many parts as possible so that each letter appears
- * in at most one part, and return a list of integers representing the size of these parts.
-
- Example 1:
- Input: S = "ababcbacadefegdehijhklij"
- Output: [9,7,8]
- Explanation:
- The partition is "ababcbaca", "defegde", "hijhklij".
- This is a partition so that each letter appears in at most one part.
- A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits S into less parts.
-
- Note:
- S will have length in range [1, 500].
- S will consist of lowercase letters ('a' to 'z') only.
- */
 public class _763 {
 
-  public static class Solution1 {
-    public List<Integer> partitionLabels(String S) {
-      List<Integer> result = new ArrayList<>();
-      int[] last = new int[26];
-      /**This is the key step:
-       * we find the last occurrence of each letter and record them in last[]*/
-      for (int i = 0; i < S.length(); i++) {
-        last[S.charAt(i) - 'a'] = i;
-      }
-      /**record the last end index of the current substring*/
-      int end = 0;
-      int start = 0;
-      for (int i = 0; i < S.length(); i++) {
-        end = Math.max(end, last[S.charAt(i) - 'a']);
-        if (end == i) {
-          result.add(end - start + 1);
-          start = end + 1;
+    public static class Solution1 {
+        public List<Integer> partitionLabels(String S) {
+            List<Integer> result = new ArrayList<>();
+            int[] last = new int[26];
+            /**This is the key step:
+             * we find the last occurrence of each letter and record them in last[]*/
+            for (int i = 0; i < S.length(); i++) {
+                last[S.charAt(i) - 'a'] = i;
+            }
+            /**record the last end index of the current substring*/
+            int end = 0;
+            int start = 0;
+            for (int i = 0; i < S.length(); i++) {
+                end = Math.max(end, last[S.charAt(i) - 'a']);
+                if (end == i) {
+                    result.add(end - start + 1);
+                    start = end + 1;
+                }
+            }
+            return result;
         }
-      }
-      return result;
     }
-  }
 
 }
