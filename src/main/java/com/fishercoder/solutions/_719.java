8879d2be05be653f6e89adcd3cfe03e3b5cd60bf
Fisher Coder <fishercoder@gmail.com>
2020-07-22 12:41:55 -0700

    refactor 719

--- src/main/java/com/fishercoder/solutions/_719.java
+++ src/main/java/com/fishercoder/solutions/_719.java
@@ -3,30 +3,6 @@ package com.fishercoder.solutions;
 import java.util.Arrays;
 import java.util.PriorityQueue;
 
-/**
- * 719. Find K-th Smallest Pair Distance
- *
- * Given an integer array, return the k-th smallest distance among all the pairs.
- * The distance of a pair (A, B) is defined as the absolute difference between A and B.
-
- Example 1:
- Input:
- nums = [1,3,1]
- k = 1
- Output: 0
- Explanation:
- Here are all the pairs:
- (1,3) -> 2
- (1,1) -> 0
- (3,1) -> 2
- Then the 1st smallest distance pair is (1,1), and its distance is 0.
-
- Note:
- 2 <= len(nums) <= 10000.
- 0 <= nums[i] < 1000000.
- 1 <= k <= len(nums) * (len(nums) - 1) / 2.
- */
-
 public class _719 {
     public static class Solution1 {
         /**
