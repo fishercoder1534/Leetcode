c6bbb4d76435a73797c4f6aeba42c31351a69f4f
Fisher Coder <fishercoder@gmail.com>
2020-07-22 12:44:44 -0700

    refactor 734

--- src/main/java/com/fishercoder/solutions/_734.java
+++ src/main/java/com/fishercoder/solutions/_734.java
@@ -1,20 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 734. Sentence Similarity
- *
- * Given two sentences words1, words2 (each represented as an array of strings), and a list of similar word pairs pairs, determine if two sentences are similar.
- * For example, "great acting skills" and "fine drama talent" are similar, if the similar word pairs are pairs = [["great", "fine"], ["acting","drama"], ["skills","talent"]].
- * Note that the similarity relation is not transitive. For example, if "great" and "fine" are similar, and "fine" and "good" are similar, "great" and "good" are not necessarily similar.
- * Also, a word is always similar with itself. For example, the sentences words1 = ["great"], words2 = ["great"], pairs = [] are similar, even though there are no specified similar word pairs.
-
- Note:
-
- The length of words1 and words2 will not exceed 1000.
- The length of pairs will not exceed 2000.
- The length of each pairs[i] will be 2.
- The length of each words[i] and pairs[i][j] will be in the range [1, 20].
- */
 public class _734 {
     public static class Solution1 {
         public boolean areSentencesSimilar(String[] words1, String[] words2, String[][] pairs) {
