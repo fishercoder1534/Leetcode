cc3ee31b0b4ff3e51f7dbad6c6e5175bbdc8b2a2
Fisher Coder <fishercoder@gmail.com>
2020-07-22 12:45:31 -0700

    refactor 738

--- src/main/java/com/fishercoder/solutions/_738.java
+++ src/main/java/com/fishercoder/solutions/_738.java
@@ -1,25 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 738. Monotone Increasing Digits
- *
- * Given a non-negative integer N, find the largest number that is less than or equal to N with monotone increasing digits.
- * (Recall that an integer has monotone increasing digits if and only if each pair of adjacent digits x and y satisfy x <= y.)
-
- Example 1:
- Input: N = 10
- Output: 9
-
- Example 2:
- Input: N = 1234
- Output: 1234
-
- Example 3:
- Input: N = 332
- Output: 299
-
- Note: N is an integer in the range [0, 10^9].
- */
 public class _738 {
     public static class Solution1 {
         /**
