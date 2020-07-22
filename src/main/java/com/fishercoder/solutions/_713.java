82d88b83d33baa4efeadf7c56a34122dec440644
Fisher Coder <fishercoder@gmail.com>
2020-07-22 07:29:57 -0700

    refactor 713

--- src/main/java/com/fishercoder/solutions/_713.java
+++ src/main/java/com/fishercoder/solutions/_713.java
@@ -1,26 +1,10 @@
 package com.fishercoder.solutions;
 
-/**
- * 713. Subarray Product Less Than K
- *
- * Your are given an array of positive integers nums.
- * Count and print the number of (contiguous) subarrays where the product of all the elements in the subarray is less than k.
-
- Example 1:
- Input: nums = [10, 5, 2, 6], k = 100
- Output: 8
- Explanation: The 8 subarrays that have product less than 100 are: [10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6].
- Note that [10, 5, 2] is not included as the product of 100 is not strictly less than k.
- Note:
-
- 0 < nums.length <= 50000.
- 0 < nums[i] < 1000.
- 0 <= k < 10^6.
-
- */
 public class _713 {
     public static class Solution1 {
-        /**O(n^2) solution, accepted initially, then Leetcode added one test case to fail it.*/
+        /**
+         * O(n^2) solution, accepted initially, then Leetcode added one test case to fail it.
+         */
         public int numSubarrayProductLessThanK(int[] nums, int k) {
             int result = 0;
             for (int i = 0; i < nums.length; i++) {
