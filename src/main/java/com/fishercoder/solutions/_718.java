049a7fcde156307a1ce8dea51c1ff30460318e9a
Fisher Coder <fishercoder@gmail.com>
2020-07-22 12:41:39 -0700

    refactor 718

--- src/main/java/com/fishercoder/solutions/_718.java
+++ src/main/java/com/fishercoder/solutions/_718.java
@@ -1,22 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 718. Maximum Length of Repeated Subarray
- *
- * Given two integer arrays A and B, return the maximum length of an subarray that appears in both arrays.
-
- Example 1:
- Input:
- A: [1,2,3,2,1]
- B: [3,2,1,4,7]
- Output: 3
-
- Explanation:
- The repeated subarray with maximum length is [3, 2, 1].
- Note:
- 1 <= len(A), len(B) <= 1000
- 0 <= A[i], B[i] < 100
- */
 public class _718 {
     public static class Solution1 {
         public int findLength(int[] A, int[] B) {
