43c83a7baf133218cbecd7d9be962c783d0d8603
Fisher Coder <fishercoder@gmail.com>
2020-07-22 12:41:24 -0700

    refactor 717

--- src/main/java/com/fishercoder/solutions/_717.java
+++ src/main/java/com/fishercoder/solutions/_717.java
@@ -1,29 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 717. 1-bit and 2-bit Characters
- *
- * We have two special characters. The first character can be represented by one bit 0. The second character can be represented by two bits (10 or 11).
- * Now given a string represented by several bits. Return whether the last character must be a one-bit character or not. The given string will always end with a zero.
-
- Example 1:
- Input:
- bits = [1, 0, 0]
- Output: True
- Explanation:
- The only way to decode it is two-bit character and one-bit character. So the last character is one-bit character.
-
- Example 2:
- Input:
- bits = [1, 1, 1, 0]
- Output: False
- Explanation:
- The only way to decode it is two-bit character and two-bit character. So the last character is NOT one-bit character.
-
- Note:
- 1 <= len(bits) <= 1000.
- bits[i] is always 0 or 1.
- */
 public class _717 {
     public static class Solution1 {
         public boolean isOneBitCharacter(int[] bits) {
