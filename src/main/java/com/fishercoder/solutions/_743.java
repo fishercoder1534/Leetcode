392f4bd656c63f98aa4a171e1ee6c8cc6ccbf56d
Fisher Coder <fishercoder@gmail.com>
2020-07-22 15:52:38 -0700

    refactor 743

--- src/main/java/com/fishercoder/solutions/_743.java
+++ src/main/java/com/fishercoder/solutions/_743.java
@@ -1,21 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 743. Network Delay Time
- *
- * There are N network nodes, labelled 1 to N.
- *
- *  Given times, a list of travel times as directed edges times[i] = (u, v, w), where u is the source node, v is the target node, and w is the time it takes for a signal to travel from source to target.
- *
- * Now, we send a signal from a certain node K. How long will it take for all nodes to receive the signal? If it is impossible, return -1.
- *
- * Note:
- *  * N will be in the range [1, 100].
- *  * K will be in the range [1, N].
- *  * The length of times will be in the range [1, 6000].
- *  * All edges times[i] = (u, v, w) will have 1 <= u, v <= N and 0 <= w <= 100.
- */
-
 public class _743 {
     public static class Solution1 {
         public static final int MAX = 100_00_00;
