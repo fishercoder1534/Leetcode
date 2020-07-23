52a64b116a873f0c8fad3d48585f7ae24233db82
Fisher Coder <fishercoder@gmail.com>
2020-07-23 07:02:24 -0700

    refactor 789

--- src/main/java/com/fishercoder/solutions/_789.java
+++ src/main/java/com/fishercoder/solutions/_789.java
@@ -1,21 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 789. Escape The Ghosts
- *
- * You are playing a simplified Pacman game. You start at the point (0, 0), and your destination is
- * (target[0], target[1]). There are several ghosts on the map, the i-th ghost starts at (ghosts[i][0], ghosts[i][1]).
- *
- * Each turn, you and all ghosts simultaneously *may* move in one of 4 cardinal directions: north, east, west, or
- * south, going from the previous point to a new point 1 unit of distance away.
- *
- * You escape if and only if you can reach the target before any ghost reaches you (for any given moves the ghosts
- * may take.)  If you reach any square (including the target) at the same time as a ghost, it doesn't count as an
- * escape.
- *
- * Return True if and only if it is possible to escape.
- */
-
 public class _789 {
 
     public static class Solution {
