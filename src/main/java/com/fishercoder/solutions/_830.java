f2f79ac9f553b05f815e7750828128c3ebcbe883
Fisher Coder <fishercoder@gmail.com>
2020-07-24 06:31:27 -0700

    refactor 830

--- src/main/java/com/fishercoder/solutions/_830.java
+++ src/main/java/com/fishercoder/solutions/_830.java
@@ -4,47 +4,23 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-/**
- * 830. Positions of Large Groups
- *
- * In a string S of lowercase letters, these letters form consecutive groups of the same character.
- * For example, a string like S = "abbxxxxzyy" has the groups "a", "bb", "xxxx", "z" and "yy".
- * Call a group large if it has 3 or more characters.  We would like the starting and ending positions of every large group.
- * The final answer should be in lexicographic order.
- *
- * Example 1:
- * Input: "abbxxxxzzy"
- * Output: [[3,6]]
- * Explanation: "xxxx" is the single large group with starting  3 and ending positions 6.
- *
- * Example 2:
- * Input: "abc"
- * Output: []
- * Explanation: We have "a","b" and "c" but no large group.
- *
- * Example 3:
- * Input: "abcdddeeeeaabbbcd"
- * Output: [[3,5],[6,9],[12,14]]
- *
- * Note:  1 <= S.length <= 1000
- */
 public class _830 {
-  public static class Solution1 {
-    public List<List<Integer>> largeGroupPositions(String S) {
-      List<List<Integer>> result = new ArrayList<>();
-      char[] chars = S.toCharArray();
-      for (int i = 0; i < chars.length; ) {
-        char first = chars[i];
-        int j = i + 1;
-        while (j < chars.length && first == chars[j]) {
-          j++;
+    public static class Solution1 {
+        public List<List<Integer>> largeGroupPositions(String S) {
+            List<List<Integer>> result = new ArrayList<>();
+            char[] chars = S.toCharArray();
+            for (int i = 0; i < chars.length; ) {
+                char first = chars[i];
+                int j = i + 1;
+                while (j < chars.length && first == chars[j]) {
+                    j++;
+                }
+                if ((j - i) >= 3) {
+                    result.add(Arrays.asList(i, j - 1));
+                }
+                i = j;
+            }
+            return result;
         }
-        if ((j - i) >= 3) {
-          result.add(Arrays.asList(i, j - 1));
-        }
-        i = j;
-      }
-      return result;
     }
-  }
 }
