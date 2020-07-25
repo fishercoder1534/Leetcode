bf0695d700db457062433c3b224e3b9fd8d0e47b
Fisher Coder <fishercoder@gmail.com>
2020-07-25 06:40:13 -0700

    refactor 856

---
+++ src/main/java/com/fishercoder/solutions/Contest.java
@@ -0,0 +1,11 @@
+package com.fishercoder.solutions;
+
+public class Contest {
+
+
+    public static void main(String... args) {
+        Contest contest = new Contest();
+        System.out.println(contest);
+        System.out.println("Finished.");
+    }
+}
