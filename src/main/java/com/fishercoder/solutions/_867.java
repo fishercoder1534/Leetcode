cd97a432b6d66957387a9dfa2f114f2f445950b6
Fisher Coder <fishercoder@gmail.com>
2020-07-25 06:41:21 -0700

    refactor 867

--- src/main/java/com/fishercoder/solutions/_867.java
+++ src/main/java/com/fishercoder/solutions/_867.java
@@ -1,25 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 867. Transpose Matrix
- *
- * Given a matrix A, return the transpose of A.
- * The transpose of a matrix is the matrix flipped over it's main diagonal, switching the row and column indices of the matrix.
- *
- * Example 1:
- *
- * Input: [[1,2,3],[4,5,6],[7,8,9]]
- * Output: [[1,4,7],[2,5,8],[3,6,9]]
- * Example 2:
- *
- * Input: [[1,2,3],[4,5,6]]
- * Output: [[1,4],[2,5],[3,6]]
- *
- * Note:
- *
- * 1 <= A.length <= 1000
- * 1 <= A[0].length <= 1000
- * */
 public class _867 {
     public static class Solution1 {
         public int[][] transpose(int[][] A) {
