8fa2025f1364431fca11d98b45b6d04408a32feb
Fisher Coder <fishercoder@gmail.com>
2020-07-27 06:50:34 -0700

    refactor 876

--- src/main/java/com/fishercoder/solutions/_876.java
+++ src/main/java/com/fishercoder/solutions/_876.java
@@ -2,41 +2,16 @@ package com.fishercoder.solutions;
 
 import com.fishercoder.common.classes.ListNode;
 
-/**
- * 876. Middle of the Linked List
- *
- * Given a non-empty, singly linked list with head node head, return a middle node of linked list.
- *
- * If there are two middle nodes, return the second middle node.
- *
- * Example 1:
- *
- * Input: [1,2,3,4,5]
- * Output: Node 3 from this list (Serialization: [3,4,5])
- * The returned node has value 3.  (The judge's serialization of this node is [3,4,5]).
- * Note that we returned a ListNode object ans, such that:
- * ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, and ans.next.next.next = NULL.
- *
- *  Example 2:
- *
- * Input: [1,2,3,4,5,6]
- * Output: Node 4 from this list (Serialization: [4,5,6])
- * Since the list has two middle nodes with values 3 and 4, we return the second one.
- *
- * Note:
- *
- * The number of nodes in the given list will be between 1 and 100.
- */
 public class _876 {
-  public static class Solution1 {
-    public ListNode middleNode(ListNode head) {
-      ListNode fast = head;
-      ListNode slow = head;
-      while (fast != null && fast.next != null) {
-        fast = fast.next.next;
-        slow = slow.next;
-      }
-      return slow;
+    public static class Solution1 {
+        public ListNode middleNode(ListNode head) {
+            ListNode fast = head;
+            ListNode slow = head;
+            while (fast != null && fast.next != null) {
+                fast = fast.next.next;
+                slow = slow.next;
+            }
+            return slow;
+        }
     }
-  }
 }
