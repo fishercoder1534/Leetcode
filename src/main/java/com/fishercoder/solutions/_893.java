ca454bfe3c61332d1f9e36aca2ff7ae666ef2f20
Fisher Coder <fishercoder@gmail.com>
2020-07-29 15:32:49 -0700

    refactor 893

--- src/main/java/com/fishercoder/solutions/_893.java
+++ src/main/java/com/fishercoder/solutions/_893.java
@@ -5,42 +5,13 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-/**
- * 893. Groups of Special-Equivalent Strings
- *
- * You are given an array A of strings.
- * A move onto S consists of swapping any two even indexed characters of S, or any two odd indexed characters of S.
- * Two strings S and T are special-equivalent if after any number of moves onto S, S == T.
- * For example, S = "zzxy" and T = "xyzz" are special-equivalent because we may make the moves "zzxy" -> "xzzy" -> "xyzz"
- * that swap S[0] and S[2], then S[1] and S[3].
- *
- * Now, a group of special-equivalent strings from A is a non-empty subset of A such that:
- * Every pair of strings in the group are special equivalent, and;
- * The group is the largest size possible (ie., there isn't a string S not in the group such that S is special equivalent to every string in the group)
- * Return the number of groups of special-equivalent strings from A.
- *
- * Example 1:
- * Input: ["abcd","cdab","cbad","xyzz","zzxy","zzyx"]
- * Output: 3
- * Explanation:
- * One group is ["abcd", "cdab", "cbad"], since they are all pairwise special equivalent, and none of the other strings are all pairwise special equivalent to these.
- * The other two groups are ["xyzz", "zzxy"] and ["zzyx"].  Note that in particular, "zzxy" is not special equivalent to "zzyx".
- *
- * Example 2:
- * Input: ["abc","acb","bac","bca","cab","cba"]
- * Output: 3
- *
- * Note:
- * 1 <= A.length <= 1000
- * 1 <= A[i].length <= 20
- * All A[i] have the same length.
- * All A[i] consist of only lowercase letters.
- * */
 public class _893 {
-    
+
     public static class Solution1 {
-        /**my original solution, a bit lengthy:
-         * generate a unique signaure as key for each equivelant group and sum them up*/
+        /**
+         * my original solution, a bit lengthy:
+         * generate a unique signaure as key for each equivelant group and sum them up
+         */
         public int numSpecialEquivGroups(String[] A) {
             return Arrays.stream(A).map(this::getCommonKey).collect(Collectors.toSet()).size();
         }
