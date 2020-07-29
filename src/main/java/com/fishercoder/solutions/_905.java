d25be281bbb030c02f14bc3c9a18bf6c484c68a8
Fisher Coder <fishercoder@gmail.com>
2020-07-29 15:37:37 -0700

    refactor 905

--- src/main/java/com/fishercoder/solutions/_905.java
+++ src/main/java/com/fishercoder/solutions/_905.java
@@ -1,23 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 905. Sort Array By Parity
- *
- * Given an array A of non-negative integers, return an array consisting of all the even elements of A, followed by all the odd elements of A.
- *
- * You may return any answer array that satisfies this condition.
- *
- * Example 1:
- *
- * Input: [3,1,2,4]
- * Output: [2,4,3,1]
- * The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
- *
- * Note:
- *
- * 1 <= A.length <= 5000
- * 0 <= A[i] <= 5000
- * */
 public class _905 {
     public static class Solution1 {
         public int[] sortArrayByParity(int[] A) {
