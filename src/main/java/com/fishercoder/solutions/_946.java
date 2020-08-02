b124bc68fd97b897a5fdb947edc376c9724b026f
Fisher Coder <fishercoder@gmail.com>
2020-08-01 17:24:38 -0700

    refactor 946

--- src/main/java/com/fishercoder/solutions/_946.java
+++ src/main/java/com/fishercoder/solutions/_946.java
@@ -2,30 +2,6 @@ package com.fishercoder.solutions;
 
 import java.util.Stack;
 
-/**
- * 946. Validate Stack Sequences
- *
- * Given two sequences pushed and popped with distinct values,
- * return true if and only if this could have been the result of a sequence of push and pop operations on an initially empty stack.
- *
- * Example 1:
- * Input: pushed = [1,2,3,4,5], popped = [4,5,3,2,1]
- * Output: true
- * Explanation: We might do the following sequence:
- * push(1), push(2), push(3), push(4), pop() -> 4,
- * push(5), pop() -> 5, pop() -> 3, pop() -> 2, pop() -> 1
- *
- * Example 2:
- * Input: pushed = [1,2,3,4,5], popped = [4,3,5,1,2]
- * Output: false
- * Explanation: 1 cannot be popped before 2.
- *
- * Note:
- * 0 <= pushed.length == popped.length <= 1000
- * 0 <= pushed[i], popped[i] < 1000
- * pushed is a permutation of popped.
- * pushed and popped have distinct values.
- * */
 public class _946 {
     public static class Solution1 {
         public boolean validateStackSequences(int[] pushed, int[] popped) {
