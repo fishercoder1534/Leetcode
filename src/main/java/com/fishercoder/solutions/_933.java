24c81ba773a9f4629593fbaa9b05e6b5a562ae27
Fisher Coder <fishercoder@gmail.com>
2020-08-01 17:22:37 -0700

    refactor 933

--- src/main/java/com/fishercoder/solutions/_933.java
+++ src/main/java/com/fishercoder/solutions/_933.java
@@ -3,24 +3,6 @@ package com.fishercoder.solutions;
 import java.util.Deque;
 import java.util.LinkedList;
 
-/**
- * 933. Number of Recent Calls
- *
- * Write a class RecentCounter to count recent requests.
- * It has only one method: ping(int t), where t represents some time in milliseconds.
- * Return the number of pings that have been made from 3000 milliseconds ago until now.
- * Any ping with time in [t - 3000, t] will count, including the current ping.
- * It is guaranteed that every call to ping uses a strictly larger value of t than before.
- *
- * Example 1:
- * Input: inputs = ["RecentCounter","ping","ping","ping","ping"], inputs = [[],[1],[100],[3001],[3002]]
- * Output: [null,1,2,3,3]
- *
- * Note:
- * Each test case will have at most 10000 calls to ping.
- * Each test case will call ping with strictly increasing values of t.
- * Each call to ping will have 1 <= t <= 10^9.
- * */
 public class _933 {
     public static class Solution1 {
         public static class RecentCounter {
