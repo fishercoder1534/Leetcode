d97bac8456843d6d8417107d8533ca0a0871d395
Fisher Coder <fishercoder@gmail.com>
2020-08-03 15:52:33 -0700

    refactor 977

--- src/main/java/com/fishercoder/solutions/_977.java
+++ src/main/java/com/fishercoder/solutions/_977.java
@@ -2,34 +2,15 @@ package com.fishercoder.solutions;
 
 import java.util.Arrays;
 
-/**
- * 977. Squares of a Sorted Array
- *
- * Given an array of integers A sorted in non-decreasing order, return an array of the squares of each number, also in sorted non-decreasing order.
- *
- * Example 1:
- * Input: [-4,-1,0,3,10]
- * Output: [0,1,9,16,100]
- *
- * Example 2:
- * Input: [-7,-3,2,3,11]
- * Output: [4,9,9,49,121]
- *
- *
- * Note:
- * 1 <= A.length <= 10000
- * -10000 <= A[i] <= 10000
- * A is sorted in non-decreasing order.
- */
 public class _977 {
-  public static class Solution1 {
-    public int[] sortedSquares(int[] A) {
-      int[] result = new int[A.length];
-      for (int i = 0; i < A.length; i++) {
-        result[i] = (int) Math.pow(A[i], 2);
-      }
-      Arrays.sort(result);
-      return result;
+    public static class Solution1 {
+        public int[] sortedSquares(int[] A) {
+            int[] result = new int[A.length];
+            for (int i = 0; i < A.length; i++) {
+                result[i] = (int) Math.pow(A[i], 2);
+            }
+            Arrays.sort(result);
+            return result;
+        }
     }
-  }
 }
