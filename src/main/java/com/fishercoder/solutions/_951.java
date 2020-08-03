274126dd0a8527c37380fa3af458340f8701bbd3
Fisher Coder <fishercoder@gmail.com>
2020-08-03 06:42:51 -0700

    refactor 951

--- src/main/java/com/fishercoder/solutions/_951.java
+++ src/main/java/com/fishercoder/solutions/_951.java
@@ -2,20 +2,6 @@ package com.fishercoder.solutions;
 
 import com.fishercoder.common.classes.TreeNode;
 
-/**
- * 951. Flip Equivalent Binary Trees
- *
- * For a binary tree T, we can define a flip operation as follows: choose any node, and swap the left and right child subtrees.
- *
- * A binary tree X is flip equivalent to a binary tree Y if and only if we can make X equal to Y after some number of flip operations.
- *
- * Write a function that determines whether two binary trees are flip equivalent.  The trees are given by root nodes root1 and root2.
- *
- * Note:
- *  * Each tree will have at most 100 nodes.
- *  * Each value in each tree will be a unique integer in the range [0, 99].
- */
-
 public class _951 {
     public static class Solution1 {
         public boolean flipEquiv(TreeNode root1, TreeNode root2) {
@@ -31,8 +17,8 @@ public class _951 {
             }
 
             return (
-                (flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right))
-                || (flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left))
+                    (flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right))
+                            || (flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left))
             );
         }
     }
