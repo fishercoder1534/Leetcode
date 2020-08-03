430a3261280bc47122535ee24dce60ac12e8eced
Fisher Coder <fishercoder@gmail.com>
2020-08-03 15:54:07 -0700

    refactor 987

--- src/main/java/com/fishercoder/solutions/_987.java
+++ src/main/java/com/fishercoder/solutions/_987.java
@@ -7,55 +7,11 @@ import java.util.ArrayList;
 import java.util.PriorityQueue;
 import java.util.TreeMap;
 
-/**
- * 987. Vertical Order Traversal of a Binary Tree
- *
- * Given a binary tree, return the vertical order traversal of its nodes values.
- * For each node at position (X, Y), its left and right children respectively will be at positions (X-1, Y-1) and (X+1, Y-1).
- * Running a vertical line from X = -infinity to X = +infinity, whenever the vertical line touches some nodes, we report the values of the nodes in order from top to bottom (decreasing Y coordinates).
- * If two nodes have the same position, then the value of the node that is reported first is the value that is smaller.
- * Return an list of non-empty reports in order of X coordinate.  Every report will have a list of values of nodes.
- *
- * Example 1:
- *
- *        3
- *       / \
- *      9  20
- *        /  \
- *       15  7
- *
- * Input: [3,9,20,null,null,15,7]
- * Output: [[9],[3,15],[20],[7]]
- * Explanation:
- * Without loss of generality, we can assume the root node is at position (0, 0):
- * Then, the node with value 9 occurs at position (-1, -1);
- * The nodes with values 3 and 15 occur at positions (0, 0) and (0, -2);
- * The node with value 20 occurs at position (1, -1);
- * The node with value 7 occurs at position (2, -2).
- *
- *
- * Example 2:
- *
- *       1
- *      / \
- *     2   3
- *    / \ / \
- *   4  5 6 7
- *
- * Input: [1,2,3,4,5,6,7]
- * Output: [[4],[2],[1,5,6],[3],[7]]
- * Explanation:
- * The node with value 5 and the node with value 6 have the same position according to the given scheme.
- * However, in the report "[1,5,6]", the node value of 5 comes first since 5 is smaller than 6.
- *
- * Note:
- *
- * The tree will have between 1 and 1000 nodes.
- * Each node's value will be between 0 and 1000.
- * */
 public class _987 {
     public static class Solution1 {
-        /**credit: https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/discuss/231148/Java-TreeMap-Solution*/
+        /**
+         * credit: https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/discuss/231148/Java-TreeMap-Solution
+         */
         public List<List<Integer>> verticalTraversal(TreeNode root) {
             TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
             dfs(root, 0, 0, map);
