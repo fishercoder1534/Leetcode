eaac0cb663a0cc5db80527a10a0f67f2c235c6b7
Fisher Coder <fishercoder@gmail.com>
2020-08-03 10:06:40 -0700

    refactor 961

--- src/main/java/com/fishercoder/solutions/_961.java
+++ src/main/java/com/fishercoder/solutions/_961.java
@@ -3,33 +3,6 @@ package com.fishercoder.solutions;
 import java.util.HashSet;
 import java.util.Set;
 
-/**
- * 961. N-Repeated Element in Size 2N Array
- *
- * In a array A of size 2N, there are N+1 unique elements, and exactly one of these elements is repeated N times.
- *
- * Return the element repeated N times.
- *
- * Example 1:
- *
- * Input: [1,2,3,3]
- * Output: 3
- * Example 2:
- *
- * Input: [2,1,2,5,3,2]
- * Output: 2
- * Example 3:
- *
- * Input: [5,1,5,2,5,3,5,4]
- * Output: 5
- *
- *
- * Note:
- *
- * 4 <= A.length <= 10000
- * 0 <= A[i] < 10000
- * A.length is even
- * */
 public class _961 {
     public static class Solution1 {
         public int repeatedNTimes(int[] A) {
