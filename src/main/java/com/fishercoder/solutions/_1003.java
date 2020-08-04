a2d91bb96ec34845ee4bb9b4d9ed7b39f826638b
Fisher Coder <fishercoder@gmail.com>
2020-08-03 17:16:21 -0700

    refactor 1003

--- src/main/java/com/fishercoder/solutions/_1003.java
+++ src/main/java/com/fishercoder/solutions/_1003.java
@@ -3,22 +3,6 @@ package com.fishercoder.solutions;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
-/**
- * 1003. Check If Word Is Valid After Substitutions
- *
- * We are given that the string "abc" is valid.
- *
- * From any valid string V, we may split V into two pieces X and Y such that X + Y (X concatenated with Y) is equal to V.  (X or Y may be empty.)  Then, X + "abc" + Y is also valid.
- *
- * If for example S = "abc", then examples of valid strings are: "abc", "aabcbc", "abcabc", "abcabcababcc".  Examples of invalid strings are: "abccba", "ab", "cababc", "bac".
- *
- * Return true if and only if the given string S is valid.
- *
- * Note:
- *  * 1 <= S.length <= 20000
- *  * S[i] is 'a', 'b', or 'c'
- */
-
 public class _1003 {
     public static class Solution1 {
         public boolean isValid(String S) {
