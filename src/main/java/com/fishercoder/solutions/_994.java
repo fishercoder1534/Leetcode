6cdd9af048768686c4ae2dd352925341e612cc89
Fisher Coder <fishercoder@gmail.com>
2020-08-03 17:15:26 -0700

    refactor 994

--- src/main/java/com/fishercoder/solutions/_994.java
+++ src/main/java/com/fishercoder/solutions/_994.java
@@ -3,38 +3,6 @@ package com.fishercoder.solutions;
 import java.util.LinkedList;
 import java.util.Queue;
 
-/**
- * 994. Rotting Oranges
- *
- * In a given grid, each cell can have one of three values:
- *
- * the value 0 representing an empty cell;
- * the value 1 representing a fresh orange;
- * the value 2 representing a rotten orange.
- * Every minute, any fresh orange that is adjacent (4-directionally) to a
- * rotten orange becomes rotten.
- * Return the minimum number of minutes that must elapse until no cell has a fresh orange.
- * If this is impossible, return -1 instead.
- *
- * Example 1:
- * Input: [[2,1,1],[1,1,0],[0,1,1]]
- * Output: 4
- *
- * Example 2:
- * Input: [[2,1,1],[0,1,1],[1,0,1]]
- * Output: -1
- * Explanation:  The orange in the bottom left corner (row 2, column 0) is never rotten, because rotting only happens 4-directionally.
- *
- * Example 3:
- * Input: [[0,2]]
- * Output: 0
- * Explanation:  Since there are already no fresh oranges at minute 0, the answer is just 0.
- *
- * Note:
- * 1 <= grid.length <= 10
- * 1 <= grid[0].length <= 10
- * grid[i][j] is only 0, 1, or 2.
- */
 public class _994 {
     public static class Solution1 {
         int[] directions = new int[]{0, 1, 0, -1, 0};
