8dc04f1dd7627ca6567eaaf4bfb289cdc5bfc211
Fisher Coder <fishercoder@gmail.com>
2020-08-06 15:26:23 -0700

    refactor 1037

--- src/main/java/com/fishercoder/solutions/_1037.java
+++ src/main/java/com/fishercoder/solutions/_1037.java
@@ -1,25 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 1037. Valid Boomerang
- *
- * A boomerang is a set of 3 points that are all distinct and not in a straight line.
- *
- * Given a list of three points in the plane, return whether these points are a boomerang.
- *
- * Example 1:
- * Input: [[1,1],[2,3],[3,2]]
- * Output: true
- *
- * Example 2:
- * Input: [[1,1],[2,2],[3,3]]
- * Output: false
- *
- * Note:
- * points.length == 3
- * points[i].length == 2
- * 0 <= points[i][j] <= 100
- * */
 public class _1037 {
     public static class Solution1 {
         public boolean isBoomerang(int[][] points) {
