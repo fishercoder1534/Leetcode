ee03f243b9834aef0e69cfc447a2eda8559c5fde
Fisher Coder <fishercoder@gmail.com>
2020-08-06 15:25:03 -0700

    refactor 1021

--- src/main/java/com/fishercoder/solutions/_1021.java
+++ src/main/java/com/fishercoder/solutions/_1021.java
@@ -3,44 +3,6 @@ package com.fishercoder.solutions;
 import java.util.ArrayList;
 import java.util.List;
 
-/**1021. Remove Outermost Parentheses
- *
- * A valid parentheses string is either empty (""), "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.  For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
- *
- * A valid parentheses string S is primitive if it is nonempty, and there does not exist a way to split it into S = A+B, with A and B nonempty valid parentheses strings.
- *
- * Given a valid parentheses string S, consider its primitive decomposition: S = P_1 + P_2 + ... + P_k, where P_i are primitive valid parentheses strings.
- *
- * Return S after removing the outermost parentheses of every primitive string in the primitive decomposition of S.
- *
- *
- * Example 1:
- * Input: "(()())(())"
- * Output: "()()()"
- * Explanation:
- * The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
- * After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
- *
- * Example 2:
- * Input: "(()())(())(()(()))"
- * Output: "()()()()(())"
- * Explanation:
- * The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
- * After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
- *
- * Example 3:
- * Input: "()()"
- * Output: ""
- * Explanation:
- * The input string is "()()", with primitive decomposition "()" + "()".
- * After removing outer parentheses of each part, this is "" + "" = "".
- *
- * Note:
- *
- * S.length <= 10000
- * S[i] is "(" or ")"
- * S is a valid parentheses string
- * */
 public class _1021 {
     public static class Solution1 {
         public String removeOuterParentheses(String S) {
