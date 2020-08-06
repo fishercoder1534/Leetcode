efbcf8ab5cf53346810ad8a46c1a6ea0faec49ed
Fisher Coder <fishercoder@gmail.com>
2020-08-06 15:18:09 -0700

    refactor 1011

--- src/main/java/com/fishercoder/solutions/_1011.java
+++ src/main/java/com/fishercoder/solutions/_1011.java
@@ -1,21 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 1011. Capacity To Ship Packages Within D Days
- *
- * A conveyor belt has packages that must be shipped from one port to another within D days.
- *
- * The i-th package on the conveyor belt has a weight of weights[i].
- * Each day, we load the ship with packages on the conveyor belt (in the order given by weights).
- * We may not load more weight than the maximum weight capacity of the ship.
- *
- * Return the least weight capacity of the ship that will result in all the packages on the conveyor belt being shipped within D days.
- *
- * Note:
- *  * 1 <= D <= weights.length <= 50000
- *  * 1 <= weights[i] <= 500
- */
-
 public class _1011 {
     public static class Solution1 {
         public int daysToShip(int[] weights, int capacity) {
