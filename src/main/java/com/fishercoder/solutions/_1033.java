7847abea300200cf3a14987d31b0761a88c14b05
Fisher Coder <fishercoder@gmail.com>
2020-08-06 15:26:08 -0700

    refactor 1033

--- src/main/java/com/fishercoder/solutions/_1033.java
+++ src/main/java/com/fishercoder/solutions/_1033.java
@@ -2,28 +2,6 @@ package com.fishercoder.solutions;
 
 import java.util.Arrays;
 
-/**
- * 1033. Moving Stones Until Consecutive
- *
- * Three stones are on a number line at positions a, b, and c.
- *
- * Each turn, you pick up a stone at an endpoint (ie., either the lowest or highest position stone),
- * and move it to an unoccupied position between those endpoints.
- * Formally, let's say the stones are currently at positions x, y, z with x < y < z.
- * You pick up the stone at either position x or position z, and move that stone to an integer position k, with x < k < z and k != y.
- *
- * The game ends when you cannot make any more moves, ie. the stones are in consecutive positions.
- *
- * When the game ends, what is the minimum and maximum number of moves that you could have made?
- * Return the answer as an length 2 array: answer = [minimum_moves, maximum_moves]
- *
- * Note:
- *  * 1 <= a <= 100
- *  *  1 <= b <= 100
- *  *  1 <= c <= 100
- *  *  a != b, b != c, c != a
- */
-
 public class _1033 {
     public static class Solution1 {
         private int minMoves(int x, int y, int z) {
