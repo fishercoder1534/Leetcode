fa7b64c82a3c8fa86c794b7f3667d3c9df4c4f1a
Fisher Coder <fishercoder@gmail.com>
2020-08-06 15:19:24 -0700

    refactor 1014

--- src/main/java/com/fishercoder/solutions/_1014.java
+++ src/main/java/com/fishercoder/solutions/_1014.java
@@ -1,21 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 1014. Best Sightseeing Pair
- *
- * Given an array A of positive integers, A[i] represents the value of the i-th sightseeing spot,
- * and two sightseeing spots i and j have distance j - i between them.
- *
- * The score of a pair (i < j) of sightseeing spots is (A[i] + A[j] + i - j) :
- * the sum of the values of the sightseeing spots, minus the distance between them.
- *
- * Return the maximum score of a pair of sightseeing spots.
- *
- * Note:
- *  * 2 <= A.length <= 50000
- *  * 1 <= A[i] <= 1000
- */
-
 public class _1014 {
     public static class Solution1 {
         public int maxScoreSightseeingPair(int[] A) {
