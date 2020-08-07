428a90349f90ff40a9793eb6ff177eea7be2eaa1
Fisher Coder <fishercoder@gmail.com>
2020-08-07 08:21:35 -0700

    refactor 1087

--- src/main/java/com/fishercoder/solutions/_1087.java
+++ src/main/java/com/fishercoder/solutions/_1087.java
@@ -4,30 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-/**
- * 1087. Brace Expansion
- *
- * A string S represents a list of words.
- * Each letter in the word has 1 or more options.  If there is one option, the letter is represented as is.
- * If there is more than one option, then curly braces delimit the options.
- * For example, "{a,b,c}" represents options ["a", "b", "c"].
- * For example, "{a,b,c}d{e,f}" represents the list ["ade", "adf", "bde", "bdf", "cde", "cdf"].
- *
- * Return all words that can be formed in this manner, in lexicographical order.
- *
- * Example 1:
- * Input: "{a,b}c{d,e}f"
- * Output: ["acdf","acef","bcdf","bcef"]
- *
- * Example 2:
- * Input: "abcd"
- * Output: ["abcd"]
- *
- * Note:
- * 1 <= S.length <= 50
- * There are no nested curly brackets.
- * All characters inside a pair of consecutive opening and ending curly brackets are different.
- * */
 public class _1087 {
     public static class Solution1 {
         public String[] expand(String S) {
