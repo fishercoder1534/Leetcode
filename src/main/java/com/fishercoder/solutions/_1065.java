c13465cec30eaa3dd2cccb644a2794611ebcfb22
Fisher Coder <fishercoder@gmail.com>
2020-08-07 08:17:13 -0700

    refactor 1065

--- src/main/java/com/fishercoder/solutions/_1065.java
+++ src/main/java/com/fishercoder/solutions/_1065.java
@@ -5,31 +5,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-/**
- * 1065. Index Pairs of a String
- *
- * Given a text string and words (a list of strings),
- * return all index pairs [i, j] so that the substring text[i]...text[j] is in the list of words.
- *
- * Example 1:
- * Input: text = "thestoryofleetcodeandme", words = ["story","fleet","leetcode"]
- * Output: [[3,7],[9,13],[10,17]]
- *
- * Example 2:
- * Input: text = "ababa", words = ["aba","ab"]
- * Output: [[0,1],[0,2],[2,3],[2,4]]
- * Explanation:
- * Notice that matches can overlap, see "aba" is found in [0,2] and [2,4].
- *
- * Note:
- *
- * All strings contains only lowercase English letters.
- * It's guaranteed that all strings in words are different.
- * 1 <= text.length <= 100
- * 1 <= words.length <= 20
- * 1 <= words[i].length <= 50
- * Return the pairs [i,j] in sorted order (i.e. sort them by their first coordinate in case of ties sort them by their second coordinate).
- * */
 public class _1065 {
     public static class Solution1 {
         public int[][] indexPairs(String text, String[] words) {
