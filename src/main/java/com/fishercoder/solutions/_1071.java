b3a412210da0098f8eae292622e03e6e37ec0448
Fisher Coder <fishercoder@gmail.com>
2020-08-07 08:17:45 -0700

    refactor 1071

--- src/main/java/com/fishercoder/solutions/_1071.java
+++ src/main/java/com/fishercoder/solutions/_1071.java
@@ -1,29 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 1071. Greatest Common Divisor of Strings
- *
- * For strings S and T, we say "T divides S" if and only if S = T + ... + T  (T concatenated with itself 1 or more times)
- * Return the largest string X such that X divides str1 and X divides str2.
- *
- * Example 1:
- * Input: str1 = "ABCABC", str2 = "ABC"
- * Output: "ABC"
- *
- * Example 2:
- * Input: str1 = "ABABAB", str2 = "ABAB"
- * Output: "AB"
- *
- * Example 3:
- * Input: str1 = "LEET", str2 = "CODE"
- * Output: ""
- *
- * Note:
- *
- * 1 <= str1.length <= 1000
- * 1 <= str2.length <= 1000
- * str1[i] and str2[i] are English uppercase letters.
- * */
 public class _1071 {
     public static class Solution1 {
         public String gcdOfStrings(String str1, String str2) {
