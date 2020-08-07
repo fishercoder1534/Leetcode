80f94ad3d65fd385724adc8da55012f4ae93c3d0
Fisher Coder <fishercoder@gmail.com>
2020-08-07 07:14:54 -0700

    refactor 1047

--- src/main/java/com/fishercoder/solutions/_1047.java
+++ src/main/java/com/fishercoder/solutions/_1047.java
@@ -1,29 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 1047. Remove All Adjacent Duplicates In String
- *
- * Given a string S of lowercase letters, a duplicate removal consists of choosing two adjacent and equal letters, and removing them.
- *
- * We repeatedly make duplicate removals on S until we no longer can.
- *
- * Return the final string after all such duplicate removals have been made.  It is guaranteed the answer is unique.
- *
- *
- *
- * Example 1:
- *
- * Input: "abbaca"
- * Output: "ca"
- * Explanation:
- * For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.
- * The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca".
- *
- *
- * Note:
- *
- * 1 <= S.length <= 20000
- * S consists only of English lowercase letters.*/
 public class _1047 {
     public static class Solution1 {
         public String removeDuplicates(String S) {
