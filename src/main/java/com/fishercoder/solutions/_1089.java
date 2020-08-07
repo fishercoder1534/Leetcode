52cdd5be235064a75c657c2cb126e9034156316d
Fisher Coder <fishercoder@gmail.com>
2020-08-07 08:21:51 -0700

    refactor 1089

--- src/main/java/com/fishercoder/solutions/_1089.java
+++ src/main/java/com/fishercoder/solutions/_1089.java
@@ -1,29 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 1089. Duplicate Zeros
- *
- * Given a fixed length array arr of integers, duplicate each occurrence of zero, shifting the remaining elements to the right.
- *
- * Note that elements beyond the length of the original array are not written.
- *
- * Do the above modifications to the input array in place, do not return anything from your function.
- *
- * Example 1:
- * Input: [1,0,2,3,0,4,5,0]
- * Output: null
- * Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
- *
- * Example 2:
- * Input: [1,2,3]
- * Output: null
- * Explanation: After calling your function, the input array is modified to: [1,2,3]
- *
- * Note:
- *
- * 1 <= arr.length <= 10000
- * 0 <= arr[i] <= 9
- * */
 public class _1089 {
     public static class Solution1 {
         public void duplicateZeros(int[] arr) {
