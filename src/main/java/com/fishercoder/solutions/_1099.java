8dbd447bd5d63ca6eff733f49c53584a697f919a
Fisher Coder <fishercoder@gmail.com>
2020-08-07 08:24:17 -0700

    refactor 1099

--- src/main/java/com/fishercoder/solutions/_1099.java
+++ src/main/java/com/fishercoder/solutions/_1099.java
@@ -2,36 +2,12 @@ package com.fishercoder.solutions;
 
 import java.util.Arrays;
 
-/**
- * 1099. Two Sum Less Than K
- *
- * Given an array A of integers and integer K,
- * return the maximum S such that there exists i < j with A[i] + A[j] = S and S < K.
- * If no i, j exist satisfying this equation, return -1.
- *
- * Example 1:
- * Input: A = [34,23,1,24,75,33,54,8], K = 60
- * Output: 58
- * Explanation:
- * We can use 34 and 24 to sum 58 which is less than 60.
- *
- * Example 2:
- * Input: A = [10,20,30], K = 15
- * Output: -1
- * Explanation:
- * In this case it's not possible to get a pair sum less that 15.
- *
- * Note:
- * 1 <= A.length <= 100
- * 1 <= A[i] <= 1000
- * 1 <= K <= 2000
- * */
 public class _1099 {
     public static class Solution1 {
         /**
          * Time: O(n^2)
          * Space: O(1)
-         * */
+         */
         public int twoSumLessThanK(int[] A, int K) {
             int maxSum = Integer.MIN_VALUE;
             for (int i = 0; i < A.length - 1; i++) {
@@ -49,7 +25,7 @@ public class _1099 {
         /**
          * Time: O(nlogn)
          * Space: O(1)
-         * */
+         */
         public int twoSumLessThanK(int[] A, int K) {
             Arrays.sort(A);
             int left = 0;
