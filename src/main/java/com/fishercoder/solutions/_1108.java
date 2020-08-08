4e16197e6a1895325e9255ea4e1e79c11bbfcb65
Fisher Coder <fishercoder@gmail.com>
2020-08-08 06:09:07 -0700

    refactor 1108

--- src/main/java/com/fishercoder/solutions/_1108.java
+++ src/main/java/com/fishercoder/solutions/_1108.java
@@ -1,22 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 1108. Defanging an IP Address
- *
- * Given a valid (IPv4) IP address, return a defanged version of that IP address.
- * A defanged IP address replaces every period "." with "[.]".
- *
- * Example 1:
- * Input: address = "1.1.1.1"
- * Output: "1[.]1[.]1[.]1"
- *
- * Example 2:
- * Input: address = "255.100.50.0"
- * Output: "255[.]100[.]50[.]0"
- *
- * Constraints:
- * The given address is a valid IPv4 address.
- * */
 public class _1108 {
     public static class Solution1 {
         public String defangIPaddr(String address) {
