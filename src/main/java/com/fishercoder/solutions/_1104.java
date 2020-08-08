4d10895e38493006bdccfed42bee8744c2488cc5
Fisher Coder <fishercoder@gmail.com>
2020-08-08 05:58:31 -0700

    refactor 1104

--- src/main/java/com/fishercoder/solutions/_1104.java
+++ src/main/java/com/fishercoder/solutions/_1104.java
@@ -12,29 +12,11 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-/**
- * 1104. Path In Zigzag Labelled Binary Tree
- *
- * In an infinite binary tree where every node has two children, the nodes are labelled in row order.
- * In the odd numbered rows (ie., the first, third, fifth,...), the labelling is left to right,
- * while in the even numbered rows (second, fourth, sixth,...), the labelling is right to left.
- *
- * Given the label of a node in this tree, return the labels in the path from the root of the tree to the node with that label.
- *
- * Example 1:
- * Input: label = 14
- * Output: [1,3,4,14]
- *
- * Example 2:
- * Input: label = 26
- * Output: [1,2,6,10,26]
- *
- * Constraints:
- * 1 <= label <= 10^6
- * */
 public class _1104 {
     public static class Solution1 {
-        /**This brute force solution is correct but results in TLE on LeetCode.*/
+        /**
+         * This brute force solution is correct but results in TLE on LeetCode.
+         */
         public List<Integer> pathInZigZagTree(int label) {
             Deque<Integer> deque = buildZigZagOrderList(label);
             CommonUtils.printDeque(deque);
@@ -101,7 +83,9 @@ public class _1104 {
     }
 
     public static class Solution2 {
-        /**We'll directly compute the index of its parent, it'll be much faster this way.*/
+        /**
+         * We'll directly compute the index of its parent, it'll be much faster this way.
+         */
         public List<Integer> pathInZigZagTree(int label) {
             List<List<Integer>> lists = buildZigZagOrderList(label);
             List<Integer> result = new ArrayList<>();
