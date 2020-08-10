8d22167bfc29644e00110a7a4a446a88ec2caf7b
Fisher Coder <fishercoder@gmail.com>
2020-08-10 13:48:27 -0700

    update tests for 332

--- src/test/java/com/fishercoder/_332Test.java
+++ src/test/java/com/fishercoder/_332Test.java
@@ -5,30 +5,40 @@ import com.fishercoder.solutions._332;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class _332Test {
-  private static _332.Solution1 solution1;
-  private static String[][] tickets;
-  private static List<String> expected;
+    private static _332.Solution1 solution1;
+    private static List<List<String>> tickets;
+    private static List<String> expected;
 
-  @BeforeClass
-  public static void setup() {
-    solution1 = new _332.Solution1();
-  }
+    @BeforeClass
+    public static void setup() {
+        solution1 = new _332.Solution1();
+    }
 
-  @Test
-  public void test1() {
-    tickets = new String[][] {{"MUC", "LHR"}, {"JFK", "MUC"}, {"SFO", "SJC"}, {"LHR", "SFO"}};
-    expected = solution1.findItinerary(tickets);
-    CommonUtils.print(expected);
-  }
+    @Test
+    public void test1() {
+        tickets = new ArrayList<>();
+        tickets.add(Arrays.asList("MUC", "LHR"));
+        tickets.add(Arrays.asList("JFK", "MUC"));
+        tickets.add(Arrays.asList("SFO", "SJC"));
+        tickets.add(Arrays.asList("LHR", "SFO"));
+        expected = solution1.findItinerary(tickets);
+        CommonUtils.print(expected);
+    }
 
-  @Test
-  public void test2() {
-    tickets = new String[][] {{"JFK", "SFO"}, {"JFK", "ATL"}, {"SFO", "ATL"}, {"ATL", "JFK"},
-        {"ATL", "SFO"}};
-    expected = solution1.findItinerary(tickets);
-    CommonUtils.print(expected);
-  }
+    @Test
+    public void test2() {
+        tickets = new ArrayList<>();
+        tickets.add(Arrays.asList("JFK", "SFO"));
+        tickets.add(Arrays.asList("JFK", "ATL"));
+        tickets.add(Arrays.asList("SFO", "ATL"));
+        tickets.add(Arrays.asList("ATL", "JFK"));
+        tickets.add(Arrays.asList("ATL", "SFO"));
+        expected = solution1.findItinerary(tickets);
+        CommonUtils.print(expected);
+    }
 }
