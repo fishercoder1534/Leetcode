6bb4a123fc18742228698bf692a0377fd6605b56
Fisher Coder <fishercoder@gmail.com>
2020-08-11 06:11:27 -0700

    refactor 1118

--- src/main/java/com/fishercoder/solutions/_1118.java
+++ src/main/java/com/fishercoder/solutions/_1118.java
@@ -1,29 +1,5 @@
 package com.fishercoder.solutions;
 
-import java.util.HashMap;
-import java.util.Map;
-
-/**
- * 1118. Number of Days in a Month
- *
- * Given a year Y and a month M, return how many days there are in that month.
- *
- * Example 1:
- * Input: Y = 1992, M = 7
- * Output: 31
- *
- * Example 2:
- * Input: Y = 2000, M = 2
- * Output: 29
- *
- * Example 3:
- * Input: Y = 1900, M = 2
- * Output: 28
- *
- * Note:
- * 1583 <= Y <= 2100
- * 1 <= M <= 12
- * */
 public class _1118 {
     public static class Solution1 {
         public int numberOfDays(int Y, int M) {
