477317dfd7f72f9a56d7a86d44cb1aa3433ee99a
Fisher Coder <fishercoder@gmail.com>
2020-08-11 10:15:44 -0700

    refactor 1128

--- src/main/java/com/fishercoder/solutions/_1128.java
+++ src/main/java/com/fishercoder/solutions/_1128.java
@@ -3,21 +3,6 @@ package com.fishercoder.solutions;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * 1128. Number of Equivalent Domino Pairs
- *
- * Given a list of dominoes, dominoes[i] = [a, b] is equivalent to dominoes[j] = [c, d] if and only if either (a==c and b==d), or (a==d and b==c) - that is, one domino can be rotated to be equal to another domino.
- * Return the number of pairs (i, j) for which 0 <= i < j < dominoes.length, and dominoes[i] is equivalent to dominoes[j].
- *
- * Example 1:
- *
- * Input: dominoes = [[1,2],[2,1],[3,4],[5,6]]
- * Output: 1
- *
- * Constraints:
- * 1 <= dominoes.length <= 40000
- * 1 <= dominoes[i][j] <= 9
- * */
 public class _1128 {
     public static class Solution1 {
         public int numEquivDominoPairs(int[][] dominoes) {
