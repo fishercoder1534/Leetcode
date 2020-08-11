2ca2289a21e01190456b3ee4b437f8d2277f4d91
Fisher Coder <fishercoder@gmail.com>
2020-08-11 08:32:54 -0700

    refactor 1119

--- src/main/java/com/fishercoder/solutions/_1119.java
+++ src/main/java/com/fishercoder/solutions/_1119.java
@@ -4,23 +4,6 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-/**
- * 1119. Remove Vowels from a String
- *
- * Given a string S, remove the vowels 'a', 'e', 'i', 'o', and 'u' from it, and return the new string.
- *
- * Example 1:
- * Input: "leetcodeisacommunityforcoders"
- * Output: "ltcdscmmntyfrcdrs"
- *
- * Example 2:
- * Input: "aeiou"
- * Output: ""
- *
- * Note:
- * S consists of lowercase English letters only.
- * 1 <= S.length <= 1000
- * */
 public class _1119 {
     public static class Solution1 {
         public String removeVowels(String S) {
