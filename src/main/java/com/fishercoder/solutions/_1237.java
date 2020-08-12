adac78a44809f8d2c4c802d91be628ccf44b010e
Fisher Coder <fishercoder@gmail.com>
2020-08-12 12:02:36 -0700

    refactor 1237

--- src/main/java/com/fishercoder/solutions/_1237.java
+++ src/main/java/com/fishercoder/solutions/_1237.java
@@ -4,41 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-/**
- * 1237. Find Positive Integer Solution for a Given Equation
- *
- * Given a function  f(x, y) and a value z, return all positive integer pairs x and y where f(x,y) == z.
- * The function is constantly increasing, i.e.:
- *
- * f(x, y) < f(x + 1, y)
- * f(x, y) < f(x, y + 1)
- * The function interface is defined like this:
- *
- * interface CustomFunction {
- * public:
- *   // Returns positive integer f(x, y) for any given positive integer x and y.
- *   int f(int x, int y);
- * };
- * For custom testing purposes you're given an integer function_id and a target z as input,
- * where function_id represent one function from an secret internal list, on the examples you'll know only two functions from the list.
- * You may return the solutions in any order.
- *
- * Example 1:
- * Input: function_id = 1, z = 5
- * Output: [[1,4],[2,3],[3,2],[4,1]]
- * Explanation: function_id = 1 means that f(x, y) = x + y
- *
- * Example 2:
- * Input: function_id = 2, z = 5
- * Output: [[1,5],[5,1]]
- * Explanation: function_id = 2 means that f(x, y) = x * y
- *
- * Constraints:
- * 1 <= function_id <= 9
- * 1 <= z <= 100
- * It's guaranteed that the solutions of f(x, y) == z will be on the range 1 <= x, y <= 1000
- * It's also guaranteed that f(x, y) will fit in 32 bit signed integer if 1 <= x, y <= 1000
- */
 public class _1237 {
 
     // This is the custom function interface.
@@ -51,8 +16,10 @@ public class _1237 {
     }
 
     public static class Solution1 {
-        /**Time: O(x*y)
-         * Space: O(1)*/
+        /**
+         * Time: O(x*y)
+         * Space: O(1)
+         */
         public List<List<Integer>> findSolution(CustomFunction customfunction, int z) {
             List<List<Integer>> result = new ArrayList<>();
             for (int x = 1; x <= 1000; x++) {
@@ -69,7 +36,7 @@ public class _1237 {
     public static class Solution2 {
         /**
          * linear search
-         * 
+         * <p>
          * Time: O(x + y)
          * Space: O(1)
          */
@@ -94,10 +61,10 @@ public class _1237 {
     public static class Solution3 {
         /**
          * binary search
-         *
+         * <p>
          * Time: O(xlogy)
          * Space: O(1)
-         * */
+         */
         public List<List<Integer>> findSolution(CustomFunction customfunction, int z) {
             List<List<Integer>> result = new ArrayList<>();
             for (int x = 1; x <= 1000; x++) {
