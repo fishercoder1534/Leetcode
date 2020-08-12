edad6e51610d3d2ab120aebfee2d08e063b967bb
Fisher Coder <fishercoder@gmail.com>
2020-08-12 11:59:35 -0700

    refactor 1214

--- src/main/java/com/fishercoder/solutions/_1214.java
+++ src/main/java/com/fishercoder/solutions/_1214.java
@@ -5,35 +5,6 @@ import com.fishercoder.common.classes.TreeNode;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 1214. Two Sum BSTs
- *
- * Given two binary search trees,
- * return True if and only if there is a node in the first tree and a node in the second tree whose values sum up to a given integer target.
- *
- * Example 1:
- *        2             1
- *       / \           / \
- *      1  4          0  3
- *
- * Input: root1 = [2,1,4], root2 = [1,0,3], target = 5
- * Output: true
- * Explanation: 2 and 3 sum up to 5.
- *
- * Example 2:
- *         0                   5
- *        / \                 / \
- *      -10  10              1   7
- *                          / \
- *                         0  2
- *
- * Input: root1 = [0,-10,10], root2 = [5,1,7,0,2], target = 18
- * Output: false
- *
- * Constraints:
- * Each tree has at most 5000 nodes.
- * -10^9 <= target, node.val <= 10^9
- * */
 public class _1214 {
     public static class Solution1 {
         public boolean twoSumBSTs(TreeNode root1, TreeNode root2, int target) {
