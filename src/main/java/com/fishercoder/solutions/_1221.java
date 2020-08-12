3df1a8222fa8a2ed90c647a21b6b38c42f49a296
Fisher Coder <fishercoder@gmail.com>
2020-08-12 12:00:16 -0700

    refactor 1221

--- src/main/java/com/fishercoder/solutions/_1221.java
+++ src/main/java/com/fishercoder/solutions/_1221.java
@@ -1,36 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 1221. Split a String in Balanced Strings
- *
- * Balanced strings are those who have equal quantity of 'L' and 'R' characters.
- * Given a balanced string s split it in the maximum amount of balanced strings.
- * Return the maximum amount of splitted balanced strings.
- *
- * Example 1:
- * Input: s = "RLRRLLRLRL"
- * Output: 4
- * Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
- *
- * Example 2:
- * Input: s = "RLLLLRRRLR"
- * Output: 3
- * Explanation: s can be split into "RL", "LLLRRR", "LR", each substring contains same number of 'L' and 'R'.
- *
- * Example 3:
- * Input: s = "LLLLRRRR"
- * Output: 1
- * Explanation: s can be split into "LLLLRRRR".
- *
- * Example 4:
- * Input: s = "RLRRRLLRLL"
- * Output: 2
- * Explanation: s can be split into "RL", "RRRLLRLL", since each substring contains an equal number of 'L' and 'R'
- *
- * Constraints:
- * 1 <= s.length <= 1000
- * s[i] = 'L' or 'R'
- * */
 public class _1221 {
     public static class Solution1 {
         public int balancedStringSplit(String s) {
