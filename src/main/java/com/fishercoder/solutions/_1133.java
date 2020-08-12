40ad1e94887cd45d9e13eb08007dc2633f823365
Fisher Coder <fishercoder@gmail.com>
2020-08-12 06:18:24 -0700

    refactor 1133

--- src/main/java/com/fishercoder/solutions/_1133.java
+++ src/main/java/com/fishercoder/solutions/_1133.java
@@ -2,28 +2,6 @@ package com.fishercoder.solutions;
 
 import java.util.TreeMap;
 
-/**
- * 1133. Largest Unique Number
- *
- * Given an array of integers A, return the largest integer that only occurs once.
- * If no integer occurs once, return -1.
- *
- * Example 1:
- * Input: [5,7,3,9,4,9,8,3,1]
- * Output: 8
- * Explanation:
- * The maximum integer in the array is 9 but it is repeated. The number 8 occurs only once, so it's the answer.
- *
- * Example 2:
- * Input: [9,9,8,8]
- * Output: -1
- * Explanation:
- * There is no number that occurs only once.
- *
- * Note:
- * 1 <= A.length <= 2000
- * 0 <= A[i] <= 1000
- * */
 public class _1133 {
     public static class Solution1 {
         public int largestUniqueNumber(int[] A) {
