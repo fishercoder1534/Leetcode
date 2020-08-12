6b958be64aa4ae8c300aa36433227bdf91c889a4
Fisher Coder <fishercoder@gmail.com>
2020-08-12 12:00:02 -0700

    refactor 1219

--- src/main/java/com/fishercoder/solutions/_1219.java
+++ src/main/java/com/fishercoder/solutions/_1219.java
@@ -3,42 +3,6 @@ package com.fishercoder.solutions;
 import java.util.LinkedList;
 import java.util.Queue;
 
-/**
- * 1219. Path with Maximum Gold
- *
- * In a gold mine grid of size m * n, each cell in this mine has an integer representing the amount of gold in that cell, 0 if it is empty.
- * Return the maximum amount of gold you can collect under the conditions:
- * Every time you are located in a cell you will collect all the gold in that cell.
- * From your position you can walk one step to the left, right, up or down.
- * You can't visit the same cell more than once.
- * Never visit a cell with 0 gold.
- * You can start and stop collecting gold from any position in the grid that has some gold.
- *
- * Example 1:
- * Input: grid = [[0,6,0],[5,8,7],[0,9,0]]
- * Output: 24
- * Explanation:
- * [[0,6,0],
- *  [5,8,7],
- *  [0,9,0]]
- * Path to get the maximum gold, 9 -> 8 -> 7.
- *
- * Example 2:
- * Input: grid = [[1,0,7],[2,0,6],[3,4,5],[0,3,0],[9,0,20]]
- * Output: 28
- * Explanation:
- * [[1,0,7],
- *  [2,0,6],
- *  [3,4,5],
- *  [0,3,0],
- *  [9,0,20]]
- * Path to get the maximum gold, 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7.
- *
- * Constraints:
- * 1 <= grid.length, grid[i].length <= 15
- * 0 <= grid[i][j] <= 100
- * There are at most 25 cells containing gold.
- * */
 public class _1219 {
     public static class Solution1 {
         public int getMaximumGold(int[][] grid) {
