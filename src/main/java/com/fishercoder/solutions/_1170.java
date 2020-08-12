835a663bf502d886482d63b7d82fd4b0ef59e047
Fisher Coder <fishercoder@gmail.com>
2020-08-12 07:29:36 -0700

    refactor 1170

--- src/main/java/com/fishercoder/solutions/_1170.java
+++ src/main/java/com/fishercoder/solutions/_1170.java
@@ -2,39 +2,13 @@ package com.fishercoder.solutions;
 
 import java.util.Arrays;
 
-/**
- * 1170. Compare Strings by Frequency of the Smallest Character
- *
- * Let's define a function f(s) over a non-empty string s,
- * which calculates the frequency of the smallest character in s.
- * For example, if s = "dcce" then f(s) = 2 because the smallest character is "c" and its frequency is 2.
- * Now, given string arrays queries and words,
- * return an integer array answer,
- * where each answer[i] is the number of words such that f(queries[i]) < f(W), where W is a word in words.
- *
- * Example 1:
- * Input: queries = ["cbd"], words = ["zaaaz"]
- * Output: [1]
- * Explanation: On the first query we have f("cbd") = 1, f("zaaaz") = 3 so f("cbd") < f("zaaaz").
- *
- * Example 2:
- * Input: queries = ["bbb","cc"], words = ["a","aa","aaa","aaaa"]
- * Output: [1,2]
- * Explanation: On the first query only f("bbb") < f("aaaa"). On the second query both f("aaa") and f("aaaa") are both > f("cc").
- *
- * Constraints:
- * 1 <= queries.length <= 2000
- * 1 <= words.length <= 2000
- * 1 <= queries[i].length, words[i].length <= 10
- * queries[i][j], words[i][j] are English lowercase letters.
- * */
 public class _1170 {
     public static class Solution1 {
         /**
          * Use simple iteration when finding counts
          * Time: O(n^m) where m is the size of queries and n is the size of words
          * Space: O(max(m, n) where m is the size of queries and n is the size of words)
-         * */
+         */
         public int[] numSmallerByFrequency(String[] queries, String[] words) {
             int[] queriesMinFrequecies = new int[queries.length];
             for (int i = 0; i < queries.length; i++) {
@@ -87,7 +61,7 @@ public class _1170 {
          * Use binary search when finding counts
          * Time: O(n^logn) where m is the size of queries and n is the size of words
          * Space: O(max(m, n) where m is the size of queries and n is the size of words)
-         * */
+         */
         public int[] numSmallerByFrequency(String[] queries, String[] words) {
             int[] queriesMinFrequecies = new int[queries.length];
             for (int i = 0; i < queries.length; i++) {
