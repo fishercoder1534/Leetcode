d581569c4749729f80aa06814b55f71eaf2f7d8f
Fisher Coder <fishercoder@gmail.com>
2020-08-12 11:57:38 -0700

    refactor 1190

--- src/main/java/com/fishercoder/solutions/_1190.java
+++ src/main/java/com/fishercoder/solutions/_1190.java
@@ -4,36 +4,6 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 
-/**
- * 1190. Reverse Substrings Between Each Pair of Parentheses
- *
- * You are given a string s that consists of lower case English letters and brackets.
- * Reverse the strings in each pair of matching parentheses, starting from the innermost one.
- * Your result should not contain any brackets.
- *
- * Example 1:
- * Input: s = "(abcd)"
- * Output: "dcba"
- *
- * Example 2:
- * Input: s = "(u(love)i)"
- * Output: "iloveu"
- * Explanation: The substring "love" is reversed first, then the whole string is reversed.
- *
- * Example 3:
- * Input: s = "(ed(et(oc))el)"
- * Output: "leetcode"
- * Explanation: First, we reverse the substring "oc", then "etco", and finally, the whole string.
- *
- * Example 4:
- * Input: s = "a(bcdefghijkl(mno)p)q"
- * Output: "apmnolkjihgfedcbq"
- *
- * Constraints:
- * 0 <= s.length <= 2000
- * s only contains lower case English characters and parentheses.
- * It's guaranteed that all parentheses are balanced.
- * */
 public class _1190 {
     public static class Solution1 {
         public String reverseParentheses(String s) {
