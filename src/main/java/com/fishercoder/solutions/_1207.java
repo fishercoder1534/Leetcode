74579b5a498cdd90d57665d216cdc37e2258d9cf
Fisher Coder <fishercoder@gmail.com>
2020-08-12 11:58:50 -0700

    refactor 1207

--- src/main/java/com/fishercoder/solutions/_1207.java
+++ src/main/java/com/fishercoder/solutions/_1207.java
@@ -6,29 +6,6 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-/**
- * 1207. Unique Number of Occurrences
- *
- * Given an array of integers arr,
- * write a function that returns true if and only if the number of occurrences of each value in the array is unique.
- *
- * Example 1:
- * Input: arr = [1,2,2,1,1,3]
- * Output: true
- * Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
- *
- * Example 2:
- * Input: arr = [1,2]
- * Output: false
- *
- * Example 3:
- * Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
- * Output: true
- *
- * Constraints:
- * 1 <= arr.length <= 1000
- * -1000 <= arr[i] <= 1000
- * */
 public class _1207 {
     public static class Solution1 {
         public boolean uniqueOccurrences(int[] arr) {
