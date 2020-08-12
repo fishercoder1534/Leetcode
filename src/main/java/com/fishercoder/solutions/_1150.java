f65cb47782383d41846cb52074075045deda1176
Fisher Coder <fishercoder@gmail.com>
2020-08-12 07:27:40 -0700

    refactor 1150

--- src/main/java/com/fishercoder/solutions/_1150.java
+++ src/main/java/com/fishercoder/solutions/_1150.java
@@ -1,33 +1,10 @@
 package com.fishercoder.solutions;
 
-/**
- * 1150. Check If a Number Is Majority Element in a Sorted Array
- *
- * Given an array nums sorted in non-decreasing order, and a number target, return True if and only if target is a majority element.
- * A majority element is an element that appears more than N/2 times in an array of length N.
- *
- * Example 1:
- * Input: nums = [2,4,5,5,5,5,5,6,6], target = 5
- * Output: true
- * Explanation:
- * The value 5 appears 5 times and the length of the array is 9.
- * Thus, 5 is a majority element because 5 > 9/2 is true.
- *
- * Example 2:
- * Input: nums = [10,100,101,101], target = 101
- * Output: false
- * Explanation:
- * The value 101 appears 2 times and the length of the array is 4.
- * Thus, 101 is not a majority element because 2 > 4/2 is false.
- *
- * Note:
- * 1 <= nums.length <= 1000
- * 1 <= nums[i] <= 10^9
- * 1 <= target <= 10^9
- **/
 public class _1150 {
     public static class Solution1 {
-        /**credit: https://leetcode.com/problems/check-if-a-number-is-majority-element-in-a-sorted-array/discuss/358130/Java-just-one-binary-search-O(logN))-0ms-beats-100*/
+        /**
+         * credit: https://leetcode.com/problems/check-if-a-number-is-majority-element-in-a-sorted-array/discuss/358130/Java-just-one-binary-search-O(logN))-0ms-beats-100
+         */
         public boolean isMajorityElement(int[] nums, int target) {
             int firstIndex = findFirstOccur(nums, target);
             int plusHalfIndex = firstIndex + nums.length / 2;
