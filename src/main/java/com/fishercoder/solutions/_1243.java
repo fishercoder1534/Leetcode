619fc55727d65631bf55787f1e7cbe2bd9dc0964
Fisher Coder <fishercoder@gmail.com>
2020-08-12 12:02:52 -0700

    refactor 1243

--- src/main/java/com/fishercoder/solutions/_1243.java
+++ src/main/java/com/fishercoder/solutions/_1243.java
@@ -4,37 +4,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-/**
- * 1243. Array Transformation
- *
- * Given an initial array arr, every day you produce a new array using the array of the previous day.
- *
- * On the i-th day, you do the following operations on the array of day i-1 to produce the array of day i:
- *
- * If an element is smaller than both its left neighbor and its right neighbor, then this element is incremented.
- * If an element is bigger than both its left neighbor and its right neighbor, then this element is decremented.
- * The first and last elements never change.
- * After some days, the array does not change. Return that final array.
- *
- * Example 1:
- * Input: arr = [6,2,3,4]
- * Output: [6,3,3,4]
- * Explanation:
- * On the first day, the array is changed from [6,2,3,4] to [6,3,3,4].
- * No more operations can be done to this array.
- *
- * Example 2:
- * Input: arr = [1,6,3,4,3,5]
- * Output: [1,4,4,4,4,5]
- * Explanation:
- * On the first day, the array is changed from [1,6,3,4,3,5] to [1,5,4,3,4,5].
- * On the second day, the array is changed from [1,5,4,3,4,5] to [1,4,4,4,4,5].
- * No more operations can be done to this array.
- *
- * Constraints:
- * 1 <= arr.length <= 100
- * 1 <= arr[i] <= 100
- * */
 public class _1243 {
     public static class Solution1 {
         public List<Integer> transformArray(int[] arr) {
