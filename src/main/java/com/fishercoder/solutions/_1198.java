65dfa4b5cff5846c70bc735697f084aedc8a8a75
Fisher Coder <fishercoder@gmail.com>
2020-08-12 11:58:07 -0700

    refactor 119

--- src/main/java/com/fishercoder/solutions/_1198.java
+++ src/main/java/com/fishercoder/solutions/_1198.java
@@ -1,20 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 1198. Find Smallest Common Element in All Rows
- *
- * Given a matrix mat where every row is sorted in increasing order, return the smallest common element in all rows.
- * If there is no common element, return -1.
- *
- * Example 1:
- * Input: mat = [[1,2,3,4,5],[2,4,5,8,10],[3,5,7,9,11],[1,3,5,7,9]]
- * Output: 5
- *
- * Constraints:
- * 1 <= mat.length, mat[i].length <= 500
- * 1 <= mat[i][j] <= 10^4
- * mat[i] is sorted in increasing order.
- * */
 public class _1198 {
     public static class Solution1 {
         public int smallestCommonElement(int[][] mat) {
