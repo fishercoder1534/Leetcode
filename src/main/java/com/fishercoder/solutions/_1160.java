4b2f693f8d22189aae7ccf7f9b1711591c8a2ed9
Fisher Coder <fishercoder@gmail.com>
2020-08-12 07:28:12 -0700

    refactor 1160

--- src/main/java/com/fishercoder/solutions/_1160.java
+++ src/main/java/com/fishercoder/solutions/_1160.java
@@ -3,33 +3,6 @@ package com.fishercoder.solutions;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * 1160. Find Words That Can Be Formed by Characters
- *
- * You are given an array of strings words and a string chars.
- * A string is good if it can be formed by characters from chars (each character can only be used once).
- * Return the sum of lengths of all good strings in words.
- *
- * Example 1:
- *
- * Input: words = ["cat","bt","hat","tree"], chars = "atach"
- * Output: 6
- * Explanation:
- * The strings that can be formed are "cat" and "hat" so the answer is 3 + 3 = 6.
- *
- * Example 2:
- *
- * Input: words = ["hello","world","leetcode"], chars = "welldonehoneyr"
- * Output: 10
- * Explanation:
- * The strings that can be formed are "hello" and "world" so the answer is 5 + 5 = 10.
- *
- * Note:
- *
- * 1 <= words.length <= 1000
- * 1 <= words[i].length, chars.length <= 100
- * All strings contain lowercase English letters only.
- * */
 public class _1160 {
     public static class Solution1 {
         public int countCharacters(String[] words, String chars) {
