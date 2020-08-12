b76212e1add3a218f5b1fd96142f7281d49f4bb0
Fisher Coder <fishercoder@gmail.com>
2020-08-12 11:57:24 -0700

    refactor 1189

--- src/main/java/com/fishercoder/solutions/_1189.java
+++ src/main/java/com/fishercoder/solutions/_1189.java
@@ -1,27 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 1189. Maximum Number of Balloons
- *
- * Given a string text, you want to use the characters of text to form as many instances of the word "balloon" as possible.
- * You can use each character in text at most once. Return the maximum number of instances that can be formed.
- *
- * Example 1:
- * Input: text = "nlaebolko"
- * Output: 1
- *
- * Example 2:
- * Input: text = "loonbalxballpoon"
- * Output: 2
- *
- * Example 3:
- * Input: text = "leetcode"
- * Output: 0
- *
- * Constraints:
- * 1 <= text.length <= 10^4
- * text consists of lower case English letters only.
- * */
 public class _1189 {
     public static class Solution1 {
         public int maxNumberOfBalloons(String text) {
