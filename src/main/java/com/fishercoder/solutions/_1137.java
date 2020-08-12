2220d07c0b41cdeed9d45ec8b4f09bb536059b27
Fisher Coder <fishercoder@gmail.com>
2020-08-12 07:10:42 -0700

    refactor 1137

--- src/main/java/com/fishercoder/solutions/_1137.java
+++ src/main/java/com/fishercoder/solutions/_1137.java
@@ -2,31 +2,6 @@ package com.fishercoder.solutions;
 
 import java.util.stream.IntStream;
 
-/**
- * 1137. N-th Tribonacci Number
- *
- * The Tribonacci sequence Tn is defined as follows:
- *
- * T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
- *
- * Given n, return the value of Tn.
- *
- * Example 1:
- * Input: n = 4
- * Output: 4
- *
- * Explanation:
- * T_3 = 0 + 1 + 1 = 2
- * T_4 = 1 + 1 + 2 = 4
- *
- * Example 2:
- * Input: n = 25
- * Output: 1389537
- *
- * Constraints:
- * 0 <= n <= 37
- * The answer is guaranteed to fit within a 32-bit integer, ie. answer <= 2^31 - 1.
- * */
 public class _1137 {
     public static class Solution1 {
         public int tribonacci(int n) {
