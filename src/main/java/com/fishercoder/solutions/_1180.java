0c5a8ea37a8ac0972ce31217c895f2b3ba433db1
Fisher Coder <fishercoder@gmail.com>
2020-08-12 07:38:52 -0700

    refactor 1180

--- src/main/java/com/fishercoder/solutions/_1180.java
+++ src/main/java/com/fishercoder/solutions/_1180.java
@@ -1,28 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 1180. Count Substrings with Only One Distinct Letter
- *
- * Given a string S, return the number of substrings that have only one distinct letter.
- *
- * Example 1:
- * Input: S = "aaaba"
- * Output: 8
- * Explanation: The substrings with one distinct letter are "aaa", "aa", "a", "b".
- * "aaa" occurs 1 time.
- * "aa" occurs 2 times.
- * "a" occurs 4 times.
- * "b" occurs 1 time.
- * So the answer is 1 + 2 + 4 + 1 = 8.
- *
- * Example 2:
- * Input: S = "aaaaaaaaaa"
- * Output: 55
- *
- * Constraints:
- * 1 <= S.length <= 1000
- * S[i] consists of only lowercase English letters.
- * */
 public class _1180 {
     public static class Solution1 {
         public int countLetters(String S) {
