f154fff03e2e336b8f0b93a6d9853f883dec4802
Fisher Coder <fishercoder@gmail.com>
2020-08-12 11:59:21 -0700

    refactor 1213

--- src/main/java/com/fishercoder/solutions/_1213.java
+++ src/main/java/com/fishercoder/solutions/_1213.java
@@ -3,25 +3,11 @@ package com.fishercoder.solutions;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 1213. Intersection of Three Sorted Arrays
- *
- * Given three integer arrays arr1, arr2 and arr3 sorted in strictly increasing order,
- * return a sorted array of only the integers that appeared in all three arrays.
- *
- * Example 1:
- * Input: arr1 = [1,2,3,4,5], arr2 = [1,2,5,7,9], arr3 = [1,3,4,5,8]
- * Output: [1,5]
- * Explanation: Only 1 and 5 appeared in the three arrays.
- *
- * Constraints:
- *
- * 1 <= arr1.length, arr2.length, arr3.length <= 1000
- * 1 <= arr1[i], arr2[i], arr3[i] <= 2000
- * */
 public class _1213 {
     public static class Solution1 {
-        /**credit: https://leetcode.com/problems/intersection-of-three-sorted-arrays/discuss/397603/Simple-Java-solution-beats-100*/
+        /**
+         * credit: https://leetcode.com/problems/intersection-of-three-sorted-arrays/discuss/397603/Simple-Java-solution-beats-100
+         */
         public List<Integer> arraysIntersection(int[] arr1, int[] arr2, int[] arr3) {
             List<Integer> result = new ArrayList();
             int i = 0;
