d3db21358ac8de31858b0990b6020443d16f4e06
Fisher Coder <fishercoder@gmail.com>
2020-08-13 12:29:41 -0700

    refactor 1258

--- src/main/java/com/fishercoder/solutions/_1258.java
+++ src/main/java/com/fishercoder/solutions/_1258.java
@@ -9,30 +9,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-/**
- * 1258. Synonymous Sentences
- *
- * Given a list of pairs of equivalent words synonyms and a sentence text, Return all possible synonymous sentences sorted lexicographically.
- *
- * Example 1:
- * Input:
- * synonyms = [["happy","joy"],["sad","sorrow"],["joy","cheerful"]],
- * text = "I am happy today but was sad yesterday"
- * Output:
- * ["I am cheerful today but was sad yesterday",
- * ​​​​​​​"I am cheerful today but was sorrow yesterday",
- * "I am happy today but was sad yesterday",
- * "I am happy today but was sorrow yesterday",
- * "I am joy today but was sad yesterday",
- * "I am joy today but was sorrow yesterday"]
- *
- * Constraints:
- * 0 <= synonyms.length <= 10
- * synonyms[i].length == 2
- * synonyms[0] != synonyms[1]
- * All words consist of at most 10 English letters only.
- * text is a single space separated sentence of at most 10 words.
- * */
 public class _1258 {
     public static class Solution1 {
         public List<String> generateSentences(List<List<String>> synonyms, String text) {
