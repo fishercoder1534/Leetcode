f3a4febb25a2d29a6be6c4a61c4bfc3c73cbe367
Fisher Coder <fishercoder@gmail.com>
2020-08-13 12:29:55 -0700

    refactor 1260

--- src/main/java/com/fishercoder/solutions/_1260.java
+++ src/main/java/com/fishercoder/solutions/_1260.java
@@ -4,40 +4,11 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-/**
- * 1260. Shift 2D Grid
- *
- * Given a 2D grid of size m x n and an integer k. You need to shift the grid k times.
- *
- * In one shift operation:
- * Element at grid[i][j] becomes at grid[i][j + 1].
- * Element at grid[i][n - 1] becomes at grid[i + 1][0].
- * Element at grid[n - 1][n - 1] becomes at grid[0][0].
- * Return the 2D grid after applying shift operation k times.
- *
- * Example 1:
- * Input: grid = [[1,2,3],[4,5,6],[7,8,9]], k = 1
- * Output: [[9,1,2],[3,4,5],[6,7,8]]
- *
- * Example 2:
- * Input: grid = [[3,8,1,9],[19,7,2,5],[4,6,11,10],[12,0,21,13]], k = 4
- * Output: [[12,0,21,13],[3,8,1,9],[19,7,2,5],[4,6,11,10]]
- *
- * Example 3:
- * Input: grid = [[1,2,3],[4,5,6],[7,8,9]], k = 9
- * Output: [[1,2,3],[4,5,6],[7,8,9]]
- *
- * Constraints:
- * m == grid.length
- * n == grid[i].length
- * 1 <= m <= 50
- * 1 <= n <= 50
- * -1000 <= grid[i][j] <= 1000
- * 0 <= k <= 100
- * */
 public class _1260 {
     public static class Solution1 {
-        /**credit: https://leetcode.com/problems/shift-2d-grid/discuss/431102/JavaPython-3-simple-code-using-mod*/
+        /**
+         * credit: https://leetcode.com/problems/shift-2d-grid/discuss/431102/JavaPython-3-simple-code-using-mod
+         */
         public List<List<Integer>> shiftGrid(int[][] grid, int k) {
             int m = grid.length;
             int n = grid[0].length;
