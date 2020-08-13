bd52b2982796fe5e73536daa5cdb6ca01092fe19
Fisher Coder <fishercoder@gmail.com>
2020-08-13 12:32:24 -0700

    refactor 1273

--- src/main/java/com/fishercoder/solutions/_1273.java
+++ src/main/java/com/fishercoder/solutions/_1273.java
@@ -2,32 +2,6 @@ package com.fishercoder.solutions;
 
 import java.util.Arrays;
 
-/**
- * 1273. Delete Tree Nodes
- *
- * A tree rooted at node 0 is given as follows:
- * The number of nodes is nodes;
- * The value of the i-th node is value[i];
- * The parent of the i-th node is parent[i].
- * Remove every subtree whose sum of values of nodes is zero.
- * After doing so, return the number of nodes remaining in the tree.
- *
- * Example 1:
- *                      0 (1)
- *                     /  \
- *               (-2) 1    2 (4)
- *                   /    / |   \
- *              (0)3 (-2)4 (-1)5 6(-1)
- *
- * Input: nodes = 7, parent = [-1,0,0,1,2,2,2], value = [1,-2,4,0,-2,-1,-1]
- * Output: 2
- *
- * Constraints:
- * 1 <= nodes <= 10^4
- * -10^5 <= value[i] <= 10^5
- * parent.length == nodes
- * parent[0] == -1 which indicates that 0 is the root.
- * */
 public class _1273 {
     public static class Solution1 {
         public int deleteTreeNodes(int nodes, int[] parent, int[] value) {
