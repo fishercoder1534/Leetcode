1ec60675786a334a798540ac8392b547e5a982f6
Fisher Coder <fishercoder@gmail.com>
2020-08-16 06:54:35 -0700

    refactor 1551

--- src/main/java/com/fishercoder/solutions/_1551.java
+++ src/main/java/com/fishercoder/solutions/_1551.java
@@ -5,10 +5,10 @@ public class _1551 {
         public int minOperations(int n) {
             int min = 1;
             int max = 2 * (n - 1) + 1;
-            int finalNumber = (max + min) / 2;
+            int equalNumber = (max + min) / 2;
             int ops = 0;
             for (int i = 0; i < n / 2; i++) {
-                ops += finalNumber - (2 * i + 1);
+                ops += equalNumber - (2 * i + 1);
             }
             return ops;
         }
