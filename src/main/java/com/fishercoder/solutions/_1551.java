9f172fadb04f1b5dfda4902a9bb56a5950772e31
Fisher Coder <fishercoder@gmail.com>
2020-08-16 06:42:13 -0700

    refactor 1551

--- src/main/java/com/fishercoder/solutions/_1551.java
+++ src/main/java/com/fishercoder/solutions/_1551.java
@@ -6,13 +6,9 @@ public class _1551 {
             int min = 1;
             int max = 2 * (n - 1) + 1;
             int finalNumber = (max + min) / 2;
-            int[] arr = new int[n];
-            for (int i = 0; i < n; i++) {
-                arr[i] = 2 * i + 1;
-            }
             int ops = 0;
             for (int i = 0; i < n / 2; i++) {
-                ops += finalNumber - arr[i];
+                ops += finalNumber - (2 * i + 1);
             }
             return ops;
         }
