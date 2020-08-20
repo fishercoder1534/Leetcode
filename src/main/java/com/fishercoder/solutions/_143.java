c58b3bb5bf19d7ae7ec67c137987198a8bcb9d47
Fisher Coder <fishercoder@gmail.com>
2020-08-20 06:15:56 -0700

    refactor 143

--- src/main/java/com/fishercoder/solutions/_143.java
+++ src/main/java/com/fishercoder/solutions/_143.java
@@ -2,17 +2,6 @@ package com.fishercoder.solutions;
 
 import com.fishercoder.common.classes.ListNode;
 
-/**
- * 143. Reorder List
- *
- * Given a singly linked list L: L0→L1→…→Ln-1→Ln,
- reorder it to: L0→Ln→L1→Ln-1→L2→Ln-2→…
-
- You must do this in-place without altering the nodes' values.
-
- For example,
- Given {1,2,3,4}, reorder it to {1,4,2,3}.
- */
 public class _143 {
     public static class Solution1 {
         public void reorderList(ListNode head) {
