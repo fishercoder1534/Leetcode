665c0a12701bb69d785277d15a6ffd4a310b60f9
Fisher Coder <fishercoder@gmail.com>
2020-08-21 06:27:12 -0700

    refactor 1286

--- src/main/java/com/fishercoder/solutions/_1286.java
+++ src/main/java/com/fishercoder/solutions/_1286.java
@@ -3,28 +3,6 @@ package com.fishercoder.solutions;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 1286. Iterator for Combination
- *
- * Design an Iterator class, which has:
- * A constructor that takes a string characters of sorted distinct lowercase English letters and a number combinationLength as arguments.
- * A function next() that returns the next combination of length combinationLength in lexicographical order.
- * A function hasNext() that returns True if and only if there exists a next combination.
- *
- * Example:
- * CombinationIterator iterator = new CombinationIterator("abc", 2); // creates the iterator.
- * iterator.next(); // returns "ab"
- * iterator.hasNext(); // returns true
- * iterator.next(); // returns "ac"
- * iterator.hasNext(); // returns true
- * iterator.next(); // returns "bc"
- * iterator.hasNext(); // returns false
- *
- * Constraints:
- * 1 <= combinationLength <= characters.length <= 15
- * There will be at most 10^4 function calls per test.
- * It's guaranteed that all calls of the function next are valid.
- * */
 public class _1286 {
     public static class Solution1 {
         public static class CombinationIterator {
