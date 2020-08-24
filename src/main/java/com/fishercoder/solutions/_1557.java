bc977afb704688f39e7e95861e745cae400b4e34
Fisher Coder <fishercoder@gmail.com>
2020-08-24 06:23:17 -0700

    refactor 1557

--- src/main/java/com/fishercoder/solutions/_1557.java
+++ src/main/java/com/fishercoder/solutions/_1557.java
@@ -11,13 +11,10 @@ public class _1557 {
     public static class Solution1 {
         public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
             List<Integer> starts = new ArrayList<>();
-            Map<Integer, Set<Integer>> indegree = new HashMap<>();
+            Map<Integer, Integer> indegree = new HashMap<>();
             for (int i = 0; i < edges.size(); i++) {
                 int end = edges.get(i).get(1);
-                if (!indegree.containsKey(end)) {
-                    indegree.put(end, new HashSet<>());
-                }
-                indegree.get(end).add(edges.get(i).get(0));
+                indegree.put(end, indegree.getOrDefault(end, 0) + 1);
             }
             for (int i = 0; i < n; i++) {
                 if (!indegree.containsKey(i)) {
