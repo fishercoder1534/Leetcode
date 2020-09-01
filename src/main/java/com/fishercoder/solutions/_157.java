7762a7e4e14330d98ede199bffff3b82a5d95780
Fisher Coder <fishercoder@gmail.com>
2020-09-01 06:34:49 -0700

    refactor 157

--- src/main/java/com/fishercoder/solutions/_157.java
+++ src/main/java/com/fishercoder/solutions/_157.java
@@ -1,29 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 157. Read N Characters Given Read4
- *
- * The API: int read4(char *buf) reads 4 characters at a time from a file.
- *
- * The return value is the actual number of characters read. For example, it returns 3 if there is only 3 characters left in the file.
- *
- * By using the read4 API, implement the function int read(char *buf, int n) that reads n characters from the file.
- *
- * Example 1:
- *
- * Input: buf = "abc", n = 4
- * Output: "abc"
- * Explanation: The actual number of characters read is 3, which is "abc".
- *
- * Example 2:
- *
- * Input: buf = "abcde", n = 5
- * Output: "abcde"
- *
- * Note:
- * The read function will only be called once for each test case.
- *
- */
 public class _157 {
     public static class Solution1 {
         public int read(char[] buf, int n) {
@@ -39,7 +15,7 @@ public class _157 {
         }
 
         private int read4(char[] buffer) {
-            //this is a fake method to make Eclipse happy
+            //this is a dummy method to make it compile
             return 0;
         }
     }
