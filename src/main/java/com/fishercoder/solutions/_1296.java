bea7a4d4d70a180a6e2a6212458ce0f8b9772168
Fisher Coder <fishercoder@gmail.com>
2020-09-03 06:30:08 -0700

    refactor 1296

--- src/main/java/com/fishercoder/solutions/_1296.java
+++ src/main/java/com/fishercoder/solutions/_1296.java
@@ -2,37 +2,6 @@ package com.fishercoder.solutions;
 
 import java.util.TreeMap;
 
-/**
- * 1296. Divide Array in Sets of K Consecutive Numbers
- *
- * Given an array of integers nums and a positive integer k,
- * find whether it's possible to divide this array into sets of k consecutive numbers
- * Return True if its possible otherwise return False.
- *
- * Example 1:
- * Input: nums = [1,2,3,3,4,4,5,6], k = 4
- * Output: true
- * Explanation: Array can be divided into [1,2,3,4] and [3,4,5,6].
- *
- * Example 2:
- * Input: nums = [3,2,1,2,3,4,3,4,5,9,10,11], k = 3
- * Output: true
- * Explanation: Array can be divided into [1,2,3] , [2,3,4] , [3,4,5] and [9,10,11].
- *
- * Example 3:
- * Input: nums = [3,3,2,2,1,1], k = 3
- * Output: true
- *
- * Example 4:
- * Input: nums = [1,2,3,4], k = 3
- * Output: false
- * Explanation: Each array should be divided in subarrays of size 3.
- *
- * Constraints:
- * 1 <= nums.length <= 10^5
- * 1 <= nums[i] <= 10^9
- * 1 <= k <= nums.length
- * */
 public class _1296 {
     public static class Solution1 {
         public boolean isPossibleDivide(int[] nums, int k) {
