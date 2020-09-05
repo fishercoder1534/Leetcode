95c308dc8411a7e788c5ab128ecfc5429cbde5ee
Fisher Coder <fishercoder@gmail.com>
2020-09-05 06:26:15 -0700

    refactor 1305

--- src/main/java/com/fishercoder/solutions/_1305.java
+++ src/main/java/com/fishercoder/solutions/_1305.java
@@ -6,35 +6,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-/**
- * 1305. All Elements in Two Binary Search Trees
- *
- * Given two binary search trees root1 and root2.
- * Return a list containing all the integers from both trees sorted in ascending order.
- *
- * Example 1:
- * Input: root1 = [2,1,4], root2 = [1,0,3]
- * Output: [0,1,1,2,3,4]
- *
- * Example 2:
- * Input: root1 = [0,-10,10], root2 = [5,1,7,0,2]
- * Output: [-10,0,0,1,2,5,7,10]
- *
- * Example 3:
- * Input: root1 = [], root2 = [5,1,7,0,2]
- * Output: [0,1,2,5,7]
- *
- * Example 4:
- * Input: root1 = [0,-10,10], root2 = []
- * Output: [-10,0,10]
- *
- * Example 5:
- * Input: root1 = [1,null,8], root2 = [8,1]
- * Output: [1,1,8,8]
- *
- * Constraints:
- * Each tree has at most 5000 nodes.
- * Each node's value is between [-10^5, 10^5].*/
 public class _1305 {
     public static class Solution1 {
         public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
