bc284b510d5f64bf20c46d45933ae7409c827077
Fisher Coder <fishercoder@gmail.com>
2020-09-07 07:13:03 -0700

    refactor 1299

--- src/main/java/com/fishercoder/solutions/_1299.java
+++ src/main/java/com/fishercoder/solutions/_1299.java
@@ -2,16 +2,6 @@ package com.fishercoder.solutions;
 
 import java.util.PriorityQueue;
 
-/**
- * 1299. Replace Elements with Greatest Element on Right Side
- *
- * Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
- * After doing so, return the array.
- *
- * Example 1:
- * Input: arr = [17,18,5,4,6,1]
- * Output: [18,6,6,6,1,-1]
- * */
 public class _1299 {
     public static class Solution1 {
         public int[] replaceElements(int[] arr) {
