b1c02d85fa0881bba84ed267ebdb8fa73ebfd033
Fisher Coder <fishercoder@gmail.com>
2020-09-08 05:38:38 -0700

    refactor 1300

--- src/main/java/com/fishercoder/solutions/_1300.java
+++ src/main/java/com/fishercoder/solutions/_1300.java
@@ -1,26 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 1300. Sum of Mutated Array Closest to Target
- *
- * Given an integer array arr and a target value target, return the integer value such that when we change all the integers larger than value in the given array to be equal to value,
- * the sum of the array gets as close as possible (in absolute difference) to target.
- * In case of a tie, return the minimum such integer.
- * Notice that the answer is not neccesarilly a number from arr.
- *
- * Example 1:
- * Input: arr = [4,9,3], target = 10
- * Output: 3
- * Explanation: When using 3 arr converts to [3, 3, 3] which sums 9 and that's the optimal answer.
- *
- * Example 2:
- * Input: arr = [2,3,5], target = 10
- * Output: 5
- *
- * Example 3:
- * Input: arr = [60864,25176,27249,21296,20204], target = 56803
- * Output: 11361
- * */
 public class _1300 {
     public static class Solution1 {
         public int findBestValue(int[] arr, int target) {
