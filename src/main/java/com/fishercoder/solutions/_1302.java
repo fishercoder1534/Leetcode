5d2d00b79de39a7bc1a16ed057e6ae2e8a858791
Fisher Coder <fishercoder@gmail.com>
2020-09-10 08:13:43 -0700

    refactor 1302

--- src/main/java/com/fishercoder/solutions/_1302.java
+++ src/main/java/com/fishercoder/solutions/_1302.java
@@ -5,27 +5,6 @@ import com.fishercoder.common.classes.TreeNode;
 import java.util.LinkedList;
 import java.util.Queue;
 
-/**
- * 1302. Deepest Leaves Sum
- *
- * Given a binary tree, return the sum of values of its deepest leaves.
- *
- * Example 1:
- *                1
- *               / \
- *              2   3
- *             / \   \
- *            4  5   6
- *           /        \
- *          7         8
- *
- * Input: root = [1,2,3,4,5,null,6,7,null,null,null,null,8]
- * Output: 15
- *
- * Constraints:
- * The number of nodes in the tree is between 1 and 10^4.
- * The value of nodes is between 1 and 100.
- * */
 public class _1302 {
     public static class Solution1 {
         public int deepestLeavesSum(TreeNode root) {
