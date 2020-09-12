bdf176e8f45d61b0c03931e98d3e116624e0e89f
Fisher Coder <fishercoder@gmail.com>
2020-09-12 06:24:14 -0700

    refactor 1304

--- src/main/java/com/fishercoder/solutions/_1304.java
+++ src/main/java/com/fishercoder/solutions/_1304.java
@@ -1,23 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 1304. Find N Unique Integers Sum up to Zero
- *
- * Given an integer n, return any array containing n unique integers such that they add up to 0.
- *
- * Example 1:
- * Input: n = 5
- * Output: [-7,-1,1,3,4]
- * Explanation: These arrays also are accepted [-5,-1,1,2,3] , [-3,-1,2,-2,4].
- *
- * Example 2:
- * Input: n = 3
- * Output: [-1,0,1]
- *
- * Example 3:
- * Input: n = 1
- * Output: [0]
- * */
 public class _1304 {
     public static class Solution1 {
         public int[] sumZero(int n) {
