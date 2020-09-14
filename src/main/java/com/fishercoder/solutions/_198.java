a24f24dee9ec3d848c38e6ce5298b7ad2ad5210f
Fisher Coder <fishercoder@gmail.com>
2020-09-14 06:13:15 -0700

    refactor 198

--- src/main/java/com/fishercoder/solutions/_198.java
+++ src/main/java/com/fishercoder/solutions/_198.java
@@ -1,26 +1,5 @@
 package com.fishercoder.solutions;
 
-/**198. House Robber
-
- You are a professional robber planning to rob houses along a street.
- Each house has a certain amount of money stashed,
- the only constraint stopping you from robbing each of them is that adjacent houses have security
- system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
- Given a list of non-negative integers representing the amount of money of each house,
- determine the maximum amount of money you can rob tonight without alerting the police.
-
- Example 1:
- Input: [1,2,3,1]
- Output: 4
- Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
- Total amount you can rob = 1 + 3 = 4.
-
- Example 2:
- Input: [2,7,9,3,1]
- Output: 12
- Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
- Total amount you can rob = 2 + 9 + 1 = 12.
- */
 public class _198 {
 
     public static class Solution1 {
