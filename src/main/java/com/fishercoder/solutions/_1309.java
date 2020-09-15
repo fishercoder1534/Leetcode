60a2d35af7bc400cc104e5a0f935b38a32d78017
Fisher Coder <fishercoder@gmail.com>
2020-09-15 06:15:30 -0700

    refactor 1309

--- src/main/java/com/fishercoder/solutions/_1309.java
+++ src/main/java/com/fishercoder/solutions/_1309.java
@@ -3,37 +3,6 @@ package com.fishercoder.solutions;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * 1309. Decrypt String from Alphabet to Integer Mapping
- *
- * Given a string s formed by digits ('0' - '9') and '#' . We want to map s to English lowercase characters as follows:
- * Characters ('a' to 'i') are represented by ('1' to '9') respectively.
- * Characters ('j' to 'z') are represented by ('10#' to '26#') respectively.
- * Return the string formed after mapping.
- * It's guaranteed that a unique mapping will always exist.
- *
- * Example 1:
- * Input: s = "10#11#12"
- * Output: "jkab"
- * Explanation: "j" -> "10#" , "k" -> "11#" , "a" -> "1" , "b" -> "2".
- *
- * Example 2:
- * Input: s = "1326#"
- * Output: "acz"
- *
- * Example 3:
- * Input: s = "25#"
- * Output: "y"
- *
- * Example 4:
- * Input: s = "12345678910#11#12#13#14#15#16#17#18#19#20#21#22#23#24#25#26#"
- * Output: "abcdefghijklmnopqrstuvwxyz"
- *
- * Constraints:
- * 1 <= s.length <= 1000
- * s[i] only contains digits letters ('0'-'9') and '#' letter.
- * s will be valid string such that mapping is always possible.
- * */
 public class _1309 {
     public static class Solution1 {
         //TODO: very silly solution, optimze it
