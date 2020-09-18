b0fba8189cb1e783a9544b28df6e91d0285352d7
Fisher Coder <fishercoder@gmail.com>
2020-09-18 06:38:15 -0700

    refactor 1314

--- src/main/java/com/fishercoder/solutions/_1314.java
+++ src/main/java/com/fishercoder/solutions/_1314.java
@@ -3,27 +3,6 @@ package com.fishercoder.solutions;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 1314. Matrix Block Sum
- *
- * Given a m * n matrix mat and an integer K,
- * return a matrix answer where each answer[i][j] is the sum of all elements
- * mat[r][c] for i - K <= r <= i + K, j - K <= c <= j + K, and (r, c) is a valid position in the matrix.
- *
- * Example 1:
- * Input: mat = [[1,2,3],[4,5,6],[7,8,9]], K = 1
- * Output: [[12,21,16],[27,45,33],[24,39,28]]
- *
- * Example 2:
- * Input: mat = [[1,2,3],[4,5,6],[7,8,9]], K = 2
- * Output: [[45,45,45],[45,45,45],[45,45,45]]
- *
- * Constraints:
- * m == mat.length
- * n == mat[i].length
- * 1 <= m, n, K <= 100
- * 1 <= mat[i][j] <= 100
- * */
 public class _1314 {
     public static class Solution1 {
         public int[][] matrixBlockSum(int[][] mat, int K) {
