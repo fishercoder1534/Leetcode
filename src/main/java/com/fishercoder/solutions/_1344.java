c6000a97192b3cf45d20fa40ec5dc8c1bfb8bfa3
Fisher Coder <fishercoder@gmail.com>
2020-09-24 06:21:02 -0700

    refactor 1344

--- src/main/java/com/fishercoder/solutions/_1344.java
+++ src/main/java/com/fishercoder/solutions/_1344.java
@@ -1,35 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 1344. Angle Between Hands of a Clock
- *
- * Given two numbers, hour and minutes. Return the smaller angle (in sexagesimal units) formed between the hour and the minute hand.
- *
- * Example 1:
- * Input: hour = 12, minutes = 30
- * Output: 165
- *
- * Example 2:
- * Input: hour = 3, minutes = 30
- * Output: 75
- *
- * Example 3:
- * Input: hour = 3, minutes = 15
- * Output: 7.5
- *
- * Example 4:
- * Input: hour = 4, minutes = 50
- * Output: 155
- *
- * Example 5:
- * Input: hour = 12, minutes = 0
- * Output: 0
- *
- * Constraints:
- * 1 <= hour <= 12
- * 0 <= minutes <= 59
- * Answers within 10^-5 of the actual value will be accepted as correct.
- * */
 public class _1344 {
     public static class Solution1 {
         public double angleClock(int hour, int minutes) {
