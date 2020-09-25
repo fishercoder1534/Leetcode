2c84a2f02c3c2330c4c024f3126a32ccaa814cfe
Fisher Coder <fishercoder@gmail.com>
2020-09-25 06:45:41 -0700

    refactor 1345

--- src/main/java/com/fishercoder/solutions/_1345.java
+++ src/main/java/com/fishercoder/solutions/_1345.java
@@ -6,47 +6,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 
-/**
- * 1345. Jump Game IV
- *
- * Given an array of integers arr, you are initially positioned at the first index of the array.
- * In one step you can jump from index i to index:
- * i + 1 where: i + 1 < arr.length.
- * i - 1 where: i - 1 >= 0.
- * j where: arr[i] == arr[j] and i != j.
- * Return the minimum number of steps to reach the last index of the array.
- * Notice that you can not jump outside of the array at any time.
- *
- * Example 1:
- * Input: arr = [100,-23,-23,404,100,23,23,23,3,404]
- * Output: 3
- * Explanation: You need three jumps from index 0 --> 4 --> 3 --> 9. Note that index 9 is the last index of the array.
- *
- * Example 2:
- * Input: arr = [7]
- * Output: 0
- * Explanation: Start index is the last index. You don't need to jump.
- *
- * Example 3:
- * Input: arr = [7,6,9,6,9,6,9,7]
- * Output: 1
- * Explanation: You can jump directly from index 0 to index 7 which is last index of the array.
- *
- * Example 4:
- * Input: arr = [6,1,9]
- * Output: 2
- *
- * Example 5:
- * Input: arr = [11,22,7,7,7,7,7,7,7,22,13]
- * Output: 3
- *
- * Constraints:
- * 1 <= arr.length <= 5 * 10^4
- * -10^8 <= arr[i] <= 10^8
- * */
 public class _1345 {
     public static class Solution1 {
-        /**credit: https://leetcode.com/problems/jump-game-iv/discuss/502699/JavaC%2B%2B-BFS-Solution-Clean-code-O(N)*/
+        /**
+         * credit: https://leetcode.com/problems/jump-game-iv/discuss/502699/JavaC%2B%2B-BFS-Solution-Clean-code-O(N)
+         */
         public int minJumps(int[] arr) {
             Map<Integer, List<Integer>> valueToIndices = new HashMap<>();
             for (int i = 0; i < arr.length; i++) {
