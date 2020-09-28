4c0f6a9cc64d5969e46ab6566671deb32cfd7776
Fisher Coder <fishercoder@gmail.com>
2020-09-28 06:38:16 -0700

    refactor 713

--- src/main/java/com/fishercoder/solutions/_713.java
+++ src/main/java/com/fishercoder/solutions/_713.java
@@ -2,30 +2,6 @@ package com.fishercoder.solutions;
 
 public class _713 {
     public static class Solution1 {
-        /**
-         * O(n^2) solution, accepted initially, then Leetcode added one test case to fail it.
-         */
-        public int numSubarrayProductLessThanK(int[] nums, int k) {
-            int result = 0;
-            for (int i = 0; i < nums.length; i++) {
-                int product = nums[i];
-                if (product < k) {
-                    result++;
-                    for (int j = i + 1; j < nums.length; j++) {
-                        product *= nums[j];
-                        if (product < k) {
-                            result++;
-                        } else {
-                            break;
-                        }
-                    }
-                }
-            }
-            return result;
-        }
-    }
-
-    public static class Solution2 {
         public int numSubarrayProductLessThanK(int[] nums, int k) {
             if (k < 2) {
                 return 0;
--- src/test/java/com/fishercoder/_713Test.java
+++ src/test/java/com/fishercoder/_713Test.java
@@ -8,13 +8,11 @@ import static org.junit.Assert.assertEquals;
 
 public class _713Test {
     private static _713.Solution1 solution1;
-    private static _713.Solution2 solution2;
     private static int[] nums;
     private static int k;
 
     @BeforeClass
     public static void setup() {
-        solution2 = new _713.Solution2();
         solution1 = new _713.Solution1();
     }
 
@@ -22,7 +20,6 @@ public class _713Test {
     public void test1() {
         nums = new int[]{1, 2, 3};
         k = 0;
-        assertEquals(0, solution2.numSubarrayProductLessThanK(nums, k));
         assertEquals(0, solution1.numSubarrayProductLessThanK(nums, k));
     }
 
