7b102171c46ac2023eb2e2f4d36bd56b224dda58
Fisher Coder <fishercoder@gmail.com>
2020-09-30 07:16:51 -0700

    refactor 1349

--- src/main/java/com/fishercoder/solutions/_1349.java
+++ src/main/java/com/fishercoder/solutions/_1349.java
@@ -2,49 +2,11 @@ package com.fishercoder.solutions;
 
 import java.util.Arrays;
 
-/**
- * 1349. Maximum Students Taking Exam
- *
- * Given a m * n matrix seats  that represent seats distributions in a classroom.
- * If a seat is broken, it is denoted by '#' character otherwise it is denoted by a '.' character.
- * Students can see the answers of those sitting next to the left, right, upper left and upper right, but he cannot see the answers of the student sitting directly in front or behind him. Return the maximum number of students that can take the exam together without any cheating being possible..
- * Students must be placed in seats in good condition.
- *
- * Example 1:
- * Input: seats = [["#",".","#","#",".","#"],
- *                 [".","#","#","#","#","."],
- *                 ["#",".","#","#",".","#"]]
- * Output: 4
- * Explanation: Teacher can place 4 students in available seats so they don't cheat on the exam.
- *
- * Example 2:
- * Input: seats = [[".","#"],
- *                 ["#","#"],
- *                 ["#","."],
- *                 ["#","#"],
- *                 [".","#"]]
- * Output: 3
- * Explanation: Place all students in available seats.
- *
- * Example 3:
- * Input: seats = [["#",".",".",".","#"],
- *                 [".","#",".","#","."],
- *                 [".",".","#",".","."],
- *                 [".","#",".","#","."],
- *                 ["#",".",".",".","#"]]
- * Output: 10
- * Explanation: Place students in available seats in column 1, 3 and 5.
- *
- * Constraints:
- * seats contains only characters '.' and'#'.
- * m == seats.length
- * n == seats[i].length
- * 1 <= m <= 8
- * 1 <= n <= 8
- * */
 public class _1349 {
     public static class Solution1 {
-        /**credit: https://leetcode.com/problems/maximum-students-taking-exam/discuss/503686/A-simple-tutorial-on-this-bitmasking-problem*/
+        /**
+         * credit: https://leetcode.com/problems/maximum-students-taking-exam/discuss/503686/A-simple-tutorial-on-this-bitmasking-problem
+         */
         public int maxStudents(char[][] seats) {
             int m = seats.length;
             int n = seats[0].length;
