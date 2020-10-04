5edfb1be32b9d1ee5f1a426f1a98810d9de283c7
Anshul Gupta <ag1507anshul@gmail.com>
2020-10-04 10:15:20 +0530

    Update _31.cpp

--- cpp/_31.cpp
+++ cpp/_31.cpp
@@ -2,9 +2,11 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
     
-        int i1,i2;
+        int i1;
+        int i2;
         bool hasPermutation = false;
-        for(int i=nums.size()-1;i>0;i--){
+        
+        for(int i=nums.size()-1; i>0; i--){
             if(nums[i-1]<nums[i]){
                 i1 = i-1;
                 i2 = i;
@@ -15,19 +17,17 @@ public:
         
         if(hasPermutation){
             int j=i2;
-            for(int i=nums.size()-1;i>i1;i--){
+            for(int i=nums.size()-1; i>i1; i--){
                 if(nums[i]>nums[i1]){
                     j=i;
                     break;
                 }
             }
-            swap(nums[i1],nums[j]);
-            reverse(nums.begin()+i1+1,nums.end());
+            swap(nums[i1], nums[j]);
+            reverse(nums.begin()+i1+1, nums.end());
         }else{
-            sort(nums.begin(),nums.end());
+            sort(nums.begin(), nums.end());
         }
-        for(auto i:nums){
-            cout << i << " ";
-        }
+  
     }
 };
