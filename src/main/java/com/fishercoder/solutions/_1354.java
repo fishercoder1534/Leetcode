99d00b666913e5fb013acef1040cbb7e79c63e60
Fisher Coder <fishercoder@gmail.com>
2020-10-06 07:10:35 -0700

    refactor 1354

--- src/main/java/com/fishercoder/solutions/_1354.java
+++ src/main/java/com/fishercoder/solutions/_1354.java
@@ -3,45 +3,13 @@ package com.fishercoder.solutions;
 import java.util.Collections;
 import java.util.PriorityQueue;
 
-/**
- * 1354. Construct Target Array With Multiple Sums
- *
- * Given an array of integers target. From a starting array, A consisting of all 1's, you may perform the following procedure :
- * let x be the sum of all elements currently in your array.
- * choose index i, such that 0 <= i < target.size and set the value of A at index i to x.
- * You may repeat this procedure as many times as needed.
- * Return True if it is possible to construct the target array from A otherwise return False.
- *
- * Example 1:
- * Input: target = [9,3,5]
- * Output: true
- * Explanation: Start with [1, 1, 1]
- * [1, 1, 1], sum = 3 choose index 1
- * [1, 3, 1], sum = 5 choose index 2
- * [1, 3, 5], sum = 9 choose index 0
- * [9, 3, 5] Done
- *
- * Example 2:
- * Input: target = [1,1,1,2]
- * Output: false
- * Explanation: Impossible to create target array from [1,1,1,1].
- *
- * Example 3:
- * Input: target = [8,5]
- * Output: true
- *
- * Constraints:
- * N == target.length
- * 1 <= target.length <= 5 * 10^4
- * 1 <= target[i] <= 10^9
- * */
 public class _1354 {
     public static class Solution1 {
         /**
          * 1. A good idea/trick to calculate the previous value of the largest number max: (2 * max - total).
          * 2. Use a PriorityQueue to store the elements in reverse order to help us get the largest element in O(1) time
          * 3. Also keep a variable of total sum
-         *
+         * <p>
          * reference: https://leetcode.com/problems/construct-target-array-with-multiple-sums/discuss/510214/C%2B%2B-Reaching-Points-Work-Backwards
          */
         public boolean isPossible(int[] target) {
