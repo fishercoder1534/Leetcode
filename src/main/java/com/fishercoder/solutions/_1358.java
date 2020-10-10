00115e4f12b8a8c358fbbba4862ce44b1c14ce17
Fisher Coder <fishercoder@gmail.com>
2020-10-10 06:30:43 -0700

    refactor 1358

--- src/main/java/com/fishercoder/solutions/_1358.java
+++ src/main/java/com/fishercoder/solutions/_1358.java
@@ -1,34 +1,11 @@
 package com.fishercoder.solutions;
 
-/**
- * 1358. Number of Substrings Containing All Three Characters
- *
- * Given a string s consisting only of characters a, b and c.
- * Return the number of substrings containing at least one occurrence of all these characters a, b and c.
- *
- * Example 1:
- * Input: s = "abcabc"
- * Output: 10
- * Explanation: The substrings containing at least one occurrence of the characters a, b and c are "abc", "abca", "abcab", "abcabc", "bca", "bcab", "bcabc", "cab", "cabc" and "abc" (again).
- *
- * Example 2:
- * Input: s = "aaacb"
- * Output: 3
- * Explanation: The substrings containing at least one occurrence of the characters a, b and c are "aaacb", "aacb" and "acb".
- *
- * Example 3:
- * Input: s = "abc"
- * Output: 1
- *
- * Constraints:
- * 3 <= s.length <= 5 x 10^4
- * s only consists of a, b or c characters.
- * */
 public class _1358 {
     public static class Solution1 {
-        /**A classic sliding window problem, no dp or backtracking, just sliding window: use two pointers.
+        /**
+         * A classic sliding window problem, no dp or backtracking, just sliding window: use two pointers.
          * my new favorite queustion!
-         * */
+         */
         public int numberOfSubstrings(String s) {
             int[] counts = new int[3];
             int i = 0;
