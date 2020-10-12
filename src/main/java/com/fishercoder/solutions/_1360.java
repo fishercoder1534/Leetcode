011fed01a358103421a849fac95b279c0f71771b
Fisher Coder <fishercoder@gmail.com>
2020-10-12 06:58:40 -0700

    refactor 1360

--- src/main/java/com/fishercoder/solutions/_1360.java
+++ src/main/java/com/fishercoder/solutions/_1360.java
@@ -1,22 +1,5 @@
 package com.fishercoder.solutions;
 
-/**
- * 1360. Number of Days Between Two Dates
- *
- * Write a program to count the number of days between two dates.
- * The two dates are given as strings, their format is YYYY-MM-DD as shown in the examples.
- *
- * Example 1:
- * Input: date1 = "2019-06-29", date2 = "2019-06-30"
- * Output: 1
- *
- * Example 2:
- * Input: date1 = "2020-01-15", date2 = "2019-12-31"
- * Output: 15
- *
- * Constraints:
- * The given dates are valid dates between the years 1971 and 2100.
- * */
 public class _1360 {
     public static class Solution1 {
         public int daysBetweenDates(String date1, String date2) {
