d1016cd332875ac5836d0fe0c5c91f65056565f5
Fisher Coder <fishercoder@gmail.com>
2020-10-13 06:35:40 -0700

    refactor 148

--- src/main/java/com/fishercoder/solutions/_148.java
+++ src/main/java/com/fishercoder/solutions/_148.java
@@ -2,19 +2,6 @@ package com.fishercoder.solutions;
 
 import com.fishercoder.common.classes.ListNode;
 
-/**
- * 148. Sort List
- *
- * Sort a linked list in O(n log n) time using constant space complexity.
- *
- * Example 1:
- * Input: 4->2->1->3
- * Output: 1->2->3->4
- *
- * Example 2:
- * Input: -1->5->3->4->0
- * Output: -1->0->3->4->5
- */
 public class _148 {
 
     public static class Solution1 {
