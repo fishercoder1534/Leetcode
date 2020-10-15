cf1de47bd80c7a2eaaa0535366e0f32e457eabdd
Fisher Coder <fishercoder@gmail.com>
2020-10-15 06:16:05 -0700

    refactor 253

--- src/main/java/com/fishercoder/solutions/_253.java
+++ src/main/java/com/fishercoder/solutions/_253.java
@@ -1,35 +1,33 @@
 package com.fishercoder.solutions;
 
-import com.fishercoder.common.classes.Interval;
-
 import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class _253 {
     public static class Solution1 {
 
-        public int minMeetingRooms(Interval[] intervals) {
+        public int minMeetingRooms(int[][] intervals) {
             if (intervals == null || intervals.length == 0) {
                 return 0;
             }
 
             // Sort the intervals by start time
-            Arrays.sort(intervals, (a, b) -> a.start - b.start);
+            Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
 
             // Use a min heap to track the minimum end time of merged intervals
-            PriorityQueue<Interval> heap = new PriorityQueue<>(intervals.length, (a, b) -> a.end - b.end);
+            PriorityQueue<int[]> heap = new PriorityQueue<>(intervals.length, (a, b) -> a[1] - b[1]);
 
             // start with the first meeting, put it to a meeting room
             heap.offer(intervals[0]);
 
             for (int i = 1; i < intervals.length; i++) {
                 // get the meeting room that finishes earliest
-                Interval interval = heap.poll();
+                int[] interval = heap.poll();
 
-                if (intervals[i].start >= interval.end) {
+                if (intervals[i][0] >= interval[1]) {
                     // if the current meeting starts right after
                     // there's no need for a new room, merge the interval
-                    interval.end = intervals[i].end;
+                    interval[1] = intervals[i][1];
                 } else {
                     // otherwise, this meeting needs a new room
                     heap.offer(intervals[i]);
