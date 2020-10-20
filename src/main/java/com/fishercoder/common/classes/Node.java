616a7e5338fd11624bb42002c5cb55237c053700
Fisher Coder <fishercoder@gmail.com>
2020-10-20 06:41:39 -0700

    refactor Node

--- src/main/java/com/fishercoder/common/classes/Node.java
+++ src/main/java/com/fishercoder/common/classes/Node.java
@@ -4,25 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Node {
-  public int val;
-  public List<Node> children;
+    public int val;
+    public List<Node> children;
 
-  public Node() {
-  }
+    public Node() {
+    }
 
-  public Node(int val, List<Node> children) {
-    this.val = val;
-    this.children = children;
-  }
+    public Node(int val, List<Node> children) {
+        this.val = val;
+        this.children = children;
+    }
 
-  public Node(int val) {
-    this.val = val;
-    this.children = new ArrayList<>();
-  }
+    public Node(int val) {
+        this.val = val;
+        this.children = new ArrayList<>();
+    }
 
-  //todo: implement this method
-  /**return a N-ary tree based on the preorder values*/
-  public static Node createNaryTree(List<Integer> preorderValues) {
-    return null;
-  }
+    //todo: implement this method
+
+    /**
+     * return a N-ary tree based on the preorder values
+     */
+    public static Node createNaryTree(List<Integer> preorderValues) {
+        return null;
+    }
 }
