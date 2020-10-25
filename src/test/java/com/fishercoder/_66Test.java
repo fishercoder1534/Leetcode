46815c1b483f695a5f1397cb741f3c40f3619ed7
WillShen7777 <40691766+WillShen7789@users.noreply.github.com>
2020-10-26 00:19:19 +0800

    add _66.java solution2 (#116)
    * add _66.java solution2
    
    * write new tests for solution2
    
    * remove import

--- src/test/java/com/fishercoder/_66Test.java
+++ src/test/java/com/fishercoder/_66Test.java
@@ -1,18 +1,21 @@
 package com.fishercoder;
 
-import com.fishercoder.solutions._66;
+import static org.junit.Assert.assertArrayEquals;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import static org.junit.Assert.assertArrayEquals;
+import com.fishercoder.solutions._66;
 
 public class _66Test {
     private static _66.Solution1 solution1;
+    private static _66.Solution2 solution2;
     private static int[] digits;
 
     @BeforeClass
     public static void setup() {
         solution1 = new _66.Solution1();
+        solution2 = new _66.Solution2();
     }
 
     @Test
@@ -32,4 +35,23 @@ public class _66Test {
         digits = new int[]{2, 4, 9, 3, 9};
         assertArrayEquals(new int[]{2, 4, 9, 4, 0}, solution1.plusOne(digits));
     }
+    
+    @Test
+    public void test4() {
+        digits = new int[]{9, 9, 9, 9, 9};
+        assertArrayEquals(new int[]{1, 0, 0, 0, 0, 0}, solution2.plusOne(digits));
+    }
+
+    @Test
+    public void test5() {
+        digits = new int[]{8, 9, 9, 9, 9};
+        assertArrayEquals(new int[]{9, 0, 0, 0, 0}, solution2.plusOne(digits));
+    }
+    
+    @Test
+    public void test6() {
+        digits = new int[]{2, 4, 9, 4, 9};
+        assertArrayEquals(new int[]{2, 4, 9, 5, 0}, solution2.plusOne(digits));
+    }
+    
 }
