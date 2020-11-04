5c8465f11ca4f4732cac9ff6c5dd84144c776339
Fisher Coder <fishercoder@gmail.com>
2020-11-04 06:50:04 -0800

    refactor 757

--- src/test/java/com/fishercoder/_757Test.java
+++ src/test/java/com/fishercoder/_757Test.java
@@ -11,17 +11,19 @@ public class _757Test {
     int intervals[][];
 
     @BeforeClass
-    public static void setup(){
+    public static void setup() {
         solution = new _757.Solution();
     }
+
     @Test
     public void test1() {
-        intervals = new int [][]{{1,3},{1, 4}, {2,5},{3,5}};
+        intervals = new int[][]{{1, 3}, {1, 4}, {2, 5}, {3, 5}};
         assertEquals(3, solution.intersectionSizeTwo(intervals));
     }
+
     @Test
     public void test2() {
-        intervals = new int [][]{{16,18},{11, 18}, {15,23},{1,16}, {10,16},{6,19}, {18,20}, {7,19}};
+        intervals = new int[][]{{16, 18}, {11, 18}, {15, 23}, {1, 16}, {10, 16}, {6, 19}, {18, 20}, {7, 19}};
         assertEquals(4, solution.intersectionSizeTwo(intervals));
     }
 }
