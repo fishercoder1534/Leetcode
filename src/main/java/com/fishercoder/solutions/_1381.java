de5dff9d054fcae0386b67c0f86b514a518e0f37
ashmichheda <ashmichheda@gmail.com>
2020-11-05 10:40:37 -0700

    added array solution for 1381 with test cases

--- src/main/java/com/fishercoder/solutions/_1381.java
+++ src/main/java/com/fishercoder/solutions/_1381.java
@@ -79,4 +79,43 @@ public class _1381 {
             }
         }
     }
+
+    /**
+     * Implementation of Stack using Array
+     */
+    public static class Solution2{
+        public static class CustomStack {
+            private int top;
+            private int maxSize;
+            private int stack[];
+            public CustomStack(int maxSize) {
+                this.maxSize = maxSize;
+                this.stack = new int[maxSize];
+            }
+
+            public void push(int x) {
+                if(top == maxSize) return;
+                stack[top] = x;
+                top++;
+            }
+
+            public int pop() {
+                if(top == 0)
+                    return -1;
+                int popValue = stack[top-1];
+                stack[top-1] = 0;
+                top--;
+                return popValue;
+            }
+
+            public void increment(int k, int val) {
+                if(top == 0 || k == 0) return;
+                for(int i = 0; i<k; i++){
+                    if(i == top)
+                        break;
+                    stack[i] += val;
+                }
+            }
+        }
+    }
 }
--- src/test/java/com/fishercoder/_1381Test.java
+++ src/test/java/com/fishercoder/_1381Test.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.assertEquals;
 
 public class _1381Test {
     private static _1381.Solution1.CustomStack customStack;
+    private static _1381.Solution2.CustomStack customStack2;
 
     @Test
     public void test1() {
@@ -23,6 +24,21 @@ public class _1381Test {
         assertEquals(202, customStack.pop());
         assertEquals(201, customStack.pop());
         assertEquals(-1, customStack.pop());
+    }
+    @Test
+    public void test2() {
+        customStack2 = new _1381.Solution2.CustomStack(3);
+        customStack2.push(-1);
+        customStack2.push(20);
+        assertEquals(20, customStack2.pop());
+        customStack2.push(30);
+        customStack2.push(40);
+        customStack2.push(50);
+        customStack2.increment(5, 100);
+        assertEquals(140, customStack2.pop());
+        assertEquals(130, customStack2.pop());
+        assertEquals(99, customStack2.pop());
+
     }
 
 }
