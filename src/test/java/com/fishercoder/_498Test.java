fabd91761b3fb7590a0547603636b590d4369328
WillShen7777 <40691766+WillShen7789@users.noreply.github.com>
2020-11-11 23:00:00 +0800

    Add _498.Solution2 And Test3 & 4 (#131)

--- src/test/java/com/fishercoder/_498Test.java
+++ src/test/java/com/fishercoder/_498Test.java
@@ -11,12 +11,14 @@ import static org.junit.Assert.assertArrayEquals;
  */
 public class _498Test {
     private static _498.Solutoin1 solutoin1;
+    private static _498.Solutoin2 solutoin2;
     private static int[][] matrix;
     private static int[] expected;
 
     @BeforeClass
     public static void setup() {
         solutoin1 = new _498.Solutoin1();
+        solutoin2 = new _498.Solutoin2();
     }
 
     @Test
@@ -41,5 +43,25 @@ public class _498Test {
         };
         expected = new int[]{1, 2, 4, 7, 5, 3, 6, 8, 10, 13, 11, 9, 12, 14, 15};
         assertArrayEquals(expected, solutoin1.findDiagonalOrder(matrix));
+    }
+
+    @Test
+    public void test3() {
+        matrix = new int[][]{
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9},
+        };
+        expected = new int[]{1, 2, 4, 7, 5, 3, 6, 8, 9};
+        assertArrayEquals(expected, solutoin2.findDiagonalOrder(matrix));
+    }
+    
+    @Test
+    public void test4() {
+        matrix = new int[][]{
+        	{2,5},{8,4},{0,-1}
+        };
+        expected = new int[]{2,5,8,0,4,-1};
+        assertArrayEquals(expected, solutoin2.findDiagonalOrder(matrix));
     }
 }
