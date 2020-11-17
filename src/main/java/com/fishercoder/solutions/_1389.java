c4e96a5a79ce3a9c880a0edd1f5d1e045d8bc4ea
Fisher Coder <fishercoder@gmail.com>
2020-11-17 06:38:57 -0800

    add 1389

--- src/main/java/com/fishercoder/solutions/_1389.java
+++ src/main/java/com/fishercoder/solutions/_1389.java
@@ -3,49 +3,6 @@ package com.fishercoder.solutions;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 1389. Create Target Array in the Given Order
- *
- * Given two arrays of integers nums and index. Your task is to create target array under the following rules:
- * Initially target array is empty.
- * From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array.
- * Repeat the previous step until there are no elements to read in nums and index.
- * Return the target array.
- *
- * It is guaranteed that the insertion operations will be valid.
- *
- * Example 1:
- * Input: nums = [0,1,2,3,4], index = [0,1,2,2,1]
- * Output: [0,4,1,3,2]
- * Explanation:
- * nums       index     target
- * 0            0        [0]
- * 1            1        [0,1]
- * 2            2        [0,1,2]
- * 3            2        [0,1,3,2]
- * 4            1        [0,4,1,3,2]
- *
- * Example 2:
- * Input: nums = [1,2,3,4,0], index = [0,1,2,3,0]
- * Output: [0,1,2,3,4]
- * Explanation:
- * nums       index     target
- * 1            0        [1]
- * 2            1        [1,2]
- * 3            2        [1,2,3]
- * 4            3        [1,2,3,4]
- * 0            0        [0,1,2,3,4]
- *
- * Example 3:
- * Input: nums = [1], index = [0]
- * Output: [1]
- *
- * Constraints:
- * 1 <= nums.length, index.length <= 100
- * nums.length == index.length
- * 0 <= nums[i] <= 100
- * 0 <= index[i] <= i
- * */
 public class _1389 {
     public static class Solution1 {
         public int[] createTargetArray(int[] nums, int[] index) {
