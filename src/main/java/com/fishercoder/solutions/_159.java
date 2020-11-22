116daaa7c3895c320ab244fefba0043359b5acd7
Fisher Coder <fishercoder@gmail.com>
2020-11-22 08:47:53 -0800

    refactor 159

--- src/main/java/com/fishercoder/solutions/_159.java
+++ src/main/java/com/fishercoder/solutions/_159.java
@@ -3,20 +3,6 @@ package com.fishercoder.solutions;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * 159. Longest Substring with At Most Two Distinct Characters
- * Given a string s , find the length of the longest substring t  that contains at most 2 distinct characters.
- *
- * Example 1:
- * Input: "eceba"
- * Output: 3
- * Explanation: t is "ece" which its length is 3.
- *
- * Example 2:
- * Input: "ccaabbb"
- * Output: 5
- * Explanation: t is "aabbb" which its length is 5.
- */
 public class _159 {
     public static class Solution1 {
         public int lengthOfLongestSubstringTwoDistinct(String s) {
