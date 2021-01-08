f4311feb425e35f0233e957df3028ca63f99b9bd
ashmichheda <ashmichheda@gmail.com>
2021-01-07 21:21:00 -0700

    add solution for 236

--- src/main/java/com/fishercoder/solutions/_236.java
+++ src/main/java/com/fishercoder/solutions/_236.java
@@ -2,6 +2,9 @@ package com.fishercoder.solutions;
 
 import com.fishercoder.common.classes.TreeNode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class _236 {
     public static class Solution1 {
 
@@ -18,4 +21,46 @@ public class _236 {
         }
     }
 
+    public static class Solution2 {
+        List<Integer> path1 = new ArrayList<>();
+        List<Integer> path2 = new ArrayList<>();
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+            return findLCA(root, p, q);
+        }
+
+        private TreeNode findLCA(TreeNode root, TreeNode p, TreeNode q){
+
+            if(!findPath(root, p, path1) || !findPath(root, q, path2)){
+                return null;
+            }
+            int i = 0;
+            while(i < path1.size() && i < path2.size()){
+                if(!path1.get(i).equals(path2.get(i))){
+                    break;
+                }
+                i++;
+            }
+            return new TreeNode(path1.get(i-1));
+        }
+        private boolean findPath(TreeNode root, TreeNode node, List<Integer> path){
+            if(root == null)
+                return false;
+
+            path.add(root.val);
+
+            if(root.val == node.val)
+                return true;
+
+            if(root.left != null && findPath(root.left, node, path)){
+                return true;
+            }
+            if(root.right != null && findPath(root.right, node, path)){
+                return true;
+            }
+
+            path.remove(path.size()-1);
+            return false;
+        }
+    }
 }
