ad94913d0da5ef11613fd0e887089f2b4f866dd6
Fisher Coder <fishercoder@gmail.com>
2021-01-09 06:56:21 -0800

    refactor 127

--- src/main/java/com/fishercoder/solutions/_127.java
+++ src/main/java/com/fishercoder/solutions/_127.java
@@ -4,32 +4,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-/**
- * 127. Word Ladder
- *
- *  Given two words (beginWord and endWord), and a dictionary's word list,
- *  find the length of shortest transformation sequence from beginWord to endWord, such that:
- *  Only one letter can be changed at a time.
- *  Each transformed word must exist in the word list. Note that beginWord is not a transformed word.
-
- For example,
-
- Given:
- beginWord = "hit"
- endWord = "cog"
- wordList = ["hot","dot","dog","lot","log","cog"]
-
- As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog", return its length 5.
-
- Note:
-
- Return 0 if there is no such transformation sequence.
- All words have the same length.
- All words contain only lowercase alphabetic characters.
- You may assume no duplicates in the word list.
- You may assume beginWord and endWord are non-empty and are not the same.
- */
-
 public class _127 {
     public static class Solution1 {
 
