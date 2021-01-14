e01aceda3eb32b41109cb9c185d5503fb672e758
Ashmi Chheda <ashmichheda@gmail.com>
2021-01-14 12:25:30 -0700

    added solution and test case for 151 (#148)

--- src/test/java/com/fishercoder/_151Test.java
+++ src/test/java/com/fishercoder/_151Test.java
@@ -8,11 +8,14 @@ import static org.junit.Assert.assertEquals;
 
 public class _151Test {
   private static _151.Solution1 solution1;
+  private static _151.Solution2 solution2;
   private static String s;
 
   @BeforeClass
-  public static void setup() {
+  public static void setup()
+  {
     solution1 = new _151.Solution1();
+    solution2 = new _151.Solution2();
   }
 
   @Test
@@ -37,5 +40,11 @@ public class _151Test {
   public void test4() {
     s = "a b  c";
     assertEquals("c b a", solution1.reverseWords(s));
+  }
+
+  @Test
+  public void test5() {
+    s = "  hello world  ";
+    assertEquals("world hello", solution2.reverseWords(s));
   }
 }
