0bcda053a4b09179e1bfcaf26c2fee1f3387241f
Fisher Coder <fishercoder@gmail.com>
2021-01-31 19:33:57 -0800

    fix build

--- src/main/java/com/fishercoder/solutions/_1745.java
+++ src/main/java/com/fishercoder/solutions/_1745.java
@@ -16,20 +16,20 @@ public class _1745 {
             for (int i = n - 1; i >= 0; i--) {
                 for (int j = i; j < n; j++) {
                     if (s.charAt(i) == s.charAt(j)) {
-                        dp[i][j] = (i + 1 < j - 1) ? dp[i + 1][j - 1] : true;
+                        dp[i][j] = (i + 1 <= j - 1) ? dp[i + 1][j - 1] : true;
                     } else {
                         dp[i][j] = false;
                     }
                 }
             }
             for (int i = 1; i < n - 1; i++) {
-                for (int j = 1; j < n - 1; j++) {
+                for (int j = i; j < n - 1; j++) {
                     if (dp[0][i - 1] && dp[i][j] && dp[j + 1][n - 1]) {
                         return true;
                     }
                 }
             }
-            return true;
+            return false;
         }
 
     }
