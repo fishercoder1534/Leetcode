e4b65534e486777e94a1da4759f867c32357294f
Fisher Coder <fishercoder@gmail.com>
2021-02-01 07:27:44 -0800

    refactor 191

--- src/main/java/com/fishercoder/solutions/_191.java
+++ src/main/java/com/fishercoder/solutions/_191.java
@@ -1,17 +1,13 @@
 package com.fishercoder.solutions;
 
-/**
- * 191. Number of 1 Bits
- *
- * Write a function that takes an unsigned integer and returns the number of ’1' bits it has (also known as the Hamming weight).
- * For example, the 32-bit integer ’11' has binary representation 00000000000000000000000000001011, so the function should return 3.*/
-
 public class _191 {
 
     public static class Solution1 {
-        /**Doing bitwise AND operation between n and n-1 will always flip the least significant 1 bit in n to zero
-         example run for the above editorial solution: input 5, n will be 5&4 and becomes 4,
-         then in the next run, n will become 4&3 which is 0, thus exit the while loop.*/
+        /**
+         * Doing bitwise AND operation between n and n-1 will always flip the least significant 1 bit in n to zero
+         * example run for the above editorial solution: input 5, n will be 5&4 and becomes 4,
+         * then in the next run, n will become 4&3 which is 0, thus exit the while loop.
+         */
         public int hammingWeight(int n) {
             int bits = 0;
             while (n != 0) {
