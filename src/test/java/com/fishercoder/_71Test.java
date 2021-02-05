8177dd1c91682d8e7f186b9362419f234b84cafa
Fisher Coder <fishercoder@gmail.com>
2021-02-05 14:29:21 -0800

    add test cases for 71

--- src/test/java/com/fishercoder/_71Test.java
+++ src/test/java/com/fishercoder/_71Test.java
@@ -7,20 +7,30 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class _71Test {
-  private static _71.Solution1 solution1;
+    private static _71.Solution1 solution1;
 
-  @BeforeClass
-  public static void setup() {
-    solution1 = new _71.Solution1();
-  }
+    @BeforeClass
+    public static void setup() {
+        solution1 = new _71.Solution1();
+    }
 
-  @Test
-  public void test1() {
-    assertEquals("/home", solution1.simplifyPath("/home/"));
-  }
+    @Test
+    public void test1() {
+        assertEquals("/home", solution1.simplifyPath("/home/"));
+    }
 
-  @Test
-  public void test2() {
-    assertEquals("/c", solution1.simplifyPath("/a/./b/../../c/"));
-  }
+    @Test
+    public void test2() {
+        assertEquals("/c", solution1.simplifyPath("/a/./b/../../c/"));
+    }
+
+    @Test
+    public void test3() {
+        assertEquals("/a/b/c", solution1.simplifyPath("/a//b////c/d//././/.."));
+    }
+
+    @Test
+    public void test4() {
+        assertEquals("/", solution1.simplifyPath("/."));
+    }
 }
