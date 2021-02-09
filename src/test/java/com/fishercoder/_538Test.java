c5242b1fa4fe41fc57b1fdf799d38593445b7bef
Fisher Coder <fishercoder@gmail.com>
2021-02-09 08:02:57 -0800

    refactor 538 test

--- src/test/java/com/fishercoder/_538Test.java
+++ src/test/java/com/fishercoder/_538Test.java
@@ -9,8 +9,8 @@ import org.junit.Test;
 import static junit.framework.Assert.assertEquals;
 
 public class _538Test {
-    private static _538.Solution2 solution2;
     private static _538.Solution1 solution1;
+    private static _538.Solution2 solution2;
     private static TreeNode expectedRoot;
     private static TreeNode root;
 
@@ -18,10 +18,6 @@ public class _538Test {
     public static void setup() {
         solution1 = new _538.Solution1();
         solution2 = new _538.Solution2();
-    }
-
-    @Before
-    public void setupForEachTest() {
     }
 
     @Test
@@ -32,15 +28,15 @@ public class _538Test {
         expectedRoot = new TreeNode(18);
         expectedRoot.left = new TreeNode(20);
         expectedRoot.right = new TreeNode(13);
-        assertEquals(expectedRoot.toString(), solution2.convertBST(root).toString());
         assertEquals(expectedRoot.toString(), solution1.convertBST(root).toString());
+        assertEquals(expectedRoot.toString(), solution2.convertBST(root).toString());
     }
 
     @Test
     public void test2() {
         root = null;
         expectedRoot = null;
-        assertEquals(expectedRoot, solution2.convertBST(root));
         assertEquals(expectedRoot, solution1.convertBST(root));
+        assertEquals(expectedRoot, solution2.convertBST(root));
     }
 }
