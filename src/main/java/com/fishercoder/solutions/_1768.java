42a6b111657c491d4cf48ce94b78beff282c7485
Fisher Coder <fishercoder@gmail.com>
2021-02-20 20:38:41 -0800

    fix build

--- src/main/java/com/fishercoder/solutions/_1768.java
+++ src/main/java/com/fishercoder/solutions/_1768.java
@@ -4,7 +4,8 @@ public class _1768 {
     public static class Solution1 {
         public String mergeAlternately(String word1, String word2) {
             StringBuilder sb = new StringBuilder();
-            int i = 0, j = 0;
+            int i = 0;
+            int j = 0;
             for (; i < word1.length() && j < word2.length(); ) {
                 sb.append(word1.charAt(i++));
                 sb.append(word2.charAt(j++));
