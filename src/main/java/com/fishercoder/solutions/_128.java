dbf2cd07ce97517c5f8c12d56b7e136ce19b2875
ashmichheda <ashmichheda@gmail.com>
2021-02-23 14:57:25 -0700

    added solution and test case for 128

--- src/main/java/com/fishercoder/solutions/_128.java
+++ src/main/java/com/fishercoder/solutions/_128.java
@@ -113,4 +113,28 @@ public class _128 {
       return max;
     }
   }
+
+  public static class Solution3 {
+    public int longestConsecutive(int[] nums) {
+      HashSet<Integer> numSet = new HashSet<>();
+      for (int num : nums) {
+        numSet.add(num);
+      }
+
+      int longestStreak = 0;
+      for (int num : nums) {
+        if (!numSet.contains(num - 1)) {
+          int currentNum = num;
+          int currentStreak = 1;
+
+          while (numSet.contains(currentNum + 1)) {
+            currentNum += 1;
+            currentStreak += 1;
+          }
+          longestStreak = Math.max(longestStreak, currentStreak);
+        }
+      }
+      return longestStreak;
+    }
+  }
 }
