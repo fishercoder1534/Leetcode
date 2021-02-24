a9cf14578040c3b49f42035e862022f683f3d044
Fisher Coder <fishercoder@gmail.com>
2021-02-24 08:37:58 -0800

    refactor 856

--- src/main/java/com/fishercoder/solutions/_856.java
+++ src/main/java/com/fishercoder/solutions/_856.java
@@ -11,13 +11,13 @@ public class _856 {
             Stack<Integer> stack = new Stack<>();
             for (int i = 0; i < S.length(); i++) {
                 if (S.charAt(i) == '(') {
-                    stack.push(-1);
+                    stack.push(-1);//we use -1 to indicate this is a left paren '('
                 } else {
                     int curr = 0;
                     while (stack.peek() != -1) {
                         curr += stack.pop();
                     }
-                    stack.pop();
+                    stack.pop();//this is to push the '(' off of the stack
                     stack.push(curr == 0 ? 1 : curr * 2);
                 }
             }
