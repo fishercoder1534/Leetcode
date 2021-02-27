98f2363109f267feacf3731221eb71058c3c7c29
Ashmi Chheda <ashmichheda@gmail.com>
2021-02-26 18:37:26 -0700

    added iterative solution for 25 and new test cases (#152)

--- src/test/java/com/fishercoder/_25Test.java
+++ src/test/java/com/fishercoder/_25Test.java
@@ -9,14 +9,16 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class _25Test {
-    private static _25 test;
+    private static _25.Solution1 test;
+    private static _25.Solution2 test2;
     private static ListNode actual;
     private static ListNode expected;
     private static ListNode head;
 
     @BeforeClass
     public static void setup() {
-        test = new _25();
+        test = new _25.Solution1();
+        test2 = new _25.Solution2();
     }
 
     @Test
@@ -26,5 +28,11 @@ public class _25Test {
         expected = LinkedListUtils.contructLinkedList(new int[]{2, 1, 4, 3, 5});
         assertEquals(actual, expected);
     }
-
+    @Test
+    public void test2() {
+        head = LinkedListUtils.contructLinkedList(new int[]{1, 2, 3, 4, 5, 6, 7});
+        actual = test2.reverseKGroup(head, 4);
+        expected = LinkedListUtils.contructLinkedList(new int[]{4, 3, 2, 1, 5, 6, 7});
+        assertEquals(actual, expected);
+    }
 }
