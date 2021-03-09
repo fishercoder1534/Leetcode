f3672ac2a7a8a81f8e235f6289bb3be937a1dd02
Fisher Coder <fishercoder@gmail.com>
2021-03-09 11:33:16 -0800

    refactor 623

--- src/test/java/com/fishercoder/_623Test.java
+++ src/test/java/com/fishercoder/_623Test.java
@@ -1,9 +1,7 @@
 package com.fishercoder;
 
 import com.fishercoder.common.classes.TreeNode;
-import com.fishercoder.common.utils.CommonUtils;
 import com.fishercoder.common.utils.TreeUtils;
-import com.fishercoder.solutions._1727;
 import com.fishercoder.solutions._623;
 import org.junit.BeforeClass;
 import org.junit.Test;
