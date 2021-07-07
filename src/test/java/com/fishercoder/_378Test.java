58f4bfae0570a44ea4e39f489a5d38cf60c24e6c
Fisher Coder <fishercoder@gmail.com>
2021-07-07 10:20:55 -0700

    add a solution for 378

--- src/test/java/com/fishercoder/_378Test.java
+++ src/test/java/com/fishercoder/_378Test.java
@@ -10,12 +10,14 @@ import static org.junit.Assert.assertEquals;
 public class _378Test {
     private static _378.Solution1 solution1;
     private static _378.Solution2 solution2;
+    private static _378.Solution3 solution3;
     private static int[][] matrix;
 
     @BeforeClass
     public static void setup() {
         solution1 = new _378.Solution1();
         solution2 = new _378.Solution2();
+        solution3 = new _378.Solution3();
     }
 
     @Test
@@ -25,6 +27,7 @@ public class _378Test {
         };
         assertEquals(-5, solution1.kthSmallest(matrix, 1));
         assertEquals(-5, solution2.kthSmallest(matrix, 1));
+        assertEquals(-5, solution3.kthSmallest(matrix, 1));
     }
 
     @Test
@@ -32,6 +35,15 @@ public class _378Test {
         matrix = CommonUtils.convertLeetCodeIrregularLengths2DArrayInputIntoJavaArray("[1,2],[1,3]");
         assertEquals(1, solution1.kthSmallest(matrix, 2));
         assertEquals(1, solution2.kthSmallest(matrix, 2));
+        assertEquals(1, solution3.kthSmallest(matrix, 2));
+    }
+
+    @Test
+    public void test3() {
+        matrix = CommonUtils.convertLeetCodeIrregularLengths2DArrayInputIntoJavaArray("[1,5,9],[10,11,13],[12,13,15]");
+        assertEquals(13, solution1.kthSmallest(matrix, 8));
+        assertEquals(13, solution2.kthSmallest(matrix, 8));
+        assertEquals(13, solution3.kthSmallest(matrix, 8));
     }
 
 }
