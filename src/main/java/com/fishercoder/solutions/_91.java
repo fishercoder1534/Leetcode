e38f140f8533f58059ccfe6e2c64e64860a99997
Fisher Coder <fishercoder@gmail.com>
2021-07-14 13:04:09 -0700

    update 91

--- src/main/java/com/fishercoder/solutions/_91.java
+++ src/main/java/com/fishercoder/solutions/_91.java
@@ -1,5 +1,8 @@
 package com.fishercoder.solutions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class _91 {
     /**
      * Credit: https://discuss.leetcode.com/topic/35840/java-clean-dp-solution-with-explanation
@@ -29,6 +32,42 @@ public class _91 {
                 }
             }
             return dp[s.length()];
+        }
+    }
+
+    public static class Solution2 {
+        /**credit: https://leetcode.com/problems/decode-ways/solution/
+         * Approach 1: Recursive Approach with Memoization
+         *
+         * The actual code goes from the right most character to the left side to build out the dp cache map.
+         * And this HashMap uses index as its key instead of a substring.
+         * */
+
+        public int numDecodings(String s) {
+            return dp(new HashMap<>(), s, 0);
+        }
+
+        private int dp(Map<Integer, Integer> cache, String s, int index) {
+            if (cache.containsKey(index)) {
+                return cache.get(index);
+            }
+            if (index == s.length()) {
+                //this means we reached the end of the string, so return 1 as success
+                return 1;
+            }
+            if (s.charAt(index) == '0') {
+                //this means this string cannot be decoded
+                return 0;
+            }
+            if (index == s.length() - 1) {
+                return 1;
+            }
+            int ways = dp(cache, s, index + 1);
+            if (Integer.parseInt(s.substring(index, index + 2)) <= 26) {
+                ways += dp(cache, s, index + 2);
+            }
+            cache.put(index, ways);
+            return cache.get(index);
         }
     }
 }
