090803fb2191d2bea652ed1345893a6221f712c3
Fisher Coder <fishercoder@gmail.com>
2021-08-16 06:43:04 -0700

    add 303

--- src/main/java/com/fishercoder/solutions/_303.java
+++ src/main/java/com/fishercoder/solutions/_303.java
@@ -23,8 +23,3 @@ public class _303 {
         }
     }
 }
-
-// Your NumArray object will be instantiated and called as such:
-// NumArray numArray = new NumArray(nums);
-// numArray.sumRange(0, 1);
-// numArray.sumRange(1, 2);
