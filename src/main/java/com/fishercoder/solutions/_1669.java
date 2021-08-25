06e4d7e85f7bbe15c6e2de39a82892c43f260c0a
ashmichheda <ashmichheda@gmail.com>
2021-08-24 21:24:03 -0700

    fix: add solution and testcase for 1669

--- src/main/java/com/fishercoder/solutions/_1669.java
+++ src/main/java/com/fishercoder/solutions/_1669.java
@@ -1,4 +1,26 @@
 package com.fishercoder.solutions;
 
+import com.fishercoder.common.classes.ListNode;
+
 public class _1669 {
+    public static class Solution1 {
+        public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+            ListNode endList = list1;
+            ListNode startList = null;
+
+            for (int i = 0; i < b; i++, endList = endList.next) {
+                if (i == a - 1) {
+                    startList = endList;
+                }
+            }
+            // Connect the startList.next to list2
+            startList.next = list2;
+            while (list2.next != null) {
+                list2 = list2.next;
+            }
+            list2.next = endList.next;
+            endList.next = null;
+            return list1;
+        }
+    }
 }
--- src/test/java/com/fishercoder/_1669Test.java
+++ src/test/java/com/fishercoder/_1669Test.java
@@ -1,4 +1,31 @@
 package com.fishercoder;
 
+import com.fishercoder.common.classes.ListNode;
+import com.fishercoder.solutions._1669;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
 public class _1669Test {
+    private static _1669.Solution1 solution1;
+    private static ListNode l1;
+    private static ListNode l2;
+    private static int a;
+    private static int b;
+
+    @BeforeClass
+    public static void setup() {
+        solution1 = new _1669.Solution1();
+    }
+    @Test
+    public void test1() {
+        l1 = ListNode.createSinglyLinkedList(Arrays.asList(0, 1, 2, 3, 4, 5));
+        l2 = ListNode.createSinglyLinkedList(Arrays.asList(1000000,1000001,1000002));
+        a = 3;
+        b = 4;
+        assertEquals(ListNode.createSinglyLinkedList(Arrays.asList(0,1,2,1000000,1000001,1000002,5)), solution1.mergeInBetween(l1, a, b, l2));
+    }
 }
