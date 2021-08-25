0ee163bd065c41ed54cff3c737c5951beca3acd5
Ashmi Chheda <ashmichheda@gmail.com>
2021-08-25 09:49:16 -0700

    Add solution and testcase for 1669 (#157)
    * fix: add solution and testcase for 1669
    
    * fix: add solution and testcase for 1669

--- src/test/java/com/fishercoder/_1669Test.java
+++ src/test/java/com/fishercoder/_1669Test.java
@@ -2,16 +2,18 @@ package com.fishercoder;
 
 import com.fishercoder.common.classes.ListNode;
 import com.fishercoder.common.utils.LinkedListUtils;
-import com.fishercoder.solutions._1;
-import com.fishercoder.solutions._1669;
 import org.junit.BeforeClass;
+import org.junit.Test;import org.junit.BeforeClass;
 import org.junit.Test;
-
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
+import com.fishercoder.solutions._1669;
 
 public class _1669Test {
     private static _1669.Solution1 solution1;
+    private static _1669.Solution1 solution2;
+    private static ListNode l1;
+    private static ListNode l2;
+    private static int a;
+    private static int b;
     private static ListNode list1;
     private static ListNode list2;
     private static ListNode expected;
@@ -20,6 +22,7 @@ public class _1669Test {
     @BeforeClass
     public static void setup() {
         solution1 = new _1669.Solution1();
+        solution2 = new _1669.Solution2();
     }
 
     @Test
@@ -31,5 +34,12 @@ public class _1669Test {
         LinkedListUtils.printList(actual);
         assertEquals(expected, actual);
     }
-
+    @Test
+    public void test2() {
+        l1 = ListNode.createSinglyLinkedList(Arrays.asList(0, 1, 2, 3, 4, 5));
+        l2 = ListNode.createSinglyLinkedList(Arrays.asList(1000000,1000001,1000002));
+        a = 3;
+        b = 4;
+        assertEquals(ListNode.createSinglyLinkedList(Arrays.asList(0,1,2,1000000,1000001,1000002,5)), solution2.mergeInBetween(l1, a, b, l2));
+    }
 }
