59d19a27a8d7824ce9199f9d4a9608968e757085
Ashmi Chheda <ashmichheda@gmail.com>
2021-08-28 07:57:03 -0700

    Add solution and test case for 1721 (#158)
    * added solution and test case for 1721
    
    * merged with master
    
    * removed comments and initialized variables on new lines
    
    * add solution and testcase for 1721

--- src/test/java/com/fishercoder/_1721Test.java
+++ src/test/java/com/fishercoder/_1721Test.java
@@ -9,8 +9,9 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class _1721Test {
-    private static _1721.Solution1 solution1;
     private static _1721.Solution2 solution2;
+    private static _1721.Solution3 solution3;
+    private static _1721.Solution1 solution1;
     private static ListNode expected;
     private static ListNode node;
     private static int k;
@@ -19,16 +20,26 @@ public class _1721Test {
     public static void setup() {
         solution1 = new _1721.Solution1();
         solution2 = new _1721.Solution2();
+        solution3 = new _1721.Solution3();
     }
 
     @Test
     public void test1() {
-        node = LinkedListUtils.contructLinkedList(new int[]{1, 2, 3, 4, 5});
-        expected = LinkedListUtils.contructLinkedList(new int[]{1, 4, 3, 2, 5});
-        k = 2;
-        assertEquals(expected, solution1.swapNodes(node, k));
-    }
+        node = new ListNode(1);
+        node.next = new ListNode(2);
+        node.next.next = new ListNode(3);
+        node.next.next.next = new ListNode(4);
+        node.next.next.next.next = new ListNode(5);
 
+        expected = new ListNode(1);
+        expected.next = new ListNode(4);
+        expected.next.next = new ListNode(3);
+        expected.next.next.next = new ListNode(2);
+        expected.next.next.next.next = new ListNode(5);
+
+        k = 2;
+        assertEquals(expected, solution2.swapNodes(node, k));
+    }
     @Test
     public void test2() {
         node = LinkedListUtils.contructLinkedList(new int[]{1, 2, 3, 4, 5});
@@ -36,4 +47,11 @@ public class _1721Test {
         k = 2;
         assertEquals(expected, solution2.swapNodes(node, k));
     }
+    @Test
+      public void test3(){
+          node = LinkedListUtils.contructLinkedList(new int[]{90, 100});
+          k = 2;
+          expected = LinkedListUtils.contructLinkedList(new int[]{100, 90});
+          assertEquals(expected, solution3.swapNodes(node, k));
+      }
 }
