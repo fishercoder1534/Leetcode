eee5a9303d910ca038733c611e359d7c3a677a2d
Fisher Coder <fishercoder@gmail.com>
2021-08-30 08:37:24 -0700

    add 598

--- src/main/java/com/fishercoder/solutions/_598.java
+++ src/main/java/com/fishercoder/solutions/_598.java
@@ -3,6 +3,10 @@ package com.fishercoder.solutions;
 public class _598 {
 
     public static class Solution1 {
+        /**
+         * Since the incrementing starts from zero to op[0] and op[1], we only need to find the range that has the most overlaps.
+         * Thus we keep finding the minimum of both x and y.
+         */
         public int maxCount(int m, int n, int[][] ops) {
             int x = m;
             int y = n;
