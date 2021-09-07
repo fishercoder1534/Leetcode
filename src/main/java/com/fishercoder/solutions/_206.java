426a50d00e03aa0824cda72134611c5d31b3d02c
Fisher Coder <fishercoder@gmail.com>
2021-09-07 07:33:53 -0700

    refactor 206

--- src/main/java/com/fishercoder/solutions/_206.java
+++ src/main/java/com/fishercoder/solutions/_206.java
@@ -2,18 +2,6 @@ package com.fishercoder.solutions;
 
 import com.fishercoder.common.classes.ListNode;
 
-/**
- * 206. Reverse Linked List
- *
- * Reverse a singly linked list.
- *
- * Example:
- * Input: 1->2->3->4->5->NULL
- * Output: 5->4->3->2->1->NULL
- *
- * Follow up:
- * A linked list can be reversed either iteratively or recursively. Could you implement both?
- * */
 public class _206 {
 
     public static class Solution1 {
