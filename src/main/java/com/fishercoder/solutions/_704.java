c20f58d353008d37fa71b109218ab2c6da51c498
Fisher Coder <fishercoder@gmail.com>
2021-09-12 10:48:42 -0700

    add a solution for 704

--- src/main/java/com/fishercoder/solutions/_704.java
+++ src/main/java/com/fishercoder/solutions/_704.java
@@ -26,4 +26,22 @@ public class _704 {
             return -1;
         }
     }
+
+    public static class Solution2 {
+        public int search(int[] nums, int target) {
+            int left = 0;
+            int right = nums.length - 1;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] == target) {
+                    return mid;
+                } else if (nums[mid] < target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            return nums[left] == target ? left : (right >= 0 && nums[right] == target) ? right : -1;
+        }
+    }
 }
--- src/test/java/com/fishercoder/_704Test.java
+++ src/test/java/com/fishercoder/_704Test.java
@@ -7,41 +7,50 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class _704Test {
-  private static _704.Solution1 solution1;
-  private static int[] nums;
+    private static _704.Solution1 solution1;
+    private static _704.Solution2 solution2;
+    private static int[] nums;
 
-  @BeforeClass
-  public static void setup() {
-    solution1 = new _704.Solution1();
-  }
+    @BeforeClass
+    public static void setup() {
+        solution1 = new _704.Solution1();
+        solution2 = new _704.Solution2();
+    }
 
-  @Test
-  public void test1() {
-    nums = new int[] {-1, 0, 3, 5, 9, 12};
-    assertEquals(4, solution1.search(nums, 9));
-  }
+    @Test
+    public void test1() {
+        nums = new int[]{-1, 0, 3, 5, 9, 12};
+        assertEquals(4, solution1.search(nums, 9));
+    }
 
-  @Test
-  public void test2() {
-    nums = new int[] {-1, 0, 3, 5, 9, 12};
-    assertEquals(-1, solution1.search(nums, 2));
-  }
+    @Test
+    public void test2() {
+        nums = new int[]{-1, 0, 3, 5, 9, 12};
+        assertEquals(-1, solution1.search(nums, 2));
+    }
 
-  @Test
-  public void test3() {
-    nums = new int[] {5};
-    assertEquals(0, solution1.search(nums, 5));
-  }
+    @Test
+    public void test3() {
+        nums = new int[]{5};
+        assertEquals(0, solution1.search(nums, 5));
+        assertEquals(0, solution2.search(nums, 5));
+    }
 
-  @Test
-  public void test4() {
-    nums = new int[] {-1, 0};
-    assertEquals(1, solution1.search(nums, 0));
-  }
+    @Test
+    public void test4() {
+        nums = new int[]{-1, 0};
+        assertEquals(1, solution1.search(nums, 0));
+    }
 
-  @Test
-  public void test5() {
-    nums = new int[] {-1, 0, 3, 5, 9, 12};
-    assertEquals(1, solution1.search(nums, 0));
-  }
+    @Test
+    public void test5() {
+        nums = new int[]{-1, 0, 3, 5, 9, 12};
+        assertEquals(1, solution1.search(nums, 0));
+    }
+
+    @Test
+    public void test6() {
+        nums = new int[]{2, 5};
+        assertEquals(-1, solution2.search(nums, 0));
+    }
 }
