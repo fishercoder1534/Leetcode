e1b93d67c16f025d6bef60acc3fb152b7e7124e2
Fisher Coder <fishercoder@gmail.com>
2021-09-12 11:26:09 -0700

    refactor 53

--- src/main/java/com/fishercoder/solutions/_53.java
+++ src/main/java/com/fishercoder/solutions/_53.java
@@ -7,13 +7,13 @@ public class _53 {
          * https://leetcode.com/problems/maximum-subarray/discuss/20211/accepted-on-solution-in-java
          */
         public int maxSubArray(int[] nums) {
-            int maxSoFar = nums[0];
-            int maxEndingHere = nums[0];
+            int globalMax = nums[0];
+            int currentMax = nums[0];
             for (int i = 1; i < nums.length; i++) {
-                maxEndingHere = Math.max(nums[i], maxEndingHere + nums[i]);
-                maxSoFar = Math.max(maxEndingHere, maxSoFar);
+                currentMax = Math.max(nums[i], currentMax + nums[i]);
+                globalMax = Math.max(currentMax, globalMax);
             }
-            return maxSoFar;
+            return globalMax;
         }
     }
 }
