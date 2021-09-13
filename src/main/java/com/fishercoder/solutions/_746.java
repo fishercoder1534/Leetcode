91b52fd0048986851beee5af6e19fcae74ee5578
Fisher Coder <fishercoder@gmail.com>
2021-09-13 07:36:20 -0700

    refactor 746

--- src/main/java/com/fishercoder/solutions/_746.java
+++ src/main/java/com/fishercoder/solutions/_746.java
@@ -7,7 +7,7 @@ public class _746 {
             dp[0] = cost[0];
             dp[1] = cost[1];
             for (int i = 2; i < cost.length; i++) {
-                dp[i] = Math.min(dp[i - 1] + cost[i], dp[i - 2] + cost[i]);
+                dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
             }
             return Math.min(dp[cost.length - 1], dp[cost.length - 2]);
         }
