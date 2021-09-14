8010a74eb2bc141fa053a7b8f5ac071255f52ca9
Fisher Coder <fishercoder@gmail.com>
2021-09-14 14:38:07 -0700

    refactor 283

--- src/main/java/com/fishercoder/solutions/_283.java
+++ src/main/java/com/fishercoder/solutions/_283.java
@@ -19,7 +19,7 @@ public class _283 {
 
     public static class Solution2 {
         public void moveZeroes(int[] nums) {
-            //this solutoin is the most optimal since it minimizes the number of operations
+            //this solution is the most optimal since it minimizes the number of operations
             //the idea is to swap the non-zero element to the first zero number position
             for (int i = 0, j = 0; i < nums.length && j < nums.length; j++) {
                 if (nums[j] != 0) {
