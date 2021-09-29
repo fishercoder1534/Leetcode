13cb0fdf1517cc9b4f39975666f2e8985149b0cc
Fisher Coder <fishercoder@gmail.com>
2021-09-29 09:14:03 -0700

    refactor 725

--- src/main/java/com/fishercoder/solutions/_725.java
+++ src/main/java/com/fishercoder/solutions/_725.java
@@ -7,18 +7,18 @@ public class _725 {
         /**
          * My very original solution, but verbose.
          */
-        public ListNode[] splitListToParts(ListNode root, int k) {
-            int len = getLength(root);
+        public ListNode[] splitListToParts(ListNode head, int k) {
+            int len = getLength(head);
             int aveSize = len / k;
             int extra = len % k;
             ListNode[] result = new ListNode[k];
             for (int i = 0; i < k; i++) {
-                result[i] = root;
+                result[i] = head;
                 int aveSizeTmp = aveSize;
                 aveSizeTmp += extra-- > 0 ? 1 : 0;
                 int aveSizeTmp2 = aveSizeTmp;
                 while (aveSizeTmp-- > 0) {
-                    root = root.next;
+                    head = head.next;
                 }
                 if (result[i] != null) {
                     ListNode tmp = result[i];
@@ -46,17 +46,17 @@ public class _725 {
         /**
          * More concise version
          */
-        public ListNode[] splitListToParts(ListNode root, int k) {
-            int len = getLength(root);
+        public ListNode[] splitListToParts(ListNode head, int k) {
+            int len = getLength(head);
             int aveSize = len / k;
             int extra = len % k;
             ListNode[] result = new ListNode[k];
             ListNode prev = null;
             for (int i = 0; i < k; i++, extra--) {
-                result[i] = root;
+                result[i] = head;
                 for (int j = 0; j < aveSize + (extra > 0 ? 1 : 0); j++) {
-                    prev = root;
-                    root = root.next;
+                    prev = head;
+                    head = head.next;
                 }
                 if (prev != null) {
                     prev.next = null;
@@ -73,6 +73,46 @@ public class _725 {
                 tmp = tmp.next;
             }
             return len;
+        }
+    }
+
+    public static class Solution3 {
+        /**
+         * My original solution on 9/29/2021.
+         */
+        public ListNode[] splitListToParts(ListNode head, int k) {
+            ListNode[] ans = new ListNode[k];
+            int size = 0;
+            ListNode tmp = head;
+            while (tmp != null) {
+                tmp = tmp.next;
+                size++;
+            }
+            int minSize = size / k;
+            int remainder = size % k;
+            int i = 0;
+            if (head == null) {
+                while (i < k) {
+                    ans[i++] = null;
+                }
+            }
+            while (i < k) {
+                ListNode node = head;
+                tmp = node;
+                int len = minSize;
+                if (remainder > 0) {
+                    remainder--;
+                    len++;
+                }
+                while (len-- > 1) {
+                    tmp = tmp.next;
+                }
+                head = tmp.next;
+                tmp.next = null;
+                ans[i] = node;
+                i++;
+            }
+            return ans;
         }
     }
 }
