829f5a55c3231531568cd4d511956e359c6406ee
Fisher Coder <fishercoder@gmail.com>
2021-10-10 16:41:26 -0700

    refactor 116

--- src/test/java/com/fishercoder/_116Test.java
+++ src/test/java/com/fishercoder/_116Test.java
@@ -6,23 +6,37 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class _116Test {
-  private static _116.Solution1 solution1;
-  private static TreeLinkNode root;
+    private static _116.Solution1 solution1;
+    private static _116.Solution2 solution2;
+    private static TreeLinkNode root;
 
-  @BeforeClass
-  public static void setup() {
-    solution1 = new _116.Solution1();
-  }
+    @BeforeClass
+    public static void setup() {
+        solution1 = new _116.Solution1();
+        solution2 = new _116.Solution2();
+    }
 
-  @Test
-  public void test1() {
-    root = new TreeLinkNode(1);
-    root.left = new TreeLinkNode(2);
-    root.right = new TreeLinkNode(3);
-    root.left.left = new TreeLinkNode(4);
-    root.left.right = new TreeLinkNode(5);
-    root.right.right = new TreeLinkNode(7);
+    @Test
+    public void test1() {
+        root = new TreeLinkNode(1);
+        root.left = new TreeLinkNode(2);
+        root.right = new TreeLinkNode(3);
+        root.left.left = new TreeLinkNode(4);
+        root.left.right = new TreeLinkNode(5);
+        root.right.right = new TreeLinkNode(7);
 
-    solution1.connect(root);
-  }
+        solution1.connect(root);
+    }
+
+    @Test
+    public void test2() {
+        root = new TreeLinkNode(1);
+        root.left = new TreeLinkNode(2);
+        root.right = new TreeLinkNode(3);
+        root.left.left = new TreeLinkNode(4);
+        root.left.right = new TreeLinkNode(5);
+        root.right.right = new TreeLinkNode(7);
+
+        solution2.connect(root);
+    }
 }
