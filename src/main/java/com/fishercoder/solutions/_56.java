490edbdf846cd6fb0c1c875545b59cd2a9b2bf6f
Fisher Coder <fishercoder@gmail.com>
2021-10-13 16:51:12 -0700

    refactor 56

--- src/main/java/com/fishercoder/solutions/_56.java
+++ src/main/java/com/fishercoder/solutions/_56.java
@@ -12,7 +12,7 @@ public class _56 {
          */
         public int[][] merge(int[][] intervals) {
             List<int[]> list = new ArrayList<>();
-            Arrays.sort(intervals, (a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
+            Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));//to avoid integer subtraction overflow
             for (int i = 0; i < intervals.length; i++) {
                 int start = intervals[i][0];
                 int end = intervals[i][1];
