f0822242184fdd293869738f2cd813f555a465e8
Fisher Coder <fishercoder@gmail.com>
2021-10-13 11:25:37 -0700

    refactor 252

--- src/main/java/com/fishercoder/solutions/_252.java
+++ src/main/java/com/fishercoder/solutions/_252.java
@@ -1,29 +1,20 @@
 package com.fishercoder.solutions;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 
 public class _252 {
     public static class Solution1 {
         public boolean canAttendMeetings(int[][] intervals) {
-
-            List<int[]> list = new ArrayList();
-            for (int[] interval : intervals) {
-                list.add(interval);
+            if (intervals == null || intervals.length == 0) {
+                return true;
             }
-
-            Collections.sort(list, (o1, o2) -> {
-                if (o1[0] > o2[0]) {
-                    return 1;
-                } else {
-                    return -1;
-                }
-            });
-
-            for (int i = 0; i < list.size() - 1; i++) {
-                if (list.get(i)[1] > list.get(i + 1)[0]) {
+            Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+            int end = intervals[0][1];
+            for (int i = 1; i < intervals.length; i++) {
+                if (intervals[i][0] < end) {
                     return false;
+                } else {
+                    end = intervals[i][1];
                 }
             }
             return true;
