41e5bd53d66728b1ec7d13c76082287962ba6999
Fisher Coder <fishercoder@gmail.com>
2021-10-14 11:11:00 -0700

    refactor 415

--- src/main/java/com/fishercoder/solutions/_415.java
+++ src/main/java/com/fishercoder/solutions/_415.java
@@ -3,35 +3,30 @@ package com.fishercoder.solutions;
 public class _415 {
 
     public static class Solution1 {
+        /**
+         * My completely original solution on 10/14/2021.
+         */
         public String addStrings(String num1, String num2) {
-            if (num1 == null || num1.length() == 0) {
-                return num2;
-            } else if (num2 == null || num2.length() == 0) {
-                return num1;
-            }
-
+            StringBuilder sb = new StringBuilder();
             int i = num1.length() - 1;
             int j = num2.length() - 1;
-            long carry = 0;
-            long sum = 0;
-            StringBuilder sb = new StringBuilder();
-            char[] char1 = num1.toCharArray();
-            char[] char2 = num2.toCharArray();
+            int carry = 0;
             while (i >= 0 || j >= 0) {
-                sum = carry;
+                int sum = carry;
                 if (i >= 0) {
-                    sum += Character.getNumericValue(char1[i--]);
+                    sum += Character.getNumericValue(num1.charAt(i));
                 }
                 if (j >= 0) {
-                    sum += Character.getNumericValue(char2[j--]);
+                    sum += Character.getNumericValue(num2.charAt(j));
                 }
-                carry = sum / 10;
                 sb.append(sum % 10);
+                carry = sum / 10;
+                i--;
+                j--;
             }
-            if (carry != 0) {
+            if (carry > 0) {
                 sb.append(carry);
             }
-
             return sb.reverse().toString();
         }
     }
