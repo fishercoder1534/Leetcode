22c8dd12821f19df8c94f4571ec53127a7eba02f
Fisher Coder <fishercoder@gmail.com>
2021-10-20 07:55:55 -0700

    add a solution for 151

--- src/test/java/com/fishercoder/_151Test.java
+++ src/test/java/com/fishercoder/_151Test.java
@@ -9,12 +9,14 @@ import static org.junit.Assert.assertEquals;
 public class _151Test {
     private static _151.Solution1 solution1;
     private static _151.Solution2 solution2;
+    private static _151.Solution3 solution3;
     private static String s;
 
     @BeforeClass
     public static void setup() {
         solution1 = new _151.Solution1();
         solution2 = new _151.Solution2();
+        solution3 = new _151.Solution3();
     }
 
     @Test
@@ -45,5 +47,13 @@ public class _151Test {
     public void test5() {
         s = "  hello world  ";
         assertEquals("world hello", solution2.reverseWords(s));
+        assertEquals("world hello", solution3.reverseWords(s));
+    }
+
+    @Test
+    public void test6() {
+        s = "Bob    Loves  Alice   ";
+        assertEquals("Alice Loves Bob", solution2.reverseWords(s));
+        assertEquals("Alice Loves Bob", solution3.reverseWords(s));
     }
 }
