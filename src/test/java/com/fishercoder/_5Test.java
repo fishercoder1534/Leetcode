4b094855a22f9ac31cfd0fb8efc078f6128ac53a
Fisher Coder <fishercoder@gmail.com>
2021-10-21 15:14:38 -0700

    add a solution for 5

--- src/test/java/com/fishercoder/_5Test.java
+++ src/test/java/com/fishercoder/_5Test.java
@@ -8,15 +8,22 @@ import static org.junit.Assert.assertEquals;
 
 public class _5Test {
     private static _5.Solution1 solution1;
+    private static _5.Solution2 solution2;
+    private static String s;
+    private static String expected;
 
     @BeforeClass
     public static void setup() {
         solution1 = new _5.Solution1();
+        solution2 = new _5.Solution2();
     }
 
     @Test
     public void test1() {
-        assertEquals("bab", solution1.longestPalindrome("babad"));
+        s = "babad";
+        expected = "bab";
+        assertEquals(expected, solution1.longestPalindrome(s));
+        assertEquals(expected, solution2.longestPalindrome(s));
     }
 
 }
