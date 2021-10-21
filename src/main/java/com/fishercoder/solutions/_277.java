c87ddd730dabf415e450acb0fc9ff9dba1264d2d
Fisher Coder <fishercoder@gmail.com>
2021-10-21 15:38:48 -0700

    add a solution for 277

--- src/main/java/com/fishercoder/solutions/_277.java
+++ src/main/java/com/fishercoder/solutions/_277.java
@@ -19,7 +19,40 @@ public class _277 {
         }
 
         //this is a mock-up method to make IDE happy.s
-        private boolean knows(int i, int candidate) {
+        boolean knows(int i, int candidate) {
+            return false;
+        }
+    }
+
+    public static class Solution2 {
+        /**
+         * My completely original solution on 10/21/2021, which turns out to match https://leetcode.com/problems/find-the-celebrity/solution/ Solution 1.
+         * Time: O(n^2)
+         * Space: O(1)
+         */
+        public int findCelebrity(int n) {
+            for (int i = 0; i < n; i++) {
+                //check if i is the celebrity
+                int j = 0;
+                for (; j < n; j++) {
+                    if (i != j) {
+                        if (knows(i, j)) {
+                            break;
+                        }
+                        if (!knows(j, i)) {
+                            break;
+                        }
+                    }
+                }
+                if (j == n) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        //this is a mock-up method to make IDE happy.s
+        boolean knows(int i, int candidate) {
             return false;
         }
     }
