9fbb852438678b3b376ee321b1baef9aa1784632
Fisher Coder <fishercoder@gmail.com>
2021-10-24 12:44:37 -0700

    refactor 82

--- src/test/java/com/fishercoder/_82Test.java
+++ src/test/java/com/fishercoder/_82Test.java
@@ -9,26 +9,26 @@ import org.junit.Test;
 
 public class _82Test {
 
-  private static _82.Solution1 solution1;
-  private static ListNode head;
-  private static ListNode expected;
+    private static _82.Solution1 solution1;
+    private static ListNode head;
+    private static ListNode expected;
 
-  @BeforeClass
-  public static void setup() {
-    solution1 = new _82.Solution1();
-  }
+    @BeforeClass
+    public static void setup() {
+        solution1 = new _82.Solution1();
+    }
 
-  @Test
-  public void test1() {
-    head = LinkedListUtils.contructLinkedList(new int[] {1, 2, 3, 3, 4, 4, 5});
-    expected = LinkedListUtils.contructLinkedList(new int[] {1, 2, 5});
-    Assert.assertEquals(expected, solution1.deleteDuplicates(head));
-  }
+    @Test
+    public void test1() {
+        head = LinkedListUtils.contructLinkedList(new int[]{1, 2, 3, 3, 4, 4, 5});
+        expected = LinkedListUtils.contructLinkedList(new int[]{1, 2, 5});
+        Assert.assertEquals(expected, solution1.deleteDuplicates(head));
+    }
 
-  @Test
-  public void test2() {
-    head = LinkedListUtils.contructLinkedList(new int[] {1, 1, 1, 2, 3});
-    expected = LinkedListUtils.contructLinkedList(new int[] {2, 3});
-    Assert.assertEquals(expected, solution1.deleteDuplicates(head));
-  }
+    @Test
+    public void test2() {
+        head = LinkedListUtils.contructLinkedList(new int[]{1, 1, 1, 2, 3});
+        expected = LinkedListUtils.contructLinkedList(new int[]{2, 3});
+        Assert.assertEquals(expected, solution1.deleteDuplicates(head));
+    }
 }
