d5901a38d4c22fb141f211056958e73b882448ae
Fisher Coder <fishercoder@gmail.com>
2021-10-25 16:44:18 -0700

    add a solution for 206

--- src/main/java/com/fishercoder/solutions/_206.java
+++ src/main/java/com/fishercoder/solutions/_206.java
@@ -34,4 +34,23 @@ public class _206 {
         }
     }
 
+    public static class Solution3 {
+
+        /**
+         * I feel like using the variable called prev makes more sense to me on 10/25/2021
+         * since it's literally a previous node when we start reversing from the head of the list.
+         * Again, using a pen and paper to visualize the steps helps a lot to convert thoughts into code.
+         */
+        public ListNode reverseList(ListNode head) {
+            ListNode prev = null;
+            while (head != null) {
+                ListNode next = head.next;
+                head.next = prev;
+                prev = head;
+                head = next;
+            }
+            return prev;
+        }
+    }
+
 }
