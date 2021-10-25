9bc71e3727caf8030d7ae8d23c051146d9871b5c
Fisher Coder <fishercoder@gmail.com>
2021-10-25 11:48:25 -0700

    add a solution for 222

--- src/main/java/com/fishercoder/solutions/_222.java
+++ src/main/java/com/fishercoder/solutions/_222.java
@@ -37,4 +37,69 @@ public class _222 {
         }
     }
 
+    public static class Solution2 {
+        /**
+         * credit: https://leetcode.com/problems/count-complete-tree-nodes/solution/
+         */
+        public int countNodes(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            int depth = getDepth(root);
+            if (depth == 0) {
+                return 1;
+            }
+            int left = 0;
+            int right = (int) Math.pow(2, depth) - 1;
+            //here the condition needs to be not bigger than right, instead of the typical strictly smaller than right.
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                //this is to suppose the elements on the last level are numbered from 1 to Math.pow(2, d) - 1, we are using
+                //binary search here to find the right-most number
+                if (exists(root, mid, depth)) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            //number of all nodes equals all nodes in the previous level + all the nodes on the last level indicated by variable "left"
+            return (int) Math.pow(2, depth) - 1 + left;
+        }
+
+        private boolean exists(TreeNode root, int target, int depth) {
+            /**An example complete tree in this algorithm:
+             *         1
+             *        /  \
+             *       2    3
+             *      / \  /
+             *     1  2  3     (we use 1, 2, 3 at this level to indicate how this program runs instead of 4, 5, 6)
+             *
+             *     first, target is 1, we found 1 <= 1 (root), so we go to root.left, after going down to the last level (depth),
+             *     we found this target exists: node != null, we return true from this function
+             * */
+            int left = 0;
+            int right = (int) Math.pow(2, depth) - 1;
+            for (int i = 0; i < depth; i++) {
+                int mid = left + (right - left) / 2;
+                if (target <= mid) {
+                    root = root.left;
+                    right = mid;
+                } else {
+                    root = root.right;
+                    left = mid + 1;
+                }
+            }
+            return root != null;
+        }
+
+        private int getDepth(TreeNode root) {
+            int depth = 0;
+            while (root.left != null) {
+                root = root.left;
+                depth++;
+            }
+            return depth;
+        }
+    }
+
 }
