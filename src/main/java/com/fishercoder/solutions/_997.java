b9a42021f1e166cc7043c997a493c1f6885796f8
Fisher Coder <fishercoder@gmail.com>
2021-11-01 15:30:46 -0700

    add a solution for 997

--- src/main/java/com/fishercoder/solutions/_997.java
+++ src/main/java/com/fishercoder/solutions/_997.java
@@ -5,8 +5,8 @@ import java.util.Set;
 
 public class _997 {
     public static class Solution1 {
-        public int findJudge(int N, int[][] trust) {
-            int[] trustPoints = new int[N];
+        public int findJudge(int n, int[][] trust) {
+            int[] trustPoints = new int[n];
             Set<Integer> trustOthers = new HashSet<>();
             for (int[] eachTrust : trust) {
                 trustPoints[eachTrust[1] - 1]++;
@@ -14,11 +14,33 @@ public class _997 {
             }
             int judge = -1;
             for (int i = 0; i < trustPoints.length; i++) {
-                if (trustPoints[i] == N - 1 && !trustOthers.contains(i + 1)) {
+                if (trustPoints[i] == n - 1 && !trustOthers.contains(i + 1)) {
                     judge = i + 1;
                 }
             }
             return judge;
+        }
+    }
+
+    public static class Solution2 {
+        /**
+         * Credit: https://leetcode.com/problems/find-the-town-judge/solution/ solution 2
+         */
+        public int findJudge(int n, int[][] trust) {
+            if (trust.length < n - 1) {
+                return -1;
+            }
+            int[] trustScores = new int[n];
+            for (int[] t : trust) {
+                trustScores[t[1] - 1]++;
+                trustScores[t[0] - 1]--;
+            }
+            for (int i = 0; i < n; i++) {
+                if (trustScores[i] == n - 1) {
+                    return i + 1;
+                }
+            }
+            return -1;
         }
     }
 }
