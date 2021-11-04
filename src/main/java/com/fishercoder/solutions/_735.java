e338d1d16731bee07fb1dab5e3fef946364a5764
Fisher Coder <fishercoder@gmail.com>
2021-11-04 16:57:47 -0700

    add a solution for 735

--- src/main/java/com/fishercoder/solutions/_735.java
+++ src/main/java/com/fishercoder/solutions/_735.java
@@ -1,11 +1,12 @@
 package com.fishercoder.solutions;
 
-import java.util.Stack;
+import java.util.Deque;
+import java.util.LinkedList;
 
 public class _735 {
     public static class Solution1 {
         public int[] asteroidCollision(int[] asteroids) {
-            Stack<Integer> stack = new Stack();
+            Deque<Integer> stack = new LinkedList<>();
             for (int i = 0; i < asteroids.length; i++) {
                 if (!stack.isEmpty() && stack.peek() > 0 && asteroids[i] < 0) {
                     if (Math.abs(stack.peek()) < Math.abs(asteroids[i])) {
@@ -27,7 +28,7 @@ public class _735 {
             return result;
         }
 
-        private void collide(Stack<Integer> stack) {
+        private void collide(Deque<Integer> stack) {
             do {
                 Integer top = stack.pop();
                 if (!stack.isEmpty() && stack.peek() * top < 0) {
@@ -45,6 +46,45 @@ public class _735 {
                     break;
                 }
             } while (!stack.isEmpty());
+        }
+    }
+
+    public static class Solution2 {
+        /**
+         * My completely original solution on 11/5/2021.
+         */
+        public int[] asteroidCollision(int[] asteroids) {
+            Deque<Integer> stack = new LinkedList<>();
+            for (int a : asteroids) {
+                if (a > 0) {
+                    stack.addLast(a);
+                } else {
+                    if (!stack.isEmpty() && stack.peekLast() > 0) {
+                        if (stack.peekLast() > Math.abs(a)) {
+                            continue;
+                        } else if (stack.peekLast() == Math.abs(a)) {
+                            stack.pollLast();
+                        } else {
+                            while (!stack.isEmpty() && stack.peekLast() > 0 && stack.peekLast() < Math.abs(a)) {
+                                stack.pollLast();
+                            }
+                            if (!stack.isEmpty() && stack.peekLast() > 0 && stack.peekLast() == Math.abs(a)) {
+                                stack.pollLast();
+                                continue;
+                            } else if (stack.isEmpty() || stack.peekLast() < 0) {
+                                stack.addLast(a);
+                            }
+                        }
+                    } else {
+                        stack.addLast(a);
+                    }
+                }
+            }
+            int[] ans = new int[stack.size()];
+            for (int i = stack.size() - 1; i >= 0; i--) {
+                ans[i] = stack.pollLast();
+            }
+            return ans;
         }
     }
 }
