06e8e3081a2d7f14731e56e5f7a2e19632b808c9
Fisher Coder <fishercoder@gmail.com>
2021-11-24 15:01:03 -0800

    refactor 986

--- src/main/java/com/fishercoder/solutions/_986.java
+++ src/main/java/com/fishercoder/solutions/_986.java
@@ -5,27 +5,23 @@ import java.util.List;
 
 public class _986 {
     public static class Solution1 {
-        public int[][] intervalIntersection(int[][] A, int[][] B) {
+        public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
             int i = 0;
             int j = 0;
             List<int[]> list = new ArrayList<>();
-            while (i < A.length && j < B.length) {
-                int start = Math.max(A[i][0], B[j][0]);
-                int end = Math.min(A[i][1], B[j][1]);
+            while (i < firstList.length && j < secondList.length) {
+                int start = Math.max(firstList[i][0], secondList[j][0]);
+                int end = Math.min(firstList[i][1], secondList[j][1]);
                 if (start <= end) {
                     list.add(new int[]{start, end});
                 }
-                if (end == A[i][1]) {
+                if (end == firstList[i][1]) {
                     i++;
                 } else {
                     j++;
                 }
             }
-            int[][] result = new int[list.size()][2];
-            for (int k = 0; k < list.size(); k++) {
-                result[k] = list.get(k);
-            }
-            return result;
+            return list.toArray(new int[list.size()][2]);
         }
     }
 }
