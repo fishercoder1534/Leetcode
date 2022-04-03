d7d90e6b7300407aa1c324ffa39c60a0608a1146
Fisher <fishercoder@gmail.com>
2022-04-03 08:05:53 -0700

    add 2220

---
+++ src/test/java/com/fishercoder/_2220Test.java
@@ -0,0 +1,22 @@
+package com.fishercoder;
+
+import com.fishercoder.solutions._2220;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class _2220Test {
+    private static _2220.Solution1 solution1;
+
+    @BeforeClass
+    public static void setup() {
+        solution1 = new _2220.Solution1();
+    }
+
+    @Test
+    public void test1() {
+        assertEquals(3, solution1.minBitFlips(10, 7));
+    }
+
+}
