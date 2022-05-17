b53857781e02d274bf801f02ad65e846f832c10f
Fisher <fishercoder@gmail.com>
2022-05-17 07:25:50 -0700

    add a solution for 1379

--- src/main/java/com/fishercoder/solutions/_1379.java
+++ src/main/java/com/fishercoder/solutions/_1379.java
@@ -18,4 +18,23 @@ public class _1379 {
             return getTargetCopy(original.right, cloned.right, target);
         }
     }
+
+    public static class Solution2 {
+        /**
+         * My completely original solution on 5/17/2022.
+         */
+        public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+            if (original == null || cloned == null) {
+                return null;
+            }
+            if (original == target) {
+                return cloned;
+            }
+            TreeNode left = getTargetCopy(original.left, cloned.left, target);
+            if (left == null) {
+                return getTargetCopy(original.right, cloned.right, target);
+            }
+            return left;
+        }
+    }
 }
