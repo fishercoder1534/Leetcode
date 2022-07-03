6144fd58c63325f5f58c7f853eab06452df120be
Fisher <fishercoder@gmail.com>
2022-07-03 16:20:40 -0700

    add tests for 785

--- src/test/java/com/fishercoder/_785Test.java
+++ src/test/java/com/fishercoder/_785Test.java
@@ -5,6 +5,8 @@ import com.fishercoder.solutions._785;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class _785Test {
     private static _785.Solution1 solution1;
     private static int[][] graph;
@@ -19,6 +21,13 @@ public class _785Test {
     public void test1() {
         graph = CommonUtils.convertLeetCodeIrregularLengths2DArrayInputIntoJavaArray("[1,2,3],[0,2],[0,1,3],[0,2]");
         CommonUtils.print2DIntArray(graph);
-        solution1.isBipartite(graph);
+        assertEquals(false, solution1.isBipartite(graph));
+    }
+
+    @Test
+    public void test2() {
+        graph = CommonUtils.convertLeetCodeIrregularLengths2DArrayInputIntoJavaArray("[1,3],[0,2],[1,3],[0,2]");
+        CommonUtils.print2DIntArray(graph);
+        assertEquals(true, solution1.isBipartite(graph));
     }
 }
