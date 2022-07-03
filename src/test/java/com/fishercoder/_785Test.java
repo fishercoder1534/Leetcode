ff116c7522fbccd78449d525e9fd1726f5b3e779
Fisher <fishercoder@gmail.com>
2022-07-03 16:39:47 -0700

    add a solution for 785

--- src/test/java/com/fishercoder/_785Test.java
+++ src/test/java/com/fishercoder/_785Test.java
@@ -5,15 +5,18 @@ import com.fishercoder.solutions._785;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class _785Test {
     private static _785.Solution1 solution1;
+    private static _785.Solution2 solution2;
     private static int[][] graph;
 
     @BeforeClass
     public static void setup() {
         solution1 = new _785.Solution1();
+        solution2 = new _785.Solution2();
         graph = new int[][]{};
     }
 
@@ -21,13 +24,15 @@ public class _785Test {
     public void test1() {
         graph = CommonUtils.convertLeetCodeIrregularLengths2DArrayInputIntoJavaArray("[1,2,3],[0,2],[0,1,3],[0,2]");
         CommonUtils.print2DIntArray(graph);
-        assertEquals(false, solution1.isBipartite(graph));
+        assertFalse(solution1.isBipartite(graph));
+        assertFalse(solution2.isBipartite(graph));
     }
 
     @Test
     public void test2() {
         graph = CommonUtils.convertLeetCodeIrregularLengths2DArrayInputIntoJavaArray("[1,3],[0,2],[1,3],[0,2]");
         CommonUtils.print2DIntArray(graph);
-        assertEquals(true, solution1.isBipartite(graph));
+        assertTrue(solution1.isBipartite(graph));
+        assertTrue(solution2.isBipartite(graph));
     }
 }
