dd2a1e78104e65b849710e08d4eea8e4cfe6b4ed
ashmichheda <ashmichheda@gmail.com>
2022-10-30 18:35:58 -0700

    add fix for 2325

--- src/test/java/com/fishercoder/_2325Test.java
+++ src/test/java/com/fishercoder/_2325Test.java
@@ -6,20 +6,20 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class _2325Test {
-    private static _2325.Solution1 solution1;
+    private static _2325.Solution2 solution2;
     private String key;
     private String message;
 
     @BeforeClass
     public static void setup() {
-        solution1 = new _2325.Solution1();
+        solution2 = new _2325.Solution2();
     }
 
     @Test
     public void test1() {
         key = "the quick brown fox jumps over the lazy dog";
         message = "vkbs bs t suepuv";
-        String actual = solution1.decodeMessage(key, message);
+        String actual = solution2.decodeMessage(key, message);
         String expected = "this is a secret";
         Assert.assertEquals(actual, expected);
     }
