edf662181157abd42fdc170fa78e436f05758b4c
Ashmi Chheda <ashmichheda@gmail.com>
2022-10-31 08:15:38 -0700

    Add fix for problem 2325 (#173)
    * add fix for 2325
    
    * add fix for 2325

---
+++ src/test/java/com/fishercoder/_2325Test.java
@@ -0,0 +1,26 @@
+package com.fishercoder;
+
+import com.fishercoder.solutions._2325;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class _2325Test {
+    private static _2325.Solution2 solution2;
+    private String key;
+    private String message;
+
+    @BeforeClass
+    public static void setup() {
+        solution2 = new _2325.Solution2();
+    }
+
+    @Test
+    public void test1() {
+        key = "the quick brown fox jumps over the lazy dog";
+        message = "vkbs bs t suepuv";
+        String actual = solution2.decodeMessage(key, message);
+        String expected = "this is a secret";
+        Assert.assertEquals(actual, expected);
+    }
+}
