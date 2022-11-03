7b3ef502097627758c326dd8dd72d520af2b77aa
Ashmi Chheda <ashmichheda@gmail.com>
2022-11-02 18:16:32 -0700

    add fix (#174)

--- src/main/java/com/fishercoder/solutions/_1844.java
+++ src/main/java/com/fishercoder/solutions/_1844.java
@@ -14,4 +14,14 @@ public class _1844 {
             return sb.toString();
         }
     }
+    public static class Solution2 {
+        public String replaceDigits(String s) {
+
+            char inpArr[] = s.toCharArray();
+            for(int i = 1; i < inpArr.length; i+= 2) {
+                inpArr[i] = (char) (inpArr[i - 1] + inpArr[i] - '0');
+            }
+            return String.valueOf(inpArr);
+        }
+    }
 }
