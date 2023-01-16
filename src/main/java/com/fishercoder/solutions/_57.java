55fdae050bc018c472ce32e48d2957d47d94a333
Fisher Coder <fishercoder@gmail.com>
2023-01-16 08:14:52 -0800

    refactor 57

--- src/main/java/com/fishercoder/solutions/_57.java
+++ src/main/java/com/fishercoder/solutions/_57.java
@@ -10,14 +10,14 @@ public class _57 {
             List<int[]> list = new ArrayList<>();
             int i = 0;
             // add all the intervals ending before newInterval starts
-            while (i < intervals.length && intervals[i][intervals[i].length - 1] < newInterval[0]) {
+            while (i < intervals.length && intervals[i][1] < newInterval[0]) {
                 list.add(intervals[i++]);
             }
             // merge all overlapping intervals to one considering newInterval
-            while (i < intervals.length && intervals[i][0] <= newInterval[newInterval.length - 1]) {
+            while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
                 newInterval = new int[]{ // we could mutate newInterval here also
                         Math.min(newInterval[0], intervals[i][0]),
-                        Math.max(newInterval[newInterval.length - 1], intervals[i][intervals[i].length - 1])};
+                        Math.max(newInterval[1], intervals[i][1])};
                 i++;
             }
             list.add(newInterval);
@@ -25,17 +25,7 @@ public class _57 {
             while (i < intervals.length) {
                 list.add(intervals[i++]);
             }
-            return convertToArray(list);
-        }
-
-        private int[][] convertToArray(List<int[]> list) {
-            int[][] result = new int[list.size()][list.get(0).length];
-            for (int i = 0; i < list.size(); i++) {
-                for (int j = 0; j < list.get(i).length; j++) {
-                    result[i][j] = list.get(i)[j];
-                }
-            }
-            return result;
+            return list.toArray(new int[list.size()][]);
         }
     }
 }
