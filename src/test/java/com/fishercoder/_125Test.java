2318676029e95071f46d40a7159131744e57b149
Fisher Coder <fishercoder@gmail.com>
2023-01-27 19:29:21 -0800

    update 125

---
+++ src/test/java/com/fishercoder/_125Test.java
@@ -0,0 +1,41 @@
+package com.fishercoder;
+
+import com.fishercoder.solutions._125;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class _125Test {
+    private static _125.Solution1 solution1;
+    private static String s;
+
+    @BeforeClass
+    public static void setup() {
+        solution1 = new _125.Solution1();
+    }
+
+    @Test
+    public void test1() {
+        s = "A man, a plan, a canal: Panama";
+        assertEquals(true, solution1.isPalindrome(s));
+    }
+
+    @Test
+    public void test2() {
+        s = "race a car";
+        assertEquals(false, solution1.isPalindrome(s));
+    }
+
+    @Test
+    public void test3() {
+        s = " ";
+        assertEquals(true, solution1.isPalindrome(s));
+    }
+
+    @Test
+    public void test4() {
+        s = "0P";
+        assertEquals(false, solution1.isPalindrome(s));
+    }
+}
