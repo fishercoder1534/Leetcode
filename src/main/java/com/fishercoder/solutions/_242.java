5f7c51251dc38743837d8530a6ad60cf74df3aef
Fisher Coder <fishercoder@gmail.com>
2023-01-29 11:35:07 -0800

    update 242

--- src/main/java/com/fishercoder/solutions/_242.java
+++ src/main/java/com/fishercoder/solutions/_242.java
@@ -1,6 +1,8 @@
 package com.fishercoder.solutions;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class _242 {
 
@@ -27,6 +29,23 @@ public class _242 {
             for (int i : counts) {
                 if (i != 0) {
                     return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    public static class Solution3 {
+        //to deal with unicode characters
+        public boolean isAnagram(String s, String t) {
+            Map<Character, Integer> map = new HashMap<>();
+            for (int i = 0; i < s.length(); i++) {
+                map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+                map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) - 1);
+            }
+            for (char c : map.keySet()) {
+                if (map.get(c) != 0) {
+                    return true;
                 }
             }
             return true;
