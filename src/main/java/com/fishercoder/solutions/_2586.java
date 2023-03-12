158e7f02de60517a1439f582ac1036da1d414998
Fisher Coder <fishercoder@gmail.com>
2023-03-12 07:42:50 -0700

    add 2586

---
+++ src/main/java/com/fishercoder/solutions/_2586.java
@@ -0,0 +1,27 @@
+package com.fishercoder.solutions;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class _2586 {
+    public static class Solution1 {
+        public int vowelStrings(String[] words, int left, int right) {
+            int count = 0;
+            for (int i = left; i <= right; i++) {
+                if (isVowelString(words[i])) {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        private boolean isVowelString(String word) {
+            Set<Character> set = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+            if (set.contains(word.charAt(0)) && set.contains(word.charAt(word.length() - 1))) {
+                return true;
+            }
+            return false;
+        }
+    }
+}
