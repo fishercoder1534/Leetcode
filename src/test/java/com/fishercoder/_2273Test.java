0138a7b931b479c3b0dfe4ef6db55e85c1bd1533
Fisher Coder <fishercoder@gmail.com>
2023-09-04 16:42:56 -0700

    add 2273

---
+++ src/test/java/com/fishercoder/_2273Test.java
@@ -0,0 +1,24 @@
+package com.fishercoder;
+
+import com.fishercoder.solutions._2273;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+public class _2273Test {
+    private static _2273.Solution1 solution1;
+
+    @BeforeClass
+    public static void setup() {
+        solution1 = new _2273.Solution1();
+    }
+
+    @Test
+    public void test1() {
+        assertEquals(Arrays.asList("abba", "cd"), solution1.removeAnagrams(new String[]{"abba", "baba", "bbaa", "cd", "cd"}));
+    }
+
+}
