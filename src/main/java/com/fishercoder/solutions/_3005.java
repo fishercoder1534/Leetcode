2c08b1895ec59f8056570ac2364f1f508af9b3ef
Fisher Coder <fishercoder@gmail.com>
2024-01-13 20:13:58 -0800

    add 3005

---
+++ src/main/java/com/fishercoder/solutions/_3005.java
@@ -0,0 +1,28 @@
+package com.fishercoder.solutions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class _3005 {
+    public static class Solution1 {
+        public int maxFrequencyElements(int[] nums) {
+            Map<Integer, Integer> map = new HashMap<>();
+            for (int num : nums) {
+                map.put(num, map.getOrDefault(num, 0) + 1);
+            }
+            int maxFreq = 0;
+            for (int key : map.keySet()) {
+                if (map.get(key) > maxFreq) {
+                    maxFreq = map.get(key);
+                }
+            }
+            int result = 0;
+            for (int key : map.keySet()) {
+                if (map.get(key) == maxFreq) {
+                    result += map.get(key);
+                }
+            }
+            return result;
+        }
+    }
+}
