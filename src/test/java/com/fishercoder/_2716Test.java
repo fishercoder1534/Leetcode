d16893f376db5a677584fb2df4e23c500f18f22f
Fisher Coder <fishercoder@gmail.com>
2024-01-20 14:55:14 -0800

    introduce Junit5

--- src/test/java/com/fishercoder/_2716Test.java
+++ src/test/java/com/fishercoder/_2716Test.java
@@ -1,16 +1,17 @@
 package com.fishercoder;
 
 import com.fishercoder.solutions._2716;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class _2716Test {
     private static _2716.Solution1 solution1;
 
-    @BeforeClass
-    public static void setup() {
+    @BeforeEach
+    public void setup() {
         solution1 = new _2716.Solution1();
     }
 
