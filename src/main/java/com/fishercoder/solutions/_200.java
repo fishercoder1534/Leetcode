bb46ef14884914fa910bd2b5326ed79868ff1f45
Fisher Coder <fishercoder@gmail.com>
2024-01-26 17:27:32 -0800

    update 200

--- src/main/java/com/fishercoder/solutions/_200.java
+++ src/main/java/com/fishercoder/solutions/_200.java
@@ -2,31 +2,33 @@ package com.fishercoder.solutions;
 
 /**
  * 200. Number of Islands
- *
+ * <p>
  * Given a 2d grid map of '1's (land) and '0's (water),
  * count the number of islands.
  * An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
  * You may assume all four edges of the grid are all surrounded by water.
-
- Example 1:
- 11110
- 11010
- 11000
- 00000
- Answer: 1
-
- Example 2:
- 11000
- 11000
- 00100
- 00011
- Answer: 3
-
+ * <p>
+ * Example 1:
+ * 11110
+ * 11010
+ * 11000
+ * 00000
+ * Answer: 1
+ * <p>
+ * Example 2:
+ * 11000
+ * 11000
+ * 00100
+ * 00011
+ * Answer: 3
  */
 public class _200 {
 
     public static class Solution1 {
 
+        /**
+         * DFS solution, note: this modifies the input.
+         */
         public int numIslands(char[][] grid) {
             if (grid == null || grid.length == 0) {
                 return 0;
