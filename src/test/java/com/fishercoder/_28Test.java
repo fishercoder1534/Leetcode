6f3d32da6f46ab7fe54ba588a39b18bf71008137
Fisher Coder <fishercoder@gmail.com>
2024-02-06 13:38:26 -0800

    migrate _28 test to Junit5

--- src/test/java/com/fishercoder/_28Test.java
+++ src/test/java/com/fishercoder/_28Test.java
@@ -1,31 +1,31 @@
 package com.fishercoder;
 
 import com.fishercoder.solutions._28;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static junit.framework.TestCase.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class _28Test {
-  private static _28.Solution1 solution1;
+    private static _28.Solution1 solution1;
 
-  @Before
-  public void setupForEachTest() {
-    solution1 = new _28.Solution1();
-  }
+    @BeforeEach
+    public void setup() {
+        solution1 = new _28.Solution1();
+    }
 
-  @Test
-  public void test1() {
-    assertEquals(0, solution1.strStr("a", ""));
-  }
+    @Test
+    public void test1() {
+        assertEquals(0, solution1.strStr("a", ""));
+    }
 
-  @Test
-  public void test2() {
-    assertEquals(-1, solution1.strStr("mississippi", "a"));
-  }
+    @Test
+    public void test2() {
+        assertEquals(-1, solution1.strStr("mississippi", "a"));
+    }
 
-  @Test
-  public void test3() {
-    assertEquals(0, solution1.strStr("a", "a"));
-  }
+    @Test
+    public void test3() {
+        assertEquals(0, solution1.strStr("a", "a"));
+    }
 }
