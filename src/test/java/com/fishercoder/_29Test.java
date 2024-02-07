29bca977fdaf92e442eed66b82b6fe1ee20642c6
Fisher Coder <fishercoder@gmail.com>
2024-02-07 10:54:33 -0800

    migrate _29 test to Junit5

--- src/test/java/com/fishercoder/_29Test.java
+++ src/test/java/com/fishercoder/_29Test.java
@@ -1,17 +1,17 @@
 package com.fishercoder;
 
 import com.fishercoder.solutions._29;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class _29Test {
     private static _29.Solution1 solution1;
     private static _29.Solution2 solution2;
 
-    @BeforeClass
-    public static void setup() {
+    @BeforeEach
+    public void setup() {
         solution1 = new _29.Solution1();
         solution2 = new _29.Solution2();
     }
