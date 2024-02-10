43d1d3b2818ca0f5810984002d2ae700973f3646
Fisher Coder <fishercoder@gmail.com>
2024-02-09 17:14:37 -0800

    migrate _31 to junit 5

--- src/test/java/com/fishercoder/_31Test.java
+++ src/test/java/com/fishercoder/_31Test.java
@@ -1,17 +1,17 @@
 package com.fishercoder;
 
 import com.fishercoder.solutions._31;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class _31Test {
     private static _31.Solution1 solution1;
     private static int[] nums;
 
-    @BeforeClass
-    public static void setup() {
+    @BeforeEach
+    public void setup() {
         solution1 = new _31.Solution1();
     }
 
