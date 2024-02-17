8a76de16666d6d761578f483f302411e0b59d3a4
Fisher Coder <fishercoder@gmail.com>
2024-02-17 10:58:38 -0800

    migrate _44 to junit 5

--- src/test/java/com/fishercoder/_44Test.java
+++ src/test/java/com/fishercoder/_44Test.java
@@ -1,51 +1,51 @@
 package com.fishercoder;
 
 import com.fishercoder.solutions._44;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static junit.framework.TestCase.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class _44Test {
-  private static _44.Solution1 solution1;
+    private static _44.Solution1 solution1;
 
-  @BeforeClass
-  public static void setup() {
-    solution1 = new _44.Solution1();
-  }
+    @BeforeEach
+    public void setup() {
+        solution1 = new _44.Solution1();
+    }
 
-  @Test
-  public void test1() {
-    assertEquals(false, solution1.isMatch("aa", "a"));
-  }
+    @Test
+    public void test1() {
+        assertEquals(false, solution1.isMatch("aa", "a"));
+    }
 
-  @Test
-  public void test2() {
-    assertEquals(true, solution1.isMatch("aa", "aa"));
-  }
+    @Test
+    public void test2() {
+        assertEquals(true, solution1.isMatch("aa", "aa"));
+    }
 
-  @Test
-  public void test3() {
-    assertEquals(false, solution1.isMatch("aaa", "aa"));
-  }
+    @Test
+    public void test3() {
+        assertEquals(false, solution1.isMatch("aaa", "aa"));
+    }
 
-  @Test
-  public void test4() {
-    assertEquals(true, solution1.isMatch("aa", "*"));
-  }
+    @Test
+    public void test4() {
+        assertEquals(true, solution1.isMatch("aa", "*"));
+    }
 
-  @Test
-  public void test5() {
-    assertEquals(true, solution1.isMatch("aa", "a*"));
-  }
+    @Test
+    public void test5() {
+        assertEquals(true, solution1.isMatch("aa", "a*"));
+    }
 
-  @Test
-  public void test6() {
-    assertEquals(true, solution1.isMatch("ab", "?*"));
-  }
+    @Test
+    public void test6() {
+        assertEquals(true, solution1.isMatch("ab", "?*"));
+    }
 
-  @Test
-  public void test7() {
-    assertEquals(false, solution1.isMatch("aab", "c*a*b"));
-  }
+    @Test
+    public void test7() {
+        assertEquals(false, solution1.isMatch("aab", "c*a*b"));
+    }
 }
