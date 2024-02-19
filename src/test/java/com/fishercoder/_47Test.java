d94f8c6e31d7b0d43a187cc915a8175c944c9bf4
Fisher Coder <fishercoder@gmail.com>
2024-02-19 10:34:35 -0800

    migrate _47 to junit 5

--- src/test/java/com/fishercoder/_47Test.java
+++ src/test/java/com/fishercoder/_47Test.java
@@ -2,8 +2,8 @@ package com.fishercoder;
 
 import com.fishercoder.common.utils.CommonUtils;
 import com.fishercoder.solutions._47;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
@@ -12,8 +12,8 @@ public class _47Test {
     private static _47.Solution2 solution2;
     private static List<List<Integer>> actual;
 
-    @BeforeClass
-    public static void setup() {
+    @BeforeEach
+    public void setup() {
         solution1 = new _47.Solution1();
         solution2 = new _47.Solution2();
     }
