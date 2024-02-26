2032e201dbd152e16b531553a388609b24773944
Fisher Coder <fishercoder@gmail.com>
2024-02-26 08:21:55 -0800

    migrate 57 to junit5

--- src/test/java/com/fishercoder/_57Test.java
+++ src/test/java/com/fishercoder/_57Test.java
@@ -1,16 +1,16 @@
 package com.fishercoder;
 
 import com.fishercoder.solutions._57;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class _57Test {
     private static _57.Solution1 solution1;
 
-    @BeforeClass
-    public static void setup() {
+    @BeforeEach
+    public void setup() {
         solution1 = new _57.Solution1();
     }
 
