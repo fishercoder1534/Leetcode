e7b1a4bada6499b1379cfdbb3a9138604cefed4e
Fisher <fishercoder@gmail.com>
2024-03-11 07:46:20 -0700

    migrate 69 test to use Junit5

--- src/test/java/com/fishercoder/_69Test.java
+++ src/test/java/com/fishercoder/_69Test.java
@@ -1,26 +1,26 @@
 package com.fishercoder;
 
 import com.fishercoder.solutions._69;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static junit.framework.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class _69Test {
-  private static _69.Solution1 solution1;
+    private static _69.Solution1 solution1;
 
-  @BeforeClass
-  public static void setup() {
-    solution1 = new _69.Solution1();
-  }
+    @BeforeEach
+    public void setup() {
+        solution1 = new _69.Solution1();
+    }
 
-  @Test
-  public void test1() {
-    assertEquals(4, solution1.mySqrt(16));
-  }
+    @Test
+    public void test1() {
+        assertEquals(4, solution1.mySqrt(16));
+    }
 
-  @Test
-  public void test2() {
-    assertEquals(2, solution1.mySqrt(8));
-  }
+    @Test
+    public void test2() {
+        assertEquals(2, solution1.mySqrt(8));
+    }
 }
