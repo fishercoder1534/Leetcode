c61ae66a59dd0fbb786b29cc64bad2766cbada06
Fisher Coder <fishercoder@gmail.com>
2024-03-13 11:32:40 -0700

    migrate 71 to junit5

--- src/test/java/com/fishercoder/_71Test.java
+++ src/test/java/com/fishercoder/_71Test.java
@@ -1,16 +1,16 @@
 package com.fishercoder;
 
 import com.fishercoder.solutions._71;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class _71Test {
     private static _71.Solution1 solution1;
 
-    @BeforeClass
-    public static void setup() {
+    @BeforeEach
+    public void setup() {
         solution1 = new _71.Solution1();
     }
 
