b05f272ffaa5618cf9f9470b19e3a3196a6c5cc9
Fisher Coder <fishercoder@gmail.com>
2024-03-16 08:12:24 -0700

    migrate 78 to junit5

--- src/test/java/com/fishercoder/_78Test.java
+++ src/test/java/com/fishercoder/_78Test.java
@@ -2,16 +2,16 @@ package com.fishercoder;
 
 import com.fishercoder.common.utils.CommonUtils;
 import com.fishercoder.solutions._78;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class _78Test {
     private static _78.Solution1 solution1;
     private static _78.Solution2 solution2;
     private static _78.Solution3 solution3;
 
-    @BeforeClass
-    public static void setup() {
+    @BeforeEach
+    public void setup() {
         solution1 = new _78.Solution1();
         solution2 = new _78.Solution2();
         solution3 = new _78.Solution3();
