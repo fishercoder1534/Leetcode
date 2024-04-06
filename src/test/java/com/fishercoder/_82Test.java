bd80c2624c94f2b03bd21e179bf1fed6986ccb67
Fisher Coder <fishercoder@gmail.com>
2024-04-06 08:55:13 -0700

    migrate 82 to junit5

--- src/test/java/com/fishercoder/_82Test.java
+++ src/test/java/com/fishercoder/_82Test.java
@@ -3,9 +3,10 @@ package com.fishercoder;
 import com.fishercoder.common.classes.ListNode;
 import com.fishercoder.common.utils.LinkedListUtils;
 import com.fishercoder.solutions._82;
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class _82Test {
 
@@ -13,8 +14,8 @@ public class _82Test {
     private static ListNode head;
     private static ListNode expected;
 
-    @BeforeClass
-    public static void setup() {
+    @BeforeEach
+    public void setup() {
         solution1 = new _82.Solution1();
     }
 
@@ -22,13 +23,13 @@ public class _82Test {
     public void test1() {
         head = LinkedListUtils.contructLinkedList(new int[]{1, 2, 3, 3, 4, 4, 5});
         expected = LinkedListUtils.contructLinkedList(new int[]{1, 2, 5});
-        Assert.assertEquals(expected, solution1.deleteDuplicates(head));
+        assertEquals(expected, solution1.deleteDuplicates(head));
     }
 
     @Test
     public void test2() {
         head = LinkedListUtils.contructLinkedList(new int[]{1, 1, 1, 2, 3});
         expected = LinkedListUtils.contructLinkedList(new int[]{2, 3});
-        Assert.assertEquals(expected, solution1.deleteDuplicates(head));
+        assertEquals(expected, solution1.deleteDuplicates(head));
     }
 }
