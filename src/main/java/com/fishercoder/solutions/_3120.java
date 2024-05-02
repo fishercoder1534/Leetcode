cfbad80c7ff51090fe0e836928096d474819edcb
Fisher Coder <fishercoder@gmail.com>
2024-05-01 21:03:09 -0700

    add 3120

---
+++ src/main/java/com/fishercoder/solutions/_3120.java
@@ -0,0 +1,24 @@
+package com.fishercoder.solutions;
+
+public class _3120 {
+    public static class Solution1 {
+        public int numberOfSpecialChars(String word) {
+            int count = 0;
+            int[] lower = new int[26];
+            int[] upper = new int[26];
+            for (char c : word.toCharArray()) {
+                if (Character.isLowerCase(c)) {
+                    lower[c - 'a']++;
+                } else {
+                    upper[c - 'A']++;
+                }
+            }
+            for (int i = 0; i < 26; i++) {
+                if (lower[i] != 0 && upper[i] != 0) {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
