ce3900a1247d9e899003d3ad1adc288b29221060
Fisher Coder <fishercoder@gmail.com>
2024-05-28 07:23:51 -0700

    migrate 85 to junit5

--- src/test/java/com/fishercoder/_85Test.java
+++ src/test/java/com/fishercoder/_85Test.java
@@ -1,16 +1,16 @@
 package com.fishercoder;
 
 import com.fishercoder.solutions._85;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class _85Test {
     private static _85.Solution1 solution1;
 
-    @BeforeClass
-    public static void setup() {
+    @BeforeEach
+    public void setup() {
         solution1 = new _85.Solution1();
     }
 
