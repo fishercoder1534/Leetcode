9573563cc485cba0fc40da15fec3224ce9896384
Fisher Coder <fishercoder@gmail.com>
2024-05-29 17:03:09 -0700

    add 426

---
+++ src/main/java/com/fishercoder/solutions/_426.java
@@ -0,0 +1,54 @@
+package com.fishercoder.solutions;
+
+public class _426 {
+
+    public static class Solution1 {
+        Node head;
+        Node tail;
+
+        public Node treeToDoublyList(Node root) {
+            if (root == null) {
+                return null;
+            }
+            dfs(root);
+            tail.right = head;
+            head.left = tail;
+            return head;
+        }
+
+        private void dfs(Node node) {
+            if (node == null) {
+                return;
+            }
+            dfs(node.left);
+            if (head == null) {
+                head = node;
+            }
+            if (tail != null) {
+                tail.right = node;
+                node.left = tail;
+            }
+            tail = node;
+            dfs(node.right);
+        }
+    }
+
+    private static class Node {
+        public int val;
+        public Node left;
+        public Node right;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right) {
+            val = _val;
+            left = _left;
+            right = _right;
+        }
+    }
+}
