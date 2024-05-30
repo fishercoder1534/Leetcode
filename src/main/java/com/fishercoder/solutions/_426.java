3baf5259ebfd01338fe2eb6921686726d4edf01e
Fisher Coder <fishercoder@gmail.com>
2024-05-29 17:21:30 -0700

    fix build

--- src/main/java/com/fishercoder/solutions/_426.java
+++ src/main/java/com/fishercoder/solutions/_426.java
@@ -41,14 +41,14 @@ public class _426 {
         public Node() {
         }
 
-        public Node(int _val) {
-            val = _val;
+        public Node(int val) {
+            this.val = val;
         }
 
-        public Node(int _val, Node _left, Node _right) {
-            val = _val;
-            left = _left;
-            right = _right;
+        public Node(int val, Node left, Node right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
         }
     }
 }
