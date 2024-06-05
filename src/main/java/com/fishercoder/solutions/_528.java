a26eb084a5525499eff095e93d7d333cd6da1aca
Fisher Coder <fishercoder@gmail.com>
2024-06-05 08:06:29 -0700

    update 528

--- src/main/java/com/fishercoder/solutions/_528.java
+++ src/main/java/com/fishercoder/solutions/_528.java
@@ -4,28 +4,40 @@ import java.util.Random;
 
 public class _528 {
     public static class Solution1 {
+        /**
+         * Credit: https://leetcode.com/problems/random-pick-with-weight/editorial/
+         * <p>
+         * Mental gymnastics (which is explained step by step in the above link):
+         * 1. picture this to be a ball throwing onto a line from the starting point (0,0);
+         * 2. where this ball is going to land on the line is a probability problem, i.e. an offset to the starting point (0,0);
+         * 3. we can use prefix sums array to simulate this line (each value in the array is positive as it represents the weight/probability of it being picked, so cannot be zero or negative);
+         * 4. we can use Random() to generate a random number called index bounded by the last value of the prefix sums array, i.e. the biggest offset possible;
+         * 5. then we can use binary search to find where this random number: index, would fit in the prefix sums array.
+         */
         Random random;
-        int[] preSums;
+        int[] prefixSums;
 
         public Solution1(int[] w) {
             this.random = new Random();
-            for (int i = 1; i < w.length; ++i) {
-                w[i] += w[i - 1];
+            this.prefixSums = new int[w.length];
+            int prefixSum = 0;
+            for (int i = 0; i < w.length; ++i) {
+                prefixSum += w[i];
+                prefixSums[i] = prefixSum;
             }
-            this.preSums = w;
         }
 
         public int pickIndex() {
-            int len = preSums.length;
-            int idx = random.nextInt(preSums[len - 1]) + 1;
+            int len = prefixSums.length;
+            int idx = random.nextInt(prefixSums[len - 1]) + 1;
             int left = 0;
             int right = len - 1;
             // search position
             while (left < right) {
                 int mid = left + (right - left) / 2;
-                if (preSums[mid] == idx) {
+                if (prefixSums[mid] == idx) {
                     return mid;
-                } else if (preSums[mid] < idx) {
+                } else if (prefixSums[mid] < idx) {
                     left = mid + 1;
                 } else {
                     right = mid;
--- src/test/java/com/fishercoder/_528Test.java
+++ src/test/java/com/fishercoder/_528Test.java
@@ -1,9 +1,9 @@
 package com.fishercoder;
 
 import com.fishercoder.solutions._528;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class _528Test {
     private static _528.Solution1 solution1;
