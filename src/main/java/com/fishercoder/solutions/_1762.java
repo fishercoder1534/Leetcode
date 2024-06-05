a1b637afc25417548aebc46f421cc51178532895
Fisher Coder <fishercoder@gmail.com>
2024-06-05 08:14:52 -0700

    update 1762

--- src/main/java/com/fishercoder/solutions/_1762.java
+++ src/main/java/com/fishercoder/solutions/_1762.java
@@ -6,21 +6,21 @@ import java.util.List;
 public class _1762 {
     public static class Solution1 {
         public int[] findBuildings(int[] heights) {
+            List<Integer> list = new ArrayList();
             int len = heights.length;
-            int tallestBuildingOnTheRight = heights[len - 1];
-            List<Integer> list = new ArrayList<>();
+            int higher = heights[len - 1];
             list.add(len - 1);
             for (int i = len - 2; i >= 0; i--) {
-                if (heights[i] > tallestBuildingOnTheRight) {
-                    list.add(0, i);
-                    tallestBuildingOnTheRight = heights[i];
+                if (heights[i] > higher) {
+                    higher = heights[i];
+                    list.add(i);
                 }
             }
-            int[] num = new int[list.size()];
-            for (int i = 0; i < list.size(); i++) {
-                num[i] = list.get(i);
+            int[] res = new int[list.size()];
+            for (int i = list.size() - 1, j = 0; i >= 0; i--, j++) {
+                res[j] = list.get(i);
             }
-            return num;
+            return res;
         }
     }
 }
