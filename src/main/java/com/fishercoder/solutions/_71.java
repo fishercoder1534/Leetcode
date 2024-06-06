ffeee6a99d9ec6220e49da170378072158a46541
Fisher Coder <fishercoder@gmail.com>
2024-06-06 08:00:44 -0700

    add a solution for 71

--- src/main/java/com/fishercoder/solutions/_71.java
+++ src/main/java/com/fishercoder/solutions/_71.java
@@ -9,6 +9,9 @@ import java.util.Set;
 public class _71 {
 
     public static class Solution1 {
+        /**
+         * For LinkedList class in Java, if you use pop(), then you'll have to use push() as its corresponding method to remove from the "top" of the stack, using pollLast() will not work.
+         */
         public String simplifyPath(String path) {
             Deque<String> stack = new LinkedList<>();
             Set<String> skipSet = new HashSet<>(Arrays.asList("..", ".", ""));
@@ -24,6 +27,30 @@ public class _71 {
                 result = "/" + dir + result;
             }
             return result.isEmpty() ? "/" : result;
+        }
+    }
+
+    public static class Solution2 {
+        /**
+         * This solution doesn't vary too much from the above one, except in that it's using pollLast() and addLast() instead of pop() and push().
+         */
+        public String simplifyPath(String path) {
+            Set<String> skip = new HashSet(Arrays.asList("..", ".", ""));
+            Deque<String> stack = new LinkedList();
+            String[] directories = path.split("/");
+            for (String dir : directories) {
+                if (dir.equals("..") && !stack.isEmpty()) {
+                    stack.pollLast();
+                } else if (!skip.contains(dir)) {
+                    stack.addLast(dir);
+                }
+            }
+            StringBuilder sb = new StringBuilder();
+            while (!stack.isEmpty()) {
+                sb.append("/");
+                sb.append(stack.pollFirst());
+            }
+            return sb.length() == 0 ? "/" : sb.toString();
         }
     }
 }
--- src/test/java/com/fishercoder/_71Test.java
+++ src/test/java/com/fishercoder/_71Test.java
@@ -8,10 +8,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class _71Test {
     private static _71.Solution1 solution1;
+    private static _71.Solution2 solution2;
 
     @BeforeEach
     public void setup() {
         solution1 = new _71.Solution1();
+        solution2 = new _71.Solution2();
     }
 
     @Test
@@ -32,5 +34,17 @@ public class _71Test {
     @Test
     public void test4() {
         assertEquals("/", solution1.simplifyPath("/."));
+    }
+
+    @Test
+    public void test5() {
+//        assertEquals("/home/user/Pictures", solution1.simplifyPath("/home/user/Documents/../Pictures"));
+        assertEquals("/home/user/Pictures", solution2.simplifyPath("/home/user/Documents/../Pictures"));
+    }
+
+    @Test
+    public void test6() {
+        assertEquals("/", solution1.simplifyPath("/../"));
+        assertEquals("/", solution2.simplifyPath("/../"));
     }
 }
