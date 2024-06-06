b95c4e10257012684f21f3cce1cb2688d36815ca
Fisher Coder <fishercoder@gmail.com>
2024-06-06 08:32:15 -0700

    update 426

--- src/main/java/com/fishercoder/solutions/_426.java
+++ src/main/java/com/fishercoder/solutions/_426.java
@@ -3,6 +3,9 @@ package com.fishercoder.solutions;
 public class _426 {
 
     public static class Solution1 {
+        /**
+         * Credit: https://leetcode.com/problems/convert-binary-search-tree-to-sorted-doubly-linked-list/solutions/1494821/simplest-java-solution-with-explanation-inorder-traversal-in-place-no-dummy-node-needed/
+         */
         Node head;
         Node tail;
 
@@ -20,20 +23,25 @@ public class _426 {
             if (node == null) {
                 return;
             }
+            //we traverse all the way to the most bottom left leaf node first
             dfs(node.left);
+            //we only need to assign head once, i.e. when it's not assigned, we'll assign the most bottom left leaf node to head
             if (head == null) {
                 head = node;
             }
+            //if the tail is already assigned, which should be all of the cases except when it's just finding the most bottom left leaf
+            // attach current node to tail's right, assign tail to current node's left
             if (tail != null) {
                 tail.right = node;
                 node.left = tail;
             }
+            //then always assign the current node to be the new tail
             tail = node;
             dfs(node.right);
         }
     }
 
-    private static class Node {
+    public static class Node {
         public int val;
         public Node left;
         public Node right;
