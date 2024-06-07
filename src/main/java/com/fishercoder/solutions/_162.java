5292c3a824b98f1c1d2fb4c8f38676654eaae588
Fisher Coder <fishercoder@gmail.com>
2024-06-07 16:35:26 -0700

    update 162 with examples

--- src/main/java/com/fishercoder/solutions/_162.java
+++ src/main/java/com/fishercoder/solutions/_162.java
@@ -7,7 +7,18 @@ public class _162 {
          * credit: https://leetcode.com/problems/find-peak-element/solutions/1290642/intuition-behind-conditions-complete-explanation-diagram-binary-search/
          * Time: O(logn)
          * <p>
-         * draw three cases with three examples, it's pretty self-explanatory
+         * draw three cases with three examples, it's pretty self-explanatory:
+         * case 1:
+         *          mid
+         *       1,  3, 2
+         * case 2:
+         *         mid
+         *      1,  2,  3
+         *      so peak should be on the right side, so code is: left = mid + 1
+         * case 3:
+         *         mid
+         *      3,  2,  1
+         *      so peak should be on the left side, so code is: right = mid - 1;
          */
         public int findPeakElement(int[] nums) {
             if (nums == null || nums.length <= 1 || nums[0] > nums[1]) {
