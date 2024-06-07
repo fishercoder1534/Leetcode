76898e3a4071dff1e1aee39fa27e6d3f4ac31f72
Fisher Coder <fishercoder@gmail.com>
2024-06-07 10:12:03 -0700

    add 973

--- src/main/java/com/fishercoder/solutions/_973.java
+++ src/main/java/com/fishercoder/solutions/_973.java
@@ -36,4 +36,30 @@ public class _973 {
             return Math.sqrt(Math.pow(point[0], 2) + Math.pow(point[1], 2));
         }
     }
+
+    public static class Solution2 {
+        public int[][] kClosest(int[][] points, int k) {
+            PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> (b[0] * b[0] + b[1] * b[1]) - (a[0] * a[0] + a[1] * a[1]));
+            for (int[] point : points) {
+                long distance = (long) point[0] * point[0] + point[1] * point[1];
+                if (maxHeap.size() < k) {
+                    maxHeap.offer(point);
+                } else {
+                    int[] peek = maxHeap.peek();
+                    long peekedDistance = (long) peek[0] * peek[0] + peek[1] * peek[1];
+                    if (peekedDistance > distance) {
+                        //this is an optimization so that the space complexity is limited to O(k)
+                        maxHeap.poll();
+                        maxHeap.offer(point);
+                    }
+                }
+            }
+            int[][] result = new int[k][2];
+            for (int i = 0; i < k; i++) {
+                int[] point = maxHeap.poll();
+                result[i] = point;
+            }
+            return result;
+        }
+    }
 }
--- src/test/java/com/fishercoder/_973Test.java
+++ src/test/java/com/fishercoder/_973Test.java
@@ -1,26 +1,30 @@
 package com.fishercoder;
 
 import com.fishercoder.solutions._973;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class _973Test {
-    private static _973.Solution1 test;
+    private static _973.Solution1 solution1;
+    private static _973.Solution2 solution2;
 
-    @BeforeClass
-    public static void setUp() {
-        test = new _973.Solution1();
+    @BeforeEach
+    public void setUp() {
+        solution1 = new _973.Solution1();
+        solution2 = new _973.Solution2();
     }
 
     @Test
     public void test1() {
-        assertArrayEquals(new int[][]{{-2, 2}}, test.kClosest(new int[][]{{1, 3}, {-2, 2}}, 1));
+        assertArrayEquals(new int[][]{{-2, 2}}, solution1.kClosest(new int[][]{{1, 3}, {-2, 2}}, 1));
+        assertArrayEquals(new int[][]{{-2, 2}}, solution2.kClosest(new int[][]{{1, 3}, {-2, 2}}, 1));
     }
 
     @Test
     public void test2() {
-        assertArrayEquals(new int[][]{{3, 3},{-2, 4}}, test.kClosest(new int[][]{{3, 3},{5, -1},{-2, 4}}, 2));
+        assertArrayEquals(new int[][]{{3, 3}, {-2, 4}}, solution1.kClosest(new int[][]{{3, 3}, {5, -1}, {-2, 4}}, 2));
+        assertArrayEquals(new int[][]{{-2, 4}, {3, 3}}, solution2.kClosest(new int[][]{{3, 3}, {5, -1}, {-2, 4}}, 2));
     }
 }
