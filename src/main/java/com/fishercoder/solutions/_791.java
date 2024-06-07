7b51f0b0ff26e3928224e5497a269393c4f9b283
Fisher Coder <fishercoder@gmail.com>
2024-06-07 09:25:24 -0700

    update 791

--- src/main/java/com/fishercoder/solutions/_791.java
+++ src/main/java/com/fishercoder/solutions/_791.java
@@ -5,13 +5,13 @@ import java.util.Map;
 
 public class _791 {
     public static class Solution1 {
-        public String customSortString(String S, String T) {
+        public String customSortString(String order, String s) {
             Map<Character, Integer> map = new HashMap<>();
-            for (char c : T.toCharArray()) {
+            for (char c : s.toCharArray()) {
                 map.put(c, map.getOrDefault(c, 0) + 1);
             }
             StringBuilder sb = new StringBuilder();
-            for (char c : S.toCharArray()) {
+            for (char c : order.toCharArray()) {
                 if (map.containsKey(c)) {
                     int count = map.get(c);
                     while (count-- > 0) {
