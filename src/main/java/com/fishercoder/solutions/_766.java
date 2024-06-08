99352121ceedbf39a885f40c7c364c1a7cba695c
Fisher Coder <fishercoder@gmail.com>
2024-06-08 06:48:01 -0700

    update 766

--- src/main/java/com/fishercoder/solutions/_766.java
+++ src/main/java/com/fishercoder/solutions/_766.java
@@ -5,31 +5,9 @@ public class _766 {
         public boolean isToeplitzMatrix(int[][] matrix) {
             int m = matrix.length;
             int n = matrix[0].length;
-            int i = 0;
-            int j = 0;
-            int sameVal = matrix[i][j];
-            while (++i < m && ++j < n) {
-                if (matrix[i][j] != sameVal) {
-                    return false;
-                }
-            }
-
-            for (i = 1, j = 0; i < m; i++) {
-                int tmpI = i;
-                int tmpJ = j;
-                sameVal = matrix[i][j];
-                while (++tmpI < m && ++tmpJ < n) {
-                    if (matrix[tmpI][tmpJ] != sameVal) {
-                        return false;
-                    }
-                }
-            }
-            for (i = 0, j = 1; j < n; j++) {
-                int tmpJ = j;
-                int tmpI = i;
-                sameVal = matrix[tmpI][tmpJ];
-                while (++tmpI < m && ++tmpJ < n) {
-                    if (matrix[tmpI][tmpJ] != sameVal) {
+            for (int i = 1; i < m; i++) {
+                for (int j = 1; j < n; j++) {
+                    if (matrix[i][j] != matrix[i - 1][j - 1]) {
                         return false;
                     }
                 }
--- src/test/java/com/fishercoder/_766Test.java
+++ src/test/java/com/fishercoder/_766Test.java
@@ -1,46 +1,46 @@
 package com.fishercoder;
 
 import com.fishercoder.solutions._766;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class _766Test {
-  private static _766.Solution1 solution1;
-  private static int[][] matrix;
+    private static _766.Solution1 solution1;
+    private static int[][] matrix;
 
-  @BeforeClass
-  public static void setup() {
-    solution1 = new _766.Solution1();
-  }
+    @BeforeEach
+    public void setup() {
+        solution1 = new _766.Solution1();
+    }
 
-  @Test
-  public void test1() {
-    matrix = new int[][] {
-        {1, 2, 3, 4},
-        {5, 1, 2, 3},
-        {9, 5, 1, 2}
-    };
-    assertEquals(true, solution1.isToeplitzMatrix(matrix));
-  }
+    @Test
+    public void test1() {
+        matrix = new int[][]{
+                {1, 2, 3, 4},
+                {5, 1, 2, 3},
+                {9, 5, 1, 2}
+        };
+        assertEquals(true, solution1.isToeplitzMatrix(matrix));
+    }
 
-  @Test
-  public void test2() {
-    matrix = new int[][] {
-        {1, 2},
-        {2, 2},
-    };
-    assertEquals(false, solution1.isToeplitzMatrix(matrix));
-  }
+    @Test
+    public void test2() {
+        matrix = new int[][]{
+                {1, 2},
+                {2, 2},
+        };
+        assertEquals(false, solution1.isToeplitzMatrix(matrix));
+    }
 
-  @Test
-  public void test3() {
-    matrix = new int[][] {
-        {1, 2, 3, 4, 5, 9},
-        {5, 1, 2, 3, 4, 5},
-        {9, 5, 1, 2, 3, 4}
-    };
-    assertEquals(true, solution1.isToeplitzMatrix(matrix));
-  }
+    @Test
+    public void test3() {
+        matrix = new int[][]{
+                {1, 2, 3, 4, 5, 9},
+                {5, 1, 2, 3, 4, 5},
+                {9, 5, 1, 2, 3, 4}
+        };
+        assertEquals(true, solution1.isToeplitzMatrix(matrix));
+    }
 }
