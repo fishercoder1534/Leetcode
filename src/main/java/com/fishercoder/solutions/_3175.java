af0f6ef89fbd70686f6875ff326c27acfd3ed017
Fisher Coder <fishercoder@gmail.com>
2024-06-08 09:16:56 -0700

    add 3175

---
+++ src/main/java/com/fishercoder/solutions/_3175.java
@@ -0,0 +1,40 @@
+package com.fishercoder.solutions;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class _3175 {
+    public static class Solution1 {
+        public int findWinningPlayer(int[] skills, int k) {
+            Deque<int[]> q = new LinkedList<>();
+            int highestSkill = 0;
+            for (int i = 0; i < skills.length; i++) {
+                q.offer(new int[]{i, skills[i], 0});
+                highestSkill = Math.max(highestSkill, skills[i]);
+            }
+            int count = 0;
+            while (true) {
+                int[] first = q.pollFirst();
+                if (first[1] == highestSkill) {
+                    //if the highest skill stands at the head of the queue, then it'll keep standing there
+                    //so it's guaranteed that it'll be the winner
+                    return first[0];
+                }
+                int[] second = q.pollFirst();
+                if (first[2] >= k) {
+                    return first[0];
+                }
+                if (first[1] > second[1]) {
+                    first[2]++;
+                    q.addLast(second);
+                    q.addFirst(first);
+                } else {
+                    second[2]++;
+                    q.addFirst(second);
+                    q.addLast(first);
+                }
+                count++;
+            }
+        }
+    }
+}
---
+++ src/test/java/com/fishercoder/_3175Test.java
@@ -0,0 +1,26 @@
+package com.fishercoder;
+
+import com.fishercoder.solutions._3175;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class _3175Test {
+    private static _3175.Solution1 solution1;
+    private static int[] skills;
+    private static int k;
+
+    @BeforeEach
+    public void setup() {
+        solution1 = new _3175.Solution1();
+    }
+
+    @Test
+    public void test1() {
+        skills = new int[]{16, 4, 7, 17};
+        k = 562084119;
+        assertEquals(3, solution1.findWinningPlayer(skills, k));
+    }
+
+}
