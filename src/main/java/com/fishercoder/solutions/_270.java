a74f999e5d3c28ec91875113473e434478a2ca93
Fisher Coder <fishercoder@gmail.com>
2024-06-08 07:17:39 -0700

    update 270

--- src/main/java/com/fishercoder/solutions/_270.java
+++ src/main/java/com/fishercoder/solutions/_270.java
@@ -5,96 +5,15 @@ import com.fishercoder.common.classes.TreeNode;
 public class _270 {
 
     public static class Solution1 {
-        //A general tree solution, this finished in 1 ms
         public int closestValue(TreeNode root, double target) {
-            if (root == null) {
-                return 0;
-            }
-            double delta = Double.MAX_VALUE;
-            return dfs(root, target, delta, root.val);
-        }
-
-        private int dfs(TreeNode root, double target, double delta, int closestVal) {
-            if (Math.abs(root.val - target) < delta) {
-                closestVal = root.val;
-                delta = Math.abs(root.val - target);
-            }
-            int leftVal = closestVal;
-            if (root.left != null) {
-                leftVal = dfs(root.left, target, delta, closestVal);
-            }
-            int rightVal = closestVal;
-            if (root.right != null) {
-                rightVal = dfs(root.right, target, delta, closestVal);
-            }
-            return (Math.abs(leftVal - target) > Math.abs(rightVal - target)) ? rightVal : leftVal;
-        }
-    }
-
-    public static class Solution2 {
-        // BST solution
-        // we can tailor the solution to use the BST feature: left subtrees are always smaller than the root the right subtrees
-        //this finished in 0 ms
-        public int closestValue(TreeNode root, double target) {
-            if (root == null) {
-                return 0;
-            }
-            return dfs(root, target, root.val);
-        }
-
-        private int dfs(TreeNode root, double target, int minVal) {
-            if (root == null) {
-                return minVal;
-            }
-            if (Math.abs(root.val - target) < Math.abs(minVal - target)) {
-                minVal = root.val;
-            }
-            if (target < root.val) {
-                minVal = dfs(root.left, target, minVal);
-            } else {
-                minVal = dfs(root.right, target, minVal);
-            }
-            return minVal;
-        }
-    }
-
-    public static class Solution3 {
-        //a more concise solution
-        public int closestValue(TreeNode root, double target) {
-            if (root == null) {
-                return 0;
-            }
-            return dfs(root, target, root.val);
-        }
-
-        private int dfs(TreeNode root, double target, int minVal) {
-            if (root == null) {
-                return minVal;
-            }
-            if (Math.abs(root.val - target) < Math.abs(minVal - target)) {
-                minVal = root.val;
-            }
-            minVal = dfs(root.left, target, minVal);
-            minVal = dfs(root.right, target, minVal);
-            return minVal;
-        }
-    }
-
-    public static class Solution4 {
-        //BST iterative solution
-        public int closestValue(TreeNode root, double target) {
-            long minVal = Long.MAX_VALUE;
+            int val;
+            int closest = root.val;
             while (root != null) {
-                if (Math.abs(root.val - target) < Math.abs(minVal - target)) {
-                    minVal = root.val;
-                }
-                if (target < root.val) {
-                    root = root.left;
-                } else {
-                    root = root.right;
-                }
+                val = root.val;
+                closest = Math.abs(val - target) < Math.abs(closest - target) || (Math.abs(val - target) == Math.abs(closest - target) && val < closest) ? val : closest;
+                root = target < root.val ? root.left : root.right;
             }
-            return minVal == Long.MAX_VALUE ? 0 : (int) minVal;
+            return closest;
         }
     }
 }
