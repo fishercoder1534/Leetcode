21ff909908e577be023a50401b97c86458ca3ac0
Fisher Coder <fishercoder@gmail.com>
2024-06-09 09:51:05 -0700

    update 636

--- src/main/java/com/fishercoder/solutions/_636.java
+++ src/main/java/com/fishercoder/solutions/_636.java
@@ -6,31 +6,27 @@ import java.util.List;
 
 public class _636 {
     public static class Solution1 {
-        /**
-         * Based on the example, it's difficult to see how function 2 executes 4 units of time, actually
-         * we can add 1 to all end times to make it easier to understand and AC'ed.
-         */
         public int[] exclusiveTime(int n, List<String> logs) {
             /**Stack is the way to go:
-             * we keep pushing the logId onto the stack whenever we just encounter this logId's start timestamp,
-             * we'll pop this logId only when we encounter this logId's end timestamp.
-             * Meanwhile, we keep a counter called prevTime,
-             * whenever the stack is not empty, we'll always deduct prevTime from the last logId on the stack.*/
+             * 1. we keep pushing the logId onto the stack whenever we just encounter this logId's start timestamp,
+             * 2. we'll pop this logId only when we encounter this logId's end timestamp.
+             * 3. Meanwhile, we keep a counter called prevTime,
+             * 4. whenever the stack is not empty, we'll always deduct prevTime from the last logId on the stack.*/
             Deque<Integer> stack = new LinkedList<>();
             int[] result = new int[n];
             int prevTime = 0;
             for (String log : logs) {
                 String[] parts = log.split(":");
                 if (!stack.isEmpty()) {
-                    result[stack.peek()] += Integer.parseInt(parts[2]) - prevTime;
+                    result[stack.peekLast()] += Integer.parseInt(parts[2]) - prevTime;
                 }
                 prevTime = Integer.parseInt(parts[2]);
                 if (parts[1].equals("start")) {
-                    stack.addFirst(Integer.parseInt(parts[0]));//i.e. stack.push()
+                    stack.addLast(Integer.parseInt(parts[0]));
                 } else {
+                    //remember to have result plus 1, i.e. when a task starts at 2, ends at 5, it should be 5 -2 + 1 = 4
                     prevTime++;
-                    //remember to have result pluse 1 to match the problem AC criteria
-                    result[stack.pollFirst()]++;//i.e. stack.pop()
+                    result[stack.pollLast()]++;
                 }
             }
             return result;
