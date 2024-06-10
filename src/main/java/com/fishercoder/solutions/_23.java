14e9ee97d8226bda62ea682d18f169ba17024d14
Fisher Coder <fishercoder@gmail.com>
2024-06-09 17:13:32 -0700

    update 23

--- src/main/java/com/fishercoder/solutions/_23.java
+++ src/main/java/com/fishercoder/solutions/_23.java
@@ -2,13 +2,12 @@ package com.fishercoder.solutions;
 
 import com.fishercoder.common.classes.ListNode;
 
-import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class _23 {
     public static class Solution1 {
         public ListNode mergeKLists(ListNode[] lists) {
-            PriorityQueue<ListNode> heap = new PriorityQueue((Comparator<ListNode>) (o1, o2) -> o1.val - o2.val);
+            PriorityQueue<ListNode> heap = new PriorityQueue<>((a, b) -> a.val - b.val);
 
             for (ListNode node : lists) {
                 if (node != null) {
@@ -21,10 +20,10 @@ public class _23 {
             while (!heap.isEmpty()) {
                 ListNode curr = heap.poll();
                 temp.next = new ListNode(curr.val);
+                temp = temp.next;
                 if (curr.next != null) {
                     heap.offer(curr.next);
                 }
-                temp = temp.next;
             }
             return pre.next;
         }
