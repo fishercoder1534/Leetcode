fdc2abe2cf7ba1597e98a55e87f58f2b28528fee
Fisher Coder <fishercoder@gmail.com>
2024-06-10 13:59:55 -0700

    add 773

---
+++ src/test/java/com/fishercoder/_773Test.java
@@ -0,0 +1,36 @@
+package com.fishercoder;
+
+import com.fishercoder.solutions._773;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class _773Test {
+    private static _773.Solution1 solution1;
+    private static int[][] board;
+
+    @BeforeEach
+    public void setup() {
+        solution1 = new _773.Solution1();
+    }
+
+    @Test
+    public void test1() {
+        board = new int[][]{
+                {1, 2, 3},
+                {4, 0, 5}
+        };
+        assertEquals(1, solution1.slidingPuzzle(board));
+    }
+
+    @Test
+    public void test2() {
+        board = new int[][]{
+                {4, 1, 2},
+                {5, 0, 3}
+        };
+        assertEquals(5, solution1.slidingPuzzle(board));
+    }
+
+}
