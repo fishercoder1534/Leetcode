e6b121062154f8eeba265a04f57bbd332e680e1a
Fisher Coder <fishercoder@gmail.com>
2024-06-11 13:11:04 -0700

    add a solution for 415

--- src/main/java/com/fishercoder/solutions/_415.java
+++ src/main/java/com/fishercoder/solutions/_415.java
@@ -31,4 +31,30 @@ public class _415 {
         }
     }
 
+    public static class Solution2 {
+        /**
+         * This is an optimized version of Solution1, on LeetCode, this version beats 100% while Solution1 beats only 67%.
+         */
+        public String addStrings(String num1, String num2) {
+            StringBuilder sb = new StringBuilder();
+            int i = num1.length() - 1;
+            int j = num2.length() - 1;
+            int carry = 0;
+            while (i >= 0 || j >= 0 || carry > 0) {
+                int sum = carry;
+                if (i >= 0) {
+                    sum += num1.charAt(i) - '0';
+                }
+                if (j >= 0) {
+                    sum += num2.charAt(j) - '0';
+                }
+                sb.append(sum % 10);
+                carry = sum / 10;
+                i--;
+                j--;
+            }
+            return sb.reverse().toString();
+        }
+    }
+
 }
--- src/test/java/com/fishercoder/_415Test.java
+++ src/test/java/com/fishercoder/_415Test.java
@@ -1,25 +1,23 @@
 package com.fishercoder;
 
 import com.fishercoder.solutions._415;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static junit.framework.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class _415Test {
     private static _415.Solution1 solution1;
+    private static _415.Solution2 solution2;
     private static String expected;
     private static String actual;
     private static String num1;
     private static String num2;
 
-    @BeforeClass
-    public static void setup() {
+    @BeforeEach
+    public void setup() {
         solution1 = new _415.Solution1();
-        expected = new String();
-        actual = new String();
-        num1 = new String();
-        num2 = new String();
+        solution2 = new _415.Solution2();
     }
 
     @Test
@@ -28,6 +26,8 @@ public class _415Test {
         num2 = "34567";
         expected = "34690";
         actual = solution1.addStrings(num1, num2);
+        assertEquals(expected, actual);
+        actual = solution2.addStrings(num1, num2);
         assertEquals(expected, actual);
     }
 
