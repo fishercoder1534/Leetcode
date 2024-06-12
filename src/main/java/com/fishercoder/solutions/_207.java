0b57d02b5f26b7335ca52798802138f49876455b
Fisher Coder <fishercoder@gmail.com>
2024-06-12 06:55:48 -0700

    update 207

--- src/main/java/com/fishercoder/solutions/_207.java
+++ src/main/java/com/fishercoder/solutions/_207.java
@@ -8,6 +8,33 @@ import java.util.List;
 import java.util.Queue;
 import java.util.Set;
 
+/**
+ * 207. Course Schedule
+ * <p>
+ * There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1.
+ * You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+ * For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+ * Return true if you can finish all courses. Otherwise, return false.
+ * <p>
+ * Example 1:
+ * Input: numCourses = 2, prerequisites = [[1,0]]
+ * Output: true
+ * Explanation: There are a total of 2 courses to take.
+ * To take course 1 you should have finished course 0. So it is possible.
+ * <p>
+ * Example 2:
+ * Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+ * Output: false
+ * Explanation: There are a total of 2 courses to take.
+ * To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
+ * <p>
+ * Constraints:
+ * 1 <= numCourses <= 2000
+ * 0 <= prerequisites.length <= 5000
+ * prerequisites[i].length == 2
+ * 0 <= ai, bi < numCourses
+ * All the pairs prerequisites[i] are unique.
+ */
 public class _207 {
 
     public static class Solution1 {
@@ -33,11 +60,11 @@ public class _207 {
                 Iterator<Integer> it = zeroDegree.iterator();
                 int course = it.next();
                 zeroDegree.remove(course);
-                for (int[] prereq : prerequisites) {
-                    if (prereq[1] == course) {
-                        indegree[prereq[0]]--;
-                        if (indegree[prereq[0]] == 0) {
-                            zeroDegree.add(prereq[0]);
+                for (int[] pre : prerequisites) {
+                    if (pre[1] == course) {
+                        indegree[pre[0]]--;
+                        if (indegree[pre[0]] == 0) {
+                            zeroDegree.add(pre[0]);
                         }
                     }
                 }
@@ -93,7 +120,7 @@ public class _207 {
     public static class Solution3 {
         /**
          * DFS, the fastest method in all, with the help of a cache and also converted edges into adjacency list,
-         * although theoretically, all these three methods' time complexity is: O(V+E)
+         * although theoretically, all these three methods' time complexity are: O(V+E)
          */
         public boolean canFinish(int numCourses, int[][] prerequisites) {
             List<List<Integer>> courseList = new ArrayList<>();
--- src/test/java/com/fishercoder/_207Test.java
+++ src/test/java/com/fishercoder/_207Test.java
@@ -1,10 +1,10 @@
 package com.fishercoder;
 
 import com.fishercoder.solutions._207;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static junit.framework.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class _207Test {
     private static _207.Solution1 solution1;
@@ -13,8 +13,8 @@ public class _207Test {
     private static int[][] prerequisites;
     private static int numCourses;
 
-    @BeforeClass
-    public static void setup() {
+    @BeforeEach
+    public void setup() {
         solution1 = new _207.Solution1();
         solution2 = new _207.Solution2();
         solution3 = new _207.Solution3();
