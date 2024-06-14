0d8f7719be59f20bdbfa65f2d8752777abda2eed
Fisher Coder <fishercoder@gmail.com>
2024-06-14 08:00:52 -0700

    add 1060

---
+++ src/test/java/com/fishercoder/_1060Test.java
@@ -0,0 +1,36 @@
+package com.fishercoder;
+
+import com.fishercoder.solutions._1060;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class _1060Test {
+    private static _1060.Solution1 solution1;
+    private static _1060.Solution2 solution2;
+
+    @BeforeEach
+    public void setup() {
+        solution1 = new _1060.Solution1();
+        solution2 = new _1060.Solution2();
+    }
+
+    @Test
+    public void test1() {
+        assertEquals(5, solution1.missingElement(new int[]{4, 7, 9, 10}, 1));
+        assertEquals(5, solution2.missingElement(new int[]{4, 7, 9, 10}, 1));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(8, solution1.missingElement(new int[]{4, 7, 9, 10}, 3));
+        assertEquals(8, solution2.missingElement(new int[]{4, 7, 9, 10}, 3));
+    }
+
+    @Test
+    public void test3() {
+        assertEquals(6, solution1.missingElement(new int[]{1, 2, 4}, 3));
+        assertEquals(6, solution2.missingElement(new int[]{1, 2, 4}, 3));
+    }
+}
