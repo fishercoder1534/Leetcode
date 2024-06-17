7c9a992d3e7114c451ebc68e1cf6848f3a1adefc
Fisher Coder <fishercoder@gmail.com>
2024-06-17 07:59:01 -0700

    add a solution for 276

--- src/main/java/com/fishercoder/solutions/firstthousand/_276.java
+++ src/main/java/com/fishercoder/solutions/firstthousand/_276.java
@@ -2,6 +2,28 @@ package com.fishercoder.solutions.firstthousand;
 
 public class _276 {
     public static class Solution1 {
+        /**
+         * Credit: https://leetcode.com/problems/paint-fence/editorial/
+         * 1. base case: dp[0] = k; dp[1] = k * k;
+         * 2. recurrence: dp[i] = dp[i - 1] * (k - 1) + dp[i - 2] * (k - 1)
+         * dp[i - 1] * (k - 1) means to use a different color than the (i-1)th post to paint ith post
+         * dp[i - 2] * (k - 1) means to use the same color as the (i - 1)th post, but different from (i - 2)th post to paint the ith post
+         */
+        public int numWays(int n, int k) {
+            int[] dp = new int[n];
+            dp[0] = k;
+            dp[1] = k * k;
+            for (int i = 2; i < n; i++) {
+                dp[i] = dp[i - 1] * (k - 1) + dp[i - 2] * (k - 1);
+            }
+            return dp[n - 1];
+        }
+    }
+
+    public static class Solution2 {
+        /**
+         * The above solution could be further optimized to use O(1) space.
+         */
         public int numWays(int n, int k) {
             if (n == 0) {
                 return 0;
---
+++ src/test/java/com/fishercoder/_276Test.java
@@ -0,0 +1,25 @@
+package com.fishercoder;
+
+import com.fishercoder.solutions.firstthousand._276;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class _276Test {
+    private static _276.Solution1 solution1;
+    private static _276.Solution2 solution2;
+
+    @BeforeEach
+    public void setup() {
+        solution1 = new _276.Solution1();
+        solution2 = new _276.Solution2();
+    }
+
+    @Test
+    public void test1() {
+        assertEquals(6, solution1.numWays(3, 2));
+        assertEquals(6, solution2.numWays(3, 2));
+    }
+
+}
