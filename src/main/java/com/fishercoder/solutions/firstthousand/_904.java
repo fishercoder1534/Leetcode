434118e8e15a9c342282a489f7ea8e28a563ff7a
Fisher Coder <fishercoder@gmail.com>
2024-06-24 07:16:40 -0700

    update 904

--- src/main/java/com/fishercoder/solutions/firstthousand/_904.java
+++ src/main/java/com/fishercoder/solutions/firstthousand/_904.java
@@ -5,26 +5,32 @@ import java.util.Set;
 
 public class _904 {
     public static class Solution1 {
-        public int totalFruit(int[] tree) {
+        /**
+         * This is a two-pointer solution, a.k.a sliding window
+         */
+        public int totalFruit(int[] fruits) {
             int maxFruits = 0;
             Set<Integer> set = new HashSet<>();
             int startIndex = 0;
-            for (int i = 0; i < tree.length; i++) {
-                if (set.size() < 2 || set.contains(tree[i])) {
-                    set.add(tree[i]);
-                    maxFruits = Math.max(maxFruits, i - startIndex + 1);
+            for (int i = 0; i < fruits.length; i++) {
+                if (set.size() < 2 || set.contains(fruits[i])) {
+                    //either one of the two cases, we keep adding fruits[i] into the set and expand i to the right
                 } else {
-                    int lastOne = tree[i - 1];
+                    //in other cases, we know there's a 3rd type of fruit we just encountered,
+                    // so keep the 2nd type of fruit as lastOne, go backwards,
+                    // find the first 1st type of fruit as j, set startIndex = j + 1,
+                    // remove the 1st type of fruit from the set and break
+                    int lastOne = fruits[i - 1];
                     for (int j = i - 2; j >= 0; j--) {
-                        if (tree[j] != lastOne) {
+                        if (fruits[j] != lastOne) {
                             startIndex = j + 1;
-                            set.remove(tree[j]);
+                            set.remove(fruits[j]);
                             break;
                         }
                     }
-                    set.add(tree[i]);
-                    maxFruits = Math.max(maxFruits, i - startIndex + 1);
                 }
+                set.add(fruits[i]);
+                maxFruits = Math.max(maxFruits, i - startIndex + 1);
             }
             return maxFruits;
         }
--- src/test/java/com/fishercoder/firstthousand/_904Test.java
+++ src/test/java/com/fishercoder/firstthousand/_904Test.java
@@ -1,47 +1,53 @@
 package com.fishercoder.firstthousand;
 
 import com.fishercoder.solutions.firstthousand._904;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class _904Test {
     private static _904.Solution1 solution1;
-    private static int[] tree;
+    private static int[] fruits;
 
-    @BeforeClass
-    public static void setup() {
+    @BeforeEach
+    public void setup() {
         solution1 = new _904.Solution1();
     }
 
     @Test
     public void test1() {
-        tree = new int[]{1, 2, 1};
-        assertEquals(3, solution1.totalFruit(tree));
+        fruits = new int[]{1, 2, 1};
+        assertEquals(3, solution1.totalFruit(fruits));
     }
 
     @Test
     public void test2() {
-        tree = new int[]{0, 1, 2, 2};
-        assertEquals(3, solution1.totalFruit(tree));
+        fruits = new int[]{0, 1, 2, 2};
+        assertEquals(3, solution1.totalFruit(fruits));
     }
 
     @Test
     public void test3() {
-        tree = new int[]{1, 2, 3, 2, 2};
-        assertEquals(4, solution1.totalFruit(tree));
+        fruits = new int[]{1, 2, 3, 2, 2};
+        assertEquals(4, solution1.totalFruit(fruits));
     }
 
     @Test
     public void test4() {
-        tree = new int[]{3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4};
-        assertEquals(5, solution1.totalFruit(tree));
+        fruits = new int[]{3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4};
+        assertEquals(5, solution1.totalFruit(fruits));
     }
 
     @Test
     public void test5() {
-        tree = new int[]{0, 1, 6, 6, 4, 4, 6};
-        assertEquals(5, solution1.totalFruit(tree));
+        fruits = new int[]{0, 1, 6, 6, 4, 4, 6};
+        assertEquals(5, solution1.totalFruit(fruits));
+    }
+
+    @Test
+    public void test6() {
+        fruits = new int[]{0, 1, 1, 4, 3};
+        assertEquals(3, solution1.totalFruit(fruits));
     }
 }
