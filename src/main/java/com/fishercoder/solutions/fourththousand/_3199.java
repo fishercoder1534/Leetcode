6f719363390f9290607faba2090f254144f13958
Fisher Coder <fishercoder@gmail.com>
2024-06-28 15:15:32 -0700

    add 3199

---
+++ src/main/java/com/fishercoder/solutions/fourththousand/_3199.java
@@ -0,0 +1,31 @@
+package com.fishercoder.solutions.fourththousand;
+
+public class _3199 {
+    public static class Solution1 {
+        public int tripletCount(int[] a, int[] b, int[] c) {
+            int count = 0;
+            for (int i = 0; i < a.length; i++) {
+                for (int j = 0; j < b.length; j++) {
+                    for (int k = 0; k < c.length; k++) {
+                        int xor = a[i] ^ b[j] ^ c[k];
+                        if (evenSetBits(xor)) {
+                            count++;
+                        }
+                    }
+                }
+            }
+            return count;
+        }
+
+        private boolean evenSetBits(int num) {
+            int bits = 0;
+            //this is the idea of calculating hamming weight:
+            //https://github.com/fishercoder1534/Leetcode/blob/master/src/main/java/com/fishercoder/solutions/firstthousand/_191.java#L16_L23
+            while (num != 0) {
+                bits++;
+                num &= num - 1;
+            }
+            return bits % 2 == 0;
+        }
+    }
+}
---
+++ src/test/java/com/fishercoder/fourththousand/_3199Test.java
@@ -0,0 +1,32 @@
+package com.fishercoder.fourththousand;
+
+import com.fishercoder.solutions.fourththousand._3199;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class _3199Test {
+    private static _3199.Solution1 solution1;
+
+    @BeforeEach
+    public void setup() {
+        solution1 = new _3199.Solution1();
+    }
+
+    @Test
+    public void test1() {
+        assertEquals(1, solution1.tripletCount(new int[]{1}, new int[]{2}, new int[]{3}));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(4, solution1.tripletCount(new int[]{1, 1}, new int[]{2, 3}, new int[]{1, 5}));
+    }
+
+    @Test
+    public void test3() {
+        assertEquals(9, solution1.tripletCount(new int[]{0, 6, 0}, new int[]{8, 8, 4}, new int[]{6, 9, 2}));
+    }
+
+}
