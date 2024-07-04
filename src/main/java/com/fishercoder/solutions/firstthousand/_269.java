d1213da50de21aaab9dfc69c77e91a4f64a5f3e3
Fisher Coder <fishercoder@gmail.com>
2024-07-04 08:58:53 -0700

    update 269

--- src/main/java/com/fishercoder/solutions/firstthousand/_269.java
+++ src/main/java/com/fishercoder/solutions/firstthousand/_269.java
@@ -9,20 +9,21 @@ import java.util.Set;
 
 public class _269 {
     public static class Solution1 {
-
         /**
          * reference: https://discuss.leetcode.com/topic/28308/java-ac-solution-using-bfs
          */
         public String alienOrder(String[] words) {
-            Map<Character, Set<Character>> map = new HashMap<>();
-            Map<Character, Integer> degree = new HashMap<>();
-            String result = "";
+            Map<Character, Set<Character>> charToSmallerCharsMap = new HashMap<>();//this map means all chars in the value set are after the char in the key
+            Map<Character, Integer> indegreeMap = new HashMap<>();//this map means how many chars are before this given char
+            StringBuilder result = new StringBuilder();
             if (words == null || words.length == 0) {
-                return result;
+                return result.toString();
             }
             for (String s : words) {
                 for (char c : s.toCharArray()) {
-                    degree.put(c, 0);
+                    //we go through each word, nothing to compare, so each char's degree should be zero
+                    //only when we compare words[i] with words[i+1], we know the order of different chars
+                    indegreeMap.put(c, 0);
                 }
             }
             for (int i = 0; i < words.length - 1; i++) {
@@ -31,46 +32,45 @@ public class _269 {
                 if (curr.length() > next.length() && curr.startsWith(next)) {
                     return "";
                 }
-                int minLen = Math.min(curr.length(), next.length());
-                for (int j = 0; j < minLen; j++) {
+                for (int j = 0; j < curr.length(); j++) {
                     char c1 = curr.charAt(j);
                     char c2 = next.charAt(j);
                     if (c1 != c2) {
-                        Set<Character> set = new HashSet<>();
-                        if (map.containsKey(c1)) {
-                            set = map.get(c1);
-                        }
+                        Set<Character> set = charToSmallerCharsMap.getOrDefault(c1, new HashSet<>());
                         if (!set.contains(c2)) {
                             set.add(c2);
-                            map.put(c1, set);
-                            degree.put(c2, degree.get(c2) + 1);
+                            charToSmallerCharsMap.put(c1, set);
+                            indegreeMap.put(c2, indegreeMap.get(c2) + 1);
                         }
+                        //no longer need to continue iterating through either one of these two words and should not to,
+                        //because the first two chars at the same position of these two words that differ decides the order
                         break;
                     }
                 }
             }
             Queue<Character> queue = new LinkedList<>();
-            for (char c : degree.keySet()) {
-                if (degree.get(c) == 0) {
+            for (char c : indegreeMap.keySet()) {
+                if (indegreeMap.get(c) == 0) {
+                    //this means no chars come before this char, so they should be at the head of this alien dictionary
                     queue.offer(c);
                 }
             }
             while (!queue.isEmpty()) {
                 char curr = queue.poll();
-                result += curr;
-                if (map.containsKey(curr)) {
-                    for (char c : map.get(curr)) {
-                        degree.put(c, degree.get(c) - 1);
-                        if (degree.get(c) == 0) {
+                result.append(curr);
+                if (charToSmallerCharsMap.containsKey(curr)) {
+                    for (char c : charToSmallerCharsMap.get(curr)) {
+                        indegreeMap.put(c, indegreeMap.get(c) - 1);
+                        if (indegreeMap.get(c) == 0) {
                             queue.offer(c);
                         }
                     }
                 }
             }
-            if (result.length() != degree.size()) {
+            if (result.length() != indegreeMap.size()) {
                 return "";
             }
-            return result;
+            return result.toString();
         }
     }
 
