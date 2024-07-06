df6af3c87211f7aa72a50ab6faa72ef8eafe09e7
Fisher Coder <fishercoder@gmail.com>
2024-07-06 09:34:03 -0700

    add 3206

---
+++ src/main/java/com/fishercoder/solutions/firstthousand/_3206.java
@@ -0,0 +1,26 @@
+package com.fishercoder.solutions.firstthousand;
+
+public class _3206 {
+    public static class Solution1 {
+        public int numberOfAlternatingGroups(int[] colors) {
+            int result = 0;
+            int len = colors.length;
+            for (int i = 0; i < len; i++) {
+                if (i == 0) {
+                    if (colors[i] != colors[len - 1] && colors[i] != colors[i + 1]) {
+                        result++;
+                    }
+                } else if (i < len - 1) {
+                    if (colors[i] != colors[i - 1] && colors[i] != colors[i + 1]) {
+                        result++;
+                    }
+                } else {
+                    if (colors[i] != colors[i - 1] && colors[i] != colors[0]) {
+                        result++;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
