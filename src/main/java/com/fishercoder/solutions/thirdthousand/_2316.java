4448cad8f342ff9c5fef3a40ca5898658f29d4e3
Fisher Coder <fishercoder@gmail.com>
2024-07-08 08:14:00 -0700

    add 2316

---
+++ src/main/java/com/fishercoder/solutions/thirdthousand/_2316.java
@@ -0,0 +1,51 @@
+package com.fishercoder.solutions.thirdthousand;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class _2316 {
+    public static class Solution1 {
+        public long countPairs(int n, int[][] edges) {
+            UnionFind uf = new UnionFind(n);
+            //this union is a first pass which doesn't union all nodes completely, if you set a debug point, you can clearly see that
+            for (int[] edge : edges) {
+                uf.union(edge[0], edge[1]);
+            }
+            Map<Integer, Integer> countMap = new HashMap<>();
+            //run i = 0 through to n - 1 again, and call find(), this will completely union all connected nodes
+            for (int i = 0; i < n; i++) {
+                int id = uf.find(i);
+                countMap.put(id, countMap.getOrDefault(id, 0) + 1);
+            }
+            long pairs = 0L;
+            long remaining = n;
+            for (int size : countMap.values()) {
+                pairs += size * (remaining - size);
+                remaining -= size;
+            }
+            return pairs;
+        }
+
+        class UnionFind {
+            int[] ids;
+
+            public UnionFind(int n) {
+                this.ids = new int[n];
+                for (int i = 0; i < n; i++) {
+                    this.ids[i] = i;
+                }
+            }
+
+            public int find(int x) {
+                if (ids[x] != x) {
+                    ids[x] = find(ids[x]);
+                }
+                return ids[x];
+            }
+
+            public void union(int x, int y) {
+                ids[find(x)] = find(y);
+            }
+        }
+    }
+}
---
+++ src/test/java/com/fishercoder/thirdthousand/_2316Test.java
@@ -0,0 +1,27 @@
+package com.fishercoder.thirdthousand;
+
+import com.fishercoder.solutions.thirdthousand._2316;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class _2316Test {
+    private static _2316.Solution1 solution1;
+
+    @BeforeEach
+    public void setup() {
+        solution1 = new _2316.Solution1();
+    }
+
+    @Test
+    public void test1() {
+        assertEquals(0, solution1.countPairs(3, new int[][]{{0, 1}, {0, 2}, {1, 2}}));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(14, solution1.countPairs(7, new int[][]{{0, 2}, {0, 5}, {2, 4}, {1, 6}, {5, 4}}));
+    }
+
+}
