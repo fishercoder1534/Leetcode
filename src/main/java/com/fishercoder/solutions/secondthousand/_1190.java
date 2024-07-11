1f4751fcdecd8bfccd0366d2cdb88da9339a3e7f
Fisher Coder <fishercoder@gmail.com>
2024-07-11 06:56:30 -0700

    add a solution for 1190

--- src/main/java/com/fishercoder/solutions/secondthousand/_1190.java
+++ src/main/java/com/fishercoder/solutions/secondthousand/_1190.java
@@ -1,5 +1,6 @@
 package com.fishercoder.solutions.secondthousand;
 
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
@@ -27,6 +28,34 @@ public class _1190 {
             StringBuilder sb = new StringBuilder();
             while (!stack.isEmpty()) {
                 sb.append(stack.pop());
+            }
+            return sb.reverse().toString();
+        }
+    }
+
+    public static class Solution2 {
+
+        public static String reverseParentheses(String s) {
+            Deque<String> stack = new LinkedList<>();
+            for (char c : s.toCharArray()) {
+                if (c == '(' || Character.isAlphabetic(c)) {
+                    stack.addLast(c + "");
+                } else {
+                    StringBuilder innerSb = new StringBuilder();
+                    while (!stack.isEmpty()) {
+                        if (stack.peekLast().equals("(")) {
+                            stack.pollLast();
+                            break;
+                        } else {
+                            innerSb.append(stack.pollLast());
+                        }
+                    }
+                    stack.addLast(innerSb.reverse().toString());
+                }
+            }
+            StringBuilder sb = new StringBuilder();
+            while (!stack.isEmpty()) {
+                sb.append(stack.pollLast());
             }
             return sb.reverse().toString();
         }
--- src/test/java/com/fishercoder/secondthousand/_1190Test.java
+++ src/test/java/com/fishercoder/secondthousand/_1190Test.java
@@ -1,22 +1,25 @@
 package com.fishercoder.secondthousand;
 
 import com.fishercoder.solutions.secondthousand._1190;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class _1190Test {
     private static _1190.Solution1 solution1;
+    private static _1190.Solution2 solution2;
 
-    @BeforeClass
-    public static void setup() {
+    @BeforeEach
+    public void setup() {
         solution1 = new _1190.Solution1();
+        solution2 = new _1190.Solution2();
     }
 
     @Test
     public void test1() {
         assertEquals("dcba", solution1.reverseParentheses("(abcd)"));
+        assertEquals("dcba", solution2.reverseParentheses("(abcd)"));
     }
 
     @Test
