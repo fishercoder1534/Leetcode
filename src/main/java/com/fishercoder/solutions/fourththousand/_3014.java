e74e7ca73dae611c1fd0a7de928d05cba19b4a3c
Fisher Coder <fishercoder@gmail.com>
2024-07-13 11:40:35 -0700

    update 3014

--- src/main/java/com/fishercoder/solutions/fourththousand/_3014.java
+++ src/main/java/com/fishercoder/solutions/fourththousand/_3014.java
@@ -2,19 +2,19 @@ package com.fishercoder.solutions.fourththousand;
 
 public class _3014 {
     public static class Solution1 {
-        int KEY_COUNT = 8;
 
         public int minimumPushes(String word) {
+            int keyCount = 8;
             int times = 1;
             int pushes = 0;
             int len = word.length();
             while (len > 0) {
-                if (len < KEY_COUNT) {
+                if (len < keyCount) {
                     pushes += times * len;
                     return pushes;
                 } else {
-                    pushes += times * KEY_COUNT;
-                    len -= KEY_COUNT;
+                    pushes += times * keyCount;
+                    len -= keyCount;
                     times++;
                 }
             }
