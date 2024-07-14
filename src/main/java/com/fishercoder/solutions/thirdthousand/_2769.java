c44bfbb06897f359cd77a9a5306a327a9c1e4bca
Fisher Coder <fishercoder@gmail.com>
2024-07-14 11:48:51 -0700

    add 2765

---
+++ src/main/java/com/fishercoder/solutions/thirdthousand/_2769.java
@@ -0,0 +1,9 @@
+package com.fishercoder.solutions.thirdthousand;
+
+public class _2769 {
+    public static class Solution1 {
+        public int theMaximumAchievableX(int num, int t) {
+            return num + t * 2;
+        }
+    }
+}
