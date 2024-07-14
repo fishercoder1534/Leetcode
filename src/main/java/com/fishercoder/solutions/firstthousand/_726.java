6f0918cca96fa2cdc543c3430912dfa90920d14c
Fisher Coder <fishercoder@gmail.com>
2024-07-14 09:14:05 -0700

    add 726

---
+++ src/main/java/com/fishercoder/solutions/firstthousand/_726.java
@@ -0,0 +1,107 @@
+package com.fishercoder.solutions.firstthousand;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class _726 {
+    public static class Solution1 {
+        /**
+         * My completely original solution:
+         * 1. use a stack;
+         * 2. whenever we encounter the open paren, we push it into the top of the stack;
+         * 3. whenever we encounter an uppercase, we check to get its full atom letters,
+         * then check to get the number after it if there's any, then form a pair objet and push onto the stack;
+         * 4. whenever we encounter the closed paren, we check if there's any number after it,
+         * then poll all items on top of the stack off onto a new/temp stack until we encounter the corresponding open paren,
+         * then add these items from the temp stack back into the original/main stack;
+         */
+        public String countOfAtoms(String formula) {
+            Deque<Pair> stack = new LinkedList<>();
+            for (int i = 0; i < formula.length(); i++) {
+                char curr = formula.charAt(i);
+                if (curr == '(') {
+                    stack.addLast(new Pair("(", 1));
+                } else if (Character.isUpperCase(curr)) {
+                    StringBuilder sb = new StringBuilder(curr + "");
+                    i++;
+                    while (i < formula.length() && Character.isLowerCase(formula.charAt(i))) {
+                        sb.append(formula.charAt(i++));
+                    }
+                    if (i < formula.length()) {
+                        if (Character.isUpperCase(formula.charAt(i)) || formula.charAt(i) == '(' || formula.charAt(i) == ')') {
+                            //no numbers
+                            stack.addLast(new Pair(sb.toString(), 1));
+                            i--;
+                        } else {
+                            //there are numbers
+                            StringBuilder numberSb = new StringBuilder();
+                            while (i < formula.length() && Character.isDigit(formula.charAt(i))) {
+                                numberSb.append(formula.charAt(i++));
+                            }
+                            i--;
+                            stack.addLast(new Pair(sb.toString(), Integer.parseInt(numberSb.toString())));
+                        }
+                    } else {
+                        stack.addLast(new Pair(sb.toString(), 1));
+                    }
+                } else if (curr == ')') {
+                    i++;
+                    StringBuilder sb = new StringBuilder();
+                    while (i < formula.length() && Character.isDigit(formula.charAt(i))) {
+                        sb.append(formula.charAt(i));
+                        i++;
+                    }
+                    i--;
+                    int number = 1;
+                    if (sb.length() > 0) {
+                        number = Integer.parseInt(sb.toString());
+                    }
+                    Deque<Pair> stack2 = new LinkedList<>();
+                    while (!stack.isEmpty() && !stack.peekLast().atom.equals("(")) {
+                        Pair pair = stack.pollLast();
+                        stack2.addLast(new Pair(pair.atom, pair.count * number));
+                    }
+                    stack.pollLast();//poll "(" off of the stack
+                    while (!stack2.isEmpty()) {
+                        stack.addLast(stack2.pollLast());
+                    }
+                }
+            }
+            List<Pair> list = new ArrayList<>();
+            while (!stack.isEmpty()) {
+                list.add(stack.pollLast());
+            }
+            //now merge the same atoms
+            Map<String, Integer> map = new HashMap<>();
+            for (Pair pair : list) {
+                map.put(pair.atom, map.getOrDefault(pair.atom, 0) + pair.count);
+            }
+            //now add the merged atoms into the list again before sorting them
+            list.clear();
+            for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                list.add(new Pair(entry.getKey(), entry.getValue()));
+            }
+            Collections.sort(list, (a, b) -> a.atom.compareToIgnoreCase(b.atom));
+            StringBuilder sb = new StringBuilder();
+            for (Pair pair : list) {
+                sb.append(pair.atom + (pair.count == 1 ? "" : pair.count));
+            }
+            return sb.toString();
+        }
+
+        class Pair {
+            String atom;
+            int count;
+
+            public Pair(String atom, int count) {
+                this.atom = atom;
+                this.count = count;
+            }
+        }
+    }
+}
---
+++ src/test/java/com/fishercoder/firstthousand/_726Test.java
@@ -0,0 +1,36 @@
+package com.fishercoder.firstthousand;
+
+import com.fishercoder.solutions.firstthousand._726;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class _726Test {
+    private static _726.Solution1 solution1;
+
+    @BeforeEach
+    public void setup() {
+        solution1 = new _726.Solution1();
+    }
+
+    @Test
+    public void test1() {
+        assertEquals("H2O", solution1.countOfAtoms("H2O"));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals("H2MgO2", solution1.countOfAtoms("Mg(OH)2"));
+    }
+
+    @Test
+    public void test3() {
+        assertEquals("K4N2O14S4", solution1.countOfAtoms("K4(ON(SO3)2)2"));
+    }
+
+    @Test
+    public void test4() {
+        assertEquals("H2MgNO", solution1.countOfAtoms("Mg(H2O)N"));
+    }
+}
