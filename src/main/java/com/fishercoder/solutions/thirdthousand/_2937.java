960eb00e5406e82412f04dee0a7b7e0b148be45a
Fisher Coder <fishercoder@gmail.com>
2024-07-14 16:38:14 -0700

    add 2937

---
+++ src/main/java/com/fishercoder/solutions/thirdthousand/_2937.java
@@ -0,0 +1,29 @@
+package com.fishercoder.solutions.thirdthousand;
+
+public class _2937 {
+    public static class Solution1 {
+        public int findMinimumOperations(String s1, String s2, String s3) {
+            if (s1.charAt(0) != s2.charAt(0) || s1.charAt(0) != s3.charAt(0) || s2.charAt(0) != s3.charAt(0)) {
+                return -1;
+            }
+            int minOps = 0;
+            int minLen = Math.min(s1.length(), Math.min(s2.length(), s3.length()));
+            int i = 1, j = 1, k = 1;
+            for (; i < minLen && j < minLen && k < minLen; i++, j++, k++) {
+                if (s1.charAt(i) != s2.charAt(j) || s2.charAt(j) != s3.charAt(k)) {
+                    minOps += s1.length() - i;
+                    minOps += s2.length() - j;
+                    minOps += s3.length() - k;
+                    i = s1.length();
+                    j = s2.length();
+                    k = s3.length();
+                    break;
+                }
+            }
+            minOps += s1.length() - i;
+            minOps += s2.length() - j;
+            minOps += s3.length() - k;
+            return minOps;
+        }
+    }
+}
---
+++ src/test/java/com/fishercoder/thirdthousand/_2937Test.java
@@ -0,0 +1,31 @@
+package com.fishercoder.thirdthousand;
+
+import com.fishercoder.solutions.thirdthousand._2937;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class _2937Test {
+    private static _2937.Solution1 solution1;
+
+    @BeforeEach
+    public void setup() {
+        solution1 = new _2937.Solution1();
+    }
+
+    @Test
+    public void test1() {
+        assertEquals(2, solution1.findMinimumOperations("abc", "abb", "ab"));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(3, solution1.findMinimumOperations("a", "aabc", "a"));
+    }
+
+    @Test
+    public void test3() {
+        assertEquals(7, solution1.findMinimumOperations("ca", "cccabb", "cb"));
+    }
+}
