e0f70f2d503fa33e6ed51be7ab445824db6b044e
Fisher Coder <fishercoder@gmail.com>
2024-07-16 07:44:32 -0700

    add 2096

---
+++ src/main/java/com/fishercoder/solutions/thirdthousand/_2096.java
@@ -0,0 +1,63 @@
+package com.fishercoder.solutions.thirdthousand;
+
+import com.fishercoder.common.classes.TreeNode;
+
+public class _2096 {
+    public static class Solution1 {
+        /**
+         * Steps for this problem:
+         * 1. find the path from root the start and dest respectively, mark them using two directions: 'L' and 'R', i.e. you can only go down from root, so there's no up, 'U' direction;
+         * 2. the LCA (the lowest common ancestor) of start and dest will be the joint of the shortest path;
+         * 3. find the longest common prefix of these two paths, that can be cut off;
+         * 4. reverse the startPath, so it becomes the path from start to LCA, then concatenate with destPath
+         */
+        public String getDirections(TreeNode root, int startValue, int destValue) {
+            StringBuilder sb = new StringBuilder();
+            String pathForStart = "";
+            if (findPathFromRoot(root, startValue, sb)) {
+                pathForStart = sb.toString();
+            }
+            sb.setLength(0);
+            String pathForDest = "";
+            if (findPathFromRoot(root, destValue, sb)) {
+                pathForDest = sb.toString();
+            }
+            int lastIdenticalCharIndex = -1;
+            int minLen = Math.min(pathForStart.length(), pathForDest.length());
+            for (int i = 0; i < minLen; i++) {
+                if (pathForStart.charAt(i) == pathForDest.charAt(i)) {
+                    lastIdenticalCharIndex = i;
+                } else {
+                    break;
+                }
+            }
+            sb.setLength(0);
+            sb.append(pathForStart.substring(lastIdenticalCharIndex + 1));
+            for (int i = 0; i < sb.length(); i++) {
+                if (sb.charAt(i) == 'L' || sb.charAt(i) == 'R') {
+                    sb.setCharAt(i, 'U');
+                }
+            }
+            sb.append(pathForDest.substring(lastIdenticalCharIndex + 1));
+            return sb.toString();
+        }
+
+        private boolean findPathFromRoot(TreeNode root, int target, StringBuilder sb) {
+            if (root == null) {
+                return false;
+            }
+            if (root.val == target) {
+                return true;
+            }
+            if (findPathFromRoot(root.left, target, sb.append("L"))) {
+                return true;
+            }
+            sb.setLength(sb.length() - 1);
+            if (findPathFromRoot(root.right, target, sb.append("R"))) {
+                return true;
+            }
+            sb.setLength(sb.length() - 1);
+            return false;
+        }
+    }
+}
---
+++ src/test/java/com/fishercoder/thirdthousand/_2096Test.java
@@ -0,0 +1,49 @@
+package com.fishercoder.thirdthousand;
+
+import com.fishercoder.common.classes.TreeNode;
+import com.fishercoder.common.utils.TreeUtils;
+import com.fishercoder.solutions.thirdthousand._2096;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class _2096Test {
+    private static _2096.Solution1 solution1;
+
+    @BeforeEach
+    public void setup() {
+        solution1 = new _2096.Solution1();
+    }
+
+    @Test
+    public void test1() {
+        TreeNode root = TreeUtils.constructBinaryTree(Arrays.asList(5, 1, 2, 3, null, 6, 4));
+        TreeUtils.printBinaryTree(root);
+        assertEquals("UURL", solution1.getDirections(root, 3, 6));
+    }
+
+    @Test
+    public void test2() {
+        TreeNode root = TreeUtils.constructBinaryTree(Arrays.asList(2, 1));
+        TreeUtils.printBinaryTree(root);
+        assertEquals("L", solution1.getDirections(root, 2, 1));
+    }
+
+    @Test
+    public void test3() {
+        TreeNode root = TreeUtils.constructBinaryTree(Arrays.asList(1, 2));
+        TreeUtils.printBinaryTree(root);
+        assertEquals("U", solution1.getDirections(root, 2, 1));
+    }
+
+    @Test
+    public void test4() {
+        TreeNode root = TreeUtils.constructBinaryTree(Arrays.asList(5, 8, 3, 1, null, 4, 7, 6, null, null, null, null, null, null, 2));
+        TreeUtils.printBinaryTree(root);
+        assertEquals("U", solution1.getDirections(root, 4, 3));
+    }
+
+}
