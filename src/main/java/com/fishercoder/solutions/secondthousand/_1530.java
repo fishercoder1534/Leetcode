e7f26c088a58d2369748a7a4baae23b9e8ba4514
Fisher Coder <fishercoder@gmail.com>
2024-07-16 09:41:53 -0700

    add 1530

---
+++ src/main/java/com/fishercoder/solutions/secondthousand/_1530.java
@@ -0,0 +1,72 @@
+package com.fishercoder.solutions.secondthousand;
+
+import com.fishercoder.common.classes.TreeNode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
+public class _1530 {
+    public static class Solution1 {
+        public int countPairs(TreeNode root, int distance) {
+            Map<TreeNode, TreeNode> childToParentMap = new HashMap<>();
+            List<TreeNode> leafNodes = findAllLeaves(root, childToParentMap, new ArrayList<>());
+            int pairs = 0;
+            for (TreeNode leaf : leafNodes) {
+                pairs += bfsToPossibleLeaves(leaf, distance, childToParentMap);
+            }
+            return pairs / 2;
+        }
+
+        private int bfsToPossibleLeaves(TreeNode leaf, int distance, Map<TreeNode, TreeNode> childToParentMap) {
+            Queue<TreeNode> q = new LinkedList<>();
+            q.offer(leaf);
+            Set<TreeNode> visited = new HashSet<>();
+            visited.add(leaf);
+            int count = 0;
+            while (!q.isEmpty() && distance >= 0) {
+                int size = q.size();
+                for (int i = 0; i < size; i++) {
+                    TreeNode curr = q.poll();
+                    if (leaf != curr && curr.left == null && curr.right == null) {
+                        count++;
+                    }
+                    if (curr.left != null && visited.add(curr.left)) {
+                        q.offer(curr.left);
+                    }
+                    if (curr.right != null && visited.add(curr.right)) {
+                        q.offer(curr.right);
+                    }
+                    if (childToParentMap.containsKey(curr) && visited.add(childToParentMap.get(curr))) {
+                        q.offer(childToParentMap.get(curr));
+                    }
+                }
+                distance--;
+            }
+            return count;
+        }
+
+        private List<TreeNode> findAllLeaves(TreeNode node, Map<TreeNode, TreeNode> childToParentMap, List<TreeNode> leafNodes) {
+            if (node == null) {
+                return leafNodes;
+            }
+            if (node.left == null && node.right == null) {
+                leafNodes.add(node);
+            }
+            if (node.left != null) {
+                childToParentMap.put(node.left, node);
+                findAllLeaves(node.left, childToParentMap, leafNodes);
+            }
+            if (node.right != null) {
+                childToParentMap.put(node.right, node);
+                findAllLeaves(node.right, childToParentMap, leafNodes);
+            }
+            return leafNodes;
+        }
+    }
+}
---
+++ src/test/java/com/fishercoder/secondthousand/_1530Test.java
@@ -0,0 +1,39 @@
+package com.fishercoder.secondthousand;
+
+import com.fishercoder.common.classes.TreeNode;
+import com.fishercoder.common.utils.TreeUtils;
+import com.fishercoder.solutions.secondthousand._1530;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class _1530Test {
+    private static _1530.Solution1 solution1;
+    private static TreeNode root;
+
+    @BeforeEach
+    public void setup() {
+        solution1 = new _1530.Solution1();
+    }
+
+    @Test
+    public void test1() {
+        root = TreeUtils.constructBinaryTree(Arrays.asList(1, 2, 3, null, 4));
+        assertEquals(1, solution1.countPairs(root, 3));
+    }
+
+    @Test
+    public void test2() {
+        root = TreeUtils.constructBinaryTree(Arrays.asList(1, 2, 3, 4, 5, 6, 7));
+        assertEquals(2, solution1.countPairs(root, 3));
+    }
+
+    @Test
+    public void test3() {
+        root = TreeUtils.constructBinaryTree(Arrays.asList(7, 1, 4, 6, null, 5, 3, null, null, null, null, null, 2));
+        assertEquals(1, solution1.countPairs(root, 3));
+    }
+}
