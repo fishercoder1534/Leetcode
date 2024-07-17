2ee8b9e96a300f541872afe71727fa7f7511834f
Fisher Coder <fishercoder@gmail.com>
2024-07-17 13:09:10 -0700

    add 3004

---
+++ src/main/java/com/fishercoder/solutions/fourththousand/_3004.java
@@ -0,0 +1,76 @@
+package com.fishercoder.solutions.fourththousand;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class _3004 {
+    public static class Solution1 {
+        /**
+         * My completely original solution.
+         * Practice makes perfect!
+         * Post-order traversal is the way to go since we need to process all children first before processing any particular node.
+         */
+        class ColoredTreeNode {
+            int val;
+            int color;
+            List<ColoredTreeNode> children;
+            boolean allSubtreeSameColor;
+            int totalChildrenCount;
+
+            public ColoredTreeNode(int val, int color) {
+                this.val = val;
+                this.color = color;
+                this.children = new ArrayList<>();
+                this.allSubtreeSameColor = true;//initialize to be true until it's built/proven to be false
+                this.totalChildrenCount = 1;//count itself as its own child
+            }
+        }
+
+        int maxSize = 0;
+
+        public int maximumSubtreeSize(int[][] edges, int[] colors) {
+            if (edges == null || edges.length == 0 || edges[0].length == 0) {
+                return colors.length > 0 ? 1 : 0;
+            }
+            ColoredTreeNode root = buildTree(edges, colors);
+            int totalNodeCount = postOrder(root);
+            if (root.allSubtreeSameColor) {
+                return totalNodeCount;
+            }
+            return maxSize;
+        }
+
+        private int postOrder(ColoredTreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            int totalChildrenCount = 1;//count itself as a child
+            for (ColoredTreeNode child : root.children) {
+                int count = postOrder(child);
+                totalChildrenCount += count;
+                if (root.color != child.color || !child.allSubtreeSameColor) {
+                    root.allSubtreeSameColor = false;
+                }
+            }
+            root.totalChildrenCount = totalChildrenCount;
+            if (root.allSubtreeSameColor) {
+                maxSize = Math.max(maxSize, root.totalChildrenCount);
+            }
+            return totalChildrenCount;
+        }
+
+        private ColoredTreeNode buildTree(int[][] edges, int[] colors) {
+            Map<Integer, ColoredTreeNode> map = new HashMap<>();
+            for (int i = 0; i < edges.length; i++) {
+                ColoredTreeNode parent = map.getOrDefault(edges[i][0], new ColoredTreeNode(edges[i][0], colors[edges[i][0]]));
+                ColoredTreeNode child = map.getOrDefault(edges[i][1], new ColoredTreeNode(edges[i][1], colors[edges[i][1]]));
+                parent.children.add(child);
+                map.put(edges[i][0], parent);
+                map.put(edges[i][1], child);
+            }
+            return map.get(0);
+        }
+    }
+}
---
+++ src/test/java/com/fishercoder/fourththousand/_3004Test.java
@@ -0,0 +1,51 @@
+package com.fishercoder.fourththousand;
+
+import com.fishercoder.solutions.fourththousand._3004;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class _3004Test {
+    private static _3004.Solution1 solution1;
+
+    @BeforeEach
+    public void setup() {
+        solution1 = new _3004.Solution1();
+    }
+
+    @Test
+    public void test1() {
+        assertEquals(1, solution1.maximumSubtreeSize(new int[][]{
+                {0, 1}, {0, 2}, {0, 3}
+        }, new int[]{1, 1, 2, 3}));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(4, solution1.maximumSubtreeSize(new int[][]{
+                {0, 1}, {0, 2}, {0, 3}
+        }, new int[]{1, 1, 1, 1}));
+    }
+
+    @Test
+    public void test3() {
+        assertEquals(3, solution1.maximumSubtreeSize(new int[][]{
+                {0, 1}, {0, 2}, {2, 3}, {2, 4}
+        }, new int[]{1, 2, 3, 3, 3}));
+    }
+
+    @Test
+    public void test4() {
+        assertEquals(1, solution1.maximumSubtreeSize(new int[][]{
+                {}
+        }, new int[]{1}));
+    }
+
+    @Test
+    public void test5() {
+        assertEquals(1, solution1.maximumSubtreeSize(new int[][]{
+                {0, 1}, {1, 2}
+        }, new int[]{1, 1, 2}));
+    }
+}
