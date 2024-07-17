e363de97c33679f7688d37058fab80ff26ba0782
Fisher Coder <fishercoder@gmail.com>
2024-07-17 10:23:02 -0700

    add a solution for 1339

--- src/main/java/com/fishercoder/solutions/secondthousand/_1339.java
+++ src/main/java/com/fishercoder/solutions/secondthousand/_1339.java
@@ -2,14 +2,18 @@ package com.fishercoder.solutions.secondthousand;
 
 import com.fishercoder.common.classes.TreeNode;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class _1339 {
     public static class Solution1 {
         public int maxProduct(TreeNode root) {
             Set<Long> set = new HashSet<>();
-            int total = dfs(root, set);
+            long total = postOrder(root, set);
             long result = 0L;
             for (long sum : set) {
                 result = Math.max(result, sum * (total - sum));
@@ -17,14 +21,69 @@ public class _1339 {
             return (int) (result % 1000000007);
         }
 
-        private int dfs(TreeNode root, Set<Long> set) {
+        private long postOrder(TreeNode root, Set<Long> set) {
             if (root == null) {
                 return 0;
             }
-            root.val += dfs(root.left, set);
-            root.val += dfs(root.right, set);
-            set.add((long) root.val);
-            return root.val;
+            long leftSum = postOrder(root.left, set);
+            long rightSum = postOrder(root.right, set);
+            long sum = root.val + leftSum + rightSum;
+            set.add(sum);
+            return sum;
         }
+    }
+
+    public static class Solution2 {
+        /**
+         * My completely original solution, but much more verbose and uses more extra space.
+         */
+        public int maxProduct(TreeNode root) {
+            Map<TreeNode, Long> sumMap = new HashMap<>();
+            long totalSum = postOrderBuildSumMap(root, sumMap);
+            sumMap.put(root, totalSum);
+            List<long[]> productList = new ArrayList<>();
+            postOrderBuildProductList(root, sumMap, productList, sumMap.get(root));
+            long result = 0L;
+            double modulo = Math.pow(10, 9) + 7;
+            for (long[] p : productList) {
+                long product = p[0] * p[1];
+                result = Math.max(result, product);
+            }
+            return (int) (result % modulo);
+        }
+
+        private void postOrderBuildProductList(TreeNode root, Map<TreeNode, Long> sumMap, List<long[]> productList, Long total) {
+            if (root == null) {
+                return;
+            }
+            if (root.left == null && root.right == null) {
+                return;
+            }
+            postOrderBuildProductList(root.left, sumMap, productList, total);
+            postOrderBuildProductList(root.right, sumMap, productList, total);
+            if (root.left != null) {
+                //cut off left child
+                long leftSum = sumMap.get(root.left);
+                long remainder = total - leftSum;
+                productList.add(new long[]{leftSum, remainder});
+            }
+            if (root.right != null) {
+                long rightSum = sumMap.get(root.right);
+                long remainder = total - rightSum;
+                productList.add(new long[]{rightSum, remainder});
+            }
+        }
+
+        private long postOrderBuildSumMap(TreeNode root, Map<TreeNode, Long> sumMap) {
+            if (root == null) {
+                return 0L;
+            }
+            long leftSum = postOrderBuildSumMap(root.left, sumMap);
+            long rightSum = postOrderBuildSumMap(root.right, sumMap);
+            long sum = leftSum + rightSum + root.val;
+            sumMap.put(root, sum);
+            return sum;
+        }
+
     }
 }
--- src/test/java/com/fishercoder/secondthousand/_1339Test.java
+++ src/test/java/com/fishercoder/secondthousand/_1339Test.java
@@ -3,26 +3,34 @@ package com.fishercoder.secondthousand;
 import com.fishercoder.common.classes.TreeNode;
 import com.fishercoder.common.utils.TreeUtils;
 import com.fishercoder.solutions.secondthousand._1339;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class _1339Test {
     private static _1339.Solution1 solution1;
+    private static _1339.Solution2 solution2;
     private static TreeNode root;
 
-    @BeforeClass
-    public static void setup() {
+    @BeforeEach
+    public void setup() {
         solution1 = new _1339.Solution1();
+        solution2 = new _1339.Solution2();
     }
 
     @Test
     public void test1() {
         root = TreeUtils.constructBinaryTree(Arrays.asList(1, 2, 3, 4, 5, 6));
         assertEquals(110, solution1.maxProduct(root));
+    }
+
+    @Test
+    public void test2() {
+        root = TreeUtils.constructBinaryTree(Arrays.asList(1, 2, 3, 4, 5, 6));
+        assertEquals(110, solution2.maxProduct(root));
     }
 
 }
