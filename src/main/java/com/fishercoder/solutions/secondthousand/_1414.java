fac119350e98d16a83e179872e7c2bc8f54134dd
Fisher Coder <fishercoder@gmail.com>
2024-07-20 16:28:45 -0700

    add 1414

---
+++ src/main/java/com/fishercoder/solutions/secondthousand/_1414.java
@@ -0,0 +1,41 @@
+package com.fishercoder.solutions.secondthousand;
+
+import java.util.PriorityQueue;
+
+public class _1414 {
+    public static class Solution1 {
+        /**
+         * My completely original solution, heap is not really necessary, a regular array/list works out as well.
+         */
+        public int findMinFibonacciNumbers(int k) {
+            PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+            int one = 1;
+            int two = 1;
+            maxHeap.offer(one);
+            maxHeap.offer(two);
+            int three = one + two;
+            while (three <= k) {
+                maxHeap.offer(three);
+                int tmp = two;
+                two = three;
+                one = tmp;
+                three = one + two;
+            }
+            int minNumbers = 0;
+            while (k > 0) {
+                if (maxHeap.contains(k)) {
+                    minNumbers++;
+                    return minNumbers;
+                } else {
+                    while (maxHeap.peek() > k) {
+                        maxHeap.poll();
+                    }
+                    Integer max = maxHeap.poll();
+                    k -= max;
+                    minNumbers++;
+                }
+            }
+            return minNumbers;
+        }
+    }
+}
---
+++ src/test/java/com/fishercoder/secondthousand/_1414Test.java
@@ -0,0 +1,27 @@
+package com.fishercoder.secondthousand;
+
+import com.fishercoder.solutions.secondthousand._1414;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class _1414Test {
+    private static _1414.Solution1 solution1;
+
+    @BeforeEach
+    public void setup() {
+        solution1 = new _1414.Solution1();
+    }
+
+    @Test
+    public void test1() {
+        assertEquals(2, solution1.findMinFibonacciNumbers(7));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(3, solution1.findMinFibonacciNumbers(19));
+    }
+
+}
