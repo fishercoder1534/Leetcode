69f96b1746178998994d0237deb95480bb9f0e79
Fisher Coder <fishercoder@gmail.com>
2024-07-22 08:23:48 -0700

    add 1711

--- src/main/java/com/fishercoder/solutions/secondthousand/_1711.java
+++ src/main/java/com/fishercoder/solutions/secondthousand/_1711.java
@@ -5,24 +5,28 @@ import java.util.Map;
 
 public class _1711 {
     public static class Solution1 {
-        private static final long MODUALR = 1000000007;
+        /**
+         * This is a very brilliant solution:
+         * 1. go through each number only once: for each number, we iterate through all possible power of twos, at max, there's only 21 due to the constraints of this problem;
+         * 2. since it's asking for the sum of two, we can check we have encountered the other number before using a hashmap
+         */
 
         public int countPairs(int[] deliciousness) {
+            final long MODULAR = 1000000007;
             Map<Integer, Integer> map = new HashMap<>();
             long pairs = 0;
-            for (int i = 0; i < deliciousness.length; i++) {
+            for (int del : deliciousness) {
                 int power = 1;
                 //we only need to go up to 21 since one of the constraints is: 0 <= deliciousness[i] <= 2 to the power of 20
                 for (int j = 0; j < 22; j++) {
-                    if (map.containsKey(power - deliciousness[i])) {
-                        pairs += map.get(power - deliciousness[i]);
-                        pairs %= MODUALR;
+                    if (map.containsKey(power - del)) {
+                        pairs += map.get(power - del);
                     }
                     power *= 2;
                 }
-                map.put(deliciousness[i], map.getOrDefault(deliciousness[i], 0) + 1);
+                map.put(del, map.getOrDefault(del, 0) + 1);
             }
-            return (int) pairs;
+            return (int) (pairs % MODULAR);
         }
 
     }
--- src/test/java/com/fishercoder/secondthousand/_1711Test.java
+++ src/test/java/com/fishercoder/secondthousand/_1711Test.java
@@ -1,17 +1,17 @@
 package com.fishercoder.secondthousand;
 
 import com.fishercoder.solutions.secondthousand._1711;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class _1711Test {
     private static _1711.Solution1 solution1;
     private static int[] deliciousness;
 
-    @BeforeClass
-    public static void setup() {
+    @BeforeEach
+    public void setup() {
         solution1 = new _1711.Solution1();
     }
 
