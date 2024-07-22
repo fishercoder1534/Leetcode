3621a47115cd83c8c04b385921a421186a43c3ee
Fisher Coder <fishercoder@gmail.com>
2024-07-22 06:53:53 -0700

    add 2437

---
+++ src/main/java/com/fishercoder/solutions/thirdthousand/_2437.java
@@ -0,0 +1,29 @@
+package com.fishercoder.solutions.thirdthousand;
+
+public class _2437 {
+    public static class Solution1 {
+        public int countTime(String time) {
+            int[] count = new int[]{2, 10, 0, 6, 10};
+            int times = 1;
+            for (int i = 0; i < time.length(); i++) {
+                if (time.charAt(i) == '?') {
+                    if (i == 0 && time.charAt(0) == '?') {
+                        if (time.charAt(1) == '?') {
+                            times *= 24;
+                        } else if (Character.getNumericValue(time.charAt(1)) < 4) {
+                            times *= 3;
+                        } else if (Character.getNumericValue(time.charAt(1)) >= 4) {
+                            times *= 2;
+                        }
+                        i++;
+                    } else if (i == 1 && (time.charAt(0) == '2' || time.charAt(0) == '?')) {
+                        times *= 4;
+                    } else {
+                        times *= count[i];
+                    }
+                }
+            }
+            return times;
+        }
+    }
+}
---
+++ src/test/java/com/fishercoder/thirdthousand/_2437Test.java
@@ -0,0 +1,31 @@
+package com.fishercoder.thirdthousand;
+
+import com.fishercoder.solutions.thirdthousand._2437;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class _2437Test {
+    private static _2437.Solution1 solution1;
+
+    @BeforeEach
+    public void setup() {
+        solution1 = new _2437.Solution1();
+    }
+
+    @Test
+    public void test1() {
+        assertEquals(1440, solution1.countTime("??:??"));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(3, solution1.countTime("?2:16"));
+    }
+
+    @Test
+    public void test3() {
+        assertEquals(2, solution1.countTime("?5:00"));
+    }
+}
