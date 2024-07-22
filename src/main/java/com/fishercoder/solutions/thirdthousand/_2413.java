3d97db9f742396b570bad2c779400bd454f1e80d
Fisher Coder <fishercoder@gmail.com>
2024-07-22 06:30:53 -0700

    add 2413

---
+++ src/main/java/com/fishercoder/solutions/thirdthousand/_2413.java
@@ -0,0 +1,9 @@
+package com.fishercoder.solutions.thirdthousand;
+
+public class _2413 {
+    public static class Solution1 {
+        public int smallestEvenMultiple(int n) {
+            return n % 2 == 0 ? n : n * 2;
+        }
+    }
+}
