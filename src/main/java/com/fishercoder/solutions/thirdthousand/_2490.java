78a34261c590d577bc38faaa7ffa7a5d1dfbcb66
Fisher Coder <fishercoder@gmail.com>
2024-07-24 12:02:32 -0700

    add 2490

---
+++ src/main/java/com/fishercoder/solutions/thirdthousand/_2490.java
@@ -0,0 +1,15 @@
+package com.fishercoder.solutions.thirdthousand;
+
+public class _2490 {
+    public static class Solution1 {
+        public boolean isCircularSentence(String sentence) {
+            String[] words = sentence.split("\\ ");
+            for (int i = 0; i < words.length - 1; i++) {
+                if (words[i].charAt(words[i].length() - 1) != words[i + 1].charAt(0)) {
+                    return false;
+                }
+            }
+            return words[0].charAt(0) == words[words.length - 1].charAt(words[words.length - 1].length() - 1);
+        }
+    }
+}
