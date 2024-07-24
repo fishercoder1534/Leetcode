357fd5ca357d379c506160ced49f5dcbec1cace6
Fisher Coder <fishercoder@gmail.com>
2024-07-24 08:27:26 -0700

    add 722

---
+++ src/main/java/com/fishercoder/solutions/firstthousand/_722.java
@@ -0,0 +1,51 @@
+package com.fishercoder.solutions.firstthousand;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _722 {
+    public static class Solution1 {
+        /**
+         * My completely original solution.
+         * Not a hard one, just some corner cases to consider.
+         */
+        public List<String> removeComments(String[] source) {
+            List<String> result = new ArrayList<>();
+            StringBuilder sb = new StringBuilder();
+            boolean possiblyMultilineComment = false;
+            for (String line : source) {
+                for (int i = 0; i < line.length(); i++) {
+                    if (line.charAt(i) == '/' && i + 1 < line.length()) {
+                        //adding (&& !possiblyMultilineComment) is for cases like test 6
+                        if (line.charAt(i + 1) == '*' && !possiblyMultilineComment) {
+                            //but cannot break here, because this might be a single line comment, so we need to find the closing "*/"
+                            possiblyMultilineComment = true;
+                            //increment i by one to bypass this '*'
+                            i++;
+                        } else if (line.charAt(i + 1) == '/') {
+                            //this is a single line comment, remove
+                            if (!possiblyMultilineComment) {
+                                //if this is not part of a possibly multiline comment, then we can safely break out, see test case 4
+                                break;
+                            }
+                        } else if (!possiblyMultilineComment) {
+                            sb.append(line.charAt(i));
+                        }
+                    } else if (line.charAt(i) == '*' && i + 1 < line.length() && line.charAt(i + 1) == '/' && possiblyMultilineComment) {
+                        //this is the end of the multiline comment
+                        possiblyMultilineComment = false;
+                        //increment i by one to bypass the closing '/'
+                        i++;
+                    } else if (!possiblyMultilineComment) {
+                        sb.append(line.charAt(i));
+                    }
+                }
+                if (sb.length() > 0 && !possiblyMultilineComment) {
+                    result.add(sb.toString());
+                    sb.setLength(0);
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ src/test/java/com/fishercoder/firstthousand/_722Test.java
@@ -0,0 +1,53 @@
+package com.fishercoder.firstthousand;
+
+import com.fishercoder.solutions.firstthousand._722;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class _722Test {
+    private static _722.Solution1 solution1;
+
+    @BeforeEach
+    public void setup() {
+        solution1 = new _722.Solution1();
+    }
+
+    @Test
+    public void test1() {
+        assertEquals(Arrays.asList("int main()", "{ ", "  ", "int a, b, c;", "a = b + c;", "}"), solution1.removeComments(new String[]{"/*Test program */", "int main()", "{ ", "  // variable declaration ", "int a, b, c;", "/* This is a test", "   multiline  ", "   comment for ", "   testing */", "a = b + c;", "}"}));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(Arrays.asList("ab"), solution1.removeComments(new String[]{"a/*comment", "line", "more_comment*/b"}));
+    }
+
+    @Test
+    public void test3() {
+        assertEquals(Arrays.asList("struct Node{", "    ", "    int size;", "    int val;", "};"),
+                solution1.removeComments(new String[]{"struct Node{", "    /*/ declare members;/**/", "    int size;", "    /**/int val;", "};"}));
+    }
+
+    @Test
+    public void test4() {
+        assertEquals(Arrays.asList("main() {", "   double s = 33;", "   cout << s;", "}"),
+                solution1.removeComments(new String[]{"main() {", "/* here is commments", "  // still comments */", "   double s = 33;", "   cout << s;", "}"}));
+    }
+
+    @Test
+    public void test5() {
+        assertEquals(Arrays.asList("void func(int k) {", "   k = k*2/4;", "   k = k/2;*/", "}"),
+                solution1.removeComments(new String[]{"void func(int k) {", "// this function does nothing /*", "   k = k*2/4;", "   k = k/2;*/", "}"}));
+    }
+
+    @Test
+    public void test6() {
+        assertEquals(Arrays.asList("a", "blank", "df"),
+                solution1.removeComments(new String[]{"a//*b/*/c", "blank", "d/*/e/*/f"}));
+    }
+
+}
