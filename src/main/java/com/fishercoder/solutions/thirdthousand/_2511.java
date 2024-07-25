1458f2dfa53ca1aeb58e1eed2767d35e07587c33
Fisher Coder <fishercoder@gmail.com>
2024-07-25 08:48:11 -0700

    add 2511

---
+++ src/main/java/com/fishercoder/solutions/thirdthousand/_2511.java
@@ -0,0 +1,22 @@
+package com.fishercoder.solutions.thirdthousand;
+
+public class _2511 {
+    public static class Solution1 {
+        public int captureForts(int[] forts) {
+            int max = 0;
+            for (int i = 0; i < forts.length; i++) {
+                if (forts[i] == 1 || forts[i] == -1) {
+                    for (int j = i + 1; j < forts.length; j++) {
+                        if ((forts[i] == 1 && forts[j] == -1) || (forts[i] == -1 && forts[j] == 1)) {
+                            max = Math.max(max, j - i - 1);
+                            break;
+                        } else if ((forts[j] == 1 && forts[i] == 1) || forts[j] == -1 && forts[i] == -1) {
+                            break;
+                        }
+                    }
+                }
+            }
+            return max;
+        }
+    }
+}
---
+++ src/test/java/com/fishercoder/thirdthousand/_2511Test.java
@@ -0,0 +1,21 @@
+package com.fishercoder.thirdthousand;
+
+import com.fishercoder.solutions.thirdthousand._2511;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class _2511Test {
+    private static _2511.Solution1 solution1;
+
+    @BeforeEach
+    public void setup() {
+        solution1 = new _2511.Solution1();
+    }
+
+    @Test
+    public void test1() {
+        assertEquals(4, solution1.captureForts(new int[]{1, 0, 0, -1, 0, 0, 0, 0, 1}));
+    }
+}
