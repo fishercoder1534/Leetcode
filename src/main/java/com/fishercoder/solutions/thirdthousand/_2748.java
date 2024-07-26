26c71e1b0ff6add9eb78bc540d9014aa3db1efc4
Fisher Coder <fishercoder@gmail.com>
2024-07-26 10:48:49 -0700

    add 2748

---
+++ src/main/java/com/fishercoder/solutions/thirdthousand/_2748.java
@@ -0,0 +1,26 @@
+package com.fishercoder.solutions.thirdthousand;
+
+public class _2748 {
+    public static class Solution1 {
+        public int countBeautifulPairs(int[] nums) {
+            int pairs = 0;
+            for (int i = 0; i < nums.length - 1; i++) {
+                for (int j = i + 1; j < nums.length; j++) {
+                    String iStr = String.valueOf(nums[i]);
+                    String jStr = String.valueOf(nums[j]);
+                    if (gcd(Integer.parseInt(iStr.charAt(0) + ""), Integer.parseInt(jStr.charAt(jStr.length() - 1) + "")) == 1) {
+                        pairs++;
+                    }
+                }
+            }
+            return pairs;
+        }
+
+        private int gcd(int a, int b) {
+            if (b == 0) {
+                return a;
+            }
+            return gcd(b, a % b);
+        }
+    }
+}
---
+++ src/test/java/com/fishercoder/thirdthousand/_2748Test.java
@@ -0,0 +1,22 @@
+package com.fishercoder.thirdthousand;
+
+import com.fishercoder.solutions.thirdthousand._2748;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class _2748Test {
+    private static _2748.Solution1 solution1;
+
+    @BeforeEach
+    public void setup() {
+        solution1 = new _2748.Solution1();
+    }
+
+    @Test
+    public void test1() {
+        assertEquals(2, solution1.countBeautifulPairs(new int[]{11, 21, 12}));
+    }
+
+}
