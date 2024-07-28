9b621384c53f1dfe0e0587d058d2eb5914af3b80
Fisher Coder <fishercoder@gmail.com>
2024-07-28 10:07:10 -0700

    update 204

--- src/main/java/com/fishercoder/solutions/firstthousand/_204.java
+++ src/main/java/com/fishercoder/solutions/firstthousand/_204.java
@@ -2,14 +2,20 @@ package com.fishercoder.solutions.firstthousand;
 
 public class _204 {
     public static class Solution1 {
+        /**
+         * Reference: https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+         * This is an ancient algorithm to efficiently count the number of primes up to a given point:
+         * it does so iteratively by marking composite (i.e. not prime) the multiples of each prime,
+         * starting from 2, then all remaining ones are prime.
+         */
         public int countPrimes(int n) {
-            boolean[] notPrime = new boolean[n];
+            boolean[] notPrimes = new boolean[n];
             int count = 0;
             for (int i = 2; i < n; i++) {
-                if (!notPrime[i]) {
+                if (!notPrimes[i]) {
                     count++;
                     for (int j = 2; i * j < n; j++) {
-                        notPrime[i * j] = true;
+                        notPrimes[i * j] = true;
                     }
                 }
             }
