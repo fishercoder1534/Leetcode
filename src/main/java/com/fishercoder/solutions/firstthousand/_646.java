6b26ebab5015d3ef6dd62ae5f52628d44300b5b8
Fisher Coder <fishercoder@gmail.com>
2024-07-29 08:11:14 -0700

    update 646

--- src/main/java/com/fishercoder/solutions/firstthousand/_646.java
+++ src/main/java/com/fishercoder/solutions/firstthousand/_646.java
@@ -3,25 +3,25 @@ package com.fishercoder.solutions.firstthousand;
 import java.util.Arrays;
 
 public class _646 {
+    /**
+     * Although this problem could be solved using DP, greedy is more efficient in both time and space complexity.
+     */
     public static class Solution1 {
         /**
-         * credit: https://discuss.leetcode.com/topic/96804/java-o-nlog-n-time-o-1-space
+         * credit: https://leetcode.com/problems/maximum-length-of-pair-chain/editorial/
          */
         public int findLongestChain(int[][] pairs) {
-            Arrays.sort(pairs, (o1, o2) -> o1[1] - o2[1]);
-            int result = 0;
-            int n = pairs.length;
-            int i = -1;
-            while (++i < n) {
-                result++;
-                int curEnd = pairs[i][1];
-                while (i + 1 < n && pairs[i + 1][0] <= curEnd) {
-                    /**This means, we'll keep incrementing i until pairs[i+1][0] is
-                     * exactly greater than curEnd.*/
-                    i++;
+            //sort by the second element
+            Arrays.sort(pairs, (a, b) -> a[1] - b[1]);
+            int ans = 0;
+            int prev = Integer.MIN_VALUE;
+            for (int[] pair : pairs) {
+                if (pair[0] > prev) {
+                    ans++;
+                    prev = pair[1];
                 }
             }
-            return result;
+            return ans;
         }
     }
 
@@ -30,7 +30,7 @@ public class _646 {
          * credit: https://leetcode.com/problems/maximum-length-of-pair-chain/discuss/105623/Java-Very-Simple-without-DP
          */
         public int findLongestChain(int[][] pairs) {
-            //sort by pair[0]
+            //sort by the first element
             Arrays.sort(pairs, (a, b) -> a[0] - b[0]);
             int len = 0;
             int pre = Integer.MIN_VALUE;
