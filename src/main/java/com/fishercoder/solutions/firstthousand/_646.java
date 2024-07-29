3b9cbafd200e6f79b7b33576b36f0c7a6edb3132
Fisher Coder <fishercoder@gmail.com>
2024-07-29 08:06:20 -0700

    update 646

--- src/main/java/com/fishercoder/solutions/firstthousand/_646.java
+++ src/main/java/com/fishercoder/solutions/firstthousand/_646.java
@@ -3,7 +3,6 @@ package com.fishercoder.solutions.firstthousand;
 import java.util.Arrays;
 
 public class _646 {
-
     public static class Solution1 {
         /**
          * credit: https://discuss.leetcode.com/topic/96804/java-o-nlog-n-time-o-1-space
@@ -31,16 +30,21 @@ public class _646 {
          * credit: https://leetcode.com/problems/maximum-length-of-pair-chain/discuss/105623/Java-Very-Simple-without-DP
          */
         public int findLongestChain(int[][] pairs) {
+            //sort by pair[0]
             Arrays.sort(pairs, (a, b) -> a[0] - b[0]);
             int len = 0;
             int pre = Integer.MIN_VALUE;
             for (int[] pair : pairs) {
                 if (pair[0] > pre) {
-                    //not overlap
+                    //no overlap
                     len++;
+                    //so we need to update the previous number to be the end of current pair: pair[1]
                     pre = pair[1];
                 } else if (pair[1] < pre) {
                     //overlap but with a smaller second number
+                    //since we want to find the maximum possible chain, so we update pre to be this smaller number
+                    //this means we decided to adopt this pair to be in this chain and give up the previous one
+                    //this logic can be seen clearly in test3 for this class
                     pre = pair[1];
                 }
             }
--- src/test/java/com/fishercoder/firstthousand/_646Test.java
+++ src/test/java/com/fishercoder/firstthousand/_646Test.java
@@ -1,19 +1,22 @@
 package com.fishercoder.firstthousand;
 
+import com.fishercoder.common.utils.CommonUtils;
 import com.fishercoder.solutions.firstthousand._646;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class _646Test {
 
     private static _646.Solution1 solution1;
+    private static _646.Solution2 solution2;
     private static int[][] pairs;
 
-    @BeforeClass
-    public static void setup() {
+    @BeforeEach
+    public void setup() {
         solution1 = new _646.Solution1();
+        solution2 = new _646.Solution2();
     }
 
     @Test
@@ -25,6 +28,7 @@ public class _646Test {
                 {3, 4}
         };
         assertEquals(3, solution1.findLongestChain(pairs));
+        assertEquals(3, solution2.findLongestChain(pairs));
     }
 
     @Test
@@ -40,6 +44,14 @@ public class _646Test {
                 {2, 10}
         };
         assertEquals(2, solution1.findLongestChain(pairs));
+        assertEquals(2, solution2.findLongestChain(pairs));
+    }
+
+    @Test
+    public void test3() {
+        pairs = CommonUtils.convertLeetCodeIrregularLengths2DArrayInputIntoJavaArray("[-6,9],[1,6],[8,10],[-1,4],[-6,-2],[-9,8],[-5,3],[0,3]");
+        assertEquals(3, solution1.findLongestChain(pairs));
+        assertEquals(3, solution2.findLongestChain(pairs));
     }
 
 }
