91888f7485efd49a2bd6ede5b111e1a5dfb477db
Fisher Coder <fishercoder@gmail.com>
2024-08-01 07:21:30 -0700

    update 1062

--- src/main/java/com/fishercoder/solutions/secondthousand/_1062.java
+++ src/main/java/com/fishercoder/solutions/secondthousand/_1062.java
@@ -6,15 +6,17 @@ import java.util.Set;
 public class _1062 {
     public static class Solution1 {
         /**
-         * My completely original, although brute-force solution, on 1/20/2022.
+         * My completely original solution, although kind of brute-force, on 1/20/2022.
+         * Two pointer technique:
+         * j starts from the right, i starts from the left,
+         * as soon as we are able to find a repeated substring, we return.
          */
         public int longestRepeatingSubstring(String s) {
             Set<String> seen = new HashSet<>();
             for (int j = s.length() - 1; j > 0; j--) {
-                int len = j;
-                for (int i = 0; i <= s.length() - j; i++) {
-                    if (!seen.add(s.substring(i, i + len))) {
-                        return len;
+                for (int i = 0; i + j <= s.length(); i++) {
+                    if (!seen.add(s.substring(i, i + j))) {
+                        return j;
                     }
                 }
                 seen.clear();
