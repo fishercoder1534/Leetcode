1af29b420391218bf6e0bd07acb6d81758bfe2f2
Fisher Coder <fishercoder@gmail.com>
2024-08-01 10:09:32 -0700

    add 3063

---
+++ src/main/java/com/fishercoder/solutions/fourththousand/_3063.java
@@ -0,0 +1,25 @@
+package com.fishercoder.solutions.fourththousand;
+
+import com.fishercoder.common.classes.ListNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class _3063 {
+    public static class Solution1 {
+        public ListNode frequenciesOfElements(ListNode head) {
+            Map<Integer, Integer> map = new HashMap<>();
+            while (head != null) {
+                map.put(head.val, map.getOrDefault(head.val, 0) + 1);
+                head = head.next;
+            }
+            ListNode pre = new ListNode(-1);
+            ListNode tmp = pre;
+            for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                tmp.next = new ListNode(entry.getValue());
+                tmp = tmp.next;
+            }
+            return pre.next;
+        }
+    }
+}
