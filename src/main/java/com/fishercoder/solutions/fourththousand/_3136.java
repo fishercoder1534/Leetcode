b3ada5161225a527388a185181caa5f2dea57fab
Fisher Coder <fishercoder@gmail.com>
2024-08-01 09:21:57 -0700

    add 3136

---
+++ src/main/java/com/fishercoder/solutions/fourththousand/_3136.java
@@ -0,0 +1,28 @@
+package com.fishercoder.solutions.fourththousand;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class _3136 {
+    public static class Solution1 {
+        public boolean isValid(String word) {
+            if (word.length() < 3) {
+                return false;
+            }
+            Set<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
+            boolean containsVowel = false;
+            boolean containsConsonant = false;
+            for (char c : word.toCharArray()) {
+                if (vowels.contains(c)) {
+                    containsVowel = true;
+                } else if (Character.isAlphabetic(c)) {
+                    containsConsonant = true;
+                } else if (!Character.isDigit(c)) {
+                    return false;
+                }
+            }
+            return containsVowel && containsConsonant;
+        }
+    }
+}
