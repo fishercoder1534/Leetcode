aec0cb3d52b5322b7a1105df7f78ab9b093562ac
Fisher Coder <fishercoder@gmail.com>
2024-08-01 10:26:26 -0700

    add 3024

---
+++ src/main/java/com/fishercoder/solutions/fourththousand/_3024.java
@@ -0,0 +1,25 @@
+package com.fishercoder.solutions.fourththousand;
+
+public class _3024 {
+    public static class Solution1 {
+        public String triangleType(int[] nums) {
+            if (nums[0] == nums[1] && nums[1] == nums[2]) {
+                return "equilateral";
+            } else {
+                if (!validTriangle(nums)) {
+                    return "none";
+                } else if (nums[0] == nums[1] || nums[0] == nums[2] || nums[1] == nums[2]) {
+                    return "isosceles";
+                } else {
+                    return "scalene";
+                }
+            }
+        }
+
+        private boolean validTriangle(int[] nums) {
+            return nums[0] + nums[1] > nums[2] &&
+                    nums[1] + nums[2] > nums[0] &&
+                    nums[0] + nums[2] > nums[1];
+        }
+    }
+}
