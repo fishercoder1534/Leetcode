088ebc7123bca404563bef5ff023b94109de924d
Fisher Coder <fishercoder@gmail.com>
2024-08-02 16:07:16 -0700

    add 519

---
+++ src/main/java/com/fishercoder/solutions/firstthousand/_519.java
@@ -0,0 +1,36 @@
+package com.fishercoder.solutions.firstthousand;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class _519 {
+    public static class Solution {
+
+        private int m;
+        private int n;
+        private Set<Integer> flipped;
+        private Random random;
+
+        public Solution(int m, int n) {
+            this.m = m;
+            this.n = n;
+            this.random = new Random();
+            this.flipped = new HashSet<>();
+        }
+
+        public int[] flip() {
+            int i = random.nextInt(m);
+            int j = random.nextInt(n);
+            while (!flipped.add(i * n + j)) {
+                i = random.nextInt(m);
+                j = random.nextInt(n);
+            }
+            return new int[]{i, j};
+        }
+
+        public void reset() {
+            this.flipped = new HashSet<>();
+        }
+    }
+}
---
+++ src/test/java/com/fishercoder/firstthousand/_519Test.java
@@ -0,0 +1,21 @@
+package com.fishercoder.firstthousand;
+
+import com.fishercoder.common.utils.CommonUtils;
+import com.fishercoder.solutions.firstthousand._519;
+import org.junit.jupiter.api.Test;
+
+public class _519Test {
+
+    private static _519.Solution solution1;
+
+    @Test
+    public void test1() {
+        solution1 = new _519.Solution(3, 1);
+        CommonUtils.printArray(solution1.flip());
+        CommonUtils.printArray(solution1.flip());
+        CommonUtils.printArray(solution1.flip());
+        solution1.reset();
+        CommonUtils.printArray(solution1.flip());
+    }
+
+}
