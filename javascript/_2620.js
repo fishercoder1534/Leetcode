c7bcbc23190923e125e042cc1a7113f91e9e7ce9
Fisher Coder <fishercoder@gmail.com>
2024-08-02 08:00:59 -0700

    add 2620

---
+++ javascript/_2620.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function (n) {
+    let counter = n;
+
+    return function () {
+        return n++;
+    };
+};
+
+/**
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
