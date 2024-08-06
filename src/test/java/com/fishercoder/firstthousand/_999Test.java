d9e659894fcba0a84ab408ddaf509ec72ca2afd9
Fisher Coder <fishercoder@gmail.com>
2024-08-06 05:40:29 -0700

    update 999 test

--- src/test/java/com/fishercoder/firstthousand/_999Test.java
+++ src/test/java/com/fishercoder/firstthousand/_999Test.java
@@ -1,62 +1,62 @@
 package com.fishercoder.firstthousand;
 
 import com.fishercoder.solutions.firstthousand._999;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class _999Test {
-  private static _999.Solution1 solution1;
-  private static char[][] board;
+    private static _999.Solution1 solution1;
+    private static char[][] board;
 
-  @BeforeClass
-  public static void setup() {
-    solution1 = new _999.Solution1();
-  }
+    @BeforeEach
+    public void setup() {
+        solution1 = new _999.Solution1();
+    }
 
-  @Test
-  public void test1() {
-    board = new char[][] {
-        {'.', '.', '.', '.', '.', '.', '.', '.'},
-        {'.', '.', '.', 'p', '.', '.', '.', '.'},
-        {'.', '.', '.', 'R', '.', '.', '.', 'p'},
-        {'.', '.', '.', '.', '.', '.', '.', '.'},
-        {'.', '.', '.', '.', '.', '.', '.', '.'},
-        {'.', '.', '.', 'p', '.', '.', '.', '.'},
-        {'.', '.', '.', '.', '.', '.', '.', '.'},
-        {'.', '.', '.', '.', '.', '.', '.', '.'},
-    };
-    assertEquals(3, solution1.numRookCaptures(board));
-  }
+    @Test
+    public void test1() {
+        board = new char[][]{
+                {'.', '.', '.', '.', '.', '.', '.', '.'},
+                {'.', '.', '.', 'p', '.', '.', '.', '.'},
+                {'.', '.', '.', 'R', '.', '.', '.', 'p'},
+                {'.', '.', '.', '.', '.', '.', '.', '.'},
+                {'.', '.', '.', '.', '.', '.', '.', '.'},
+                {'.', '.', '.', 'p', '.', '.', '.', '.'},
+                {'.', '.', '.', '.', '.', '.', '.', '.'},
+                {'.', '.', '.', '.', '.', '.', '.', '.'},
+        };
+        assertEquals(3, solution1.numRookCaptures(board));
+    }
 
-  @Test
-  public void test2() {
-    board = new char[][] {
-        {'.', '.', '.', '.', '.', '.', '.', '.'},
-        {'.', 'p', 'p', 'p', 'p', 'p', '.', '.'},
-        {'.', 'p', 'p', 'B', 'p', 'p', '.', '.'},
-        {'.', 'p', 'B', 'R', 'B', 'p', '.', '.'},
-        {'.', 'p', 'p', 'B', 'p', 'p', '.', '.'},
-        {'.', 'p', 'p', 'p', 'p', 'p', '.', '.'},
-        {'.', '.', '.', '.', '.', '.', '.', '.'},
-        {'.', '.', '.', '.', '.', '.', '.', '.'},
-    };
-    assertEquals(0, solution1.numRookCaptures(board));
-  }
+    @Test
+    public void test2() {
+        board = new char[][]{
+                {'.', '.', '.', '.', '.', '.', '.', '.'},
+                {'.', 'p', 'p', 'p', 'p', 'p', '.', '.'},
+                {'.', 'p', 'p', 'B', 'p', 'p', '.', '.'},
+                {'.', 'p', 'B', 'R', 'B', 'p', '.', '.'},
+                {'.', 'p', 'p', 'B', 'p', 'p', '.', '.'},
+                {'.', 'p', 'p', 'p', 'p', 'p', '.', '.'},
+                {'.', '.', '.', '.', '.', '.', '.', '.'},
+                {'.', '.', '.', '.', '.', '.', '.', '.'},
+        };
+        assertEquals(0, solution1.numRookCaptures(board));
+    }
 
-  @Test
-  public void test3() {
-    board = new char[][] {
-        {'.', '.', '.', '.', '.', '.', '.', '.'},
-        {'.', '.', '.', 'p', '.', '.', '.', '.'},
-        {'.', '.', '.', 'p', '.', '.', '.', 'p'},
-        {'p', 'p', '.', 'R', '.', 'p', 'B', '.'},
-        {'.', '.', '.', '.', '.', '.', '.', '.'},
-        {'.', '.', '.', 'B', '.', '.', '.', '.'},
-        {'.', '.', '.', 'p', '.', '.', '.', '.'},
-        {'.', '.', '.', '.', '.', '.', '.', '.'},
-    };
-    assertEquals(3, solution1.numRookCaptures(board));
-  }
+    @Test
+    public void test3() {
+        board = new char[][]{
+                {'.', '.', '.', '.', '.', '.', '.', '.'},
+                {'.', '.', '.', 'p', '.', '.', '.', '.'},
+                {'.', '.', '.', 'p', '.', '.', '.', 'p'},
+                {'p', 'p', '.', 'R', '.', 'p', 'B', '.'},
+                {'.', '.', '.', '.', '.', '.', '.', '.'},
+                {'.', '.', '.', 'B', '.', '.', '.', '.'},
+                {'.', '.', '.', 'p', '.', '.', '.', '.'},
+                {'.', '.', '.', '.', '.', '.', '.', '.'},
+        };
+        assertEquals(3, solution1.numRookCaptures(board));
+    }
 }
