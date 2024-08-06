051f2b65d35752b98a94dff390f9a94c0b092d2f
Fisher Coder <fishercoder@gmail.com>
2024-08-06 05:41:56 -0700

    update 1985 test

--- src/test/java/com/fishercoder/secondthousand/_1985Test.java
+++ src/test/java/com/fishercoder/secondthousand/_1985Test.java
@@ -1,27 +1,25 @@
 package com.fishercoder.secondthousand;
 
 import com.fishercoder.solutions.secondthousand._1985;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class _1985Test {
     private static _1985.Solution1 solution1;
 
-    @BeforeClass
-    public static void setup() {
+    @BeforeEach
+    public void setup() {
         solution1 = new _1985.Solution1();
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void test1() {
-        assertEquals("3", solution1.kthLargestNumber(new String[]{"3", "6", "7", "10"}, 4));
+        Assertions.assertEquals("3", solution1.kthLargestNumber(new String[]{"3", "6", "7", "10"}, 4));
     }
 
     @Test
     public void test2() {
-        assertEquals("6", solution1.kthLargestNumber(new String[]{"3", "6", "7", "10", "8", "1", "5"}, 4));
+        Assertions.assertEquals("2", solution1.kthLargestNumber(new String[]{"2", "21", "12", "1"}, 3));
     }
-
 }
