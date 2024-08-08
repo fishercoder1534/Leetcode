bce197311bd65293b94ddf2bfb6817068476c684
Fisher Coder <fishercoder@gmail.com>
2024-08-08 08:20:22 -0700

    update 351 test

---
+++ src/test/java/com/fishercoder/firstthousand/_351Test.java
@@ -0,0 +1,29 @@
+package com.fishercoder.firstthousand;
+
+import com.fishercoder.solutions.firstthousand._985;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class _351Test {
+    private static _985.Solution1 solution1;
+    private static int[] expected;
+    private static int[] actual;
+    private static int[] A;
+    private static int[][] queries;
+
+    @BeforeEach
+    public void setup() {
+        solution1 = new _985.Solution1();
+    }
+
+    @Test
+    public void test1() {
+        A = new int[]{1, 2, 3, 4};
+        queries = new int[][]{{1, 0}, {-3, 1}, {-4, 0}, {2, 3}};
+        expected = new int[]{8, 6, 2, 4};
+        actual = solution1.sumEvenAfterQueries(A, queries);
+        assertArrayEquals(expected, actual);
+    }
+}
