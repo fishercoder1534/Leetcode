66802d0c79681ba44429dcd5411c64445d34695d
Valentyn Kolesnikov <javadev75@gmail.com>
2024-08-18 10:18:03 +0300

    Corrected line endings from CRLF to LF

--- src/test/java/com/fishercoder/fourththousand/_3258Test.java
+++ src/test/java/com/fishercoder/fourththousand/_3258Test.java
@@ -1,10 +1,10 @@
 package com.fishercoder.fourththousand;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import com.fishercoder.solutions.fourththousand._3258;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class _3258Test {
     private _3258.Solution1 solution1;
