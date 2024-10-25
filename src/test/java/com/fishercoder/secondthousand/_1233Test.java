ed6cf34b7851f14cfef7ed6903e686a760a8edf5
Fisher Coder <fishercoder@gmail.com>
2024-10-25 10:02:15 -0700

    [LEET-1233] add 1233

--- src/test/java/com/fishercoder/secondthousand/_1233Test.java
+++ src/test/java/com/fishercoder/secondthousand/_1233Test.java
@@ -1,12 +1,11 @@
 package com.fishercoder.secondthousand;
 
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
 import com.fishercoder.solutions.secondthousand._1233;
+import java.util.ArrayList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 public class _1233Test {
     private _1233.Solution1 solution1;
@@ -19,7 +18,7 @@ public class _1233Test {
 
     @Test
     public void test1() {
-        folder = new String[]{"/a", "/a/b", "/c/d", "/c/d/e", "/c/f"};
+        folder = new String[] {"/a", "/a/b", "/c/d", "/c/d/e", "/c/f"};
         ArrayList expected = new ArrayList();
         expected.add("/a");
         expected.add("/c/d");
@@ -29,7 +28,7 @@ public class _1233Test {
 
     @Test
     public void test2() {
-        folder = new String[]{"/a", "/a/b/c", "/a/b/d"};
+        folder = new String[] {"/a", "/a/b/c", "/a/b/d"};
         ArrayList expected = new ArrayList();
         expected.add("/a");
         assertThat(expected).hasSameElementsAs(solution1.removeSubfolders(folder));
