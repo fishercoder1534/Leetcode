a87e53d06609b81c2992bff46cd1416a06c76a25
Fisher Coder <fishercoder@gmail.com>
2024-11-10 07:48:23 -0800

    [LEET-3349] add 3349

--- src/test/java/com/fishercoder/fourththousand/_3349Test.java
+++ src/test/java/com/fishercoder/fourththousand/_3349Test.java
@@ -1,13 +1,12 @@
 package com.fishercoder.fourththousand;
 
-import com.fishercoder.solutions.fourththousand._3349;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import java.util.Arrays;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.fishercoder.solutions.fourththousand._3349;
+import java.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class _3349Test {
     private _3349.Solution1 solution1;
@@ -19,17 +18,18 @@ public class _3349Test {
 
     @Test
     public void test1() {
-        assertTrue(solution1.hasIncreasingSubarrays(Arrays.asList(2, 5, 7, 8, 9, 2, 3, 4, 3, 1), 3));
+        assertTrue(
+                solution1.hasIncreasingSubarrays(Arrays.asList(2, 5, 7, 8, 9, 2, 3, 4, 3, 1), 3));
     }
 
     @Test
     public void test2() {
-        assertFalse(solution1.hasIncreasingSubarrays(Arrays.asList(1, 2, 3, 4, 4, 4, 4, 5, 6, 7), 5));
+        assertFalse(
+                solution1.hasIncreasingSubarrays(Arrays.asList(1, 2, 3, 4, 4, 4, 4, 5, 6, 7), 5));
     }
 
     @Test
     public void test3() {
         assertTrue(solution1.hasIncreasingSubarrays(Arrays.asList(5, 8, -2, -1), 2));
     }
-
 }
