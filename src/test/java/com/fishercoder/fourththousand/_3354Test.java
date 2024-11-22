7f4897d3f375bca70c7b297b7171f29079fda0c9
Fisher Coder <fishercoder@gmail.com>
2024-11-22 08:12:27 -0800

    [LEET-3354] fix build

--- src/test/java/com/fishercoder/fourththousand/_3354Test.java
+++ src/test/java/com/fishercoder/fourththousand/_3354Test.java
@@ -1,10 +1,10 @@
 package com.fishercoder.fourththousand;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import com.fishercoder.solutions.fourththousand._3354;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class _3354Test {
     private _3354.Solution1 solution1;
@@ -16,11 +16,12 @@ public class _3354Test {
 
     @Test
     public void test1() {
-        assertEquals(2, solution1.countValidSelections(new int[]{1, 0, 2, 0, 3}));
+        assertEquals(2, solution1.countValidSelections(new int[] {1, 0, 2, 0, 3}));
     }
 
     @Test
     public void test2() {
-        assertEquals(3, solution1.countValidSelections(new int[]{16, 13, 10, 0, 0, 0, 10, 6, 7, 8, 7}));
+        assertEquals(
+                3, solution1.countValidSelections(new int[] {16, 13, 10, 0, 0, 0, 10, 6, 7, 8, 7}));
     }
 }
