efa91d4dc97a874d3d4e1f2039682f82d1d69462
Fisher Coder <fishercoder@gmail.com>
2025-03-03 13:42:24 -0800

    [LEET-3471] add 3471 test

--- src/test/java/com/fishercoder/fourththousand/_3471Test.java
+++ src/test/java/com/fishercoder/fourththousand/_3471Test.java
@@ -1,10 +1,10 @@
 package com.fishercoder.fourththousand;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import com.fishercoder.solutions.fourththousand._3471;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class _3471Test {
     private _3471.Solution1 solution1;
@@ -16,11 +16,11 @@ public class _3471Test {
 
     @Test
     public void test1() {
-        assertEquals(7, solution1.largestInteger(new int[]{3, 9, 2, 1, 7}, 3));
+        assertEquals(7, solution1.largestInteger(new int[] {3, 9, 2, 1, 7}, 3));
     }
 
     @Test
     public void test2() {
-        assertEquals(0, solution1.largestInteger(new int[]{0, 0}, 2));
+        assertEquals(0, solution1.largestInteger(new int[] {0, 0}, 2));
     }
 }
