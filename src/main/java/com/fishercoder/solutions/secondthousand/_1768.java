de1c3025964edd842d4173b893a43129d9de3f8a
fishercoder <fishercoder@gmail.com>
2026-06-22 06:13:59 -0700

    update 1769 again

--- src/main/java/com/fishercoder/solutions/secondthousand/_1768.java
+++ src/main/java/com/fishercoder/solutions/secondthousand/_1768.java
@@ -1,5 +1,6 @@
 package com.fishercoder.solutions.secondthousand;
 
+
 public class _1768 {
     public static class Solution1 {
         public String mergeAlternately(String word1, String word2) {
