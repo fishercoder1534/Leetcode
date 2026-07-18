51f7710e21c089292c9ea9f92800fce8507f38dd
fishercoder1534 <20746576+fishercoder1534@users.noreply.github.com>
2026-07-18 08:54:26 -0700

    update 1456

--- src/main/java/com/fishercoder/solutions/secondthousand/_1456.java
+++ src/main/java/com/fishercoder/solutions/secondthousand/_1456.java
@@ -1,44 +1,34 @@
 package com.fishercoder.solutions.secondthousand;
 
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 public class _1456 {
     public static class Solution1 {
         public int maxVowels(String s, int k) {
-            Map<Character, Integer> vowels = new HashMap<>();
-            Set<Character> set = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
-            int left = 0;
-            for (; left < k; left++) {
-                if (set.contains(s.charAt(left))) {
-                    vowels.put(s.charAt(left), vowels.getOrDefault(s.charAt(left), 0) + 1);
+            Set<Character> set = new HashSet<>();
+            set.add('a');
+            set.add('e');
+            set.add('i');
+            set.add('o');
+            set.add('u');
+            int max = Integer.MIN_VALUE;
+            int i = 0;
+            int vowels = 0;
+            for (; i < k; i++) {
+                if (set.contains(s.charAt(i))) {
+                    vowels++;
                 }
             }
-            int max = 0;
-            for (char c : vowels.keySet()) {
-                max += vowels.get(c);
-            }
-            int right = left;
-            left = 0;
-            for (; right < s.length(); right++, left++) {
-                char leftChar = s.charAt(left);
-                if (set.contains(leftChar)
-                        && vowels.containsKey(leftChar)
-                        && vowels.get(leftChar) > 0) {
-                    vowels.put(leftChar, vowels.get(leftChar) - 1);
+            max = Math.max(max, vowels);
+            for (; i < s.length(); i++) {
+                if (set.contains(s.charAt(i))) {
+                    vowels++;
                 }
-                char rightChar = s.charAt(right);
-                if (set.contains(rightChar)) {
-                    vowels.put(rightChar, vowels.getOrDefault(rightChar, 0) + 1);
+                if (set.contains(s.charAt(i - k))) {
+                    vowels--;
                 }
-                int thisMax = 0;
-                for (char c : vowels.keySet()) {
-                    thisMax += vowels.get(c);
-                }
-                max = Math.max(max, thisMax);
+                max = Math.max(max, vowels);
             }
             return max;
         }
