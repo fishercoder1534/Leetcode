1ca526a46a8f8ab0e73089147b38eb67bded043e
fishercoder1534 <20746576+fishercoder1534@users.noreply.github.com>
2026-07-21 19:10:37 -0700

    update 1493

--- src/main/java/com/fishercoder/solutions/secondthousand/_1493.java
+++ src/main/java/com/fishercoder/solutions/secondthousand/_1493.java
@@ -1,42 +1,37 @@
 package com.fishercoder.solutions.secondthousand;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class _1493 {
     public static class Solution1 {
         public int longestSubarray(int[] nums) {
-            List<int[]> brackets = new ArrayList<>();
+            boolean hasZero = false;
             for (int i = 0; i < nums.length; i++) {
-                if (nums[i] == 1) {
-                    int right = i + 1;
-                    while (right < nums.length && nums[right] == 1) {
-                        right++;
+                if (nums[i] == 0) {
+                    hasZero = true;
+                    break;
+                }
+            }
+            if (!hasZero) {
+                // this is for this case: {1, 1, 1} => 2, since you have to delete one item
+                return nums.length - 1;
+            }
+
+            int zero = 0;
+            int left = 0;
+            int ans = 0;
+            // sliding window solution
+            for (int right = 0; right < nums.length; right++) {
+                if (nums[right] == 0) {
+                    zero++;
+                }
+                while (zero > 1) {
+                    if (nums[left] == 0) {
+                        zero--;
                     }
-                    if (right < nums.length && nums[right] == 1) {
-                        brackets.add(new int[] {i, right});
-                    } else {
-                        brackets.add(new int[] {i, right - 1});
-                    }
-                    i = right;
+                    left++;
                 }
+                ans = Math.max(ans, right - left + 1 - zero);
             }
-            int longest = 0;
-            for (int[] bracket : brackets) {
-                if (bracket[1] == nums.length - 1 && bracket[0] == 0) {
-                    return nums.length - 1;
-                }
-                longest = Math.max(bracket[1] - bracket[0] + 1, longest);
-            }
-            for (int i = 0; i < brackets.size() - 1; i++) {
-                int[] first = brackets.get(i);
-                int[] second = brackets.get(i + 1);
-                if (first[1] + 2 == second[0]) {
-                    int connected = (first[1] - first[0] + 1) + (second[1] - second[0] + 1);
-                    longest = Math.max(longest, connected);
-                }
-            }
-            return longest;
+            return ans;
         }
     }
 
