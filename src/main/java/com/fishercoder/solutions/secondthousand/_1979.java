87cb82d64a7bc07dc17f026b2837d2fe315aa2c7
fishercoder1534 <20746576+fishercoder1534@users.noreply.github.com>
2026-08-03 19:19:48 -0700

    add 1979

--- src/main/java/com/fishercoder/solutions/secondthousand/_1979.java
+++ src/main/java/com/fishercoder/solutions/secondthousand/_1979.java
@@ -13,4 +13,25 @@ public class _1979 {
             return b == 0 ? a : getGcd(b, a % b);
         }
     }
+
+    public static class Solution2 {
+        public int findGCD(int[] nums) {
+            int min = Integer.MAX_VALUE;
+            int max = Integer.MIN_VALUE;
+            for (int num : nums) {
+                min = Math.min(min, num);
+                max = Math.max(max, num);
+            }
+            return gcd(min, max);
+        }
+
+        private int gcd(int x, int y) {
+            while (y != 0) {
+                int temp = y;
+                y = x % y;
+                x = temp;
+            }
+            return x;
+        }
+    }
 }
